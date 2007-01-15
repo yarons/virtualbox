79357df0e0f4b4c4ce011f577960a9af9bdbd97d
Sander van Leeuwen <noreply@oracle.com>
2007-01-15 17:44:46 +0000

    Update
    svn:sync-xref-src-repo-rev: r17469

--- src/VBox/VMM/PGM.cpp
+++ src/VBox/VMM/PGM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGM.cpp 39 2007-01-15 17:35:58Z noreply@oracle.com $ */
+/* $Id: PGM.cpp 40 2007-01-15 17:44:46Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor. (Mixing stuff here, not good?)
  */
@@ -1599,12 +1599,6 @@ static DECLCALLBACK(int) pgmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Version
         /* any memory associated with the range. */
         if (pRam->fFlags & MM_RAM_FLAGS_DYNAMIC_ALLOC)
         {
-            bool fPreAlloc = false;
-
-            rc = CFGMR3QueryBool(CFGMR3GetRoot(pVM), "PreAlloc", &fPreAlloc);
-            if (rc == VERR_CFGM_VALUE_NOT_FOUND)
-                fPreAlloc = false;
-
             for (unsigned iChunk = 0; iChunk < (pRam->cb >> PGM_DYNAMIC_CHUNK_SHIFT); iChunk++)
             {
                 uint8_t fValidChunk;
@@ -1617,9 +1611,7 @@ static DECLCALLBACK(int) pgmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Version
 
                 if (fValidChunk)
                 {
-                    /* The first chunk is always present. (all chunks are present if guest ram is preallocated) */
-                    if (    iChunk != 0 
-                        && !fPreAlloc)
+                    if (!pRam->pavHCChunkHC[iChunk])
                     {
                         rc = pgmr3PhysGrowRange(pVM, pRam->GCPhys + iChunk * PGM_DYNAMIC_CHUNK_SIZE);
                         if (VBOX_FAILURE(rc))
