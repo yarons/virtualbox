38ef85a124c616cec04429f454cc78041473419b
Sander van Leeuwen <noreply@oracle.com>
2007-02-08 10:06:49 +0000

    Added @todo
    svn:sync-xref-src-repo-rev: r18428

--- src/VBox/VMM/CPUM.cpp
+++ src/VBox/VMM/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 465 2007-01-31 15:05:57Z noreply@oracle.com $ */
+/* $Id: CPUM.cpp 767 2007-02-08 10:06:49Z noreply@oracle.com $ */
 /** @file
  * CPUM - CPU Monitor(/Manager)
  */
@@ -264,6 +264,7 @@ static int cpumR3CpuIdInit(PVM pVM)
                                        | X86_CPUID_AMD_FEATURE_EDX_CX8
                                        //| X86_CPUID_AMD_FEATURE_EDX_APIC   - set by the APIC device if present.
                                        /** @note we don't report sysenter/sysexit support due to our inability to keep the IOPL part of eflags in sync while in ring 1 (see #1757) */
+                                       /** @todo this doesn't apply to hwaccm -> we find out much later whether it's enabled or not */
                                        //| X86_CPUID_AMD_FEATURE_EDX_SEP
                                        //| X86_CPUID_AMD_FEATURE_EDX_MTRR   - not virtualized.
                                        | X86_CPUID_AMD_FEATURE_EDX_PGE
