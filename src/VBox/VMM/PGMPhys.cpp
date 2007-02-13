a6ff26e4769f7fd677d2092b777218abe02e3c0d
Sander van Leeuwen <noreply@oracle.com>
2007-02-13 13:34:23 +0000

    Use VMR3SuspendNoSave to suspend the VM when running out of memory trying to allocate more guest RAM.
    svn:sync-xref-src-repo-rev: r18578

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 853 2007-02-13 09:58:47Z noreply@oracle.com $ */
+/* $Id: PGMPhys.cpp 871 2007-02-13 13:34:23Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -391,7 +391,7 @@ int pgmr3PhysGrowRange(PVM pVM, RTGCPHYS GCPhys)
         LogRel(("pgmr3PhysGrowRange: out of memory. pause until the user resumes execution.\n"));
         VMSetRuntimeError(pVM, false, "HostMemoryLow", "Unable to allocate and lock memory. The virtual machine will be paused. Please close applications to free up memory or save and close the VM.");
  
-        rc = VMR3Suspend(pVM);
+        rc = VMR3SuspendNoSave(pVM);
         AssertRC(rc);
 
         /* Wait for resume event; will only return in that case. If the VM is stopped, the EMT thread will be destroyed. */
