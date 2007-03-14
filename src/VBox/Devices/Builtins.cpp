50e96fac41338456b6f2e5f3c20e4b6aff20f31a
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2007-03-14 16:36:04 +0000

    First go at a serial port device with I/O hooked to named pipe/local socket. Doesn't support mounting/unmounting at runtime yet.
    svn:sync-xref-src-repo-rev: r19537

--- src/VBox/Devices/Builtins.cpp
+++ src/VBox/Devices/Builtins.cpp
@@ -1,4 +1,4 @@
-/** $Id: Builtins.cpp 1458 2007-03-14 01:15:29Z knut.osmundsen@oracle.com $ */
+/** $Id: Builtins.cpp 1476 2007-03-14 16:36:04Z klaus.espenlaub@oracle.com $ */
 /** @file
  * Built-in drivers & devices (part 1)
  */
@@ -202,6 +202,13 @@ extern "C" DECLEXPORT(int) VBoxDriversRegister(PCPDMDRVREGCB pCallbacks, uint32_
     if (VBOX_FAILURE(rc))
         return rc;
 #endif
+
+    rc = pCallbacks->pfnRegister(pCallbacks, &g_DrvChar);
+    if (VBOX_FAILURE(rc))
+        return rc;
+    rc = pCallbacks->pfnRegister(pCallbacks, &g_DrvNamedPipe);
+    if (VBOX_FAILURE(rc))
+        return rc;
 
     return VINF_SUCCESS;
 }
