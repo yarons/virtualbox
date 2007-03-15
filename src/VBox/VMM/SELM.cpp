db271cc2cd3476b5bf091ad4edf5429663a4e332
Sander van Leeuwen <noreply@oracle.com>
2007-03-15 12:02:23 +0000

    Same for the SELMR3CheckTSS function
    svn:sync-xref-src-repo-rev: r19583

--- src/VBox/VMM/SELM.cpp
+++ src/VBox/VMM/SELM.cpp
@@ -1,4 +1,4 @@
-/* $Id: SELM.cpp 1508 2007-03-15 12:00:18Z noreply@oracle.com $ */
+/* $Id: SELM.cpp 1509 2007-03-15 12:02:23Z noreply@oracle.com $ */
 /** @file
  * SELM - The Selector manager.
  */
@@ -1698,10 +1698,7 @@ SELMR3DECL(bool) SELMR3CheckTSS(PVM pVM)
         if (pDesc->Gen.u1Granularity)
             cbTss = (cbTss << PAGE_SHIFT) | PAGE_OFFSET_MASK;
         cbTss++;
-        /* Don't bother with anything but the core structure. (Actually all we care for is the r0 ss.) */
-        if (cbTss > sizeof(VBOXTSS))
-            cbTss = sizeof(VBOXTSS);
-        AssertMsg((GCPtrTss >> PAGE_SHIFT) == ((GCPtrTss + cbTss - 1) >> PAGE_SHIFT),
+        AssertMsg((GCPtrTss >> PAGE_SHIFT) == ((GCPtrTss + sizeof(VBOXTSS) - 1) >> PAGE_SHIFT),
                   ("GCPtrTss=%VGv cbTss=%#x - We assume everything is inside one page!\n", GCPtrTss, cbTss));
 
         // All system GDTs are marked not present above. That explains why this check fails.
