0c50909d81fb9395fb9afac9819cbfc874dbae79
Frank Mehnert <noreply@oracle.com>
2007-03-20 21:03:10 +0000

    warnings
    svn:sync-xref-src-repo-rev: r19694

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWSVMR0.cpp 1406 2007-03-12 09:06:41Z noreply@oracle.com $ */
+/* $Id: HWSVMR0.cpp 1585 2007-03-20 21:03:10Z noreply@oracle.com $ */
 /** @file
  * HWACCM SVM - Host Context Ring 0.
  */
@@ -513,7 +513,7 @@ HWACCMR0DECL(int) SVMR0LoadGuestState(PVM pVM, CPUMCTX *pCtx)
 HWACCMR0DECL(int) SVMR0RunGuestCode(PVM pVM, CPUMCTX *pCtx)
 {
     int         rc = VINF_SUCCESS;
-    uint64_t    exitCode = SVM_EXIT_INVALID;
+    uint64_t    exitCode = (uint64_t)SVM_EXIT_INVALID;
     SVM_VMCB   *pVMCB;
     bool        fForceTLBFlush = false;
 
@@ -651,7 +651,7 @@ ResumeExecution:
     /* Reason for the VM exit */
     exitCode = pVMCB->ctrl.u64ExitCode;
 
-    if (exitCode == SVM_EXIT_INVALID)          /* Invalid guest state. */
+    if (exitCode == (uint64_t)SVM_EXIT_INVALID)          /* Invalid guest state. */
     {
         HWACCMDumpRegs(pCtx);
 #ifdef DEBUG
