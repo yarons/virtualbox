bdf939683ccd59df1fb8d24f96206d45491b5d07
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-03-30 18:30:16 +0000

    style
    svn:sync-xref-src-repo-rev: r20026

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllBth.h 1828 2007-03-30 12:52:55Z noreply@oracle.com $ */
+/* $Id: PGMAllBth.h 1839 2007-03-30 18:30:16Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox - Page Manager, Shadow+Guest Paging Template - All context code.
  *
@@ -1625,11 +1625,10 @@ PGM_BTH_DECL(int, CheckPageFault)(PVM pVM, uint32_t uErr, PSHWPDE pPdeDst, PVBOX
                  * Map shadow page table.
                  */
                 PPGMPOOLPAGE    pShwPage = pgmPoolGetPageByHCPhys(pVM, pPdeDst->u & SHW_PDE_PG_MASK);
-
                 if (pShwPage)
                 {
-                    PSHWPT          pPTDst   = (PSHWPT)PGMPOOL_PAGE_2_PTR(pVM, pShwPage);
-                    PSHWPTE         pPteDst  = &pPTDst->a[(GCPtrPage >> SHW_PT_SHIFT) & SHW_PT_MASK];
+                    PSHWPT      pPTDst   = (PSHWPT)PGMPOOL_PAGE_2_PTR(pVM, pShwPage);
+                    PSHWPTE     pPteDst  = &pPTDst->a[(GCPtrPage >> SHW_PT_SHIFT) & SHW_PT_MASK];
                     if (    pPteDst->n.u1Present    /** @todo Optimize accessed bit emulation? */
                         &&  (pPteDst->u & PGM_PTFLAGS_TRACK_DIRTY))
                     {
