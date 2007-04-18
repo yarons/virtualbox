a0b01bc5b3068f948c542e58633776e72c9f4368
Sander van Leeuwen <noreply@oracle.com>
2007-04-18 14:39:39 +0000

    Argh
    svn:sync-xref-src-repo-rev: r20514

--- src/VBox/VMM/VMMAll/SELMAll.cpp
+++ src/VBox/VMM/VMMAll/SELMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: SELMAll.cpp 2181 2007-04-18 14:27:49Z noreply@oracle.com $ */
+/* $Id: SELMAll.cpp 2183 2007-04-18 14:39:39Z noreply@oracle.com $ */
 /** @file
  * SELM All contexts.
  */
@@ -138,7 +138,7 @@ SELMDECL(int) SELMToFlatEx(PVM pVM, X86EFLAGS eflags, RTSEL Sel, RTGCPTR Addr, C
             if (    pHiddenSel
                 &&  CPUMAreHiddenSelRegsValid(pVM))
             {
-                *ppvGC = (RTGCPTR)pHiddenSel->u32Base;
+                *ppvGC = (RTGCPTR)(pHiddenSel->u32Base + Addr);
             }
             else
             {
@@ -421,7 +421,7 @@ SELMDECL(int) SELMValidateAndConvertCSAddr(PVM pVM, X86EFLAGS eflags, RTSEL SelC
             if (!CPUMAreHiddenSelRegsValid(pVM))
                 uFlat = ((RTGCUINTPTR)Addr & 0xffff) + ((RTGCUINTPTR)SelCS << 4);
             else
-                uFlat = pHiddenCSSel->u32Base;
+                uFlat = pHiddenCSSel->u32Base + Addr;
 
             *ppvFlat = (RTGCPTR)uFlat;
         }
