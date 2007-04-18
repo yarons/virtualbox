c57dd11fbf766d447d72ba969629f8b0b64cd648
Alexey Strelnikov <noreply@oracle.com>
2007-04-18 14:17:27 +0000

    more verbose assertion
    svn:sync-xref-src-repo-rev: r20511

--- src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
+++ src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
@@ -1,4 +1,4 @@
-/* $Id: IOMAllMMIO.cpp 2128 2007-04-17 12:42:00Z noreply@oracle.com $ */
+/* $Id: IOMAllMMIO.cpp 2180 2007-04-18 14:17:27Z noreply@oracle.com $ */
 /** @file
  * IOM - Input / Output Monitor - Guest Context.
  */
@@ -1302,7 +1302,7 @@ IOMDECL(int) IOMMMIOHandler(PVM pVM, RTGCUINT uErrorCode, PCPUMCTXCORE pRegFrame
     int rc = SELMValidateAndConvertCSAddr(pVM, pRegFrame->eflags, pRegFrame->ss, pRegFrame->cs, &pRegFrame->csHid, (RTGCPTR)(cpu.mode == CPUMODE_32BIT ? pRegFrame->eip : pRegFrame->eip & 0xffff), &pvCode);
     if (VBOX_FAILURE(rc))
     {
-        AssertMsgFailed(("Internal error! cs:eip=%04x:%08x\n", pRegFrame->cs, pRegFrame->eip));
+        AssertMsgFailed(("Internal error! cs:eip=%04x:%08x rc=%Vrc\n", pRegFrame->cs, pRegFrame->eip, rc));
         return VERR_IOM_MMIO_HANDLER_BOGUS_CALL;
     }
 
