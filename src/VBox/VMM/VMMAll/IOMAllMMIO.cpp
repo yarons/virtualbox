d72f6d38ac4ce2f7baccd7f36c7484d30f862c3d
Sander van Leeuwen <noreply@oracle.com>
2007-04-19 12:27:00 +0000

    Don't call the outs/ins string handler if we're only supposed to transfer a single unit.
    svn:sync-xref-src-repo-rev: r20570

--- src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
+++ src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
@@ -1,4 +1,4 @@
-/* $Id: IOMAllMMIO.cpp 2217 2007-04-19 12:00:24Z noreply@oracle.com $ */
+/* $Id: IOMAllMMIO.cpp 2220 2007-04-19 12:27:00Z noreply@oracle.com $ */
 /** @file
  * IOM - Input / Output Monitor - Guest Context.
  */
@@ -1646,16 +1646,19 @@ IOMDECL(int) IOMInterpretINSEx(PVM pVM, PCPUMCTXCORE pRegFrame, uint32_t uPort, 
     }
 
     Log(("IOM: rep ins%d port %#x count %d\n", cbTransfer * 8, uPort, cTransfers));
+    if (cTransfers > 1)
+    {
+        /* If the device supports string transfers, ask it to do as
+         * much as it wants. The rest is done with single-word transfers. */
+        const RTGCUINTREG cTransfersOrg = cTransfers;
+        rc = IOMIOPortReadString(pVM, uPort, &GCPtrDst, &cTransfers, cbTransfer);
+        AssertRC(rc); Assert(cTransfers <= cTransfersOrg);
+        pRegFrame->edi += (cTransfersOrg - cTransfers) * cbTransfer;
+    }
+
 #ifdef IN_GC
     MMGCRamRegisterTrapHandler(pVM);
 #endif
-
-    /* If the device supports string transfers, ask it to do as
-     * much as it wants. The rest is done with single-word transfers. */
-    const RTGCUINTREG cTransfersOrg = cTransfers;
-    rc = IOMIOPortReadString(pVM, uPort, &GCPtrDst, &cTransfers, cbTransfer);
-    AssertRC(rc); Assert(cTransfers <= cTransfersOrg);
-    pRegFrame->edi += (cTransfersOrg - cTransfers) * cbTransfer;
 
     while (cTransfers && rc == VINF_SUCCESS)
     {
@@ -1774,17 +1777,21 @@ IOMDECL(int) IOMInterpretOUTSEx(PVM pVM, PCPUMCTXCORE pRegFrame, uint32_t uPort,
     }
 
     Log(("IOM: rep outs%d port %#x count %d\n", cbTransfer * 8, uPort, cTransfers));
+    if (cTransfers > 1)
+    {
+        /*
+        * If the device supports string transfers, ask it to do as
+        * much as it wants. The rest is done with single-word transfers.
+        */
+        const RTGCUINTREG cTransfersOrg = cTransfers;
+        rc = IOMIOPortWriteString(pVM, uPort, &GCPtrSrc, &cTransfers, cbTransfer);
+        AssertRC(rc); Assert(cTransfers <= cTransfersOrg);
+        pRegFrame->esi += (cTransfersOrg - cTransfers) * cbTransfer;
+    }
+
 #ifdef IN_GC
     MMGCRamRegisterTrapHandler(pVM);
 #endif
-    /*
-     * If the device supports string transfers, ask it to do as
-     * much as it wants. The rest is done with single-word transfers.
-     */
-    const RTGCUINTREG cTransfersOrg = cTransfers;
-    rc = IOMIOPortWriteString(pVM, uPort, &GCPtrSrc, &cTransfers, cbTransfer);
-    AssertRC(rc); Assert(cTransfers <= cTransfersOrg);
-    pRegFrame->esi += (cTransfersOrg - cTransfers) * cbTransfer;
 
     while (cTransfers && rc == VINF_SUCCESS)
     {
