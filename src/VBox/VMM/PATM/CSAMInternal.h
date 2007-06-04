ed7be24c8af51a9872937cc64d5704e18b51021f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-06-04 12:49:31 +0000

    Added missing space after ')' in macro invocations so VCC doesn't mess up the precompiler output.
    svn:sync-xref-src-repo-rev: r21778

--- src/VBox/VMM/PATM/CSAMInternal.h
+++ src/VBox/VMM/PATM/CSAMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: CSAMInternal.h 2981 2007-06-01 16:01:28Z noreply@oracle.com $ */
+/* $Id: CSAMInternal.h 3025 2007-06-04 12:49:31Z knut.osmundsen@oracle.com $ */
 /** @file
  * CSAM - Internal header file.
  */
@@ -251,7 +251,7 @@ typedef int (VBOXCALL *PFN_CSAMR3ANALYSE)(PVM pVM, DISCPUSTATE *pCpu, GCPTRTYPE(
  * @returns             true if patched
  *
  */
-bool csamCheckGuestSpecificPatch(PVM pVM, RTGCPTR pInstrGC, HCPTRTYPE(uint8_t *)pInstrHC, uint32_t opcode);
+bool csamCheckGuestSpecificPatch(PVM pVM, RTGCPTR pInstrGC, HCPTRTYPE(uint8_t *) pInstrHC, uint32_t opcode);
 
 /**
  * Calculate the branch destination
