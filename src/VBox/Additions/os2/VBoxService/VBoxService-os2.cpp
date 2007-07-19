5adcb41aa823900acfb0b2e24ea8f37775f821b0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-07-19 19:01:52 +0000

    Double underscore cleanup.
    svn:sync-xref-src-repo-rev: r23024

--- src/VBox/Additions/os2/VBoxService/VBoxService-os2.cpp
+++ src/VBox/Additions/os2/VBoxService/VBoxService-os2.cpp
@@ -1,4 +1,4 @@
-/** $Id: VBoxService-os2.cpp 3655 2007-07-16 18:47:26Z knut.osmundsen@oracle.com $ */
+/** $Id: VBoxService-os2.cpp 3725 2007-07-19 19:01:52Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxService - Guest Additions Service Skeleton.
  */
@@ -22,11 +22,11 @@
 /*******************************************************************************
 *   Global Variables                                                           *
 *******************************************************************************/
-#ifdef __OS2__
+#ifdef RT_OS_OS2
 # define INCL_BASE
 # define INCL_ERRORS
 # include <os2.h>
-#endif 
+#endif
 #include <errno.h>
 
 #include <iprt/string.h>
