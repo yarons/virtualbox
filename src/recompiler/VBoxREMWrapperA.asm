6bb53fda87b8670dedbad5e369cff9374490695b
Sander van Leeuwen <noreply@oracle.com>
2007-08-03 09:28:11 +0000

    Updated headers
    svn:sync-xref-src-repo-rev: r23447

--- src/recompiler/VBoxREMWrapperA.asm
+++ src/recompiler/VBoxREMWrapperA.asm
@@ -1,16 +1,21 @@
-; $Id: VBoxREMWrapperA.asm 3726 2007-07-19 19:07:48Z knut.osmundsen@oracle.com $
+; $Id: VBoxREMWrapperA.asm 4018 2007-08-03 09:28:11Z noreply@oracle.com $
 ;; @file
 ;
 ; VBoxREM Wrapper, Assembly routines and wrapper Templates.
 ;
-; innotek GmbH confidential
-;
 ; Copyright (C) 2006-2007 innotek GmbH
 ;
-; Author: knut st. osmundsen <bird@innotek.de>
+; This file is part of VirtualBox Open Source Edition (OSE), as
+; available from http://www.virtualbox.org. This file is free software;
+; you can redistribute it and/or modify it under the terms of the GNU
+; General Public License as published by the Free Software Foundation,
+; in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+; distribution. VirtualBox OSE is distributed in the hope that it will
+; be useful, but WITHOUT ANY WARRANTY of any kind.
 ;
-; All Rights Reserved
-;
+; If you received this file as part of a commercial VirtualBox
+; distribution, then only the terms of your commercial VirtualBox
+; license agreement apply instead of the previous paragraph.
 ;
 
 
