1bccf95ed5a9c2d4b5b50ea63bdb02576515e64c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-08-15 19:55:25 +0000

    Changed from $ to $.
    svn:sync-xref-src-repo-rev: r23649

--- src/VBox/Runtime/r0drv/memobj-r0drv.cpp
+++ src/VBox/Runtime/r0drv/memobj-r0drv.cpp
@@ -1,4 +1,4 @@
-/* $Id: memobj-r0drv.cpp 4155 2007-08-15 19:41:26Z knut.osmundsen@oracle.com $ */
+/* $Revision: 4159 $ */
 /** @file
  * innotek Portable Runtime - Ring-0 Memory Objects, Common Code.
  */
