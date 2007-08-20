6b9095b441872281f52cca76e395f62645882626
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-08-20 20:18:41 +0000

    Version 5.1: Export RTLogPrintf and RTLogPrintfV.
    svn:sync-xref-src-repo-rev: r23756

--- src/VBox/HostDrivers/Support/SUPDRVIOC.h
+++ src/VBox/HostDrivers/Support/SUPDRVIOC.h
@@ -1,4 +1,4 @@
-/* $Revision: 4071 $ */
+/* $Revision: 4249 $ */
 /** @file
  * VirtualBox Support Driver - IOCtl definitions.
  */
@@ -174,7 +174,7 @@ typedef struct SUPDRVIOCTLDATA
 /** Current interface version.
  * The upper 16-bit is the major version, the the lower the minor version.
  * When incompatible changes are made, the upper major number has to be changed. */
-#define SUPDRVIOC_VERSION           0x00050000
+#define SUPDRVIOC_VERSION           0x00050001
 
 /** SUP_IOCTL_COOKIE Input. */
 typedef struct SUPCOOKIE_IN
