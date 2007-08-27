b77cfb75ff52430a83800f085977e42afda69562
Sander van Leeuwen <noreply@oracle.com>
2007-08-27 09:58:08 +0000

    Fixed reserved memory reservation
    svn:sync-xref-src-repo-rev: r23929

--- src/recompiler/VBoxRecompiler.c
+++ src/recompiler/VBoxRecompiler.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxRecompiler.c 4379 2007-08-27 09:08:39Z noreply@oracle.com $ */
+/* $Id: VBoxRecompiler.c 4380 2007-08-27 09:58:08Z noreply@oracle.com $ */
 /** @file
  * VBox Recompiler - QEMU.
  */
@@ -2503,7 +2503,10 @@ REMR3DECL(void) REMR3NotifyPhysRamRegister(PVM pVM, RTGCPHYS GCPhys, RTUINT cb, 
 # ifndef REM_PHYS_ADDR_IN_TLB
         uint32_t i;
 # endif
-        cpu_register_physical_memory(GCPhys, cb, GCPhys | (fFlags & MM_RAM_FLAGS_RESERVED ? IO_MEM_UNASSIGNED : 0));
+        if (fFlags & MM_RAM_FLAGS_RESERVED)
+            cpu_register_physical_memory(GCPhys, cb, IO_MEM_UNASSIGNED);
+        else
+            cpu_register_physical_memory(GCPhys, cb, GCPhys);
 
 # ifndef REM_PHYS_ADDR_IN_TLB
         AssertRelease(pVM->rem.s.cPhysRegistrations < REM_MAX_PHYS_REGISTRATIONS);
