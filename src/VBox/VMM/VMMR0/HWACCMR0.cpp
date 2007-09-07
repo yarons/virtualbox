11074e29a85c910746e8ff6010ab5b8e899f151e
Sander van Leeuwen <noreply@oracle.com>
2007-09-07 09:23:11 +0000

    Only check for vmx root mode for 64 bits hosts.
    svn:sync-xref-src-repo-rev: r24236

--- src/VBox/VMM/VMMR0/HWACCMR0.cpp
+++ src/VBox/VMM/VMMR0/HWACCMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWACCMR0.cpp 4487 2007-09-03 08:55:48Z noreply@oracle.com $ */
+/* $Id: HWACCMR0.cpp 4598 2007-09-07 09:23:11Z noreply@oracle.com $ */
 /** @file
  * HWACCM - Host Context Ring 0.
  */
@@ -135,19 +135,21 @@ HWACCMR0DECL(int) HWACCMR0Init(PVM pVM)
                         /* Set revision dword at the beginning of the structure. */
                         *(uint32_t *)pVM->hwaccm.s.vmx.pVMXON = MSR_IA32_VMX_BASIC_INFO_VMCS_ID(pVM->hwaccm.s.vmx.msr.vmx_basic_info);
 
+#if HC_ARCH_BITS == 64
                         /* Enter VMX Root Mode */
                         int rc = VMXEnable(pVM->hwaccm.s.vmx.pVMXONPhys);
                         if (VBOX_FAILURE(rc))
                         {
                             /* KVM leaves the CPU in VMX root mode. Not only is this not allowed, it will crash the host when we enter raw mode, because
-                             * (a) clearing X86_CR4_VMXE in CR4 causes a #GP
-                             * (b) turning off paging causes a #GP
+                             * (a) clearing X86_CR4_VMXE in CR4 causes a #GP    (we no longer modify this bit)
+                             * (b) turning off paging causes a #GP              (unavoidable when switching from long to 32 bits mode)
                              *
                              * They should fix their code, but until they do we simply refuse to run.
                              */
                             return VERR_VMX_IN_VMX_ROOT_MODE;
                         }
                         VMXDisable();
+#endif
                     }
                 }
                 else
