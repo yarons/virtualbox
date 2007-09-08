d36534a76a380b4c4a2606896e6f8b3565215730
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-09-08 07:00:57 +0000

    Forgot this USE_PDMTHREAD usage.
    svn:sync-xref-src-repo-rev: r24271

--- src/VBox/Devices/testcase/tstDeviceStructSizeGC.cpp
+++ src/VBox/Devices/testcase/tstDeviceStructSizeGC.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstDeviceStructSizeGC.cpp 4430 2007-08-30 06:11:55Z noreply@oracle.com $ */
+/* $Id: tstDeviceStructSizeGC.cpp 4625 2007-09-08 07:00:57Z knut.osmundsen@oracle.com $ */
 /** @file
  * tstDeviceStructSizeGC - Generate structure member and size checks from the GC perspective.
  *
@@ -352,11 +352,7 @@ int main()
     GEN_CHECK_OFF(PCNetState, ILeds);
     GEN_CHECK_OFF(PCNetState, pLedsConnector);
     GEN_CHECK_OFF(PCNetState, hSendEventSem);
-#ifdef USE_PDMTHREAD
     GEN_CHECK_OFF(PCNetState, pSendThread);
-#else
-    GEN_CHECK_OFF(PCNetState, hSendThread);
-#endif 
     GEN_CHECK_OFF(PCNetState, CritSect);
     GEN_CHECK_OFF(PCNetState, cPendingSends);
 #ifdef PCNET_NO_POLLING
