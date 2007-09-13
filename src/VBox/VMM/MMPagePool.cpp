720c1e77993dc5c8fc6f4d38bae5f8fe308e05bb
Sander van Leeuwen <noreply@oracle.com>
2007-09-13 11:36:09 +0000

    Use the hypervisor heap for page pool blocks.
    svn:sync-xref-src-repo-rev: r24456

--- src/VBox/VMM/MMPagePool.cpp
+++ src/VBox/VMM/MMPagePool.cpp
@@ -1,4 +1,4 @@
-/* $Id: MMPagePool.cpp 4766 2007-09-13 11:05:58Z noreply@oracle.com $ */
+/* $Id: MMPagePool.cpp 4768 2007-09-13 11:36:09Z noreply@oracle.com $ */
 /** @file
  * MM - Memory Monitor(/Manager) - Page Pool.
  */
@@ -217,16 +217,28 @@ DECLINLINE(void *) mmr3PagePoolAlloc(PMMPAGEPOOL pPool)
      * Allocate new subpool.
      */
     unsigned        cPages = !pPool->fLow ? 128 : 32;
+#if 1
+    PMMPAGESUBPOOL  pSub;
+    int rc = MMHyperAlloc(pPool->pVM, 
+                          RT_OFFSETOF(MMPAGESUBPOOL, auBitmap[cPages / (sizeof(pSub->auBitmap[0] * 8))])
+                          + (sizeof(SUPPAGE) + sizeof(MMPPLOOKUPHCPHYS)) * cPages
+                          + sizeof(MMPPLOOKUPHCPTR),
+                          0,
+                          MM_TAG_MM_PAGE,
+                          (void **)&pSub);
+    if (VBOX_FAILURE(rc))
+        return NULL;
+#else
     PMMPAGESUBPOOL  pSub = (PMMPAGESUBPOOL)MMR3HeapAlloc(pPool->pVM, MM_TAG_MM_PAGE,
                                                          RT_OFFSETOF(MMPAGESUBPOOL, auBitmap[cPages / (sizeof(pSub->auBitmap[0] * 8))])
                                                          + (sizeof(SUPPAGE) + sizeof(MMPPLOOKUPHCPHYS)) * cPages
                                                          + sizeof(MMPPLOOKUPHCPTR));
+#endif
     if (!pSub)
         return NULL;
 
     PSUPPAGE paPhysPages = (PSUPPAGE)&pSub->auBitmap[cPages / (sizeof(pSub->auBitmap[0]) * 8)];
     Assert((uintptr_t)paPhysPages >= (uintptr_t)&pSub->auBitmap[1]);
-    int rc;
     if (!pPool->fLow)
     {
         /*
