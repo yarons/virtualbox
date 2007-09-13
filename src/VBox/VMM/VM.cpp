3a599ebbc6a7455c72236b7629d4295847bea87b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-09-13 08:43:31 +0000

    silly me. 0x100 not Start+0x100.
    svn:sync-xref-src-repo-rev: r24442

--- src/VBox/VMM/VM.cpp
+++ src/VBox/VMM/VM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VM.cpp 4754 2007-09-13 07:59:39Z knut.osmundsen@oracle.com $ */
+/* $Id: VM.cpp 4759 2007-09-13 08:43:31Z knut.osmundsen@oracle.com $ */
 /** @file
  * VM - Virtual Machine
  */
@@ -1257,7 +1257,7 @@ static DECLCALLBACK(int) vmR3PowerOff(PVM pVM)
             RTLogRelPrintf("***\n"
                            "ss:sp=0000:%04x ", esp);
             uint32_t Start = esp & ~(uint32_t)63;
-            int rc = PGMPhysReadGCPhys(pVM, abBuf, Start, Start + 0x100);
+            int rc = PGMPhysReadGCPhys(pVM, abBuf, Start, 0x100);
             if (VBOX_SUCCESS(rc))
                 RTLogRelPrintf("0000:%04x TO 0000:%04x:\n"
                                "%.*Rhxd\n",
