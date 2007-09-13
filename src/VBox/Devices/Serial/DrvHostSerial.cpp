f7cbdbc299ee12e4b214d8ba17ca742b77d0360a
Frank Mehnert <noreply@oracle.com>
2007-09-13 06:49:58 +0000

    eol style
    svn:sync-xref-src-repo-rev: r24431

--- src/VBox/Devices/Serial/DrvHostSerial.cpp
+++ src/VBox/Devices/Serial/DrvHostSerial.cpp
@@ -1,4 +1,4 @@
-/** $Id: DrvHostSerial.cpp 4748 2007-09-13 06:49:17Z noreply@oracle.com $ */
+/** $Id: DrvHostSerial.cpp 4749 2007-09-13 06:49:58Z noreply@oracle.com $ */
 /** @file
  * VBox stream I/O devices: Host serial driver
  *
