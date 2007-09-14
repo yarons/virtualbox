329a1d9d6348c3e8edfd06ee5cea2c91de1e3287
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-09-14 14:53:37 +0000

    blanks.
    svn:sync-xref-src-repo-rev: r24501

--- src/VBox/VMM/VMM.cpp
+++ src/VBox/VMM/VMM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMM.cpp 4789 2007-09-14 11:10:35Z noreply@oracle.com $ */
+/* $Id: VMM.cpp 4799 2007-09-14 14:53:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMM - The Virtual Machine Monitor Core.
  */
@@ -2181,7 +2181,6 @@ static int vmmR3ServiceCallHostRequest(PVM pVM)
             pVM->vmm.s.rcCallHost = PGMR3PhysAllocateHandyPages(pVM);
             break;
         }
-
 #ifndef NEW_PHYS_CODE
 
         case VMMCALLHOST_PGM_RAM_GROW_RANGE:
