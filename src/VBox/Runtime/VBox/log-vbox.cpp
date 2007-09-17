6805b379517071bba6f1a3b03c07ba6b379e1253
Buildbox User <noreply@oracle.com>
2007-09-17 15:22:23 +0000

    sys/procfs.h doesn't like _FILE_OFFSET_BITS=64, undefine it.
    svn:sync-xref-src-repo-rev: r24577

--- src/VBox/Runtime/VBox/log-vbox.cpp
+++ src/VBox/Runtime/VBox/log-vbox.cpp
@@ -1,4 +1,4 @@
-/* $Id: log-vbox.cpp 4555 2007-09-06 00:24:20Z knut.osmundsen@oracle.com $ */
+/* $Id: log-vbox.cpp 4858 2007-09-17 15:22:23Z noreply@oracle.com $ */
 /** @file
  * Virtual Box Runtime - Logging configuration.
  */
@@ -125,6 +125,7 @@
 #  include <unistd.h>
 # elif defined(RT_OS_SOLARIS)
 #  define _STRUCTURED_PROC 1
+#  undef _FILE_OFFSET_BITS /* procfs doesn't like this */
 #  include <sys/procfs.h>
 #  include <unistd.h>
 # elif defined(RT_OS_L4)
