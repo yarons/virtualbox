ffa09e0623d005ef807f289da7c74270c55e8b8c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-09-24 14:33:11 +0000

    This wasn't a typo.
    svn:sync-xref-src-repo-rev: r24794

--- src/VBox/Runtime/RTErrConvertToErrno.cpp
+++ src/VBox/Runtime/RTErrConvertToErrno.cpp
@@ -1,4 +1,4 @@
-/* $Rev: 4994 $ */
+/* $Rev: 5009 $ */
 /** @file
  * innotek Portable Runtime - Convert iprt status codes to errno.
  */
@@ -426,7 +426,7 @@ RTDECL(int) RTErrConvertToErrno(int iErr)
 #endif
 
         default:
-            AssertMsgFailed(("Unhandled error code %Vrc\n", iErr));
+            AssertMsgFailed(("Unhandled error code %Rrc\n", iErr));
             return EPROTO;
     }
 }
