3190e8d564cc2ad4e7503dcf173f7caa9be2b9b9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-10-16 14:58:57 +0000

    Use mmap/munmap instead of RTMemPage* (valloc/free).
    svn:sync-xref-src-repo-rev: r25357

--- src/VBox/HostDrivers/Support/solaris/SUPLib-solaris.cpp
+++ src/VBox/HostDrivers/Support/solaris/SUPLib-solaris.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPLib-solaris.cpp 4925 2007-09-20 12:07:28Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPLib-solaris.cpp 5331 2007-10-16 14:58:57Z knut.osmundsen@oracle.com $ */
 /** @file
  *
  * VBox host drivers - Ring-0 support drivers - Solaris host:
@@ -41,6 +41,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/mman.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -153,16 +154,17 @@ int suplibOsIOCtlFast(uintptr_t uFunction)
 
 int suplibOsPageAlloc(size_t cPages, void **ppvPages)
 {
-    *ppvPages = RTMemPageAllocZ(cPages << PAGE_SHIFT);
-    if (*ppvPages)
+    *ppvPages = mmap(NULL, cPages * PAGE_SIZE, PROT_EXEC | PROT_READ | PROT_WRITE, 
+                     MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (*ppvPages != (void *)-1)
         return VINF_SUCCESS;
     return RTErrConvertFromErrno(errno);
 }
 
 
-int suplibOsPageFree(void *pvPages, size_t /* cPages */)
+int suplibOsPageFree(void *pvPages, size_t cPages)
 {
-    RTMemPageFree(pvPages);
+    munmap(pvPages, cPages * PAGE_SIZE);
     return VINF_SUCCESS;
 }
 
