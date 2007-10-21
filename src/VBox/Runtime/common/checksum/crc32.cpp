5e54f1abcf44bbae17b5238e9b5f096b787c1da5
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-10-21 20:40:10 +0000

    crc* md5* -> common/checksum
    svn:sync-xref-src-repo-rev: r25526

--- src/VBox/Runtime/common/checksum/crc32.cpp
+++ src/VBox/Runtime/common/checksum/crc32.cpp
@@ -1,4 +1,4 @@
-/* $Id: crc32.cpp 4071 2007-08-07 17:07:59Z noreply@oracle.com $ */
+/* $Id: crc32.cpp 5412 2007-10-21 20:40:10Z knut.osmundsen@oracle.com $ */
 /** @file
  * innotek Portable Runtime - CRC32.
  */
--- src/VBox/Runtime/common/checksum/md5.cpp
+++ src/VBox/Runtime/common/checksum/md5.cpp
@@ -1,4 +1,4 @@
-/* $Id: md5.cpp 4071 2007-08-07 17:07:59Z noreply@oracle.com $ */
+/* $Id: md5.cpp 5412 2007-10-21 20:40:10Z knut.osmundsen@oracle.com $ */
 /** @file
  * MD5 message digest functions
  */
