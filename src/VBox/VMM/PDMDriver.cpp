76638ac4477e031768c6000fde10c55296f65584
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-12-23 13:30:42 +0000

    This is where I meant.
    svn:sync-xref-src-repo-rev: r26943

--- src/VBox/VMM/PDMDriver.cpp
+++ src/VBox/VMM/PDMDriver.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMDriver.cpp 6162 2007-12-20 23:27:44Z alexander.eichner@oracle.com $ */
+/* $Id: PDMDriver.cpp 6182 2007-12-23 13:30:42Z knut.osmundsen@oracle.com $ */
 /** @file
  * PDM - Pluggable Device and Driver Manager, Driver parts.
  */
@@ -139,6 +139,7 @@ const PDMDRVHLP g_pdmR3DrvHlp =
     pdmR3DrvHlp_SUPCallVMMR0Ex,
     pdmR3DrvHlp_USBRegisterHub,
     pdmR3DrvHlp_PDMThreadCreate,
+    /** @todo add PDMAsyncCompletionTemplateCreate wrapper here. */
     0 /* the end */
 };
 
