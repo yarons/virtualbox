80cd8a4905d874d3cbd7a30887c4dfac115d35b2
Frank Mehnert <noreply@oracle.com>
2008-01-11 07:23:53 +0000

    todo
    svn:sync-xref-src-repo-rev: r27220

--- src/VBox/VMM/VMMR0/GMMR0.cpp
+++ src/VBox/VMM/VMMR0/GMMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: GMMR0.cpp 6311 2008-01-09 18:46:06Z knut.osmundsen@oracle.com $ */
+/* $Id: GMMR0.cpp 6333 2008-01-11 07:23:53Z noreply@oracle.com $ */
 /** @file
  * GMM - Global Memory Manager.
  */
@@ -1401,6 +1401,7 @@ static int gmmR0RegisterChunk(PGMM pGMM, PGMMCHUNKFREESET pSet, RTR0MEMOBJ MemOb
             pChunk->aPages[iPage].Free.iNext = iPage + 1;
         }
         pChunk->aPages[RT_ELEMENTS(pChunk->aPages) - 1].Free.u2State = GMM_PAGE_STATE_FREE;
+        /* XXX sizeof(iNext) < 32, please fix this!! */
         pChunk->aPages[RT_ELEMENTS(pChunk->aPages) - 1].Free.iNext = UINT32_MAX;
 
         /*
