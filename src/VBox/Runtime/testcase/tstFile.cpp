b610e70c3968ebeaf0d92f3fecbd5f7c011b9f79
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-01-21 22:22:11 +0000

    r=bird: file sizes and offets are given as RTFOFF now (the uint64_t use is just legacy that will have to be removed sooner or later). Added a Ex version and filled in the missing documentation. Added a todo about the function *not* working on Windows (vista at least).
    svn:sync-xref-src-repo-rev: r27396

--- src/VBox/Runtime/testcase/tstFile.cpp
+++ src/VBox/Runtime/testcase/tstFile.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstFile.cpp 6421 2008-01-21 17:48:46Z noreply@oracle.com $ */
+/* $Id: tstFile.cpp 6429 2008-01-21 22:22:11Z knut.osmundsen@oracle.com $ */
 /** @file
  * innotek Portable Runtime Testcase - File I/O.
  */
@@ -49,7 +49,27 @@ int main()
         return 1;
     }
 
-    RTPrintf("Maximum file size is %lld bytes.\n", RTFileGetMaxSize(File));
+    RTFOFF cbMax = -2;
+    rc = RTFileGetMaxSizeEx(File, &cbMax);
+    if (RT_FAILURE(rc))
+    {
+        RTPrintf("tstFile: RTFileGetMaxSizeEx failed: %Rrc\n", rc);
+        cErrors++;
+    }
+    else if (cbMax <= 0)
+    {
+        RTPrintf("tstFile: RTFileGetMaxSizeEx failed: cbMax=%RTfoff\n", cbMax);
+        cErrors++;
+    }
+    else if (RTFileGetMaxSize(File) != cbMax)
+    {
+        RTPrintf("tstFile: RTFileGetMaxSize failed; returns %RTfoff instead of %RTfoff\n", RTFileGetMaxSize(File), cbMax);
+        cErrors++;
+    }
+    else
+        RTPrintf("Maximum file size is %RTfoff bytes.\n", cbMax);
+
+return 0;
 
     /* grow file beyond 2G */
     rc = RTFileSetSize(File, _2G + _1M);
