fa576321e88a58f71beecd092dfa760d73b7f45e
Dmitry Kuminov <noreply@oracle.com>
2008-02-08 11:40:26 +0000

    VBoxHDD-new: The size argument of RTDirRead must be initialized if not NULL.
    svn:sync-xref-src-repo-rev: r28002

--- src/VBox/Devices/Storage/VBoxHDD-new.cpp
+++ src/VBox/Devices/Storage/VBoxHDD-new.cpp
@@ -1,4 +1,4 @@
-/** $Id: VBoxHDD-new.cpp 6735 2008-02-01 20:56:30Z alexander.eichner@oracle.com $ */
+/** $Id: VBoxHDD-new.cpp 6864 2008-02-08 11:40:26Z noreply@oracle.com $ */
 /** @file
  * VBox HDD Container implementation.
  */
@@ -745,9 +745,9 @@ VBOXDDU_DECL(int) VDGetFormat(const char *pszFilename, char **ppszFormat)
             break;
 
         PRTDIRENTRY pPluginDirEntry = NULL;
-        unsigned cbPluginDirEntry;
+        unsigned cbPluginDirEntry = sizeof(RTDIRENTRY);
         pPluginDirEntry = (PRTDIRENTRY)RTMemAllocZ(sizeof(RTDIRENTRY));
-        if (!pPluginDir)
+        if (!pPluginDirEntry)
         {
             rc = VERR_NO_MEMORY;
             break;
