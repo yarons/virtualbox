fde41005ab7df8012722f54edce4bcee629c797a
Sander van Leeuwen <noreply@oracle.com>
2008-03-27 16:08:49 +0000

    Corrected wrong macro use to mask physical addresses
    svn:sync-xref-src-repo-rev: r29098

--- src/VBox/VMM/VMMAll/PGMAllPhys.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPhys.cpp 7597 2008-03-27 16:01:26Z noreply@oracle.com $ */
+/* $Id: PGMAllPhys.cpp 7598 2008-03-27 16:08:49Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -1063,7 +1063,7 @@ static void pgmPhysCacheAdd(PVM pVM, PGMPHYSCACHE *pCache, RTGCPHYS GCPhys, uint
 
     Assert(VM_IS_EMT(pVM));
 
-    GCPhys = GCPhys & X86_PTE_PAE_PG_MASK;
+    GCPhys = PHYS_PAGE_ADDRESS(GCPhys);
     pbHC   = (uint8_t *)PAGE_ADDRESS(pbHC);
 
     iCacheIndex = ((GCPhys >> PAGE_SHIFT) & PGM_MAX_PHYSCACHE_ENTRIES_MASK);
