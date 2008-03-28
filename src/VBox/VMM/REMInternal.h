0beba5d0496506dd26dad8e43f83c2bf56940cd2
Sander van Leeuwen <noreply@oracle.com>
2008-03-28 10:22:43 +0000

    Alignment
    svn:sync-xref-src-repo-rev: r29122

--- src/VBox/VMM/REMInternal.h
+++ src/VBox/VMM/REMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: REMInternal.h 7620 2008-03-28 10:12:44Z noreply@oracle.com $ */
+/* $Id: REMInternal.h 7621 2008-03-28 10:22:43Z noreply@oracle.com $ */
 /** @file
  * REM - Internal header file.
  */
@@ -127,6 +127,7 @@ typedef struct REMPHYSREGISTRATION
     RTGCPHYS        GCPhys;
     RTHCUINTPTR     HCVirt;
     RTUINT          cb;
+    uint32_t        u32Padding;
 } REMPHYSREGISTRATION, *PREMPHYSREGISTRATION;
 
 /**
