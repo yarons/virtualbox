3b245b36cdb54860dca05c8a3719a87b8df278eb
Michael Thayer <noreply@oracle.com>
2008-03-28 10:00:01 +0000

    VMM/PATM: conditionally ifdef'ed out an assertion which I have been seeing for months now.
    svn:sync-xref-src-repo-rev: r29119

--- src/VBox/VMM/PATM/PATM.cpp
+++ src/VBox/VMM/PATM/PATM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PATM.cpp 6107 2007-12-17 15:38:53Z noreply@oracle.com $ */
+/* $Id: PATM.cpp 7618 2008-03-28 10:00:01Z noreply@oracle.com $ */
 /** @file
  * PATM - Dynamic Guest OS Patching Manager
  *
@@ -5381,7 +5381,12 @@ int patmR3RefreshPatch(PVM pVM, PPATMPATCHREC pPatchRec)
 
     pPatch = &pPatchRec->patch;
     AssertReturn(pPatch->flags & (PATMFL_DUPLICATE_FUNCTION|PATMFL_IDTHANDLER|PATMFL_TRAPHANDLER), VERR_PATCHING_REFUSED);
+#ifndef DEBUG_michael  /* This has been triggering for months now */
     AssertReturn(!(pPatch->flags & PATMFL_EXTERNAL_JUMP_INSIDE), VERR_PATCHING_REFUSED);
+#else
+    if (pPatch->flags & PATMFL_EXTERNAL_JUMP_INSIDE)
+        return VERR_PATCHING_REFUSED;
+#endif
 
     /** Note: quite ugly to enable/disable/remove/insert old and new patches, but there's no easy way around it. */
 
