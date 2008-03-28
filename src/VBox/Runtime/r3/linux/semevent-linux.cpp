5dc4ca354c486de9170810a3b7595e2cbcdba8d9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-03-28 18:54:28 +0000

    More strictness.
    svn:sync-xref-src-repo-rev: r29145

--- src/VBox/Runtime/r3/linux/semevent-linux.cpp
+++ src/VBox/Runtime/r3/linux/semevent-linux.cpp
@@ -1,4 +1,4 @@
-/* $Id: semevent-linux.cpp 6747 2008-02-02 00:57:41Z knut.osmundsen@oracle.com $ */
+/* $Id: semevent-linux.cpp 7639 2008-03-28 18:54:28Z knut.osmundsen@oracle.com $ */
 /** @file
  * innotek Portable Runtime - Event Semaphore, Linux (2.6.x+).
  */
@@ -206,7 +206,7 @@ static int rtSemEventWait(RTSEMEVENT EventSem, unsigned cMillies, bool fAutoResu
      * Convert timeout value.
      */
     struct timespec ts;
-    struct timespec *pTimeout = 0;
+    struct timespec *pTimeout = NULL;
     if (cMillies != RT_INDEFINITE_WAIT)
     {
         ts.tv_sec  = cMillies / 1000;
@@ -279,6 +279,7 @@ RTDECL(int)  RTSemEventWait(RTSEMEVENT EventSem, unsigned cMillies)
 {
     int rc = rtSemEventWait(EventSem, cMillies, true);
     Assert(rc != VERR_INTERRUPTED);
+    Assert(rc != VERR_TIMEOUT || cMillies != RT_INDEFINITE_WAIT);
     return rc;
 }
 
