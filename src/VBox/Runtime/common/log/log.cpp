2ec146aab483a63cd49df6e9a934c714cf55e623
Frank Mehnert <noreply@oracle.com>
2008-04-02 16:52:26 +0000

    typo
    svn:sync-xref-src-repo-rev: r29240

--- src/VBox/Runtime/common/log/log.cpp
+++ src/VBox/Runtime/common/log/log.cpp
@@ -1,4 +1,4 @@
-/* $Id: log.cpp 7710 2008-04-02 16:52:01Z noreply@oracle.com $ */
+/* $Id: log.cpp 7711 2008-04-02 16:52:26Z noreply@oracle.com $ */
 /** @file
  * Runtime VBox - Logger.
  */
@@ -2047,7 +2047,7 @@ static DECLCALLBACK(size_t) rtLogOutputPrefixed(void *pv, const char *pachChars,
                     const RTCPUID idCpu = RTMpCpuId();
 #endif
                     psz += RTStrFormatNumber(psz, idCpu, 16, sizeof(idCpu) * 2, 0, RTSTR_F_ZEROPAD);
-                    psz++ = ' ';
+                    *psz++ = ' ';
                 }
                 if (pLogger->fFlags & RTLOGFLAGS_PREFIX_FLAG_NO)
                 {
