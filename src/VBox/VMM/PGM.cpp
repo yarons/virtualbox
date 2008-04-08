fef846c08f9064c77bfab075f6f9bd79aa87404e
Sander van Leeuwen <noreply@oracle.com>
2008-04-08 13:58:52 +0000

    Init aGCPhysGstPaePDsMonitored properly
    svn:sync-xref-src-repo-rev: r29373

--- src/VBox/VMM/PGM.cpp
+++ src/VBox/VMM/PGM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGM.cpp 7753 2008-04-04 20:35:44Z knut.osmundsen@oracle.com $ */
+/* $Id: PGM.cpp 7805 2008-04-08 13:58:52Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor. (Mixing stuff here, not good?)
  */
@@ -891,9 +891,10 @@ PGMR3DECL(int) PGMR3Init(PVM pVM)
     pVM->pgm.s.pGstPaePDPTGC    = 0;
     for (unsigned i = 0; i < ELEMENTS(pVM->pgm.s.apGstPaePDsHC); i++)
     {
-        pVM->pgm.s.apGstPaePDsHC[i]     = NULL;
-        pVM->pgm.s.apGstPaePDsGC[i]     = 0;
-        pVM->pgm.s.aGCPhysGstPaePDs[i]  = NIL_RTGCPHYS;
+        pVM->pgm.s.apGstPaePDsHC[i]             = NULL;
+        pVM->pgm.s.apGstPaePDsGC[i]             = 0;
+        pVM->pgm.s.aGCPhysGstPaePDs[i]          = NIL_RTGCPHYS;
+        pVM->pgm.s.aGCPhysGstPaePDsMonitored[i] = NIL_RTGCPHYS;
     }
 
 #ifdef VBOX_STRICT
