c7b29365c76076452778525616c790c96b1d7636
Sander van Leeuwen <noreply@oracle.com>
2008-04-15 13:29:05 +0000

    Backed out 29633 & 29635 for now
    svn:sync-xref-src-repo-rev: r29640

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1,4 +1,4 @@
-/** $Id: ConsoleImpl2.cpp 7982 2008-04-15 12:32:02Z noreply@oracle.com $ */
+/** $Id: ConsoleImpl2.cpp 7987 2008-04-15 13:29:05Z noreply@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation
  *
@@ -166,11 +166,6 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
         rc = CFGMR3InsertInteger(pHWVirtExt, "Enabled", 1);                         RC_CHECK();
     }
 #endif
-
-    /* Physical Address Extension (PAE) */
-    BOOL fEnablePAE = false;
-    hrc = pMachine->COMGETTER(PAEEnabled)(&fEnablePAE);                             H();
-    rc = CFGMR3InsertInteger(pRoot, "EnablePAE", fEnablePAE);                       RC_CHECK();
 
     BOOL fIOAPIC;
     hrc = biosSettings->COMGETTER(IOAPICEnabled)(&fIOAPIC);                          H();
--- src/VBox/Main/include/MachineImpl.h
+++ src/VBox/Main/include/MachineImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.h 7982 2008-04-15 12:32:02Z noreply@oracle.com $ */
+/* $Id: MachineImpl.h 7987 2008-04-15 13:29:05Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -230,7 +230,6 @@ public:
         ULONG          mVRAMSize;
         ULONG          mMonitorCount;
         TSBool_T       mHWVirtExEnabled;
-        BOOL           mPAEEnabled;
 
         DeviceType_T   mBootOrder [SchemaDefs::MaxBootPosition];
 
@@ -459,8 +458,6 @@ public:
     STDMETHOD(COMGETTER(BIOSSettings))(IBIOSSettings **biosSettings);
     STDMETHOD(COMGETTER(HWVirtExEnabled))(TSBool_T *enabled);
     STDMETHOD(COMSETTER(HWVirtExEnabled))(TSBool_T enabled);
-    STDMETHOD(COMGETTER(PAEEnabled))(BOOL *enabled);
-    STDMETHOD(COMSETTER(PAEEnabled))(BOOL enabled);
     STDMETHOD(COMGETTER(SnapshotFolder))(BSTR *aSavedStateFolder);
     STDMETHOD(COMSETTER(SnapshotFolder))(INPTR BSTR aSavedStateFolder);
     STDMETHOD(COMGETTER(HardDiskAttachments))(IHardDiskAttachmentCollection **attachments);
