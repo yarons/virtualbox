3385dd84859fbb66797701253123ac0e2093382e
Sander van Leeuwen <noreply@oracle.com>
2008-04-21 14:34:11 +0000

    Reapplied 29907
    svn:sync-xref-src-repo-rev: r29966

--- src/VBox/VMM/VMMAll/EMAll.cpp
+++ src/VBox/VMM/VMMAll/EMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: EMAll.cpp 8234 2008-04-21 14:29:54Z noreply@oracle.com $ */
+/* $Id: EMAll.cpp 8236 2008-04-21 14:34:11Z noreply@oracle.com $ */
 /** @file
  * EM - Execution Monitor(/Manager) - All contexts
  */
@@ -352,7 +352,7 @@ static RTGCPTR emConvertToFlatAddr(PVM pVM, PCPUMCTXCORE pRegFrame, PDISCPUSTATE
     return SELMToFlat(pVM, pRegFrame->eflags, sel, pSelHidReg, pvAddr);
 }
 
-#if defined(VBOX_STRICT) || defined(LOG_ENABLED)
+#if defined(IN_GC) && (defined(VBOX_STRICT) || defined(LOG_ENABLED))
 /** 
  * Get the mnemonic for the disassembled instruction.
  *  
@@ -1249,7 +1249,7 @@ static int emInterpretCmpXchg(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCORE pRegFrame
     OP_PARAMVAL param1, param2;
 
 #ifdef LOG_ENABLED
-    char *pszInstr;
+    const char *pszInstr;
 
     if (pCpu->prefix & PREFIX_LOCK)
         pszInstr = "Lock CmpXchg";
@@ -1337,7 +1337,7 @@ static int emInterpretCmpXchg8b(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCORE pRegFra
     OP_PARAMVAL param1;
 
 #ifdef LOG_ENABLED
-    char *pszInstr;
+    const char *pszInstr;
 
     if (pCpu->prefix & PREFIX_LOCK)
         pszInstr = "Lock CmpXchg8b";
