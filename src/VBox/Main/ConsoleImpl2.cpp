100739dd49f5060a68b458392be24d327364ba17
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-04-22 19:03:17 +0000

    Set Force11Device on a per device level so that we don't make any unnecessary modifications for full or low speed devices with bcdUSB=2.0.
    svn:sync-xref-src-repo-rev: r30065

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1,4 +1,4 @@
-/** $Id: ConsoleImpl2.cpp 8241 2008-04-21 15:35:36Z klaus.espenlaub@oracle.com $ */
+/** $Id: ConsoleImpl2.cpp 8308 2008-04-22 19:03:17Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation
  *
@@ -1539,12 +1539,14 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
 #endif
             {
                 /*
-                 * Enable the 2.0 -> 1.1 device morphing of proxied devies to keep windows quiet.
+                 * Global USB options, currently unused as we'll apply the 2.0 -> 1.1 morphing
+                 * on a per device level now.
                  */
                 rc = CFGMR3InsertNode(pRoot, "USB", &pCfg);                                RC_CHECK();
                 rc = CFGMR3InsertNode(pCfg, "USBProxy", &pCfg);                            RC_CHECK();
                 rc = CFGMR3InsertNode(pCfg, "GlobalConfig", &pCfg);                        RC_CHECK();
-                rc = CFGMR3InsertInteger(pCfg, "Force11Device", true);                     RC_CHECK();
+                // This globally enables the 2.0 -> 1.1 device morphing of proxied devies to keep windows quiet.
+                //rc = CFGMR3InsertInteger(pCfg, "Force11Device", true);                     RC_CHECK();
                 // The following breaks stuff, but it makes MSDs work in vista. (I include it here so
                 // that it's documented somewhere.) Users needing it can use:
                 //      VBoxManage setextradata "myvm" "VBoxInternal/USB/USBProxy/GlobalConfig/Force11PacketSize" 1
