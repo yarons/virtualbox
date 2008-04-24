8b6853aa1f8e16cf818b3e7172d7452a59eb40ec
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2008-04-24 07:08:53 +0000

    SolaudiO: Fix for ubuntu guest desktop crashing on startup with audio enabled.
    svn:sync-xref-src-repo-rev: r30121

--- src/VBox/Devices/Audio/solaudio.c
+++ src/VBox/Devices/Audio/solaudio.c
@@ -1,4 +1,4 @@
-/* $Id: solaudio.c 8155 2008-04-18 15:16:47Z noreply@oracle.com $ */
+/* $Id: solaudio.c 8345 2008-04-24 07:08:53Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VirtualBox Audio Driver - Solaris host.
  */
@@ -839,11 +839,11 @@ static struct audio_pcm_ops solaudio_pcm_ops =
     solaudio_write,
     solaudio_ctl_out,
 
-    solaudio_init_in,
-    solaudio_fini_in,
-    solaudio_run_in,
-    solaudio_read,
-    solaudio_ctl_in
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL
 };
 
 
@@ -869,7 +869,7 @@ struct audio_driver solaudio_audio_driver =
     INIT_FIELD (pcm_ops        = ) &solaudio_pcm_ops,
     INIT_FIELD (can_be_default = ) 1,
     INIT_FIELD (max_voices_out = ) INT_MAX,
-    INIT_FIELD (max_voices_in  = ) 0,           /* Input not supported. */
+    INIT_FIELD (max_voices_in  = ) 0,           /* Input not really supported. */
     INIT_FIELD (voice_size_out = ) sizeof (solaudioVoiceOut),
     INIT_FIELD (voice_size_in  = ) 0
 };
