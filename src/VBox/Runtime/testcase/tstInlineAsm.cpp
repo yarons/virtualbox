737b1720bdf17a3985a9cea5627eea3ff7bbb1a4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-05-16 11:55:20 +0000

    Added ASMGetCpuFamily, ASMGetCpuModel and ASMGetCpuStepping to pack up the bit fiddling.
    svn:sync-xref-src-repo-rev: r30917

--- src/VBox/Runtime/testcase/tstInlineAsm.cpp
+++ src/VBox/Runtime/testcase/tstInlineAsm.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstInlineAsm.cpp 8245 2008-04-21 17:24:28Z noreply@oracle.com $ */
+/* $Id: tstInlineAsm.cpp 8882 2008-05-16 11:55:20Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase - inline assembly.
  */
@@ -181,16 +181,16 @@ void tstASMCpuId(void)
     if (cFunctions >= 1)
     {
         ASMCpuId(1, &s.uEAX, &s.uEBX, &s.uECX, &s.uEDX);
-        RTPrintf("Family:                          %d  \tExtended: %d \tEffectiv: %d\n"
-                 "Model:                           %d  \tExtended: %d \tEffectiv: %d\n"
+        RTPrintf("Family:                          %#x \tExtended: %#x \tEffectiv: %#x\n"
+                 "Model:                           %#x \tExtended: %#x \tEffectiv: %#x\n"
                  "Stepping:                        %d\n"
                  "APIC ID:                         %#04x\n"
                  "Logical CPUs:                    %d\n"
                  "CLFLUSH Size:                    %d\n"
                  "Brand ID:                        %#04x\n",
-                 (s.uEAX >> 8) & 0xf, (s.uEAX >> 20) & 0x7f, ((s.uEAX >> 8) & 0xf) + (((s.uEAX >> 8) & 0xf) == 0xf ? (s.uEAX >> 20) & 0x7f : 0),
-                 (s.uEAX >> 4) & 0xf, (s.uEAX >> 16) & 0x0f, ((s.uEAX >> 4) & 0xf) | (((s.uEAX >> 4) & 0xf) == 0xf ? (s.uEAX >> 16) & 0x0f : 0),
-                 (s.uEAX >> 0) & 0xf,
+                 (s.uEAX >> 8) & 0xf, (s.uEAX >> 20) & 0x7f, ASMGetCpuFamily(s.uEAX),
+                 (s.uEAX >> 4) & 0xf, (s.uEAX >> 16) & 0x0f, ASMGetCpuModel(s.uEAX),
+                 ASMGetCpuStepping(s.uEAX),
                  (s.uEBX >> 24) & 0xff,
                  (s.uEBX >> 16) & 0xff,
                  (s.uEBX >>  8) & 0xff,
@@ -279,13 +279,13 @@ void tstASMCpuId(void)
     if (cExtFunctions >= 0x80000001)
     {
         ASMCpuId(0x80000001, &s.uEAX, &s.uEBX, &s.uECX, &s.uEDX);
-        RTPrintf("Family:                          %d  \tExtended: %d \tEffectiv: %d\n"
-                 "Model:                           %d  \tExtended: %d \tEffectiv: %d\n"
+        RTPrintf("Family:                          %#x \tExtended: %#x \tEffectiv: %#x\n"
+                 "Model:                           %#x \tExtended: %#x \tEffectiv: %#x\n"
                  "Stepping:                        %d\n"
                  "Brand ID:                        %#05x\n",
-                 (s.uEAX >> 8) & 0xf, (s.uEAX >> 20) & 0x7f, ((s.uEAX >> 8) & 0xf) + (((s.uEAX >> 8) & 0xf) == 0xf ? (s.uEAX >> 20) & 0x7f : 0),
-                 (s.uEAX >> 4) & 0xf, (s.uEAX >> 16) & 0x0f, ((s.uEAX >> 4) & 0xf) | (((s.uEAX >> 4) & 0xf) == 0xf ? (s.uEAX >> 16) & 0x0f : 0),
-                 (s.uEAX >> 0) & 0xf,
+                 (s.uEAX >> 8) & 0xf, (s.uEAX >> 20) & 0x7f, ASMGetCpuFamily(s.uEAX),
+                 (s.uEAX >> 4) & 0xf, (s.uEAX >> 16) & 0x0f, ASMGetCpuModel(s.uEAX),
+                 ASMGetCpuStepping(s.uEAX),
                  s.uEBX & 0xfff);
 
         RTPrintf("Features EDX:                   ");
