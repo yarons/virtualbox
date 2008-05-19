674e456093a4ab999490ce0bddee8e4698bbc4e0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-05-19 17:41:44 +0000

    Don't return VERR_GETOP_UNKNOWN_OPTION when encountering a file.
    svn:sync-xref-src-repo-rev: r30984

--- src/VBox/Runtime/common/misc/getopt.cpp
+++ src/VBox/Runtime/common/misc/getopt.cpp
@@ -1,4 +1,4 @@
-/* $Id: getopt.cpp 8245 2008-04-21 17:24:28Z noreply@oracle.com $ */
+/* $Id: getopt.cpp 8926 2008-05-19 17:41:44Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Command Line Parsing
  */
@@ -54,6 +54,7 @@ RTDECL(int) RTGetOpt(int argc, char **argv, PCRTOPTIONDEF paOptions, size_t cOpt
 
     if (*pszArgThis == '-')
     {
+/** @todo implement '--'. */
         for (size_t i = 0; i < cOptions; i++)
         {
             Assert(!(paOptions[i].fFlags & ~RTGETOPT_VALID_MASK));
@@ -190,11 +191,17 @@ RTDECL(int) RTGetOpt(int argc, char **argv, PCRTOPTIONDEF paOptions, size_t cOpt
                 return paOptions[i].iShort;
             }
         }
+
+
+        return VERR_GETOPT_UNKNOWN_OPTION;
     }
 
+    /*
+     * Not an option.
+     */
     /** @todo Sort options and arguments (i.e. stuff that doesn't start with '-'), stop when
      * encountering the first argument. */
 
-    return VERR_GETOPT_UNKNOWN_OPTION;
+    return 0;
 }
 
