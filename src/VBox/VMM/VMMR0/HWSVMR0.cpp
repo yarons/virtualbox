7b7aba420cbbe336dbc9bb1ba7328644e6de3d98
Sander van Leeuwen <noreply@oracle.com>
2008-05-23 12:14:51 +0000

    Fixed wrong call to TRPMResetTrap
    svn:sync-xref-src-repo-rev: r31178

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWSVMR0.cpp 9074 2008-05-23 12:11:36Z noreply@oracle.com $ */
+/* $Id: HWSVMR0.cpp 9075 2008-05-23 12:14:51Z noreply@oracle.com $ */
 /** @file
  * HWACCM SVM - Host Context Ring 0.
  */
@@ -1160,10 +1160,8 @@ ResumeExecution:
             {   /* A genuine pagefault.
                  * Forward the trap to the guest by injecting the exception and resuming execution.
                  */
-                Log2(("Page fault at %VGv cr2=%VGv error code %x\n", pCtx->eip, uFaultAddress, errCode));
+                Log(("Page fault at %VGv cr2=%VGv error code %x\n", pCtx->eip, uFaultAddress, errCode));
                 STAM_COUNTER_INC(&pVM->hwaccm.s.StatExitGuestPF);
-
-                TRPMResetTrap(pVM);
 
                 /* Now we must update CR2. */
                 pCtx->cr2 = uFaultAddress;
