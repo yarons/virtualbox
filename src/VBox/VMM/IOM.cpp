653bbf2dce899540b1262566b35a2d8e938aa508
Sander van Leeuwen <noreply@oracle.com>
2008-06-02 12:20:51 +0000

    Wrong string format specifiers
    svn:sync-xref-src-repo-rev: r31500

--- src/VBox/VMM/IOM.cpp
+++ src/VBox/VMM/IOM.cpp
@@ -1,4 +1,4 @@
-/* $Id: IOM.cpp 9293 2008-06-02 12:18:45Z noreply@oracle.com $ */
+/* $Id: IOM.cpp 9294 2008-06-02 12:20:51Z noreply@oracle.com $ */
 /** @file
  * IOM - Input / Output Monitor.
  */
@@ -733,7 +733,7 @@ IOMR3DECL(int)  IOMR3IOPortRegisterR0(PVM pVM, PPDMDEVINS pDevIns, RTIOPORT Port
                                       R0PTRTYPE(PFNIOMIOPORTOUTSTRING) pfnOutStrCallback, R0PTRTYPE(PFNIOMIOPORTINSTRING) pfnInStrCallback,
                                       const char *pszDesc)
 {
-    LogFlow(("IOMR3IOPortRegisterR0: pDevIns=%p PortStart=%#x cPorts=%#x pvUser=%VHv pfnOutCallback=%VGv pfnInCallback=%VGv pfnOutStrCallback=%VGv  pfnInStrCallback=%VGv pszDesc=%s\n",
+    LogFlow(("IOMR3IOPortRegisterR0: pDevIns=%p PortStart=%#x cPorts=%#x pvUser=%VHv pfnOutCallback=%VHv pfnInCallback=%VHv pfnOutStrCallback=%VHv  pfnInStrCallback=%VHv pszDesc=%s\n",
              pDevIns, PortStart, cPorts, pvUser, pfnOutCallback, pfnInCallback, pfnOutStrCallback, pfnInStrCallback, pszDesc));
 
     /*
