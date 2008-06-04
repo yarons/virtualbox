1f12160da14acf3b34f00b2aaa4d1c5ddcc9e618
Frank Mehnert <noreply@oracle.com>
2008-06-04 16:22:54 +0000

    fixed more places with incorrect descriptor base address calculation
    svn:sync-xref-src-repo-rev: r31628

--- src/VBox/VMM/VMMAll/SELMAll.cpp
+++ src/VBox/VMM/VMMAll/SELMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: SELMAll.cpp 9212 2008-05-29 09:38:38Z noreply@oracle.com $ */
+/* $Id: SELMAll.cpp 9395 2008-06-04 16:22:54Z noreply@oracle.com $ */
 /** @file
  * SELM All contexts.
  */
@@ -69,7 +69,7 @@ static RTGCPTR selmToFlat(PVM pVM, RTSEL Sel, RTGCPTR Addr)
     }
 
     return (RTGCPTR)( (RTGCUINTPTR)Addr
-                       + (   (Desc.Gen.u8BaseHigh2 << 24)
+                       + (   ((RTGCPTR)Desc.Gen.u8BaseHigh2 << 24)
                           |  (Desc.Gen.u8BaseHigh1 << 16)
                           |   Desc.Gen.u16BaseLow));
 }
@@ -202,7 +202,7 @@ SELMDECL(int) SELMToFlatEx(PVM pVM, X86EFLAGS eflags, RTSEL Sel, RTGCPTR Addr, C
 
         /* calc address assuming straight stuff. */
         pvFlat = (RTGCPTR)(  (RTGCUINTPTR)Addr
-                           + (   (Desc.Gen.u8BaseHigh2 << 24)
+                           + (   ((RTGCPTR)Desc.Gen.u8BaseHigh2 << 24)
                               |  (Desc.Gen.u8BaseHigh1 << 16)
                               |   Desc.Gen.u16BaseLow )
                              );
@@ -407,7 +407,7 @@ DECLINLINE(int) selmValidateAndConvertCSAddrStd(PVM pVM, RTSEL SelCPL, RTSEL Sel
                 if ((RTGCUINTPTR)Addr <= u32Limit)
                 {
                     *ppvFlat = (RTGCPTR)(  (RTGCUINTPTR)Addr
-                                           + (   (Desc.Gen.u8BaseHigh2 << 24)
+                                           + (   ((RTGCPTR)Desc.Gen.u8BaseHigh2 << 24)
                                               |  (Desc.Gen.u8BaseHigh1 << 16)
                                               |   Desc.Gen.u16BaseLow)
                                              );
