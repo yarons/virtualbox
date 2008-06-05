a043214030db7791b776029e2225818ee6fbe614
Frank Mehnert <noreply@oracle.com>
2008-06-05 12:06:48 +0000

    macro
    svn:sync-xref-src-repo-rev: r31652

--- src/VBox/VMM/VMMR0/HWACCMR0.cpp
+++ src/VBox/VMM/VMMR0/HWACCMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWACCMR0.cpp 9413 2008-06-05 12:03:27Z noreply@oracle.com $ */
+/* $Id: HWACCMR0.cpp 9414 2008-06-05 12:06:48Z noreply@oracle.com $ */
 /** @file
  * HWACCM - Host Context Ring 0.
  */
@@ -911,7 +911,7 @@ HWACCMR0DECL(void) HWACCMR0DumpDescriptor(PX86DESCHC pDesc, RTSEL Sel, const cha
     /*
      * Limit and Base and format the output.
      */
-    uint32_t    u32Limit = pDesc->Gen.u4LimitHigh << 16 | pDesc->Gen.u16LimitLow;
+    uint32_t    u32Limit = X86DESC_LIMIT(*pDesc);
     if (pDesc->Gen.u1Granularity)
         u32Limit = u32Limit << PAGE_SHIFT | PAGE_OFFSET_MASK;
 
