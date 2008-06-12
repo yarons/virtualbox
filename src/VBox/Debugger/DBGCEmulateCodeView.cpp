abded36c3ec1dfb36dfeecaee8058bf369ff2f95
Sander van Leeuwen <noreply@oracle.com>
2008-06-12 12:32:03 +0000

    Missing u1Reserved->u1Long
    svn:sync-xref-src-repo-rev: r31986

--- src/VBox/Debugger/DBGCEmulateCodeView.cpp
+++ src/VBox/Debugger/DBGCEmulateCodeView.cpp
@@ -1,4 +1,4 @@
-/** $Id: DBGCEmulateCodeView.cpp 9465 2008-06-06 11:38:59Z noreply@oracle.com $ */
+/** $Id: DBGCEmulateCodeView.cpp 9657 2008-06-12 12:32:03Z noreply@oracle.com $ */
 /** @file
  * DBGC - Debugger Console, CodeView / WinDbg Emulation.
  */
@@ -1636,7 +1636,7 @@ static int dbgcCmdDumpDTWorker32(PDBGCCMDHLP pCmdHlp, PCX86DESC pDesc, unsigned 
         rc = pCmdHlp->pfnPrintf(pCmdHlp, NULL, "%04x %s Bas=%08x Lim=%08x DPL=%d %s %s %s %s AVL=%d R=%d%s\n",
                                 iEntry, s_apszTypes[pDesc->Gen.u4Type], u32Base, cbLimit,
                                 pDesc->Gen.u2Dpl, pszPresent, pszAccessed, pszGranularity, pszBig,
-                                pDesc->Gen.u1Available, pDesc->Gen.u1Reserved, pszHyper);
+                                pDesc->Gen.u1Available, pDesc->Gen.u1Long, pszHyper);
     }
     else
     {
@@ -1690,7 +1690,7 @@ static int dbgcCmdDumpDTWorker32(PDBGCCMDHLP pCmdHlp, PCX86DESC pDesc, unsigned 
                 rc = pCmdHlp->pfnPrintf(pCmdHlp, NULL, "%04x %s Bas=%08x Lim=%08x DPL=%d %s %s %s %s AVL=%d R=%d%s\n",
                                         iEntry, s_apszTypes[pDesc->Gen.u4Type], u32Base, cbLimit,
                                         pDesc->Gen.u2Dpl, pszPresent, pszBusy, pszGranularity, pszBig,
-                                        pDesc->Gen.u1Available, pDesc->Gen.u1Reserved | (pDesc->Gen.u1DefBig << 1),
+                                        pDesc->Gen.u1Available, pDesc->Gen.u1Long | (pDesc->Gen.u1DefBig << 1),
                                         pszHyper);
                 break;
             }
