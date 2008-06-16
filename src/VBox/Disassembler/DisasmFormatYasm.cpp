0839f012a4607090e7f187cc35492a3dd26375ff
Sander van Leeuwen <noreply@oracle.com>
2008-06-16 15:38:20 +0000

    And again
    svn:sync-xref-src-repo-rev: r32084

--- src/VBox/Disassembler/DisasmFormatYasm.cpp
+++ src/VBox/Disassembler/DisasmFormatYasm.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisasmFormatYasm.cpp 9730 2008-06-16 15:37:46Z noreply@oracle.com $ */
+/* $Id: DisasmFormatYasm.cpp 9731 2008-06-16 15:38:20Z noreply@oracle.com $ */
 /** @file
  * VBox Disassembler - Yasm(/Nasm) Style Formatter.
  */
@@ -350,7 +350,7 @@ DISDECL(size_t) DISFormatYasmEx(PCDISCPUSTATE pCpu, char *pszBuf, size_t cchBuf,
 #define PUT_NUM_S8(num)  PUT_NUM_SIGN(4,  "0%02xh", num, int8_t,  uint8_t)
 #define PUT_NUM_S16(num) PUT_NUM_SIGN(6,  "0%04xh", num, int16_t, uint16_t)
 #define PUT_NUM_S32(num) PUT_NUM_SIGN(10, "0%08xh", num, int32_t, uint32_t)
-#define PUT_NUM_S64(num) PUT_NUM_SIGN(18, "0%08xh", num, int64_t, uint64_t)
+#define PUT_NUM_S64(num) PUT_NUM_SIGN(18, "0%016xh", num, int64_t, uint64_t)
 
 
     /*
