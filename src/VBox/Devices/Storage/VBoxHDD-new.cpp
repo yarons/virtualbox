5e854fe531227979f3e198b4689e1372529eb2e0
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2008-06-16 17:20:49 +0000

    Extended the interface with backends to support time stamps and parent's filename.
    svn:sync-xref-src-repo-rev: r32089

--- src/VBox/Devices/Storage/VBoxHDD-new.cpp
+++ src/VBox/Devices/Storage/VBoxHDD-new.cpp
@@ -1,4 +1,4 @@
-/** $Id: VBoxHDD-new.cpp 9599 2008-06-11 11:26:09Z klaus.espenlaub@oracle.com $ */
+/** $Id: VBoxHDD-new.cpp 9734 2008-06-16 17:20:49Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * VBox HDD Container implementation.
  */
@@ -1528,6 +1528,7 @@ VBOXDDU_DECL(int) VDCreateDiff(PVBOXHDD pDisk, const char *pszBackend,
         if (VBOX_SUCCESS(rc))
         {
             RTUUID Uuid;
+            RTTIMESPEC ts;
             int rc2;
 
             rc2 = pDisk->pLast->Backend->pfnGetUuid(pDisk->pLast->pvBackendData,
@@ -1539,6 +1540,12 @@ VBOXDDU_DECL(int) VDCreateDiff(PVBOXHDD pDisk, const char *pszBackend,
             if (VBOX_SUCCESS(rc2))
                 pImage->Backend->pfnSetParentModificationUuid(pImage->pvBackendData,
                                                               &Uuid);
+            rc2 = pDisk->pLast->Backend->pfnGetTimeStamp(pDisk->pLast->pvBackendData,
+                                                             &ts);
+            if (VBOX_SUCCESS(rc2))
+                pImage->Backend->pfnSetParentTimeStamp(pImage->pvBackendData, &ts);
+
+            rc2 = pImage->Backend->pfnSetParentFilename(pImage->pvBackendData, pDisk->pLast->pszFilename);
         }
 
         if (VBOX_SUCCESS(rc))
