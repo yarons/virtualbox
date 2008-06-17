41f6a2fb270d581528fccc90f205ea897c59f080
Sander van Leeuwen <noreply@oracle.com>
2008-06-17 11:41:16 +0000

    Wrong assertion
    svn:sync-xref-src-repo-rev: r32115

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllBth.h 9757 2008-06-17 11:39:36Z noreply@oracle.com $ */
+/* $Id: PGMAllBth.h 9758 2008-06-17 11:41:16Z noreply@oracle.com $ */
 /** @file
  * VBox - Page Manager, Shadow+Guest Paging Template - All context code.
  *
@@ -3068,8 +3068,6 @@ PGM_BTH_DECL(int, SyncCR3)(PVM pVM, uint64_t cr0, uint64_t cr3, uint64_t cr4, bo
             /* Fetch the pgm pool shadow descriptor if the shadow pdpte is present. */
             if (pPdptDst->a[iPDPTE].n.u1Present) 
                 pShwPde = pgmPoolGetPage(pPool, pPdptDst->a[iPDPTE].u & X86_PDPE_PG_MASK);
-            else
-                Assert(pPDSrc == NULL);
 
             if (pPDSrc == NULL)
             {
