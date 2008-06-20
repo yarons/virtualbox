3844a6b07383a0cd951f6b521e4cc88c572076bf
Sander van Leeuwen <noreply@oracle.com>
2008-06-20 09:27:40 +0000

    Unassemble fix for 64 bits code
    svn:sync-xref-src-repo-rev: r32222

--- src/VBox/Debugger/DBGCEmulateCodeView.cpp
+++ src/VBox/Debugger/DBGCEmulateCodeView.cpp
@@ -1,4 +1,4 @@
-/** $Id: DBGCEmulateCodeView.cpp 9834 2008-06-20 08:24:49Z noreply@oracle.com $ */
+/** $Id: DBGCEmulateCodeView.cpp 9840 2008-06-20 09:27:40Z noreply@oracle.com $ */
 /** @file
  * DBGC - Debugger Console, CodeView / WinDbg Emulation.
  */
@@ -908,9 +908,23 @@ static DECLCALLBACK(int) dbgcCmdUnassemble(PCDBGCCMD pCmd, PDBGCCMDHLP pCmdHlp, 
     {
         if (!DBGCVAR_ISPOINTER(pDbgc->DisasmPos.enmType))
         {
-            pDbgc->DisasmPos.enmType     = DBGCVAR_TYPE_GC_FAR;
-            pDbgc->SourcePos.u.GCFar.off = pDbgc->fRegCtxGuest ? CPUMGetGuestEIP(pVM) : CPUMGetHyperEIP(pVM);
-            pDbgc->SourcePos.u.GCFar.sel = pDbgc->fRegCtxGuest ? CPUMGetGuestCS(pVM)  : CPUMGetHyperCS(pVM);
+            PCPUMCTX pCtx;
+            int rc = CPUMQueryGuestCtxPtr(pVM, &pCtx);
+            AssertRC(rc);
+
+            if (    pDbgc->fRegCtxGuest
+                &&  CPUMIsGuestIn64BitCodeEx(pCtx))
+            {
+                pDbgc->DisasmPos.enmType    = DBGCVAR_TYPE_GC_FLAT;
+                pDbgc->SourcePos.u.GCFlat   = CPUMGetGuestRIP(pVM);
+            }
+            else
+            {
+                pDbgc->DisasmPos.enmType     = DBGCVAR_TYPE_GC_FAR;
+                pDbgc->SourcePos.u.GCFar.off = pDbgc->fRegCtxGuest ? CPUMGetGuestEIP(pVM) : CPUMGetHyperEIP(pVM);
+                pDbgc->SourcePos.u.GCFar.sel = pDbgc->fRegCtxGuest ? CPUMGetGuestCS(pVM)  : CPUMGetHyperCS(pVM);
+            }
+
             if (pDbgc->fRegCtxGuest)
                 fFlags |= DBGF_DISAS_FLAGS_CURRENT_GUEST;
             else
