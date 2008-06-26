12ab1897d20045e85de91bebb0e121d4d3cc439a
Sander van Leeuwen <noreply@oracle.com>
2008-06-26 14:09:13 +0000

    pgmPoolTrackClearPageUser fix for
    svn:sync-xref-src-repo-rev: r32384

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPool.cpp 9953 2008-06-26 13:47:21Z noreply@oracle.com $ */
+/* $Id: PGMAllPool.cpp 9955 2008-06-26 14:09:13Z noreply@oracle.com $ */
 /** @file
  * PGM Shadow Page Pool.
  */
@@ -2514,8 +2514,11 @@ static void pgmPoolTrackClearPageUser(PPGMPOOL pPool, PPGMPOOLPAGE pPage, PCPGMP
         case PGMPOOLKIND_PAE_PD_FOR_PAE_PD:
         case PGMPOOLKIND_64BIT_PD_FOR_64BIT_PD:
         case PGMPOOLKIND_64BIT_PDPT_FOR_64BIT_PDPT:
-        case PGMPOOLKIND_64BIT_PML4_FOR_64BIT_PML4:
             u.pau64[pUser->iUserTable] = 0;
+            break;
+
+        case PGMPOOLKIND_64BIT_PML4_FOR_64BIT_PML4:
+            /* There are no references to this page; it's loaded into CR3. */
             break;
 
         default:
