b9882542fa77423ecdcd12115b6d14347fc55a00
Sander van Leeuwen <noreply@oracle.com>
2008-06-27 11:30:35 +0000

    Enabled rdmsr/wrmsr emulation again. (VT-x & AMD-v regression fixed)
    svn:sync-xref-src-repo-rev: r32433

--- src/VBox/VMM/VMMAll/EMAll.cpp
+++ src/VBox/VMM/VMMAll/EMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: EMAll.cpp 9984 2008-06-27 09:58:09Z noreply@oracle.com $ */
+/* $Id: EMAll.cpp 9989 2008-06-27 11:30:35Z noreply@oracle.com $ */
 /** @file
  * EM - Execution Monitor(/Manager) - All contexts
  */
@@ -18,7 +18,6 @@
  * Clara, CA 95054 USA or visit http://www.sun.com if you need
  * additional information or have any questions.
  */
-
 
 /*******************************************************************************
 *   Header Files                                                               *
@@ -2528,10 +2527,8 @@ DECLINLINE(int) emInterpretInstructionCPU(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCO
         INTERPRET_CASE(OP_CLTS,Clts);
         INTERPRET_CASE(OP_MONITOR, Monitor);
         INTERPRET_CASE(OP_MWAIT, MWait);
-#ifdef VBOX_WITH_MSR_EMULATION
         INTERPRET_CASE(OP_RDMSR, Rdmsr);
         INTERPRET_CASE(OP_WRMSR, Wrmsr);
-#endif
         INTERPRET_CASE_EX_PARAM3(OP_ADD,Add, AddSub, EMEmulateAdd);
         INTERPRET_CASE_EX_PARAM3(OP_SUB,Sub, AddSub, EMEmulateSub);
         INTERPRET_CASE(OP_ADC,Adc);
