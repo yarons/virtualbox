989afd9d2bb768941056a06fe3fad11694e7d36e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-07-04 20:13:35 +0000

    fixed comment.
    svn:sync-xref-src-repo-rev: r32851

--- src/VBox/HostDrivers/Support/SUPDRV.h
+++ src/VBox/HostDrivers/Support/SUPDRV.h
@@ -1,4 +1,4 @@
-/* $Revision: 10250 $ */
+/* $Revision: 10251 $ */
 /** @file
  * VirtualBox Support Driver - Internal header.
  */
@@ -198,7 +198,7 @@ __END_DECLS
  */
 #elif defined(RT_OS_FREEBSD)
 
-/* No log API in OS/2 only COM port. */
+/* debug printf */
 # define OSDBGPRINT(a) printf a
 
 
