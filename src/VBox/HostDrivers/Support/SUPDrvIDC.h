5995b566d85bb43bdb1b0c676a1784e7ea32d351
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-07-05 00:38:39 +0000

    bad constant.
    svn:sync-xref-src-repo-rev: r32870

--- src/VBox/HostDrivers/Support/SUPDrvIDC.h
+++ src/VBox/HostDrivers/Support/SUPDrvIDC.h
@@ -1,4 +1,4 @@
-/* $Id: SUPDrvIDC.h 10258 2008-07-04 23:31:26Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPDrvIDC.h 10264 2008-07-05 00:38:39Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox Support Driver - Inter-Driver Communciation (IDC) definitions.
  */
@@ -44,7 +44,7 @@
 #ifdef RT_OS_WINDOWS
 # define SUP_IDC_CODE(Function)                 CTL_CODE(FILE_DEVICE_UNKNOWN, (Function) + 2542, METHOD_BUFFERED, FILE_WRITE_ACCESS)
 #else
-# define SUP_IDC_CODE(Function)                 ( UINT32_C(0xc0ffee) | (uint32_t)(0x000000ff & (Function)) )
+# define SUP_IDC_CODE(Function)                 ( UINT32_C(0xc0ffee00) | (uint32_t)(0x000000ff & (Function)) )
 #endif
 
 
