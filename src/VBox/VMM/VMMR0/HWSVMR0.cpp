8175b271cc64a8497608df4aa9de86e71ae57f88
Sander van Leeuwen <noreply@oracle.com>
2008-07-08 11:14:24 +0000

    Extra assertion
    svn:sync-xref-src-repo-rev: r32987

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWSVMR0.cpp 10353 2008-07-08 11:12:52Z noreply@oracle.com $ */
+/* $Id: HWSVMR0.cpp 10354 2008-07-08 11:14:24Z noreply@oracle.com $ */
 /** @file
  * HWACCM SVM - Host Context Ring 0.
  */
@@ -626,7 +626,10 @@ HWACCMR0DECL(int) SVMR0LoadGuestState(PVM pVM, CPUMCTX *pCtx)
             pVMCB->guest.u64CR3             = pCtx->cr3;
         }
         else
+        {
             pVMCB->guest.u64CR3             = PGMGetHyperCR3(pVM);
+            Assert(pVMCB->guest.u64CR3);
+        }
     }
 
     if (pVM->hwaccm.s.fContextUseFlags & HWACCM_CHANGED_GUEST_CR4)
