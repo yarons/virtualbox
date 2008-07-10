49c0edee70b4290f1a2dbd2d5cbebf1c37b76742
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2008-07-10 07:08:24 +0000

    Solaris/r0drv: fixed lockkernel. Found this while exploring vboxvfs mapping, though currently unused.
    svn:sync-xref-src-repo-rev: r33125

--- src/VBox/Runtime/r0drv/solaris/memobj-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/memobj-r0drv-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: memobj-r0drv-solaris.c 8528 2008-05-02 07:09:28Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: memobj-r0drv-solaris.c 10457 2008-07-10 07:08:24Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IPRT - Ring-0 Memory Objects, Solaris.
  */
@@ -113,12 +113,12 @@ int rtR0MemObjNativeFree(RTR0MEMOBJ pMem)
         case RTR0MEMOBJTYPE_LOCK:
         {
             struct as *addrSpace;
-            if (pMemSolaris->Core.u.Lock.R0Process == NIL_RTR0PROCESS)
-                addrSpace = &kas;
-            else
+            if (pMemSolaris->Core.u.Lock.R0Process != NIL_RTR0PROCESS)
+            {
                 addrSpace = ((proc_t *)pMemSolaris->Core.u.Lock.R0Process)->p_as;
-
-            as_pageunlock(addrSpace, pMemSolaris->ppShadowPages, pMemSolaris->Core.pv, pMemSolaris->Core.cb, S_WRITE);
+                as_pageunlock(addrSpace, pMemSolaris->ppShadowPages, pMemSolaris->Core.pv, pMemSolaris->Core.cb, S_WRITE);
+            }
+            /* Nothing to unlock for kernel addresses. */
             break;
         }
 
@@ -299,28 +299,12 @@ int rtR0MemObjNativeLockKernel(PPRTR0MEMOBJINTERNAL ppMem, void *pv, size_t cb)
     if (!pMemSolaris)
         return VERR_NO_MEMORY;
 
-    caddr_t virtAddr = (caddr_t)((uintptr_t)pv & (uintptr_t)PAGEMASK);
-    page_t **ppl;
+    /* Nothing to do here for kernel addresses. */
 
-    /* Lock down kernel pages */
-    int rc = as_pagelock(&kas, &ppl, virtAddr, cb, S_WRITE);
-    if (!rc)
-    {
-        if (ppl)
-        {
-            pMemSolaris->Core.u.Lock.R0Process = NIL_RTR0PROCESS;   /* means kernel, see rtR0MemObjNativeFree() */
-            pMemSolaris->ppShadowPages = ppl;
-            *ppMem = &pMemSolaris->Core;
-            return VINF_SUCCESS;
-        }
-
-        as_pageunlock(&kas, ppl, virtAddr, cb, S_WRITE);
-        cmn_err(CE_NOTE, "rtR0MemObjNativeLockKernel: failed to get shadow pages\n");
-    }
-    else
-        cmn_err(CE_NOTE,"rtR0MemObjNativeLockKernel: as_pagelock failed rc=%d\n", rc);
-    rtR0MemObjDelete(&pMemSolaris->Core);
-    return VERR_LOCK_FAILED;
+    pMemSolaris->Core.u.Lock.R0Process = RTR0ProcHandleSelf();
+    pMemSolaris->ppShadowPages = NULL;
+    *ppMem = &pMemSolaris->Core;
+    return VINF_SUCCESS;    
 }
 
 
