940fed8345447485e621a9ddd9139716f5f365d6
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-07-11 19:42:41 +0000

    Fixed two completely nutty SUPR0ObjRelease where the spinlock was acquired first. Fortunately these calls were in untrodden error paths.
    svn:sync-xref-src-repo-rev: r33240

--- src/VBox/Devices/Network/SrvIntNetR0.cpp
+++ src/VBox/Devices/Network/SrvIntNetR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: SrvIntNetR0.cpp 10548 2008-07-11 19:36:43Z knut.osmundsen@oracle.com $ */
+/* $Id: SrvIntNetR0.cpp 10549 2008-07-11 19:42:41Z knut.osmundsen@oracle.com $ */
 /** @file
  * Internal networking - The ring 0 service.
  */
@@ -1204,12 +1204,8 @@ static int intnetNetworkClose(PINTNETNETWORK pNetwork, PSUPDRVSESSION pSession)
     LogFlow(("intnetNetworkClose: pNetwork=%p pSession=%p\n", pNetwork, pSession));
     AssertPtrReturn(pSession, VERR_INVALID_PARAMETER);
     AssertPtrReturn(pNetwork, VERR_INVALID_PARAMETER);
-    PINTNET         pIntNet = pNetwork->pIntNet;
-    RTSPINLOCKTMP   Tmp = RTSPINLOCKTMP_INITIALIZER;
 
-    RTSpinlockAcquire(pIntNet->Spinlock, &Tmp);
     int rc = SUPR0ObjRelease(pNetwork->pvObj, pSession);
-    RTSpinlockRelease(pIntNet->Spinlock, &Tmp);
     LogFlow(("intnetNetworkClose: return %Vrc\n", rc));
     return rc;
 }
@@ -1345,11 +1341,7 @@ static int intnetOpenNetwork(PINTNET pIntNet, PSUPDRVSESSION pSession, const cha
                         if (RT_SUCCESS(rc))
                             *ppNetwork = pCur;
                         else
-                        {
-                            RTSpinlockAcquire(pIntNet->Spinlock, &Tmp);
                             SUPR0ObjRelease(pCur->pvObj, pSession);
-                            RTSpinlockRelease(pIntNet->Spinlock, &Tmp);
-                        }
                     }
                 }
                 else
