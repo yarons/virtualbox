61667607500a51faa096ab221dc6a0aaf882b6b2
Sander van Leeuwen <noreply@oracle.com>
2008-07-14 14:53:52 +0000

    Logging update
    svn:sync-xref-src-repo-rev: r33298

--- src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
+++ src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
@@ -1,4 +1,4 @@
-/* $Id: IOMAllMMIO.cpp 10538 2008-07-11 17:53:49Z noreply@oracle.com $ */
+/* $Id: IOMAllMMIO.cpp 10600 2008-07-14 14:53:52Z noreply@oracle.com $ */
 /** @file
  * IOM - Input / Output Monitor - Guest Context.
  */
@@ -1487,7 +1487,7 @@ IOMDECL(int) IOMInterpretINSEx(PVM pVM, PCPUMCTXCORE pRegFrame, uint32_t uPort, 
         return VINF_EM_RAW_EMULATE_INSTR;
     }
 
-    Log(("IOM: rep ins%d port %#x count %d\n", cbTransfer * 8, uPort, cTransfers));
+    Log(("IOM: rep ins%d port %#x count %d -> GCPtr %VGv\n", cbTransfer * 8, uPort, cTransfers, GCPtrDst));
     if (cTransfers > 1)
     {
         /* If the device supports string transfers, ask it to do as
