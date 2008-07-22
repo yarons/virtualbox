b634c8af0be010cf9b3e8249840cd499200fed93
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-07-22 09:44:31 +0000

    VMM+SUPDrv: Changed the VMMR0EntryEx interface to also take the session as an argument, this means breaking the current IOC interface and thus a major driver version change.
    svn:sync-xref-src-repo-rev: r33583

--- src/VBox/HostDrivers/Support/SUPDrvIOC.h
+++ src/VBox/HostDrivers/Support/SUPDrvIOC.h
@@ -1,4 +1,4 @@
-/* $Revision: 10732 $ */
+/* $Revision: 10805 $ */
 /** @file
  * VirtualBox Support Driver - IOCtl definitions.
  */
@@ -181,7 +181,7 @@ typedef SUPREQHDR *PSUPREQHDR;
 /** Current interface version.
  * The upper 16-bit is the major version, the the lower the minor version.
  * When incompatible changes are made, the upper major number has to be changed. */
-#define SUPDRV_IOC_VERSION                              0x00080001
+#define SUPDRV_IOC_VERSION                              0x00090000
 
 /** SUP_IOCTL_COOKIE. */
 typedef struct SUPCOOKIE
--- src/VBox/HostDrivers/Support/SUPLib.cpp
+++ src/VBox/HostDrivers/Support/SUPLib.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPLib.cpp 10732 2008-07-17 15:52:48Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPLib.cpp 10805 2008-07-22 09:44:31Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Common code.
  */
@@ -211,9 +211,9 @@ SUPR3DECL(int) SUPInit(PSUPDRVSESSION *ppSession /* NULL */, size_t cbReserve /*
         CookieReq.Hdr.rc = VERR_INTERNAL_ERROR;
         strcpy(CookieReq.u.In.szMagic, SUPCOOKIE_MAGIC);
         CookieReq.u.In.u32ReqVersion = SUPDRV_IOC_VERSION;
-        const uint32_t MinVersion = (SUPDRV_IOC_VERSION & 0xffff0000) == 0x00080000
+        const uint32_t MinVersion = /*(SUPDRV_IOC_VERSION & 0xffff0000) == 0x00080000
                                   ? 0x00080001
-                                  : SUPDRV_IOC_VERSION & 0xffff0000;
+                                  : */ SUPDRV_IOC_VERSION & 0xffff0000;
         CookieReq.u.In.u32MinVersion = MinVersion;
         rc = suplibOsIOCtl(SUP_IOCTL_COOKIE, &CookieReq, SUP_IOCTL_COOKIE_SIZE);
         if (    RT_SUCCESS(rc)
