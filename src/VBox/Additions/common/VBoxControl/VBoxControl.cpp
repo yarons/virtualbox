c888d4e243db0d22c955b99fbc53befb0eb0e760
Michael Thayer <noreply@oracle.com>
2008-08-04 14:29:30 +0000

    Additions/common: handle guestproperty set with no value correctly in VBoxControl too
    svn:sync-xref-src-repo-rev: r34035

--- src/VBox/Additions/common/VBoxControl/VBoxControl.cpp
+++ src/VBox/Additions/common/VBoxControl/VBoxControl.cpp
@@ -1,4 +1,4 @@
-/** $Id: VBoxControl.cpp 10985 2008-07-30 13:39:37Z noreply@oracle.com $ */
+/** $Id: VBoxControl.cpp 11104 2008-08-04 14:29:30Z noreply@oracle.com $ */
 /** @file
  * VBoxControl - Guest Additions Command Line Management Interface
  */
@@ -974,19 +974,16 @@ static int setGuestProperty(int argc, char *argv[])
     const char *pszFlags = NULL;
     if (2 == argc)
     {
-        pszName = argv[0];
         pszValue = argv[1];
     }
     else if (3 == argc)
     {
-        pszName = argv[0];
         if (strcmp(argv[1], "-flags") != 0)
             usageOK = false;
         pszFlags = argv[2];
     }
     else if (4 == argc)
     {
-        pszName = argv[0];
         pszValue = argv[1];
         if (strcmp(argv[2], "-flags") != 0)
             usageOK = false;
@@ -999,6 +996,8 @@ static int setGuestProperty(int argc, char *argv[])
         usage(GUEST_PROP);
         return 1;
     }
+    /* This is always needed. */
+    pszName = argv[0];
 
 /*
  * Do the actual setting.
