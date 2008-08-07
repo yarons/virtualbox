3fe9ecc82eade3a7c7e54ca1e5ac0e7717afa8b1
Frank Mehnert <noreply@oracle.com>
2008-08-07 17:12:06 +0000

    build fix
    svn:sync-xref-src-repo-rev: r34265

--- src/VBox/Devices/PC/DevRTC.cpp
+++ src/VBox/Devices/PC/DevRTC.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevRTC.cpp 11213 2008-08-07 16:19:39Z knut.osmundsen@oracle.com $ */
+/* $Id: DevRTC.cpp 11214 2008-08-07 17:12:06Z noreply@oracle.com $ */
 /** @file
  * Motorola MC146818 RTC/CMOS Device.
  */
@@ -53,6 +53,8 @@
 #include <iprt/asm.h>
 #include <iprt/assert.h>
 #include <iprt/string.h>
+
+#include "Builtins.h"
 
 struct RTCState;
 typedef struct RTCState RTCState;
