a9b690a72eb18a34a50ab11881c9e43e8c0130b0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-08-08 17:03:34 +0000

    RTMpGetCount/solaris: Prefer _SC_NPROCESSORS_MAX over _SC_NPROCESSORS_CONF.
    svn:sync-xref-src-repo-rev: r34356

--- src/VBox/Runtime/r3/solaris/mp-solaris.cpp
+++ src/VBox/Runtime/r3/solaris/mp-solaris.cpp
@@ -1,4 +1,4 @@
-/* $Id: mp-solaris.cpp 11256 2008-08-08 14:17:59Z klaus.espenlaub@oracle.com $ */
+/* $Id: mp-solaris.cpp 11272 2008-08-08 17:03:34Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Multiprocessor, Solaris.
  */
@@ -188,13 +188,18 @@ RTDECL(uint32_t) RTMpGetMaxFrequency(RTCPUID idCpu)
     return rtMpSolarisGetFrequency(idCpu, "clock_MHz");
 }
 
+
 RTDECL(RTCPUID) RTMpGetCount(void)
 {
     /*
      * Solaris has sysconf.
      */
-    return sysconf(_SC_NPROCESSORS_CONF);
+    int cCpus = sysconf(_SC_NPROCESSORS_MAX)
+    if (cCpus < 0)
+        cCpus = sysconf(_SC_NPROCESSORS_CONF);
+    return cCpus;
 }
+
 
 RTDECL(RTCPUID) RTMpGetOnlineCount(void)
 {
