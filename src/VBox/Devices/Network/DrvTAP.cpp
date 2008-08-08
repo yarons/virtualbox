8d5aba57b2f4dcfb4a6c1b5cc1bf365a80dded00
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-08-08 13:41:43 +0000

    DrvTAP: don't leave mess behind.
    svn:sync-xref-src-repo-rev: r34323

--- src/VBox/Devices/Network/DrvTAP.cpp
+++ src/VBox/Devices/Network/DrvTAP.cpp
@@ -1,4 +1,4 @@
-/** $Id: DrvTAP.cpp 11242 2008-08-08 13:14:37Z aleksey.ilyushin@oracle.com $ */
+/** $Id: DrvTAP.cpp 11246 2008-08-08 13:41:43Z knut.osmundsen@oracle.com $ */
 /** @file
  * Universial TAP network transport driver.
  */
@@ -285,9 +285,6 @@ static DECLCALLBACK(int) drvTAPAsyncIoThread(PPDMDRVINS pDrvIns, PPDMTHREAD pThr
 #endif
             if (VBOX_SUCCESS(rc))
             {
-                // Assertion removed to be able to support jumbo frames.
-                //AssertMsg(cbRead <= 1536, ("cbRead=%d\n", cbRead));
-
                 /*
                  * Wait for the device to have space for this frame.
                  * Most guests use frame-sized receive buffers, hence non-zero cbMax
