30cc5ed6cccca565edec2fa414098efbc018464e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-08-28 15:35:52 +0000

    iprt: Pass TS_RUN to thread_create (non-vbi, solaris ring-0).
    svn:sync-xref-src-repo-rev: r35548

--- src/VBox/Runtime/r0drv/solaris/thread2-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/thread2-r0drv-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: thread2-r0drv-solaris.c 8921 2008-05-19 15:40:57Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: thread2-r0drv-solaris.c 11770 2008-08-28 15:35:52Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Threads (Part 2), Ring-0 Driver, Solaris.
  */
@@ -102,7 +102,7 @@ int rtThreadNativeCreate(PRTTHREADINT pThreadInt, PRTNATIVETHREAD pNativeThread)
 {
     int rc;
     kthread_t* pKernThread = thread_create(NULL, NULL, rtThreadNativeMain, pThreadInt, 0,
-                                           curproc, LMS_USER, minclsyspri);
+                                           curproc, TS_RUN, minclsyspri);
     if (pKernThread)
     {
         *pNativeThread = (RTNATIVETHREAD)pKernThread;
