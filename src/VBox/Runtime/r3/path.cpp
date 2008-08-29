d4656f2d58e1f065e9de817596b3edf12a111726
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-08-29 17:48:20 +0000

    path.cpp: build fix.
    svn:sync-xref-src-repo-rev: r35673

--- src/VBox/Runtime/r3/path.cpp
+++ src/VBox/Runtime/r3/path.cpp
@@ -1,4 +1,4 @@
-/* $Id: path.cpp 11836 2008-08-29 16:52:20Z knut.osmundsen@oracle.com $ */
+/* $Id: path.cpp 11839 2008-08-29 17:48:20Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Path Manipulation.
  */
@@ -155,7 +155,7 @@ RTDECL(size_t) RTPathParse(const char *pszPath, size_t *pcchDir, ssize_t *poffNa
 #if defined(RT_OS_WINDOWS) || defined(RT_OS_OS2)
             case ':':
                 pszName = psz + 1;
-                cchRoot = pszName - psz;
+                offRoot = pszName - psz;
                 break;
 
             case '\\':
