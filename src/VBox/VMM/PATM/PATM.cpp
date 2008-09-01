4b5e3e0946fe38c251ed6c1e300fe96808e2fc7b
Sander van Leeuwen <noreply@oracle.com>
2008-09-01 15:40:03 +0000

    More logging
    svn:sync-xref-src-repo-rev: r35806

--- src/VBox/VMM/PATM/PATM.cpp
+++ src/VBox/VMM/PATM/PATM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PATM.cpp 11925 2008-09-01 15:03:24Z noreply@oracle.com $ */
+/* $Id: PATM.cpp 11929 2008-09-01 15:40:03Z noreply@oracle.com $ */
 /** @file
  * PATM - Dynamic Guest OS Patching Manager
  *
@@ -6180,7 +6180,9 @@ PATMR3DECL(int) PATMR3HandleTrap(PVM pVM, PCPUMCTX pCtx, RTRCPTR pEip, RTGCPTR *
     }
 
     Log2(("pPatchBlockGC %VRv - pEip %VRv corresponding GC address %VRv\n", PATCHCODE_PTR_GC(&pPatch->patch), pEip, pNewEip));
-
+#ifdef LOG_ENABLED
+    CPUMR3DisasmInstr(pVM, pCtx, pNewEip, "PATCHRET: ");
+#endif
     if (pNewEip >= pPatch->patch.pPrivInstrGC && pNewEip < pPatch->patch.pPrivInstrGC + pPatch->patch.cbPatchJump)
     {
         /* We can't jump back to code that we've overwritten with a 5 byte jump! */
