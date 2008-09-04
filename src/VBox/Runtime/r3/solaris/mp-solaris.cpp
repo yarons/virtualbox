07d63c3c630d316e62816a96ac3b5432c68d8b01
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-09-04 19:46:14 +0000

    iprt: Solaris R3 implementation of RTMp*Present*.
    svn:sync-xref-src-repo-rev: r36046

--- src/VBox/Runtime/r3/solaris/mp-solaris.cpp
+++ src/VBox/Runtime/r3/solaris/mp-solaris.cpp
@@ -1,4 +1,4 @@
-/* $Id: mp-solaris.cpp 11719 2008-08-27 18:16:22Z aleksey.ilyushin@oracle.com $ */
+/* $Id: mp-solaris.cpp 12105 2008-09-04 19:46:14Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Multiprocessor, Solaris.
  */
@@ -223,6 +223,12 @@ RTDECL(bool) RTMpIsCpuOnline(RTCPUID idCpu)
 }
 
 
+RTDECL(bool) RTMpIsCpuPresent(RTCPUID idCpu)
+{
+    return p_online() != -1;
+}
+
+
 RTDECL(RTCPUID) RTMpGetCount(void)
 {
     /*
@@ -262,5 +268,34 @@ RTDECL(PRTCPUSET) RTMpGetOnlineSet(PRTCPUSET pSet)
         if (RTMpIsCpuOnline(idCpu))
             RTCpuSetAdd(pSet, idCpu);
     return pSet;
+}
+
+
+RTDECL(PRTCPUSET) RTMpGetPresentSet(PRTCPUSET pSet)
+{
+#ifdef RT_STRICT
+    long cCpusPresent = 0;
+#endif
+    RTCpuSetEmpty(pSet);
+    RTCPUID cCpus = RTMpGetCount();
+    for (RTCPUID idCpu = 0; idCpu < cCpus; idCpu++)
+        if (RTMpIsCpuPresent(idCpu))
+        {
+            RTCpuSetAdd(pSet, idCpu);
+#ifdef RT_STRICT
+            cCpusPresent++;
+#endif
+        }
+    Assert(cCpusPresent == RTMpGetPresentCount());
+    return pSet;
+}
+
+
+RTDECL(RTCPUID) RTMpGetPresentCount(void)
+{
+    /*
+     * Solaris has sysconf.
+     */
+    return sysconf(_SC_NPROCESSORS_CONF);
 }
 
