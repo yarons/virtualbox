1407a47505ed56e23c3dc8a04e20244f50044841
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-09-05 21:00:37 +0000

    VBoxDrv/win: Use the IPRT versions of AssertMsg[12].
    svn:sync-xref-src-repo-rev: r36123

--- src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
+++ src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-win.cpp 11814 2008-08-29 12:47:44Z noreply@oracle.com $ */
+/* $Id: SUPDrv-win.cpp 12157 2008-09-05 21:00:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Windows NT specifics.
  */
@@ -564,15 +564,6 @@ static NTSTATUS     VBoxDrvNtErr2NtStatus(int rc)
 }
 
 
-
-/** @todo move this to IPRT */
-RTDECL(void) AssertMsg1(const char *pszExpr, unsigned uLine, const char *pszFile, const char *pszFunction)
-{
-    DbgPrint("\n!!Assertion Failed!!\n"
-             "Expression: %s\n"
-             "Location  : %s(%d) %s\n",
-             pszExpr, pszFile, uLine, pszFunction);
-}
 
 /** @todo use the nocrt stuff? */
 int VBOXCALL mymemcmp(const void *pv1, const void *pv2, size_t cb)
