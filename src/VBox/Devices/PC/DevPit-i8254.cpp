5468be68ee12c2f73d6e8053797961fb62117039
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-09-22 13:21:22 +0000

    DevPit-i8254.cpp: file header.
    svn:sync-xref-src-repo-rev: r36927

--- src/VBox/Devices/PC/DevPit-i8254.cpp
+++ src/VBox/Devices/PC/DevPit-i8254.cpp
@@ -1,4 +1,4 @@
-/** $Id: DevPit-i8254.cpp 11269 2008-08-08 16:24:48Z knut.osmundsen@oracle.com $ */
+/* $Id: DevPit-i8254.cpp 12643 2008-09-22 13:21:22Z knut.osmundsen@oracle.com $ */
 /** @file
  * DevPIT-i8254 - Intel 8254 Programmable Interval Timer (PIT) And Dummy Speaker Device.
  */
