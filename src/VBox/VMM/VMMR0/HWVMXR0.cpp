711334db952b57849ba2842ed6d2b71358264169
Sander van Leeuwen <noreply@oracle.com>
2008-09-25 09:32:13 +0000

    More logging
    svn:sync-xref-src-repo-rev: r37020

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWVMXR0.cpp 12708 2008-09-25 09:14:29Z noreply@oracle.com $ */
+/* $Id: HWVMXR0.cpp 12711 2008-09-25 09:32:13Z noreply@oracle.com $ */
 /** @file
  * HWACCM VMX - Host Context Ring 0.
  */
@@ -1372,6 +1372,10 @@ ResumeExecution:
 
             VMXReadVMCS(VMX_VMCS_HOST_FIELD_CS, &val);
             Log(("VMX_VMCS_HOST_FIELD_CS %08x\n", val));
+
+            VMXReadVMCS(VMX_VMCS_GUEST_RFLAGS, &val);
+            Log(("VMX_VMCS_GUEST_RFLAGS %08x\n", val));
+
             if (val < gdtr.cbGdt)
             {
                 pDesc  = &((PX86DESCHC)gdtr.pGdt)[val >> X86_SEL_SHIFT_HC];
