6574995af405115088c165be5d74bf31787920ae
Frank Mehnert <noreply@oracle.com>
2008-09-26 12:15:55 +0000

    ATA: disabled SCSI_GET_EVENT_STATUS_NOTIFICATION passthrough command as we cannot emulate this command properly if passthrough is disabled
    svn:sync-xref-src-repo-rev: r37099

--- src/VBox/Devices/Storage/DevATA.cpp
+++ src/VBox/Devices/Storage/DevATA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevATA.cpp 12740 2008-09-25 14:24:38Z noreply@oracle.com $ */
+/* $Id: DevATA.cpp 12766 2008-09-26 12:15:55Z noreply@oracle.com $ */
 /** @file
  * VBox storage devices: ATA/ATAPI controller device (disk and cdrom).
  */
@@ -2423,10 +2423,15 @@ static void atapiParseCmdPassthrough(ATADevState *s)
             cbTransfer = ataBE2H_U16(pbPacket + 7);
             uTxDir = PDMBLOCKTXDIR_FROM_DEVICE;
             goto sendcmd;
+#if 0
+        /* Disable this passthrough command. The guest should fallback to other means to
+         * detect the disk status. We cannot emulate this command properly when in non-
+         * passthrough mode. */
         case SCSI_GET_EVENT_STATUS_NOTIFICATION:
             cbTransfer = ataBE2H_U16(pbPacket + 7);
             uTxDir = PDMBLOCKTXDIR_FROM_DEVICE;
             goto sendcmd;
+#endif
         case SCSI_GET_PERFORMANCE:
             cbTransfer = s->uATARegLCyl | (s->uATARegHCyl << 8); /* use ATAPI transfer length */
             uTxDir = PDMBLOCKTXDIR_FROM_DEVICE;
