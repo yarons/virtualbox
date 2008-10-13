9277bb047963a124dca0695f30ac524f1d85359b
Sander van Leeuwen <noreply@oracle.com>
2008-10-13 10:02:04 +0000

    Missing shadow cases for EPT.
    svn:sync-xref-src-repo-rev: r37747

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllBth.h 13188 2008-10-11 01:58:30Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMAllBth.h 13203 2008-10-13 10:02:04Z noreply@oracle.com $ */
 /** @file
  * VBox - Page Manager, Shadow+Guest Paging Template - All context code.
  *
@@ -1436,7 +1436,7 @@ DECLINLINE(void) PGM_BTH_NAME(SyncPageWorker)(PVM pVM, PSHWPTE pPteDst, GSTPDE P
                     PteDst.n.u1Execute   = 1;
                     PteDst.n.u1IgnorePAT = 1;
                     PteDst.n.u3EMT       = VMX_EPT_MEMTYPE_WB;
-                    /* PteDst.n.u1Write = 0 && PteDst.n.u1Big = 0 */
+                    /* PteDst.n.u1Write = 0 && PteDst.n.u1Size = 0 */
 #else
                     PteDst.u = (PteSrc.u & ~(X86_PTE_PAE_PG_MASK | X86_PTE_AVL_MASK | X86_PTE_PAT | X86_PTE_PCD | X86_PTE_PWT | X86_PTE_RW))
                              | (HCPhys & X86_PTE_PAE_PG_MASK);
@@ -1485,7 +1485,7 @@ DECLINLINE(void) PGM_BTH_NAME(SyncPageWorker)(PVM pVM, PSHWPTE pPteDst, GSTPDE P
                     PteDst.n.u1Execute   = 1;
                     PteDst.n.u1IgnorePAT = 1;
                     PteDst.n.u3EMT       = VMX_EPT_MEMTYPE_WB;
-                    /* PteDst.n.u1Big = 0 */
+                    /* PteDst.n.u1Size = 0 */
 #else
                     PteDst.u = (PteSrc.u & ~(X86_PTE_PAE_PG_MASK | X86_PTE_AVL_MASK | X86_PTE_PAT | X86_PTE_PCD | X86_PTE_PWT))
                              | (HCPhys & X86_PTE_PAE_PG_MASK);
