722d59cafd97796dc184d490aa826d0f8d11d01e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-10-13 21:25:50 +0000

    #1865: More polish.
    svn:sync-xref-src-repo-rev: r37819

--- src/VBox/VMM/VMMR0/PGMR0.cpp
+++ src/VBox/VMM/VMMR0/PGMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMR0.cpp 13085 2008-10-08 15:12:13Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMR0.cpp 13236 2008-10-13 21:25:50Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Ring-0.
  */
@@ -162,28 +162,4 @@ VMMR0DECL(int) PGMR0Trap0eHandlerNestedPaging(PVM pVM, PGMMODE enmShwPagingMode,
     STAM_PROFILE_STOP_EX(&pVM->pgm.s.StatRZTrap0e, pVM->pgm.s.CTX_SUFF(pStatTrap0eAttribution), a);
     return rc;
 }
-
-
-#ifdef VBOX_WITH_2X_4GB_ADDR_SPACE
-
-/** darwin stub */
-VMMR0DECL(int) PGMR0DynMapGCPage(PVM pVM, RTGCPHYS GCPhys, void **ppv)
-{
-    Assert(!(GCPhys & PAGE_OFFSET_MASK));
-    return VERR_NOT_IMPLEMENTED;
-}
-
-/** darwin stub */
-VMMR0DECL(int) PGMR0DynMapGCPageEx(PVM pVM, RTGCPHYS GCPhys, void **ppv)
-{
-    return VERR_NOT_IMPLEMENTED;
-}
-
-/** darwin stub */
-VMMR0DECL(int) PGMR0DynMapHCPage(PVM pVM, RTHCPHYS HCPhys, void **ppv)
-{
-    return VERR_NOT_IMPLEMENTED;
-}
-
-#endif /* VBOX_WITH_2X_4GB_ADDR_SPACE */
 
