c7b6ef0f3834193d17180169f8cd0e310a5ffc6e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-10-15 21:30:44 +0000

    build fix.
    svn:sync-xref-src-repo-rev: r37983

--- src/VBox/Runtime/r0drv/darwin/assert-r0drv-darwin.cpp
+++ src/VBox/Runtime/r0drv/darwin/assert-r0drv-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: assert-r0drv-darwin.cpp 13306 2008-10-15 21:17:04Z knut.osmundsen@oracle.com $ */
+/* $Id: assert-r0drv-darwin.cpp 13310 2008-10-15 21:30:44Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT -  Assertion Workers, Ring-0 Drivers, Darwin.
  */
@@ -38,6 +38,8 @@
 #include <iprt/log.h>
 #include <iprt/string.h>
 #include <iprt/stdarg.h>
+#include <iprt/asm.h>
+
 
 /*******************************************************************************
 *   Global Variables                                                           *
