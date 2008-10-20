7529bf9cbc72267081e2f973355f81c025491195
Sander van Leeuwen <noreply@oracle.com>
2008-10-20 15:47:38 +0000

    Accidental commit
    svn:sync-xref-src-repo-rev: r38153

--- src/VBox/Devices/Graphics/DevVGA.h
+++ src/VBox/Devices/Graphics/DevVGA.h
@@ -1,4 +1,4 @@
-/* $Id: DevVGA.h 13401 2008-10-20 15:46:38Z noreply@oracle.com $ */
+/* $Id: DevVGA.h 13402 2008-10-20 15:47:38Z noreply@oracle.com $ */
 /** @file
  * DevVGA - VBox VGA/VESA device, internal header.
  */
@@ -260,11 +260,7 @@ typedef struct VGAState {
     /** Indicates if the R0 extensions are enabled or not. */
     bool                        fR0Enabled;
     /** Flag indicating that there are dirty bits. This is used to optimize the handler resetting. */
-    bool                        fHasDirtyBits;
-    /** Flag indicating that the VGA memory in the 0xa0000-0xbffff region has been remapped to allow direct access. */
-    bool                        fRemappedVGA;
-    bool                        padding9[3];
-
+    bool                        fHaveDirtyBits;
     /** Pointer to vgaGCLFBAccessHandler(). */
     RTRCPTR                     RCPtrLFBHandler;
     /** Bitmap tracking dirty pages. */
