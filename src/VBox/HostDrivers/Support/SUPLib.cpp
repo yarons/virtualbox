3701676efcaf280732884f6bd07b0fd1fb7391c9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-10-22 09:55:45 +0000

    SUPLib: Require minimum version 9.0001 now.
    svn:sync-xref-src-repo-rev: r38284

--- src/VBox/HostDrivers/Support/SUPLib.cpp
+++ src/VBox/HostDrivers/Support/SUPLib.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPLib.cpp 13280 2008-10-15 11:29:07Z klaus.espenlaub@oracle.com $ */
+/* $Id: SUPLib.cpp 13482 2008-10-22 09:55:45Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Common code.
  */
@@ -268,9 +268,9 @@ SUPR3DECL(int) SUPR3Init(PSUPDRVSESSION *ppSession)
         CookieReq.Hdr.rc = VERR_INTERNAL_ERROR;
         strcpy(CookieReq.u.In.szMagic, SUPCOOKIE_MAGIC);
         CookieReq.u.In.u32ReqVersion = SUPDRV_IOC_VERSION;
-        const uint32_t MinVersion = /*(SUPDRV_IOC_VERSION & 0xffff0000) == 0x00080000
-                                  ? 0x00080001
-                                  : */ SUPDRV_IOC_VERSION & 0xffff0000;
+        const uint32_t MinVersion = (SUPDRV_IOC_VERSION & 0xffff0000) == 0x00090000
+                                  ? 0x00090001
+                                  :  SUPDRV_IOC_VERSION & 0xffff0000;
         CookieReq.u.In.u32MinVersion = MinVersion;
         rc = suplibOsIOCtl(&g_supLibData, SUP_IOCTL_COOKIE, &CookieReq, SUP_IOCTL_COOKIE_SIZE);
         if (    RT_SUCCESS(rc)
