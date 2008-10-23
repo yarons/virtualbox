4a8e45b492bfa08bb0c3bc00c01eee43840bd0b4
Sander van Leeuwen <noreply@oracle.com>
2008-10-23 11:27:32 +0000

    Wrap the RTPowerSignalEvent call.
    svn:sync-xref-src-repo-rev: r38371

--- src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
+++ src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-win.cpp 13496 2008-10-22 14:56:33Z noreply@oracle.com $ */
+/* $Id: SUPDrv-win.cpp 13529 2008-10-23 11:27:32Z noreply@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Windows NT specifics.
  */
@@ -82,6 +82,10 @@ static VOID     _stdcall   VBoxPowerDispatchCallback(PVOID pCallbackContext, PVO
 static NTSTATUS _stdcall   VBoxDrvNtNotSupportedStub(PDEVICE_OBJECT pDevObj, PIRP pIrp);
 static NTSTATUS            VBoxDrvNtErr2NtStatus(int rc);
 
+/*******************************************************************************
+*   External Functions                                                         *
+*******************************************************************************/
+DECLASM(int)   UNWIND_WRAP(RTPowerSignalEvent)(RTPOWEREVENT enmEvent);
 
 /*******************************************************************************
 *   Exported Functions                                                         *
@@ -541,7 +545,7 @@ VOID _stdcall VBoxPowerDispatchCallback(PVOID pCallbackContext, PVOID pArgument1
             dprintf(("VBoxPowerDispatchCallback: resumed!\n"));
 
         /* Inform any clients that have registered themselves with IPRT. */
-        RTPowerSignalEvent(((unsigned)pArgument2 == 0) ? RTPOWEREVENT_SUSPEND : RTPOWEREVENT_RESUME);
+        UNWIND_WRAP(RTPowerSignalEvent)(((unsigned)pArgument2 == 0) ? RTPOWEREVENT_SUSPEND : RTPOWEREVENT_RESUME);
     }
 }
 
