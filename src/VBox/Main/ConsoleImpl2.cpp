5e3971a55809b041e580beedd7ca76aa34124541
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2008-10-27 14:28:37 +0000

    Solaris/Main: VBoxNetFlt/Zones: drop promiscuous setting for VMs in non global zones.
    svn:sync-xref-src-repo-rev: r38483

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl2.cpp 13580 2008-10-27 14:04:18Z noreply@oracle.com $ */
+/* $Id: ConsoleImpl2.cpp 13581 2008-10-27 14:28:37Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation
  *
@@ -55,6 +55,11 @@
 
 #include <VBox/com/string.h>
 #include <VBox/com/array.h>
+
+#if defined(RT_OS_SOLARIS) && defined(VBOX_WITH_NETFLT)
+# include <zone.h>
+#endif
+
 
 /*
  * VC++ 8 / amd64 has some serious trouble with this function.
@@ -1184,6 +1189,12 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
                         *pszColon = '\0';
 
                     const char *pszTrunk = szTrunk;
+
+                    /* Zone access controls*/
+                    zoneid_t ZoneId = getzoneid();
+                    if (ZoneId != GLOBAL_ZONEID)
+                        rc = CFGMR3InsertInteger(pCfg, "QuietlyIgnoreAllPromisc", true);   RC_CHECK();
+
 # elif defined(RT_OS_WINDOWS)
                     ComPtr<IHostNetworkInterfaceCollection> coll;
                     hrc = host->COMGETTER(NetworkInterfaces)(coll.asOutParam());    H();
