91566fef32aba0bdee65840c9820ce17edf9d7e9
Sander van Leeuwen <noreply@oracle.com>
2008-11-03 15:56:59 +0000

    Compile fix
    svn:sync-xref-src-repo-rev: r38732

--- src/VBox/VMM/testcase/tstVMMFork.cpp
+++ src/VBox/VMM/testcase/tstVMMFork.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstVMMFork.cpp 13742 2008-11-03 12:19:40Z noreply@oracle.com $ */
+/* $Id: tstVMMFork.cpp 13757 2008-11-03 15:56:59Z noreply@oracle.com $ */
 /** @file
  * VMM Fork Test.
  */
@@ -136,7 +136,7 @@ int main(int argc, char* argv[])
                 RTPrintf(TESTCASE ": fork() returned fine.\n");
                 RTPrintf(TESTCASE ": testing VM after fork.\n");
                 PVMREQ pReq1 = NULL;
-                rc = VMR3ReqCall(pVM, &pReq1, RT_INDEFINITE_WAIT, (PFNRT)VMMDoTest, 1, pVM);
+                rc = VMR3ReqCall(pVM, VMREQDEST_ALL, &pReq1, RT_INDEFINITE_WAIT, (PFNRT)VMMDoTest, 1, pVM);
                 AssertRC(rc);
                 VMR3ReqFree(pReq1);
 
