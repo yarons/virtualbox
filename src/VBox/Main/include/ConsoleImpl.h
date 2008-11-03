14c6d79851098093225f832016c335a34841f2ea
Sander van Leeuwen <noreply@oracle.com>
2008-11-03 16:34:35 +0000

    Backed out 38734.
    svn:sync-xref-src-repo-rev: r38737

--- src/VBox/Main/include/ConsoleImpl.h
+++ src/VBox/Main/include/ConsoleImpl.h
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.h 13759 2008-11-03 16:13:04Z noreply@oracle.com $ */
+/* $Id: ConsoleImpl.h 13762 2008-11-03 16:34:35Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -493,11 +493,6 @@ private:
 #ifdef VBOX_WITH_GUEST_PROPS
     static DECLCALLBACK(int)    doGuestPropNotification (void *pvExtension, uint32_t,
                                                          void *pvParms, uint32_t cbParms);
-    HRESULT doEnumerateGuestProperties (INPTR BSTR aPatterns,
-                                        ComSafeArrayOut(BSTR, aNames),
-                                        ComSafeArrayOut(BSTR, aValues),
-                                        ComSafeArrayOut(ULONG64, aTimestamps),
-                                        ComSafeArrayOut(BSTR, aFlags));
 #endif
 
     bool mSavedStateDataLoaded : 1;
