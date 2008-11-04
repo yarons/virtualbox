a758a1ad4dbf04813978277882f5b26ddd26193a
Michael Thayer <noreply@oracle.com>
2008-11-04 10:13:43 +0000

    HostServices/GuestProperties: integer type fix
    svn:sync-xref-src-repo-rev: r38765

--- src/VBox/HostServices/GuestProperties/testcase/tstGuestPropSvc.cpp
+++ src/VBox/HostServices/GuestProperties/testcase/tstGuestPropSvc.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstGuestPropSvc.cpp 13779 2008-11-04 09:46:13Z noreply@oracle.com $ */
+/* $Id: tstGuestPropSvc.cpp 13781 2008-11-04 10:13:43Z noreply@oracle.com $ */
 /** @file
  *
  * Testcase for the guest property service.
@@ -217,7 +217,7 @@ const char *apcszValueBlock[] =
  */
 uint64_t au64TimestampBlock[] =
 {
-    0, 999, 999999, 999999999999, 0
+    0, 999, 999999, UINT64_C(999999999999), 0
 };
 
 /**
@@ -615,7 +615,7 @@ getProperties[] =
     { "TEST NAME", "TEST VALUE\0RDONLYHOST", sizeof("TEST VALUE\0RDONLYHOST"),
       true, true, 999999 },
     { "/test/name", "/test/value\0RDONLYGUEST",
-      sizeof("/test/value\0RDONLYGUEST"), true, true, 999999999999 },
+      sizeof("/test/value\0RDONLYGUEST"), true, true, UINT64_C(999999999999) },
     { "Green", "Go!\0READONLY", sizeof("Go!\0READONLY"), true, false, 0 },
     { "Blue", "What on earth...?\0", sizeof("What on earth...?\0"), true,
       false, 0 },
