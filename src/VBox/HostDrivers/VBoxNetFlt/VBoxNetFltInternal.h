cbb48b203373cceb73265b8f99839d3d54334460
Mikhail Sennikovsky <noreply@oracle.com>
2008-11-14 20:59:31 +0000

    NetFlt/win: cleaning/bugfixing for rediscovery/reconnect functionality
    svn:sync-xref-src-repo-rev: r39344

--- src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
+++ src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFltInternal.h 14208 2008-11-14 13:26:25Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxNetFltInternal.h 14226 2008-11-14 20:59:31Z noreply@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Internal Header.
  */
@@ -201,7 +201,7 @@ typedef struct VBOXNETFLTINS
 # if defined(VBOX_NETFLT_ONDEMAND_BIND)
         uint8_t abPadding[192];
 # else
-        uint8_t abPadding[512];
+        uint8_t abPadding[576];
 # endif
 #elif defined(RT_OS_LINUX)
         uint8_t abPadding[192];
