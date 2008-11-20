2e86b0d588855a68b037ba136f267f8c1ca9c3d5
Frank Mehnert <noreply@oracle.com>
2008-11-20 13:21:35 +0000

    try to fix 64-bit Linux builds
    svn:sync-xref-src-repo-rev: r39641

--- src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
+++ src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFltInternal.h 14236 2008-11-17 08:37:16Z noreply@oracle.com $ */
+/* $Id: VBoxNetFltInternal.h 14410 2008-11-20 13:21:35Z noreply@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Internal Header.
  */
@@ -204,7 +204,7 @@ typedef struct VBOXNETFLTINS
         uint8_t abPadding[1024];
 # endif
 #elif defined(RT_OS_LINUX)
-        uint8_t abPadding[192];
+        uint8_t abPadding[224];
 #else
         uint8_t abPadding[64];
 #endif
