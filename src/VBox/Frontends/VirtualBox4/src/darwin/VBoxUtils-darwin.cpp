7dc72a9c4fa4308679ea4b75c6d03b028ddf4a3b
Christian Poetzsch <noreply@oracle.com>
2008-11-21 12:40:17 +0000

    crOpenGLÂ­OSX: bad bug
    svn:sync-xref-src-repo-rev: r39723

--- src/VBox/Frontends/VirtualBox4/src/darwin/VBoxUtils-darwin.cpp
+++ src/VBox/Frontends/VirtualBox4/src/darwin/VBoxUtils-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxUtils-darwin.cpp 14397 2008-11-20 11:01:52Z noreply@oracle.com $ */
+/* $Id: VBoxUtils-darwin.cpp 14453 2008-11-21 12:40:17Z noreply@oracle.com $ */
 /** @file
  * Qt GUI - Utility Classes and Functions specific to Darwin.
  */
@@ -375,7 +375,7 @@ OSStatus darwinRegionHandler (EventHandlerCallRef aInHandlerCallRef, EventRef aI
 
 OSStatus darwinOverlayWindowHandler (EventHandlerCallRef aInHandlerCallRef, EventRef aInEvent, void *aInUserData)
 {
-    if (aInUserData)
+    if (!aInUserData)
         return ::CallNextEventHandler (aInHandlerCallRef, aInEvent);
 
     UInt32 eventClass = ::GetEventClass (aInEvent);
@@ -423,7 +423,9 @@ OSStatus darwinOverlayWindowHandler (EventHandlerCallRef aInHandlerCallRef, Even
             HISize s;
             if (GetEventParameter (aInEvent, kEventParamDimensions, typeHISize, NULL, sizeof (s), NULL, &s) != noErr)
                 return noErr;
+            ChangeWindowGroupAttributes (GetWindowGroup (w), 0, kWindowGroupAttrMoveTogether);
             SizeWindow (w, s.width, s.height, true);
+            ChangeWindowGroupAttributes (GetWindowGroup (w), kWindowGroupAttrMoveTogether, 0);
             return noErr;
         }
     }
