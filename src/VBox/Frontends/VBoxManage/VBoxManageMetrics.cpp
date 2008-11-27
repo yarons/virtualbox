3f6ce93036c677e39834206edea645325a5153e0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-11-27 18:47:42 +0000

    VBoxManageMetrics.cpp: file header
    svn:sync-xref-src-repo-rev: r40050

--- src/VBox/Frontends/VBoxManage/VBoxManageMetrics.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageMetrics.cpp
@@ -1,6 +1,6 @@
-/* $Id: VBoxManageMetrics.cpp 14735 2008-11-27 18:46:52Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxManageMetrics.cpp 14736 2008-11-27 18:47:42Z knut.osmundsen@oracle.com $ */
 /** @file
- * VBoxManage - Metrics handling code.
+ * VBoxManage - The 'metrics' command.
  */
 
 /*
