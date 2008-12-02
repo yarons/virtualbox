0f3526fd5f11f5c32ec6cdbd37b273aa2b7c42a1
Nikolay Igotty <noreply@oracle.com>
2008-12-02 15:41:18 +0000

    fixed math related failures, auto stack alignment fixes
    svn:sync-xref-src-repo-rev: r40261

--- src/recompiler_new/Sun/testmath.c
+++ src/recompiler_new/Sun/testmath.c
@@ -1,4 +1,4 @@
-/* $Id: testmath.c 14424 2008-11-20 16:12:33Z noreply@oracle.com $ */
+/* $Id: testmath.c 14916 2008-12-02 15:41:18Z noreply@oracle.com $ */
 /** @file
  * Testcase for the no-crt math stuff.
  */
@@ -277,8 +277,8 @@ extern int testmath(void)
     CHECKLL(testremainder(), 1);
 
 
-#ifndef RT_OS_DARWIN
-    /** @todo: figure out why it doesn't work on MacOS */
+    /* Only works in extended precision, while double precesion is defaul on BSD (including Darwin) */
+    set_cw(0x37f);
     CHECK(rintl(1.0L), 1.0);
     CHECK(rintl(1.4L), 1.0);
     CHECK(rintl(1.3L), 1.0);
@@ -286,7 +286,7 @@ extern int testmath(void)
     CHECK(rintl(3123.1232L), 3123.0);
     CHECK(rint(3985.13454), 3985.0);
     CHECK(rintf(9999.999), 10000.0);
-#endif
+    set_cw(0x27f);
 
     CHECK(sinl(1.0L),  0.84147098480789650664L);
     lrd = 180.0L;
