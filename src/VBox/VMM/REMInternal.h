917ba9cb9d34aa23ff5059e6be06f52e5492e608
Nikolay Igotty <noreply@oracle.com>
2008-12-04 23:27:46 +0000

    new_recompiler: cleanup, optimization, compile with the right tool - gets rid of the nasty bug with bootmenu
    svn:sync-xref-src-repo-rev: r40381

--- src/VBox/VMM/REMInternal.h
+++ src/VBox/VMM/REMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: REMInternal.h 13565 2008-10-24 17:48:59Z knut.osmundsen@oracle.com $ */
+/* $Id: REMInternal.h 15009 2008-12-04 23:27:46Z noreply@oracle.com $ */
 /** @file
  * REM - Internal header file.
  */
@@ -287,7 +287,7 @@ void    remR3ProtectCode(CPUState *env, RTGCPTR GCPtr);
 void    remR3ChangeCpuMode(CPUState *env);
 void    remR3DmaRun(CPUState *env);
 void    remR3TimersRun(CPUState *env);
-int     remR3NotifyTrap(CPUState *env, uint32_t uTrap, uint32_t uErrorCode, uint32_t pvNextEIP);
+int     remR3NotifyTrap(CPUState *env, uint32_t uTrap, uint32_t uErrorCode, RTGCPTR pvNextEIP);
 void    remR3TrapStat(CPUState *env, uint32_t uTrap);
 void    remR3CpuId(CPUState *env, unsigned uOperator, void *pvEAX, void *pvEBX, void *pvECX, void *pvEDX);
 void    remR3RecordCall(CPUState *env);
