00f08a369a448cde23dadd7c2244ebe2708bd5d0
Sander van Leeuwen <noreply@oracle.com>
2008-12-09 14:52:36 +0000

    Restrict instruction emulation for 64 bits guests on 32 bits hosts.
    svn:sync-xref-src-repo-rev: r40591

--- src/VBox/VMM/VMMAll/EMAll.cpp
+++ src/VBox/VMM/VMMAll/EMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: EMAll.cpp 15161 2008-12-09 13:03:20Z noreply@oracle.com $ */
+/* $Id: EMAll.cpp 15181 2008-12-09 14:52:36Z noreply@oracle.com $ */
 /** @file
  * EM - Execution Monitor(/Manager) - All contexts
  */
@@ -706,6 +706,7 @@ static int emInterpretOrXorAnd(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCORE pRegFram
                                PFNEMULATEPARAM3 pfnEmulate)
 {
     OP_PARAMVAL param1, param2;
+
     int rc = DISQueryParamVal(pRegFrame, pCpu, &pCpu->param1, &param1, PARAM_DEST);
     if(RT_FAILURE(rc))
         return VERR_EM_INTERPRETER;
@@ -800,8 +801,12 @@ static int emInterpretLockOrXorAnd(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCORE pReg
                                    uint32_t *pcbSize, PFNEMULATELOCKPARAM3 pfnEmulate)
 {
     void *pvParam1;
-
     OP_PARAMVAL param1, param2;
+
+#ifdef HC_ARCH_BITS == 32
+    Assert(pCpu->param1.size <= 4);
+#endif
+
     int rc = DISQueryParamVal(pRegFrame, pCpu, &pCpu->param1, &param1, PARAM_DEST);
     if(RT_FAILURE(rc))
         return VERR_EM_INTERPRETER;
@@ -1409,6 +1414,10 @@ static int emInterpretStosWD(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCORE pRegFrame,
 static int emInterpretCmpXchg(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCORE pRegFrame, RTGCPTR pvFault, uint32_t *pcbSize)
 {
     OP_PARAMVAL param1, param2;
+
+#ifdef HC_ARCH_BITS == 32
+    Assert(pCpu->param1.size <= 4);
+#endif
 
     /* Source to make DISQueryParamVal read the register value - ugly hack */
     int rc = DISQueryParamVal(pRegFrame, pCpu, &pCpu->param1, &param1, PARAM_SOURCE);
@@ -2918,6 +2927,20 @@ DECLINLINE(int) emInterpretInstructionCPU(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCO
         STAM_COUNTER_INC(&pVM->em.s.CTX_SUFF(pStats)->CTX_MID_Z(Stat,FailedPrefix));
         return VERR_EM_INTERPRETER;
     }
+
+#ifdef HC_ARCH_BITS == 32
+    if (CPUMIsGuestIn64BitCode(pVM, pRegFrame))
+    {
+        /* Unable to emulate in 32 bits mode. 
+         * Whitelisted instructions are safe.
+         */
+        if (    pCpu->param1.size > 4
+            &&  pCpu->pCurInstr->opcode != OP_STOSWD
+            &&  pCpu->pCurInstr->opcode != OP_MOV
+            &&  pCpu->pCurInstr->opcode != OP_CMPXCHG8B)
+            return VERR_EM_INTERPRETER;
+    }
+#endif
 
     int rc;
 #if (defined(VBOX_STRICT) || defined(LOG_ENABLED))
