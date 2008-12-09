e5dec1920951967a17d7db6f25970991c9e16fe7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-12-09 14:11:35 +0000

    #1865: don't ever return invalid ring-0 address on 2x4G systems, simply return NIL_RTR0PTR.
    svn:sync-xref-src-repo-rev: r40584

--- src/VBox/VMM/PGM.cpp
+++ src/VBox/VMM/PGM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGM.cpp 15172 2008-12-09 14:02:13Z noreply@oracle.com $ */
+/* $Id: PGM.cpp 15174 2008-12-09 14:11:35Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor. (Mixing stuff here, not good?)
  */
@@ -2009,7 +2009,11 @@ VMMR3DECL(void) PGMR3Relocate(PVM pVM, RTGCINTPTR offDelta)
      * The Zero page.
      */
     pVM->pgm.s.pvZeroPgR0 = MMHyperR3ToR0(pVM, pVM->pgm.s.pvZeroPgR3);
-    AssertRelease(pVM->pgm.s.pvZeroPgR0);
+#ifdef VBOX_WITH_2X_4GB_ADDR_SPACE
+    AssertRelease(pVM->pgm.s.pvZeroPgR0 != NIL_RTR0PTR || !VMMIsHwVirtExtForced(pVM));
+#else
+    AssertRelease(pVM->pgm.s.pvZeroPgR0 != NIL_RTR0PTR);
+#endif
 
     /*
      * Physical and virtual handlers.
