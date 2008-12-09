096f58f91ac69b56c83fb73b1257e02e07b911c2
Sander van Leeuwen <noreply@oracle.com>
2008-12-09 14:55:41 +0000

    Duh
    svn:sync-xref-src-repo-rev: r40593

--- src/VBox/VMM/PGM.cpp
+++ src/VBox/VMM/PGM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGM.cpp 15182 2008-12-09 14:53:53Z noreply@oracle.com $ */
+/* $Id: PGM.cpp 15183 2008-12-09 14:55:41Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor. (Mixing stuff here, not good?)
  */
@@ -2888,7 +2888,7 @@ static int pgmR3ModeDataInit(PVM pVM, bool fResolveGCAndR0)
     /* The shadow part of the nested callback mode depends on the host paging mode (AMD-V only). */
     switch(pVM->pgm.s.enmHostMode)
     {
-#ifdef HC_ARCH_BITS == 32
+#if HC_ARCH_BITS == 32
     case SUPPAGINGMODE_32_BIT:
     case SUPPAGINGMODE_32_BIT_GLOBAL:
         for (unsigned i=PGM_TYPE_REAL;i<=PGM_TYPE_PAE;i++)
