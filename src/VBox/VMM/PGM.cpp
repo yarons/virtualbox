cbaee7e7cff118290076e4415b43705e17a87a12
Sander van Leeuwen <noreply@oracle.com>
2008-12-09 14:53:53 +0000

    Argh
    svn:sync-xref-src-repo-rev: r40592

--- src/VBox/VMM/PGM.cpp
+++ src/VBox/VMM/PGM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGM.cpp 15177 2008-12-09 14:36:23Z noreply@oracle.com $ */
+/* $Id: PGM.cpp 15182 2008-12-09 14:53:53Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor. (Mixing stuff here, not good?)
  */
@@ -2918,7 +2918,7 @@ static int pgmR3ModeDataInit(PVM pVM, bool fResolveGCAndR0)
         break;
 #endif /* HC_ARCH_BITS != 32 */
 
-#if HC_ARCH_BITS == 64 || RT_OS_DARWIN
+#if HC_ARCH_BITS == 64 || defined(RT_OS_DARWIN)
     case SUPPAGINGMODE_AMD64:
     case SUPPAGINGMODE_AMD64_GLOBAL:
     case SUPPAGINGMODE_AMD64_NX:
