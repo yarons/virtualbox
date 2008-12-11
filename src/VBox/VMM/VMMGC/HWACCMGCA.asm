2af76bc70171cc16cf06966ca5340a3fea6f4e87
Sander van Leeuwen <noreply@oracle.com>
2008-12-11 18:40:59 +0000

    Messed up return code.
    svn:sync-xref-src-repo-rev: r40763

--- src/VBox/VMM/VMMGC/HWACCMGCA.asm
+++ src/VBox/VMM/VMMGC/HWACCMGCA.asm
@@ -1,4 +1,4 @@
-; $Id: HWACCMGCA.asm 15331 2008-12-11 18:38:19Z noreply@oracle.com $
+; $Id: HWACCMGCA.asm 15332 2008-12-11 18:40:59Z noreply@oracle.com $
 ;; @file
 ; VMXM - GC vmx helpers
 ;
@@ -307,10 +307,10 @@ ALIGN(16)
 
 .vmstart64_end:
     ; Signal that we're going back to 32 bits mode!
-    mov      eax, VMX_VMCS_CTRL_EXIT_CONTROLS
-    vmread   rdx, rax
+    mov      ebx, VMX_VMCS_CTRL_EXIT_CONTROLS
+    vmread   rdx, rbx
     and      rdx, ~VMX_VMCS_CTRL_EXIT_CONTROLS_HOST_AMD64
-    vmwrite  rax, rdx
+    vmwrite  rbx, rdx
 
     ; Write back the data and disable the VMCS
     vmclear qword [rsp]  ;Pushed pVMCS
