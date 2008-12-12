f89f857510b30f5320a0b7b72a4df95b28c633af
Sander van Leeuwen <noreply@oracle.com>
2008-12-12 14:08:56 +0000

    Paranoia
    svn:sync-xref-src-repo-rev: r40807

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWVMXR0.cpp 15364 2008-12-12 13:19:02Z noreply@oracle.com $ */
+/* $Id: HWVMXR0.cpp 15368 2008-12-12 14:08:56Z noreply@oracle.com $ */
 /** @file
  * HWACCM VMX - Host Context Ring 0.
  */
@@ -3542,6 +3542,8 @@ VMMR0DECL(int) VMXR0Execute64BitsHandler(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx, R
     /* @todo This code is not guest SMP safe (hyper context) */
     AssertReturn(pVM->cCPUs == 1, VERR_ACCESS_DENIED);
     AssertReturn(pVM->hwaccm.s.pfnHost32ToGuest64R0, VERR_INTERNAL_ERROR);
+    Assert(pVCpu->hwaccm.s.vmx.VMCSCache.Write.cValidEntries <= RT_ELEMENTS(pVCpu->hwaccm.s.vmx.VMCSCache.Write.aField));
+    Assert(pVCpu->hwaccm.s.vmx.VMCSCache.Read.cValidEntries <= RT_ELEMENTS(pVCpu->hwaccm.s.vmx.VMCSCache.Read.aField));
 
     pCpu = HWACCMR0GetCurrentCpu();
     pPageCpuPhys = RTR0MemObjGetPagePhysAddr(pCpu->pMemObj, 0);
