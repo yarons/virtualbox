6c89b506006a730226b65ed5bfa8923893d4b93e
Frank Mehnert <noreply@oracle.com>
2008-12-15 21:34:32 +0000

    fixed header
    svn:sync-xref-src-repo-rev: r41048

--- src/VBox/Installer/solaris/VBoxZoneAccess.c
+++ src/VBox/Installer/solaris/VBoxZoneAccess.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxZoneAccess.c 13982 2008-11-08 15:46:38Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: VBoxZoneAccess.c 15550 2008-12-15 21:34:32Z noreply@oracle.com $ */
 /** @file
  * VBoxZoneAccess - Hack that keeps vboxdrv referenced for granting zone access, Solaris hosts.
  */
@@ -13,15 +13,6 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
- *
- * The contents of this file may alternatively be used under the terms
- * of the Common Development and Distribution License Version 1.0
- * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
- * VirtualBox OSE distribution, in which case the provisions of the
- * CDDL are applicable instead of those of the GPL.
- *
- * You may elect to license modified versions of this file under the
- * terms and conditions of either the GPL or the CDDL or both.
  *
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, CA 95054 USA or visit http://www.sun.com if you need
