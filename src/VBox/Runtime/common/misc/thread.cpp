a5dc1eb594adf9d651115c451d92040ea07e36e7
Nikolay Igotty <noreply@oracle.com>
2008-12-24 12:36:19 +0000

    restored waiting on non-waitable assert (things I do to please Knut :) )
    svn:sync-xref-src-repo-rev: r41315

--- src/VBox/Runtime/common/misc/thread.cpp
+++ src/VBox/Runtime/common/misc/thread.cpp
@@ -1,4 +1,4 @@
-/* $Id: thread.cpp 15740 2008-12-24 10:16:41Z noreply@oracle.com $ */
+/* $Id: thread.cpp 15747 2008-12-24 12:36:19Z noreply@oracle.com $ */
 /** @file
  * IPRT - Threads, common routines.
  */
@@ -1011,6 +1011,7 @@ static int rtThreadWait(RTTHREAD Thread, unsigned cMillies, int *prc, bool fAuto
             else
             {
                 rc = VERR_THREAD_NOT_WAITABLE;
+                AssertRC(rc);
             }
             rtThreadRelease(pThread);
         }
