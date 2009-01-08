8d56ce7c84183ab4e0242e1b58be0c721834f3e9
Sander van Leeuwen <noreply@oracle.com>
2009-01-08 12:09:23 +0000

    Compile fix
    svn:sync-xref-src-repo-rev: r41457

--- src/VBox/VMM/HWACCM.cpp
+++ src/VBox/VMM/HWACCM.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWACCM.cpp 15858 2009-01-08 11:56:52Z noreply@oracle.com $ */
+/* $Id: HWACCM.cpp 15859 2009-01-08 12:09:23Z noreply@oracle.com $ */
 /** @file
  * HWACCM - Intel/AMD VM Hardware Support Manager
  */
@@ -281,6 +281,10 @@ VMMR3DECL(int) HWACCMR3InitCPU(PVM pVM)
                              "/PROF/HWACCM/CPU%d/InGC", i);
         AssertRC(rc);
 
+# define HWACCM_REG_COUNTER(a, b) \
+        rc = STAMR3RegisterF(pVM, a, STAMTYPE_COUNTER, STAMVISIBILITY_ALWAYS, STAMUNIT_OCCURENCES, "Profiling of vmlaunch", b, i); \
+        AssertRC(rc);
+
 # if HC_ARCH_BITS == 32 && defined(VBOX_ENABLE_64_BITS_GUESTS) && !defined(VBOX_WITH_HYBRID_32BIT_KERNEL)
         rc = STAMR3RegisterF(pVM, &pVCpu->hwaccm.s.StatWorldSwitch3264, STAMTYPE_PROFILE, STAMVISIBILITY_USED, STAMUNIT_TICKS_PER_CALL, "Profiling of the 32/64 switcher",
                              "/PROF/HWACCM/CPU%d/Switcher3264", i);
@@ -288,10 +292,6 @@ VMMR3DECL(int) HWACCMR3InitCPU(PVM pVM)
         HWACCM_REG_COUNTER(&pVCpu->hwaccm.s.StatTimeoutResume,           "/HWACCM/CPU%d/Timeout/Resume");
         HWACCM_REG_COUNTER(&pVCpu->hwaccm.s.StatTimeoutSwitcher3264,     "/HWACCM/CPU%d/Timeout/Switcher3264");
 # endif
-
-# define HWACCM_REG_COUNTER(a, b) \
-        rc = STAMR3RegisterF(pVM, a, STAMTYPE_COUNTER, STAMVISIBILITY_ALWAYS, STAMUNIT_OCCURENCES, "Profiling of vmlaunch", b, i); \
-        AssertRC(rc);
 
         HWACCM_REG_COUNTER(&pVCpu->hwaccm.s.StatExitShadowNM,           "/HWACCM/CPU%d/Exit/Trap/Shw/#NM");
         HWACCM_REG_COUNTER(&pVCpu->hwaccm.s.StatExitGuestNM,            "/HWACCM/CPU%d/Exit/Trap/Gst/#NM");
