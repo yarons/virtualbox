c8e4f64cc008519175717db6127645a5baaa7794
Frank Mehnert <noreply@oracle.com>
2009-01-14 20:14:36 +0000

    fix OSE headers
    svn:sync-xref-src-repo-rev: r41598

--- src/VBox/Devices/Network/DevE1000Phy.cpp
+++ src/VBox/Devices/Network/DevE1000Phy.cpp
@@ -1,4 +1,4 @@
-/** $Id: DevE1000Phy.cpp 15953 2009-01-14 19:21:31Z noreply@oracle.com $ */
+/** $Id: DevE1000Phy.cpp 15955 2009-01-14 20:14:36Z noreply@oracle.com $ */
 /** @file
  * DevE1000Phy - Intel 82540EM Ethernet Controller Internal PHY Emulation.
  *
@@ -12,8 +12,17 @@
 /*
  * Copyright (C) 2007 Sun Microsystems, Inc.
  *
- * Sun Microsystems, Inc. confidential
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
  */
 
 #define LOG_GROUP LOG_GROUP_DEV_E1000
