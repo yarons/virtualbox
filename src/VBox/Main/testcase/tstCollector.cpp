d16cd9928d0e2c2e2aefcacfa252107a957c923f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-01-18 00:30:41 +0000

    Main: FreeBSD stubbing.
    svn:sync-xref-src-repo-rev: r41700

--- src/VBox/Main/testcase/tstCollector.cpp
+++ src/VBox/Main/testcase/tstCollector.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstCollector.cpp 14831 2008-11-30 10:31:16Z knut.osmundsen@oracle.com $ */
+/* $Id: tstCollector.cpp 16018 2009-01-18 00:30:41Z knut.osmundsen@oracle.com $ */
 
 /** @file
  *
@@ -21,23 +21,26 @@
  * additional information or have any questions.
  */
 
-#ifdef RT_OS_SOLARIS
-#include "../solaris/PerformanceSolaris.cpp"
+#ifdef RT_OS_DARWIN
+#include "../darwin/PerformanceDarwin.cpp"
+#endif
+#ifdef RT_OS_FREEBSD
+#include "../freebsd/PerformanceFreeBSD.cpp"
 #endif
 #ifdef RT_OS_LINUX
 #include "../linux/PerformanceLinux.cpp"
+#endif
+#ifdef RT_OS_OS2
+#include "../os2/PerformanceOS2.cpp"
+#endif
+#ifdef RT_OS_SOLARIS
+#include "../solaris/PerformanceSolaris.cpp"
 #endif
 #ifdef RT_OS_WINDOWS
 #define _WIN32_DCOM
 #include <objidl.h>
 #include <objbase.h>
 #include "../win/PerformanceWin.cpp"
-#endif
-#ifdef RT_OS_OS2
-#include "../os2/PerformanceOS2.cpp"
-#endif
-#ifdef RT_OS_DARWIN
-#include "../darwin/PerformanceDarwin.cpp"
 #endif
 
 #include <iprt/initterm.h>
