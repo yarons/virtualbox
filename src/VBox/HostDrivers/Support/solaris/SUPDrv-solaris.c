bffcfde7928f75782e3ec6fdcf7e289291b52477
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2009-01-22 12:04:46 +0000

    oops.
    svn:sync-xref-src-repo-rev: r41900

--- src/VBox/HostDrivers/Support/solaris/SUPDrv-solaris.c
+++ src/VBox/HostDrivers/Support/solaris/SUPDrv-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-solaris.c 16160 2009-01-22 12:03:33Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: SUPDrv-solaris.c 16161 2009-01-22 12:04:46Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Solaris specifics.
  */
@@ -757,7 +757,6 @@ static int VBoxDrvSolarisIOCtlSlow(PSUPDRVSESSION pSession, int iCmd, int Mode, 
      * Process the IOCtl.
      */
     rc = supdrvIOCtl(iCmd, &g_DevExt, pSession, pHdr);
-    PFNRT a = RTMpIsCpuWorkPending;
     
     /*
      * Copy ioctl data and output buffer back to user space.
