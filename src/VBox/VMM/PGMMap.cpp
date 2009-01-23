1ee36df61f6c98c9e7d5e13a8e039370adb0969e
Sander van Leeuwen <noreply@oracle.com>
2009-01-23 16:36:23 +0000

    Updates in preparation for PGM pool based paging everywhere.
    svn:sync-xref-src-repo-rev: r41982

--- src/VBox/VMM/PGMMap.cpp
+++ src/VBox/VMM/PGMMap.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMMap.cpp 16182 2009-01-22 17:30:28Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMMap.cpp 16203 2009-01-23 16:36:23Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager, Guest Context Mappings.
  */
@@ -60,7 +60,7 @@ static void pgmR3MapIntermediateDoOne(PVM pVM, uintptr_t uAddress, RTHCPHYS HCPh
 VMMR3DECL(int) PGMR3MapPT(PVM pVM, RTGCPTR GCPtr, uint32_t cb, PFNPGMRELOCATE pfnRelocate, void *pvUser, const char *pszDesc)
 {
     LogFlow(("PGMR3MapPT: GCPtr=%#x cb=%d pfnRelocate=%p pvUser=%p pszDesc=%s\n", GCPtr, cb, pfnRelocate, pvUser, pszDesc));
-    AssertMsg(pVM->pgm.s.pInterPD && pVM->pgm.s.pShw32BitPdR3, ("Paging isn't initialized, init order problems!\n"));
+    AssertMsg(pVM->pgm.s.pInterPD && pVM->pgm.s.pShwNestedRootR3, ("Paging isn't initialized, init order problems!\n"));
 
     /*
      * Validate input.
