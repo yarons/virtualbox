9c706fd03c3b56e7bae01ad0d91a424c7d20cf0e
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2009-01-26 10:03:46 +0000

    Devices/Serial: Data unavaiable while reading shouldn't terminate thread.
    svn:sync-xref-src-repo-rev: r42007

--- src/VBox/Devices/Serial/DrvHostSerial.cpp
+++ src/VBox/Devices/Serial/DrvHostSerial.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostSerial.cpp 15836 2009-01-07 15:45:20Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: DrvHostSerial.cpp 16217 2009-01-26 10:03:46Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBox stream I/O devices: Host serial driver
  *
@@ -757,7 +757,7 @@ static DECLCALLBACK(int) drvHostSerialRecvThread(PPDMDRVINS pDrvIns, PPDMTHREAD 
             rc = RTFileRead(pThis->DeviceFileR, abBuffer, sizeof(abBuffer), &cbRead);
             if (RT_FAILURE(rc))
             {
-                LogRel(("HostSerial#%d: Read failed with %Rrc, terminating the worker thread.\n", pDrvIns->iInstance, rc));
+                LogRel(("HostSerial#%d: (1) Read failed with %Rrc, terminating the worker thread.\n", pDrvIns->iInstance, rc));
                 rcThread = rc;
                 break;
             }
@@ -795,7 +795,11 @@ static DECLCALLBACK(int) drvHostSerialRecvThread(PPDMDRVINS pDrvIns, PPDMTHREAD 
             rc = RTFileRead(pThis->DeviceFile, abBuffer, sizeof(abBuffer), &cbRead);
             if (RT_FAILURE(rc))
             {
-                LogRel(("HostSerial#%d: Read failed with %Rrc, terminating the worker thread.\n", pDrvIns->iInstance, rc));
+                /* don't terminate worker thread when data unavailable */
+                if (rc == VERR_TRY_AGAIN)
+                    continue;
+
+                LogRel(("HostSerial#%d: (2) Read failed with %Rrc, terminating the worker thread.\n", pDrvIns->iInstance, rc));
                 rcThread = rc;
                 break;
             }
