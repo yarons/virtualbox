823a9ca9c555f0824368781403ec6a8108ef457d
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2009-02-10 16:48:40 +0000

    Frontend/BFE: more fixes, the Format config to DrvVD is no longer optional.
    svn:sync-xref-src-repo-rev: r42627

--- src/VBox/Frontends/VBoxBFE/VBoxBFE.cpp
+++ src/VBox/Frontends/VBoxBFE/VBoxBFE.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxBFE.cpp 16585 2009-02-09 13:38:40Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxBFE.cpp 16646 2009-02-10 16:48:40Z klaus.espenlaub@oracle.com $ */
 /** @file
  * Basic Frontend (BFE): VBoxBFE main routines.
  *
@@ -1677,6 +1677,14 @@ static DECLCALLBACK(int) vboxbfeConfigConstructor(PVM pVM, void *pvUser)
         {
             rc = CFGMR3InsertString(pCfg, "Format",       "SPF");                   UPDATE_RC();
         }
+        else if (!strcmp(RTPathExt(g_pszHdaFile), ".vdi"))
+        {
+            rc = CFGMR3InsertString(pCfg, "Format",       "VDI");                   UPDATE_RC();
+        }
+        else
+        {
+            rc = CFGMR3InsertString(pCfg, "Format",       "VMDK");                  UPDATE_RC();
+        }
     }
 
     if (g_pszHdbFile)
@@ -1695,6 +1703,14 @@ static DECLCALLBACK(int) vboxbfeConfigConstructor(PVM pVM, void *pvUser)
         if (g_fHdbSpf)
         {
             rc = CFGMR3InsertString(pCfg, "Format",       "SPF");                   UPDATE_RC();
+        }
+        else if (!strcmp(RTPathExt(g_pszHdbFile), ".vdi"))
+        {
+            rc = CFGMR3InsertString(pCfg, "Format",       "VDI");                   UPDATE_RC();
+        }
+        else
+        {
+            rc = CFGMR3InsertString(pCfg, "Format",       "VMDK");                  UPDATE_RC();
         }
     }
 
