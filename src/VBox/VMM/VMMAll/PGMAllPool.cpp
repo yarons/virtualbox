e4541064d4f285a6d946e7898c9ae9531d21fe03
Sander van Leeuwen <noreply@oracle.com>
2009-02-10 15:58:42 +0000

    Same for PGMPOOLKIND_PAE_PDPT_PHYS
    svn:sync-xref-src-repo-rev: r42624

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPool.cpp 16643 2009-02-10 15:57:02Z noreply@oracle.com $ */
+/* $Id: PGMAllPool.cpp 16644 2009-02-10 15:58:42Z noreply@oracle.com $ */
 /** @file
  * PGM Shadow Page Pool.
  */
@@ -3856,6 +3856,7 @@ static void pgmPoolTrackDeref(PPGMPOOL pPool, PPGMPOOLPAGE pPage)
 
         case PGMPOOLKIND_PAE_PDPT_FOR_32BIT:
         case PGMPOOLKIND_PAE_PDPT:
+        case PGMPOOLKIND_PAE_PDPT_PHYS:
 #endif
         case PGMPOOLKIND_64BIT_PDPT_FOR_PHYS:
         case PGMPOOLKIND_64BIT_PDPT_FOR_64BIT_PDPT:
