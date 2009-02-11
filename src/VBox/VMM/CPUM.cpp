7570e65a7e4c0541013e73080825ae52050a6fb4
Sander van Leeuwen <noreply@oracle.com>
2009-02-11 13:57:23 +0000

    Ignore CPU stepping when restoring a saved state.
    svn:sync-xref-src-repo-rev: r42658

--- src/VBox/VMM/CPUM.cpp
+++ src/VBox/VMM/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 15962 2009-01-15 12:33:49Z noreply@oracle.com $ */
+/* $Id: CPUM.cpp 16665 2009-02-11 13:57:23Z noreply@oracle.com $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -1022,9 +1022,14 @@ static DECLCALLBACK(int) cpumR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Versio
     rc = SSMR3GetMem(pSSM, &au32CpuIdSaved[0], sizeof(au32CpuIdSaved));
     if (RT_SUCCESS(rc))
     {
+        /* Ignore CPU stepping. */
+        au32CpuId[0]      &=  0xfffffff0;
+        au32CpuIdSaved[0] &=  0xfffffff0;
+
         /* Ignore APIC ID (AMD specs). */
         au32CpuId[5]      &= ~0xff000000;
         au32CpuIdSaved[5] &= ~0xff000000;
+
         /* Ignore the number of Logical CPUs (AMD specs). */
         au32CpuId[5]      &= ~0x00ff0000;
         au32CpuIdSaved[5] &= ~0x00ff0000;
