7f54454689582be05e3b7484a38388c62b087a57
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-02-20 02:16:49 +0000

    VBoxHDD: Hacked VDCopy so that 'VBoxManage clonehd nt4.vdi nt4.raw -format RAW' works.
    svn:sync-xref-src-repo-rev: r43128

--- src/VBox/Devices/Storage/VBoxHDD.cpp
+++ src/VBox/Devices/Storage/VBoxHDD.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxHDD.cpp 16962 2009-02-20 00:07:46Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxHDD.cpp 16963 2009-02-20 02:16:49Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxHDD - VBox HDD Container implementation.
  */
@@ -2015,7 +2015,12 @@ VBOXDDU_DECL(int) VDCopy(PVBOXHDD pDiskFrom, unsigned nImage, PVBOXHDD pDiskTo,
             rc = VDCreateDiff(pDiskTo, pszBackend, pszFilename, uImageFlagsFrom,
                               szComment, &ImageUuid, &ParentUuid, uOpenFlagsFrom & ~VD_OPEN_FLAGS_READONLY, NULL, NULL);
         } else {
-            rc = VDCreateBase(pDiskTo, pszBackend, pszFilename, enmTypeFrom,
+            VDIMAGETYPE enmTypeTo = enmTypeFrom; /** @todo Please, review this! It's an ugly hack I think... */
+            if (    !strcmp(pszBackend, "RAW")
+                &&  enmTypeTo != VD_IMAGE_TYPE_FIXED)
+                enmTypeTo = VD_IMAGE_TYPE_FIXED;
+
+            rc = VDCreateBase(pDiskTo, pszBackend, pszFilename, enmTypeTo,
                               cbSize, uImageFlagsFrom, szComment,
                               &PCHSGeometryFrom, &LCHSGeometryFrom,
                               NULL, uOpenFlagsFrom & ~VD_OPEN_FLAGS_READONLY, NULL, NULL);
