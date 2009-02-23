fadb6e5264dadcc36286f86be66587cafc50a7a1
Sander van Leeuwen <noreply@oracle.com>
2009-02-23 13:15:27 +0000

    Less logging
    svn:sync-xref-src-repo-rev: r43216

--- src/VBox/VMM/VMMAll/PGMAllMap.cpp
+++ src/VBox/VMM/VMMAll/PGMAllMap.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllMap.cpp 17015 2009-02-23 12:50:03Z noreply@oracle.com $ */
+/* $Id: PGMAllMap.cpp 17017 2009-02-23 13:15:27Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor - All context code.
  */
@@ -503,8 +503,6 @@ void pgmMapCheckShadowPDEs(PVM pVM, PPGMPOOLPAGE pShwPageCR3, PPGMMAPPING pMap, 
 VMMDECL(void) PGMMapCheck(PVM pVM)
 {
 #ifdef VBOX_WITH_PGMPOOL_PAGING_ONLY
-    Log(("PGMMapCheck fixed mappings=%d\n", pVM->pgm.s.fMappingsFixed));
-
     /*
      * Can skip this if mappings are disabled.
      */
