b8c93d770e6654ae343ccc298e42f11b58118bbd
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-02-24 21:37:20 +0000

    RTGetOpt: Fixed 'tstHeadless -startvm vm' breakage from earlier today.
    svn:sync-xref-src-repo-rev: r43331

--- src/VBox/Runtime/testcase/tstGetOpt.cpp
+++ src/VBox/Runtime/testcase/tstGetOpt.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstGetOpt.cpp 17093 2009-02-24 19:58:09Z knut.osmundsen@oracle.com $ */
+/* $Id: tstGetOpt.cpp 17100 2009-02-24 21:37:20Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase - RTGetOpt
  */
@@ -63,7 +63,7 @@ int main()
         { "--verbose",          'v', RTGETOPT_REQ_NOTHING },
         { NULL,                 'q', RTGETOPT_REQ_NOTHING },
         { "--quiet",            384, RTGETOPT_REQ_NOTHING },
-        { "-quiet2",            385, RTGETOPT_REQ_NOTHING },
+        { "-startvm",           385, RTGETOPT_REQ_NOTHING },
     };
 
     char *argv2[] =
@@ -84,9 +84,10 @@ int main()
         "--verbose",
         "-q",
         "--quiet",
-        "-quiet2",
-        /* "filename1", */
-        /* "filename2", */
+        "-startvm",
+        "filename1",
+        "-q",
+        "filename2",
         NULL
     };
     int argc2 = (int)RT_ELEMENTS(argv2) - 1;
@@ -132,6 +133,14 @@ int main()
     CHECK(Val.pDef == &s_aOpts2[4]);
     CHECK_GETOPT(RTGetOpt(&GetState, &Val), 385, 1);
     CHECK(Val.pDef == &s_aOpts2[5]);
+
+    CHECK_GETOPT(RTGetOpt(&GetState, &Val), VINF_GETOPT_NOT_OPTION, 1);
+    CHECK(Val.psz && !strcmp(Val.psz, "filename1"));
+    CHECK_GETOPT(RTGetOpt(&GetState, &Val), 'q', 1);
+    CHECK(Val.pDef == &s_aOpts2[3]);
+    CHECK_GETOPT(RTGetOpt(&GetState, &Val), VINF_GETOPT_NOT_OPTION, 1);
+    CHECK(Val.psz && !strcmp(Val.psz, "filename2"));
+
     CHECK_GETOPT(RTGetOpt(&GetState, &Val), 0, 0);
     CHECK(Val.pDef == NULL);
     CHECK(argc2 == GetState.iNext);
@@ -147,3 +156,4 @@ int main()
 
     return !!cErrors;
 }
+
