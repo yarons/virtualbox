6a1c4ed0a4860bfc67064abafbc8946326f890ec
Christian Poetzsch <noreply@oracle.com>
2009-02-26 10:59:22 +0000

    OVF: spaces
    svn:sync-xref-src-repo-rev: r43408

--- src/VBox/Main/ApplianceImpl.cpp
+++ src/VBox/Main/ApplianceImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImpl.cpp 17109 2009-02-25 08:18:39Z noreply@oracle.com $ */
+/* $Id: ApplianceImpl.cpp 17152 2009-02-26 10:59:22Z noreply@oracle.com $ */
 /** @file
  *
  * IAppliance and IVirtualSystem COM class implementations.
@@ -1374,11 +1374,11 @@ STDMETHODIMP Appliance::Interpret()
                                    vsysThis.strSoundCardType,
                                    Utf8StrFmt("%RI32", (uint32_t)AudioControllerType_AC97));
 
- #ifdef VBOX_WITH_USB
+#ifdef VBOX_WITH_USB
             /* USB Controller */
             if (vsysThis.fHasUsbController)
                 pNewDesc->addEntry(VirtualSystemDescriptionType_USBController, "", "", "");
- #endif /* VBOX_WITH_USB */
+#endif /* VBOX_WITH_USB */
 
             NetworksMap::const_iterator itN;
             for (itN = m->mapNetworks.begin();
