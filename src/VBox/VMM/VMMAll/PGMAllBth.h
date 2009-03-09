1ed2ad270ae2cdc4551439beec2f74f87ef3ee77
Sander van Leeuwen <noreply@oracle.com>
2009-03-09 10:55:59 +0000

    Backed out 44000; code was already inactive
    svn:sync-xref-src-repo-rev: r44004

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllBth.h 17561 2009-03-09 10:31:28Z noreply@oracle.com $ */
+/* $Id: PGMAllBth.h 17562 2009-03-09 10:55:59Z noreply@oracle.com $ */
 /** @file
  * VBox - Page Manager, Shadow+Guest Paging Template - All context code.
  *
@@ -180,10 +180,8 @@ PGM_BTH_DECL(int, Trap0eHandler)(PVM pVM, RTGCUINT uErr, PCPUMCTXCORE pRegFrame,
     /* Did we mark the PDPT as not present in SyncCR3? */
     unsigned        iPdpt  = (pvFault >> SHW_PDPT_SHIFT) & SHW_PDPT_MASK;
     PX86PDPT        pPdptDst = pgmShwGetPaePDPTPtr(&pVM->pgm.s);
-#    ifndef VBOX_WITH_PGMPOOL_PAGING_ONLY
     if (!pPdptDst->a[iPdpt].n.u1Present)
         pPdptDst->a[iPdpt].n.u1Present = 1;
-#    endif
 #   endif
 
 #  elif PGM_SHW_TYPE == PGM_TYPE_AMD64
