0eb151915d35f53b48ed1486456c5fd26f705a35
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-03-11 11:10:39 +0000

    export pfnGetFunctions.
    svn:sync-xref-src-repo-rev: r44177

--- src/VBox/Main/cbinding/VBoxXPCOMCGlue.h
+++ src/VBox/Main/cbinding/VBoxXPCOMCGlue.h
@@ -1,4 +1,4 @@
-/* $Revision: 17304 $ */
+/* $Revision: 17677 $ */
 /** @file VBoxXPCOMCGlue.h
  * Glue for dynamically linking with VBoxXPCOMC.
  */
@@ -43,6 +43,9 @@ extern void *g_hVBoxXPCOMC;
 extern char g_szVBoxErrMsg[256];
 /** Pointer to the VBoxXPCOMC function table.  */
 extern PCVBOXXPCOM g_pVBoxFuncs;
+/** Pointer to VBoxGetXPCOMCFunctions for the loaded VBoxXPCOMC so/dylib/dll. */
+extern PFNVBOXGETXPCOMCFUNCTIONS g_pfnGetFunctions;
+
 
 int VBoxCGlueInit(const char *pszMsgPrefix);
 void VBoxCGlueTerm(void);
