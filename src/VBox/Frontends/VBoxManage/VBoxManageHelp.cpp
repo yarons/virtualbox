6f5e3186bf013ea5631913e039b9a0169a167381
Alexander Eichner <alexander.eichner@oracle.com>
2009-03-16 14:28:53 +0000

    VBoxManage: Make it possible to specify the SCSI controller type
    svn:sync-xref-src-repo-rev: r44513

--- src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageHelp.cpp 17886 2009-03-15 15:04:47Z noreply@oracle.com $ */
+/* $Id: VBoxManageHelp.cpp 17939 2009-03-16 14:28:53Z alexander.eichner@oracle.com $ */
 /** @file
  * VBoxManage - help and other message output.
  */
@@ -157,9 +157,10 @@ void printUsage(USAGECATEGORY u64Cmd)
                  "                            [-sataport<1-30> none|<uuid>|<filename>]\n"
                  "                            [-sataideemulation<1-4> <1-30>]\n"
 #endif
-#ifdef VBOX_WITH_LSILOGIC
-                 "                            [-lsilogic on|off]\n"
-                 "                            [-lsilogicport<1-16> none|<uuid>|<filename>]\n"
+#ifdef VBOX_WITH_SCSI
+                 "                            [-scsi on|off]\n"
+                 "                            [-scsiport<1-16> none|<uuid>|<filename>]\n"
+                 "                            [-scsitype LsiLogic|BusLogic]\n"
 #endif
                  "                            [-dvd none|<uuid>|<filename>|host:<drive>]\n"
                  "                            [-dvdpassthrough on|off]\n"
