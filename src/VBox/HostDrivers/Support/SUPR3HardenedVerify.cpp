3a4964c0906c06914ac7076d6aafdba71e1077a4
Frank Mehnert <noreply@oracle.com>
2009-03-17 09:59:38 +0000

    added hardened entry for VBoxNetDHCP
    svn:sync-xref-src-repo-rev: r44590

--- src/VBox/HostDrivers/Support/SUPR3HardenedVerify.cpp
+++ src/VBox/HostDrivers/Support/SUPR3HardenedVerify.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPR3HardenedVerify.cpp 17794 2009-03-13 09:36:06Z andreas.loeffler@oracle.com $ */
+/* $Id: SUPR3HardenedVerify.cpp 18003 2009-03-17 09:59:38Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Verification of Hardened Installation.
  */
@@ -89,7 +89,7 @@
  */
 static SUPINSTFILE const    g_aSupInstallFiles[] =
 {
-    /*         type,                    dir,fOptional, pszFile                */
+    /*  type,         dir,                      fOpt,  pszFile                */
     /* ---------------------------------------------------------------------- */
     {   kSupIFT_Dll,  kSupID_AppPrivArch,       false, "VMMR0.r0" },
     {   kSupIFT_Dll,  kSupID_AppPrivArch,       false, "VBoxDDR0.r0" },
@@ -176,12 +176,16 @@ static SUPINSTFILE const    g_aSupInstallFiles[] =
 //#endif
 
 //#ifdef VBOX_WITH_WEBSERVICES
-    {   kSupIFT_Exe, kSupID_AppBin,             true, "vboxwebsrv" SUPLIB_EXE_SUFF },
+    {   kSupIFT_Exe,  kSupID_AppBin,            true, "vboxwebsrv" SUPLIB_EXE_SUFF },
 //#endif
 
 #ifdef RT_OS_LINUX
-    {   kSupIFT_Exe, kSupID_AppBin,             true, "VBoxTunctl" SUPLIB_EXE_SUFF },
+    {   kSupIFT_Exe,  kSupID_AppBin,            true, "VBoxTunctl" SUPLIB_EXE_SUFF },
 #endif
+
+//#ifdef VBOX_WITH_NETFLT
+    {   kSupIFT_Exe,  kSupID_AppBin,            true, "VBoxNetDHCP" SUPLIB_EXE_SUFF },
+//#endif
 };
 
 
