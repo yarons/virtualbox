249f509fa5b34c6b83ce009284c3552a34f27867
Frank Mehnert <noreply@oracle.com>
2009-03-17 10:32:56 +0000

    VBoxManageDhcp: allow ''VBoxManage modify -disable / -enable'' without further changing the server parameters
    svn:sync-xref-src-repo-rev: r44600

--- src/VBox/Frontends/VBoxManage/VBoxManageDhcpServer.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageDhcpServer.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageDhcpServer.cpp 17892 2009-03-15 17:30:53Z noreply@oracle.com $ */
+/* $Id: VBoxManageDhcpServer.cpp 18011 2009-03-17 10:32:56Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - Implementation of hostonlyif command.
  */
@@ -252,9 +252,12 @@ static int handleOp(HandlerArg *a, OPCODE enmCode, int iStart, int *pcProcessed)
 
     if(enmCode != OP_REMOVE)
     {
-        CHECK_ERROR(svr, SetConfiguration (Bstr(pIp).mutableRaw(), Bstr(pNetmask).mutableRaw(), Bstr(pLowerIp).mutableRaw(), Bstr(pUpperIp).mutableRaw()));
-        if(FAILED(rc))
-            return errorArgument("failed to set configuration");
+        if (pIp || pNetmask || pLowerIp || pUpperIp)
+        {
+            CHECK_ERROR(svr, SetConfiguration (Bstr(pIp).mutableRaw(), Bstr(pNetmask).mutableRaw(), Bstr(pLowerIp).mutableRaw(), Bstr(pUpperIp).mutableRaw()));
+            if(FAILED(rc))
+                return errorArgument("failed to set configuration");
+        }
 
         if(enable >= 0)
         {
