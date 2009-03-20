b992a6ef9b948125d83281828e00ffa5fa4625e7
Frank Mehnert <noreply@oracle.com>
2009-03-20 11:18:48 +0000

    gcc warning (not ideal btw)
    svn:sync-xref-src-repo-rev: r44776

--- src/VBox/Runtime/testcase/tstGetOpt.cpp
+++ src/VBox/Runtime/testcase/tstGetOpt.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstGetOpt.cpp 17441 2009-03-06 07:05:58Z knut.osmundsen@oracle.com $ */
+/* $Id: tstGetOpt.cpp 18110 2009-03-20 11:18:48Z noreply@oracle.com $ */
 /** @file
  * IPRT Testcase - RTGetOpt
  */
@@ -90,7 +90,7 @@ int main()
         { "--mac",              'm', RTGETOPT_REQ_MACADDR },
     };
 
-    char *argv2[] =
+    const char *argv2[] =
     {
         "-s",               "string1",
         "--optwithstring",  "string2",
@@ -133,7 +133,7 @@ int main()
     };
     int argc2 = (int)RT_ELEMENTS(argv2) - 1;
 
-    CHECK(RT_SUCCESS(RTGetOptInit(&GetState, argc2, argv2, &s_aOpts2[0], RT_ELEMENTS(s_aOpts2), 0, 0 /* fFlags */)));
+    CHECK(RT_SUCCESS(RTGetOptInit(&GetState, argc2, (char**)argv2, &s_aOpts2[0], RT_ELEMENTS(s_aOpts2), 0, 0 /* fFlags */)));
 
     CHECK_GETOPT(RTGetOpt(&GetState, &Val), 's', 2);
     CHECK(VALID_PTR(Val.psz) && !strcmp(Val.psz, "string1"));
