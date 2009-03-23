b3336ffecba25d57132a26d02139bc4fbf85d00f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-03-23 23:07:53 +0000

    PGM: More info on allocation error.
    svn:sync-xref-src-repo-rev: r44872

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 18144 2009-03-23 15:20:41Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMPhys.cpp 18166 2009-03-23 23:07:53Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -3085,6 +3085,12 @@ VMMR3DECL(int) PGMR3PhysAllocateHandyPages(PVM pVM)
                 pVM->pgm.s.cPrivatePages,
                 pVM->pgm.s.cSharedPages,
                 pVM->pgm.s.cZeroPages));
+#if 1
+        for (uint32_t i = 0; i < RT_ELEMENTS(pVM->pgm.s.aHandyPages); i++)
+            LogRel(("PGM: aHandyPages[#%-2d] = {.HCPhysGCPhys=%RHp, .idPage=%#08x, .idSharedPage=%#08x}\n",
+                    i, pVM->pgm.s.aHandyPages[i].HCPhysGCPhys, pVM->pgm.s.aHandyPages[i].idPage,
+                    pVM->pgm.s.aHandyPages[i].idSharedPage));
+#endif
         rc = VERR_EM_NO_MEMORY;
         //rc = VINF_EM_NO_MEMORY;
         //VM_FF_SET(pVM, VM_FF_PGM_WE_ARE_SCREWED?);
