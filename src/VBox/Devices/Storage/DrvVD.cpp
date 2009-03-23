a5e2513f0ca0062d1c044bb6d332709a1e19cd50
Frank Mehnert <noreply@oracle.com>
2009-03-23 12:33:12 +0000

    DrvVD: fix r44820 for the snapshot case
    svn:sync-xref-src-repo-rev: r44828

--- src/VBox/Devices/Storage/DrvVD.cpp
+++ src/VBox/Devices/Storage/DrvVD.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvVD.cpp 18127 2009-03-23 09:20:31Z noreply@oracle.com $ */
+/* $Id: DrvVD.cpp 18134 2009-03-23 12:33:12Z noreply@oracle.com $ */
 /** @file
  * DrvVD - Generic VBox disk media driver.
  */
@@ -1014,7 +1014,8 @@ static DECLCALLBACK(int) drvvdConstruct(PPDMDRVINS pDrvIns,
                  iLevel, pszName,
                  VDIsReadOnly(pThis->pDisk) ? "read-only" : "read-write"));
             if (   VDIsReadOnly(pThis->pDisk)
-                && !fReadOnly)
+                && !fReadOnly
+                && iLevel == 0)
             {
                 rc = PDMDrvHlpVMSetError(pDrvIns, VERR_VD_IMAGE_READ_ONLY, RT_SRC_POS,
                                          N_("Failed to open image '%s' for writing due to wrong "
