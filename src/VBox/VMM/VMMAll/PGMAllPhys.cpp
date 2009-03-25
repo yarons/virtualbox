b38e6b35785335a9b20335b509d9d08768d5b208
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-03-25 20:06:01 +0000

    PGMAllPhys.cpp: reverted r44713.
    svn:sync-xref-src-repo-rev: r45029

--- src/VBox/VMM/VMMAll/PGMAllPhys.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPhys.cpp 18266 2009-03-25 17:25:53Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMAllPhys.cpp 18279 2009-03-25 20:06:01Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -1195,8 +1195,7 @@ VMMDECL(int) PGMPhysGCPtr2CCPtr(PVM pVM, RTGCPTR GCPtr, void **ppv, PPGMPAGEMAPL
  */
 VMMDECL(int) PGMPhysGCPtr2CCPtrReadOnly(PVM pVM, RTGCPTR GCPtr, void const **ppv, PPGMPAGEMAPLOCK pLock)
 {
-    /* Debugger uses this API too */
-    //VM_ASSERT_EMT(pVM);
+    VM_ASSERT_EMT(pVM);
     RTGCPHYS GCPhys;
     int rc = PGMPhysGCPtr2GCPhys(pVM, GCPtr, &GCPhys);
     if (RT_SUCCESS(rc))
