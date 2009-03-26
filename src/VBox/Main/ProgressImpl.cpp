f64cd56ff63bff65067389daf2f0e955a9ad74dc
Ulrich Moeller <noreply@oracle.com>
2009-03-26 11:57:52 +0000

    OVF: fix progress values for disks on import
    svn:sync-xref-src-repo-rev: r45073

--- src/VBox/Main/ProgressImpl.cpp
+++ src/VBox/Main/ProgressImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ProgressImpl.cpp 18277 2009-03-25 19:40:58Z noreply@oracle.com $ */
+/* $Id: ProgressImpl.cpp 18306 2009-03-26 11:57:52Z noreply@oracle.com $ */
 /** @file
  *
  * VirtualBox Progress COM class implementation
@@ -858,7 +858,7 @@ HRESULT Progress::setNextOperation(CBSTR bstrNextOperationDescription, ULONG ulN
     m_ulCurrentOperationWeight = ulNextOperationsWeight;
     m_ulOperationPercent = 0;
 
-    Log(("Progress::setNextOperation(%ls): ulNextOperationsWeight = %d; m_ulCurrentOperation is now %d, m_ulOperationsCompletedWeightis now %d\n",
+    Log(("Progress::setNextOperation(%ls): ulNextOperationsWeight = %d; m_ulCurrentOperation is now %d, m_ulOperationsCompletedWeight is now %d\n",
          m_bstrOperationDescription.raw(), ulNextOperationsWeight, m_ulCurrentOperation, m_ulOperationsCompletedWeight));
 
     /* wake up all waiting threads */
