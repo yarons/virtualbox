553bc73c66cb765c1a3aec2abc99143cdc4f2b9e
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2009-03-27 15:13:28 +0000

    Frontends/VBoxManage: update help to show only the relevant format variants
    svn:sync-xref-src-repo-rev: r45206

--- src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageHelp.cpp 18396 2009-03-27 14:06:29Z noreply@oracle.com $ */
+/* $Id: VBoxManageHelp.cpp 18403 2009-03-27 15:13:28Z klaus.espenlaub@oracle.com $ */
 /** @file
  * VBoxManage - help and other message output.
  */
@@ -323,7 +323,7 @@ void printUsage(USAGECATEGORY u64Cmd)
         RTPrintf("VBoxManage createhd         --filename <filename>\n"
                  "                            --size <megabytes>\n"
                  "                            [--format VDI|VMDK|VHD] (default: VDI)\n"
-                 "                            [--variant Standard,Fixed,Diff,Split2G,Stream]\n"
+                 "                            [--variant Standard,Fixed,Split2G,StreamOptimized]\n"
                  "                            [--type normal|writethrough] (default: normal)\n"
                  "                            [--comment <comment>]\n"
                  "                            [--remember]\n"
@@ -342,7 +342,7 @@ void printUsage(USAGECATEGORY u64Cmd)
     {
         RTPrintf("VBoxManage clonehd          <uuid>|<filename> <outputfile>\n"
                  "                            [--format VDI|VMDK|VHD|RAW|<other>]\n"
-                 "                            [--variant Standard,Fixed,Diff,Split2G,Stream]\n"
+                 "                            [--variant Standard,Fixed,Split2G,StreamOptimized]\n"
                  "                            [--type normal|writethrough|immutable]\n"
                  "                            [--remember]\n"
                  "\n");
@@ -352,10 +352,10 @@ void printUsage(USAGECATEGORY u64Cmd)
     {
         RTPrintf("VBoxManage convertfromraw   <filename> <outputfile>\n"
                  "                            [--format VDI|VMDK|VHD]\n"
-                 "                            [--variant Standard,Fixed,Diff,Split2G,Stream]\n"
+                 "                            [--variant Standard,Fixed,Split2G,StreamOptimized]\n"
                  "VBoxManage convertfromraw   stdin <outputfile> <bytes>\n"
                  "                            [--format VDI|VMDK|VHD]\n"
-                 "                            [--variant Standard,Fixed,Diff,Split2G,Stream]\n"
+                 "                            [--variant Standard,Fixed,Split2G,StreamOptimized]\n"
                  "\n");
     }
 
