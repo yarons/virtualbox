e1f9dd40c2b917155efe4e9e4f29ac9a3e61fb0f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-03-28 23:23:56 +0000

    DBGFR0.cpp: 32->64 bit propagation warning.
    svn:sync-xref-src-repo-rev: r45301

--- src/VBox/VMM/VMMR0/DBGFR0.cpp
+++ src/VBox/VMM/VMMR0/DBGFR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFR0.cpp 16105 2009-01-20 22:02:48Z knut.osmundsen@oracle.com $ */
+/* $Id: DBGFR0.cpp 18469 2009-03-28 23:23:56Z knut.osmundsen@oracle.com $ */
 /** @file
  * DBGF - Debugger Facility, R0 part.
  *
@@ -57,7 +57,7 @@ VMMR0DECL(int) DBGFR0Trap01Handler(PVM pVM, PCPUMCTXCORE pRegFrame, RTGCUINTREG 
         Assert(X86_DR6_B0 == 1 && X86_DR6_B1 == 2 && X86_DR6_B2 == 4 && X86_DR6_B3 == 8);
         for (unsigned iBp = 0; iBp < RT_ELEMENTS(pVM->dbgf.s.aHwBreakpoints); iBp++)
         {
-            if (    (uDr6 & RT_BIT(iBp))
+            if (    ((uint32_t)uDr6 & RT_BIT_32(iBp))
                 &&  pVM->dbgf.s.aHwBreakpoints[iBp].enmType == DBGFBPTYPE_REG)
             {
                 pVM->dbgf.s.iActiveBp = pVM->dbgf.s.aHwBreakpoints[iBp].iBp;
