e30645876d8fa2bd85b813729836bc30ade29128
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-04-01 16:25:17 +0000

    mmR3Load: Ignore the saved cBasePages.
    svn:sync-xref-src-repo-rev: r45513

--- src/VBox/VMM/MM.cpp
+++ src/VBox/VMM/MM.cpp
@@ -1,4 +1,4 @@
-/* $Id: MM.cpp 18046 2009-03-17 22:30:35Z knut.osmundsen@oracle.com $ */
+/* $Id: MM.cpp 18607 2009-04-01 16:25:17Z knut.osmundsen@oracle.com $ */
 /** @file
  * MM - Memory Manager.
  */
@@ -593,9 +593,9 @@ static DECLCALLBACK(int) mmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Version)
     int rc;
     RTUINT cb1;
 
-    /* cBasePages */
+    /* cBasePages (ignored) */
     uint64_t cPages;
-    if (u32Version != 1)
+    if (u32Version >= 2)
         rc = SSMR3GetU64(pSSM, &cPages);
     else
     {
@@ -604,15 +604,6 @@ static DECLCALLBACK(int) mmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Version)
     }
     if (RT_FAILURE(rc))
         return rc;
-#ifdef VBOX_WITH_NEW_PHYS_CODE
-    AssertLogRelMsgReturn(cPages <= pVM->mm.s.cBasePages, /* shadowed rom is counted twice, exact match here isn't really important. */
-                          ("Memory configuration has changed. cPages=%#RX64 saved=%#RX64\n", pVM->mm.s.cBasePages, cPages),
-                          VERR_SSM_LOAD_MEMORY_SIZE_MISMATCH);
-#else
-    AssertLogRelMsgReturn(cPages == pVM->mm.s.cBasePages,
-                          ("Memory configuration has changed. cPages=%#RX64 saved=%#RX64\n", pVM->mm.s.cBasePages, cPages),
-                          VERR_SSM_LOAD_MEMORY_SIZE_MISMATCH);
-#endif
 
     /* cbRamBase */
     uint64_t cb;
@@ -625,7 +616,7 @@ static DECLCALLBACK(int) mmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Version)
     }
     if (RT_FAILURE(rc))
         return rc;
-    AssertLogRelMsgReturn(cb == pVM->mm.s.cbRamBase, 
+    AssertLogRelMsgReturn(cb == pVM->mm.s.cbRamBase,
                           ("Memory configuration has changed. cbRamBase=%#RX64 save=%#RX64\n", pVM->mm.s.cbRamBase, cb),
                           VERR_SSM_LOAD_MEMORY_SIZE_MISMATCH);
 
