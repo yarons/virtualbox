f7243556b58f8265d9a98f79fb22596f6436fb8d
Frank Mehnert <noreply@oracle.com>
2009-04-07 13:48:13 +0000

    filemuncher
    svn:sync-xref-src-repo-rev: r45828

--- include/VBox/GuestHost/clipboard-helper.h
+++ include/VBox/GuestHost/clipboard-helper.h
@@ -1,4 +1,4 @@
-/* $Id: clipboard-helper.h 18398 2009-03-27 14:21:19Z noreply@oracle.com $ */
+/* $Id: clipboard-helper.h 18824 2009-04-07 13:48:13Z noreply@oracle.com $ */
 /** @file
  * Shared Clipboard: Some helper function for converting between the various eol.
  */
@@ -13,6 +13,15 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
  *
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, CA 95054 USA or visit http://www.sun.com if you need
