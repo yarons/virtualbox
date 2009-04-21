ac5ab663ef49f929fb4fce0f831b91df68f19a56
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-04-21 20:59:37 +0000

    PGMAllPhys.cpp: Fixed wrong (logrel) assertion.
    svn:sync-xref-src-repo-rev: r46240

--- src/VBox/VMM/VMMAll/PGMAllPhys.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPhys.cpp 19015 2009-04-20 07:54:29Z noreply@oracle.com $ */
+/* $Id: PGMAllPhys.cpp 19092 2009-04-21 20:59:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -1913,9 +1913,10 @@ static int pgmPhysWriteHandler(PVM pVM, PPGMPAGE pPage, RTGCPHYS GCPhys, void co
                 STAM_PROFILE_START(&pVirt->Stat, h);
                 int rc2 = pVirt->CTX_SUFF(pfnHandler)(pVM, GCPtr, pvDst, (void *)pvBuf, cbRange, PGMACCESSTYPE_WRITE, /*pCur->CTX_SUFF(pvUser)*/ NULL);
                 STAM_PROFILE_STOP(&pVirt->Stat, h);
-                AssertLogRelMsg(rc2 != VINF_SUCCESS && rc2 != VINF_PGM_HANDLER_DO_DEFAULT, ("rc=%Rrc GCPhys=%RGp pPage=%R[pgmpage] %s\n", rc, GCPhys, pPage, pVirt->pszDesc));
                 if (rc2 == VINF_SUCCESS && rc == VINF_PGM_HANDLER_DO_DEFAULT)
                     rc = VINF_SUCCESS;
+                else
+                    AssertLogRelMsg(rc2 == VINF_SUCCESS || rc2 == VINF_PGM_HANDLER_DO_DEFAULT, ("rc=%Rrc GCPhys=%RGp pPage=%R[pgmpage] %s\n", rc, GCPhys, pPage, pVirt->pszDesc));
             }
             pPhys = NULL;
             pVirt = NULL;
