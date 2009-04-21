828376291349291885bd0fa76fb8647c0b6e8bb6
Frank Mehnert <noreply@oracle.com>
2009-04-21 08:24:09 +0000

    Main: fixed potential deadlock during VM creation (internal ticket #3851)
    svn:sync-xref-src-repo-rev: r46165

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 18829 2009-04-07 15:42:32Z noreply@oracle.com $ */
+/* $Id: ConsoleImpl.cpp 19046 2009-04-21 08:24:09Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -6499,11 +6499,15 @@ DECLCALLBACK (int) Console::powerUpThread (RTTHREAD Thread, void *pvUser)
                 AssertComRC (rc2);
             }
 
+            alock.leave();
+
             /* Deregister the VMSetError callback. This is necessary as the
              * pfnVMAtError() function passed to VMR3Create() is supposed to
              * be sticky but our error callback isn't. */
             VMR3AtErrorDeregister(pVM, task->mSetVMErrorCallback, task.get());
             /** @todo register another VMSetError callback? */
+
+            alock.enter();
         }
         else
         {
