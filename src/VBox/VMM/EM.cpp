681884fde435a187422fc800637559edfdebea8a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-04-23 19:07:54 +0000

    EM: build fix.
    svn:sync-xref-src-repo-rev: r46394

--- src/VBox/VMM/EM.cpp
+++ src/VBox/VMM/EM.cpp
@@ -1,4 +1,4 @@
-/* $Id: EM.cpp 19141 2009-04-23 13:52:18Z noreply@oracle.com $ */
+/* $Id: EM.cpp 19151 2009-04-23 19:07:54Z knut.osmundsen@oracle.com $ */
 /** @file
  * EM - Execution Monitor / Manager.
  */
@@ -893,7 +893,7 @@ static int emR3RemExecute(PVM pVM, PVMCPU pVCpu, bool *pfFFDone)
     STAM_REL_PROFILE_ADV_START(&pVCpu->em.s.StatREMTotal, a);
 
 #if defined(VBOX_STRICT) && defined(DEBUG_bird)
-    AssertMsg(   VM_FF_ISPENDING(pVM, VM_FF_PGM_SYNC_CR3|VM_FF_PGM_SYNC_CR3_NON_GLOBAL)
+    AssertMsg(   VMCPU_FF_ISPENDING(pVCpu, VMCPU_FF_PGM_SYNC_CR3 | VMCPU_FF_PGM_SYNC_CR3_NON_GLOBAL)
               || !MMHyperIsInsideArea(pVM, CPUMGetGuestEIP(pVCpu)),  /** @todo #1419 - get flat address. */
               ("cs:eip=%RX16:%RX32\n", CPUMGetGuestCS(pVCpu), CPUMGetGuestEIP(pVCpu)));
 #endif
