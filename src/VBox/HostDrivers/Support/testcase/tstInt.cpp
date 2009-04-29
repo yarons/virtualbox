9ab79aef37db0b3868fa6033fa8ba70be8f4b87e
Sander van Leeuwen <noreply@oracle.com>
2009-04-29 10:14:59 +0000

    Backed out 46655-57 for now
    svn:sync-xref-src-repo-rev: r46658

--- src/VBox/HostDrivers/Support/testcase/tstInt.cpp
+++ src/VBox/HostDrivers/Support/testcase/tstInt.cpp
@@ -1,4 +1,4 @@
-/** $Id: tstInt.cpp 19252 2009-04-29 09:56:19Z noreply@oracle.com $ */
+/** $Id: tstInt.cpp 19255 2009-04-29 10:14:59Z noreply@oracle.com $ */
 /** @file
  * Testcase: Test the interrupt gate feature of the support library.
  */
@@ -123,7 +123,7 @@ int main(int argc, char **argv)
                      */
                     for (i = cIterations; i > 0; i--)
                     {
-                        rc = SUPCallVMMR0(pVMR0, 0, VMMR0_DO_SLOW_NOP, NULL);
+                        rc = SUPCallVMMR0(pVMR0, VMMR0_DO_SLOW_NOP, NULL);
                         if (rc != VINF_SUCCESS)
                         {
                             RTPrintf("tstInt: SUPCallVMMR0 -> rc=%Rrc i=%d Expected VINF_SUCCESS!\n", rc, i);
@@ -173,7 +173,7 @@ int main(int argc, char **argv)
                         for (i = 0; i < 1000000; i++)
                         {
                             uint64_t OneStartTick = ASMReadTSC();
-                            rc = SUPCallVMMR0Ex(pVMR0, 0, VMMR0_DO_SLOW_NOP, 0, NULL);
+                            rc = SUPCallVMMR0Ex(pVMR0, VMMR0_DO_SLOW_NOP, 0, NULL);
                             uint64_t Ticks = ASMReadTSC() - OneStartTick;
                             if (Ticks < MinTicks)
                                 MinTicks = Ticks;
