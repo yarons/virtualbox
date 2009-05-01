c11d4d77a32a81dc709a932d5b7004de3ebb7a28
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-05-01 17:27:28 +0000

    EM: Fixed FF mixup causing cpu specific requests to be ignored.
    svn:sync-xref-src-repo-rev: r46753

--- src/VBox/VMM/EM.cpp
+++ src/VBox/VMM/EM.cpp
@@ -1,4 +1,4 @@
-/* $Id: EM.cpp 19286 2009-05-01 12:41:07Z knut.osmundsen@oracle.com $ */
+/* $Id: EM.cpp 19298 2009-05-01 17:27:28Z knut.osmundsen@oracle.com $ */
 /** @file
  * EM - Execution Monitor / Manager.
  */
@@ -3421,7 +3421,7 @@ static int emR3ForcedActions(PVM pVM, PVMCPU pVCpu, int rc)
         /*
          * Requests from other threads.
          */
-        if (VM_FF_IS_PENDING_EXCEPT(pVM, VM_FF_REQUEST, VM_FF_PGM_NO_MEMORY))
+        if (VMCPU_FF_IS_PENDING_EXCEPT(pVCpu, VMCPU_FF_REQUEST, VM_FF_PGM_NO_MEMORY))
         {
             rc2 = VMR3ReqProcessU(pVM->pUVM, (VMREQDEST)pVCpu->idCpu);
             if (rc2 == VINF_EM_OFF || rc2 == VINF_EM_TERMINATE)
