66372bb18302830929b8f34104745e011303fb57
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-05-06 18:07:53 +0000

    PDMQueue.cpp: stricter assertions.
    svn:sync-xref-src-repo-rev: r46980

--- src/VBox/VMM/PDMQueue.cpp
+++ src/VBox/VMM/PDMQueue.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMQueue.cpp 19420 2009-05-06 11:07:11Z noreply@oracle.com $ */
+/* $Id: PDMQueue.cpp 19449 2009-05-06 18:07:53Z knut.osmundsen@oracle.com $ */
 /** @file
  * PDM Queue - Transport data and tasks to EMT and R3.
  */
@@ -198,7 +198,7 @@ VMMR3DECL(int) PDMR3QueueCreateDevice(PVM pVM, PPDMDEVINS pDevIns, RTUINT cbItem
     /*
      * Validate input.
      */
-    VM_ASSERT_EMT(pVM);
+    VMCPU_ASSERT_EMT(&pVM->aCpus[0]);
     if (!pfnCallback)
     {
         AssertMsgFailed(("No consumer callback!\n"));
@@ -247,7 +247,7 @@ VMMR3DECL(int) PDMR3QueueCreateDriver(PVM pVM, PPDMDRVINS pDrvIns, RTUINT cbItem
     /*
      * Validate input.
      */
-    VM_ASSERT_EMT(pVM);
+    VMCPU_ASSERT_EMT(&pVM->aCpus[0]);
     if (!pfnCallback)
     {
         AssertMsgFailed(("No consumer callback!\n"));
@@ -296,7 +296,7 @@ VMMR3DECL(int) PDMR3QueueCreateInternal(PVM pVM, RTUINT cbItem, RTUINT cItems, u
     /*
      * Validate input.
      */
-    VM_ASSERT_EMT(pVM);
+    VMCPU_ASSERT_EMT(&pVM->aCpus[0]);
     if (!pfnCallback)
     {
         AssertMsgFailed(("No consumer callback!\n"));
@@ -342,7 +342,7 @@ VMMR3DECL(int) PDMR3QueueCreateExternal(PVM pVM, RTUINT cbItem, RTUINT cItems, u
     /*
      * Validate input.
      */
-    VM_ASSERT_EMT(pVM);
+    VMCPU_ASSERT_EMT(&pVM->aCpus[0]);
     if (!pfnCallback)
     {
         AssertMsgFailed(("No consumer callback!\n"));
@@ -386,7 +386,7 @@ VMMR3DECL(int) PDMR3QueueDestroy(PPDMQUEUE pQueue)
         return VERR_INVALID_PARAMETER;
     Assert(pQueue && pQueue->pVMR3);
     PVM pVM = pQueue->pVMR3;
-    VM_ASSERT_EMT(pVM);
+    VMCPU_ASSERT_EMT(&pVM->aCpus[0]);
 
     /*
      * Unlink it.
@@ -470,7 +470,7 @@ VMMR3DECL(int) PDMR3QueueDestroyDevice(PVM pVM, PPDMDEVINS pDevIns)
      */
     if (!pDevIns)
         return VERR_INVALID_PARAMETER;
-    VM_ASSERT_EMT(pVM);
+    VMCPU_ASSERT_EMT(&pVM->aCpus[0]);
 
     /*
      * Unlink it.
@@ -519,7 +519,7 @@ VMMR3DECL(int) PDMR3QueueDestroyDriver(PVM pVM, PPDMDRVINS pDrvIns)
      */
     if (!pDrvIns)
         return VERR_INVALID_PARAMETER;
-    VM_ASSERT_EMT(pVM);
+    VMCPU_ASSERT_EMT(&pVM->aCpus[0]);
 
     /*
      * Unlink it.
