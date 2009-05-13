ee5acd14bf5ea0fdd6a2380808d192f327f66763
Frank Mehnert <noreply@oracle.com>
2009-05-13 09:05:14 +0000

    log the name of the executable
    svn:sync-xref-src-repo-rev: r47238

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 19300 2009-05-01 18:06:59Z knut.osmundsen@oracle.com $ */
+/* $Id: ConsoleImpl.cpp 19648 2009-05-13 09:05:14Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -4270,13 +4270,17 @@ HRESULT Console::consoleInitReleaseLog (const ComPtr <IMachine> aMachine)
         if (RT_SUCCESS(vrc) || vrc == VERR_BUFFER_OVERFLOW)
             RTLogRelLogger(loggerRelease, 0, ~0U, "OS Service Pack: %s\n", szTmp);
         /* the package type is interesting for Linux distributions */
+        char szExecName[RTPATH_MAX];
+        char *pszExecName = RTProcGetExecutableName(szExecName, sizeof(szExecName));
         RTLogRelLogger(loggerRelease, 0, ~0U,
+                       "Executable: %s\n"
                        "Process ID: %u\n"
                        "Package type: %s"
 #ifdef VBOX_OSE
                        " (OSE)"
 #endif
                        "\n",
+                       pszExecName ? pszExecName : "unknown",
                        RTProcSelf(),
                        VBOX_PACKAGE_STRING);
 
