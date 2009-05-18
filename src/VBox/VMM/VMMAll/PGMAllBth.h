a42f0d72d38ed33dfac7aa755372524ac4cf75e9
Sander van Leeuwen <noreply@oracle.com>
2009-05-18 11:59:17 +0000

    PrefetchPage: Locking
    svn:sync-xref-src-repo-rev: r47415

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllBth.h 19779 2009-05-18 11:53:30Z noreply@oracle.com $ */
+/* $Id: PGMAllBth.h 19781 2009-05-18 11:59:17Z noreply@oracle.com $ */
 /** @file
  * VBox - Page Manager, Shadow+Guest Paging Template - All context code.
  *
@@ -3025,6 +3025,9 @@ PGM_BTH_DECL(int, PrefetchPage)(PVMCPU pVCpu, RTGCPTR GCPtrPage)
 
     if (PdeSrc.n.u1Present && PdeSrc.n.u1Accessed)
     {
+        PVM pVM = pVCpu->CTX_SUFF(pVM);
+        pgmLock(pVM);
+
 # if PGM_SHW_TYPE == PGM_TYPE_32BIT
         const X86PDE    PdeDst = pgmShwGet32BitPDE(&pVCpu->pgm.s, GCPtrPage);
 # elif PGM_SHW_TYPE == PGM_TYPE_PAE
@@ -3040,6 +3043,7 @@ PGM_BTH_DECL(int, PrefetchPage)(PVMCPU pVCpu, RTGCPTR GCPtrPage)
         int rc = pgmShwSyncPaePDPtr(pVCpu, GCPtrPage, &PdpeSrc, &pPDDst);
         if (rc != VINF_SUCCESS)
         {
+            pgmUnlock(pVM);
             AssertRC(rc);
             return rc;
         }
@@ -3065,6 +3069,7 @@ PGM_BTH_DECL(int, PrefetchPage)(PVMCPU pVCpu, RTGCPTR GCPtrPage)
         int rc = pgmShwSyncLongModePDPtr(pVCpu, GCPtrPage, pPml4eSrc, &PdpeSrc, &pPDDst);
         if (rc != VINF_SUCCESS)
         {
+            pgmUnlock(pVM);
             AssertRC(rc);
             return rc;
         }
@@ -3075,11 +3080,8 @@ PGM_BTH_DECL(int, PrefetchPage)(PVMCPU pVCpu, RTGCPTR GCPtrPage)
         {
             if (!PdeDst.n.u1Present)
             {
-                PVM pVM = pVCpu->CTX_SUFF(pVM);
                 /** r=bird: This guy will set the A bit on the PDE, probably harmless. */
-                pgmLock(pVM);
                 rc = PGM_BTH_NAME(SyncPT)(pVCpu, iPDSrc, pPDSrc, GCPtrPage);
-                pgmUnlock(pVM);
             }
             else
             {
@@ -3092,6 +3094,7 @@ PGM_BTH_DECL(int, PrefetchPage)(PVMCPU pVCpu, RTGCPTR GCPtrPage)
                     rc = VINF_SUCCESS;
             }
         }
+        pgmUnlock(pVM);
     }
     return rc;
 
