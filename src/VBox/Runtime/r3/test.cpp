0dc1cf4e6d9b406f56d3937d6090be6a4aa0eee0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-05-23 23:58:05 +0000

    RTTest: More macros. Made RTTestFailure check for trailing new line to avoid getting an extra blank line.
    svn:sync-xref-src-repo-rev: r47654

--- src/VBox/Runtime/r3/test.cpp
+++ src/VBox/Runtime/r3/test.cpp
@@ -1,4 +1,4 @@
-/* $Id: test.cpp 19944 2009-05-23 21:48:08Z knut.osmundsen@oracle.com $ */
+/* $Id: test.cpp 19952 2009-05-23 23:58:05Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Testcase Framework.
  */
@@ -992,8 +992,12 @@ RTR3DECL(int) RTTestFailedV(RTTEST hTest, const char *pszFormat, va_list va)
         va_list va2;
         va_copy(va2, va);
 
+        const char *pszEnd = strchr(pszFormat, '\0');
+        bool fHasNewLine = pszFormat != pszEnd
+                        && pszEnd[-1] == '\n';
+
         RTCritSectEnter(&pTest->OutputLock);
-        cch += rtTestPrintf(pTest, "%N\n", pszFormat, &va2);
+        cch += rtTestPrintf(pTest, fHasNewLine ? "%N" : "%N\n", pszFormat, &va2);
         RTCritSectLeave(&pTest->OutputLock);
 
         va_end(va2);
