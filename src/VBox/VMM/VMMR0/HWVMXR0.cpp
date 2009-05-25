9fb128270f5b557f52f319cd829b797e5cd5335f
Sander van Leeuwen <noreply@oracle.com>
2009-05-25 14:54:21 +0000

    Logging change
    svn:sync-xref-src-repo-rev: r47721

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWVMXR0.cpp 20003 2009-05-25 14:38:49Z noreply@oracle.com $ */
+/* $Id: HWVMXR0.cpp 20004 2009-05-25 14:54:21Z noreply@oracle.com $ */
 /** @file
  * HWACCM VMX - Host Context Ring 0.
  */
@@ -3459,7 +3459,7 @@ ResumeExecution:
             GCPhys &= PAGE_BASE_GC_MASK;
             GCPhys += VMX_EXIT_QUALIFICATION_APIC_ACCESS_OFFSET(exitQualification);
 
-            Log(("Apic access at %RGp\n", GCPhys));
+            LogFlow(("Apic access at %RGp\n", GCPhys));
             rc = IOMMMIOPhysHandler(pVM, (uAccessType == VMX_APIC_ACCESS_TYPE_LINEAR_READ) ? 0 : X86_TRAP_PF_RW, CPUMCTX2CORE(pCtx), GCPhys);
             if (rc == VINF_SUCCESS)
                 goto ResumeExecution;   /* rip already updated */
