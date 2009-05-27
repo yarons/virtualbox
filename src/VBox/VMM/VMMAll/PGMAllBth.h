dcc8fd1037adb498be87e3a70b46caac9c7ae80e
Sander van Leeuwen <noreply@oracle.com>
2009-05-27 11:14:30 +0000

    Logging
    svn:sync-xref-src-repo-rev: r47806

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllBth.h 20064 2009-05-27 11:13:16Z noreply@oracle.com $ */
+/* $Id: PGMAllBth.h 20065 2009-05-27 11:14:30Z noreply@oracle.com $ */
 /** @file
  * VBox - Page Manager, Shadow+Guest Paging Template - All context code.
  *
@@ -1685,6 +1685,7 @@ PGM_BTH_DECL(int, SyncPage)(PVMCPU pVCpu, GSTPDE PdeSrc, RTGCPTR GCPtrPage, unsi
     if (!PdeDst.n.u1Present)
     {
         AssertMsg(pVM->cCPUs > 1, ("%Unexpected missing PDE p=%llx\n", pPdeDst, (uint64_t)PdeDst.u));
+        Log(("CPU%d: SyncPage: Pde at %RGv changed behind our back!\n", GCPtrPage));
         return VINF_SUCCESS;    /* force the instruction to be executed again. */
     }
 
