19d2897d7a3c2c77aa9f1c17f0a012bd23ecf057
Sander van Leeuwen <noreply@oracle.com>
2009-06-05 13:25:55 +0000

    Missing profile stop calls
    svn:sync-xref-src-repo-rev: r48234

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWSVMR0.cpp 20328 2009-06-05 13:23:52Z noreply@oracle.com $ */
+/* $Id: HWSVMR0.cpp 20330 2009-06-05 13:25:55Z noreply@oracle.com $ */
 /** @file
  * HWACCM SVM - Host Context Ring 0.
  */
@@ -1721,7 +1721,10 @@ ResumeExecution:
 
                 rc = IOMMMIOPhysHandler(pVM, errCode, CPUMCTX2CORE(pCtx), uFaultAddress);
                 if (rc == VINF_SUCCESS)
+                {
+                    STAM_PROFILE_ADV_STOP(&pVCpu->hwaccm.s.StatExit1, x);
                     goto ResumeExecution;   /* rip already updated */
+                }
             }
         }
 #endif
