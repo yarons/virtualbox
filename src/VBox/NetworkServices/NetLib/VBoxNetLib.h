a4db97b17f6b1f5287b56ab51f718ab7fa14a863
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-06-08 00:27:14 +0000

    Misc: __BEGIN_DECLS -> RT_BEGIN_DECLS; __END_DECLS -> RT_END_DECLS.
    svn:sync-xref-src-repo-rev: r48293

--- src/VBox/NetworkServices/NetLib/VBoxNetLib.h
+++ src/VBox/NetworkServices/NetLib/VBoxNetLib.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetLib.h 18829 2009-04-07 15:42:32Z noreply@oracle.com $ */
+/* $Id: VBoxNetLib.h 20370 2009-06-08 00:27:14Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxNetUDP - IntNet Client Library.
  */
@@ -25,7 +25,7 @@
 #include <iprt/net.h>
 #include <VBox/intnet.h>
 
-__BEGIN_DECLS
+RT_BEGIN_DECLS
 
 
 /**
@@ -67,7 +67,7 @@ int     VBoxNetIntIfRingWriteFrame(PINTNETBUF pBuf, PINTNETRINGBUF pRingBuf, siz
 int     VBoxNetIntIfSend(PSUPDRVSESSION pSession, INTNETIFHANDLE hIf, PINTNETBUF pBuf, size_t cSegs, PCINTNETSEG paSegs, bool fFlush);
 
 
-__END_DECLS
+RT_END_DECLS
 
 #endif
 
