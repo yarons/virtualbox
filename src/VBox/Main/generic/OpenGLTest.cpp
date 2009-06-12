ad3b0ebe1fbb8f2a7fc1bac539aa4db3ef1c90a0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-06-12 11:30:19 +0000

    Main/generic/Open*.cpp: license header fix
    svn:sync-xref-src-repo-rev: r48493

--- src/VBox/Main/generic/OpenGLTest.cpp
+++ src/VBox/Main/generic/OpenGLTest.cpp
@@ -1,4 +1,4 @@
-/* $Id: OpenGLTest.cpp 20499 2009-06-12 11:27:00Z knut.osmundsen@oracle.com $ */
+/* $Id: OpenGLTest.cpp 20500 2009-06-12 11:30:19Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox host opengl support test - generic implementation.
  */
@@ -6,8 +6,17 @@
 /*
  * Copyright (C) 2009 Sun Microsystems, Inc.
  *
- * Sun Microsystems, Inc. confidential
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
  */
 
 #include <VBox/err.h>
