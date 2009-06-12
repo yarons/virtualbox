629802059deb5682f524ec405c75dcf2f3923cf7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-06-12 11:05:46 +0000

    process-posix.cpp: papszArgs must not be NULL (see the notes in the execve man page on linux).
    svn:sync-xref-src-repo-rev: r48489

--- src/VBox/Runtime/r3/posix/process-posix.cpp
+++ src/VBox/Runtime/r3/posix/process-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: process-posix.cpp 15026 2008-12-05 09:43:41Z knut.osmundsen@oracle.com $ */
+/* $Id: process-posix.cpp 20498 2009-06-12 11:05:46Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Process, POSIX.
  */
@@ -74,6 +74,7 @@ RTR3DECL(int)   RTProcCreate(const char *pszExec, const char * const *papszArgs,
     AssertReturn(Env != NIL_RTENV, VERR_INVALID_PARAMETER);
     const char * const *papszEnv = RTEnvGetExecEnvP(Env);
     AssertPtrReturn(papszEnv, VERR_INVALID_HANDLE);
+    AssertPtrReturn(papszArgs, VERR_INVALID_PARAMETER);
     /* later: path searching. */
 
 
