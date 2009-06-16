a392832bc99a7355c6191bc5a751d5d2059270a4
Frank Mehnert <noreply@oracle.com>
2009-06-16 09:37:47 +0000

    fixed tstMMHyperHeap
    svn:sync-xref-src-repo-rev: r48673

--- src/VBox/VMM/testcase/tstMMHyperHeap.cpp
+++ src/VBox/VMM/testcase/tstMMHyperHeap.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstMMHyperHeap.cpp 13818 2008-11-04 22:59:47Z knut.osmundsen@oracle.com $ */
+/* $Id: tstMMHyperHeap.cpp 20615 2009-06-16 09:37:47Z noreply@oracle.com $ */
 /** @file
  * MM Hypervisor Heap testcase.
  */
@@ -37,6 +37,9 @@
 #include <iprt/stream.h>
 #include <iprt/string.h>
 
+/* does not work for more CPUs as SUPLowAlloc() would refuse to allocate more pages */
+#define NUM_CPUS  16
+
 
 int main(int argc, char **argv)
 {
@@ -51,7 +54,7 @@ int main(int argc, char **argv)
      */
     PVM         pVM;
     RTR0PTR     pvR0;
-    SUPPAGE     aPages[RT_ALIGN_Z(sizeof(*pVM), PAGE_SIZE) >> PAGE_SHIFT];
+    SUPPAGE     aPages[RT_ALIGN_Z(sizeof(*pVM) + NUM_CPUS*sizeof (VMCPU), PAGE_SIZE) >> PAGE_SHIFT];
     int rc = SUPR3Init(NULL);
     if (RT_SUCCESS(rc))
         rc = SUPLowAlloc(RT_ELEMENTS(aPages), (void **)&pVM, &pvR0, &aPages[0]);
@@ -68,6 +71,9 @@ int main(int argc, char **argv)
     PUVM pUVM = &s_UVM;
     pUVM->pVM = pVM;
     pVM->pUVM = pUVM;
+
+    pVM->cCPUs = NUM_CPUS;
+    pVM->cbSelf = RT_UOFFSETOF(VM, aCpus[pVM->cCPUs]);
 
     rc = STAMR3InitUVM(pUVM);
     if (RT_FAILURE(rc))
