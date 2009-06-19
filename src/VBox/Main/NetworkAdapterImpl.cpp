2f67bc1270b5c58a0fd4c2f4533f53e44716c257
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-06-19 09:31:18 +0000

    NetworkAdapterImpl.cpp: completely useless file description.
    svn:sync-xref-src-repo-rev: r48809

--- src/VBox/Main/NetworkAdapterImpl.cpp
+++ src/VBox/Main/NetworkAdapterImpl.cpp
@@ -1,6 +1,6 @@
+/* $Id: NetworkAdapterImpl.cpp 20704 2009-06-19 09:31:18Z knut.osmundsen@oracle.com $ */
 /** @file
- *
- * VirtualBox COM class implementation
+ * Implementation of INetworkAdaptor in VBoxSVC.
  */
 
 /*
