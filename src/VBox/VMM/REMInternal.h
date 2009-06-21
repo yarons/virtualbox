71520c77379cf019853bd7cd0ed39dd33a68a7a4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-06-21 20:57:37 +0000

    REMNotifyInvalidatePage: Don't overflow aGCPtrInvalidatedPages. 2xtodo
    svn:sync-xref-src-repo-rev: r48865

--- src/VBox/VMM/REMInternal.h
+++ src/VBox/VMM/REMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: REMInternal.h 20747 2009-06-21 20:23:22Z knut.osmundsen@oracle.com $ */
+/* $Id: REMInternal.h 20749 2009-06-21 20:57:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * REM - Internal header file.
  */
@@ -164,7 +164,7 @@ typedef struct REM
     uint32_t                u32PendingInterrupt;
 
     /** Number of recorded invlpg instructions. */
-    uint32_t                cInvalidatedPages;
+    uint32_t volatile       cInvalidatedPages;
 #if HC_ARCH_BITS == 32
     uint32_t                uPadding2;
 #endif
