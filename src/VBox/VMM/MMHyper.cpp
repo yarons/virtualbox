29bc4533eef5cb27ee64bdd1621d8465063c1630
Sander van Leeuwen <noreply@oracle.com>
2009-06-22 13:13:22 +0000

    Running out of hyper heap often here; guest SMP needs more
    svn:sync-xref-src-repo-rev: r48897

--- src/VBox/VMM/MMHyper.cpp
+++ src/VBox/VMM/MMHyper.cpp
@@ -1,4 +1,4 @@
-/* $Id: MMHyper.cpp 20775 2009-06-22 13:00:11Z noreply@oracle.com $ */
+/* $Id: MMHyper.cpp 20777 2009-06-22 13:13:22Z noreply@oracle.com $ */
 /** @file
  * MM - Memory Manager - Hypervisor Memory Area.
  */
@@ -78,9 +78,7 @@ int mmR3HyperInit(PVM pVM)
     uint32_t cbHyperHeap;
     int rc = CFGMR3QueryU32(CFGMR3GetChild(CFGMR3GetRoot(pVM), "MM"), "cbHyperHeap", &cbHyperHeap);
     if (rc == VERR_CFGM_NO_PARENT || rc == VERR_CFGM_VALUE_NOT_FOUND)
-        cbHyperHeap = VMMIsHwVirtExtForced(pVM)
-                    ? 640*_1K
-                    : 1280*_1K;
+        cbHyperHeap = 1280*_1K;
     else
         AssertLogRelRCReturn(rc, rc);
     cbHyperHeap = RT_ALIGN_32(cbHyperHeap, PAGE_SIZE);
