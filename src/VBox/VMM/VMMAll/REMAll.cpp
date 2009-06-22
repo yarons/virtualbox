7495225fb39cf494ceac436d8b10f134213f7a8c
Sander van Leeuwen <noreply@oracle.com>
2009-06-22 15:22:55 +0000

    Backed out 48906; redundant
    svn:sync-xref-src-repo-rev: r48907

--- src/VBox/VMM/VMMAll/REMAll.cpp
+++ src/VBox/VMM/VMMAll/REMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: REMAll.cpp 20786 2009-06-22 15:12:41Z noreply@oracle.com $ */
+/* $Id: REMAll.cpp 20787 2009-06-22 15:22:55Z noreply@oracle.com $ */
 /** @file
  * REM - Recompiled Execution Monitor, all Contexts part.
  */
@@ -119,7 +119,6 @@ static void remNotifyHandlerInsert(PVM pVM, PREMHANDLERNOTIFICATION pRec)
             {
                 Assert(cFlushes++ != 128);
                 AssertFatal(cFlushes < _1M);
-                VM_FF_SET(pVM, VM_FF_REM_HANDLER_NOTIFY);
                 remFlushHandlerNotifications(pVM);
                 idxFree = ASMAtomicUoReadU32(&pVM->rem.s.idxFreeList);
             } while (idxFree == (uint32_t)-1);
