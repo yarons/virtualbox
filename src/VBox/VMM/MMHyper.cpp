29157b99ae91278f8bd4220cd5161fb0f2fe17c9
Sander van Leeuwen <noreply@oracle.com>
2009-06-22 13:35:55 +0000

    Default to a 2 MB hyper heap for guest SMP
    svn:sync-xref-src-repo-rev: r48901

--- src/VBox/VMM/MMHyper.cpp
+++ src/VBox/VMM/MMHyper.cpp
@@ -1,4 +1,4 @@
-/* $Id: MMHyper.cpp 20780 2009-06-22 13:32:24Z noreply@oracle.com $ */
+/* $Id: MMHyper.cpp 20781 2009-06-22 13:35:55Z noreply@oracle.com $ */
 /** @file
  * MM - Memory Manager - Hypervisor Memory Area.
  */
@@ -78,9 +78,14 @@ int mmR3HyperInit(PVM pVM)
     uint32_t cbHyperHeap;
     int rc = CFGMR3QueryU32(CFGMR3GetChild(CFGMR3GetRoot(pVM), "MM"), "cbHyperHeap", &cbHyperHeap);
     if (rc == VERR_CFGM_NO_PARENT || rc == VERR_CFGM_VALUE_NOT_FOUND)
-        cbHyperHeap = (VMMIsHwVirtExtForced(pVM) && pVM->cCPUs == 1)
-                    ? 640*_1K
-                    : 1280*_1K;
+    {
+        if (pVM->cCPUs > 1)
+            cbHyperHeap = _2M;
+        else
+            cbHyperHeap = VMMIsHwVirtExtForced(pVM)
+                        ? 640*_1K
+                        : 1280*_1K;
+    }
     else
         AssertLogRelRCReturn(rc, rc);
     cbHyperHeap = RT_ALIGN_32(cbHyperHeap, PAGE_SIZE);
