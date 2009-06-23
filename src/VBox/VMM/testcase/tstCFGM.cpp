456bf63f44ea17063a936f847a06c452a6d8d974
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-06-23 18:28:26 +0000

    tstCFGM.cpp: unintentional change in r49006.
    svn:sync-xref-src-repo-rev: r49007

--- src/VBox/VMM/testcase/tstCFGM.cpp
+++ src/VBox/VMM/testcase/tstCFGM.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstCFGM.cpp 20862 2009-06-23 18:25:31Z knut.osmundsen@oracle.com $ */
+/* $Id: tstCFGM.cpp 20863 2009-06-23 18:28:26Z knut.osmundsen@oracle.com $ */
 /** @file
  * Testcase for CFGM.
  */
@@ -50,7 +50,7 @@ int main()
     PVM         pVM;
     int rc = SUPR3Init(NULL);
     if (RT_SUCCESS(rc))
-        rc = RTMemPageAlloc(RT_ALIGN_Z(sizeof(*pVM), PAGE_SIZE) >> PAGE_SHIFT, (void **)&pVM);
+        rc = SUPPageAlloc(RT_ALIGN_Z(sizeof(*pVM), PAGE_SIZE) >> PAGE_SHIFT, (void **)&pVM);
     if (RT_FAILURE(rc))
     {
         RTPrintf("Fatal error: SUP Failure! rc=%Rrc\n", rc);
