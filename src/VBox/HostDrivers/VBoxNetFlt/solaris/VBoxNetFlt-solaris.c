aacd1b2367b065105422d7a1f4d3bb550b637495
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2009-06-30 15:20:30 +0000

    Solaris/VBoxNetFlt: Paranoia part 2.
    svn:sync-xref-src-repo-rev: r49336

--- src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
+++ src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFlt-solaris.c 21076 2009-06-30 15:19:04Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: VBoxNetFlt-solaris.c 21078 2009-06-30 15:20:30Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Solaris Specific Code.
  */
@@ -2892,15 +2892,6 @@ static int vboxNetFltSolarisRecv(PVBOXNETFLTINS pThis, vboxnetflt_stream_t *pStr
     }
 
     /*
-     * Don't loopback packets we transmit to the wire.
-     */
-    if (vboxNetFltSolarisIsOurMBlk(pThis, pPromiscStream, pMsg))
-    {
-        LogFlow((DEVICE_NAME ":Avoiding packet loopback.\n"));
-        return VINF_SUCCESS;
-    }
-
-    /*
      * Paranoia...
      */
     if (MBLKL(pMsg) < sizeof(RTNETETHERHDR))
@@ -2929,6 +2920,15 @@ static int vboxNetFltSolarisRecv(PVBOXNETFLTINS pThis, vboxnetflt_stream_t *pStr
         }
         else
             return VERR_NO_MEMORY;
+    }
+
+    /*
+     * Don't loopback packets we transmit to the wire.
+     */
+    if (vboxNetFltSolarisIsOurMBlk(pThis, pPromiscStream, pMsg))
+    {
+        LogFlow((DEVICE_NAME ":Avoiding packet loopback.\n"));
+        return VINF_SUCCESS;
     }
 
     /*
