1669a8549db2a86267f952239b3e711ae6735ca2
Frank Mehnert <noreply@oracle.com>
2009-07-07 14:46:38 +0000

    typo
    svn:sync-xref-src-repo-rev: r49682

--- src/VBox/Devices/PC/DevACPI.cpp
+++ src/VBox/Devices/PC/DevACPI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevACPI.cpp 21335 2009-07-07 14:43:58Z noreply@oracle.com $ */
+/* $Id: DevACPI.cpp 21336 2009-07-07 14:46:38Z noreply@oracle.com $ */
 /** @file
  * DevACPI - Advanced Configuration and Power Interface (ACPI) Device.
  */
@@ -705,7 +705,7 @@ static void acpiSetupFADT(ACPIState *s, RTGCPHYS32 addr_acpi1, RTGCPHYS32 addr_a
     fadt.header.u32Length     = ACPITBLFADT_VERSION1_SIZE;
     fadt.u8IntModel           = INT_MODEL_DUAL_PIC;
     fadt.header.u8Checksum    = acpiChecksum((uint8_t *)&fadt, ACPITBLFADT_VERSION1_SIZE);
-    acpiPhyscpy(s, addr_acpi1, &fadt, sizeof(fadt));
+    acpiPhyscpy(s, addr_acpi1, &fadt, ACPITBLFADT_VERSION1_SIZE);
 }
 
 /**
