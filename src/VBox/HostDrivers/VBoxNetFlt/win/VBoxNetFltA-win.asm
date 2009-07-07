6e6104c6dfd85b6a47c75dead1c4df5f3edb2750
Frank Mehnert <noreply@oracle.com>
2009-07-07 15:30:08 +0000

    export vboxnetflt for Windows to OSE
    svn:sync-xref-src-repo-rev: r49692

--- src/VBox/HostDrivers/VBoxNetFlt/win/VBoxNetFltA-win.asm
+++ src/VBox/HostDrivers/VBoxNetFlt/win/VBoxNetFltA-win.asm
@@ -1,4 +1,4 @@
-; $Id: VBoxNetFltA-win.asm 21333 2009-07-07 14:39:32Z noreply@oracle.com $
+; $Id: VBoxNetFltA-win.asm 21343 2009-07-07 15:30:08Z noreply@oracle.com $
 ;; @file
 ; VBoxNetFlt - Unwind Wrappers for 64-bit NT.
 ;
@@ -6,8 +6,17 @@
 ;
 ; Copyright (C) 2008 Sun Microsystems, Inc.
 ;
-; Sun Microsystems, Inc. confidential
-; All rights reserved
+; This file is part of VirtualBox Open Source Edition (OSE), as
+; available from http://www.virtualbox.org. This file is free software;
+; you can redistribute it and/or modify it under the terms of the GNU
+; General Public License (GPL) as published by the Free Software
+; Foundation, in version 2 as it comes in the "COPYING" file of the
+; VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+; hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+;
+; Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+; Clara, CA 95054 USA or visit http://www.sun.com if you need
+; additional information or have any questions.
 ;
 
 %include "iprt/ntwrap.mac"
--- src/VBox/HostDrivers/VBoxNetFlt/win/VBoxNetFltMp-win.h
+++ src/VBox/HostDrivers/VBoxNetFlt/win/VBoxNetFltMp-win.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFltMp-win.h 21333 2009-07-07 14:39:32Z noreply@oracle.com $ */
+/* $Id: VBoxNetFltMp-win.h 21343 2009-07-07 15:30:08Z noreply@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Windows Specific Code. Miniport edge of ndis filter driver
  */
@@ -6,8 +6,17 @@
 /*
  * Copyright (C) 2008 Sun Microsystems, Inc.
  *
- * Sun Microsystems, Inc. confidential
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
  */
 /*
  * Based in part on Microsoft DDK sample code for Ndis Intermediate Miniport passthru driver sample.
