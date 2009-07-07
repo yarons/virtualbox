50d8a8c00fceb7eff6fd5198fc54d8c90cddc109
Ulrich Moeller <noreply@oracle.com>
2009-07-07 16:38:46 +0000

    backing out r49709 to fix windows burns for now
    svn:sync-xref-src-repo-rev: r49711

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 21360 2009-07-07 16:33:26Z noreply@oracle.com $ */
+/* $Id: ConsoleImpl.cpp 21362 2009-07-07 16:38:46Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -6665,7 +6665,7 @@ DECLCALLBACK (int) Console::powerUpThread (RTTHREAD Thread, void *pvUser)
                 alock.leave();
 
                 /* Load saved state? */
-                if (!task->mSavedStateFile.isEmpty())
+                if (!!task->mSavedStateFile)
                 {
                     LogFlowFunc (("Restoring saved state from '%s'...\n",
                                   task->mSavedStateFile.raw()));
