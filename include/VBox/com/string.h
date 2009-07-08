22b2767425a2a180d44e08b1633ac47fc691e62e
Ulrich Moeller <noreply@oracle.com>
2009-07-08 15:36:59 +0000

    IPRT: rename ministring, add iprt:: namespace
    svn:sync-xref-src-repo-rev: r49785

--- include/VBox/com/string.h
+++ include/VBox/com/string.h
@@ -1,4 +1,4 @@
-/* $Id: string.h 21369 2009-07-07 18:52:12Z noreply@oracle.com $ */
+/* $Id: string.h 21409 2009-07-08 15:36:59Z noreply@oracle.com $ */
 
 /** @file
  * MS COM / XPCOM Abstraction Layer:
@@ -323,18 +323,18 @@ inline bool operator!=(BSTR l, const Bstr &r) { return r.operator!= (l); }
  *    of COM methods using the #asOutParam() operation and correctly free them
  *    afterwards.
  */
-class Utf8Str : public ministring
+class Utf8Str : public iprt::MiniString
 {
 public:
 
     Utf8Str() {}
 
     Utf8Str(const Utf8Str &that)
-        : ministring(that)
+        : MiniString(that)
     {}
 
     Utf8Str(const char *that)
-        : ministring(that)
+        : MiniString(that)
     {}
 
     Utf8Str(const Bstr &that)
@@ -349,13 +349,13 @@ public:
 
     Utf8Str& operator=(const Utf8Str &that)
     {
-        ministring::operator=(that);
+        MiniString::operator=(that);
         return *this;
     }
 
     Utf8Str& operator=(const char *that)
     {
-        ministring::operator=(that);
+        MiniString::operator=(that);
         return *this;
     }
 
