9b309b8866c776b51147c4be4f93e9c4e7219492
Christian Poetzsch <noreply@oracle.com>
2009-07-09 12:35:16 +0000

    OVF: enable OVF remote export again
    svn:sync-xref-src-repo-rev: r49836

--- src/VBox/Main/ApplianceImpl.cpp
+++ src/VBox/Main/ApplianceImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImpl.cpp 21404 2009-07-08 15:19:42Z noreply@oracle.com $ */
+/* $Id: ApplianceImpl.cpp 21438 2009-07-09 12:35:16Z noreply@oracle.com $ */
 /** @file
  *
  * IAppliance and IVirtualSystem COM class implementations.
@@ -2718,15 +2718,13 @@ HRESULT Appliance::writeImpl(int aFormat, Utf8Str aPath, ComObjPtr<Progress> &aP
         /* Check the URI for the target format */
         if (m->strPath.startsWith("SunCloud://", Utf8Str::CaseInsensitive)) /* Sun Cloud service */
         {
-            throw E_NOTIMPL;
-//            task->storageType = VFSType_S3;
-//            strProtocol = "SunCloud://";
+            task->storageType = VFSType_S3;
+            strProtocol = "SunCloud://";
         }
         else if (m->strPath.startsWith("S3://", Utf8Str::CaseInsensitive)) /* S3 service */
         {
-            throw E_NOTIMPL;
-//            task->storageType = VFSType_S3;
-//            strProtocol = "S3://";
+            task->storageType = VFSType_S3;
+            strProtocol = "S3://";
         }
         else if (m->strPath.startsWith("webdav://", Utf8Str::CaseInsensitive)) /* webdav service */
             throw E_NOTIMPL;
