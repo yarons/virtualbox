3af91395b4f67e225ecacc4119f39eef8bfd3a6b
Sander van Leeuwen <noreply@oracle.com>
2009-07-14 10:53:43 +0000

    Put back relevant comment
    svn:sync-xref-src-repo-rev: r50055

--- src/VBox/VMM/VMMR0/HWACCMR0A.asm
+++ src/VBox/VMM/VMMR0/HWACCMR0A.asm
@@ -1,4 +1,4 @@
-; $Id: HWACCMR0A.asm 21570 2009-07-14 10:15:40Z noreply@oracle.com $
+; $Id: HWACCMR0A.asm 21572 2009-07-14 10:53:43Z noreply@oracle.com $
 ;; @file
 ; VMXM - R0 vmx helpers
 ;
@@ -948,6 +948,10 @@ BEGINPROC SVMR0InvlpgA
     movzx   rax, edi
     mov     ecx, esi
  %else
+    ; from http://www.cs.cmu.edu/~fp/courses/15213-s06/misc/asm64-handout.pdf:
+    ; ``Perhaps unexpectedly, instructions that move or generate 32-bit register
+    ;   values also set the upper 32 bits of the register to zero. Consequently
+    ;   there is no need for an instruction movzlq.''
     mov     eax, ecx
     mov     ecx, edx
  %endif
