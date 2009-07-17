e87856159c7f071dc0d34176e24a52fb2c5dce5f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-07-17 12:18:30 +0000

    IPRT: Moved rtPathVolumeSpecLen.
    svn:sync-xref-src-repo-rev: r50224

--- src/VBox/Runtime/include/internal/fs.h
+++ src/VBox/Runtime/include/internal/fs.h
@@ -1,4 +1,4 @@
-/* $Id: fs.h 20374 2009-06-08 00:43:21Z knut.osmundsen@oracle.com $ */
+/* $Id: fs.h 21675 2009-07-17 12:18:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Internal RTFs header.
  */
@@ -44,7 +44,6 @@ RTFMODE rtFsModeNormalize(RTFMODE fMode, const char *pszName, size_t cbName);
 bool    rtFsModeIsValid(RTFMODE fMode);
 bool    rtFsModeIsValidPermissions(RTFMODE fMode);
 
-size_t  rtPathVolumeSpecLen(const char *pszPath);
 #ifndef RT_OS_WINDOWS
 void    rtFsConvertStatToObjInfo(PRTFSOBJINFO pObjInfo, const struct stat *pStat, const char *pszName, unsigned cbName);
 #endif
