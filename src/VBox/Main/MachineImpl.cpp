d97beb07994d19e3ac726b36c950ff82dbdb247d
Ulrich Moeller <noreply@oracle.com>
2009-07-27 20:00:42 +0000

    Main: fix wrong snapshot counts introduced by r50442 (erroneous error message when discarding snapshots)
    svn:sync-xref-src-repo-rev: r50445

--- src/VBox/Main/MachineImpl.cpp
+++ src/VBox/Main/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 21823 2009-07-27 18:46:11Z noreply@oracle.com $ */
+/* $Id: MachineImpl.cpp 21826 2009-07-27 20:00:42Z noreply@oracle.com $ */
 
 /** @file
  * Implementation of IMachine in VBoxSVC.
@@ -1729,7 +1729,7 @@ STDMETHODIMP Machine::COMGETTER(SnapshotCount)(ULONG *aSnapshotCount)
 
     *aSnapshotCount = !mData->mFirstSnapshot
                           ? 0
-                          : mData->mFirstSnapshot->getGrandChildrenCount() + 1;
+                          : mData->mFirstSnapshot->getAllChildrenCount() + 1;
 
     return S_OK;
 }
@@ -4330,7 +4330,7 @@ HRESULT Machine::trySetRegistered (BOOL aRegistered)
 
         size_t snapshotCount = 0;
         if (mData->mFirstSnapshot)
-            snapshotCount = mData->mFirstSnapshot->getGrandChildrenCount() + 1;
+            snapshotCount = mData->mFirstSnapshot->getAllChildrenCount() + 1;
         if (snapshotCount)
             return setError (VBOX_E_INVALID_OBJECT_STATE,
                 tr ("Cannot unregister the machine '%ls' because it "
@@ -9213,7 +9213,7 @@ STDMETHODIMP SessionMachine::DiscardSnapshot(IConsole *aInitiator,
 
     AutoWriteLock snapshotLock(snapshot);
 
-    size_t childrenCount = snapshot->getGrandChildrenCount();
+    size_t childrenCount = snapshot->getChildrenCount();
     if (childrenCount > 1)
         return setError(VBOX_E_INVALID_OBJECT_STATE,
                         tr("Snapshot '%ls' of the machine '%ls' has more than one child snapshot (%d)"),
