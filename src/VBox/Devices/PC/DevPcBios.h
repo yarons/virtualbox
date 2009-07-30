17727703ca3627936bb4be16c22b9fd3cebc63c6
Frank Mehnert <noreply@oracle.com>
2009-07-30 12:12:23 +0000

    Dev/PcBios: added chassis information; don't change the number of DMI structures but use the ''inactive'' type to disable dedicated structures
    svn:sync-xref-src-repo-rev: r50520

--- src/VBox/Devices/PC/DevPcBios.h
+++ src/VBox/Devices/PC/DevPcBios.h
@@ -1,4 +1,4 @@
-/* $Id: DevPcBios.h 21869 2009-07-30 09:03:19Z andreas.loeffler@oracle.com $ */
+/* $Id: DevPcBios.h 21874 2009-07-30 12:12:23Z noreply@oracle.com $ */
 /** @file
  * DevPcBios - PC BIOS Device, header shared with the BIOS code.
  */
@@ -25,7 +25,7 @@
 /** @def VBOX_DMI_TABLE_BASE */
 #define VBOX_DMI_TABLE_BASE          0xe1000
 #define VBOX_DMI_TABLE_VER           0x25
-#define VBOX_DMI_TABLE_ENTR          4
+#define VBOX_DMI_TABLE_ENTR          5
 #define VBOX_DMI_TABLE_SIZE          0x100
 
 
