3df2a6cb46eca7c1f7d7430795f4d3ef6000d101
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-08-02 18:21:53 +0000

    SSM: release log entry for successful save.
    svn:sync-xref-src-repo-rev: r50602

--- src/VBox/VMM/SSM-new.cpp
+++ src/VBox/VMM/SSM-new.cpp
@@ -1,4 +1,4 @@
-/* $Id: SSM-new.cpp 21932 2009-08-02 17:39:04Z knut.osmundsen@oracle.com $ */
+/* $Id: SSM-new.cpp 21935 2009-08-02 18:21:53Z knut.osmundsen@oracle.com $ */
 /** @file
  * SSM - Saved State Manager.
  */
@@ -2880,7 +2880,9 @@ VMMR3DECL(int) SSMR3Save(PVM pVM, const char *pszFilename, SSMAFTER enmAfter, PF
         {
             if (pfnProgress)
                 pfnProgress(pVM, 100, pvUser);
-            Log(("SSM: Successfully saved the vm state to '%s'.\n", pszFilename));
+            LogRel(("SSM: Successfully saved the VM state to '%s'\n"
+                    "SSM: Footer at %#llx (%lld), %u directory entries.\n",
+                    pszFilename, u.Footer.offStream, u.Footer.offStream, u.Footer.cDirEntries));
             return VINF_SUCCESS;
         }
     }
