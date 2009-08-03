ffd213de11c76c583ce44a9432301263516a8c23
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-08-03 14:39:00 +0000

    VMM: detect syscall usage on the host.
    svn:sync-xref-src-repo-rev: r50627

--- src/VBox/VMM/VMMSwitcher.cpp
+++ src/VBox/VMM/VMMSwitcher.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMMSwitcher.cpp 20864 2009-06-23 19:19:42Z knut.osmundsen@oracle.com $ */
+/* $Id: VMMSwitcher.cpp 21942 2009-08-03 14:39:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMM - The Virtual Machine Monitor, World Switcher(s).
  */
@@ -557,13 +557,13 @@ static void vmmR3SwitcherGenericRelocate(PVM pVM, PVMMSWITCHERDEF pSwitcher, RTR
             }
 
             /*
-             * Insert relative jump to specified target it SYSENTER isn't used by the host.
+             * Insert relative jump to specified target it SYSCALL isn't used by the host.
              */
             case FIX_NO_SYSCALL_JMP:
             {
                 uint32_t offTrg = *u.pu32++;
                 Assert(offTrg < pSwitcher->cbCode);
-                if (!CPUMIsHostUsingSysEnter(pVM))
+                if (!CPUMIsHostUsingSysCall(pVM))
                 {
                     *uSrc.pu8++ = 0xe9; /* jmp rel32 */
                     *uSrc.pu32++ = offTrg - (offSrc + 5);
