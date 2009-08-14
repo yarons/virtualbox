e6592b1c7e71a6bd51e2e3cd788c12e3756d2c68
Sander van Leeuwen <noreply@oracle.com>
2009-08-14 15:26:04 +0000

    Accidental change
    svn:sync-xref-src-repo-rev: r51100

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl2.cpp 22264 2009-08-14 15:23:54Z noreply@oracle.com $ */
+/* $Id: ConsoleImpl2.cpp 22265 2009-08-14 15:26:04Z noreply@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation
  *
@@ -236,7 +236,7 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
     rc = CFGMR3InsertInteger(pRoot, "PATMEnabled",          1);     /* boolean */   RC_CHECK();
     rc = CFGMR3InsertInteger(pRoot, "CSAMEnabled",          1);     /* boolean */   RC_CHECK();
 
-    if (osTypeId == "WindowsNT")
+    if (osTypeId == "WindowsNT4")
     {
         /*
          * We must limit CPUID count for Windows NT 4, as otherwise it stops
