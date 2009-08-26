60585006ba21bc8192a8e0bcd4657dc74c1cd89f
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2009-08-26 12:49:54 +0000

    HGSMI/VBVA VGA saved state updates.
    svn:sync-xref-src-repo-rev: r51482

--- src/VBox/Devices/Graphics/DevVGA.h
+++ src/VBox/Devices/Graphics/DevVGA.h
@@ -1,4 +1,4 @@
-/* $Id: DevVGA.h 22444 2009-08-25 17:03:25Z vitali.pelenjow@oracle.com $ */
+/* $Id: DevVGA.h 22468 2009-08-26 12:49:54Z vitali.pelenjow@oracle.com $ */
 /** @file
  * DevVGA - VBox VGA/VESA device, internal header.
  */
@@ -50,6 +50,12 @@
 #define VGA_PORT_OFF_HGSMI_HOST  0
 #define VGA_PORT_OFF_HGSMI_GUEST 4
 #endif /* VBOX_WITH_HGSMI */
+
+#ifdef VBOX_WITH_HGSMI
+# define VGA_SAVEDSTATE_VERSION  3
+#else
+# define VGA_SAVEDSTATE_VERSION  2
+#endif
 
 #define MSR_COLOR_EMULATION 0x01
 #define MSR_PAGE_SELECT     0x20
