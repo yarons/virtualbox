69989e2bb6ca4814bfe41cd955f37b90546fc8c1
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-08-26 19:02:36 +0000

    EMHwaccm.cpp: Only throw an internal error if it's an informational status code.
    svn:sync-xref-src-repo-rev: r51506

--- src/VBox/VMM/EMHwaccm.cpp
+++ src/VBox/VMM/EMHwaccm.cpp
@@ -1,4 +1,4 @@
-/* $Id: EMHwaccm.cpp 22482 2009-08-26 17:27:29Z knut.osmundsen@oracle.com $ */
+/* $Id: EMHwaccm.cpp 22486 2009-08-26 19:02:36Z knut.osmundsen@oracle.com $ */
 /** @file
  * EM - Execution Monitor / Manager - hardware virtualization
  */
@@ -307,7 +307,7 @@ static int emR3ExecuteIOInstruction(PVM pVM, PVMCPU pVCpu)
         STAM_PROFILE_STOP(&pVCpu->em.s.StatIOEmu, a);
         return rc;     /* rip already updated. */
     }
-    AssertMsgReturn(rc == VERR_NOT_FOUND, ("%Rrc\n", rc), VERR_INTERNAL_ERROR_5);
+    AssertMsgReturn(rc == VERR_NOT_FOUND, ("%Rrc\n", rc), RT_SUCCESS_NP(rc) ? VERR_INTERNAL_ERROR_5 : rc);
 
     /** @todo probably we should fall back to the recompiler; otherwise we'll go back and forth between HC & GC
      *   as io instructions tend to come in packages of more than one
