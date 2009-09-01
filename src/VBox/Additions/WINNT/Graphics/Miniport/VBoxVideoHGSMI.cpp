7b52aac4f06fa9551216659fce0950bab82af171
Frank Mehnert <noreply@oracle.com>
2009-09-01 12:00:53 +0000

    fixed OSE headers
    svn:sync-xref-src-repo-rev: r51743

--- src/VBox/Additions/WINNT/Graphics/Miniport/VBoxVideoHGSMI.cpp
+++ src/VBox/Additions/WINNT/Graphics/Miniport/VBoxVideoHGSMI.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxVideoHGSMI.cpp 22645 2009-09-01 10:42:04Z noreply@oracle.com $ */
+/* $Id: VBoxVideoHGSMI.cpp 22652 2009-09-01 12:00:53Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Video miniport driver for NT/2k/XP - HGSMI related functions.
  */
@@ -6,8 +6,17 @@
 /*
  * Copyright (C) 2006-2009 Sun Microsystems, Inc.
  *
- * Sun Microsystems, Inc. confidential
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
  */
 
 
