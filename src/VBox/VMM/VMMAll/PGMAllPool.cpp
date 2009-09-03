dd21121b9e4764b6bd45ffa6e7aa692be3086aae
Sander van Leeuwen <noreply@oracle.com>
2009-09-03 13:20:54 +0000

    Updates
    svn:sync-xref-src-repo-rev: r51892

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPool.cpp 22745 2009-09-03 13:10:03Z noreply@oracle.com $ */
+/* $Id: PGMAllPool.cpp 22747 2009-09-03 13:20:54Z noreply@oracle.com $ */
 /** @file
  * PGM Shadow Page Pool.
  */
@@ -1382,11 +1382,10 @@ DECLEXPORT(int) pgmPoolAccessHandler(PVM pVM, RTGCUINT uErrorCode, PCPUMCTXCORE 
 DECLINLINE(void) pgmPoolTrackCheckPTPaePae(PPGMPOOL pPool, PPGMPOOLPAGE pPage, PX86PTPAE pShwPT, PCX86PTPAE pGstPT)
 {
     unsigned cErrors = 0;
+    Assert(pPage->iFirstPresent != ~0);
 #ifdef VBOX_STRICT
     for (unsigned i = 0; i < pPage->iFirstPresent; i++)
-    {
-        Assert(!pShwPT->a[i].n.u1Present);
-    }
+        AssertMsg(!pShwPT->a[i].n.u1Present, ("Unexpected PTE: idx=%d %RX64 (first=%d)\n", i, pShwPT->a[i].u,  pPage->iFirstPresent));
 #endif
     for (unsigned i = pPage->iFirstPresent; i < RT_ELEMENTS(pShwPT->a); i++)
     {
@@ -1444,11 +1443,10 @@ DECLINLINE(unsigned) pgmPoolTrackFlushPTPaePae(PPGMPOOL pPool, PPGMPOOLPAGE pPag
 {
     unsigned cChanged = 0;
 
+    Assert(pPage->iFirstPresent != ~0);
 #ifdef VBOX_STRICT
     for (unsigned i = 0; i < pPage->iFirstPresent; i++)
-    {
-        Assert(!pShwPT->a[i].n.u1Present);
-    }
+        AssertMsg(!pShwPT->a[i].n.u1Present, ("Unexpected PTE: idx=%d %RX64 (first=%d)\n", i, pShwPT->a[i].u,  pPage->iFirstPresent));
 #endif
     for (unsigned i = pPage->iFirstPresent; i < RT_ELEMENTS(pShwPT->a); i++)
     {
