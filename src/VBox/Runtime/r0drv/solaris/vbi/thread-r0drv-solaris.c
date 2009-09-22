faf6fa129ca03249a9a4303647df2ca79fb50427
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2009-09-22 13:29:49 +0000

    Solaris/thread-r0drv-solaris: try calling splx without really calling splr.
    svn:sync-xref-src-repo-rev: r52674

--- src/VBox/Runtime/r0drv/solaris/vbi/thread-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/vbi/thread-r0drv-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: thread-r0drv-solaris.c 23076 2009-09-17 09:07:03Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: thread-r0drv-solaris.c 23216 2009-09-22 13:29:49Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IPRT - Threads, Ring-0 Driver, Solaris.
  */
@@ -119,7 +119,7 @@ RTDECL(void) RTThreadPreemptDisable(PRTTHREADPREEMPTSTATE pState)
 
     vbi_preempt_disable();
 
-    pState->uOldPil = splr(ipltospl(0));
+    pState->uOldPil = getpil();
     Assert(pState->uOldPil != UINT32_MAX);
     RT_ASSERT_PREEMPT_CPUID_DISABLE(pState);
 }
