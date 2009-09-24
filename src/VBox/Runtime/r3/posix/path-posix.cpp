8b98001602de8e8c429d1a0a1e49f7d021e66e62
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-09-24 20:14:12 +0000

    IPRT: Fixed mac-ose build break caused by 10.4 not having lutimes().
    svn:sync-xref-src-repo-rev: r52809

--- src/VBox/Runtime/r3/posix/path-posix.cpp
+++ src/VBox/Runtime/r3/posix/path-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: path-posix.cpp 23291 2009-09-24 16:08:19Z knut.osmundsen@oracle.com $ */
+/* $Id: path-posix.cpp 23312 2009-09-24 20:14:12Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Path Manipulation, POSIX.
  */
@@ -590,7 +590,10 @@ RTR3DECL(int) RTPathSetTimesEx(const char *pszPath, PCRTTIMESPEC pAccessTime, PC
                     if (utimes(pszNativePath, aTimevals))
                         rc = RTErrConvertFromErrno(errno);
                 }
-#if defined(RT_OS_DARWIN) || defined(RT_OS_FREEBSD) || defined(RT_OS_LINUX) || defined(RT_OS_OS2) /** @todo who really has lutimes? */
+#if (defined(RT_OS_DARWIN) && MAC_OS_X_VERSION_MIN_REQUIRED >= 1050) \
+ || defined(RT_OS_FREEBSD) \
+ || defined(RT_OS_LINUX) \
+ || defined(RT_OS_OS2) /** @todo who really has lutimes? */
                 else
                 {
                     if (lutimes(pszNativePath, aTimevals))
