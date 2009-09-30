3548f12db8a48e71dfbc6136ab52576cad186f3e
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2009-09-30 18:59:14 +0000

    whitespace
    svn:sync-xref-src-repo-rev: r53048

--- src/VBox/Main/MediumImpl.cpp
+++ src/VBox/Main/MediumImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MediumImpl.cpp 23337 2009-09-25 15:04:23Z klaus.espenlaub@oracle.com $ */
+/* $Id: MediumImpl.cpp 23451 2009-09-30 18:59:14Z klaus.espenlaub@oracle.com $ */
 
 /** @file
  *
@@ -360,7 +360,7 @@ HRESULT Medium::Task::runNow()
  *       Medium::treeLock() is held in read mode.
  */
 class Medium::MergeChain : public Medium::List,
-                             public com::SupportErrorInfoBase
+                           public com::SupportErrorInfoBase
 {
 public:
 
