24a5e3bf5848917d093e7f8a16c286f00c90f92d
Frank Mehnert <noreply@oracle.com>
2009-09-30 12:44:51 +0000

    fixed OSE header
    svn:sync-xref-src-repo-rev: r53033

--- src/VBox/Additions/common/crOpenGL/pack/packspu_glsl.c
+++ src/VBox/Additions/common/crOpenGL/pack/packspu_glsl.c
@@ -1,4 +1,4 @@
-/* $Id: packspu_glsl.c 23437 2009-09-30 12:33:25Z noreply@oracle.com $ */
+/* $Id: packspu_glsl.c 23440 2009-09-30 12:44:51Z noreply@oracle.com $ */
 
 /** @file
  * VBox OpenGL GLSL related functions
@@ -7,8 +7,17 @@
 /*
  * Copyright (C) 2009 Sun Microsystems, Inc.
  *
- * Sun Microsystems, Inc. confidential
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
  */
 
 #include "packspu.h"
