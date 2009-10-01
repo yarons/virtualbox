9b0dbcc7db06a97066c514e977caac95e298023d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-10-01 01:13:47 +0000

    PGMPAGE: Enable HCPhys and idPage merge.
    svn:sync-xref-src-repo-rev: r53056

--- src/VBox/VMM/PGMInternal.h
+++ src/VBox/VMM/PGMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: PGMInternal.h 23458 2009-10-01 01:12:59Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMInternal.h 23459 2009-10-01 01:13:47Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Internal header file.
  */
@@ -619,7 +619,6 @@ AssertCompile(PGMPAGETYPE_END <= 7);
 #define PGMPAGETYPE_IS_NP(type)         ( (type) == PGMPAGETYPE_MMIO )
 /** @} */
 
-#if 0 /* disabled temporarily */
 /** Enables the new structure layout.
  * Some bits are better others are not.  */
 #define PGM_PAGE_WITH_PAGEID_IN_HCPHYS
@@ -627,8 +626,6 @@ AssertCompile(PGMPAGETYPE_END <= 7);
 /** When non-zero, PGMPAGE is packed. When zero some of the important fields
  *  are expaned to 8-bit size to allow the compiler to use byte access. */
 #define PGM_PAGE_PACKED     0
-#endif
-
 
 /**
  * A Physical Guest Page tracking structure.
