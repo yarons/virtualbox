6d2f00b9abcef8cc135a247c5bb4a1a490b19ec2
Andreas Loeffler <andreas.loeffler@oracle.com>
2009-10-09 07:54:09 +0000

    VBoxService: No need for RTStrCurrentCPToUtf8() anymore.
    svn:sync-xref-src-repo-rev: r53320

--- src/VBox/Additions/common/VBoxService/VBoxServiceExec.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceExec.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceExec.cpp 23608 2009-10-07 20:41:52Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxServiceExec.cpp 23634 2009-10-09 07:54:09Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxServiceExec - Host-driven Command Execution.
  */
@@ -138,14 +138,6 @@ static int VBoxServiceExecReadHostProp(const char *pszPropName, char **ppszValue
     int     rc;
     *ppszValue = NULL;
 
-    char *pszPropNameUTF8;
-    rc = RTStrCurrentCPToUtf8(&pszPropNameUTF8, pszPropName);
-    if (RT_FAILURE(rc))
-    {
-        VBoxServiceError("Exec: Failed to convert property name \"%s\" to UTF-8!\n", pszPropName);
-        return rc;
-    }
-
     for (unsigned cTries = 0; cTries < 10; cTries++)
     {
         /*
@@ -162,7 +154,7 @@ static int VBoxServiceExecReadHostProp(const char *pszPropName, char **ppszValue
         char    *pszValue;
         char    *pszFlags;
         uint64_t uTimestamp;
-        rc = VbglR3GuestPropRead(g_uExecGuestPropSvcClientID, pszPropNameUTF8,
+        rc = VbglR3GuestPropRead(g_uExecGuestPropSvcClientID, pszPropName,
                                  pvBuf, cbBuf,
                                  &pszValue, &uTimestamp, &pszFlags, NULL);
         if (RT_FAILURE(rc))
@@ -207,7 +199,6 @@ static int VBoxServiceExecReadHostProp(const char *pszPropName, char **ppszValue
         break; /* done */
     }
     RTMemFree(pvBuf);
-    RTStrFree(pszPropNameUTF8);
     return rc;
 }
 
