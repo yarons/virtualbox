df48f747c98d965b2a73fad9606c06393b9c8b55
Alexander Eichner <alexander.eichner@oracle.com>
2009-10-10 21:44:27 +0000

    Windows build fix
    svn:sync-xref-src-repo-rev: r53387

--- src/VBox/Main/MachineImpl.cpp
+++ src/VBox/Main/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 23670 2009-10-10 03:08:58Z knut.osmundsen@oracle.com $ */
+/* $Id: MachineImpl.cpp 23674 2009-10-10 21:44:27Z alexander.eichner@oracle.com $ */
 
 /** @file
  * Implementation of IMachine in VBoxSVC.
@@ -3033,8 +3033,8 @@ STDMETHODIMP Machine::CanShowConsoleWindow (BOOL *aCanShow)
 
         if (mData->mSession.mState != SessionState_Open)
             return setError(VBOX_E_INVALID_VM_STATE,
-                            tr("Machine session is not open (session state: %s)"),
-                            Global::stringifyMachineState(mData->mSession.mState));
+                            tr("Machine session is not open (session state: %d)"),
+                            mData->mSession.mState);
 
         directControl = mData->mSession.mDirectControl;
     }
