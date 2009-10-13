9e4a2040ab9708bcf8b29625be55a07f8c1cb41b
Mikhail Sennikovsky <noreply@oracle.com>
2009-10-13 20:01:51 +0000

    solaris burn fix
    svn:sync-xref-src-repo-rev: r53465

--- src/VBox/Main/generic/OpenGLTestApp.cpp
+++ src/VBox/Main/generic/OpenGLTestApp.cpp
@@ -1,4 +1,4 @@
-/* $Id: OpenGLTestApp.cpp 23740 2009-10-13 19:54:22Z noreply@oracle.com $ */
+/* $Id: OpenGLTestApp.cpp 23741 2009-10-13 20:01:51Z noreply@oracle.com $ */
 /** @file
  * VBox host opengl support test application.
  */
@@ -26,6 +26,8 @@
 #ifdef RT_OS_WINDOWS
 #include <Windows.h>
 #endif
+
+#include <string>
 
 #ifdef VBOX_WITH_CROGL
 
