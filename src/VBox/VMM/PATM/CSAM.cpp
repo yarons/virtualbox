56cd68f4e0c34a94473b60450836c04cc78c4f59
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-10-14 15:47:54 +0000

    CSAM: The right fix (hopefully).
    svn:sync-xref-src-repo-rev: r53507

--- src/VBox/VMM/PATM/CSAM.cpp
+++ src/VBox/VMM/PATM/CSAM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CSAM.cpp 23768 2009-10-14 15:39:56Z knut.osmundsen@oracle.com $ */
+/* $Id: CSAM.cpp 23770 2009-10-14 15:47:54Z knut.osmundsen@oracle.com $ */
 /** @file
  * CSAM - Guest OS Code Scanning and Analysis Manager
  */
@@ -128,7 +128,7 @@ static const SSMFIELD g_aCsamFields[] =
     SSMFIELD_ENTRY(             CSAM, iCallInstruction),
     SSMFIELD_ENTRY(             CSAM, fScanningStarted),
     SSMFIELD_ENTRY(             CSAM, fGatesChecked),
-    SSMFIELD_ENTRY_PAD_HC(      CSAM, Alignment1, 4, 4),
+    SSMFIELD_ENTRY_PAD_HC(      CSAM, Alignment1, 6, 2),
     SSMFIELD_ENTRY_IGNORE(      CSAM, StatNrTraps),
     SSMFIELD_ENTRY_IGNORE(      CSAM, StatNrPages),
     SSMFIELD_ENTRY_IGNORE(      CSAM, StatNrPagesInv),
