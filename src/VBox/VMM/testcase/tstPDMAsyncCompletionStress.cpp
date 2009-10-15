6eff612b0aaab2414c842eee202335f0c99a132e
Alexander Eichner <alexander.eichner@oracle.com>
2009-10-15 19:50:32 +0000

    Async completion testcase: bugfixes
    svn:sync-xref-src-repo-rev: r53560

--- src/VBox/VMM/testcase/tstPDMAsyncCompletionStress.cpp
+++ src/VBox/VMM/testcase/tstPDMAsyncCompletionStress.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstPDMAsyncCompletionStress.cpp 23811 2009-10-15 17:52:07Z alexander.eichner@oracle.com $ */
+/* $Id: tstPDMAsyncCompletionStress.cpp 23813 2009-10-15 19:50:32Z alexander.eichner@oracle.com $ */
 /** @file
  * PDM Asynchronous Completion Stresstest.
  *
@@ -221,7 +221,7 @@ static int tstPDMACStressTestFileWrite(PPDMACTESTFILE pTestFile, PPDMACTESTFILET
     pTestTask->fWrite        = true;
     pTestTask->DataSeg.cbSeg = RTRandU32Ex(512, TASK_TRANSFER_SIZE_MAX) & ~511;
 
-    RTFOFF offMax;
+    uint64_t offMax;
 
     /* Did we reached the maximum file size */
     if (pTestFile->cbFileCurr < pTestFile->cbFileMax)
@@ -243,7 +243,7 @@ static int tstPDMACStressTestFileWrite(PPDMACTESTFILE pTestFile, PPDMACTESTFILET
     if (pTestFile->cbFileCurr == pTestFile->cbFileMax)
         offMin = 0;
     else
-	offMin = (offMax - pTestTask->DataSeg.cbSeg) < 0 ? 0 : (offMax - pTestTask->DataSeg.cbSeg);
+        offMin = RT_MIN(pTestFile->cbFileCurr, offMax);
 
 
     pTestTask->off = RTRandU64Ex(offMin, offMax) & ~511;
