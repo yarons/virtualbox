321b1514b693d2c087a2fa5643407f531262e7c5
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-10-16 21:10:24 +0000

    PGMPhys.cpp: reverted r53588 - wrong fix.
    svn:sync-xref-src-repo-rev: r53590

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 23831 2009-10-16 21:08:39Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMPhys.cpp 23833 2009-10-16 21:10:24Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -3113,8 +3113,7 @@ VMMR3DECL(int) PGMR3PhysAllocateHandyPages(PVM pVM)
                 pVM->pgm.s.cSharedPages,
                 pVM->pgm.s.cZeroPages));
         if (    rc != VERR_NO_MEMORY
-            &&  rc != VERR_LOCK_FAILED
-            &&  rc != VERR_TRY_AGAIN)
+            &&  rc != VERR_LOCK_FAILED)
         {
             for (uint32_t i = 0; i < RT_ELEMENTS(pVM->pgm.s.aHandyPages); i++)
             {
@@ -3142,8 +3141,7 @@ VMMR3DECL(int) PGMR3PhysAllocateHandyPages(PVM pVM)
         VM_FF_SET(pVM, VM_FF_PGM_NEED_HANDY_PAGES);
         VM_FF_SET(pVM, VM_FF_PGM_NO_MEMORY);
         if (    rc == VERR_NO_MEMORY
-            ||  rc == VERR_LOCK_FAILED
-            ||  rc == VERR_TRY_AGAIN)
+            ||  rc == VERR_LOCK_FAILED)
             rc = VINF_EM_NO_MEMORY;
     }
 
