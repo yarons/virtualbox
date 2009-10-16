738091706e06a35c9b305b657b3e25368b5f088d
Andreas Loeffler <andreas.loeffler@oracle.com>
2009-10-16 22:24:40 +0000

    VBoxClient/VBoxTray/VBgl: More bits for host version checking.
    svn:sync-xref-src-repo-rev: r53593

--- src/VBox/Additions/WINNT/VBoxTray/helpers.h
+++ src/VBox/Additions/WINNT/VBoxTray/helpers.h
@@ -1,4 +1,4 @@
-/* $Id: helpers.h 23053 2009-09-16 08:42:56Z andreas.loeffler@oracle.com $ */
+/* $Id: helpers.h 23835 2009-10-16 22:24:40Z andreas.loeffler@oracle.com $ */
 /** @file
  * helpers - Guest Additions Service helper functions header
  */
@@ -32,7 +32,6 @@
 
 void resizeRect(RECTL *paRects, unsigned nRects, unsigned iPrimary, unsigned iResized, int NewWidth, int NewHeight);
 int showBalloonTip (HINSTANCE hInst, HWND hWnd, UINT uID, const char *pszMsg, const char *pszTitle, UINT uTimeout, DWORD dwInfoFlags);
-int getAdditionsVersion(char *pszVer, size_t cbSizeVer, char *pszRev, size_t cbSizeRev);
 
 #endif /* !___VBOXTRAY_HELPERS_H */
 
