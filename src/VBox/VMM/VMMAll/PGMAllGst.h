3a88c779ed46b8debcebf3bd60c6535f5c1e0760
Sander van Leeuwen <noreply@oracle.com>
2009-10-19 10:14:02 +0000

    Annoying assertion
    svn:sync-xref-src-repo-rev: r53614

--- src/VBox/VMM/VMMAll/PGMAllGst.h
+++ src/VBox/VMM/VMMAll/PGMAllGst.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllGst.h 23844 2009-10-19 08:21:09Z noreply@oracle.com $ */
+/* $Id: PGMAllGst.h 23851 2009-10-19 10:14:02Z noreply@oracle.com $ */
 /** @file
  * VBox - Page Manager, Guest Paging Template - All context code.
  */
@@ -61,8 +61,6 @@ PGM_GST_DECL(int, GetPage)(PVMCPU pVCpu, RTGCPTR GCPtr, uint64_t *pfFlags, PRTGC
     return VINF_SUCCESS;
 
 #elif PGM_GST_TYPE == PGM_TYPE_32BIT || PGM_GST_TYPE == PGM_TYPE_PAE || PGM_GST_TYPE == PGM_TYPE_AMD64
-
-    Assert((GCPtr & PAGE_OFFSET_MASK) == 0);
 
     PVM pVM = pVCpu->CTX_SUFF(pVM);
     /*
