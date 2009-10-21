d7b0284c76fc5428fa3a1f38319b9fc019e076c7
Sander van Leeuwen <noreply@oracle.com>
2009-10-21 15:54:04 +0000

    Format specifier fix
    svn:sync-xref-src-repo-rev: r53761

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWVMXR0.cpp 23943 2009-10-21 15:48:36Z noreply@oracle.com $ */
+/* $Id: HWVMXR0.cpp 23944 2009-10-21 15:54:04Z noreply@oracle.com $ */
 /** @file
  * HWACCM VMX - Host Context Ring 0.
  */
@@ -4155,7 +4155,7 @@ static void vmxR0FlushVPID(PVM pVM, PVMCPU pVCpu, VMX_FLUSH enmFlush, RTGCPTR GC
         descriptor[0] = pVCpu->hwaccm.s.uCurrentASID;
         descriptor[1] = GCPtr;
         int rc = VMXR0InvVPID(enmFlush, &descriptor[0]);
-        AssertMsg(rc == VINF_SUCCESS, ("VMXR0InvVPID %x %x %VGv failed with %rc\n", enmFlush, pVCpu->hwaccm.s.uCurrentASID, GCPtr, rc));
+        AssertMsg(rc == VINF_SUCCESS, ("VMXR0InvVPID %x %x %RGv failed with %d\n", enmFlush, pVCpu->hwaccm.s.uCurrentASID, GCPtr, rc));
     }
 }
 #endif /* HWACCM_VTX_WITH_VPID */
