2a1b31c64a774807ff1eead6eef28e3ceeb9f86f
Sander van Leeuwen <noreply@oracle.com>
2009-10-22 12:59:53 +0000

    Cleaned up
    svn:sync-xref-src-repo-rev: r53808

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWVMXR0.cpp 23976 2009-10-22 12:57:29Z noreply@oracle.com $ */
+/* $Id: HWVMXR0.cpp 23977 2009-10-22 12:59:53Z noreply@oracle.com $ */
 /** @file
  * HWACCM VMX - Host Context Ring 0.
  */
@@ -2257,7 +2257,6 @@ VMMR0DECL(int) VMXR0RunGuestCode(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx)
     RTGCUINTPTR errCode, instrInfo;
     bool        fSetupTPRCaching = false;
     uint8_t     u8LastTPR = 0;
-    PHWACCM_CPUINFO pCpu = 0;
     RTCCUINTREG uOldEFlags = ~(RTCCUINTREG)0;
     unsigned    cResume = 0;
 #ifdef VBOX_STRICT
@@ -2497,6 +2496,8 @@ ResumeExecution:
 # endif /* HWACCM_VTX_WITH_VPID */
         )
     {
+        PHWACCM_CPUINFO pCpu;
+
         pCpu = HWACCMR0GetCurrentCpu();
         if (    pVCpu->hwaccm.s.idLastCpu   != pCpu->idCpu
             ||  pVCpu->hwaccm.s.cTLBFlushes != pCpu->cTLBFlushes)
