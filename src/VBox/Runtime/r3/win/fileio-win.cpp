5150d91041c23d9394ab12972af4dc2c14104553
Mikhail Sennikovsky <noreply@oracle.com>
2009-10-23 06:06:46 +0000

    disable incorrect assert
    svn:sync-xref-src-repo-rev: r53840

--- src/VBox/Runtime/r3/win/fileio-win.cpp
+++ src/VBox/Runtime/r3/win/fileio-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: fileio-win.cpp 23973 2009-10-22 12:34:22Z knut.osmundsen@oracle.com $ */
+/* $Id: fileio-win.cpp 24003 2009-10-23 06:06:46Z noreply@oracle.com $ */
 /** @file
  * IPRT - File I/O, native implementation for the Windows host platform.
  */
@@ -239,7 +239,7 @@ RTR3DECL(int) RTFileOpen(PRTFILE pFile, const char *pszFilename, uint32_t fOpen)
     }
 
     DWORD dwShareMode;
-    Assert(RTFILE_O_DENY_READWRITE == RTFILE_O_DENY_ALL && !RTFILE_O_DENY_NONE);
+/* misha: disable incorrect assert   Assert(RTFILE_O_DENY_READWRITE == RTFILE_O_DENY_ALL && !RTFILE_O_DENY_NONE); */
     switch (fOpen & RTFILE_O_DENY_MASK)
     {
         case RTFILE_O_DENY_NONE:                                dwShareMode = FILE_SHARE_READ | FILE_SHARE_WRITE; break;
