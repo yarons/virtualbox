031d67d6756aa0eac128063e7c28b1cf8ec45bd7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-10-26 16:14:52 +0000

    DevRTC: Added UseUTC config options (default is false). Save and verify the configuration.
    svn:sync-xref-src-repo-rev: r53966

--- src/VBox/Devices/testcase/tstDeviceStructSizeGC.cpp
+++ src/VBox/Devices/testcase/tstDeviceStructSizeGC.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstDeviceStructSizeGC.cpp 24087 2009-10-26 15:48:03Z knut.osmundsen@oracle.com $ */
+/* $Id: tstDeviceStructSizeGC.cpp 24089 2009-10-26 16:14:52Z knut.osmundsen@oracle.com $ */
 /** @file
  * tstDeviceStructSizeGC - Generate structure member and size checks from the GC perspective.
  *
@@ -554,6 +554,8 @@ int main()
     GEN_CHECK_OFF(RTCState, current_tm.tm_wday);
     GEN_CHECK_OFF(RTCState, current_tm.tm_yday);
     GEN_CHECK_OFF(RTCState, irq);
+    GEN_CHECK_OFF(RTCState, fUTC);
+    GEN_CHECK_OFF(RTCState, IOPortBase);
     GEN_CHECK_OFF(RTCState, pPeriodicTimerR0);
     GEN_CHECK_OFF(RTCState, pPeriodicTimerR3);
     GEN_CHECK_OFF(RTCState, pPeriodicTimerRC);
@@ -568,7 +570,6 @@ int main()
     GEN_CHECK_OFF(RTCState, pDevInsR0);
     GEN_CHECK_OFF(RTCState, pDevInsR3);
     GEN_CHECK_OFF(RTCState, pDevInsRC);
-    GEN_CHECK_OFF(RTCState, fUTC);
     GEN_CHECK_OFF(RTCState, RtcReg);
     GEN_CHECK_OFF(RTCState, pRtcHlpR3);
     GEN_CHECK_OFF(RTCState, cRelLogEntries);
