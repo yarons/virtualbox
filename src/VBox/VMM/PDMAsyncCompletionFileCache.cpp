53d426363d90ca1491ec06cb7e7f6fe30a2ff0e2
Alexander Eichner <alexander.eichner@oracle.com>
2009-11-05 21:07:56 +0000

    AsyncCompletion: Release the write lock when done. Should fix guest hangs
    svn:sync-xref-src-repo-rev: r54464

--- src/VBox/VMM/PDMAsyncCompletionFileCache.cpp
+++ src/VBox/VMM/PDMAsyncCompletionFileCache.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMAsyncCompletionFileCache.cpp 24359 2009-11-04 22:28:48Z alexander.eichner@oracle.com $ */
+/* $Id: PDMAsyncCompletionFileCache.cpp 24415 2009-11-05 21:07:56Z alexander.eichner@oracle.com $ */
 /** @file
  * PDM Async I/O - Transport data asynchronous in R3 using EMT.
  * File data cache.
@@ -1068,6 +1068,7 @@ int pdmacFileEpCacheRead(PPDMASYNCCOMPLETIONENDPOINTFILE pEndpoint, PPDMASYNCCOM
                         OffDiff  += cbCopy;
                         ASMAtomicSubS32(&pTask->cbTransferLeft, cbCopy);
                     }
+                    RTSemRWReleaseWrite(pEndpointCache->SemRWEntries);
                 }
                 else
                 {
