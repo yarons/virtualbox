0666251d54598e1fc85ba7d14fb9211c79d8cd72
Michael Thayer <noreply@oracle.com>
2009-11-09 23:17:57 +0000

    Additions/linux/installer: more fixes
    svn:sync-xref-src-repo-rev: r54636

--- src/VBox/Additions/linux/installer/vboxadd-x11.sh
+++ src/VBox/Additions/linux/installer/vboxadd-x11.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Sun VirtualBox
-# Linux Additions X11 setup init script ($Revision: 24502 $)
+# Linux Additions X11 setup init script ($Revision: 24531 $)
 #
 
 #
@@ -420,7 +420,7 @@ will not be installed."
         if [ $dox11config15suse -eq 1 ]
         then
             cp /etc/X11/xorg.conf /etc/X11/xorg.conf.bak 2> /dev/null
-            cp "$lib_dir/linux_xorg_suse11.conf" /etc/X11/xorg.conf 2> /dev/null
+            cp "$share_dir/linux_xorg_suse11.conf" /etc/X11/xorg.conf 2> /dev/null
         elif [ $dox11config15 -eq 1 ]
         then
             "$lib_dir/x11config15.pl" >> $LOG 2>&1
