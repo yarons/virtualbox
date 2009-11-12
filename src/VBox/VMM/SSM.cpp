76ed798c14d82a87e224e65395f119543090bb4e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-11-12 17:04:42 +0000

    SSM.cpp: assertion instead of bulky code.
    svn:sync-xref-src-repo-rev: r54764

--- src/VBox/VMM/SSM.cpp
+++ src/VBox/VMM/SSM.cpp
@@ -1,4 +1,4 @@
-/* $Id: SSM.cpp 24575 2009-11-11 09:54:23Z knut.osmundsen@oracle.com $ */
+/* $Id: SSM.cpp 24613 2009-11-12 17:04:42Z knut.osmundsen@oracle.com $ */
 /** @file
  * SSM - Saved State Manager.
  */
@@ -1470,12 +1470,10 @@ VMMR3_INT_DECL(int) SSMR3DeregisterDriver(PVM pVM, PPDMDRVINS pDrvIns, const cha
                 rc = VINF_SUCCESS;
                 continue;
             }
-            else if (pszName)
-            {
-                AssertMsgFailed(("Caller is not owner! Owner=%p Caller=%p %s\n",
-                                 pUnit->u.Drv.pDrvIns, pDrvIns, pszName));
-                return VERR_SSM_UNIT_NOT_OWNER;
-            }
+
+            AssertMsgReturn(!pszName,
+                            ("Caller is not owner! Owner=%p Caller=%p %s\n", pUnit->u.Drv.pDrvIns, pDrvIns, pszName),
+                            VERR_SSM_UNIT_NOT_OWNER);
         }
 
         /* next */
