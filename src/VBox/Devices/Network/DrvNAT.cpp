0e40fad26bee4a7a1b03e6ef04bee86530299563
Sander van Leeuwen <noreply@oracle.com>
2009-11-13 13:18:25 +0000

    Moved slirp_ext_m_free up a bit in drvNATUrgRecvWorker
    svn:sync-xref-src-repo-rev: r54792

--- src/VBox/Devices/Network/DrvNAT.cpp
+++ src/VBox/Devices/Network/DrvNAT.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvNAT.cpp 24635 2009-11-13 12:55:35Z noreply@oracle.com $ */
+/* $Id: DrvNAT.cpp 24636 2009-11-13 13:18:25Z noreply@oracle.com $ */
 /** @file
  * DrvNAT - NAT network transport driver.
  */
@@ -298,12 +298,12 @@ static DECLCALLBACK(void) drvNATUrgRecvWorker(PDRVNAT pThis, uint8_t *pu8Buf, in
     rc = RTCritSectLeave(&pThis->csDevAccess);
     AssertReleaseRC(rc);
 
+    slirp_ext_m_free(pThis->pNATState, pvArg);
     if (ASMAtomicDecU32(&pThis->cUrgPkt) == 0) 
     {
         drvNATRecvWakeup(pThis->pDrvIns, pThis->pRecvThread);
         drvNATNotifyNATThread(pThis);
     }
-    slirp_ext_m_free(pThis->pNATState, pvArg);
 }
 
 
