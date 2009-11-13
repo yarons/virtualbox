3f74fb45d1fc5f818a3f9f55d437ec8bd6f09d90
Nikolay Igotty <noreply@oracle.com>
2009-11-13 17:39:27 +0000

    Disabling asserts preventing ISO image mounts
    svn:sync-xref-src-repo-rev: r54816

--- src/VBox/Runtime/r3/fileio.cpp
+++ src/VBox/Runtime/r3/fileio.cpp
@@ -1,4 +1,4 @@
-/* $Id: fileio.cpp 23973 2009-10-22 12:34:22Z knut.osmundsen@oracle.com $ */
+/* $Id: fileio.cpp 24650 2009-11-13 17:39:27Z noreply@oracle.com $ */
 /** @file
  * IPRT - File I/O.
  */
@@ -149,7 +149,9 @@ int rtFileRecalcAndValidateFlags(uint32_t *pfOpen)
     switch (fOpen & RTFILE_O_ACTION_MASK)
     {
         case 0: /* temporarily */
+#ifdef DEBUG_bird
             AssertMsgFailed(("Missing RTFILE_O_OPEN/CREATE*! (continuable assertion)\n"));
+#endif
             fOpen |= RTFILE_O_OPEN;
             break;
         case RTFILE_O_OPEN:
@@ -166,7 +168,9 @@ int rtFileRecalcAndValidateFlags(uint32_t *pfOpen)
     switch (fOpen & RTFILE_O_DENY_MASK)
     {
         case 0: /* temporarily */
+#ifdef DEBUG_bird
             AssertMsgFailed(("Missing RTFILE_O_DENY_*! (continuable assertion)\n"));
+#endif
             fOpen |= RTFILE_O_DENY_NONE;
             break;
         case RTFILE_O_DENY_NONE:
