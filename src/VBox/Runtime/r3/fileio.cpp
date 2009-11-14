397f68df0e9e8885f786e1b41b3f3396f74619ba
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-11-14 20:14:49 +0000

    Please, don't do this.
    svn:sync-xref-src-repo-rev: r54829

--- src/VBox/Runtime/r3/fileio.cpp
+++ src/VBox/Runtime/r3/fileio.cpp
@@ -1,4 +1,4 @@
-/* $Id: fileio.cpp 24650 2009-11-13 17:39:27Z noreply@oracle.com $ */
+/* $Id: fileio.cpp 24653 2009-11-14 20:14:49Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - File I/O.
  */
@@ -149,9 +149,7 @@ int rtFileRecalcAndValidateFlags(uint32_t *pfOpen)
     switch (fOpen & RTFILE_O_ACTION_MASK)
     {
         case 0: /* temporarily */
-#ifdef DEBUG_bird
             AssertMsgFailed(("Missing RTFILE_O_OPEN/CREATE*! (continuable assertion)\n"));
-#endif
             fOpen |= RTFILE_O_OPEN;
             break;
         case RTFILE_O_OPEN:
@@ -168,9 +166,7 @@ int rtFileRecalcAndValidateFlags(uint32_t *pfOpen)
     switch (fOpen & RTFILE_O_DENY_MASK)
     {
         case 0: /* temporarily */
-#ifdef DEBUG_bird
             AssertMsgFailed(("Missing RTFILE_O_DENY_*! (continuable assertion)\n"));
-#endif
             fOpen |= RTFILE_O_DENY_NONE;
             break;
         case RTFILE_O_DENY_NONE:
