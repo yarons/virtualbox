50797b22125c73f3c48944dd3b423e3b20b59204
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-11-21 22:08:39 +0000

    SSM.cpp: build fix
    svn:sync-xref-src-repo-rev: r55079

--- src/VBox/VMM/SSM.cpp
+++ src/VBox/VMM/SSM.cpp
@@ -1,4 +1,4 @@
-/* $Id: SSM.cpp 24843 2009-11-21 21:57:48Z knut.osmundsen@oracle.com $ */
+/* $Id: SSM.cpp 24845 2009-11-21 22:08:39Z knut.osmundsen@oracle.com $ */
 /** @file
  * SSM - Saved State Manager.
  */
@@ -8664,7 +8664,7 @@ VMMR3DECL(uint32_t) SSMR3HandleHostBits(PSSMHANDLE pSSM)
  * Get the VirtualBox SVN revision that created the saved state.
  *
  * @returns The revision number on success.
- *          form.  If we don't know, it's an empty string.
+ *          form.  If we don't know, it's 0.
  * @param   pSSM            The saved state handle.
  *
  * @remarks This method should ONLY be used for hacks when loading OLDER saved
@@ -8676,7 +8676,11 @@ VMMR3DECL(uint32_t)     SSMR3HandleRevision(PSSMHANDLE pSSM)
 {
     if (pSSM->enmOp >= SSMSTATE_LOAD_PREP)
         return pSSM->u.Read.u32SvnRev;
+#ifdef SSM_STANDALONE
     return VMMGetSvnRev();
+#else
+    return 0;
+#endif
 }
 
 
