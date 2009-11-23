b05313e07b3a263c304fca0814f984005e4110b7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-11-23 16:07:22 +0000

    PGMSavedState: refinement.
    svn:sync-xref-src-repo-rev: r55137

--- src/VBox/VMM/PGMSavedState.cpp
+++ src/VBox/VMM/PGMSavedState.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMSavedState.cpp 24875 2009-11-23 16:04:20Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMSavedState.cpp 24876 2009-11-23 16:07:22Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, The Saved State Part.
  */
@@ -1823,7 +1823,7 @@ static DECLCALLBACK(int)  pgmR3LiveVote(PVM pVM, PSSMHANDLE pSSM, uint32_t uPass
      */
     if (    cDirtyPagesShort <= cDirtyPagesLong
         &&  (   cDirtyNow    <= cDirtyPagesShort
-             || cDirtyNow - cDirtyPagesShort < cDirtyPagesShort / 8
+             || cDirtyNow - cDirtyPagesShort < RT_MIN(cDirtyPagesShort / 8, 16)
             )
        )
     {
