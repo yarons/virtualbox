88dbc18ff91d795e055bba9927d5454c32fb37d7
Leonid Lobachev <noreply@oracle.com>
2009-11-24 11:36:06 +0000

    crOpenGL: fix black screen in compiz after snapshot load (public #4868)
    svn:sync-xref-src-repo-rev: r55165

--- src/VBox/HostServices/SharedOpenGL/crserver/crservice.cpp
+++ src/VBox/HostServices/SharedOpenGL/crserver/crservice.cpp
@@ -1,4 +1,4 @@
-/* $Id: crservice.cpp 24503 2009-11-09 13:29:40Z noreply@oracle.com $ */
+/* $Id: crservice.cpp 24891 2009-11-24 11:36:06Z noreply@oracle.com $ */
 
 /** @file
  * VBox crOpenGL: Host service entry points.
@@ -70,7 +70,7 @@ static ULONG64 g_winId = 0;
 #define CR_USE_HGCM
 
 static const char* gszVBoxOGLSSMMagic = "***OpenGL state data***";
-#define SHCROGL_SSM_VERSION 10
+#define SHCROGL_SSM_VERSION 11
 
 typedef struct
 {
