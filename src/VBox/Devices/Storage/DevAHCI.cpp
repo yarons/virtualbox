dc327453e1e13f263a8b2ec485131747f498f6e3
Alexander Eichner <alexander.eichner@oracle.com>
2009-11-25 11:46:46 +0000

    AHCI: Fix 64bit build
    svn:sync-xref-src-repo-rev: r55235

--- src/VBox/Devices/Storage/DevAHCI.cpp
+++ src/VBox/Devices/Storage/DevAHCI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevAHCI.cpp 24944 2009-11-25 11:36:28Z alexander.eichner@oracle.com $ */
+/* $Id: DevAHCI.cpp 24945 2009-11-25 11:46:46Z alexander.eichner@oracle.com $ */
 /** @file
  * VBox storage devices: AHCI controller device (disk and cdrom).
  *                       Implements the AHCI standard 1.1
@@ -439,7 +439,11 @@ typedef struct AHCIPort
      */
     R3PTRTYPE(PAHCIPORTTASKSTATE)   aCachedTasks[AHCI_NR_COMMAND_SLOTS];
 
-    uint32_t                        u32Alignment5[5];
+    uint32_t                        u32Alignment5[4];
+
+#if HC_ARCH_BITS == 32
+    uint32_t                        u32Alignment6;
+#endif
 
     /** Release statistics: number of DMA commands. */
     STAMCOUNTER                     StatDMA;
@@ -479,7 +483,7 @@ typedef struct AHCIPort
     /** The revision string for SCSI INQUIRY commands. */
     char                            szInquiryRevision[AHCI_ATAPI_INQUIRY_REVISION_LENGTH+1];
 
-    uint32_t                        Alignment6;
+    uint32_t                        Alignment7;
 
 } AHCIPort, *PAHCIPort;
 
