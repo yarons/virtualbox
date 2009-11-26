133683db3cb52a1bccdb09308dbf6ec0d4f2ce3f
Sander van Leeuwen <noreply@oracle.com>
2009-11-26 09:41:32 +0000

    Compile fix
    svn:sync-xref-src-repo-rev: r55283

--- src/VBox/Devices/Network/DevVirtioNet.cpp
+++ src/VBox/Devices/Network/DevVirtioNet.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVirtioNet.cpp 24834 2009-11-20 20:51:12Z aleksey.ilyushin@oracle.com $ */
+/* $Id: DevVirtioNet.cpp 24982 2009-11-26 09:41:32Z noreply@oracle.com $ */
 /** @file
  * DevVirtioNet - Virtio Network Device
  *
@@ -30,7 +30,7 @@
 # include <iprt/mem.h>
 #endif /* IN_RING3 */
 #include "../Builtins.h"
-#include "Virtio.h"
+#include "../VirtIO/Virtio.h"
 
 
 #ifndef VBOX_DEVICE_STRUCT_TESTCASE
