877e5f2aa9d3072950245278410d4eebc0126012
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-11-28 22:55:30 +0000

    RTFileQueryFsSizes: Moving it to a separate file so we can select platform specific implementations where it becomes necessary..
    svn:sync-xref-src-repo-rev: r55430

--- src/VBox/Runtime/r3/posix/fileio-posix.cpp
+++ src/VBox/Runtime/r3/posix/fileio-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: fileio-posix.cpp 24889 2009-11-24 11:09:45Z knut.osmundsen@oracle.com $ */
+/* $Id: fileio-posix.cpp 25076 2009-11-28 22:55:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - File I/O, POSIX.
  */
@@ -46,7 +46,6 @@
 #else
 # include <unistd.h>
 # include <sys/time.h>
-# include <sys/statvfs.h>
 #endif
 #ifdef RT_OS_LINUX
 # include <sys/file.h>
@@ -716,32 +715,6 @@ RTR3DECL(int) RTFileSetMode(RTFILE File, RTFMODE fMode)
     }
     return VINF_SUCCESS;
 }
-
-
-RTR3DECL(int) RTFileQueryFsSizes(RTFILE hFile, PRTFOFF pcbTotal, RTFOFF *pcbFree,
-                                 uint32_t *pcbBlock, uint32_t *pcbSector)
-{
-    struct statvfs StatVFS;
-    RT_ZERO(StatVFS);
-    if (fstatvfs(hFile, &StatVFS))
-        return RTErrConvertFromErrno(errno);
-
-    /*
-     * Calc the returned values.
-     */
-    if (pcbTotal)
-        *pcbTotal = (RTFOFF)StatVFS.f_blocks * StatVFS.f_frsize;
-    if (pcbFree)
-        *pcbFree = (RTFOFF)StatVFS.f_bavail * StatVFS.f_frsize;
-    if (pcbBlock)
-        *pcbBlock = StatVFS.f_frsize;
-    /* no idea how to get the sector... */
-    if (pcbSector)
-        *pcbSector = 512;
-
-    return VINF_SUCCESS;
-}
-
 
 
 RTR3DECL(int) RTFileRename(const char *pszSrc, const char *pszDst, unsigned fRename)
