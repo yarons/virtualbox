8ec81b28515916335e8e58e38f350ea2b93a58f1
Ulrich Moeller <noreply@oracle.com>
2009-12-04 15:59:34 +0000

    Main: stop USBController from being a VirtualBoxBaseWithChildren; make its instance data private
    svn:sync-xref-src-repo-rev: r55674

--- src/VBox/Main/VirtualBoxImpl.cpp
+++ src/VBox/Main/VirtualBoxImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxImpl.cpp 25184 2009-12-04 11:37:03Z noreply@oracle.com $ */
+/* $Id: VirtualBoxImpl.cpp 25194 2009-12-04 15:59:34Z noreply@oracle.com $ */
 
 /** @file
  * Implementation of IVirtualBox in VBoxSVC.
@@ -138,7 +138,7 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-// VirtualBox data definition
+// VirtualBox private member data definition
 //
 ////////////////////////////////////////////////////////////////////////////////
 
