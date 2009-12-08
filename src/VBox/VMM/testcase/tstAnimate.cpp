2f1107714cf0f887ad4e4075d9f84c89a2d75376
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-12-08 18:26:38 +0000

    tstAnimate.cpp: -Wshadow
    svn:sync-xref-src-repo-rev: r55778

--- src/VBox/VMM/testcase/tstAnimate.cpp
+++ src/VBox/VMM/testcase/tstAnimate.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstAnimate.cpp 24475 2009-11-08 19:04:06Z knut.osmundsen@oracle.com $ */
+/* $Id: tstAnimate.cpp 25253 2009-12-08 18:26:38Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Animation Testcase / Tool.
  */
@@ -679,7 +679,7 @@ int main(int argc, char **argv)
 
                 case 'o':
                 {
-                    int rc = RTStrToUInt64Ex(argv[++i], NULL, 0, &offRawMem);
+                    rc = RTStrToUInt64Ex(argv[++i], NULL, 0, &offRawMem);
                     if (RT_FAILURE(rc))
                     {
                         RTPrintf("tstAnimate: Syntax error: Invalid offset given to -o.\n");
@@ -691,7 +691,7 @@ int main(int argc, char **argv)
                 case 'm':
                 {
                     char *pszNext;
-                    int rc = RTStrToUInt64Ex(argv[++i], &pszNext, 0, &cbMem);
+                    rc = RTStrToUInt64Ex(argv[++i], &pszNext, 0, &cbMem);
                     if (RT_FAILURE(rc))
                     {
                         RTPrintf("tstAnimate: Syntax error: Invalid memory size given to -m.\n");
@@ -725,7 +725,7 @@ int main(int argc, char **argv)
 
                 case 'w':
                 {
-                    int rc = RTStrToUInt32Ex(argv[++i], NULL, 0, &u32WarpDrive);
+                    rc = RTStrToUInt32Ex(argv[++i], NULL, 0, &u32WarpDrive);
                     if (RT_FAILURE(rc))
                     {
                         RTPrintf("tstAnimate: Syntax error: Invalid number given to -w.\n");
@@ -798,7 +798,7 @@ int main(int argc, char **argv)
     {
         if (FileRawMem != NIL_RTFILE)
         {
-            int rc = RTFileGetSize(FileRawMem, &cbMem);
+            rc = RTFileGetSize(FileRawMem, &cbMem);
             AssertReleaseRC(rc);
             cbMem -= offRawMem;
             cbMem &= ~(PAGE_SIZE - 1);
