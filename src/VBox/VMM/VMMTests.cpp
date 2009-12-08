933a9f1c2fdcd12d92f7a9a276e469f07de7b436
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-12-08 12:56:11 +0000

    VMMTests.cpp: -Wshadow
    svn:sync-xref-src-repo-rev: r55753

--- src/VBox/VMM/VMMTests.cpp
+++ src/VBox/VMM/VMMTests.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMMTests.cpp 20864 2009-06-23 19:19:42Z knut.osmundsen@oracle.com $ */
+/* $Id: VMMTests.cpp 25239 2009-12-08 12:56:11Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMM - The Virtual Machine Monitor Core, Tests.
  */
@@ -452,8 +452,8 @@ VMMR3DECL(int) VMMDoTest(PVM pVM)
         if (pHyperCtx->reg)                                                             \
         {                                                                               \
             DBGFSELINFO selInfo;                                                        \
-            int rc = SELMR3GetShadowSelectorInfo(pVM, pHyperCtx->reg, &selInfo);        \
-            AssertRC(rc);                                                               \
+            int rc2 = SELMR3GetShadowSelectorInfo(pVM, pHyperCtx->reg, &selInfo);       \
+            AssertRC(rc2);                                                              \
                                                                                         \
             pHyperCtx->reg##Hid.u64Base              = selInfo.GCPtrBase;               \
             pHyperCtx->reg##Hid.u32Limit             = selInfo.cbLimit;                 \
