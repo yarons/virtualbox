dd260b36e6d49707498436730d1dd6165cb6e17a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-12-14 12:59:38 +0000

    SUPDrv-win.cpp: STATUS_INVALID_IMAGE_HASH isn't defined in our headers.
    svn:sync-xref-src-repo-rev: r55962

--- src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
+++ src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-win.cpp 25360 2009-12-14 12:52:49Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPDrv-win.cpp 25361 2009-12-14 12:59:38Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Windows NT specifics.
  */
@@ -708,7 +708,7 @@ int  VBOXCALL   supdrvOSLdrOpen(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE pImage, c
                 case /* 0xC0000263 */ STATUS_DRIVER_ENTRYPOINT_NOT_FOUND:
                     rc = VERR_LDR_IMPORTED_SYMBOL_NOT_FOUND;
                     break;
-                case /* 0xC0000428 */ STATUS_INVALID_IMAGE_HASH:
+                case    0xC0000428 /* STATUS_INVALID_IMAGE_HASH */ :
                     rc = VERR_LDR_IMAGE_HASH;
                     break;
                 default:
