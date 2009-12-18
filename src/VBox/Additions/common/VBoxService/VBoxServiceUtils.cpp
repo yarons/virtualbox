886cbc865c5e4452a2757aca262c93b6694338d6
Andreas Loeffler <andreas.loeffler@oracle.com>
2009-12-18 11:00:22 +0000

    VBoxService: Return error in VBoxServiceReadPropUInt32() when value is not within the limits.
    svn:sync-xref-src-repo-rev: r56144

--- src/VBox/Additions/common/VBoxService/VBoxServiceUtils.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceUtils.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceUtils.cpp 25390 2009-12-15 11:16:26Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxServiceUtils.cpp 25476 2009-12-18 11:00:22Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxServiceUtils - Some utility functions.
  */
@@ -137,8 +137,8 @@ int VBoxServiceReadPropUInt32(uint32_t u32ClientId, const char *pszPropName, uin
         if (   RT_SUCCESS(rc)
             && (*pu32 < u32Min || *pu32 > u32Max))
         {
-            VBoxServiceError("The guest property value %s = %RU32 is out of range [%RU32..%RU32].\n",
-                             pszPropName, *pu32, u32Min, u32Max);
+            rc = VBoxServiceError("The guest property value %s = %RU32 is out of range [%RU32..%RU32].\n",
+                                  pszPropName, *pu32, u32Min, u32Max);
         }
         RTStrFree(pszValue);
     }
