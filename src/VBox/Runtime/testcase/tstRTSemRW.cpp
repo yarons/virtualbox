35967f4cb8a22514c68ff468fdc40f36fa271689
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2009-12-20 14:46:49 +0000

    tstSemRW.cpp -> tstRTSemRW.cpp
    svn:sync-xref-src-repo-rev: r56189

--- src/VBox/Runtime/testcase/tstRTSemRW.cpp
+++ src/VBox/Runtime/testcase/tstRTSemRW.cpp
@@ -1,6 +1,6 @@
-/* $Id: tstSemRW.cpp 25514 2009-12-20 14:43:55Z knut.osmundsen@oracle.com $ */
+/* $Id: tstRTSemRW.cpp 25516 2009-12-20 14:46:49Z knut.osmundsen@oracle.com $ */
 /** @file
- * IPRT Testcase - Reader/Writer Semaphore Test.
+ * IPRT Testcase - Reader/Writer Semaphore.
  */
 
 /*
