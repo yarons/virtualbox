a99fc2293e42e27184e726d4f54848a78a7169f1
Sander van Leeuwen <noreply@oracle.com>
2009-12-21 14:02:28 +0000

    Missing pgmUnlock
    svn:sync-xref-src-repo-rev: r56213

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPool.cpp 25539 2009-12-21 13:36:47Z noreply@oracle.com $ */
+/* $Id: PGMAllPool.cpp 25540 2009-12-21 14:02:28Z noreply@oracle.com $ */
 /** @file
  * PGM Shadow Page Pool.
  */
@@ -1125,6 +1125,7 @@ DECLEXPORT(int) pgmPoolAccessHandler(PVM pVM, RTGCUINT uErrorCode, PCPUMCTXCORE 
     if (pPage->fDirty)
     {
         Assert(VMCPU_FF_ISSET(pVCpu, VMCPU_FF_TLB_FLUSH));
+        pgmUnlock(pVM);
         return VINF_SUCCESS;    /* SMP guest case where we were blocking on the pgm lock while the same page was being marked dirty. */
     }
 #endif
