37b73676cdf13f45f89f9e6d1a568a9892887418
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-01-13 17:10:01 +0000

    VBoxCredProv: Enabled building by default on OSE (second try).
    svn:sync-xref-src-repo-rev: r56584

---
+++ src/VBox/Additions/WINNT/VBoxCredProv/VBoxCredPoller.h
@@ -0,0 +1,55 @@
+/* $Id: VBoxCredPoller.h 25811 2010-01-13 17:10:01Z andreas.loeffler@oracle.com $ */
+/** @file
+ * VBoxCredPoller - Thread for retrieving user credentials.
+ */
+
+/*
+ * Copyright (C) 2009 Sun Microsystems, Inc.
+ *
+ * Sun Microsystems, Inc. confidential
+ * All rights reserved
+ */
+
+#ifndef ___VBoxCredPoller_h
+#define ___VBoxCredPoller_h
+
+#include <iprt/critsect.h>
+#include <iprt/thread.h>
+#include <iprt/semaphore.h>
+
+class VBoxCredProv;
+class VBoxCredential;
+
+class VBoxCredPoller
+{
+    public:
+
+        VBoxCredPoller();
+        virtual ~VBoxCredPoller();
+
+    public:
+
+        bool Initialize(VBoxCredProv *pProvider);
+        bool Shutdown();
+        VBoxCredProv* GetProvider() { return m_pProv; }
+        bool QueryCredentials(VBoxCredential *pCred);
+
+    protected:
+
+        void credentialsReset(void);
+        int credentialsRetrieve(void);
+        static int threadPoller(RTTHREAD ThreadSelf, void *pvUser);
+
+    protected:
+
+        RTTHREAD      m_hThreadPoller;  /* Thread handle */
+        RTCRITSECT    m_csCredUpate;
+        VBoxCredProv *m_pProv;          /* Pointer to parent */
+
+        char         *m_pszUser;        /* Our lovely credentials */
+        char         *m_pszPw;
+        char         *m_pszDomain;
+
+};
+
+#endif /* ___VBoxCredPoller_h */
