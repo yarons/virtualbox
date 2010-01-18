4a30e7a87186d2336a91868b7253cdfb3dd5a472
Frank Mehnert <noreply@oracle.com>
2010-01-18 11:37:07 +0000

    Additions: more header fixes
    svn:sync-xref-src-repo-rev: r56691

--- src/VBox/Additions/WINNT/VBoxCredProv/VBoxCredPoller.cpp
+++ src/VBox/Additions/WINNT/VBoxCredProv/VBoxCredPoller.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxCredPoller.cpp 25811 2010-01-13 17:10:01Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxCredPoller.cpp 25883 2010-01-18 11:37:07Z noreply@oracle.com $ */
 /** @file
  * VBoxCredPoller - Thread for retrieving user credentials.
  */
@@ -6,8 +6,17 @@
 /*
  * Copyright (C) 2009 Sun Microsystems, Inc.
  *
- * Sun Microsystems, Inc. confidential
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
  */
 
 
