2f6ad35c60a67c6cf161052fe2d666381e68fddd
Sander van Leeuwen <noreply@oracle.com>
2010-01-27 15:40:29 +0000

    Call VBoxServiceTimeSyncCancelAdjust when stopping the time service (!).
    svn:sync-xref-src-repo-rev: r57007

--- src/VBox/Additions/common/VBoxService/VBoxServiceTimeSync.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceTimeSync.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceTimeSync.cpp 25563 2009-12-22 12:59:20Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxServiceTimeSync.cpp 26074 2010-01-27 15:40:29Z noreply@oracle.com $ */
 /** @file
  * VBoxService - Guest Additions TimeSync Service.
  */
@@ -596,6 +596,7 @@ DECLCALLBACK(int) VBoxServiceTimeSyncWorker(bool volatile *pfShutdown)
         }
     }
 
+    VBoxServiceTimeSyncCancelAdjust();
     RTSemEventMultiDestroy(g_TimeSyncEvent);
     g_TimeSyncEvent = NIL_RTSEMEVENTMULTI;
     return rc;
