1330469c7b8896860f95ebc63883a907cf3ce84a
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-02-02 14:26:59 +0000

    VbglR3/pam_vbox: Tabs.
    svn:sync-xref-src-repo-rev: r57158

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibCredentials.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibCredentials.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR3LibCredentials.cpp 26148 2010-02-02 14:24:55Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxGuestR3LibCredentials.cpp 26149 2010-02-02 14:26:59Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxGuestR3Lib - Ring-3 Support Library for VirtualBox guest additions, user credentials.
  */
@@ -91,9 +91,9 @@ VBGLR3DECL(int) VbglR3CredentialsRetrieve(char **ppszUser, char **ppszPassword, 
  * Clears and frees the strings
  *
  * @returns IPRT status value
- * @param   pszUser        Receives pointer of the user name string to destroy.  
+ * @param   pszUser        Receives pointer of the user name string to destroy.
  *                         Optional.
- * @param   pszPassword    Receives pointer of the password string to destroy.  
+ * @param   pszPassword    Receives pointer of the password string to destroy.
  *                         Optional.
  * @param   pszDomain      Receives pointer of allocated domain name string.
  *                         Optional.
@@ -101,29 +101,29 @@ VBGLR3DECL(int) VbglR3CredentialsRetrieve(char **ppszUser, char **ppszPassword, 
  */
 VBGLR3DECL(void) VbglR3CredentialsDestroy(char *pszUser, char *pszPassword, char *pszDomain, uint8_t u8NumPasses)
 {
-	size_t l;
-	
-	if (u8NumPasses == 0) /* We at least want to have one wipe pass. */
-		u8NumPasses = 1;
-	
-	/** @todo add some for-loop with randomized content instead of 
-	 *        zero'ing out the string only one time. Use u8NumPasses for that. */
-	if (pszUser)
-	{
-		l = strlen(pszUser);
-		RT_BZERO(pszUser, l);
-		RTStrFree(pszUser);
-	}
-	if (pszPassword)
-	{
-		l = strlen(pszPassword);
-		RT_BZERO(pszPassword, l);
-		RTStrFree(pszPassword);
-	}
-	if (pszUser)
-	{
-		l = strlen(pszDomain);
-		RT_BZERO(pszDomain, l);
-		RTStrFree(pszDomain);
-	}
+    size_t l;
+
+    if (u8NumPasses == 0) /* We at least want to have one wipe pass. */
+        u8NumPasses = 1;
+
+    /** @todo add some for-loop with randomized content instead of
+     *        zero'ing out the string only one time. Use u8NumPasses for that. */
+    if (pszUser)
+    {
+        l = strlen(pszUser);
+        RT_BZERO(pszUser, l);
+        RTStrFree(pszUser);
+    }
+    if (pszPassword)
+    {
+        l = strlen(pszPassword);
+        RT_BZERO(pszPassword, l);
+        RTStrFree(pszPassword);
+    }
+    if (pszUser)
+    {
+        l = strlen(pszDomain);
+        RT_BZERO(pszDomain, l);
+        RTStrFree(pszDomain);
+    }
 }
