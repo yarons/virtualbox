5d3141338cde24a80b92e0aafbaac6e8c428690a
Frank Mehnert <noreply@oracle.com>
2010-02-09 09:36:16 +0000

    gcc-4.5 warning
    svn:sync-xref-src-repo-rev: r57439

--- src/VBox/Runtime/common/string/strformatrt.cpp
+++ src/VBox/Runtime/common/string/strformatrt.cpp
@@ -1,4 +1,4 @@
-/* $Id: strformatrt.cpp 26344 2010-02-09 03:39:45Z knut.osmundsen@oracle.com $ */
+/* $Id: strformatrt.cpp 26351 2010-02-09 09:36:16Z noreply@oracle.com $ */
 /** @file
  * IPRT - IPRT String Formatter Extensions.
  */
@@ -357,7 +357,7 @@ size_t rtstrFormatRt(PFNRTSTROUTPUT pfnOutput, void *pvArgOutput, const char **p
                  * Fetch the argument.
                  * It's important that a signed value gets sign-extended up to 64-bit.
                  */
-                u.u64 = 0;
+                RT_ZERO(u);
                 if (fFlags & RTSTR_F_VALSIGNED)
                 {
                     switch (s_aTypes[i].cb)
