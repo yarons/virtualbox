db6ac98a11136ffcbb0380a83ae028f78d3f3d28
Frank Mehnert <noreply@oracle.com>
2010-02-11 17:02:44 +0000

    more EFI OSE fixes
    svn:sync-xref-src-repo-rev: r57589

---
+++ src/VBox/Devices/EFI/Firmware2/VBoxPkg/Include/VBoxMemLayout.h
@@ -0,0 +1,20 @@
+/* $Id: VBoxMemLayout.h 26446 2010-02-11 17:02:44Z noreply@oracle.com $ */
+/** @file
+ * VBoxMemLayou.h - Constants defining the memory layout. (Merge with DevEFI?)
+ */
+
+/*
+ * Copyright (C) 2009 Sun Microsystems, Inc.
+ *
+ * Sun Microsystems, Inc. confidential
+ * All rights reserved
+ */
+
+#ifndef ___VBoxMemLayout_h
+#define ___VBoxMemLayout_h
+
+#define VBOXSEC_STACK_BASE  (1*1024*1024)
+#define VBOXSEC_STACK_SIZE  (128*1024)
+
+#endif
+
