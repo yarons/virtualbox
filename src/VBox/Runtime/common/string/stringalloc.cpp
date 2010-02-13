9d753bc431a26c058487d76298537533c60f3f1f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-02-13 23:08:49 +0000

    IPRT: common/string/string.cpp -> stringalloc.cpp.
    svn:sync-xref-src-repo-rev: r57647

--- src/VBox/Runtime/common/string/stringalloc.cpp
+++ src/VBox/Runtime/common/string/stringalloc.cpp
@@ -1,4 +1,4 @@
-/* $Id: string.cpp 26344 2010-02-09 03:39:45Z knut.osmundsen@oracle.com $ */
+/* $Id: stringalloc.cpp 26480 2010-02-13 23:08:49Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - String Manipulation.
  */
