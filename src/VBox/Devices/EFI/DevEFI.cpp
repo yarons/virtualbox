fec4ab555eedad2a9435c2c76f4b3e2d16310136
Nikolay Igotty <noreply@oracle.com>
2010-02-16 17:50:38 +0000

    EFI: better CPU frequency esteemation
    svn:sync-xref-src-repo-rev: r57783

--- src/VBox/Devices/EFI/DevEFI.cpp
+++ src/VBox/Devices/EFI/DevEFI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevEFI.cpp 26546 2010-02-15 14:30:08Z noreply@oracle.com $ */
+/* $Id: DevEFI.cpp 26591 2010-02-16 17:50:38Z noreply@oracle.com $ */
 /** @file
  * DevEFI - EFI <-> VirtualBox Integration Framework.
  */
@@ -39,6 +39,7 @@
 #include <iprt/uuid.h>
 #include <iprt/path.h>
 #include <iprt/string.h>
+#include <iprt/mp.h>
 #ifdef DEBUG
 # include <iprt/stream.h>
 # define DEVEFI_WITH_VBOXDBG_SCRIPT
@@ -1140,7 +1141,9 @@ static DECLCALLBACK(int)  efiConstruct(PPDMDEVINS pDevIns, int iInstance, PCFGMN
      * CPU frequencies
      */
     // @todo: we need to have VMM API to access TSC increase speed, for now provide reasonable default
-    pThis->u64TscFrequency = 2500000000; // TMCpuTicksPerSecond(PDMDevHlpGetVM(pDevIns));
+    pThis->u64TscFrequency = RTMpGetMaxFrequency(0) * 1024 * 1024;// TMCpuTicksPerSecond(PDMDevHlpGetVM(pDevIns));
+    if (pThis->u64TscFrequency == 0)
+        pThis->u64TscFrequency = 2500000000;
     /* Multiplier is read from MSR_IA32_PERF_STATUS, and now is hardcoded as 4 */
     pThis->u64FsbFrequency = pThis->u64TscFrequency / 4;
     pThis->u64CpuFrequency = pThis->u64TscFrequency;
