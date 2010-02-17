42505ebc6236c8d18ff6479102af33e0f5a17da1
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-02-17 14:29:14 +0000

    BoxServiceCpuHotPlug.cpp: todo update.
    svn:sync-xref-src-repo-rev: r57806

--- src/VBox/Additions/common/VBoxService/VBoxServiceCpuHotPlug.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceCpuHotPlug.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceCpuHotPlug.cpp 26599 2010-02-17 10:33:37Z alexander.eichner@oracle.com $ */
+/* $Id: VBoxServiceCpuHotPlug.cpp 26611 2010-02-17 14:29:14Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxService - Guest Additions CPU Hot Plugging Service.
  */
@@ -173,8 +173,12 @@ static void VBoxServiceCpuHotPlugHandlePlugEvent(uint32_t idCpuCore, uint32_t id
             while (RT_SUCCESS(RTDirRead(pDirDevices, &DirFolderContent, NULL))) /* Assumption that szName has always enough space */
             {
                 /** @todo r-bird: This code is bringing all CPUs online; the idCpuCore and
-                 *        idCpuPackage parameters are unused!   
-                 *        aeichner: These files are not available at this point unfortunately. (see comment above) 
+                 *        idCpuPackage parameters are unused!
+                 *        aeichner: These files are not available at this point unfortunately. (see comment above)
+                 *        bird: Yes, but isn't that easily dealt with by doing:
+                 *              if (matching_topology() || !have_topology_directory())
+                 *                  bring_cpu_online()
+                 *              That could save you the cpu0 and cpuidle checks to.
                  */
                 /*
                  * Check if this is a CPU object.
