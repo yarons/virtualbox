9bdfaf9bb6643159f21a890c15eb527cb7edb0f5
Nikolay Igotty <noreply@oracle.com>
2010-02-19 13:16:11 +0000

    CPUM: better cache sharing descriptor - L2 cache is shared amongst all cores
    svn:sync-xref-src-repo-rev: r57859

--- src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
+++ src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUMAllRegs.cpp 26648 2010-02-19 13:10:03Z noreply@oracle.com $ */
+/* $Id: CPUMAllRegs.cpp 26649 2010-02-19 13:16:11Z noreply@oracle.com $ */
 /** @file
  * CPUM - CPU Monitor(/Manager) - Getters and Setters.
  */
@@ -1121,7 +1121,7 @@ VMMDECL(void) CPUMGetGuestCpuId(PVMCPU pVCpu, uint32_t iLeaf, uint32_t *pEax, ui
             case 2:
                 level = 2;
                 type = 3;
-                sharing = 1;
+                sharing = cores; /* our L2 cache is modelled as shared between all cores */ 
                 linesize = 64;
                 associativity = 24;
                 sets = 4096;
