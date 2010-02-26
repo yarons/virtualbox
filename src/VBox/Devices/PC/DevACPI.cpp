349fecebe17417a6050cd2642f719bdbee06900a
Sander van Leeuwen <noreply@oracle.com>
2010-02-26 15:02:06 +0000

    Two more dword write cases
    svn:sync-xref-src-repo-rev: r58139

--- src/VBox/Devices/PC/DevACPI.cpp
+++ src/VBox/Devices/PC/DevACPI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevACPI.cpp 26857 2010-02-26 14:37:00Z noreply@oracle.com $ */
+/* $Id: DevACPI.cpp 26861 2010-02-26 15:02:06Z noreply@oracle.com $ */
 /** @file
  * DevACPI - Advanced Configuration and Power Interface (ACPI) Device.
  */
@@ -1625,6 +1625,9 @@ PDMBOTHCBDECL(int) acpiPM1aEnWrite(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Po
         case 2:
             acpiPM1aEnWritew((ACPIState*)pvUser, Port, u32);
             break;
+        case 4:
+            acpiPM1aEnWritew((ACPIState*)pvUser, Port, u32 & 0xffff);
+            break;
         default:
             AssertMsgFailed(("Port=%#x cb=%d u32=%#x\n", Port, cb, u32));
             break;
@@ -1655,6 +1658,8 @@ PDMBOTHCBDECL(int) acpiPM1aCtlWrite(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT P
     {
         case 2:
             return acpiPM1aCtlWritew((ACPIState*)pvUser, Port, u32);
+        case 4:
+            return acpiPM1aCtlWritew((ACPIState*)pvUser, Port, u32 & 0xffff);
         default:
             AssertMsgFailed(("Port=%#x cb=%d u32=%#x\n", Port, cb, u32));
             break;
