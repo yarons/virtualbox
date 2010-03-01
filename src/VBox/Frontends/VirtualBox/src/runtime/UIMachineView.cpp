f25e4a17d30cf366691157baf968a6bc75fee8b4
Christian Poetzsch <noreply@oracle.com>
2010-03-01 14:47:38 +0000

    FE/Qt4: new core: small fix for OSX and todo added
    svn:sync-xref-src-repo-rev: r58198

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineView.cpp 26897 2010-02-28 18:13:19Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineView.cpp 26914 2010-03-01 14:47:38Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -645,18 +645,22 @@ void UIMachineView::cleanupCommon()
         DestroyIcon(m_alphaCursor);
 #endif
 
-#if defined (Q_WS_MAC)
-# if !defined (QT_MAC_USE_COCOA)
+#ifdef Q_WS_MAC
+    /* We have to make sure the callback for the keyboard events is released
+     * when closing this view. */
+    if (m_fKeyboardGrabbed)
+        darwinGrabKeyboardEvents (false);
+# ifndef QT_MAC_USE_COCOA
     if (mDarwinWindowOverlayHandlerRef)
     {
         ::RemoveEventHandler(mDarwinWindowOverlayHandlerRef);
         mDarwinWindowOverlayHandlerRef = NULL;
     }
-# endif
+# endif /* !QT_MAC_USE_COCOA */
     // TODO_NEW_CORE
 //    delete mDockIconPreview;
     mDockIconPreview = NULL;
-#endif
+#endif /* Q_WS_MAC */
 }
 
 void UIMachineView::cleanupFrameBuffer()
@@ -668,6 +672,7 @@ void UIMachineView::cleanupFrameBuffer()
         display.SetFramebuffer(VBOX_VIDEO_PRIMARY_SCREEN, CFramebuffer(NULL));
         /* Release the reference: */
         m_pFrameBuffer->Release();
+//        delete m_pFrameBuffer; // TODO_NEW_CORE: possibly necessary to really cleanup
         m_pFrameBuffer = NULL;
     }
 }
