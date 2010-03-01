3c40c82455a3ab0d88faf1c211ea024ddaab0cf2
Sander van Leeuwen <noreply@oracle.com>
2010-03-01 12:49:26 +0000

    Only valid on 64 bits hosts
    svn:sync-xref-src-repo-rev: r58191

--- src/VBox/VMM/VMMAll/PGMAllPhys.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPhys.cpp 26718 2010-02-23 16:12:09Z noreply@oracle.com $ */
+/* $Id: PGMAllPhys.cpp 26907 2010-03-01 12:49:26Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -367,6 +367,7 @@ int pgmPhysAllocPage(PVM pVM, PPGMPAGE pPage, RTGCPHYS GCPhys)
     AssertMsg(PGM_PAGE_IS_ZERO(pPage) || PGM_PAGE_IS_SHARED(pPage), ("%R[pgmpage] %RGp\n", pPage, GCPhys));
     Assert(!PGM_PAGE_IS_MMIO(pPage));
 
+# if HC_ARCH_BITS == 64
     if (    PGMIsUsingLargePages(pVM)
         &&  PGM_PAGE_GET_TYPE(pPage) == PGMPAGETYPE_RAM)
     {
@@ -376,6 +377,7 @@ int pgmPhysAllocPage(PVM pVM, PPGMPAGE pPage, RTGCPHYS GCPhys)
 
         /* fall back to 4kb pages. */
     }
+# endif
 
     /*
      * Flush any shadow page table mappings of the page.
