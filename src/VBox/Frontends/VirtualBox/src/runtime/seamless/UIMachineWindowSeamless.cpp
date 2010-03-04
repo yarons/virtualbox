e409cf9438d56d718c23285cde43bb158aaa9857
Sergey Dubov <sergey.dubov@oracle.com>
2010-03-04 11:55:54 +0000

    FE/Qt4: New running VM core: win build fix for r58321.
    svn:sync-xref-src-repo-rev: r58322

--- src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineWindowSeamless.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineWindowSeamless.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineWindowSeamless.cpp 27012 2010-03-04 11:47:23Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineWindowSeamless.cpp 27013 2010-03-04 11:55:54Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -244,7 +244,7 @@ void UIMachineWindowSeamless::setMask(const QRegion &constRegion)
     /* Set the current visible region and clean the previous */
     SetWindowRgn(winId(), newReg, FALSE);
     RedrawWindow(0, 0, diffReg, RDW_INVALIDATE | RDW_UPDATENOW | RDW_ALLCHILDREN);
-    RedrawWindow(mConsole->viewport()->winId(), 0, 0, RDW_INVALIDATE);
+    RedrawWindow(machineView()->viewport()->winId(), 0, 0, RDW_INVALIDATE);
 
     m_prevRegion = region;
 #elif defined (Q_WS_MAC)
