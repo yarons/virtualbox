cb56ef4bd054aac3d462cfe3b1df9d0df3ed3289
Sander van Leeuwen <noreply@oracle.com>
2010-03-05 14:54:32 +0000

    Large page enabling for myself on AMD
    svn:sync-xref-src-repo-rev: r58427

--- src/VBox/VMM/HWACCM.cpp
+++ src/VBox/VMM/HWACCM.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWACCM.cpp 26685 2010-02-22 17:48:23Z noreply@oracle.com $ */
+/* $Id: HWACCM.cpp 27099 2010-03-05 14:54:32Z noreply@oracle.com $ */
 /** @file
  * HWACCM - Intel/AMD VM Hardware Support Manager
  */
@@ -1279,7 +1279,13 @@ VMMR3DECL(int) HWACCMR3InitFinalizeR0(PVM pVM)
                 pVM->hwaccm.s.svm.fEnabled = true;
 
                 if (pVM->hwaccm.s.fNestedPaging)
+                {
+#ifdef DEBUG_sandervl
+                    /* Use large (2 MB) pages for our EPT PDEs where possible. */
+                    PGMSetLargePageUsage(pVM, true);
+#endif
                     LogRel(("HWACCM:    Enabled nested paging\n"));
+                }
 
                 hwaccmR3DisableRawMode(pVM);
                 CPUMSetGuestCpuIdFeature(pVM, CPUMCPUIDFEATURE_SEP);
