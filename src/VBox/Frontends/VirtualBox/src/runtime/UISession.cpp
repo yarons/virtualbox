6303f696409bd26e515669cb86ef785702718811
Sergey Dubov <sergey.dubov@oracle.com>
2010-03-09 17:34:39 +0000

    FE/Qt4: New running VM core: mouse functionality cumulative patch.
    svn:sync-xref-src-repo-rev: r58609

--- src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
@@ -1,4 +1,4 @@
-/* $Id: UISession.cpp 27218 2010-03-09 14:55:14Z noreply@oracle.com $ */
+/* $Id: UISession.cpp 27227 2010-03-09 17:34:39Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -554,7 +554,7 @@ UISession::UISession(UIMachine *pMachine, CSession &sessionReference)
     , m_fIsMouseCaptured(false)
     , m_fIsMouseIntegrated(true)
     , m_fIsValidPointerShapePresent(false)
-    , m_fIsHideHostPointer(true)
+    , m_fIsHidingHostPointer(true)
 {
     /* Register console callback: */
     session().GetConsole().RegisterCallback(m_callback);
@@ -666,7 +666,7 @@ bool UISession::event(QEvent *pEvent)
             if (pConsoleEvent->shapeData())
             {
                 /* We are ignoring visibility flag: */
-                m_fIsHideHostPointer = false;
+                m_fIsHidingHostPointer = false;
 
                 /* And updating current cursor shape: */
                 setPointerShape(pConsoleEvent->shapeData(), pConsoleEvent->hasAlpha(),
@@ -677,7 +677,7 @@ bool UISession::event(QEvent *pEvent)
             else
             {
                 /* Remember if we should hide the cursor: */
-                m_fIsHideHostPointer = !pConsoleEvent->isVisible();
+                m_fIsHidingHostPointer = !pConsoleEvent->isVisible();
             }
 
             /* Notify listeners about mouse capability changed: */
