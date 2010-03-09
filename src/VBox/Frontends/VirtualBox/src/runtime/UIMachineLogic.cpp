a7492d8e4bfd0fac40578fd338be5b30a3669c2d
Christian Poetzsch <noreply@oracle.com>
2010-03-09 17:01:41 +0000

    FE/Qt4: new core: make a boundary checking
    svn:sync-xref-src-repo-rev: r58606

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachineLogic.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachineLogic.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineLogic.cpp 27218 2010-03-09 14:55:14Z noreply@oracle.com $ */
+/* $Id: UIMachineLogic.cpp 27224 2010-03-09 17:01:41Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -658,7 +658,7 @@ void UIMachineLogic::prepareDock()
     if (cGuestScreens > 1)
     {
         pDockSettingsMenu->addSeparator();
-        m_DockIconPreviewMonitor = session().GetMachine().GetExtraData(VBoxDefs::GUI_RealtimeDockIconUpdateMonitor).toInt();
+        m_DockIconPreviewMonitor = qMin(session().GetMachine().GetExtraData(VBoxDefs::GUI_RealtimeDockIconUpdateMonitor).toInt(), cGuestScreens - 1);
         m_pDockPreviewSelectMonitorGroup = new QActionGroup(this);
         for (int i = 0; i < cGuestScreens; ++i)
         {
@@ -1571,7 +1571,8 @@ void UIMachineLogic::sltChangeDockIconUpdate(const VBoxChangeDockIconUpdateEvent
     {
         setDockIconPreviewEnabled(event.mChanged);
         m_pDockPreviewSelectMonitorGroup->setEnabled(event.mChanged);
-        m_DockIconPreviewMonitor = session().GetMachine().GetExtraData(VBoxDefs::GUI_RealtimeDockIconUpdateMonitor).toInt();
+        CMachine machine = session().GetMachine();
+        m_DockIconPreviewMonitor = qMin(machine.GetExtraData(VBoxDefs::GUI_RealtimeDockIconUpdateMonitor).toInt(), (int)machine.GetMonitorCount() - 1);
         updateDockOverlay();
     }
 }
