799aa76e027766369bd0d624014ad630d917913c
Alexander Eichner <alexander.eichner@oracle.com>
2010-03-18 12:17:32 +0000

    VMDK: No need to update the line if cb is 0
    svn:sync-xref-src-repo-rev: r58974

--- src/VBox/Devices/Storage/VmdkHDDCore.cpp
+++ src/VBox/Devices/Storage/VmdkHDDCore.cpp
@@ -1,4 +1,4 @@
-/* $Id: VmdkHDDCore.cpp 27476 2010-03-18 11:13:17Z alexander.eichner@oracle.com $ */
+/* $Id: VmdkHDDCore.cpp 27477 2010-03-18 12:17:32Z alexander.eichner@oracle.com $ */
 /** @file
  * VMDK Disk image, Core Code.
  */
@@ -2467,10 +2467,15 @@ static int vmdkWriteDescriptor(PVMDKIMAGE pImage)
 
         if (cbLimit && uOffset + cb + 1 > cbLimit)
             return vmdkError(pImage, VERR_BUFFER_OVERFLOW, RT_SRC_POS, N_("VMDK: descriptor too long in '%s'"), pImage->pszFilename);
-        rc = vmdkFileWriteAt(pDescFile, uOffset, psz, cb, NULL);
-        if (RT_FAILURE(rc))
-            return vmdkError(pImage, rc, RT_SRC_POS, N_("VMDK: error writing descriptor in '%s'"), pImage->pszFilename);
-        uOffset += cb;
+
+        if (cb > 0)
+        {
+            rc = vmdkFileWriteAt(pDescFile, uOffset, psz, cb, NULL);
+            if (RT_FAILURE(rc))
+                return vmdkError(pImage, rc, RT_SRC_POS, N_("VMDK: error writing descriptor in '%s'"), pImage->pszFilename);
+            uOffset += cb;
+        }
+
         rc = vmdkFileWriteAt(pDescFile, uOffset, "\n", 1, NULL);
         if (RT_FAILURE(rc))
             return vmdkError(pImage, rc, RT_SRC_POS, N_("VMDK: error writing descriptor in '%s'"), pImage->pszFilename);
