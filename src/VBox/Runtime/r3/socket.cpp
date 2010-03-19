f04eadca3d6f115a50eea2a2ca60413d139c486a
Nikolay Igotty <noreply@oracle.com>
2010-03-19 15:31:53 +0000

    RT: temporary commented out owner assert in RT socket's shutdown() - we do that concurrently sometimes
    svn:sync-xref-src-repo-rev: r59054

--- src/VBox/Runtime/r3/socket.cpp
+++ src/VBox/Runtime/r3/socket.cpp
@@ -1,4 +1,4 @@
-/* $Id: socket.cpp 27510 2010-03-19 01:37:08Z knut.osmundsen@oracle.com $ */
+/* $Id: socket.cpp 27541 2010-03-19 15:31:53Z noreply@oracle.com $ */
 /** @file
  * IPRT - Network Sockets.
  */
@@ -574,7 +574,7 @@ RTDECL(int) RTSocketShutdown(RTSOCKET hSocket, bool fRead, bool fWrite)
     AssertPtrReturn(pThis, VERR_INVALID_HANDLE);
     AssertReturn(pThis->u32Magic == RTSOCKET_MAGIC, VERR_INVALID_HANDLE);
     AssertReturn(fRead || fWrite, VERR_INVALID_PARAMETER);
-    AssertReturn(rtSocketTryLock(pThis), VERR_CONCURRENT_ACCESS);
+    //AssertReturn(rtSocketTryLock(pThis), VERR_CONCURRENT_ACCESS);
 
     /*
      * Do the job.
