16aa69d6e40d926ce3d380e0a65a01e6564a28b2
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-03-23 01:29:07 +0000

    iprt: Implemented RTPipeFromNative on posix.
    svn:sync-xref-src-repo-rev: r59163

--- src/VBox/Runtime/r3/win/pipe-win.cpp
+++ src/VBox/Runtime/r3/win/pipe-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: pipe-win.cpp 27613 2010-03-23 01:12:54Z knut.osmundsen@oracle.com $ */
+/* $Id: pipe-win.cpp 27614 2010-03-23 01:29:07Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Anonymous Pipes, Windows Implementation.
  */
@@ -436,6 +436,7 @@ RTDECL(int)  RTPipeClose(RTPIPE hPipe)
 
 RTDECL(int)  RTPipeFromNative(PRTPIPE phPipe, RTHCINTPTR hNativePipe, uint32_t fFlags)
 {
+    AssertPtrReturn(phPipe, VERR_INVALID_POINTER);
     AssertReturn(!(fFlags & ~RTPIPE_N_VALID_MASK), VERR_INVALID_PARAMETER);
     AssertReturn(!!(fFlags & RTPIPE_N_READ) != !!(fFlags & RTPIPE_N_WRITE), VERR_INVALID_PARAMETER);
 
