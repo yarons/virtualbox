47069401629296483781010ef31b5e5062af625e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-03-26 14:53:31 +0000

    build fix
    svn:sync-xref-src-repo-rev: r59377

--- src/VBox/Runtime/r3/win/process-win.cpp
+++ src/VBox/Runtime/r3/win/process-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: process-win.cpp 27743 2010-03-26 14:50:55Z knut.osmundsen@oracle.com $ */
+/* $Id: process-win.cpp 27745 2010-03-26 14:53:31Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Process, Windows.
  */
@@ -489,7 +489,7 @@ RTR3DECL(int)   RTProcCreateEx(const char *pszExec, const char * const *papszArg
                         if (RT_SUCCESS(rc))
                         {
                             rc = rtProcCreateAsUserHlp(pwszUser, pwszPassword,
-                                                       pwszExec, pwszCmdLine, pwszzBlock, dwCreationFlags
+                                                       pwszExec, pwszCmdLine, pwszzBlock, dwCreationFlags,
                                                        &StartupInfo, &ProcInfo);
 
                             RTUtf16Free(pwszPassword);
