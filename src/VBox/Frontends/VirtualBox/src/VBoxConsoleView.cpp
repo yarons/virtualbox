d142dcfb8700a498732251a70cc8ed415b9db734
Mikhail Sennikovsky <noreply@oracle.com>
2010-03-26 17:59:29 +0000

    2d: dont destroy framebuffer on mode change
    svn:sync-xref-src-repo-rev: r59391

--- src/VBox/Frontends/VirtualBox/src/VBoxConsoleView.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxConsoleView.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxConsoleView.cpp 27689 2010-03-25 09:55:48Z vitali.pelenjow@oracle.com $ */
+/* $Id: VBoxConsoleView.cpp 27757 2010-03-26 17:59:29Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -819,7 +819,7 @@ VBoxConsoleView::VBoxConsoleView (VBoxConsoleWnd *mainWnd,
 #ifdef VBOX_WITH_VIDEOHWACCEL
                     /* these two additional template args is a workaround to this [VBox|UI] duplication
                      * @todo: they are to be removed once VBox stuff is gone */
-                    mAccelerate2DVideo ? new VBoxOverlayFrameBuffer<VBoxQImageFrameBuffer, VBoxConsoleView, VBoxResizeEvent> (this, viewport(), &mainWnd->session()) :
+                    mAccelerate2DVideo ? new VBoxOverlayFrameBuffer<VBoxQImageFrameBuffer, VBoxConsoleView, VBoxResizeEvent> (this, &mainWnd->session()) :
 #endif
                     new VBoxQImageFrameBuffer (this);
             break;
@@ -838,7 +838,7 @@ VBoxConsoleView::VBoxConsoleView (VBoxConsoleWnd *mainWnd,
 #if defined(VBOX_WITH_VIDEOHWACCEL) && defined(DEBUG_misha) /* not tested yet */
                     /* these two additional template args is a workaround to this [VBox|UI] duplication
                      * @todo: they are to be removed once VBox stuff is gone */
-                    mAccelerate2DVideo ? new VBoxOverlayFrameBuffer<VBoxSDLFrameBuffer, VBoxConsoleView, VBoxResizeEvent> (this, viewport(), &mainWnd->session()) :
+                    mAccelerate2DVideo ? new VBoxOverlayFrameBuffer<VBoxSDLFrameBuffer, VBoxConsoleView, VBoxResizeEvent> (this, &mainWnd->session()) :
 #endif
                     new VBoxSDLFrameBuffer (this);
             /*
@@ -863,7 +863,7 @@ VBoxConsoleView::VBoxConsoleView (VBoxConsoleWnd *mainWnd,
 #ifdef VBOX_WITH_VIDEOHWACCEL
                     /* these two additional template args is a workaround to this [VBox|UI] duplication
                      * @todo: they are to be removed once VBox stuff is gone */
-                    mAccelerate2DVideo ? new VBoxOverlayFrameBuffer<VBoxQuartz2DFrameBuffer, VBoxConsoleView, VBoxResizeEvent> (this, viewport(), &mainWnd->session()) :
+                    mAccelerate2DVideo ? new VBoxOverlayFrameBuffer<VBoxQuartz2DFrameBuffer, VBoxConsoleView, VBoxResizeEvent> (this, &mainWnd->session()) :
 #endif
                     new VBoxQuartz2DFrameBuffer (this);
             break;
