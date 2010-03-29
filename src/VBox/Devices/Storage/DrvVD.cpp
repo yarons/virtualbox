990bec4760831cd17f68d5656f373ccefe0b7769
Alexander Eichner <alexander.eichner@oracle.com>
2010-03-29 20:55:16 +0000

    Compile fix
    svn:sync-xref-src-repo-rev: r59469

--- src/VBox/Devices/Storage/DrvVD.cpp
+++ src/VBox/Devices/Storage/DrvVD.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvVD.cpp 27808 2010-03-29 20:52:56Z alexander.eichner@oracle.com $ */
+/* $Id: DrvVD.cpp 27809 2010-03-29 20:55:16Z alexander.eichner@oracle.com $ */
 /** @file
  * DrvVD - Generic VBox disk media driver.
  */
@@ -1477,7 +1477,7 @@ static DECLCALLBACK(int) drvvdConstruct(PPDMDRVINS pDrvIns,
         }
     }
 
-#if 0 /* Temporary disabled. WIP
+#if 0 /* Temporary disabled. WIP */
     if (pThis->pDrvMediaAsyncPort)
         pThis->fAsyncIOSupported = true;
 #else
