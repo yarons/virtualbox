4c6015a0a8b7fba07e906c37300a074c71ff72b1
Sander van Leeuwen <noreply@oracle.com>
2010-04-07 13:14:34 +0000

    Typo
    svn:sync-xref-src-repo-rev: r59757

--- src/VBox/Additions/common/VBoxService/VBoxServiceBalloon.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceBalloon.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceBalloon.cpp 27972 2010-04-02 22:59:54Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxServiceBalloon.cpp 28048 2010-04-07 13:14:34Z noreply@oracle.com $ */
 /** @file
  * VBoxService - Memory Ballooning.
  */
@@ -61,7 +61,7 @@ static bool g_fSysMadviseWorks;
 
 
 /**
- * Check weather madvise() works.
+ * Check whether madvise() works.
  */
 static void VBoxServiceBalloonInitMadvise(void)
 {
