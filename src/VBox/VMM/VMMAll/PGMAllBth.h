e0a84f323efbdc0d21aee8cc49cdcdfb36ada5a0
Sander van Leeuwen <noreply@oracle.com>
2010-04-08 14:03:02 +0000

    Compile fix
    svn:sync-xref-src-repo-rev: r59808

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllBth.h 28090 2010-04-08 13:29:25Z noreply@oracle.com $ */
+/* $Id: PGMAllBth.h 28094 2010-04-08 14:03:02Z noreply@oracle.com $ */
 /** @file
  * VBox - Page Manager, Shadow+Guest Paging Template - All context code.
  *
@@ -168,7 +168,7 @@ PGM_BTH_DECL(int, Trap0eHandler)(PVMCPU pVCpu, RTGCUINT uErr, PCPUMCTXCORE pRegF
         /* Fake PDPT entry; access control handled on the page table level, so allow everything. */
         PdpeSrc.u  = X86_PDPE_P;   /* rw/us are reserved for PAE pdpte's; accessed bit causes invalid VT-x guest state errors */
 #   endif
-        int rc = pgmShwSyncPaePDPtr(pVCpu, pvFault, &PdpeSrc, &pPDDst);
+        rc = pgmShwSyncPaePDPtr(pVCpu, pvFault, &PdpeSrc, &pPDDst);
         if (rc != VINF_SUCCESS)
         {
             pgmUnlock(pVM);
