b6715e85eeba3c171c29ff01cccdf91536696fc5
Christian Poetzsch <noreply@oracle.com>
2010-04-13 17:16:54 +0000

    FE/Qt4: new core: use the exact bpp of every screen for the calculation
    svn:sync-xref-src-repo-rev: r60010

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMultiScreenLayout.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMultiScreenLayout.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMultiScreenLayout.cpp 28215 2010-04-12 15:31:54Z vitali.pelenjow@oracle.com $ */
+/* $Id: UIMultiScreenLayout.cpp 28268 2010-04-13 17:16:54Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -205,8 +205,8 @@ quint64 UIMultiScreenLayout::memoryRequirements(const QMap<int, int> *pScreenLay
     ULONG width = 0;
     ULONG height = 0;
     ULONG guestBpp = 0;
-    m_pMachineLogic->uisession()->session().GetConsole().GetDisplay().GetScreenResolution(0, width, height, guestBpp);
     quint64 usedBits = 0;
+    CDisplay display = m_pMachineLogic->uisession()->session().GetConsole().GetDisplay();
     for (int i = 0; i < m_cGuestScreens; ++ i)
     {
         QRect screen;
@@ -214,6 +214,7 @@ quint64 UIMultiScreenLayout::memoryRequirements(const QMap<int, int> *pScreenLay
             screen = QApplication::desktop()->availableGeometry(pScreenLayout->value(i, 0));
         else
             screen = QApplication::desktop()->screenGeometry(pScreenLayout->value(i, 0));
+        display.GetScreenResolution(i, width, height, guestBpp);
         usedBits += screen.width() * /* display width */
                     screen.height() * /* display height */
                     guestBpp + /* guest bits per pixel */
