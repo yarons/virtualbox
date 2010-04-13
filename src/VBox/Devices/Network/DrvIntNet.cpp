6571721ed2b6dbb25c109ad5790bee39913f7b6c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-04-13 14:52:53 +0000

    oops
    svn:sync-xref-src-repo-rev: r60001

--- src/VBox/Devices/Network/DrvIntNet.cpp
+++ src/VBox/Devices/Network/DrvIntNet.cpp
@@ -1,5 +1,4 @@
-#define VBOX_WITH_R0_AND_RC_DRIVERS
-/* $Id: DrvIntNet.cpp 28258 2010-04-13 14:51:16Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvIntNet.cpp 28259 2010-04-13 14:52:53Z knut.osmundsen@oracle.com $ */
 /** @file
  * DrvIntNet - Internal network transport driver.
  */
