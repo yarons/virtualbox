cb97563bb2f1ce2d7b384c02e99c963a3569a423
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-04-13 15:55:36 +0000

    PDM: Put pAsyncCompletionTemplates under the ListCritSect.
    svn:sync-xref-src-repo-rev: r60005

--- src/VBox/VMM/PDMInternal.h
+++ src/VBox/VMM/PDMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: PDMInternal.h 28262 2010-04-13 15:40:10Z knut.osmundsen@oracle.com $ */
+/* $Id: PDMInternal.h 28263 2010-04-13 15:55:36Z knut.osmundsen@oracle.com $ */
 /** @file
  * PDM - Internal header file.
  */
@@ -90,6 +90,19 @@ typedef struct PDMRTC *PPDMRTC;
 
 /** Pointer to an USB HUB registration record. */
 typedef struct PDMUSBHUB *PPDMUSBHUB;
+
+/**
+ * Supported asynchronous completion endpoint classes.
+ */
+typedef enum PDMASYNCCOMPLETIONEPCLASSTYPE
+{
+    /** File class. */
+    PDMASYNCCOMPLETIONEPCLASSTYPE_FILE = 0,
+    /** Number of supported classes. */
+    PDMASYNCCOMPLETIONEPCLASSTYPE_MAX,
+    /** 32bit hack. */
+    PDMASYNCCOMPLETIONEPCLASSTYPE_32BIT_HACK = 0x7fffffff
+} PDMASYNCCOMPLETIONEPCLASSTYPE;
 
 /**
  * Private device instance data.
@@ -999,6 +1012,7 @@ AssertCompileMemberAlignment(PDM, StatQueuedCritSectLeaves, 8);
 typedef PDM *PPDM;
 
 
+
 /**
  * PDM data kept in the UVM.
  */
@@ -1016,20 +1030,20 @@ typedef struct PDMUSERPERVM
     /** Tail of the PDM Thread list. (singly linked) */
     R3PTRTYPE(PPDMTHREAD)           pThreadsTail;
 
-    /** @name   PDM Async Completion
-     * @{ */
-    /** Pointer to the array of supported endpoint classes. */
-    R3PTRTYPE(PPDMASYNCCOMPLETIONEPCLASS *)  papAsyncCompletionEndpointClass;
-    /** Head of the templates. (singly linked) */
-    R3PTRTYPE(PPDMASYNCCOMPLETIONTEMPLATE) pAsyncCompletionTemplates;
-    /** @} */
-
     /** Lock protecting the lists below it. */
     RTCRITSECT                      ListCritSect;
     /** Pointer to list of loaded modules. */
     PPDMMOD                         pModules;
     /** List of initialized critical sections. (LIFO) */
     R3PTRTYPE(PPDMCRITSECTINT)      pCritSects;
+
+    /** @name   PDM Async Completion
+     * @{ */
+    /** Pointer to the array of supported endpoint classes. */
+    PPDMASYNCCOMPLETIONEPCLASS      apAsyncCompletionEndpointClass[PDMASYNCCOMPLETIONEPCLASSTYPE_MAX];
+    /** Head of the templates. Singly linked, protected by ListCritSect. */
+    R3PTRTYPE(PPDMASYNCCOMPLETIONTEMPLATE) pAsyncCompletionTemplates;
+    /** @} */
 
 } PDMUSERPERVM;
 /** Pointer to the PDM data kept in the UVM. */
