63c5bd6e3a8e7b3cda6564a25bedce47523f35dc
Sander van Leeuwen <noreply@oracle.com>
2010-04-14 10:00:45 +0000

    We don't pass on invlpg information to the recompiler for nested paging guests, so we must make sure the recompiler flushes its TLB the next time it executes code.
    svn:sync-xref-src-repo-rev: r60032

--- src/VBox/VMM/VMMR0/HWACCMR0.cpp
+++ src/VBox/VMM/VMMR0/HWACCMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWACCMR0.cpp 27630 2010-03-23 13:48:50Z knut.osmundsen@oracle.com $ */
+/* $Id: HWACCMR0.cpp 28285 2010-04-14 10:00:45Z noreply@oracle.com $ */
 /** @file
  * HWACCM - Host Context Ring 0.
  */
@@ -1152,6 +1152,15 @@ VMMR0DECL(int) HWACCMR0Leave(PVM pVM, PVMCPU pVCpu)
     }
 
     rc = HWACCMR0Globals.pfnLeaveSession(pVM, pVCpu, pCtx);
+
+    /* We don't pass on invlpg information to the recompiler for nested paging guests, so we must make sure the recompiler flushes its TLB 
+     * the next time it executes code.
+     */
+    if (    pVM->hwaccm.s.fNestedPaging
+        &&  CPUMIsGuestInPagedProtectedModeEx(pCtx))
+    {
+        CPUMSetChangedFlags(pVCpu, CPUM_CHANGED_GLOBAL_TLB_FLUSH);
+    }
 
     /* keep track of the CPU owning the VMCS for debugging scheduling weirdness and ring-3 calls. */
 #ifdef RT_STRICT
