59cec936d2d73a14244c06e6be47c4e6f050ff73
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-04-19 12:49:13 +0000

    time-win.cpp: Fixed 1000000 scale down bug in RTTimeSystemMilliTS (always) and RTTimeMilliTS when not in a VM process.  Been causing trouble since r14735.
    svn:sync-xref-src-repo-rev: r60233

--- src/VBox/Runtime/r3/win/time-win.cpp
+++ src/VBox/Runtime/r3/win/time-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: time-win.cpp 26212 2010-02-03 16:59:08Z knut.osmundsen@oracle.com $ */
+/* $Id: time-win.cpp 28459 2010-04-19 12:49:13Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Time, Windows.
  */
@@ -97,8 +97,7 @@ DECLINLINE(uint64_t) rtTimeGetSystemNanoTS(void)
     LARGE_INTEGER   ll;
     if (QueryPerformanceCounter(&ll))
         return (ll.QuadPart * uMult) / llFreq.QuadPart;
-    else
-        return (uint64_t)GetTickCount() * (uint64_t)1000000;
+    return (uint64_t)GetTickCount() * (uint64_t)1000000;
 
 #elif defined USE_FILE_TIME
     /*
@@ -146,7 +145,7 @@ RTDECL(uint64_t) RTTimeSystemNanoTS(void)
 
 RTDECL(uint64_t) RTTimeSystemMilliTS(void)
 {
-    return rtTimeGetSystemNanoTS();
+    return rtTimeGetSystemNanoTS() / 1000000;
 }
 
 
