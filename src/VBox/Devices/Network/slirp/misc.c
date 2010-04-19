86feb1f8b7c83e2d5f5a7a7ab324e675181e11a9
Vasily Levchenko <noreply@oracle.com>
2010-04-19 18:49:19 +0000

    NAT: correctly returns null values to DrvNAT in case zone has run out of room.
    svn:sync-xref-src-repo-rev: r60289

--- src/VBox/Devices/Network/slirp/misc.c
+++ src/VBox/Devices/Network/slirp/misc.c
@@ -1,4 +1,4 @@
-/* $Id: misc.c 28490 2010-04-19 18:22:12Z noreply@oracle.com $ */
+/* $Id: misc.c 28494 2010-04-19 18:49:19Z noreply@oracle.com $ */
 /** @file
  * NAT - helpers.
  */
@@ -383,6 +383,12 @@ struct mbuf *slirp_ext_m_get(PNATState pData, size_t cbMin, void **ppvBuf, size_
         AssertMsgFailed(("Unsupported size"));
 
     m = m_getjcl(pData, M_NOWAIT, MT_HEADER, M_PKTHDR, size);
+    if (m == NULL)
+    {
+        *ppvBuf = NULL;
+        *pcbBuf = 0;
+        return NULL;
+    }
     m->m_len = size;
     *ppvBuf = mtod(m, void *);
     *pcbBuf = size;
