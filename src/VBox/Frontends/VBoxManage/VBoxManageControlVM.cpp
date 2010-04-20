77802f63e47b7a55adb03a5ec4cfdd7932e92fb5
Frank Mehnert <noreply@oracle.com>
2010-04-20 14:16:20 +0000

    VBoxManageControlVM: use ''IGuest::SetMemoryBalloonSize'' for changing the balloon size as this method calls ''IMachine::SetMemoryBalloonSize()''. This reverts r59756.
    svn:sync-xref-src-repo-rev: r60333

--- src/VBox/Frontends/VBoxManage/VBoxManageControlVM.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageControlVM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageControlVM.cpp 28047 2010-04-07 12:46:52Z noreply@oracle.com $ */
+/* $Id: VBoxManageControlVM.cpp 28528 2010-04-20 14:16:20Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - VirtualBox's command-line interface.
  */
@@ -782,7 +782,11 @@ int handleControlVM(HandlerArg *a)
                 rc = E_FAIL;
                 break;
             }
-            CHECK_ERROR(machine, COMSETTER(MemoryBalloonSize)(uVal));
+            /* guest is running; update IGuest */
+            ComPtr <IGuest> guest;
+            rc = console->COMGETTER(Guest)(guest.asOutParam());
+            if (SUCCEEDED(rc))
+                CHECK_ERROR(guest, COMSETTER(MemoryBalloonSize)(uVal));
         }
         else if (!strcmp(a->argv[1], "teleport"))
         {
