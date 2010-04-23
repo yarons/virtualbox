68d2f7bd830f9d13b942e2efe3e7d393a44f7b66
Sander van Leeuwen <noreply@oracle.com>
2010-04-23 15:56:01 +0000

    Already loaded error translation
    svn:sync-xref-src-repo-rev: r60529

--- src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
+++ src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-win.cpp 28659 2010-04-23 15:27:05Z noreply@oracle.com $ */
+/* $Id: SUPDrv-win.cpp 28661 2010-04-23 15:56:01Z noreply@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Windows NT specifics.
  */
@@ -686,7 +686,7 @@ int  VBOXCALL   supdrvOSLdrOpen(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE pImage, c
         else
         {
             Log(("rcNt=%#x '%ls'\n", rcNt, pwcsFilename));
-            SUPR0Printf("VBoxDrv: rcNt=%#x '%ws'\n", rcNt, pwcsFilename);
+            SUPR0Printf("VBoxDrv: rcNt=%x '%ws'\n", rcNt, pwcsFilename);
             switch (rcNt)
             {
                 case /* 0xc0000003 */ STATUS_INVALID_INFO_CLASS:
@@ -711,6 +711,9 @@ int  VBOXCALL   supdrvOSLdrOpen(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE pImage, c
                     break;
                 case    0xC0000428 /* STATUS_INVALID_IMAGE_HASH */ :
                     rc = VERR_LDR_IMAGE_HASH;
+                    break;
+                case    0xC000010E /* STATUS_ALREADY_LOADED */ :
+                    rc = VERR_ALREADY_LOADED;
                     break;
                 default:
                     rc = VERR_LDR_GENERAL_FAILURE;
