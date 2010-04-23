8895f6d9256d77deee800cfe667616d5e05af2b0
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-04-23 13:43:45 +0000

    Guest Control/VBoxService: Not necessary.
    svn:sync-xref-src-repo-rev: r60507

--- src/VBox/Additions/common/VBoxService/VBoxServiceControlExec.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceControlExec.cpp
@@ -1,5 +1,5 @@
 
-/* $Id: VBoxServiceControlExec.cpp 28646 2010-04-23 13:39:58Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxServiceControlExec.cpp 28647 2010-04-23 13:43:45Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxServiceControlExec - Utility functions for process execution.
  */
@@ -624,8 +624,6 @@ int VBoxServiceControlExecAllocateThreadData(PVBOXSERVICECTRLTHREAD pThread,
                                     pszArgsTemp, NULL);
         RTStrFree(pszArgsTemp);
     }
-    else
-        rc = VERR_NO_MEMORY;
 
     /* Did we get the same result? Also count in the added argv[0] above! */
     Assert((uNumArgs + 1) == pData->uNumArgs);
