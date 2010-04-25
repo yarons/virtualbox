e47c74eee7ccaf9c869a637ce3a5cc62c7d20d05
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-04-25 00:37:30 +0000

    VBoxNetFlt/linux: Added VBOXNETFLT_LINUX_NO_XMIT_QUEUE since we can call pfnRecv at interrupt / atomic time now.
    svn:sync-xref-src-repo-rev: r60575

--- src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
+++ src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFltInternal.h 28667 2010-04-23 17:43:23Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxNetFltInternal.h 28703 2010-04-25 00:37:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Internal Header.
  */
@@ -169,8 +169,10 @@ typedef struct VBOXNETFLTINS
             RTMAC MacAddr;
             struct notifier_block Notifier;
             struct packet_type    PacketType;
+#  ifndef VBOXNETFLT_LINUX_NO_XMIT_QUEUE
             struct sk_buff_head   XmitQueue;
             struct work_struct    XmitTask;
+#  endif
             /** @} */
 # elif defined(RT_OS_SOLARIS)
             /** @name Solaris instance data.
