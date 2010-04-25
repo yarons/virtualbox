ae2c79bca0f46e8d47524d53c0c2ffbbd9280da9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-04-25 21:36:26 +0000

    DrvIntNet.cpp: Enable the ring-0 part of the driver.
    svn:sync-xref-src-repo-rev: r60593

--- src/VBox/Devices/Network/DrvIntNet.cpp
+++ src/VBox/Devices/Network/DrvIntNet.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvIntNet.cpp 28720 2010-04-25 21:35:30Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvIntNet.cpp 28721 2010-04-25 21:36:26Z knut.osmundsen@oracle.com $ */
 /** @file
  * DrvIntNet - Internal network transport driver.
  */
@@ -48,8 +48,12 @@
 
 #include "../Builtins.h"
 
-// To play with R0 drivers.
-//#define VBOX_WITH_DRVINTNET_IN_R0
+
+/*******************************************************************************
+*   Defined Constants And Macros                                               *
+*******************************************************************************/
+/** Enables the ring-0 part. */
+#define VBOX_WITH_DRVINTNET_IN_R0
 
 
 /*******************************************************************************
