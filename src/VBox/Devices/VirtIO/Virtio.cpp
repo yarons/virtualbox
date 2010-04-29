12fa907210f478107493301d1b467b55afd09139
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2010-04-29 16:09:43 +0000

    Devices/VirtIO: make it build for release builds with logging enabled
    svn:sync-xref-src-repo-rev: r60853

--- src/VBox/Devices/VirtIO/Virtio.cpp
+++ src/VBox/Devices/VirtIO/Virtio.cpp
@@ -1,4 +1,4 @@
-/* $Id: Virtio.cpp 28800 2010-04-27 08:22:32Z noreply@oracle.com $ */
+/* $Id: Virtio.cpp 28907 2010-04-29 16:09:43Z klaus.espenlaub@oracle.com $ */
 /** @file
  * Virtio - Virtio Common Functions (VRing, VQueue, Virtio PCI)
  */
@@ -26,7 +26,7 @@
 #define INSTANCE(pState) pState->szInstance
 #define IFACE_TO_STATE(pIface, ifaceName) ((VPCISTATE *)((char*)pIface - RT_OFFSETOF(VPCISTATE, ifaceName)))
 
-#ifdef DEBUG
+#ifdef LOG_ENABLED
 #define QUEUENAME(s, q) (q->pcszName)
 #endif /* DEBUG */
 
