6e85ca6150843fdd788cf94ef5d4ef34efef032b
Alexander Eichner <alexander.eichner@oracle.com>
2010-04-30 21:17:27 +0000

    AsyncCompletion: Fix an I/O hang after the manager grew. The I/O manager should check for pending requests afterwards
    svn:sync-xref-src-repo-rev: r60937

--- src/VBox/VMM/PDMAsyncCompletionFileCache.cpp
+++ src/VBox/VMM/PDMAsyncCompletionFileCache.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMAsyncCompletionFileCache.cpp 28800 2010-04-27 08:22:32Z noreply@oracle.com $ */
+/* $Id: PDMAsyncCompletionFileCache.cpp 28947 2010-04-30 21:17:27Z alexander.eichner@oracle.com $ */
 /** @file
  * PDM Async I/O - Transport data asynchronous in R3 using EMT.
  * File data cache.
@@ -1721,8 +1721,6 @@ int pdmacFileEpCacheRead(PPDMASYNCCOMPLETIONENDPOINTFILE pEndpoint, PPDMASYNCCOM
     LogFlowFunc((": pEndpoint=%#p{%s} pTask=%#p off=%RTfoff paSegments=%#p cSegments=%u cbRead=%u\n",
                  pEndpoint, pEndpoint->Core.pszUri, pTask, off, paSegments, cSegments, cbRead));
 
-    pTask->cbTransferLeft = cbRead;
-    pTask->rc             = VINF_SUCCESS;
     /* Set to completed to make sure that the task is valid while we access it. */
     ASMAtomicWriteBool(&pTask->fCompleted, true);
 
@@ -1955,8 +1953,6 @@ int pdmacFileEpCacheWrite(PPDMASYNCCOMPLETIONENDPOINTFILE pEndpoint, PPDMASYNCCO
     LogFlowFunc((": pEndpoint=%#p{%s} pTask=%#p off=%RTfoff paSegments=%#p cSegments=%u cbWrite=%u\n",
                  pEndpoint, pEndpoint->Core.pszUri, pTask, off, paSegments, cSegments, cbWrite));
 
-    pTask->cbTransferLeft = cbWrite;
-    pTask->rc             = VINF_SUCCESS;
     /* Set to completed to make sure that the task is valid while we access it. */
     ASMAtomicWriteBool(&pTask->fCompleted, true);
 
