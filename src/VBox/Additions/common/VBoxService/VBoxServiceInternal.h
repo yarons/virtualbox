871d4114d3c802d41cbab69bc193b470dafba007
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-05-03 14:55:34 +0000

    VBoxService/PropCache: Make it thread safe (for a global cache later).
    svn:sync-xref-src-repo-rev: r61001

--- src/VBox/Additions/common/VBoxService/VBoxServiceInternal.h
+++ src/VBox/Additions/common/VBoxService/VBoxServiceInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceInternal.h 28968 2010-05-03 11:47:26Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxServiceInternal.h 28983 2010-05-03 14:55:34Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxService - Guest Additions Services.
  */
@@ -24,10 +24,8 @@
 # include <process.h> /* Needed for file version information. */
 #endif
 
-#ifdef VBOX_WITH_GUEST_CONTROL
-# include <iprt/list.h>
-# include <iprt/semaphore.h>
-#endif
+#include <iprt/list.h>
+#include <iprt/semaphore.h>
 
 /**
  * A service descriptor.
@@ -221,8 +219,9 @@ typedef VBOXSERVICECTRLSTDINBUF *PVBOXSERVICECTRLSTDINBUF;
 typedef struct
 {    
     /** The client ID for HGCM communication. */
-    uint32_t uClientID;
+    uint32_t   uClientID;
     RTLISTNODE Node;
+    RTSEMMUTEX Mutex;
 } VBOXSERVICEVEPROPCACHE;
 /** Pointer to a guest property cache. */
 typedef VBOXSERVICEVEPROPCACHE *PVBOXSERVICEVEPROPCACHE;
