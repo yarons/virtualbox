cdcc0efb131f6725849b5f0bf24d8a01adba64ac
Sander van Leeuwen <noreply@oracle.com>
2010-05-03 13:17:06 +0000

    Removed wrong comment
    svn:sync-xref-src-repo-rev: r60993

--- src/VBox/VMM/GMM.cpp
+++ src/VBox/VMM/GMM.cpp
@@ -1,4 +1,4 @@
-/* $Id: GMM.cpp 28975 2010-05-03 13:16:51Z noreply@oracle.com $ */
+/* $Id: GMM.cpp 28976 2010-05-03 13:17:06Z noreply@oracle.com $ */
 /** @file
  * GMM - Global Memory Manager, ring-3 request wrappers.
  */
@@ -329,7 +329,6 @@ GMMR3DECL(int)  GMMR3QueryMemoryStats(PVM pVM, uint64_t *pcAllocPages, uint64_t 
     *pcMaxPages         = 0;
     *pcBalloonPages     = 0;
 
-    /* Must be callable from any thread, so can't use VMMR3CallR0. */
     int rc = VMMR3CallR0(pVM, VMMR0_DO_GMM_QUERY_MEM_STATS, 0, &Req.Hdr);
     if (rc == VINF_SUCCESS)
     {
