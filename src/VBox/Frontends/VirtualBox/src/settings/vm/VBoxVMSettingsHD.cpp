87a910f7887e46739e77b34d43cdcd5fa48b0608
Sergey Dubov <sergey.dubov@oracle.com>
2010-05-07 11:55:12 +0000

    FE/Qt: 4396: Storage Settings UI is now correctly allows to add SAS controllers after all others being added.
    svn:sync-xref-src-repo-rev: r61274

--- src/VBox/Frontends/VirtualBox/src/settings/vm/VBoxVMSettingsHD.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/vm/VBoxVMSettingsHD.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxVMSettingsHD.cpp 29010 2010-05-04 12:17:43Z sergey.dubov@oracle.com $ */
+/* $Id: VBoxVMSettingsHD.cpp 29197 2010-05-07 11:55:12Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -2319,7 +2319,7 @@ void VBoxVMSettingsHD::updateActionsState()
     bool isAttachment = mStorageModel->data (index, StorageModel::R_IsAttachment).toBool();
     bool isAttachmentsPossible = mStorageModel->data (index, StorageModel::R_IsMoreAttachmentsPossible).toBool();
 
-    mAddCtrAction->setEnabled (isIDEPossible || isSATAPossible || isSCSIPossible || isFloppyPossible);
+    mAddCtrAction->setEnabled (isIDEPossible || isSATAPossible || isSCSIPossible || isFloppyPossible || isSASPossible);
     mAddIDECtrAction->setEnabled (isIDEPossible);
     mAddSATACtrAction->setEnabled (isSATAPossible);
     mAddSCSICtrAction->setEnabled (isSCSIPossible);
