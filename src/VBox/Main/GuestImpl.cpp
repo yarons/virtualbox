f02f490562f4bf7759d84882b32df27f51104e97
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2010-05-10 12:20:08 +0000

    Main/GuestImpl: corrected error code.
    svn:sync-xref-src-repo-rev: r61387

--- src/VBox/Main/GuestImpl.cpp
+++ src/VBox/Main/GuestImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestImpl.cpp 29225 2010-05-07 16:01:34Z noreply@oracle.com $ */
+/* $Id: GuestImpl.cpp 29299 2010-05-10 12:20:08Z ramshankar.venkataraman@oracle.com $ */
 
 /** @file
  *
@@ -788,7 +788,7 @@ STDMETHODIMP Guest::ExecuteProcess(IN_BSTR aCommand, ULONG aFlags,
             com::SafeArray<IN_BSTR> args(ComSafeArrayInArg(aArguments));
             uNumArgs = args.size();
             papszArgv = (char**)RTMemAlloc(sizeof(char*) * (uNumArgs + 1));
-            AssertReturn(papszArgv, VERR_NO_MEMORY);
+            AssertReturn(papszArgv, E_OUTOFMEMORY);
             for (unsigned i = 0; RT_SUCCESS(vrc) && i < uNumArgs; i++)
                 vrc = RTStrAPrintf(&papszArgv[i], "%s", Utf8Str(args[i]).raw());
             papszArgv[uNumArgs] = NULL;
