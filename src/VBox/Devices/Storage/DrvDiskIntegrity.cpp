a3188ea4ae53d704abfcdc24b3ad0a20ded4ae99
Alexander Eichner <alexander.eichner@oracle.com>
2010-05-10 15:08:10 +0000

    DiskIntegrity: Build fix
    svn:sync-xref-src-repo-rev: r61394

--- src/VBox/Devices/Storage/DrvDiskIntegrity.cpp
+++ src/VBox/Devices/Storage/DrvDiskIntegrity.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvDiskIntegrity.cpp 28948 2010-04-30 21:19:04Z alexander.eichner@oracle.com $ */
+/* $Id: DrvDiskIntegrity.cpp 29306 2010-05-10 15:08:10Z alexander.eichner@oracle.com $ */
 /** @file
  * VBox storage devices: Disk integrity check.
  */
@@ -30,6 +30,7 @@
 #include <iprt/sg.h>
 #include <iprt/time.h>
 #include <iprt/semaphore.h>
+#include <iprt/asm.h>
 
 #include "Builtins.h"
 
