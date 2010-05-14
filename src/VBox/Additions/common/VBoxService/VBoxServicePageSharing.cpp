2b0f4d0acc8702a39d56cdaf8cfc5343785f06ff
Sander van Leeuwen <noreply@oracle.com>
2010-05-14 15:10:29 +0000

    Added note
    svn:sync-xref-src-repo-rev: r61607

--- src/VBox/Additions/common/VBoxService/VBoxServicePageSharing.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServicePageSharing.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServicePageSharing.cpp 29473 2010-05-14 14:20:01Z noreply@oracle.com $ */
+/* $Id: VBoxServicePageSharing.cpp 29479 2010-05-14 15:10:29Z noreply@oracle.com $ */
 /** @file
  * VBoxService - Guest page sharing.
  */
@@ -232,6 +232,7 @@ void VBoxServicePageSharingInspectModules(DWORD dwProcessId, PAVLPVNODECORE *ppN
     bRet = Module32First(hSnapshot, &ModuleInfo);
     do
     {
+        /** todo when changing this make sure VBoxService.exe is excluded! */
         char *pszDot = strrchr(ModuleInfo.szModule, '.');
         if (    pszDot 
             &&  (pszDot[1] == 'e' || pszDot[1] == 'E'))
