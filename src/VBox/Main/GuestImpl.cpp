652328009b8676c0231d03d389f54ca10b3231f5
Frank Mehnert <noreply@oracle.com>
2010-05-17 09:30:12 +0000

    Main/GuestImpl: more correct handling of the ''RTStrAPrintf()'' result
    svn:sync-xref-src-repo-rev: r61651

--- src/VBox/Main/GuestImpl.cpp
+++ src/VBox/Main/GuestImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestImpl.cpp 29460 2010-05-14 11:06:44Z noreply@oracle.com $ */
+/* $Id: GuestImpl.cpp 29512 2010-05-17 09:30:12Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -780,7 +780,12 @@ STDMETHODIMP Guest::ExecuteProcess(IN_BSTR aCommand, ULONG aFlags,
             papszArgv = (char**)RTMemAlloc(sizeof(char*) * (uNumArgs + 1));
             AssertReturn(papszArgv, E_OUTOFMEMORY);
             for (unsigned i = 0; RT_SUCCESS(vrc) && i < uNumArgs; i++)
-                vrc = RTStrAPrintf(&papszArgv[i], "%s", Utf8Str(args[i]).raw());
+            {
+                int cbLen = RTStrAPrintf(&papszArgv[i], "%s", Utf8Str(args[i]).raw());
+                if (cbLen < 0)
+                    vrc = VERR_NO_MEMORY;
+
+            }
             papszArgv[uNumArgs] = NULL;
         }
 
