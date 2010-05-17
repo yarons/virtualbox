5c95b5d2ea5386ee41c048ffe082f5659fd80be7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-05-17 10:14:22 +0000

    PDM: Added PDMDevHlpCallR0 and PDMDEV_VALIDATE_CONFIG_RETURN.
    svn:sync-xref-src-repo-rev: r61661

--- src/VBox/VMM/VMMR0/VMMR0.cpp
+++ src/VBox/VMM/VMMR0/VMMR0.cpp
@@ -1,10 +1,10 @@
-/* $Id: VMMR0.cpp 29458 2010-05-14 10:16:06Z noreply@oracle.com $ */
+/* $Id: VMMR0.cpp 29521 2010-05-17 10:14:22Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMM - Host Context Ring 0.
  */
 
 /*
- * Copyright (C) 2006-2007 Oracle Corporation
+ * Copyright (C) 2006-2010 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -1012,6 +1012,13 @@ static int vmmR0EntryExWorker(PVM pVM, VMCPUID idCpu, VMMR0OPERATION enmOperatio
             if (!pVM || !pReqHdr || u64Arg || idCpu != NIL_VMCPUID)
                 return VERR_INVALID_PARAMETER;
             return PDMR0DriverCallReqHandler(pVM, (PPDMDRIVERCALLREQHANDLERREQ)pReqHdr);
+        }
+
+        case VMMR0_DO_PDM_DEVICE_CALL_REQ_HANDLER:
+        {
+            if (!pVM || !pReqHdr || u64Arg || idCpu != NIL_VMCPUID)
+                return VERR_INVALID_PARAMETER;
+            return PDMR0DeviceCallReqHandler(pVM, (PPDMDEVICECALLREQHANDLERREQ)pReqHdr);
         }
 
         /*
