b6b588c7f0f4c385e80d1fbdd3529beed5123abe
Frank Mehnert <noreply@oracle.com>
2010-05-18 11:54:24 +0000

    Main: implemented ''IHost::GetProcessorCoreCount()''
    svn:sync-xref-src-repo-rev: r61767

--- src/VBox/Main/HostImpl.cpp
+++ src/VBox/Main/HostImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: HostImpl.cpp 29385 2010-05-11 18:05:44Z knut.osmundsen@oracle.com $ */
+/* $Id: HostImpl.cpp 29615 2010-05-18 11:54:24Z noreply@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation: Host
  */
@@ -764,6 +764,20 @@ STDMETHODIMP Host::COMGETTER(ProcessorOnlineCount)(ULONG *aCount)
 
     *aCount = RTMpGetOnlineCount();
     return S_OK;
+}
+
+/**
+ * Returns the number of installed physical processor cores.
+ *
+ * @returns COM status code
+ * @param   count address of result variable
+ */
+STDMETHODIMP Host::COMGETTER(ProcessorCoreCount)(ULONG *aCount)
+{
+    CheckComArgOutPointerValid(aCount);
+    // no locking required
+
+    return E_NOTIMPL;
 }
 
 /**
