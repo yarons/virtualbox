80eea19733e26c01d41b4d213019f6ad7c6f900d
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-05-31 14:36:04 +0000

    IPRT/process-win: Only deal with NULL domains on NT4.
    svn:sync-xref-src-repo-rev: r62178

--- src/VBox/Runtime/r3/win/process-win.cpp
+++ src/VBox/Runtime/r3/win/process-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: process-win.cpp 29807 2010-05-26 10:13:20Z andreas.loeffler@oracle.com $ */
+/* $Id: process-win.cpp 29912 2010-05-31 14:36:04Z andreas.loeffler@oracle.com $ */
 /** @file
  * IPRT - Process, Windows.
  */
@@ -524,14 +524,18 @@ static int rtProcCreateAsUserHlp(PRTUTF16 pwszUser, PRTUTF16 pwszPassword, PRTUT
          *  - SE_INCREASE_QUOTA_NAME
          *
          * We may fail here with ERROR_PRIVILEGE_NOT_HELD.
-         *
-         * Because we have to deal with http://support.microsoft.com/kb/245683 for NULL domain names
-         * on NT4 here, pass an empty string. However, passing FQDNs should work!
          */
         PHANDLE phToken = NULL;
         HANDLE hTokenLogon = INVALID_HANDLE_VALUE;
         fRc = LogonUserW(pwszUser,
-                         L"",
+                         /* 
+                          * Because we have to deal with http://support.microsoft.com/kb/245683
+                          * for NULL domain names when running on NT4 here, pass an empty string if so.
+                          * However, passing FQDNs should work! 
+                          */
+                         ((DWORD)(LOBYTE(LOWORD(GetVersion()))) < 5)  /* < Windows 2000. */
+                         ? L""   /* NT4 and older. */
+                         : NULL, /* Windows 2000 and up. */
                          pwszPassword,
                          LOGON32_LOGON_INTERACTIVE,
                          LOGON32_PROVIDER_DEFAULT,
