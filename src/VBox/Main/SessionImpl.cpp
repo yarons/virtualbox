109c394075155d7576552ad2b8bb4d670698de04
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-06-01 15:03:06 +0000

    SessionImpl.cpp: Don't use ComAssertComRC because it's evil - it doesn't work the same way in all builds wrt error info. Also it is not nice to assert here just because some remote VM process died.
    svn:sync-xref-src-repo-rev: r62235

--- src/VBox/Main/SessionImpl.cpp
+++ src/VBox/Main/SessionImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: SessionImpl.cpp 29853 2010-05-27 23:49:38Z knut.osmundsen@oracle.com $ */
+/* $Id: SessionImpl.cpp 29953 2010-06-01 15:03:06Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Client Session COM Class implementation in VBoxC.
  */
@@ -180,13 +180,21 @@ STDMETHODIMP Session::COMGETTER(Machine)(IMachine **aMachine)
 
     CHECK_OPEN();
 
-    HRESULT rc = E_FAIL;
-
+    HRESULT rc;
     if (mConsole)
         rc = mConsole->machine().queryInterfaceTo(aMachine);
     else
         rc = mRemoteMachine.queryInterfaceTo(aMachine);
-    ComAssertComRC(rc);
+    if (FAILED(rc))
+    {
+        /** @todo VBox 3.3: replace E_FAIL with rc here. */
+        if (mConsole)
+            setError(E_FAIL, tr("Failed to query the session machine (%Rhrc)"), rc);
+        else if (FAILED_DEAD_INTERFACE(rc))
+            setError(E_FAIL, tr("Peer process crashed"));
+        else
+            setError(E_FAIL, tr("Failed to query the remote session machine (%Rhrc)"), rc);
+    }
 
     return rc;
 }
@@ -202,13 +210,21 @@ STDMETHODIMP Session::COMGETTER(Console)(IConsole **aConsole)
 
     CHECK_OPEN();
 
-    HRESULT rc = E_FAIL;
-
+    HRESULT rc;
     if (mConsole)
         rc = mConsole.queryInterfaceTo(aConsole);
     else
         rc = mRemoteConsole.queryInterfaceTo(aConsole);
-    ComAssertComRC(rc);
+    if (FAILED(rc))
+    {
+        /** @todo VBox 3.3: replace E_FAIL with rc here. */
+        if (mConsole)
+            setError(E_FAIL, tr("Failed to query the console (%Rhrc)"), rc);
+        else if (FAILED_DEAD_INTERFACE(rc))
+            setError(E_FAIL, tr("Peer process crashed"));
+        else
+            setError(E_FAIL, tr("Failed to query the remote console (%Rhrc)"), rc);
+    }
 
     return rc;
 }
