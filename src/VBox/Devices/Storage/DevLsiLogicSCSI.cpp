e764834dc34e560079b97cc5332bd3fb059c218c
Alexander Eichner <alexander.eichner@oracle.com>
2010-06-09 16:06:37 +0000

    LsiLogic: Don't reset the interrupt mask during a unit reset. Fixes older Linux guests (SLES 9 for example) which were hanging in an interrupt loop during initialization of the controller
    svn:sync-xref-src-repo-rev: r62526

--- src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
+++ src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevLsiLogicSCSI.cpp 29913 2010-05-31 14:51:38Z knut.osmundsen@oracle.com $ */
+/* $Id: DevLsiLogicSCSI.cpp 30128 2010-06-09 16:06:37Z alexander.eichner@oracle.com $ */
 /** @file
  * VBox storage devices: LsiLogic LSI53c1030 SCSI controller.
  */
@@ -985,8 +985,7 @@ static int lsilogicRegisterWrite(PLSILOGICSCSI pThis, uint32_t uOffset, void *pv
                     {
                         pThis->enmState = LSILOGICSTATE_RESET;
 
-                        /* Reset interrupt states. */
-                        pThis->uInterruptMask   = 0;
+                        /* Reset interrupt status. */
                         pThis->uInterruptStatus = 0;
                         lsilogicUpdateInterrupt(pThis);
 
