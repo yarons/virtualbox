45eba8d5e7f569e03ad68c40b79ed6bd56553bb8
Sander van Leeuwen <noreply@oracle.com>
2010-06-16 09:58:17 +0000

    Typo
    svn:sync-xref-src-repo-rev: r62728

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 30234 2010-06-16 09:55:56Z noreply@oracle.com $ */
+/* $Id: PGMPhys.cpp 30235 2010-06-16 09:58:17Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -3416,7 +3416,7 @@ int pgmR3PhysChunkMap(PVM pVM, uint32_t idChunk, PPPGMCHUNKR3MAP ppChunk)
 #endif
             pVM->pgm.s.ChunkR3Map.c--;
 
-            /* Chunk removed, so clear the page map TBL as well (might still be referenced). */
+            /* Chunk removed, so clear the page map TLB as well (might still be referenced). */
             PGMPhysInvalidatePageMapTLB(pVM);
         }
     }
