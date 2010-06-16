ac4a610817913df0f61d22cf48afe76c04870e7c
Sander van Leeuwen <noreply@oracle.com>
2010-06-16 09:55:56 +0000

    This is very fatal
    svn:sync-xref-src-repo-rev: r62726

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 30233 2010-06-16 09:49:50Z noreply@oracle.com $ */
+/* $Id: PGMPhys.cpp 30234 2010-06-16 09:55:56Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -3422,7 +3422,7 @@ int pgmR3PhysChunkMap(PVM pVM, uint32_t idChunk, PPPGMCHUNKR3MAP ppChunk)
     }
     else
     {
-        AssertRC(rc);
+        AssertFatalRC(rc);
 #ifdef VBOX_WITH_2X_4GB_ADDR_SPACE
         MMR3HeapFree(pChunk);
 #else
