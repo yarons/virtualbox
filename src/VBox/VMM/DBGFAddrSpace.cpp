ee85beb1f03e463344c12fc02586ee24b1313816
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-06-16 16:17:08 +0000

    2x use AssertCompileNS.
    svn:sync-xref-src-repo-rev: r62761

--- src/VBox/VMM/DBGFAddrSpace.cpp
+++ src/VBox/VMM/DBGFAddrSpace.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFAddrSpace.cpp 30078 2010-06-07 14:41:10Z knut.osmundsen@oracle.com $ */
+/* $Id: DBGFAddrSpace.cpp 30257 2010-06-16 16:17:08Z knut.osmundsen@oracle.com $ */
 /** @file
  * DBGF - Debugger Facility, Address Space Management.
  */
@@ -467,7 +467,7 @@ static void dbgfR3AsLazyPopulate(PVM pVM, RTDBGAS hAlias)
 VMMR3DECL(RTDBGAS) DBGFR3AsResolve(PVM pVM, RTDBGAS hAlias)
 {
     VM_ASSERT_VALID_EXT_RETURN(pVM, NULL);
-    AssertCompile(NIL_RTDBGAS == (RTDBGAS)0);
+    AssertCompileNS(NIL_RTDBGAS == (RTDBGAS)0);
 
     uintptr_t   iAlias = DBGF_AS_ALIAS_2_INDEX(hAlias);
     if (iAlias < DBGF_AS_COUNT)
@@ -488,7 +488,7 @@ VMMR3DECL(RTDBGAS) DBGFR3AsResolve(PVM pVM, RTDBGAS hAlias)
 VMMR3DECL(RTDBGAS) DBGFR3AsResolveAndRetain(PVM pVM, RTDBGAS hAlias)
 {
     VM_ASSERT_VALID_EXT_RETURN(pVM, NULL);
-    AssertCompile(NIL_RTDBGAS == (RTDBGAS)0);
+    AssertCompileNS(NIL_RTDBGAS == (RTDBGAS)0);
 
     uint32_t    cRefs;
     uintptr_t   iAlias = DBGF_AS_ALIAS_2_INDEX(hAlias);
