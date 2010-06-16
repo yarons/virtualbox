4ab42d84f6c91a204ec7ddb467c6ecf2b88f5500
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-06-16 01:47:21 +0000

    slirp: VCC100 errno adj.
    svn:sync-xref-src-repo-rev: r62703

--- src/VBox/Devices/Network/slirp/slirp.h
+++ src/VBox/Devices/Network/slirp/slirp.h
@@ -1,4 +1,4 @@
-/* $Id: slirp.h 30016 2010-06-03 18:31:14Z noreply@oracle.com $ */
+/* $Id: slirp.h 30218 2010-06-16 01:47:21Z knut.osmundsen@oracle.com $ */
 /** @file
  * NAT - slirp (declarations/defines).
  */
@@ -77,6 +77,16 @@ typedef int socklen_t;
 # include <sys/timeb.h>
 # include <iphlpapi.h>
 
+/* We don't want the errno.h versions of these error defines. */
+# if defined(_MSC_VER) && _MSC_VER >= 1600
+#  include <errno.h>
+#  undef EWOULDBLOCK
+#  undef EINPROGRESS
+#  undef ENOTCONN
+#  undef EHOSTUNREACH
+#  undef ENETUNREACH
+#  undef ECONNREFUSED
+# endif
 # define EWOULDBLOCK WSAEWOULDBLOCK
 # define EINPROGRESS WSAEINPROGRESS
 # define ENOTCONN WSAENOTCONN
