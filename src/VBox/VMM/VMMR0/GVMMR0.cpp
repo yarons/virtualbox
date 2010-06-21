5c04d32d405297366c7387e1da8735ad0f189c27
Sander van Leeuwen <noreply@oracle.com>
2010-06-21 14:19:33 +0000

    Must initialize idHostCpu properly (breaks SMP guest restore)
    svn:sync-xref-src-repo-rev: r62902

--- src/VBox/VMM/VMMR0/GVMMR0.cpp
+++ src/VBox/VMM/VMMR0/GVMMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: GVMMR0.cpp 30237 2010-06-16 11:14:20Z noreply@oracle.com $ */
+/* $Id: GVMMR0.cpp 30334 2010-06-21 14:19:33Z noreply@oracle.com $ */
 /** @file
  * GVMM - Global VM Manager.
  */
@@ -638,8 +638,9 @@ GVMMR0DECL(int) GVMMR0CreateVM(PSUPDRVSESSION pSession, uint32_t cCpus, PVM *ppV
                                     /* Initialize all the VM pointers. */
                                     for (uint32_t i = 0; i < cCpus; i++)
                                     {
-                                        pVM->aCpus[i].pVMR0 = pVM;
-                                        pVM->aCpus[i].pVMR3 = pVM->pVMR3;
+                                        pVM->aCpus[i].pVMR0     = pVM;
+                                        pVM->aCpus[i].pVMR3     = pVM->pVMR3;
+                                        pVM->aCpus[i].idHostCpu = NIL_RTCPUID;
                                     }
 
                                     rc = RTR0MemObjMapUser(&pGVM->gvmm.s.VMPagesMapObj, pGVM->gvmm.s.VMPagesMemObj, (RTR3PTR)-1, 0,
