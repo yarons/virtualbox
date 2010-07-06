4bbd8ca30e89fd3bb1bda634094740bb1e6564d1
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-07-06 08:09:01 +0000

    SELM.cpp: Assertion -> log statement as it trigged during guru meditations.
    svn:sync-xref-src-repo-rev: r63382

--- src/VBox/VMM/SELM.cpp
+++ src/VBox/VMM/SELM.cpp
@@ -1,4 +1,4 @@
-/* $Id: SELM.cpp 30160 2010-06-11 13:26:50Z noreply@oracle.com $ */
+/* $Id: SELM.cpp 30647 2010-07-06 08:09:01Z knut.osmundsen@oracle.com $ */
 /** @file
  * SELM - The Selector Manager.
  */
@@ -1951,7 +1951,7 @@ VMMDECL(int) SELMGetLDTFromSel(PVM pVM, RTSEL SelLdt, PRTGCPTR ppvLdt, unsigned 
     if (RT_FAILURE(rc))
     {
         /* fatal */
-        AssertMsgFailed(("Can't read LDT descriptor for selector=%04X\n", SelLdt));
+        Log(("Can't read LDT descriptor for selector=%04X\n", SelLdt));
         return VERR_SELECTOR_NOT_PRESENT;
     }
 
