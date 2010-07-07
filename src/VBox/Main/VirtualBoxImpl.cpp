9b62260cd0f9031c23126e0e7046cf851b4c5cce
Ulrich Moeller <noreply@oracle.com>
2010-07-07 16:53:36 +0000

    windows build fix
    svn:sync-xref-src-repo-rev: r63482

--- src/VBox/Main/VirtualBoxImpl.cpp
+++ src/VBox/Main/VirtualBoxImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxImpl.cpp 30714 2010-07-07 16:20:03Z noreply@oracle.com $ */
+/* $Id: VirtualBoxImpl.cpp 30717 2010-07-07 16:53:36Z noreply@oracle.com $ */
 
 /** @file
  * Implementation of IVirtualBox in VBoxSVC.
@@ -2432,7 +2432,11 @@ VirtualBox::SVCHelperClientThread(RTTHREAD aThread, void *aUser)
         /* get the path to the executable */
         char exePathBuf[RTPATH_MAX];
         char *exePath = RTProcGetExecutableName(exePathBuf, RTPATH_MAX);
-        ComAssertBreak(exePath, E_FAIL);
+        if (!exePath)
+        {
+            rc = d->that->setError(E_FAIL, tr("Cannot get executable name"));
+            break;
+        }
 
         Utf8Str argsStr = Utf8StrFmt("/Helper %s", client.name().raw());
 
