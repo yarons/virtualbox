f933fa80f8f27963241545438876a2853f6e3a02
Alexander Eichner <alexander.eichner@oracle.com>
2010-07-12 20:14:15 +0000

    Try to fix burning debug builds
    svn:sync-xref-src-repo-rev: r63603

--- src/VBox/Main/GuestImpl.cpp
+++ src/VBox/Main/GuestImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestImpl.cpp 30794 2010-07-12 12:55:41Z andreas.loeffler@oracle.com $ */
+/* $Id: GuestImpl.cpp 30796 2010-07-12 20:14:15Z alexander.eichner@oracle.com $ */
 
 /** @file
  *
@@ -1184,7 +1184,7 @@ STDMETHODIMP Guest::ExecuteProcess(IN_BSTR aCommand, ULONG aFlags,
             RTMemFree(papszArgv);
         }
 
-        if (RT_FAILURE(rc))
+        if (RT_FAILURE(vrc))
             LogRel(("Executing guest process \"%s\" as user \"%s\" failed with %Rrc\n",
                     Utf8Command.raw(), Utf8UserName.raw(), vrc));
     }
