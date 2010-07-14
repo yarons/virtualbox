0b608d73de5c67848a1105a4c6e632959247ec1a
Sander van Leeuwen <noreply@oracle.com>
2010-07-14 12:00:30 +0000

    Compile fix
    svn:sync-xref-src-repo-rev: r63639

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 30822 2010-07-14 11:56:42Z noreply@oracle.com $ */
+/* $Id: PGMPhys.cpp 30823 2010-07-14 12:00:30Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -3280,7 +3280,7 @@ static DECLCALLBACK(int) pgmR3PhysChunkUnmapCandidateCallback(PAVLLU32NODECORE p
             /*
              * Check that it's not in any of the TLBs.
              */
-            PVM pVM = (PGMR3PHYSCHUNKUNMAPCB()pvUser)->pVM;
+            PVM pVM = ((PPGMR3PHYSCHUNKUNMAPCB)pvUser)->pVM;
             for (unsigned i = 0; i < RT_ELEMENTS(pVM->pgm.s.ChunkR3Map.Tlb.aEntries); i++)
                 if (pVM->pgm.s.ChunkR3Map.Tlb.aEntries[i].pChunk == pChunk)
                 {
