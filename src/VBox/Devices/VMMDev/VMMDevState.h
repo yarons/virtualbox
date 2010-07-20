ae651f2e292bf48df79e42c8d8ce48a28e420719
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-07-20 19:40:41 +0000

    bigger buffer.
    svn:sync-xref-src-repo-rev: r63821

--- src/VBox/Devices/VMMDev/VMMDevState.h
+++ src/VBox/Devices/VMMDev/VMMDevState.h
@@ -1,4 +1,4 @@
-/* $Id: VMMDevState.h 30852 2010-07-14 18:16:12Z knut.osmundsen@oracle.com $ */
+/* $Id: VMMDevState.h 30944 2010-07-20 19:40:41Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMMDev - Guest <-> VMM/Host communication device, internal header.
  */
@@ -271,13 +271,13 @@ typedef struct VMMDevState
     /** For buffering the what comes in over the testing data port. */
     union
     {
-        char            padding[128];
+        char            padding[512];
 
         /** VMMDEV_TESTING_CMD_INIT, VMMDEV_TESTING_CMD_SUB_NEW,
          *  VMMDEV_TESTING_CMD_FAILED. */
         struct
         {
-            char        sz[128];
+            char        sz[512];
         } String, Init, SubNew, Failed;
 
         /** VMMDEV_TESTING_CMD_TERM, VMMDEV_TESTING_CMD_SUB_DONE. */
@@ -291,7 +291,7 @@ typedef struct VMMDevState
         {
             RTUINT64U   u64Value;
             uint32_t    u32Unit;
-            char        szName[128 - 8 - 4];
+            char        szName[512 - 8 - 4];
         } Value;
     } TestingData;
 #endif /* !VBOX_WITHOUT_TESTING_FEATURES */
