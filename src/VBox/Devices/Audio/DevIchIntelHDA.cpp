056c2e6dfdc8c32048638b7c1f1d1fa48ba662e7
Michal Necasek <michal.necasek@oracle.com>
2010-07-22 16:33:44 +0000

    HDA: Allow Windows to see the HDA controller.
    svn:sync-xref-src-repo-rev: r63934

--- src/VBox/Devices/Audio/DevIchIntelHDA.cpp
+++ src/VBox/Devices/Audio/DevIchIntelHDA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevIchIntelHDA.cpp 31009 2010-07-22 15:41:21Z michal.necasek@oracle.com $ */
+/* $Id: DevIchIntelHDA.cpp 31015 2010-07-22 16:33:44Z michal.necasek@oracle.com $ */
 /** @file
  * DevIchIntelHD - VBox ICH Intel HD Audio Controller.
  */
@@ -789,6 +789,8 @@ DECLCALLBACK(int)hdaRegWriteGCTL(INTELHDLinkState* pState, uint32_t offset, uint
     {
         /* enter reset state*/
         hdaReset(ICH6_HDASTATE_2_DEVINS(pState));
+        GCTL(pState) &= ~HDA_REG_FIELD_FLAG_MASK(GCTL, RST);
+        //** @todo r=michaln: The device isn't supposed to respond to any writes except to this bit now.
     }
     if (u32Value & HDA_REG_FIELD_FLAG_MASK(GCTL, FSH))
     {
