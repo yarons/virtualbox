d3163503522811d84937ab7be410ecb986c72275
Alexander Eichner <alexander.eichner@oracle.com>
2010-07-26 11:04:01 +0000

    Really fix windows burn
    svn:sync-xref-src-repo-rev: r64058

--- src/VBox/Runtime/r3/socket.cpp
+++ src/VBox/Runtime/r3/socket.cpp
@@ -1,4 +1,4 @@
-/* $Id: socket.cpp 31104 2010-07-26 11:01:06Z alexander.eichner@oracle.com $ */
+/* $Id: socket.cpp 31105 2010-07-26 11:04:01Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT - Network Sockets.
  */
@@ -257,7 +257,8 @@ DECLINLINE(int) rtSocketSwitchBlockingMode(RTSOCKETINT *pThis, bool fBlocking)
     if (pThis->fBlocking != fBlocking)
     {
 #ifdef RT_OS_WINDOWS
-        if (ioctlsocket(pThis->hNative, FIONBIO, fBlocking ? 0 : 1))
+        u_long uBlocking = fBlocking ? 0 : 1;
+        if (ioctlsocket(pThis->hNative, FIONBIO, &uBlocking))
 #else
         if (fcntl(pThis->hNative, F_SETFL, O_NONBLOCK))
 #endif
