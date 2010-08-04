3294ccda47090e83a403678a10a7a12699a4b734
Sander van Leeuwen <noreply@oracle.com>
2010-08-04 15:31:04 +0000

    Keep track of the native R0 thread handle for each EMT too. Use that to find the right VCPU
    svn:sync-xref-src-repo-rev: r64410

--- src/VBox/VMM/VMMAll/VMMAll.cpp
+++ src/VBox/VMM/VMMAll/VMMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMMAll.cpp 31356 2010-08-04 12:59:29Z noreply@oracle.com $ */
+/* $Id: VMMAll.cpp 31359 2010-08-04 15:31:04Z noreply@oracle.com $ */
 /** @file
  * VMM All Contexts.
  */
@@ -60,21 +60,6 @@ VMMDECL(VMCPUID) VMMGetCpuId(PVM pVM)
     if (pVM->cCpus == 1)
         return 0;
 
-    /* Search first by host cpu id (most common case)
-     * and then by native thread id (page fusion case).
-     */
-    /* RTMpCpuId had better be cheap. */ 
-    RTCPUID idHostCpu = RTMpCpuId(); 
-
-    /** @todo optimize for large number of VCPUs when that becomes more common. */
-    for (VMCPUID idCpu = 0; idCpu < pVM->cCpus; idCpu++)
-    {
-        PVMCPU pVCpu = &pVM->aCpus[idCpu];
-
-        if (pVCpu->idHostCpu == idHostCpu)
-            return pVCpu->idCpu;
-    }
-
     /* RTThreadGetNativeSelf had better be cheap. */
     RTNATIVETHREAD hThread = RTThreadNativeSelf();
 
@@ -83,7 +68,7 @@ VMMDECL(VMCPUID) VMMGetCpuId(PVM pVM)
     {
         PVMCPU pVCpu = &pVM->aCpus[idCpu];
 
-        if (pVCpu->hNativeThread == hThread)
+        if (pVCpu->hNativeThreadR0 == hThread)
             return pVCpu->idCpu;
     }
     return NIL_VMCPUID;
@@ -114,22 +99,6 @@ VMMDECL(PVMCPU) VMMGetCpu(PVM pVM)
     if (pVM->cCpus == 1)
         return &pVM->aCpus[0];
 
-    /* Search first by host cpu id (most common case)
-     * and then by native thread id (page fusion case).
-     */
-
-    /* RTMpCpuId had better be cheap. */ 
-    RTCPUID idHostCpu = RTMpCpuId(); 
-
-    /** @todo optimize for large number of VCPUs when that becomes more common. */
-    for (VMCPUID idCpu = 0; idCpu < pVM->cCpus; idCpu++)
-    {
-        PVMCPU pVCpu = &pVM->aCpus[idCpu];
-
-        if (pVCpu->idHostCpu == idHostCpu)
-            return pVCpu;
-    }
-
     /* RTThreadGetNativeSelf had better be cheap. */
     RTNATIVETHREAD hThread = RTThreadNativeSelf();
 
@@ -138,7 +107,7 @@ VMMDECL(PVMCPU) VMMGetCpu(PVM pVM)
     {
         PVMCPU pVCpu = &pVM->aCpus[idCpu];
 
-        if (pVCpu->hNativeThread == hThread)
+        if (pVCpu->hNativeThreadR0 == hThread)
             return pVCpu;
     }
     return NULL;
