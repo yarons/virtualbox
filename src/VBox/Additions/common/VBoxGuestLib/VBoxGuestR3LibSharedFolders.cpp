2a0165e245853a7d2d7699e1a42dc1235cdf5deb
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-08-04 12:37:50 +0000

    SharedFolders/AutoMount: Added support for dynamic mount point prefix.
    svn:sync-xref-src-repo-rev: r64403

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibSharedFolders.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibSharedFolders.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR3LibSharedFolders.cpp 31130 2010-07-27 07:38:29Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxGuestR3LibSharedFolders.cpp 31355 2010-08-04 12:37:50Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxGuestR3Lib - Ring-3 Support Library for VirtualBox guest additions, shared folders.
  */
@@ -258,3 +258,29 @@ VBGLR3DECL(int) VbglR3SharedFolderGetName(uint32_t u32ClientId, uint32_t u32Root
     return rc;
 }
 
+/**
+ * Retrieves the prefix for a shared folder mount point.  If no prefix
+ * is set in the guest properties "sf_" is returned.
+ *
+ * @returns VBox status code.
+ * @param   ppszPrefix      Where to return the prefix string.  This shall be
+ *                          freed by calling RTStrFree.
+ */
+VBGLR3DECL(int) VbglR3SharedFolderGetMountPrefix(char **ppszPrefix)
+{
+    AssertPtrReturn(ppszPrefix, VERR_INVALID_POINTER);
+
+    uint32_t u32ClientIdGuestProp;
+    int rc = VbglR3GuestPropConnect(&u32ClientIdGuestProp);
+    if (RT_SUCCESS(rc))
+    {
+        rc = VbglR3GuestPropReadValueAlloc(u32ClientIdGuestProp, "/VirtualBox/GuestAdd/SharedFolders/MountPrefix", ppszPrefix);
+        if (rc == VERR_NOT_FOUND) /* No prefix set? Then set the default. */
+        {
+            if (RTStrAPrintf(ppszPrefix, "sf_"))
+                rc = VINF_SUCCESS;
+        }
+        VbglR3GuestPropDisconnect(u32ClientIdGuestProp);
+    }
+    return rc;
+}
