e7374fa43d819f409f52ff404a18e4fcc26ffd19
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-08-05 13:03:58 +0000

    path.cpp: grr. build fix
    svn:sync-xref-src-repo-rev: r64470

--- src/VBox/Runtime/r3/path.cpp
+++ src/VBox/Runtime/r3/path.cpp
@@ -1,4 +1,4 @@
-/* $Id: path.cpp 31404 2010-08-05 13:02:38Z knut.osmundsen@oracle.com $ */
+/* $Id: path.cpp 31405 2010-08-05 13:03:58Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Path Manipulation.
  */
@@ -193,7 +193,7 @@ RTR3DECL(int) RTPathGetMode(const char *pszPath, PRTFMODE pfMode)
     AssertPtrReturn(pfMode, VERR_INVALID_POINTER);
 
     RTFSOBJINFO ObjInfo;
-    rc = RTPathQueryInfoEx(pszPath, &ObjInfo, RTFSOBJATTRADD_NOTHING, RTPATH_F_FOLLOW_LINK);
+    int rc = RTPathQueryInfoEx(pszPath, &ObjInfo, RTFSOBJATTRADD_NOTHING, RTPATH_F_FOLLOW_LINK);
     if (RT_SUCCESS(rc))
         *pfMode = ObjInfo.Attr.fMode;
 
