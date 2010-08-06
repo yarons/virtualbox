f94642cd99f8430e62b84107b447d04fc007cd65
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-08-06 09:37:59 +0000

    Guest Additions version lookup/status: Fixed formatting bug.
    svn:sync-xref-src-repo-rev: r64498

--- src/VBox/Main/GuestImpl.cpp
+++ src/VBox/Main/GuestImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestImpl.cpp 31364 2010-08-04 16:44:20Z andreas.loeffler@oracle.com $ */
+/* $Id: GuestImpl.cpp 31433 2010-08-06 09:37:59Z andreas.loeffler@oracle.com $ */
 
 /** @file
  *
@@ -1504,7 +1504,7 @@ void Guest::setAdditionsInfo2(Bstr aAdditionsVersion, Bstr aVersionName)
     AutoWriteLock alock(this COMMA_LOCKVAL_SRC_POS);
 
     if (!aVersionName.isEmpty())
-        mData.mAdditionsVersion = aAdditionsVersion + " " + aVersionName;
+        mData.mAdditionsVersion = BstrFmt("%ls %ls", aAdditionsVersion.raw(), aVersionName.raw());
     else
         mData.mAdditionsVersion = aAdditionsVersion;
 }
