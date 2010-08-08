dfe64d8d63087f6279096021d959e3442ee75c31
Alexander Eichner <alexander.eichner@oracle.com>
2010-08-08 13:33:35 +0000

    Build fix
    svn:sync-xref-src-repo-rev: r64527

--- src/VBox/Runtime/r3/win/poll-win.cpp
+++ src/VBox/Runtime/r3/win/poll-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: poll-win.cpp 31453 2010-08-08 13:30:35Z alexander.eichner@oracle.com $ */
+/* $Id: poll-win.cpp 31454 2010-08-08 13:33:35Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT - Polling I/O Handles, Windows Implementation.
  *
@@ -571,7 +571,7 @@ RTDECL(int) RTPollSetEventsChange(RTPOLLSET hPollSet, uint32_t id, uint32_t fEve
     int         rc = VERR_POLL_HANDLE_ID_NOT_FOUND;
     uint32_t    i  = pThis->cHandles;
     while (i-- > 0)
-        if (pThis->paHandles[i].id == id)
+        if (pThis->aHandles[i].id == id)
         {
             pThis->aHandles[i].fEvents = fEvents;
             rc = VINF_SUCCESS;
