934aa769548672fdf462d725d50981dc208c3400
Alexander Eichner <alexander.eichner@oracle.com>
2010-08-08 15:11:02 +0000

    DrvVD: Don't change the events if the socket is not connected
    svn:sync-xref-src-repo-rev: r64530

--- src/VBox/Devices/Storage/DrvVD.cpp
+++ src/VBox/Devices/Storage/DrvVD.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvVD.cpp 31456 2010-08-08 13:46:41Z alexander.eichner@oracle.com $ */
+/* $Id: DrvVD.cpp 31457 2010-08-08 15:11:02Z alexander.eichner@oracle.com $ */
 /** @file
  * DrvVD - Generic VBox disk media driver.
  */
@@ -1191,7 +1191,8 @@ static DECLCALLBACK(int) drvvdTcpSelectOneEx(VDSOCKET Sock, uint32_t fEvents,
 
     *pfEvents = 0;
 
-    if (pSockInt->fEventsOld != fEvents)
+    if (   pSockInt->fEventsOld != fEvents
+        && pSockInt->hSocket != NIL_RTSOCKET)
     {
         uint32_t fPollEvents = 0;
 
@@ -1239,7 +1240,7 @@ static DECLCALLBACK(int) drvvdTcpSelectOneEx(VDSOCKET Sock, uint32_t fEvents,
             size_t cbRead = 0;
             uint8_t abBuf[10];
             Assert(id == VDSOCKET_POLL_ID_PIPE);
-            Assert((fEvents & RTPOLL_EVT_VALID_MASK) == RTPOLL_EVT_READ);
+            Assert((fEventsRecv & RTPOLL_EVT_VALID_MASK) == RTPOLL_EVT_READ);
 
             /* We got interrupted, drain the pipe. */
             rc = RTPipeRead(pSockInt->hPipeR, abBuf, sizeof(abBuf), &cbRead);
