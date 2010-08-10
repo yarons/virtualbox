418e308e3efbcb474cef519450ac6b81ba67f3ac
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-08-10 11:47:25 +0000

    VMMDev: understandable comment.
    svn:sync-xref-src-repo-rev: r64620

--- src/VBox/Devices/VMMDev/VMMDev.cpp
+++ src/VBox/Devices/VMMDev/VMMDev.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMMDev.cpp 31523 2010-08-10 11:46:08Z knut.osmundsen@oracle.com $ */
+/* $Id: VMMDev.cpp 31524 2010-08-10 11:47:25Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMMDev - Guest <-> VMM/Host communication device.
  */
@@ -424,8 +424,8 @@ static DECLCALLBACK(int) vmmdevRequestHandler(PPDMDEVINS pDevIns, void *pvUser, 
 
     Log2(("VMMDev request issued: %d\n", requestHeader.requestType));
 
-    /* Because VMMDevReq_ReportGuestInfo is sent last in the first information chain
-     * from the guest also check for VMMDevReq_ReportGuestInfo2. */
+    /* Newer additions starts with VMMDevReq_ReportGuestInfo2, older additions
+       started with VMMDevReq_ReportGuestInfo. */
     if (   !pThis->fu32AdditionsOk
         && requestHeader.requestType != VMMDevReq_ReportGuestInfo2
         && requestHeader.requestType != VMMDevReq_ReportGuestInfo)
