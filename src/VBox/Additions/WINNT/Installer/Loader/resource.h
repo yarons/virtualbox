a16f8d5705db4522738e8196bb6a3e2b5aa37a3b
Frank Mehnert <noreply@oracle.com>
2010-08-13 11:17:20 +0000

    Windows Additions: export shared folders and installer to OSE
    svn:sync-xref-src-repo-rev: r64770

---
+++ src/VBox/Additions/WINNT/Installer/Loader/resource.h
@@ -0,0 +1,13 @@
+/* $Id: resource.h 31634 2010-08-13 11:17:20Z noreply@oracle.com $ */
+/** @file
+ * AutoStartMenu - resource header file.
+ */
+
+/*
+ * Copyright (C) 2008 Oracle Corporation
+ *
+ * Oracle Corporation confidential
+ * All rights reserved
+ */
+
+#define IDI_VIRTUALBOX 101
