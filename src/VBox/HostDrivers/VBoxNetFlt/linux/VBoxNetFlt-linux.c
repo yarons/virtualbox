3c173eb873256e59e2670dff100e68af59b5e5f8
Frank Mehnert <noreply@oracle.com>
2010-08-16 15:20:05 +0000

    vboxnetflt: typo
    svn:sync-xref-src-repo-rev: r64848

--- src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
+++ src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFlt-linux.c 31702 2010-08-16 15:15:39Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxNetFlt-linux.c 31703 2010-08-16 15:20:05Z noreply@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Linux Specific Code.
  */
@@ -226,7 +226,7 @@ unsigned dev_get_flags(const struct net_device *dev)
 #define QDISC_IS_BUSY(dev, qdisc) (test_bit(__QDISC_STATE_RUNNING, &qdisc->state) || \
                                    test_bit(__QDISC_STATE_SCHED, &qdisc->state))
 #else /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 36) */
-#define QDISC_IS_BUSY(dev, qdisc) (qdisc_is_running(q) || \
+#define QDISC_IS_BUSY(dev, qdisc) (qdisc_is_running(qdisc) || \
                                    test_bit(__QDISC_STATE_SCHED, &qdisc->state))
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 36) */
 
