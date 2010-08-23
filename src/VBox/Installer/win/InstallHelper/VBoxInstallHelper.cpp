3c52a373c1ae80d7ffb815a65683187c67be4ae9
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-08-23 12:13:39 +0000

    VBoxInstallerHelper/win: Move _stprintf_s to common code base.
    svn:sync-xref-src-repo-rev: r65065

--- src/VBox/Installer/win/InstallHelper/VBoxInstallHelper.cpp
+++ src/VBox/Installer/win/InstallHelper/VBoxInstallHelper.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxInstallHelper.cpp 31856 2010-08-23 12:11:12Z noreply@oracle.com $ */
+/* $Id: VBoxInstallHelper.cpp 31857 2010-08-23 12:13:39Z andreas.loeffler@oracle.com $ */
 /** @file
 * VBoxInstallHelper - Various helper routines for Windows host installer.
 */
@@ -58,18 +58,6 @@ BOOL APIENTRY DllMain(HANDLE hModule,
 {
     return TRUE;
 }
-
-#if (_MSC_VER < 1400) /* Provide _stprintf_s to VC < 8.0. */
-int _stprintf_s(TCHAR *buffer, size_t cbBuffer, const TCHAR *format, ...)
-{
-    int ret;
-    va_list args;
-    va_start(args, format);
-    ret = _vsntprintf(buffer, cbBuffer, format, args);
-    va_end(args);
-    return ret;
-}
-#endif
 
 void LogString(MSIHANDLE hInstall, TCHAR* szString, ...)
 {
