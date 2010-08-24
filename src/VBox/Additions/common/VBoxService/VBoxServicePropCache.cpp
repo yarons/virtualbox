82e3e027e13ba32dcba0c379f6f9760f2c415ce3
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-08-24 08:08:32 +0000

    VBoxService/PropCache: Bugfix.
    svn:sync-xref-src-repo-rev: r65101

--- src/VBox/Additions/common/VBoxService/VBoxServicePropCache.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServicePropCache.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServicePropCache.cpp 31881 2010-08-24 07:14:36Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxServicePropCache.cpp 31893 2010-08-24 08:08:32Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxServicePropCache - Guest property cache.
  */
@@ -98,16 +98,18 @@ int vboxServicePropCacheWritePropF(uint32_t u32ClientId, const char *pszName, ui
         char *pszValue;
         if (RTStrAPrintfV(&pszValue, pszValueFormat, va) >= 0)
         {
-            /*
-             * Because a value can be temporary we have to make sure it also
-             * gets deleted when the property cache did not have the chance to
-             * gracefully clean it up (due to a hard VM reset etc), so set this
-             * guest property using the TRANSIENT flag.
-             */
-            rc = VbglR3GuestPropWrite(u32ClientId, pszName, pszValue,
-                                        (fFlags & VBOXSERVICEPROPCACHEFLAG_TEMPORARY)
-                                      ? "TRANSIENT"
-                                      : NULL);
+            if (fFlags & VBOXSERVICEPROPCACHEFLAG_TEMPORARY)
+            {
+                /*
+                 * Because a value can be temporary we have to make sure it also
+                 * gets deleted when the property cache did not have the chance to
+                 * gracefully clean it up (due to a hard VM reset etc), so set this
+                 * guest property using the TRANSIENT flag.
+                 */
+                rc = VbglR3GuestPropWrite(u32ClientId, pszName, pszValue, "TRANSIENT");
+            }
+            else
+                rc = VbglR3GuestPropWriteValue(u32ClientId, pszName, pszValue);
             RTStrFree(pszValue);
         }
         va_end(va);
