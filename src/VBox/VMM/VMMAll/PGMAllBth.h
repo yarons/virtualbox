1adcd16d1face5092963c5e4ee495d092e949229
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-08-26 13:18:54 +0000

    Safe X86_PDPE_PG_MASK -> X86_PDPE_PG_MASK_FULL conversions.
    svn:sync-xref-src-repo-rev: r65237

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1,4 +1,4 @@
-/* $Id: PGMAllBth.h 31989 2010-08-26 12:43:18Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMAllBth.h 31994 2010-08-26 13:18:54Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox - Page Manager, Shadow+Guest Paging Template - All context code.
  *
@@ -4396,7 +4396,7 @@ PGM_BTH_DECL(int, MapCR3)(PVMCPU pVCpu, RTGCPHYS GCPhysCR3)
                 {
                     RTHCPTR     HCPtr;
                     RTHCPHYS    HCPhys;
-                    RTGCPHYS    GCPhys = pGuestPDPT->a[i].u & X86_PDPE_PG_MASK;
+                    RTGCPHYS    GCPhys = pGuestPDPT->a[i].u & X86_PDPE_PG_MASK_FULL;
                     pgmLock(pVM);
                     PPGMPAGE    pPage  = pgmPhysGetPage(&pVM->pgm.s, GCPhys);
                     AssertReturn(pPage, VERR_INTERNAL_ERROR_2);
