a3ae6bdb1bfe540551cbee54a81cd1155f22570c
Frank Mehnert <noreply@oracle.com>
2010-08-27 07:01:42 +0000

    OSE header fix
    svn:sync-xref-src-repo-rev: r65271

--- src/VBox/Runtime/include/internal/ldrElf.h
+++ src/VBox/Runtime/include/internal/ldrElf.h
@@ -1,4 +1,4 @@
-/* $Id: ldrElf.h 31965 2010-08-25 15:27:55Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: ldrElf.h 32019 2010-08-27 07:01:42Z noreply@oracle.com $ */
 /** @file
  * ELF types, current architecture.
  */
@@ -13,6 +13,15 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
  */
 
 
