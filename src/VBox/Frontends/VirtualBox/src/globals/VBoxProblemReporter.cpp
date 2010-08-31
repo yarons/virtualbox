079de974307bb18e7b66be73701e941aa1c268ff
Sergey Dubov <sergey.dubov@oracle.com>
2010-08-31 12:24:38 +0000

    FE/Qt: Problem Reporter: Fixed segfaults while trying to acquire data from (or just delete) QIMessageBox already deleted by it's parent.
    svn:sync-xref-src-repo-rev: r65411

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxProblemReporter.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxProblemReporter.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxProblemReporter.cpp 31563 2010-08-11 12:14:50Z sergey.dubov@oracle.com $ */
+/* $Id: VBoxProblemReporter.cpp 32136 2010-08-31 12:24:38Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -198,8 +198,8 @@ int VBoxProblemReporter::message (QWidget *aParent, Type aType, const QString &a
             break;
     }
 
-    QIMessageBox *box = new QIMessageBox (title, aMessage, icon, aButton1, aButton2,
-                                          aButton3, aParent, aAutoConfirmId);
+    QPointer<QIMessageBox> box = new QIMessageBox (title, aMessage, icon, aButton1, aButton2,
+                                                   aButton3, aParent, aAutoConfirmId);
 
     if (!aText1.isNull())
         box->setButtonText (0, aText1);
@@ -221,14 +221,15 @@ int VBoxProblemReporter::message (QWidget *aParent, Type aType, const QString &a
 
     if (aAutoConfirmId)
     {
-        if (box->isFlagChecked())
+        if (box && box->isFlagChecked())
         {
             msgs << aAutoConfirmId;
             vbox.SetExtraData (VBoxDefs::GUI_SuppressMessages, msgs.join (","));
         }
     }
 
-    delete box;
+    if (box)
+        delete box;
 
     return rc;
 }
