e7bcef424f00d083168a4d986673357c8d9c9c9d
Mikhail Sennikovsky <noreply@oracle.com>
2010-09-01 16:37:20 +0000

    wine: shared render target support; wddm/d3d: aero multi-monitor fixes (still buggy)
    svn:sync-xref-src-repo-rev: r65468

--- src/VBox/Additions/WINNT/Graphics/Wine/wined3d/vboxsharedrc.h
+++ src/VBox/Additions/WINNT/Graphics/Wine/wined3d/vboxsharedrc.h
@@ -1,4 +1,4 @@
-/* $Id: vboxsharedrc.h 32061 2010-08-27 17:00:41Z noreply@oracle.com $ */
+/* $Id: vboxsharedrc.h 32184 2010-09-01 16:37:20Z noreply@oracle.com $ */
 /** @file
  *
  * VBox extension to Wine D3D
@@ -24,6 +24,12 @@
 #define VBOXSHRC_GET_SHAREHANDLE(_o) ((_o)->resource.sharerc_handle)
 #define VBOXSHRC_SET_SHAREHANDLE(_o, _h) ((_o)->resource.sharerc_handle = (_h))
 #define VBOXSHRC_SET_INITIALIZED(_o) (VBOXSHRC_GET_SHAREFLAFS(_o) |= VBOXSHRC_F_INITIALIZED)
+#define VBOXSHRC_COPY_SHAREDATA(_oDst, _oSrc) do { \
+        VBOXSHRC_GET_SHAREFLAFS(_oDst) = VBOXSHRC_GET_SHAREFLAFS(_oSrc); \
+        VBOXSHRC_SET_SHAREHANDLE(_oDst, VBOXSHRC_GET_SHAREFLAFS(_oSrc)); \
+    } while (0)
+#define VBOXSHRC_SET_SHARED(_o) (VBOXSHRC_GET_SHAREFLAFS(_o) |= VBOXSHRC_F_SHARED)
+#define VBOXSHRC_SET_SHARED_OPENED(_o) (VBOXSHRC_GET_SHAREFLAFS(_o) |= VBOXSHRC_F_SHARED_OPENED)
 #define VBOXSHRC_IS_SHARED(_o) (!!(VBOXSHRC_GET_SHAREFLAFS(_o) & VBOXSHRC_F_SHARED))
 #define VBOXSHRC_IS_SHARED_OPENED(_o) (!!(VBOXSHRC_GET_SHAREFLAFS(_o) & VBOXSHRC_F_SHARED_OPENED))
 #define VBOXSHRC_IS_INITIALIZED(_o) (!!(VBOXSHRC_GET_SHAREFLAFS(_o) & VBOXSHRC_F_INITIALIZED))
