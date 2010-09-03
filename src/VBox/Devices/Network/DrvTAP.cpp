9e94070be76da4045c6d34ccf04f009db2892692
Frank Mehnert <noreply@oracle.com>
2010-09-03 06:35:13 +0000

    warning
    svn:sync-xref-src-repo-rev: r65535

--- src/VBox/Devices/Network/DrvTAP.cpp
+++ src/VBox/Devices/Network/DrvTAP.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvTAP.cpp 32139 2010-08-31 12:33:45Z noreply@oracle.com $ */
+/* $Id: DrvTAP.cpp 32226 2010-09-03 06:35:13Z noreply@oracle.com $ */
 /** @file
  * DrvTAP - Universial TAP network transport driver.
  */
@@ -282,6 +282,7 @@ static DECLCALLBACK(int) drvTAPNetworkUp_SendBuf(PPDMINETWORKUP pInterface, PPDM
         uint8_t const  *pbFrame = (uint8_t const *)pSgBuf->aSegs[0].pvSeg;
         PCPDMNETWORKGSO pGso    = (PCPDMNETWORKGSO)pSgBuf->pvUser;
         uint32_t const  cSegs   = PDMNetGsoCalcSegmentCount(pGso, pSgBuf->cbUsed);  Assert(cSegs > 1);
+        rc = VINF_SUCCESS;
         for (size_t iSeg = 0; iSeg < cSegs; iSeg++)
         {
             uint32_t cbSegFrame;
