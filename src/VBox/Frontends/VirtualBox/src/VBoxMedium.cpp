02a5d1494042df28e3f006e5d3817ba34c481a3b
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2010-09-15 17:26:45 +0000

    year
    svn:sync-xref-src-repo-rev: r65914

--- src/VBox/Frontends/VirtualBox/src/VBoxMedium.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxMedium.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMedium.cpp 32531 2010-09-15 17:04:48Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxMedium.cpp 32532 2010-09-15 17:26:45Z klaus.espenlaub@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -6,7 +6,7 @@
  */
 
 /*
- * Copyright (C) 2009 Oracle Corporation
+ * Copyright (C) 2009-2010 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
