b919d7e9c4341ea9f0432ae0ba5dfe8809068509
Sander van Leeuwen <noreply@oracle.com>
2010-09-16 08:56:59 +0000

    Disallow virtual handler registration for VT-x and AMD-V.
    svn:sync-xref-src-repo-rev: r65923

--- src/VBox/VMM/PGMHandler.cpp
+++ src/VBox/VMM/PGMHandler.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMHandler.cpp 30326 2010-06-21 12:35:33Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMHandler.cpp 32540 2010-09-16 08:56:59Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager / Monitor, Access Handlers.
  */
@@ -234,6 +234,10 @@ VMMR3DECL(int) PGMR3HandlerVirtualRegister(PVM pVM, PGMVIRTHANDLERTYPE enmType, 
     LogFlow(("PGMR3HandlerVirtualRegisterEx: enmType=%d GCPtr=%RGv GCPtrLast=%RGv pszHandlerRC=%p:{%s} pszModRC=%p:{%s} pszDesc=%s\n",
              enmType, GCPtr, GCPtrLast, pszHandlerRC, pszHandlerRC, pszModRC, pszModRC, pszDesc));
 
+    /* Not supported/relevant for VT-x and AMD-V. */
+    if (HWACCMIsEnabled(pVM))
+        return VERR_NOT_IMPLEMENTED;
+
     /*
      * Validate input.
      */
@@ -282,6 +286,10 @@ VMMDECL(int) PGMR3HandlerVirtualRegisterEx(PVM pVM, PGMVIRTHANDLERTYPE enmType, 
 {
     Log(("PGMR3HandlerVirtualRegister: enmType=%d GCPtr=%RGv GCPtrLast=%RGv pfnInvalidateR3=%RHv pfnHandlerR3=%RHv pfnHandlerRC=%RRv pszDesc=%s\n",
          enmType, GCPtr, GCPtrLast, pfnInvalidateR3, pfnHandlerR3, pfnHandlerRC, pszDesc));
+
+    /* Not supported/relevant for VT-x and AMD-V. */
+    if (HWACCMIsEnabled(pVM))
+        return VERR_NOT_IMPLEMENTED;
 
     /*
      * Validate input.
