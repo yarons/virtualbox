65fd5749675fef8217a752c2fc29882fb641f611
Christian Poetzsch <noreply@oracle.com>
2010-09-16 15:42:51 +0000

    Runtime: fix burn
    svn:sync-xref-src-repo-rev: r65962

--- src/VBox/Runtime/common/checksum/RTSha1Digest.cpp
+++ src/VBox/Runtime/common/checksum/RTSha1Digest.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTSha1Digest.cpp 32568 2010-09-16 15:26:13Z noreply@oracle.com $ */
+/* $Id: RTSha1Digest.cpp 32569 2010-09-16 15:42:51Z noreply@oracle.com $ */
 /** @file
  * IPRT - SHA1 digest creation
  */
@@ -40,7 +40,7 @@
 #include <openssl/sha.h>
 
 
-RTR3DECL(int) RTSha1Digest(void* pvBuf, size_t cbBuf, char **ppszDigest, FNRTPROGRESS pfnProgressCallback, void *pvUser)
+RTR3DECL(int) RTSha1Digest(void* pvBuf, size_t cbBuf, char **ppszDigest, PFNRTPROGRESS pfnProgressCallback, void *pvUser)
 {
     /* Validate input */
     AssertPtrReturn(pvBuf, VERR_INVALID_POINTER);
