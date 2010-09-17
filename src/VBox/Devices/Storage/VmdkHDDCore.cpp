b744aa547dd6bf94125bf2f511cd43e897db0680
Alexander Eichner <alexander.eichner@oracle.com>
2010-09-17 16:46:14 +0000

    VMDK: Fix return code for vmdksCheckIfValid
    svn:sync-xref-src-repo-rev: r66022

--- src/VBox/Devices/Storage/VmdkHDDCore.cpp
+++ src/VBox/Devices/Storage/VmdkHDDCore.cpp
@@ -1,4 +1,4 @@
-/* $Id: VmdkHDDCore.cpp 32615 2010-09-17 15:49:42Z alexander.eichner@oracle.com $ */
+/* $Id: VmdkHDDCore.cpp 32618 2010-09-17 16:46:14Z alexander.eichner@oracle.com $ */
 /** @file
  * VMDK disk image, core code.
  */
@@ -5658,7 +5658,7 @@ static int vmdksCheckIfValid(const char *pszFilename, PVDINTERFACE pVDIfsDisk,
 
     /* Always return failure, to avoid opening other VMDK files via this
      * special VMDK streamOptimized format backend. */
-    rc = VERR_NOT_SUPPORTED;
+    rc = VERR_VD_VMDK_INVALID_HEADER;
 
 out:
     LogFlowFunc(("returns %Rrc\n", rc));
