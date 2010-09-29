61e0236d143c766a374db0ff1c44d6f44f8752ac
Vasily Levchenko <noreply@oracle.com>
2010-09-29 05:41:11 +0000

    Audio/HDA: some todos.
    svn:sync-xref-src-repo-rev: r66255

--- src/VBox/Devices/Audio/DevCodec.cpp
+++ src/VBox/Devices/Audio/DevCodec.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevCodec.cpp 32804 2010-09-29 05:39:06Z noreply@oracle.com $ */
+/* $Id: DevCodec.cpp 32805 2010-09-29 05:41:11Z noreply@oracle.com $ */
 /** @file
  * DevCodec - VBox ICH Intel HD Audio Codec.
  */
@@ -754,11 +754,10 @@ static int codecSetPinSense(struct CODECState *pState, uint32_t cmd, uint64_t *p
     }
     *pResp = 0;
     uint32_t *pu32Reg = NULL;
-    //** @todo r=michaln: Copy/paste bug? This can't be F08_parm!
     if (codecIsPortNode(pState, CODEC_NID(cmd)))
-        pu32Reg = &pState->pNodes[CODEC_NID(cmd)].port.u32F08_param;
+        pu32Reg = &pState->pNodes[CODEC_NID(cmd)].port.u32F09_param;
     else if (codecIsDigInPinNode(pState, CODEC_NID(cmd)))
-        pu32Reg = &pState->pNodes[CODEC_NID(cmd)].digin.u32F08_param;
+        pu32Reg = &pState->pNodes[CODEC_NID(cmd)].digin.u32F09_param;
     Assert(pu32Reg);
     if(pu32Reg)
         codecSetRegisterU8(pu32Reg, cmd, 0);
