25c9e57bfa4625a5a349ae6988ab431491412ec2
Sander van Leeuwen <noreply@oracle.com>
2010-10-01 14:53:25 +0000

    Annoying assertion
    svn:sync-xref-src-repo-rev: r66345

--- src/VBox/VMM/VMM.cpp
+++ src/VBox/VMM/VMM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMM.cpp 32848 2010-09-30 14:40:06Z noreply@oracle.com $ */
+/* $Id: VMM.cpp 32869 2010-10-01 14:53:25Z noreply@oracle.com $ */
 /** @file
  * VMM - The Virtual Machine Monitor Core.
  */
@@ -1267,7 +1267,9 @@ VMMR3DECL(int) VMMR3HwAccRunGC(PVM pVM, PVMCPU pVCpu)
 #endif
         } while (rc == VINF_EM_RAW_INTERRUPT_HYPER);
 
+#if 0 /* todo triggers too often */
         Assert(!VMCPU_FF_ISSET(pVCpu, VMCPU_FF_TO_R3));
+#endif
 
 #ifdef LOG_ENABLED
         /*
