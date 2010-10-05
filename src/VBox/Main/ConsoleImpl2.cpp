d4ce948abd3378eefdda65599b7c8efe747056bd
Nikolay Igotty <noreply@oracle.com>
2010-10-05 12:33:56 +0000

    VMM: hyper heap size selection
    svn:sync-xref-src-repo-rev: r66393

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl2.cpp 32908 2010-10-05 11:52:17Z noreply@oracle.com $ */
+/* $Id: ConsoleImpl2.cpp 32910 2010-10-05 12:33:56Z noreply@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation
  *
@@ -308,23 +308,6 @@ static int getSmcDeviceKey(IMachine *pMachine, BSTR *aKey, bool *pfGetKeyFromRea
 #endif
 
     return rc;
-}
-
-static uint32_t computeHyperHeapSize(ChipsetType_T chipsetType, uint32_t cCpus, bool fHwVirtExtForced)
-{
-    uint32_t singleCpuBaseSize;
-
-    if (cCpus > 1)
-        return _2M + cCpus * _64K;
-
-    if (chipsetType == ChipsetType_PIIX3)
-    {
-        /* Size must be kept like this for saved state compatibility */
-        return fHwVirtExtForced ? 640*_1K : 1280*_1K;
-    }
-
-
-    return 1280*_1K;
 }
 
 class ConfigError : public iprt::Error
@@ -699,9 +682,8 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
          * MM values.
          */
         PCFGMNODE pMM;
-        uint32_t cbHyperHeap = computeHyperHeapSize(chipsetType, cCpus, fHwVirtExtForced);
         InsertConfigNode(pRoot, "MM", &pMM);
-        InsertConfigInteger(pMM, "cbHyperHeap", cbHyperHeap);
+        InsertConfigInteger(pMM, "CanUseLargerHeap", chipsetType == ChipsetType_ICH9);
 
         /*
          * Hardware virtualization settings.
