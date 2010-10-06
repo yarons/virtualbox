266a4d338031e5ee110999a62914f79b716809c3
Nikolay Igotty <noreply@oracle.com>
2010-10-06 09:48:26 +0000

    LSI logic: continue, even if no MSI on chipset
    svn:sync-xref-src-repo-rev: r66424

--- src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
+++ src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevLsiLogicSCSI.cpp 32935 2010-10-06 09:28:42Z noreply@oracle.com $ */
+/* $Id: DevLsiLogicSCSI.cpp 32938 2010-10-06 09:48:26Z noreply@oracle.com $ */
 /** @file
  * VBox storage devices: LsiLogic LSI53c1030 SCSI controller.
  */
@@ -4875,9 +4875,11 @@ static DECLCALLBACK(int) lsilogicConstruct(PPDMDEVINS pDevIns, int iInstance, PC
     aMsiReg.iNextOffset = 0x0;
     aMsiReg.iMsiFlags = 0;
     rc = PDMDevHlpPCIRegisterMsi(pDevIns, &aMsiReg);
-    AssertRC(rc);
     if (RT_FAILURE (rc))
-        return rc;
+    {
+        LogRel(("Chipset cannot do MSI: %Rrc\n", rc));
+        /* That's OK, we can work without MSI */
+    }
 #endif
 
     rc = PDMDevHlpPCIIORegionRegister(pDevIns, 0, LSILOGIC_PCI_SPACE_IO_SIZE, PCI_ADDRESS_SPACE_IO, lsilogicMap);
