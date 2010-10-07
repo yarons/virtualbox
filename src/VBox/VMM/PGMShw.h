35afcc363c9c41fc756746b88b2e1ed9a07eeeb2
Sander van Leeuwen <noreply@oracle.com>
2010-10-07 14:17:45 +0000

    Do *not* unlock the nested root page when exiting the shadow mode. Triggers an assertion when trying to free this page later on.
    svn:sync-xref-src-repo-rev: r66490

--- src/VBox/VMM/PGMShw.h
+++ src/VBox/VMM/PGMShw.h
@@ -1,4 +1,4 @@
-/* $Id: PGMShw.h 32036 2010-08-27 10:14:39Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMShw.h 32980 2010-10-07 14:17:45Z noreply@oracle.com $ */
 /** @file
  * VBox - Page Manager / Monitor, Shadow Paging Template.
  */
@@ -246,8 +246,10 @@ PGM_SHW_DECL(int, Exit)(PVMCPU pVCpu)
 
         pgmLock(pVM);
 
-        /* Mark the page as unlocked; allow flushing again. */
-        pgmPoolUnlockPage(pPool, pVCpu->pgm.s.CTX_SUFF(pShwPageCR3));
+        /* Do *not* unlock this page as we have two of them floating around in the 32-bit host & 64-bit guest case.
+         * We currently assert when you try to free one of them; don't bother to really allow this.
+         */
+        /* pgmPoolUnlockPage(pPool, pVCpu->pgm.s.CTX_SUFF(pShwPageCR3)); */
 
         pgmPoolFreeByPage(pPool, pVCpu->pgm.s.CTX_SUFF(pShwPageCR3), pVCpu->pgm.s.iShwUser, pVCpu->pgm.s.iShwUserTable);
         pVCpu->pgm.s.pShwPageCR3R3 = 0;
