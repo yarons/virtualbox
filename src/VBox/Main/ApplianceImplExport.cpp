6d6b4b2575c99196e9640eb22baaead718e53391
Christian Poetzsch <noreply@oracle.com>
2010-10-13 08:57:44 +0000

    Main-OVF: disable digest creation for the manifest file
    svn:sync-xref-src-repo-rev: r66619

--- src/VBox/Main/ApplianceImplExport.cpp
+++ src/VBox/Main/ApplianceImplExport.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImplExport.cpp 33088 2010-10-13 07:49:04Z klaus.espenlaub@oracle.com $ */
+/* $Id: ApplianceImplExport.cpp 33090 2010-10-13 08:57:44Z noreply@oracle.com $ */
 /** @file
  *
  * IAppliance and IVirtualSystem COM class implementations.
@@ -1864,6 +1864,8 @@ HRESULT Appliance::writeFSImpl(TaskOVF *pTask, AutoWriteLockBase& writeLock, PVD
                 throw setError(VBOX_E_FILE_ERROR,
                                tr("Could not create manifest file '%s' (%Rrc)"),
                                strMfFileName.c_str(), vrc);
+            /* Disable digest creation for the manifest file. */
+            pStorage->fCreateDigest = false;
             /* Write the manifest file to disk. */
             vrc = Sha1WriteBuf(strMfFilePath.c_str(), pvBuf, cbSize, pCallbacks, pStorage);
             RTMemFree(pvBuf);
