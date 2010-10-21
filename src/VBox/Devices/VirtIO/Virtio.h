f52580d3cdc3ed82d99eeae15996aecfe0f307a4
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2010-10-21 20:34:14 +0000

    virtio-net: large (GSO) receive packet support (#4807)
    svn:sync-xref-src-repo-rev: r66905

--- src/VBox/Devices/VirtIO/Virtio.h
+++ src/VBox/Devices/VirtIO/Virtio.h
@@ -1,4 +1,4 @@
-/* $Id: Virtio.h 28800 2010-04-27 08:22:32Z noreply@oracle.com $ */
+/* $Id: Virtio.h 33325 2010-10-21 20:34:14Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * Virtio.h - Virtio Declarations
  */
@@ -304,7 +304,7 @@ DECLINLINE(uint16_t) vringReadAvailIndex(PVPCISTATE pState, PVRING pVRing)
 }
 
 bool vqueueGet(PVPCISTATE pState, PVQUEUE pQueue, PVQUEUEELEM pElem);
-void vqueuePut(PVPCISTATE pState, PVQUEUE pQueue, PVQUEUEELEM pElem, uint32_t uLen);
+void vqueuePut(PVPCISTATE pState, PVQUEUE pQueue, PVQUEUEELEM pElem, uint32_t uLen, uint32_t uReserved = 0);
 void vqueueNotify(PVPCISTATE pState, PVQUEUE pQueue);
 void vqueueSync(PVPCISTATE pState, PVQUEUE pQueue);
 
