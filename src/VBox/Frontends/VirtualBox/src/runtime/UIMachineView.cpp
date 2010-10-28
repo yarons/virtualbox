d8931e0ca08a375b6c352e25ead6ff80fd5d1b0c
Christian Poetzsch <noreply@oracle.com>
2010-10-28 17:39:48 +0000

    FE/Qt4: shadow var
    svn:sync-xref-src-repo-rev: r67185

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineView.cpp 32741 2010-09-23 23:21:52Z knut.osmundsen@oracle.com $ */
+/* $Id: UIMachineView.cpp 33579 2010-10-28 17:39:48Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -387,7 +387,7 @@ void UIMachineView::prepareFrameBuffer()
      * framebuffer size. On X11 this will be additional done when the machine
      * state was 'saved'. */
     if (session().GetMachine().GetState() == KMachineState_Saved)
-        QSize size = guestSizeHint();
+        size = guestSizeHint();
 #endif /* Q_WS_X11 */
     /* If there is a preview image saved, we will resize the framebuffer to the
      * size of that image. */
@@ -519,10 +519,10 @@ QSize UIMachineView::sizeHint() const
     // TODO: Fix all DEBUGGER stuff!
     /* HACK ALERT! Really ugly workaround for the resizing to 9x1 done by DevVGA if provoked before power on. */
     QSize fb(m_pFrameBuffer->width(), m_pFrameBuffer->height());
-    if (fb.width() < 16 || fb.height() < 16) 
+    if (fb.width() < 16 || fb.height() < 16)
     {
         CMachine machine = uisession()->session().GetMachine();
-        if (   vboxGlobal().isStartPausedEnabled() 
+        if (   vboxGlobal().isStartPausedEnabled()
             || vboxGlobal().isDebuggerAutoShowEnabled(machine))
         fb = QSize(640, 480);
     }
