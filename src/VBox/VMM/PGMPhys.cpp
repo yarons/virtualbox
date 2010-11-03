065671dfc4599b45fc8784ab03a7e6017d65a2dd
Sander van Leeuwen <noreply@oracle.com>
2010-11-03 13:35:35 +0000

    Allocation timeout > 1 second means immediate disabling of large page allocation.
    svn:sync-xref-src-repo-rev: r67355

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 33725 2010-11-03 13:31:24Z noreply@oracle.com $ */
+/* $Id: PGMPhys.cpp 33726 2010-11-03 13:35:35Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -3924,16 +3924,18 @@ VMMR3DECL(int) PGMR3PhysAllocateLargeHandyPage(PVM pVM, RTGCPHYS GCPhys)
     if (RT_SUCCESS(rc))
     {
         static uint32_t cTimeOut = 0;
+        uint64_t u64TimeStampDelta = u64TimeStamp2 - u64TimeStamp1;
 
-        if (u64TimeStamp2 - u64TimeStamp1 > 100)
+        if (u64TimeStampDelta > 100)
         {
             STAM_COUNTER_INC(&pVM->pgm.s.CTX_SUFF(pStats)->StatLargePageOverflow);
-            if (++cTimeOut > 10)
+            if (    ++cTimeOut > 10
+                ||  u64TimeStampDelta > 1000 /* more than one second forces an early retirement from allocating large pages. */)
             {
                 /* If repeated attempts to allocate a large page takes more than 100 ms, then we fall back to normal 4k pages.
                  * E.g. Vista 64 tries to move memory around, which takes a huge amount of time.
                  */
-                LogRel(("PGMR3PhysAllocateLargePage: allocating large pages takes too long (last attempt %d ms); DISABLE\n", u64TimeStamp2 - u64TimeStamp1));
+                LogRel(("PGMR3PhysAllocateLargePage: allocating large pages takes too long (last attempt %d ms; nr of timeouts %d); DISABLE\n", u64TimeStampDelta, cTimeOut));
                 PGMSetLargePageUsage(pVM, false);
             }
         }
