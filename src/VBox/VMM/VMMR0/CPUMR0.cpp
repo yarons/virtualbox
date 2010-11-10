8fdac6808e0ff9768465a83d22863bf9cb61bd92
Frank Mehnert <noreply@oracle.com>
2010-11-10 15:50:41 +0000

    removed logging
    svn:sync-xref-src-repo-rev: r67598

--- src/VBox/VMM/VMMR0/CPUMR0.cpp
+++ src/VBox/VMM/VMMR0/CPUMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUMR0.cpp 33936 2010-11-10 15:41:04Z noreply@oracle.com $ */
+/* $Id: CPUMR0.cpp 33938 2010-11-10 15:50:41Z noreply@oracle.com $ */
 /** @file
  * CPUM - Host Context Ring 0.
  */
@@ -92,12 +92,6 @@ VMMR0DECL(int) CPUMR0ModuleTerm(void)
 VMMR0DECL(int) CPUMR0Init(PVM pVM)
 {
     LogFlow(("CPUMR0Init: %p\n", pVM));
-
-#ifdef VBOX_WITH_VMMR0_DISABLE_LAPIC_NMI
-    for (unsigned i = 0; i < RT_ELEMENTS(g_aLApics); i++)
-        if (g_aLApics[i].pv)
-            SUPR0Printf(" CPU%d: %llx => %llx\n", i, g_aLApics[i].PhysBase, (uint64_t)g_aLApics[i].pv);
-#endif
 
     /*
      * Check CR0 & CR4 flags.
