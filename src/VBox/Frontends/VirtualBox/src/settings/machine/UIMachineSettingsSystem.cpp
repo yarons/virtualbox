1bc885321fc3e34dbe194f2b659e306e6a23bec1
Christian Poetzsch <noreply@oracle.com>
2010-11-11 18:37:19 +0000

    FE/Qt4: still necessary for older Qt versions
    svn:sync-xref-src-repo-rev: r67671

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsSystem.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsSystem.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsSystem.cpp 34004 2010-11-11 18:23:37Z noreply@oracle.com $ */
+/* $Id: UIMachineSettingsSystem.cpp 34006 2010-11-11 18:37:19Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -541,6 +541,14 @@ void UIMachineSettingsSystem::adjustBootOrderTWSize()
 
     int h = 2 * mTwBootOrder->frameWidth();
     int w = h;
+#if defined(Q_WS_MAC) && (QT_VERSION < 0x040700)
+    int left, top, right, bottom;
+    mTwBootOrder->getContentsMargins (&left, &top, &right, &bottom);
+    h += top + bottom;
+    w += left + right;
+#else /* defined(Q_WS_MAC) && (QT_VERSION < 0x040700) */
+    w += 4;
+#endif /* !(defined(Q_WS_MAC) && (QT_VERSION < 0x040700)) */
     mTwBootOrder->setFixedSize(
         iv->sizeHintForColumn(0) + w,
         iv->sizeHintForRow(0) * mTwBootOrder->topLevelItemCount() + h);
