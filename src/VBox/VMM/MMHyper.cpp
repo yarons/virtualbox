03663b49ca8dc7afa8d809b30d55240bfa09c348
Nikolay Igotty <noreply@oracle.com>
2010-11-11 14:52:22 +0000

    MM: even larger hyperheap for ICH9
    svn:sync-xref-src-repo-rev: r67663

--- src/VBox/VMM/MMHyper.cpp
+++ src/VBox/VMM/MMHyper.cpp
@@ -1,4 +1,4 @@
-/* $Id: MMHyper.cpp 33540 2010-10-28 09:27:05Z noreply@oracle.com $ */
+/* $Id: MMHyper.cpp 33998 2010-11-11 14:52:22Z noreply@oracle.com $ */
 /** @file
  * MM - Memory Manager - Hypervisor Memory Area.
  */
@@ -48,12 +48,14 @@ DECLINLINE(uint32_t) mmR3ComputeHyperHeapSize(PVM pVM, bool fCanUseLargerHeap)
 {
     bool fHwVirtExtForced = VMMIsHwVirtExtForced(pVM);
 
+    /* Newer chipset allows for much more devices, putting additional pressure on hyperheap */
+    /* @todo: maybe base hyperheap size upon number of devices attached too */
+    if (fCanUseLargerHeap)
+        return _2M + pVM->cCpus * 2 * _64K;
+
     if (pVM->cCpus > 1)
         return _1M + pVM->cCpus * 2 * _64K;
 
-    if (fCanUseLargerHeap)
-        return 1280*_1K;
-    else
     if (fHwVirtExtForced)
     {
         uint64_t cbRam = 0;
