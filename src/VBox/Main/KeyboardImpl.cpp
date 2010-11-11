bda72629b7e6ebff0c5a76c2e13a721296287bbf
Nikolay Igotty <noreply@oracle.com>
2010-11-11 20:35:42 +0000

    Main: keyboard events now work with RDP
    svn:sync-xref-src-repo-rev: r67677

--- src/VBox/Main/KeyboardImpl.cpp
+++ src/VBox/Main/KeyboardImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: KeyboardImpl.cpp 33061 2010-10-12 12:42:20Z noreply@oracle.com $ */
+/* $Id: KeyboardImpl.cpp 34012 2010-11-11 20:35:42Z noreply@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -184,6 +184,11 @@ STDMETHODIMP Keyboard::PutScancode(LONG scancode)
                       tr("Could not send scan code 0x%08X to the virtual keyboard (%Rrc)"),
                       scancode, vrc);
 
+    VBoxEventDesc evDesc;
+    com::SafeArray<LONG> scancodes(1);
+    scancodes[0] = scancode;
+    evDesc.init(mEventSource, VBoxEventType_OnGuestKeyboardEvent, ComSafeArrayAsInParam(scancodes));
+    evDesc.fire(0);
     return rc;
 }
 
@@ -242,17 +247,10 @@ STDMETHODIMP Keyboard::PutScancodes(ComSafeArrayIn(LONG, scancodes),
     /// @todo is it actually possible that not all scancodes can be transmitted?
     if (codesStored)
         *codesStored = (uint32_t)keys.size();
-#if 1
+
     VBoxEventDesc evDesc;
-    evDesc.init(mEventSource, VBoxEventType_OnGuestKeyboardEvent,
-#ifdef RT_OS_WINDOWS
-                scancodes
-#else
-                scancodesSize, scancodes
-#endif
-                );
+    evDesc.init(mEventSource, VBoxEventType_OnGuestKeyboardEvent, ComSafeArrayAsInParam(keys));
     evDesc.fire(0);
-#endif
 
     return rc;
 }
