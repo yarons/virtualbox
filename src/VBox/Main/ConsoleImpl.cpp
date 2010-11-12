169a39c7beda5fd38d859f0c1d0f72dc4202f203
Michael Thayer <noreply@oracle.com>
2010-11-12 22:54:22 +0000

    Main/GuestProperties: two missing NULL checks
    svn:sync-xref-src-repo-rev: r67730

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 34023 2010-11-12 09:30:23Z klaus.espenlaub@oracle.com $ */
+/* $Id: ConsoleImpl.cpp 34048 2010-11-12 22:54:22Z noreply@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation
  */
@@ -4424,10 +4424,14 @@ HRESULT Console::getGuestProperty(IN_BSTR aName, BSTR *aValue,
                 Utf8Str strBuffer(pszBuffer);
                 strBuffer.cloneTo(aValue);
 
-                *aTimestamp = parm[2].u.uint64;
+                if (aTimestamp)
+                    *aTimestamp = parm[2].u.uint64;
 
-                size_t iFlags = strBuffer.length() + 1;
-                Utf8Str(pszBuffer + iFlags).cloneTo(aFlags);
+                if (aFlags)
+                {
+                    size_t iFlags = strBuffer.length() + 1;
+                    Utf8Str(pszBuffer + iFlags).cloneTo(aFlags);
+                }
             }
             else
                 aValue = NULL;
