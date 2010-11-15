f5869a6477bdaef4641d31b117f408756211fac9
Frank Mehnert <noreply@oracle.com>
2010-11-15 13:05:48 +0000

    Additions/common/VBoxGuestLib: don't leak HGCM handle during termination
    svn:sync-xref-src-repo-rev: r67748

--- src/VBox/Additions/common/VBoxGuestLib/Init.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/Init.cpp
@@ -1,4 +1,4 @@
-/* $Revision: 33540 $ */
+/* $Revision: 34066 $ */
 /** @file
  * VBoxGuestLibR0 - Library initialization.
  */
@@ -251,11 +251,11 @@ DECLVBGL(int) VbglInit (void)
 
 DECLVBGL(void) VbglTerminate (void)
 {
-    vbglTerminateCommon ();
-
 # ifdef VBOX_WITH_HGCM
     vbglR0HGCMTerminate ();
 # endif
+
+    vbglTerminateCommon ();
 
     return;
 }
