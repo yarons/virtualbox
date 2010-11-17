95273936275f25f35bce3c60a6576fab6db58994
Frank Mehnert <noreply@oracle.com>
2010-11-17 20:05:36 +0000

    Additions/Solaris/Virtio: export to OSE
    svn:sync-xref-src-repo-rev: r67863

---
+++ src/VBox/Additions/solaris/Virtio/VirtioPci-solaris.h
@@ -0,0 +1,21 @@
+/* $Id: VirtioPci-solaris.h 34143 2010-11-17 20:05:36Z noreply@oracle.com $ */
+/** @file
+ * VirtualBox Guest Additions: Virtio Driver for Solaris, PCI Hypervisor Interface.
+ */
+
+/*
+ * Copyright (C) 2010 Oracle Corporation
+ *
+ * Oracle Corporation confidential
+ * All rights reserved
+ */
+
+#ifndef ___VirtioPCI_solaris_h
+#define ___VirtioPCI_solaris_h
+
+#include "Virtio-solaris.h"
+
+extern VIRTIOHYPEROPS g_VirtioHyperOpsPci;
+
+#endif
+
