89e3c2e5e3deb4c2e1db9c837f9cf809d96eb5d0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-11-18 15:49:17 +0000

    iprt/tarvfs: Rewrote the tar parser to deal with header sequences used by solaris, gnu and pax while being offline yesterday. Only GNU long link and long names are supported, but adding pax and solaris support is possible. This also resolves some of the ustar<space><space> vs. ustart<null>00 problems (the latter was not readable previously).
    svn:sync-xref-src-repo-rev: r67908

--- src/VBox/Runtime/common/vfs/vfschain.cpp
+++ src/VBox/Runtime/common/vfs/vfschain.cpp
@@ -1,4 +1,4 @@
-/* $Id: vfschain.cpp 33973 2010-11-11 11:10:10Z knut.osmundsen@oracle.com $ */
+/* $Id: vfschain.cpp 34179 2010-11-18 15:49:17Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Virtual File System, Chains.
  */
@@ -567,6 +567,8 @@ RTDECL(int) RTVfsChainOpenFile(const char *pszSpec, uint32_t fOpen, PRTVFSFILE p
     AssertPtrReturn(pszSpec, VERR_INVALID_POINTER);
     AssertReturn(*pszSpec != '\0', VERR_INVALID_PARAMETER);
     AssertPtrReturn(phVfsFile, VERR_INVALID_POINTER);
+    if (ppszError)
+        *ppszError = NULL;
 
     /*
      * If it's not a VFS chain spec, treat it as a file.
@@ -612,6 +614,8 @@ RTDECL(int) RTVfsChainOpenIoStream(const char *pszSpec, uint32_t fOpen, PRTVFSIO
     AssertPtrReturn(pszSpec, VERR_INVALID_POINTER);
     AssertReturn(*pszSpec != '\0', VERR_INVALID_PARAMETER);
     AssertPtrReturn(phVfsIos, VERR_INVALID_POINTER);
+    if (ppszError)
+        *ppszError = NULL;
 
     /*
      * If it's not a VFS chain spec, treat it as a file.
