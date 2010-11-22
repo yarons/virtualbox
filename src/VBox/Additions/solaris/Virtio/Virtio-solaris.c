bad4419deafeabb3955fc33318dccb428ec377e2
Frank Mehnert <noreply@oracle.com>
2010-11-22 11:25:55 +0000

    header fixes
    svn:sync-xref-src-repo-rev: r67980

--- src/VBox/Additions/solaris/Virtio/Virtio-solaris.c
+++ src/VBox/Additions/solaris/Virtio/Virtio-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: Virtio-solaris.c 34143 2010-11-17 20:05:36Z noreply@oracle.com $ */
+/* $Id: Virtio-solaris.c 34233 2010-11-22 11:25:55Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Guest Additions: Virtio Driver for Solaris.
  */
@@ -6,8 +6,22 @@
 /*
  * Copyright (C) 2010 Oracle Corporation
  *
- * Oracle Corporation confidential
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
  */
 
 /*******************************************************************************
