2ff0c9b672c6cfa74128be69b7f14a5860343310
Frank Mehnert <noreply@oracle.com>
2010-11-22 18:18:11 +0000

    another typo
    svn:sync-xref-src-repo-rev: r68011

--- src/VBox/HostDrivers/VBoxUSB/win/USBLib-win.cpp
+++ src/VBox/HostDrivers/VBoxUSB/win/USBLib-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: USBLib-win.cpp 34262 2010-11-22 18:15:59Z noreply@oracle.com $ */
+/* $Id: USBLib-win.cpp 34263 2010-11-22 18:18:11Z noreply@oracle.com $ */
 /** @file
  * USBLIB - USB support library interface, Windows.
  */
@@ -2807,7 +2807,7 @@ USBLIB_DECL(int) USBLibGetDevices(PUSBDEVICE *ppDevices, uint32_t *pcDevices)
     /* 2: Get all the USB devices that the filter has captured for us */
 
     /* Get the required info for each captured device */
-    PUSBDEVICE *ppCaptured = (PUSBDEVICE*)RTMemAllocZ(sizeof(PUSBDEVICE) * numDev.cUSBDevices);
+    PUSBDEVICE *ppCaptured = (PUSBDEVICE*)RTMemAllocZ(sizeof(PUSBDEVICE) * numdev.cUSBDevices);
     if (!ppCaptured)
         goto failure;
 
