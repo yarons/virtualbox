f0d90a9b2e780b3c8921282b7ebccf97118f2215
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-11-24 12:45:20 +0000

    Added VBoxRes.dll for shared resources which are presented to the running (guest) OS.
    svn:sync-xref-src-repo-rev: r68072

---
+++ src/VBox/Installer/win/Resources/resource.h
@@ -0,0 +1,19 @@
+/* $Id: resource.h 34314 2010-11-24 12:45:20Z andreas.loeffler@oracle.com $ */
+/** @file
+ * resource.h - resource header file.
+ */
+
+/*
+ * Copyright (C) 2010 Oracle Corporation
+ *
+ * Oracle Corporation confidential
+ * All rights reserved
+ */
+
+#define IDI_VIRTUALBOX 							101
+
+#define IDI_FILETYPE_VBOX 					201
+#define IDI_FILETYPE_VBOX_EXTPACK 	202
+
+#define IDI_FILETYPE_VBOX_OVF			 	301
+#define IDI_FILETYPE_VBOX_OVA			 	302
