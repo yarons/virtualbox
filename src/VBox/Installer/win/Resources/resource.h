6b0b0ea8bfc955205065f2ac0bd207f37fd94149
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-11-24 14:31:57 +0000

    Tabs, not confidential.
    svn:sync-xref-src-repo-rev: r68086

--- src/VBox/Installer/win/Resources/resource.h
+++ src/VBox/Installer/win/Resources/resource.h
@@ -1,4 +1,4 @@
-/* $Id: resource.h 34314 2010-11-24 12:45:20Z andreas.loeffler@oracle.com $ */
+/* $Id: resource.h 34327 2010-11-24 14:31:57Z andreas.loeffler@oracle.com $ */
 /** @file
  * resource.h - resource header file.
  */
@@ -6,14 +6,14 @@
 /*
  * Copyright (C) 2010 Oracle Corporation
  *
- * Oracle Corporation confidential
+ * Oracle Corporation
  * All rights reserved
  */
 
-#define IDI_VIRTUALBOX 							101
+#define IDI_VIRTUALBOX              101
 
-#define IDI_FILETYPE_VBOX 					201
-#define IDI_FILETYPE_VBOX_EXTPACK 	202
+#define IDI_FILETYPE_VBOX           201
+#define IDI_FILETYPE_VBOX_EXTPACK   202
 
-#define IDI_FILETYPE_VBOX_OVF			 	301
-#define IDI_FILETYPE_VBOX_OVA			 	302
+#define IDI_FILETYPE_VBOX_OVF       301
+#define IDI_FILETYPE_VBOX_OVA       302
