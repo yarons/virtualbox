d7fde852d6fd9206883fc6189f2af758c4ca038f
Vasily Levchenko <noreply@oracle.com>
2010-11-25 08:06:13 +0000

    Audio/HDA: logs.
    svn:sync-xref-src-repo-rev: r68114

--- src/VBox/Devices/Audio/DevCodec.cpp
+++ src/VBox/Devices/Audio/DevCodec.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevCodec.cpp 34005 2010-11-11 18:34:27Z noreply@oracle.com $ */
+/* $Id: DevCodec.cpp 34351 2010-11-25 08:06:13Z noreply@oracle.com $ */
 /** @file
  * DevCodec - VBox ICH Intel HD Audio Codec.
  */
@@ -2253,14 +2253,14 @@ static int codecLookup(CODECState *pState, uint32_t cmd, PPFNCODECVERBPROCESSOR 
     Assert(CODEC_CAD(cmd) == pState->id);
     if (codecIsReservedNode(pState, CODEC_NID(cmd)))
     {
-        LogRel(("HDAcodec: cmd %x was addressed to reserved node\n", cmd));
+        Log(("HDAcodec: cmd %x was addressed to reserved node\n", cmd));
     }
     if (   CODEC_VERBDATA(cmd) == 0
         || CODEC_NID(cmd) >= pState->cTotalNodes)
     {
         *pfn = codecUnimplemented;
         //** @todo r=michaln: There needs to be a counter to avoid log flooding (see e.g. DevRTC.cpp)
-        LogRel(("HDAcodec: cmd %x was ignored\n", cmd));
+        Log(("HDAcodec: cmd %x was ignored\n", cmd));
         return VINF_SUCCESS;
     }
     for (int i = 0; i < pState->cVerbs; ++i)
