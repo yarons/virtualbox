19d1feb6a6e40d652e88dac07ab3755d99b2e70d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-11-28 21:46:04 +0000

    VBoxExtPackHelperApp.cpp: missing release calls and call RTManifestPtIosAddEntryNow explicitly to catch errors.
    svn:sync-xref-src-repo-rev: r68210

--- src/VBox/Main/VBoxExtPackHelperApp.cpp
+++ src/VBox/Main/VBoxExtPackHelperApp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxExtPackHelperApp.cpp 34435 2010-11-28 14:58:25Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxExtPackHelperApp.cpp 34437 2010-11-28 21:46:04Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox Main - Extension Pack Helper Application, usually set-uid-to-root.
  */
@@ -517,26 +517,39 @@ static RTEXITCODE UnpackExtPackFile(const char *pszName, const char *pszDstFilen
             rc = RTVfsUtilPumpIoStreams(hVfsIosSrc, hVfsIosDst, (uint32_t)RT_MIN(ObjInfo.cbObject, _1G));
             if (RT_SUCCESS(rc))
             {
-                /*
-                 * Set the mode mask.
-                 */
-                ObjInfo.Attr.fMode &= ~(RTFS_UNIX_IWOTH | RTFS_UNIX_IWGRP);
-                rc = RTFileSetMode(hFile, ObjInfo.Attr.fMode);
-                /** @todo Windows needs to do more here, I think. */
+                rc = RTManifestPtIosAddEntryNow(hVfsIosDst);
                 if (RT_SUCCESS(rc))
                 {
-                    return RTEXITCODE_SUCCESS;
+                    RTVfsIoStrmRelease(hVfsIosDst);
+                    hVfsIosDst = NIL_RTVFSIOSTREAM;
+
+                    /*
+                     * Set the mode mask.
+                     */
+                    ObjInfo.Attr.fMode &= ~(RTFS_UNIX_IWOTH | RTFS_UNIX_IWGRP);
+                    rc = RTFileSetMode(hFile, ObjInfo.Attr.fMode);
+                    /** @todo Windows needs to do more here, I think. */
+                    if (RT_SUCCESS(rc))
+                    {
+                        RTFileClose(hFile);
+                        return RTEXITCODE_SUCCESS;
+                    }
+
+                    RTMsgError("Failed to set the mode of '%s' to %RTfmode: %Rrc", pszDstFilename, ObjInfo.Attr.fMode, rc);
                 }
-                RTMsgError("Failed to set the mode of '%s' to %RTfmode: %Rrc", pszDstFilename, ObjInfo.Attr.fMode, rc);
+                else
+                    RTMsgError("RTManifestPtIosAddEntryNow failed for '%s': %Rrc", pszDstFilename, rc);
             }
             else
                 RTMsgError("RTVfsUtilPumpIoStreams failed for '%s': %Rrc", pszDstFilename, rc);
+            RTVfsIoStrmRelease(hVfsIosDst);
         }
         else
             RTMsgError("RTManifestEntryAddPassthruIoStream failed: %Rrc", rc);
     }
     else
         RTMsgError("RTVfsIoStrmFromRTFile failed: %Rrc", rc);
+    RTFileClose(hFile);
     return RTEXITCODE_FAILURE;
 }
 
