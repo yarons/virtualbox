92422a4fbfab5f6be100cd81c2fc3937389bea08
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2010-11-29 10:35:43 +0000

    SUPR3HardenedMain: copy/paste bug
    svn:sync-xref-src-repo-rev: r68222

--- src/VBox/HostDrivers/Support/SUPR3HardenedMain.cpp
+++ src/VBox/HostDrivers/Support/SUPR3HardenedMain.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPR3HardenedMain.cpp 34445 2010-11-29 10:09:02Z klaus.espenlaub@oracle.com $ */
+/* $Id: SUPR3HardenedMain.cpp 34448 2010-11-29 10:35:43Z klaus.espenlaub@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Hardened main().
  */
@@ -659,7 +659,7 @@ static void supR3HardenedMainGrabCapabilites(void)
         priv_emptyset(pPrivSet);
         /* The inheritable privilege set contains the privileges the process
          * would have received if it weren't marked as suid root. */
-        int rc = getppriv(PRIV_SET, PRIV_INHERITABLE, pPrivSet);
+        int rc = getppriv(PRIV_INHERITABLE, pPrivSet);
         if (!rc)
         {
             priv_addset(pPrivSet, PRIV_NET_ICMPACCESS);
