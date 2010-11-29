2cd25ad82e781f8f71ff9d32f05593d675a485a1
Frank Mehnert <noreply@oracle.com>
2010-11-29 11:27:20 +0000

    OSE header fix
    svn:sync-xref-src-repo-rev: r68228

--- src/VBox/Installer/win/Resources/resource.h
+++ src/VBox/Installer/win/Resources/resource.h
@@ -1,4 +1,4 @@
-/* $Id: resource.h 34327 2010-11-24 14:31:57Z andreas.loeffler@oracle.com $ */
+/* $Id: resource.h 34454 2010-11-29 11:27:20Z noreply@oracle.com $ */
 /** @file
  * resource.h - resource header file.
  */
@@ -6,8 +6,13 @@
 /*
  * Copyright (C) 2010 Oracle Corporation
  *
- * Oracle Corporation
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
 #define IDI_VIRTUALBOX              101
