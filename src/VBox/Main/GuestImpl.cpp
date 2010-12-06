518a2020b2014795607372939c84ecaaae469cd4
Andreas Loeffler <andreas.loeffler@oracle.com>
2010-12-06 16:05:40 +0000

    Automatic Guest Additions update/Main: One more process step.
    svn:sync-xref-src-repo-rev: r68566

--- src/VBox/Main/GuestImpl.cpp
+++ src/VBox/Main/GuestImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestImpl.cpp 34758 2010-12-06 15:42:52Z andreas.loeffler@oracle.com $ */
+/* $Id: GuestImpl.cpp 34760 2010-12-06 16:05:40Z andreas.loeffler@oracle.com $ */
 
 /** @file
  *
@@ -429,6 +429,8 @@ HRESULT Guest::taskUpdateGuestAdditions(TaskGuest *aTask)
                      * complete the progress object now so that the caller can do other work. */
                     if (aTask->uFlags & AdditionsUpdateFlag_WaitForUpdateStartOnly)
                         aTask->progress->notifyComplete(S_OK);
+                    else
+                        aTask->progress->SetCurrentOperationProgress(70);
 
                     LogRel(("Guest Additions update is running ...\n"));
                     while (SUCCEEDED(progressInstaller->COMGETTER(Completed(&fCompleted))))
@@ -451,7 +453,12 @@ HRESULT Guest::taskUpdateGuestAdditions(TaskGuest *aTask)
                         if (fCompleted)
                         {
                             if (uRetExitCode == 0)
+                            {
                                 LogRel(("Guest Additions update successful!\n"));
+                                if (   SUCCEEDED(aTask->progress->COMGETTER(Completed(&fCompleted)))
+                                    && !fCompleted)
+                                    aTask->progress->notifyComplete(S_OK);
+                            }
                             else
                             {
                                 rc = setError(VBOX_E_IPRT_ERROR,
