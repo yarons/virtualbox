7f44b32647679d529124b340f7a6e7fd003b21e6
Christian Poetzsch <noreply@oracle.com>
2010-12-07 15:01:15 +0000

    FE/Qt4: close IMedium on delete
    svn:sync-xref-src-repo-rev: r68601

--- src/VBox/Frontends/VirtualBox/src/VBoxMediaManagerDlg.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxMediaManagerDlg.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMediaManagerDlg.cpp 34736 2010-12-06 11:19:30Z sergey.dubov@oracle.com $ */
+/* $Id: VBoxMediaManagerDlg.cpp 34791 2010-12-07 15:01:15Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -1076,25 +1076,16 @@ void VBoxMediaManagerDlg::doRemoveMedium()
 
             if (deleteStorage)
             {
-                bool success = false;
-
                 CProgress progress = hardDisk.DeleteStorage();
                 if (hardDisk.isOk())
                 {
-                    vboxProblem().showModalProgressDialog (progress, windowTitle(), ":/progress_delete_90px.png", this, true);
-                    if (progress.isOk() && progress.GetResultCode() == S_OK)
-                        success = true;
+                    vboxProblem().showModalProgressDialog(progress, windowTitle(), ":/progress_delete_90px.png", this, true);
+                    if (!(progress.isOk() && progress.GetResultCode() == S_OK))
+                    {
+                        vboxProblem().cannotDeleteHardDiskStorage(this, hardDisk, progress);
+                        return;
+                    }
                 }
-
-                if (success)
-                    vboxGlobal().removeMedium (VBoxDefs::MediumType_HardDisk, id);
-                else
-                    vboxProblem().cannotDeleteHardDiskStorage (this, hardDisk, progress);
-
-                /* We don't want to close the hard disk because it was
-                 * implicitly closed and removed from the list of known media
-                 * on storage deletion */
-                return;
             }
 
             hardDisk.Close();
