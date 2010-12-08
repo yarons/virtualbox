d485951190d506bdaccf230425c923b722a5734e
Christian Poetzsch <noreply@oracle.com>
2010-12-08 11:14:32 +0000

    FE/Qt4: Remove no display, cause it prevent the display of the shortcut with some window managers.
    svn:sync-xref-src-repo-rev: r68639

--- src/VBox/Frontends/VirtualBox/src/platform/x11/UIDesktopServices_x11.cpp
+++ src/VBox/Frontends/VirtualBox/src/platform/x11/UIDesktopServices_x11.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIDesktopServices_x11.cpp 34462 2010-11-29 13:14:35Z noreply@oracle.com $ */
+/* $Id: UIDesktopServices_x11.cpp 34826 2010-12-08 11:14:32Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -43,8 +43,7 @@ bool UIDesktopServices::createMachineShortcut(const QString & /* strSrcFile */, 
             << "Comment=Starts the VirtualBox machine " << strName << endl
             << "Type=Application" << endl
             << "Exec=" << QCoreApplication::applicationFilePath() << " --comment \"" << strName << "\" --startvm \"" << strUuid << "\"" << endl
-            << "Icon=virtualbox-vbox" << endl
-            << "NoDisplay=true" << endl;
+            << "Icon=virtualbox-vbox" << endl;
         /* This would be a real file link entry, but then we could also simply
          * use a soft link (on most UNIX fs):
         out << "[Desktop Entry]" << endl
