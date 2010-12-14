2a681ed816448561686eddbb943108e6a973d039
Sergey Dubov <sergey.dubov@oracle.com>
2010-12-14 11:21:12 +0000

    FE/Qt: Mini-toolbar auto-fill-background property to make it use own background color instead of parent's color.
    svn:sync-xref-src-repo-rev: r68895

--- src/VBox/Frontends/VirtualBox/src/widgets/VBoxMiniToolBar.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/VBoxMiniToolBar.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMiniToolBar.cpp 32760 2010-09-24 13:41:31Z noreply@oracle.com $ */
+/* $Id: VBoxMiniToolBar.cpp 35064 2010-12-14 11:21:12Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -125,6 +125,9 @@ VBoxMiniToolBar::VBoxMiniToolBar(QWidget *pParent, Alignment alignment, bool fAc
 
     /* Enable mouse-tracking for this & children allowing to get mouse-move events: */
     setMouseTrackingEnabled(m_fAutoHide);
+
+    /* Auto-fill background to make it use own color: */
+    setAutoFillBackground(true);
 }
 
 /* Appends passed menus into internal menu-list */
