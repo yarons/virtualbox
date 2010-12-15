1283fabc00d6575d1d7581f6f7f50a8530de05a3
Christian Poetzsch <noreply@oracle.com>
2010-12-15 11:28:30 +0000

    Runtime: too much replaced in the last fix
    svn:sync-xref-src-repo-rev: r68956

--- src/VBox/Runtime/common/zip/tar.cpp
+++ src/VBox/Runtime/common/zip/tar.cpp
@@ -1,4 +1,4 @@
-/* $Id: tar.cpp 34952 2010-12-10 14:15:46Z knut.osmundsen@oracle.com $ */
+/* $Id: tar.cpp 35122 2010-12-15 11:28:30Z noreply@oracle.com $ */
 /** @file
  * IPRT - Tar archive I/O.
  */
@@ -254,7 +254,7 @@ DECLINLINE(uint64_t) rtTarRecToSize(PRTTARRECORD pRecord)
             cbSize = (cbSize << 8) | *puchField++;
         }
     }else
-        cbSize = rtTarRecToSize(pRecord);
+        RTStrToInt64Full(pRecord->h.size, 8, &cbSize);
 
     if (cbSize < 0)
         cbSize = 0;
