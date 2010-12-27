7528de279d90704e84886b2bf603dfd7a6ed885a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2010-12-27 16:57:10 +0000

    build fix...
    svn:sync-xref-src-repo-rev: r69210

--- src/VBox/VMM/Docs-RawMode.cpp
+++ src/VBox/VMM/Docs-RawMode.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMMDocsRawMode.cpp 35333 2010-12-27 12:10:56Z knut.osmundsen@oracle.com $ */
+/* $Id: Docs-RawMode.cpp 35350 2010-12-27 16:57:10Z knut.osmundsen@oracle.com $ */
 /** @file
  * This file contains the documentation of the raw-mode execution.
  */
