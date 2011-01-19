cfc6a71002a4f69aa807bc003058fd5abd93b512
Christian Poetzsch <noreply@oracle.com>
2011-01-19 16:13:31 +0000

    FE/Qt4: allow free configurable key shortcuts in the selector and machine window
    svn:sync-xref-src-repo-rev: r69539

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGlobal.cpp 35564 2011-01-14 13:52:02Z noreply@oracle.com $ */
+/* $Id: VBoxGlobal.cpp 35634 2011-01-19 16:13:31Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -4205,14 +4205,17 @@ QString VBoxGlobal::removeAccelMark (const QString &aText)
 }
 
 /* static */
-QString VBoxGlobal::insertKeyToActionText (const QString &aText, const QString &aKey)
+QString VBoxGlobal::insertKeyToActionText(const QString &strText, const QString &strKey)
 {
 #ifdef Q_WS_MAC
-    QString key ("%1 (Host+%2)");
+    QString pattern("%1 (Host+%2)");
 #else
-    QString key ("%1 \tHost+%2");
+    QString pattern("%1 \tHost+%2");
 #endif
-    return key.arg (aText).arg (QKeySequence (aKey).toString (QKeySequence::NativeText));
+    if (strKey.isEmpty())
+        return strText;
+    else
+        return pattern.arg(strText).arg(QKeySequence(strKey).toString(QKeySequence::NativeText));
 }
 
 /* static */
