71b39da7d3efbc8a2fbf868ce00b816b722c822b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-01-19 17:42:59 +0000

    Debugger console: Made the evaluator a bit smarter wrt to the register and variable operators (e.g. don't confuse @ah with a hexadecimal number).
    svn:sync-xref-src-repo-rev: r69544

--- src/VBox/Debugger/DBGCCmdHlp.cpp
+++ src/VBox/Debugger/DBGCCmdHlp.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGCCmdHlp.cpp 35632 2011-01-19 15:55:05Z knut.osmundsen@oracle.com $ */
+/* $Id: DBGCCmdHlp.cpp 35637 2011-01-19 17:42:59Z knut.osmundsen@oracle.com $ */
 /** @file
  * DBGC - Debugger Console, Command Helpers.
  */
@@ -672,7 +672,7 @@ static DECLCALLBACK(int) dbgcHlpEvalV(PDBGCCMDHLP pCmdHlp, PDBGCVAR pResult, con
     size_t cb = RTStrPrintfExV(dbgcStringFormatter, pDbgc, szExprFormatted, sizeof(szExprFormatted), pszExpr, va);
     /* ignore overflows. */
 
-    return dbgcEvalSub(pDbgc, &szExprFormatted[0], cb, pResult);
+    return dbgcEvalSub(pDbgc, &szExprFormatted[0], cb, DBGCVAR_CAT_ANY, pResult);
 }
 
 
