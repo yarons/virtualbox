14e4a4bb6722f085ea1d13a15ac19b04f20f46f7
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2011-01-24 17:27:05 +0000

    Main/NetIf: Third attempt to fix VBoxSVC crash when listing host interfaces with no default route (#5491)
    svn:sync-xref-src-repo-rev: r69619

--- src/VBox/Main/src-server/darwin/NetIf-darwin.cpp
+++ src/VBox/Main/src-server/darwin/NetIf-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: NetIf-darwin.cpp 35691 2011-01-24 16:59:46Z aleksey.ilyushin@oracle.com $ */
+/* $Id: NetIf-darwin.cpp 35693 2011-01-24 17:27:05Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * Main - NetIfList, Darwin implementation.
  */
@@ -249,7 +249,7 @@ static int getDefaultIfaceIndex(unsigned short *pu16Index)
         {
             /* Extract addresses from the message. */
             extractAddresses(pRtMsg->rtm_addrs, (char *)(pRtMsg + 1),
-                             pRtMsg->rtm_msglen + (char *)pRtMsg, addresses);
+                             pRtMsg->rtm_msglen + 1 + (char *)pRtMsg, addresses);
             if ((pRtMsg->rtm_addrs & RTA_DST)
                 && (pRtMsg->rtm_addrs & RTA_NETMASK))
             {
