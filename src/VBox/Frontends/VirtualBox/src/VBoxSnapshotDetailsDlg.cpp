d6554d30f1d66d7870f573ca091d098fef5e99ec
Christian Poetzsch <noreply@oracle.com>
2011-01-25 13:14:13 +0000

    FE/Qt4: fix the update of a snapshot name or description
    svn:sync-xref-src-repo-rev: r69646

--- src/VBox/Frontends/VirtualBox/src/VBoxSnapshotDetailsDlg.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxSnapshotDetailsDlg.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxSnapshotDetailsDlg.cpp 29354 2010-05-11 13:44:25Z noreply@oracle.com $ */
+/* $Id: VBoxSnapshotDetailsDlg.cpp 35710 2011-01-25 13:14:13Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -105,8 +105,16 @@ void VBoxSnapshotDetailsDlg::putBackToSnapshot()
 {
     AssertReturn (!mSnapshot.isNull(), (void) 0);
 
-    mSnapshot.SetName (mLeName->text());
-    mSnapshot.SetDescription (mTeDescription->toPlainText());
+    /* We need a session when we manipulate the snapshot data of a machine. */
+    CSession session = vboxGlobal().openSession(mSnapshot.GetMachine().GetId(), true);
+    if (session.isNull())
+        return;
+
+    mSnapshot.SetName(mLeName->text());
+    mSnapshot.SetDescription(mTeDescription->toPlainText());
+
+    /* Close the session again. */
+    session.UnlockMachine();
 }
 
 void VBoxSnapshotDetailsDlg::retranslateUi()
