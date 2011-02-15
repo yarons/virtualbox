1b40903cb1d20c64a6156d44eec78600c855a1ee
Andreas Loeffler <andreas.loeffler@oracle.com>
2011-02-15 11:07:02 +0000

    Build fix for Mac.
    svn:sync-xref-src-repo-rev: r70041

--- src/VBox/Main/src-server/xpcom/server.cpp
+++ src/VBox/Main/src-server/xpcom/server.cpp
@@ -1,4 +1,4 @@
-/* $Id: server.cpp 35368 2010-12-30 13:38:23Z knut.osmundsen@oracle.com $ */
+/* $Id: server.cpp 35972 2011-02-15 11:07:02Z andreas.loeffler@oracle.com $ */
 /** @file
  * XPCOM server process (VBoxSVC) start point.
  */
@@ -155,6 +155,9 @@ NS_IMPL_THREADSAFE_ISUPPORTS1_CI(HostNetworkInterface, IHostNetworkInterface)
 
 NS_DECL_CLASSINFO(DHCPServer)
 NS_IMPL_THREADSAFE_ISUPPORTS1_CI(DHCPServer, IDHCPServer)
+
+NS_DECL_CLASSINFO(AdditionsFacility)
+NS_IMPL_THREADSAFE_ISUPPORTS1_CI(AdditionsFacility, IAdditionsFacility)
 
 NS_DECL_CLASSINFO(GuestOSType)
 NS_IMPL_THREADSAFE_ISUPPORTS1_CI(GuestOSType, IGuestOSType)
