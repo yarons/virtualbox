17530e74868d3e9de98e5181e98d07d548bb10f3
Alexander Eichner <alexander.eichner@oracle.com>
2011-02-22 23:11:22 +0000

    Main/BandwidthControl: Don't use direct references to bandwidth group objects in the medium attachment object. It will point to the old uninitialized instance if the settings are changed. Uses the name of the bandwidth group now instead without changing the public interface to make a backport to 4.0 possible
    svn:sync-xref-src-repo-rev: r70171

--- src/VBox/Main/include/MachineImpl.h
+++ src/VBox/Main/include/MachineImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.h 35836 2011-02-03 14:55:17Z noreply@oracle.com $ */
+/* $Id: MachineImpl.h 36058 2011-02-22 23:11:22Z alexander.eichner@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -32,6 +32,7 @@
 #include "BIOSSettingsImpl.h"
 #include "StorageControllerImpl.h"          // required for MachineImpl.h to compile on Windows
 #include "BandwidthControlImpl.h"
+#include "BandwidthGroupImpl.h"
 #include "VBox/settings.h"
 #ifdef VBOX_WITH_RESOURCE_USAGE_API
 #include "Performance.h"
@@ -714,6 +715,15 @@ public:
                                MachineState_T *aState = NULL,
                                BOOL *aRegistered = NULL);
     void releaseStateDependency();
+
+    HRESULT getBandwidthGroup(const Utf8Str &strBandwidthGroup,
+                              ComObjPtr<BandwidthGroup> &pBandwidthGroup,
+                              bool fSetError = false)
+    {
+        return mBandwidthControl->getBandwidthGroupByName(strBandwidthGroup,
+                                                          pBandwidthGroup,
+                                                          fSetError);
+    }
 
 protected:
 
