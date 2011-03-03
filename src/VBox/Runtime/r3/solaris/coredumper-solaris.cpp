0d74378bb70cd6cd7d3c2f6c7d2292dfb15920b9
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2011-03-03 18:12:18 +0000

    Runtime/r3/solaris/coredumper: Make sure directory exists during RTCoreDumperSetup()
    svn:sync-xref-src-repo-rev: r70340

--- src/VBox/Runtime/r3/solaris/coredumper-solaris.cpp
+++ src/VBox/Runtime/r3/solaris/coredumper-solaris.cpp
@@ -1,4 +1,4 @@
-/* $Id: coredumper-solaris.cpp 34178 2010-11-18 15:18:05Z klaus.espenlaub@oracle.com $ */
+/* $Id: coredumper-solaris.cpp 36155 2011-03-03 18:12:18Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IPRT Testcase - Core Dumper.
  */
@@ -2276,6 +2276,15 @@ RTDECL(int) RTCoreDumperSetup(const char *pszOutputDir, uint32_t fFlags)
                               | RTCOREDUMPER_FLAGS_LIVE_CORE)),
                  VERR_INVALID_PARAMETER);
 
+    RT_ZERO(g_szCoreDumpDir);
+    if (pszOutputDir)
+    {
+        if (RTDirExists(pszOutputDir))
+            RTStrCopy(g_szCoreDumpDir, sizeof(g_szCoreDumpDir), pszOutputDir);
+        else
+            return VERR_NOT_A_DIRECTORY;
+    }
+
     /*
      * Install core dump signal handler only if the flags changed or if it's the first time.
      */
@@ -2307,10 +2316,6 @@ RTDECL(int) RTCoreDumperSetup(const char *pszOutputDir, uint32_t fFlags)
         ASMAtomicWriteU32(&g_fCoreDumpFlags, fFlags);
         ASMAtomicWriteBool(&g_fCoreDumpSignalSetup, true);
     }
-
-    RT_ZERO(g_szCoreDumpDir);
-    if (pszOutputDir)
-        RTStrCopy(g_szCoreDumpDir, sizeof(g_szCoreDumpDir), pszOutputDir);
 
     return VINF_SUCCESS;
 }
