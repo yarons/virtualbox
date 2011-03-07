0206183d980cad8c42d1a0b3df1fac95826d2626
Frank Mehnert <noreply@oracle.com>
2011-03-07 16:49:54 +0000

    Additions/common/HGCMInternal: typo which caused to allocate a buffer of cb * PAGE_SIZE
    svn:sync-xref-src-repo-rev: r70397

--- src/VBox/Additions/common/VBoxGuestLib/HGCMInternal.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/HGCMInternal.cpp
@@ -1,4 +1,4 @@
-/* $Revision: 33540 $ */
+/* $Revision: 36193 $ */
 /** @file
  * VBoxGuestLib - Host-Guest Communication Manager internal functions, implemented by VBoxGuest
  */
@@ -399,7 +399,7 @@ static int vbglR0HGCMInternalPreprocessCall(VBoxGuestHGCMCallInfo const *pCallIn
 
                     if (VBGLR0_CAN_USE_PHYS_PAGE_LIST())
                     {
-                        size_t cPages = RTR0MemObjSize(hObj);
+                        size_t const cPages = RTR0MemObjSize(hObj) >> PAGE_SHIFT;
                         *pcbExtra += RT_OFFSETOF(HGCMPageListInfo, aPages[cPages]);
                     }
                 }
