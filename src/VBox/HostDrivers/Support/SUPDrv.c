d3730f3e49d2398fb4db93cb7d91520b1c6390ab
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-03-09 13:05:38 +0000

    SUPDrv.c: Reverted r70430 - Linux does not implement the two functions in questions and will thus cause load failures.
    svn:sync-xref-src-repo-rev: r70438

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -1,4 +1,4 @@
-/* $Revision: 36218 $ */
+/* $Revision: 36223 $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Common code.
  */
@@ -288,13 +288,6 @@ static SUPFUNC g_aFunctions[] =
     { "RTThreadUserReset",                      (void *)RTThreadUserReset },
     { "RTThreadUserWait",                       (void *)RTThreadUserWait },
     { "RTThreadUserWaitNoResume",               (void *)RTThreadUserWaitNoResume },
-#else
-    /** 
-     * @todo: remove me, once above code enabled. 
-     * We need RTThreadCreate/RTThreadWait in the PCI driver.
-     */
-    { "RTThreadCreate",                         (void *)RTThreadCreate },
-    { "RTThreadWait",                           (void *)RTThreadWait },
 #endif
     { "RTThreadPreemptIsEnabled",               (void *)RTThreadPreemptIsEnabled },
     { "RTThreadPreemptIsPending",               (void *)RTThreadPreemptIsPending },
