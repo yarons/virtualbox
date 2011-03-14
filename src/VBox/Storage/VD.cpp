e8496ff1cfb0849ba468db681cc0ac845ea341b8
Alexander Eichner <alexander.eichner@oracle.com>
2011-03-14 22:37:24 +0000

    VD: Fix leak if queuing a I/O request failed
    svn:sync-xref-src-repo-rev: r70553

--- src/VBox/Storage/VD.cpp
+++ src/VBox/Storage/VD.cpp
@@ -1,4 +1,4 @@
-/* $Id: VD.cpp 36221 2011-03-09 11:01:08Z alexander.eichner@oracle.com $ */
+/* $Id: VD.cpp 36278 2011-03-14 22:37:24Z alexander.eichner@oracle.com $ */
 /** @file
  * VBoxHDD - VBox HDD Container implementation.
  */
@@ -3046,6 +3046,7 @@ static int vdIOIntReadUserAsync(void *pvUser, PVDIOSTORAGE pIoStorage,
         else if (rc != VERR_VD_ASYNC_IO_IN_PROGRESS)
         {
             ASMAtomicDecU32(&pIoCtx->cDataTransfersPending);
+            vdIoTaskFree(pDisk, pIoTask);
             break;
         }
 
@@ -3118,6 +3119,7 @@ static int vdIOIntWriteUserAsync(void *pvUser, PVDIOSTORAGE pIoStorage,
         else if (rc != VERR_VD_ASYNC_IO_IN_PROGRESS)
         {
             ASMAtomicDecU32(&pIoCtx->cDataTransfersPending);
+            vdIoTaskFree(pDisk, pIoTask);
             break;
         }
 
