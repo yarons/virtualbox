91f67ca1de260c800f8244e10c9f1662b2c00d4a
Alexander Eichner <alexander.eichner@oracle.com>
2011-03-22 09:45:16 +0000

    DevATA: Disable feature list for now because it breaks at least Windows XP
    svn:sync-xref-src-repo-rev: r70671

--- src/VBox/Devices/Storage/DevATA.cpp
+++ src/VBox/Devices/Storage/DevATA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevATA.cpp 36336 2011-03-21 22:19:21Z alexander.eichner@oracle.com $ */
+/* $Id: DevATA.cpp 36337 2011-03-22 09:45:16Z alexander.eichner@oracle.com $ */
 /** @file
  * VBox storage devices: ATA/ATAPI controller device (disk and cdrom).
  */
@@ -2471,6 +2471,7 @@ static bool atapiGetConfigurationSS(ATADevState *s)
     cbBuf -= cbCopied;
     pbBuf += cbCopied;
 
+#if 0
     cbCopied = atapiGetConfigurationFillFeatureCore(s, pbBuf, cbBuf);
     cbBuf -= cbCopied;
     pbBuf += cbCopied;
@@ -2498,9 +2499,10 @@ static bool atapiGetConfigurationSS(ATADevState *s)
     cbCopied = atapiGetConfigurationFillFeatureTimeout(s, pbBuf, cbBuf);
     cbBuf -= cbCopied;
     pbBuf += cbCopied;
+#endif
 
-    /* Set data length now. */
-    ataH2BE_U32(s->CTX_SUFF(pbIOBuffer), s->cbIOBuffer - cbBuf);
+    /* Set data length now - the field is not included in the final length. */
+    ataH2BE_U32(s->CTX_SUFF(pbIOBuffer), s->cbIOBuffer - cbBuf - 4);
 
     /* Other profiles we might want to add in the future: 0x40 (BD-ROM) and 0x50 (HDDVD-ROM) */
     s->iSourceSink = ATAFN_SS_NULL;
