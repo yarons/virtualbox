6ee7615d4513f94eff4f0112becb41438ab8c996
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-03-25 21:13:29 +0000

    duh
    svn:sync-xref-src-repo-rev: r70807

--- src/VBox/VMM/VMMR3/HWACCM.cpp
+++ src/VBox/VMM/VMMR3/HWACCM.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWACCM.cpp 36441 2011-03-25 21:11:56Z knut.osmundsen@oracle.com $ */
+/* $Id: HWACCM.cpp 36442 2011-03-25 21:13:29Z knut.osmundsen@oracle.com $ */
 /** @file
  * HWACCM - Intel/AMD VM Hardware Support Manager
  */
@@ -643,7 +643,7 @@ VMMR3_INT_DECL(int) HWACCMR3InitCompleted(PVM pVM, VMINITCOMPLETED enmWhat)
         case VMINITCOMPLETED_RING3:
             return hwaccmR3InitCPU(pVM);
         case VMINITCOMPLETED_RING0:
-            return HWACCMR3InitFinalizeR0(pVM);
+            return hwaccmR3InitFinalizeR0(pVM);
         default:
             return VINF_SUCCESS;
     }
