1614cbaeedfabc001ed9561c2c2f60305e87d1b1
Sergey Dubov <sergey.dubov@oracle.com>
2011-04-05 13:02:12 +0000

    FE/Qt: Spelling fix for r70984.
    svn:sync-xref-src-repo-rev: r70991

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxProblemReporter.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxProblemReporter.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxProblemReporter.cpp 36553 2011-04-05 11:28:35Z sergey.dubov@oracle.com $ */
+/* $Id: VBoxProblemReporter.cpp 36558 2011-04-05 13:02:12Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -2085,9 +2085,9 @@ void VBoxProblemReporter::warnAboutCannotCreateMachineFolder(QWidget *pParent, c
 {
     QFileInfo fi(strFolderName);
     message(pParent ? pParent : mainWindowShown(), Critical,
-            tr("<p>Cannot create the machine folder <b>%1</b> in the parent directory <nobr><b>%2</b>.</nobr></p>"
-               "<p>Please check what the parent directory is really exists and "
-               "you have the permissions required to do so.</p>").arg(fi.fileName()).arg(fi.absolutePath()));
+            tr("<p>Cannot create the machine folder <b>%1</b> in the parent folder <nobr><b>%2</b>.</nobr></p>"
+               "<p>Please check that the parent really exists and that you have permissions to create the machine folder.</p>")
+               .arg(fi.fileName()).arg(fi.absolutePath()));
 }
 
 /**
