e5451809513342591e66be86e0540cf2ed4632be
Michal Necasek <michal.necasek@oracle.com>
2011-04-12 15:56:46 +0000

    VMM: Change initial TSS attribs to what guests expect.
    svn:sync-xref-src-repo-rev: r71136

--- src/VBox/VMM/VMMR3/CPUM.cpp
+++ src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 35490 2011-01-11 15:17:10Z knut.osmundsen@oracle.com $ */
+/* $Id: CPUM.cpp 36660 2011-04-12 15:56:46Z michal.necasek@oracle.com $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -1095,7 +1095,7 @@ VMMR3DECL(void) CPUMR3ResetCpu(PVMCPU pVCpu)
 
     pCtx->trHid.u32Limit            = 0xffff;
     pCtx->trHid.Attr.n.u1Present    = 1;
-    pCtx->trHid.Attr.n.u4Type       = X86_SEL_TYPE_SYS_286_TSS_BUSY;
+    pCtx->trHid.Attr.n.u4Type       = X86_SEL_TYPE_SYS_386_TSS_BUSY;    /* Deduction, not properly documented by Intel. */
 
     pCtx->dr[6]                     = X86_DR6_INIT_VAL;
     pCtx->dr[7]                     = X86_DR7_INIT_VAL;
