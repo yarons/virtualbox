5e74960efef3d4605344cffa53db55ab7df25b83
Michael Thayer <noreply@oracle.com>
2011-04-14 10:19:53 +0000

    Installer/linux/runasroot: support kdesu as well
    svn:sync-xref-src-repo-rev: r71155

--- src/VBox/Installer/linux/runasroot.sh
+++ src/VBox/Installer/linux/runasroot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: runasroot.sh 36658 2011-04-12 15:40:51Z noreply@oracle.com $
+# $Id: runasroot.sh 36667 2011-04-14 10:19:53Z noreply@oracle.com $
 ## @file
 # VirtualBox privileged execution helper script for Linux and Solaris
 #
@@ -53,6 +53,13 @@ case "$DISPLAY" in ?*)
     KDESUDO="`mywhich kdesudo`"
     case "$KDESUDO" in ?*)
         eval "`quotify "$KDESUDO"` --comment `quotify "$DESCRIPTION"` -- $COMMAND"
+        exit
+        ;;
+    esac
+
+    KDESU="`mywhich kdesu`"
+    case "$KDESU" in ?*)
+        "$KDESU" -c "$COMMAND"
         exit
         ;;
     esac
