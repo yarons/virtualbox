7355521c0feed82c9dae19d3657e6407dd2a26ca
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2011-04-26 08:48:39 +0000

    3D for VRDP: initial commit, build fix (xTracker 5565).
    svn:sync-xref-src-repo-rev: r71406

--- src/VBox/HostServices/SharedOpenGL/crserverlib/server_muralfbo.c
+++ src/VBox/HostServices/SharedOpenGL/crserverlib/server_muralfbo.c
@@ -1,4 +1,4 @@
-/* $Id: server_muralfbo.c 36843 2011-04-26 08:33:19Z vitali.pelenjow@oracle.com $ */
+/* $Id: server_muralfbo.c 36845 2011-04-26 08:48:39Z vitali.pelenjow@oracle.com $ */
 
 /** @file
  * VBox crOpenGL: Window to FBO redirect support.
@@ -71,7 +71,7 @@ void crServerSetupOutputRedirect(CRMuralInfo *mural)
                                                                   pachFormats, cbFormats, &cbFormats);
             if (RT_SUCCESS(rc))
             {
-                if (strstr(pachFormats, "H3DOR_FMT_RGBA_TOPDOWN"))
+                if (RTStrStr(pachFormats, "H3DOR_FMT_RGBA_TOPDOWN"))
                 {
                     cr_server.outputRedirect.CRORBegin(cr_server.outputRedirect.pvContext,
                                                        &mural->pvOutputRedirectInstance,
