2d1e9659b4721c6e8f3cacd0faa645ee44d52718
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-05-19 13:11:14 +0000

    build fix
    svn:sync-xref-src-repo-rev: r71836

--- src/VBox/Runtime/r3/solaris/thread-affinity-solaris.cpp
+++ src/VBox/Runtime/r3/solaris/thread-affinity-solaris.cpp
@@ -1,4 +1,4 @@
-/* $Id: thread-affinity-solaris.cpp 37154 2011-05-19 12:54:32Z knut.osmundsen@oracle.com $ */
+/* $Id: thread-affinity-solaris.cpp 37156 2011-05-19 13:11:14Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Thread Affinity, Solaris ring-3 implementation.
  */
@@ -40,6 +40,7 @@
 #include <sys/processor.h>
 #include <sys/procset.h>
 #include <unistd.h>
+#include <errno.h>
 
 
 /* Note! The current implementation can only bind to a single CPU. */
@@ -53,7 +54,7 @@ RTR3DECL(int) RTThreadSetAffinity(PCRTCPUSET pCpuSet)
     else
     {
         RTCPUSET PresentSet;
-        int cCpusInSet = RTCpuSetCount(&pCpuSet);
+        int cCpusInSet = RTCpuSetCount(pCpuSet);
         if (cCpusInSet == 1)
         {
             unsigned iCpu = 0;
