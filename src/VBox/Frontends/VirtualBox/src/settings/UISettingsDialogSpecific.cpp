8c88c892733e83b934b3e17259773e134d0eaf0f
Sergey Dubov <sergey.dubov@oracle.com>
2011-05-20 17:00:27 +0000

    FE/Qt: VM Settings Dialog: Prevent machine data auto-update for the time GUI is saving settings.
    svn:sync-xref-src-repo-rev: r71858

--- src/VBox/Frontends/VirtualBox/src/settings/UISettingsDialogSpecific.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/UISettingsDialogSpecific.cpp
@@ -1,4 +1,4 @@
-/* $Id: UISettingsDialogSpecific.cpp 37168 2011-05-20 16:56:46Z sergey.dubov@oracle.com $ */
+/* $Id: UISettingsDialogSpecific.cpp 37169 2011-05-20 17:00:27Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -759,6 +759,9 @@ void UISettingsDialogMachine::saveData()
 
     /* Call for base-class: */
     UISettingsDialog::saveData();
+
+    /* Disconnect global VBox events from this dialog: */
+    gVBoxEvents->disconnect(this);
 
     /* Prepare session: */
     bool fSessionShared = dialogType() != SettingsDialogType_Offline;
