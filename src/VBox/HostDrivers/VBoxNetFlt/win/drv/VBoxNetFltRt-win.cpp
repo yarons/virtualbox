6bcefc15fcb6a857850bbea476fcf7517fdccc77
Mikhail Sennikovsky <noreply@oracle.com>
2011-05-20 07:28:21 +0000

    netflt/win: zero up the device object field on device destruction
    svn:sync-xref-src-repo-rev: r71846

--- src/VBox/HostDrivers/VBoxNetFlt/win/drv/VBoxNetFltRt-win.cpp
+++ src/VBox/HostDrivers/VBoxNetFlt/win/drv/VBoxNetFltRt-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFltRt-win.cpp 36184 2011-03-07 10:57:04Z noreply@oracle.com $ */
+/* $Id: VBoxNetFltRt-win.cpp 37161 2011-05-20 07:28:21Z noreply@oracle.com $ */
 /** @file
  * VBoxNetFltRt-win.cpp - Bridged Networking Driver, Windows Specific Code.
  * NetFlt Runtime
@@ -2962,6 +2962,7 @@ static NDIS_STATUS vboxNetFltWinDevDestroy(PVBOXNETFLTGLOBALS_WIN pGlobals)
     if (Status == NDIS_STATUS_SUCCESS)
     {
         pGlobals->hDevice = NULL;
+        pGlobals->pDevObj = NULL;
     }
     return Status;
 }
