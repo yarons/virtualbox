f5ae2b6e626bb641522228fcc0b5bdf8c5b4de46
Alexander Eichner <alexander.eichner@oracle.com>
2011-05-22 11:01:33 +0000

    Relax the hardening for PC-BSD a bit. They have a group writable application directory. (Thanks to Kris Moore and Bernhard Froehlich)
    svn:sync-xref-src-repo-rev: r71871

--- src/VBox/HostDrivers/Support/SUPR3HardenedVerify.cpp
+++ src/VBox/HostDrivers/Support/SUPR3HardenedVerify.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPR3HardenedVerify.cpp 35403 2011-01-05 11:23:09Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPR3HardenedVerify.cpp 37177 2011-05-22 11:01:33Z alexander.eichner@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Verification of Hardened Installation.
  */
@@ -1186,6 +1186,11 @@ static int supR3HardenedVerifyFsObject(PCSUPR3HARDENEDFSOBJSTATE pFsObjState, bo
            full access. So, to work around we relax the hardening a bit and
            permit grand parents and beyond to be group writable by admin. */
         if (pFsObjState->Stat.st_gid != 80 /*admin*/) /** @todo dynamically resolve the admin group? */
+#endif
+#ifdef RT_OS_FREEBSD
+        /* PC-BSD 9 has group-writable application directory, similar to OSX and
+           their Applications directory */
+        if (pFsObjState->Stat.st_gid != 5 /*operators*/) /* Allow operators group-writes */
 #endif
             return supR3HardenedSetError3(VERR_SUPLIB_WRITE_NON_SYS_GROUP, pErrInfo,
                                           "The group is not a system group and it has write access to '", pszPath, "'");
