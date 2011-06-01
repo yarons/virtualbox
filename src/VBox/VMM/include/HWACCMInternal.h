b2a27cfdc408037bed0765d65c93be5f9829e743
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-06-01 16:30:11 +0000

    HWACCM: Added todo on race in HWACCMR0EnableAllCpus. Hide dummy methods.
    svn:sync-xref-src-repo-rev: r72042

--- src/VBox/VMM/include/HWACCMInternal.h
+++ src/VBox/VMM/include/HWACCMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: HWACCMInternal.h 35346 2010-12-27 16:13:13Z knut.osmundsen@oracle.com $ */
+/* $Id: HWACCMInternal.h 37298 2011-06-01 16:30:11Z knut.osmundsen@oracle.com $ */
 /** @file
  * HWACCM - Internal header file.
  */
@@ -893,18 +893,6 @@ VMMR0DECL(void) HWACCMR0DumpDescriptor(PCX86DESCHC pDesc, RTSEL Sel, const char 
 # define HWACCMDumpRegs(a, b ,c)            do { } while (0)
 # define HWACCMR0DumpDescriptor(a, b, c)    do { } while (0)
 #endif
-
-/* Dummy callback handlers. */
-VMMR0DECL(int) HWACCMR0DummyEnter(PVM pVM, PVMCPU pVCpu, PHWACCM_CPUINFO pCpu);
-VMMR0DECL(int) HWACCMR0DummyLeave(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx);
-VMMR0DECL(int) HWACCMR0DummyEnableCpu(PHWACCM_CPUINFO pCpu, PVM pVM, void *pvPageCpu, RTHCPHYS pPageCpuPhys);
-VMMR0DECL(int) HWACCMR0DummyDisableCpu(PHWACCM_CPUINFO pCpu, void *pvPageCpu, RTHCPHYS pPageCpuPhys);
-VMMR0DECL(int) HWACCMR0DummyInitVM(PVM pVM);
-VMMR0DECL(int) HWACCMR0DummyTermVM(PVM pVM);
-VMMR0DECL(int) HWACCMR0DummySetupVM(PVM pVM);
-VMMR0DECL(int) HWACCMR0DummyRunGuestCode(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx);
-VMMR0DECL(int) HWACCMR0DummySaveHostState(PVM pVM, PVMCPU pVCpu);
-VMMR0DECL(int) HWACCMR0DummyLoadGuestState(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx);
 
 # ifdef VBOX_WITH_KERNEL_USING_XMM
 DECLASM(int)   hwaccmR0VMXStartVMWrapXMM(RTHCUINT fResume, PCPUMCTX pCtx, PVMCSCACHE pCache, PVM pVM, PVMCPU pVCpu, PFNHWACCMVMXSTARTVM pfnStartVM);
