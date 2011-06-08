f66f72e7ae58c32d8fd97aca9c48b739f6dd8934
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-06-08 15:09:14 +0000

    1 million rectangles ought to be enough for anybody...
    svn:sync-xref-src-repo-rev: r72169

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibSeamless.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibSeamless.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR3LibSeamless.cpp 37381 2011-06-08 14:47:58Z noreply@oracle.com $ */
+/* $Id: VBoxGuestR3LibSeamless.cpp 37384 2011-06-08 15:09:14Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxGuestR3Lib - Ring-3 Support Library for VirtualBox guest additions, Seamless mode.
  */
@@ -146,13 +146,13 @@ VBGLR3DECL(int) VbglR3SeamlessSendRects(uint32_t cRects, PRTRECT pRects)
     VMMDevVideoSetVisibleRegion *pReq;
     int rc;
 
-    AssertReturn(pRects || (cRects == 0), VERR_INVALID_PARAMETER);
-    AssertReturn(sizeof(VMMDevVideoSetVisibleRegion)+(cRects-1)*sizeof(RTRECT)
-                 == sizeof(VMMDevVideoSetVisibleRegion)+(uint64_t)(cRects-1)*sizeof(RTRECT), VERR_INVALID_PARAMETER);
+    AssertReturn(pRects || cRects == 0, VERR_INVALID_PARAMETER);
+    AssertMsgReturn(cRects <= _1M, ("%u\n", cRects), VERR_OUT_OF_RANGE);
 
     rc = vbglR3GRAlloc((VMMDevRequestHeader **)&pReq,
                          sizeof(VMMDevVideoSetVisibleRegion)
-                       + cRects * sizeof(RTRECT) - sizeof(RTRECT),
+                       + cRects * sizeof(RTRECT)
+                       - sizeof(RTRECT),
                        VMMDevReq_VideoSetVisibleRegion);
     if (RT_SUCCESS(rc))
     {
