1ded5967b1d4a196c630ee11cda06996ec01ae28
Christian Poetzsch <noreply@oracle.com>
2011-06-14 16:44:47 +0000

    VBoxManage: help item
    svn:sync-xref-src-repo-rev: r72257

--- src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageHelp.cpp 37449 2011-06-14 16:34:16Z noreply@oracle.com $ */
+/* $Id: VBoxManageHelp.cpp 37450 2011-06-14 16:44:47Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - help and other message output.
  */
@@ -334,6 +334,7 @@ void printUsage(USAGECATEGORY u64Cmd, PRTSTREAM pStrm)
         RTStrmPrintf(pStrm,
                      "VBoxManage clonevm          <uuid>|<name>\n"
                      "                            [--snapshot <uuid>|<name>]\n"
+                     "                            [--mode machine|machineandchilds|all]\n"
                      "                            [--name <name>]\n"
                      "                            [--basefolder <basefolder>]\n"
                      "                            [--uuid <uuid>]\n"
