72ef4d005b32cfd410abce35dae8227e047b1118
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-06-15 13:08:45 +0000

    IOM: Clean up locking now that all devices has its own CS.
    svn:sync-xref-src-repo-rev: r72281

--- src/VBox/VMM/testcase/tstVMStructSize.cpp
+++ src/VBox/VMM/testcase/tstVMStructSize.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstVMStructSize.cpp 37354 2011-06-07 15:05:32Z knut.osmundsen@oracle.com $ */
+/* $Id: tstVMStructSize.cpp 37467 2011-06-15 13:08:45Z knut.osmundsen@oracle.com $ */
 /** @file
  * tstVMStructSize - testcase for check structure sizes/alignment
  *                   and to verify that HC and GC uses the same
@@ -377,7 +377,7 @@ int main()
     PRINT_OFFSET(VM, StatGCToQemu);
 #endif
 
-    CHECK_MEMBER_ALIGNMENT(IOM, EmtLock, sizeof(uintptr_t));
+    CHECK_MEMBER_ALIGNMENT(IOM, CritSect, sizeof(uintptr_t));
     CHECK_MEMBER_ALIGNMENT(EM, CritSectREM, sizeof(uintptr_t));
     CHECK_MEMBER_ALIGNMENT(PGM, CritSect, sizeof(uintptr_t));
     CHECK_MEMBER_ALIGNMENT(PDM, CritSect, sizeof(uintptr_t));
