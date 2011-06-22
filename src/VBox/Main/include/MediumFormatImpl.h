52f75dbf70da0143d0df2d3940ddd71d81177fdc
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2011-06-22 12:02:13 +0000

    vcc build fix
    svn:sync-xref-src-repo-rev: r72431

--- src/VBox/Main/include/MediumFormatImpl.h
+++ src/VBox/Main/include/MediumFormatImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MediumFormatImpl.h 37586 2011-06-22 11:49:40Z klaus.espenlaub@oracle.com $ */
+/* $Id: MediumFormatImpl.h 37587 2011-06-22 12:02:13Z klaus.espenlaub@oracle.com $ */
 
 /** @file
  *
@@ -58,7 +58,7 @@ public:
 
     struct Data
     {
-        Data() : capabilities(0) {}
+        Data() : capabilities((MediumFormatCapabilities_T)0) {}
 
         const Utf8Str        strId;
         const Utf8Str        strName;
