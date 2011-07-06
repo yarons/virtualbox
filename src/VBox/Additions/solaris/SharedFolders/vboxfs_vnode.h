27ff0cbd7532fe820d325ee6e45be79f52498ad9
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2011-07-06 15:11:02 +0000

    Additions/Solaris/SharedFolders: readonly mmap support bits.
    svn:sync-xref-src-repo-rev: r72685

--- src/VBox/Additions/solaris/SharedFolders/vboxfs_vnode.h
+++ src/VBox/Additions/solaris/SharedFolders/vboxfs_vnode.h
@@ -1,4 +1,4 @@
-/* $Id: vboxfs_vnode.h 31691 2010-08-16 12:59:23Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: vboxfs_vnode.h 37806 2011-07-06 15:11:02Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VirtualBox File System for Solaris Guests, VNode header.
  */
@@ -60,6 +60,9 @@ typedef struct sfnode {
 	uint64_t	sf_stat_time;	/* last-modified time of sf_stat */
 	sffs_dirents_t	*sf_dir_list;	/* list of entries for this directory */
 	sffs_stats_t	*sf_dir_stats;	/* file attrs for the above entries */
+#if 0
+    volatile uint64_t	sf_mapcnt;  /* number of mapped pages */
+#endif
 } sfnode_t;
 
 #define VN2SFN(vp) ((sfnode_t *)(vp)->v_data)
