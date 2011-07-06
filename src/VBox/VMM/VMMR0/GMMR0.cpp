ecca4c62589843b6b328395519944d0cb993d9db
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-07-06 14:57:54 +0000

    GMMR0: Reset the page descriptors when bailing out on a allocation failure.
    svn:sync-xref-src-repo-rev: r72683

--- src/VBox/VMM/VMMR0/GMMR0.cpp
+++ src/VBox/VMM/VMMR0/GMMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: GMMR0.cpp 37803 2011-07-06 14:45:27Z knut.osmundsen@oracle.com $ */
+/* $Id: GMMR0.cpp 37804 2011-07-06 14:57:54Z knut.osmundsen@oracle.com $ */
 /** @file
  * GMM - Global Memory Manager.
  */
@@ -2542,6 +2542,9 @@ static int gmmR0AllocatePagesNew(PGMM pGMM, PGVM pGVM, uint32_t cPages, PGMMPAGE
             }
             else
                 AssertMsgFailed(("idPage=%#x\n", idPage));
+            paPages[iPage].idPage       = NIL_GMM_PAGEID;
+            paPages[iPage].idSharedPage = NIL_GMM_PAGEID;
+            paPages[iPage].HCPhysGCPhys = NIL_RTHCPHYS;
         }
 
         /* Free empty chunks. */
