99c6fa0597ba9fe1c688d90bc33b4aa51fddc16b
Frank Mehnert <noreply@oracle.com>
2011-07-07 08:15:02 +0000

    gcc-4.6 warnings (shadowed declaration)
    svn:sync-xref-src-repo-rev: r72690

--- src/VBox/Main/src-client/ConsoleVRDPServer.cpp
+++ src/VBox/Main/src-client/ConsoleVRDPServer.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleVRDPServer.cpp 37282 2011-06-01 02:56:05Z noreply@oracle.com $ */
+/* $Id: ConsoleVRDPServer.cpp 37809 2011-07-07 08:15:02Z noreply@oracle.com $ */
 /** @file
  * VBox Console VRDP Helper class
  */
@@ -2137,7 +2137,7 @@ AuthResult ConsoleVRDPServer::Authenticate(const Guid &uuid, AuthGuestJudgement 
 
         if (RT_SUCCESS(rc))
         {
-            typedef struct AuthEntryInfo
+            typedef struct AuthEntryInfoStruct
             {
                 const char *pszName;
                 void **ppvAddress;
