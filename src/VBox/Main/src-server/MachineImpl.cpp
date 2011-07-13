0b2f9b0d6b65a6b79f869fb2cfdbde5b5709bf1c
Frank Mehnert <noreply@oracle.com>
2011-07-13 20:39:58 +0000

    Main: TODO
    svn:sync-xref-src-repo-rev: r72842

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 37930 2011-07-13 19:56:55Z noreply@oracle.com $ */
+/* $Id: MachineImpl.cpp 37931 2011-07-13 20:39:58Z noreply@oracle.com $ */
 /** @file
  * Implementation of IMachine in VBoxSVC.
  */
@@ -793,6 +793,11 @@ void Machine::uninit()
 
     // uninit media from this machine's media registry, if they're still there
     Guid uuidMachine(getId());
+
+    /* XXX This will fail with
+     *   "cannot be closed because it is still attached to 1 virtual machines"
+     * because at this point we did not call uninitDataAndChildObjects() yet
+     * and therefore also removeBackReference() for all these mediums was not called! */
     if (!uuidMachine.isEmpty())     // can be empty if we're called from a failure of Machine::init
         mParent->unregisterMachineMedia(uuidMachine);
 
