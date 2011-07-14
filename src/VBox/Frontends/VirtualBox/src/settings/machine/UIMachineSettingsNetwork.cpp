23198611984a5b601aa8dd7f085b950c8c4325e9
Frank Mehnert <noreply@oracle.com>
2011-07-14 08:56:03 +0000

    FE/Qt: allow to change generic networking attributes at runtime
    svn:sync-xref-src-repo-rev: r72853

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsNetwork.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsNetwork.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsNetwork.cpp 37779 2011-07-05 12:13:15Z klaus.espenlaub@oracle.com $ */
+/* $Id: UIMachineSettingsNetwork.cpp 37940 2011-07-14 08:56:03Z noreply@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -293,7 +293,6 @@ void UIMachineSettingsNetwork::polishTab()
     m_pMACLabel->setEnabled(m_pParent->isMachineOffline());
     m_pMACEditor->setEnabled(m_pParent->isMachineOffline());
     m_pMACButton->setEnabled(m_pParent->isMachineOffline());
-    m_pGenericPropertiesTextEdit->setEnabled(m_pParent->isMachineOffline());
     m_pPortForwardingButton->setEnabled(m_pParent->isMachineInValidMode() &&
                                         attachmentType() == KNetworkAttachmentType_NAT);
 
