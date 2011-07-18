831ee58b306417adf3a390f9e0110cfe0f5be481
Andreas Loeffler <andreas.loeffler@oracle.com>
2011-07-18 08:51:46 +0000

    Initialize extended common control classes.
    svn:sync-xref-src-repo-rev: r72922

--- src/VBox/Installer/win/Stub/VBoxStub.cpp
+++ src/VBox/Installer/win/Stub/VBoxStub.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxStub.cpp 37987 2011-07-18 08:11:16Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxStub.cpp 37989 2011-07-18 08:51:46Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxStub - VirtualBox's Windows installer stub.
  */
@@ -574,6 +574,9 @@ int WINAPI WinMain(HINSTANCE  hInstance,
                          * styles (XP+). Also, an integrated manifest is required. */
                         INITCOMMONCONTROLSEX ccEx;
                         ccEx.dwSize = sizeof(INITCOMMONCONTROLSEX);
+                        ccEx.dwICC = ICC_LINK_CLASS | ICC_LISTVIEW_CLASSES | ICC_PAGESCROLLER_CLASS |
+                                     ICC_PROGRESS_CLASS | ICC_STANDARD_CLASSES | ICC_TAB_CLASSES | ICC_TREEVIEW_CLASSES |
+                                     ICC_UPDOWN_CLASS | ICC_USEREX_CLASSES | ICC_WIN95_CLASSES;
                         InitCommonControlsEx(&ccEx); /* Ignore failure. */
 
                         UINT uStatus = ::MsiInstallProductA(pszTempFile, szMSIArgs);
