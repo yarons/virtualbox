aff49dea2b5f9903a4e8e50dbff271f44450fc64
Vasily Levchenko <noreply@oracle.com>
2011-07-19 09:17:18 +0000

    NAT: typo.
    svn:sync-xref-src-repo-rev: r73001

--- src/VBox/Devices/Network/slirp/socket.c
+++ src/VBox/Devices/Network/slirp/socket.c
@@ -1,4 +1,4 @@
-/* $Id: socket.c 37936 2011-07-14 03:54:41Z noreply@oracle.com $ */
+/* $Id: socket.c 38057 2011-07-19 09:17:18Z noreply@oracle.com $ */
 /** @file
  * NAT - socket handling.
  */
@@ -625,7 +625,7 @@ sowrite(PNATState pData, struct socket *so)
     /* Update sbuf */
     sb->sb_cc -= nn;
     sb->sb_rptr += nn;
-    Log2(("%s: update so_rcv (written nn = %d) %R[sbuf]\n", __PRETTY_FUNCTION__, sb, nn));
+    Log2(("%s: update so_rcv (written nn = %d) %R[sbuf]\n", __PRETTY_FUNCTION__, nn, sb));
     if (sb->sb_rptr >= (sb->sb_data + sb->sb_datalen))
     {
         sb->sb_rptr -= sb->sb_datalen;
