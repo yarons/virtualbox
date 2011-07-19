7ad55656e565a397501b53492284fa5e3fa8a4e5
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2011-07-19 09:55:57 +0000

    netflt: another attempt to fix regression introduced in r72926
    svn:sync-xref-src-repo-rev: r73006

--- src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
+++ src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFlt-linux.c 38054 2011-07-19 08:50:19Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxNetFlt-linux.c 38062 2011-07-19 09:55:57Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Linux Specific Code.
  */
@@ -962,9 +962,10 @@ static void vboxNetFltLinuxHookDev(PVBOXNETFLTINS pThis, struct net_device *pDev
     if (!pOverride)
         return;
     pOverride->pOrgOps              = pDev->OVR_OPS;
-    /* We only need to save ethtool_ops structure if it is present (#5712) */
-    if (VALID_PTR(pDev->OVR_OPS))
-        pOverride->Ops              = *pDev->OVR_OPS;
+    /* Cancel override if ethtool_ops is missing (host-only case, #5712) */
+    if (!VALID_PTR(pDev->OVR_OPS))
+        return;
+    pOverride->Ops                  = *pDev->OVR_OPS;
 # if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 29)
     pOverride->pfnStartXmit         = pDev->hard_start_xmit;
 # else /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 29) */
