264d2e44c5802ae98bf921e9e4172883dc6e74a0
Vasily Levchenko <noreply@oracle.com>
2011-07-22 03:42:21 +0000

    NAT: logs
    svn:sync-xref-src-repo-rev: r73061

--- src/VBox/Devices/Network/slirp/slirp.c
+++ src/VBox/Devices/Network/slirp/slirp.c
@@ -1,4 +1,4 @@
-/* $Id: slirp.c 38101 2011-07-21 14:16:20Z noreply@oracle.com $ */
+/* $Id: slirp.c 38109 2011-07-22 03:42:21Z noreply@oracle.com $ */
 /** @file
  * NAT - slirp glue.
  */
@@ -2080,11 +2080,13 @@ void slirp_arp_who_has(PNATState pData, uint32_t dst)
     struct mbuf *m;
     struct ethhdr *ehdr;
     struct arphdr *ahdr;
+    LogFunc(("ENTER: %RTnaipv4\n", dst));
 
     m = m_getcl(pData, M_NOWAIT, MT_HEADER, M_PKTHDR);
     if (m == NULL)
     {
         Log(("NAT: Can't alloc mbuf for ARP request\n"));
+        LogFlowFuncLeave();
         return;
     }
     ehdr = mtod(m, struct ethhdr *);
@@ -2106,6 +2108,7 @@ void slirp_arp_who_has(PNATState pData, uint32_t dst)
     m->m_data += ETH_HLEN;
     m->m_len -= ETH_HLEN;
     if_encap(pData, ETH_P_ARP, m, ETH_ENCAP_URG);
+    LogFlowFuncLeave();
 }
 
 /* updates the arp cache
