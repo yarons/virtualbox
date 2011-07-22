7205e5596dd9c74dca82352a931ebb72cf491ebd
Mikhail Sennikovsky <noreply@oracle.com>
2011-07-22 13:58:48 +0000

    wddm/3d: missing files
    svn:sync-xref-src-repo-rev: r73066

--- src/VBox/Additions/WINNT/Graphics/Video/disp/wddm/VBoxDispMp.h
+++ src/VBox/Additions/WINNT/Graphics/Video/disp/wddm/VBoxDispMp.h
@@ -1,11 +1,11 @@
-/* $Id: VBoxDispMp.h 36867 2011-04-28 07:27:03Z noreply@oracle.com $ */
+/* $Id: VBoxDispMp.h 38114 2011-07-22 13:58:48Z noreply@oracle.com $ */
 
 /** @file
- * VBoxVideo Display D3D User mode dll
+ * VBoxVideo Display external interface
  */
 
 /*
- * Copyright (C) 2011 Oracle Corporation
+ * Copyright (C) 2010 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
