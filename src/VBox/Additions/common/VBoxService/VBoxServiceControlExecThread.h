4be2a5e2f116a6ecc2e6c97f0da8674a3e116652
Andreas Loeffler <andreas.loeffler@oracle.com>
2011-07-22 13:57:35 +0000

    VBoxService/GuestCtrl: Fixed stability issues due to PID recycling, enhanced and unified debug logging along with thread names to stdout.
    svn:sync-xref-src-repo-rev: r73065

--- src/VBox/Additions/common/VBoxService/VBoxServiceControlExecThread.h
+++ src/VBox/Additions/common/VBoxService/VBoxServiceControlExecThread.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceControlExecThread.h 36548 2011-04-05 09:27:33Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxServiceControlExecThread.h 38113 2011-07-22 13:57:35Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxServiceControlExecThread - Thread for an executed guest process.
  */
@@ -26,6 +26,7 @@ int VBoxServiceControlExecThreadAlloc(PVBOXSERVICECTRLTHREAD pThread,
                                       const char *pszArgs, uint32_t uNumArgs,
                                       const char *pszEnv, uint32_t cbEnv, uint32_t uNumEnvVars,
                                       const char *pszUser, const char *pszPassword, uint32_t uTimeLimitMS);
+int VBoxServiceControlExecThreadAssignPID(PVBOXSERVICECTRLTHREADDATAEXEC pData, uint32_t uPID);
 void VBoxServiceControlExecThreadDestroy(PVBOXSERVICECTRLTHREADDATAEXEC pData);
 int VBoxServiceControlExecThreadSetInput(uint32_t uPID, bool fPendingClose, uint8_t *pBuf,
                                          uint32_t cbSize, uint32_t *pcbWritten);
