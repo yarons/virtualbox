d76ae926ae2f88b4a3170d55897953b1e5cb3ccd
Andreas Loeffler <andreas.loeffler@oracle.com>
2011-08-03 09:33:11 +0000

    Build fix.
    svn:sync-xref-src-repo-rev: r73300

--- src/VBox/Main/src-client/GuestCtrlImpl.cpp
+++ src/VBox/Main/src-client/GuestCtrlImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestCtrlImpl.cpp 38290 2011-08-03 09:22:12Z andreas.loeffler@oracle.com $ */
+/* $Id: GuestCtrlImpl.cpp 38293 2011-08-03 09:33:11Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation: Guest
  */
@@ -1171,7 +1171,7 @@ HRESULT Guest::executeProcessResult(const char *pszCommand, const char *pszUser,
 
 /**
  * Gets the next stream block from a formerly processed guest stream. Will return
- * E_PENDING if not enough guest stream data was read yet, otherwise S_OK or an appropriate
+ * E_UNEXPECTED if not enough guest stream data was read yet, otherwise S_OK or an appropriate
  * error.
  *
  * @return  HRESULT
@@ -1186,7 +1186,6 @@ HRESULT Guest::executeStreamCollectBlock(ULONG aPID,
 
     SafeArray<BYTE> aOutputData;
     ULONG cbOutputData = 0;
-    int vrc = VINF_SUCCESS;
     for (;;)
     {
         rc = this->GetProcessOutput(aPID, ProcessOutputFlag_None,
@@ -1225,7 +1224,7 @@ HRESULT Guest::executeStreamCollectBlock(ULONG aPID,
         else /* No more output! */
         {
             if (vrc == VERR_MORE_DATA)
-                rc = E_PENDING; /** @todo Find a better rc! */
+                rc = E_UNEXPECTED; /** @todo Find a better rc! */
             break;
         }
     }
