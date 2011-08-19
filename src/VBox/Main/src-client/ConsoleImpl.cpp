4a92326848213332921ea22b037261ef6bcbf416
Christian Poetzsch <noreply@oracle.com>
2011-08-19 09:06:33 +0000

    Main: shared folder setup: reorder the path checks; if a shared folder not exists this should be reported first (otherwise the error is very misleading)
    svn:sync-xref-src-repo-rev: r73593

--- src/VBox/Main/src-client/ConsoleImpl.cpp
+++ src/VBox/Main/src-client/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 37851 2011-07-08 17:04:03Z klaus.espenlaub@oracle.com $ */
+/* $Id: ConsoleImpl.cpp 38495 2011-08-19 09:06:33Z noreply@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation
  */
@@ -7133,7 +7133,6 @@ HRESULT Console::createSharedFolder(const Utf8Str &strName, const SharedFolderDa
     Log(("Adding shared folder '%s' -> '%s'\n", strName.c_str(), aData.m_strHostPath.c_str()));
 
     // check whether the path is valid and exists
-    /* Check whether the path is full (absolute) */
     char hostPathFull[RTPATH_MAX];
     int vrc = RTPathAbsEx(NULL,
                           aData.m_strHostPath.c_str(),
@@ -7143,14 +7142,14 @@ HRESULT Console::createSharedFolder(const Utf8Str &strName, const SharedFolderDa
         return setError(E_INVALIDARG,
                         tr("Invalid shared folder path: '%s' (%Rrc)"),
                         aData.m_strHostPath.c_str(), vrc);
-
-    if (RTPathCompare(aData.m_strHostPath.c_str(), hostPathFull) != 0)
-        return setError(E_INVALIDARG,
-                        tr("Shared folder path '%s' is not absolute"),
-                        aData.m_strHostPath.c_str());
     if (!RTPathExists(hostPathFull))
         return setError(E_INVALIDARG,
                         tr("Shared folder path '%s' does not exist on the host"),
+                        aData.m_strHostPath.c_str());
+    /* Check whether the path is full (absolute) */
+    if (RTPathCompare(aData.m_strHostPath.c_str(), hostPathFull) != 0)
+        return setError(E_INVALIDARG,
+                        tr("Shared folder path '%s' is not absolute"),
                         aData.m_strHostPath.c_str());
 
     // now that we know the path is good, give it to HGCM
