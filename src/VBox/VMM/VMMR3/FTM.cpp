c4aba6a6697ff911f47381202b36f8c33fcd7adf
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-09-02 12:33:03 +0000

    doxygen bugs.
    svn:sync-xref-src-repo-rev: r73820

--- src/VBox/VMM/VMMR3/FTM.cpp
+++ src/VBox/VMM/VMMR3/FTM.cpp
@@ -1,4 +1,4 @@
-/* $Id: FTM.cpp 35346 2010-12-27 16:13:13Z knut.osmundsen@oracle.com $ */
+/* $Id: FTM.cpp 38614 2011-09-02 12:33:03Z knut.osmundsen@oracle.com $ */
 /** @file
  * FTM - Fault Tolerance Manager
  */
@@ -1232,13 +1232,13 @@ static DECLCALLBACK(VBOXSTRICTRC) ftmR3SetCheckpointRendezvous(PVM pVM, PVMCPU p
     int rc = VINF_SUCCESS;
     bool fSuspended = false;
 
-    /** We don't call VMR3Suspend here to avoid the overhead of state changes and notifications. This
-     *  is only a short suspend.
+    /* We don't call VMR3Suspend here to avoid the overhead of state changes and notifications. This
+     * is only a short suspend.
      */
     STAM_PROFILE_START(&pVM->ftm.s.StatCheckpointPause, a);
     PDMR3Suspend(pVM);
 
-    /** Hack alert: as EM is responsible for dealing with the suspend state. We must do this here ourselves, but only for this EMT.*/
+    /* Hack alert: as EM is responsible for dealing with the suspend state. We must do this here ourselves, but only for this EMT.*/
     EMR3NotifySuspend(pVM);
     STAM_PROFILE_STOP(&pVM->ftm.s.StatCheckpointPause, a);
 
@@ -1270,14 +1270,14 @@ static DECLCALLBACK(VBOXSTRICTRC) ftmR3SetCheckpointRendezvous(PVM pVM, PVMCPU p
     rc = PGMR3PhysWriteProtectRAM(pVM);
     AssertRC(rc);
 
-    /** We don't call VMR3Resume here to avoid the overhead of state changes and notifications. This
-     *  is only a short suspend.
+    /* We don't call VMR3Resume here to avoid the overhead of state changes and notifications. This
+     * is only a short suspend.
      */
     STAM_PROFILE_START(&pVM->ftm.s.StatCheckpointResume, b);
     PGMR3ResetNoMorePhysWritesFlag(pVM);
     PDMR3Resume(pVM);
 
-    /** Hack alert as EM is responsible for dealing with the suspend state. We must do this here ourselves, but only for this EMT.*/
+    /* Hack alert as EM is responsible for dealing with the suspend state. We must do this here ourselves, but only for this EMT.*/
     EMR3NotifyResume(pVM);
     STAM_PROFILE_STOP(&pVM->ftm.s.StatCheckpointResume, b);
 
