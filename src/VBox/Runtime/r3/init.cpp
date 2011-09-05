0f9dcf1bdfa6622941e1bf2e197a3fa0b0bc9205
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-09-05 14:02:29 +0000

    iprt: Change the console CP to CP_ACP if initializing from an executable. (untested)
    svn:sync-xref-src-repo-rev: r73845

--- src/VBox/Runtime/r3/init.cpp
+++ src/VBox/Runtime/r3/init.cpp
@@ -1,4 +1,4 @@
-/* $Id: init.cpp 38636 2011-09-05 13:49:45Z knut.osmundsen@oracle.com $ */
+/* $Id: init.cpp 38638 2011-09-05 14:02:29Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Init Ring-3.
  */
@@ -33,6 +33,7 @@
 
 #ifdef RT_OS_WINDOWS
 # include <process.h>
+# include <Windows.h>
 #else
 # include <unistd.h>
 # ifndef RT_OS_OS2
@@ -315,6 +316,18 @@ static int rtR3InitBody(uint32_t fFlags, int cArgs, char ***papszArgs, const cha
      * paths or we'll end up using the "C" locale for path conversion.
      */
     setlocale(LC_CTYPE, "");
+
+#ifdef RT_OS_WINDOWS
+    /*
+     * On windows, make sure we talk CP_ACP to the console. It frequently uses
+     * a different code page, which would cause %[lL]S to fail.
+     */
+    if (!(fFlags & RTR3INIT_FLAGS_DLL))
+    {
+        SetConsoleCP(GetACP());
+        SetConsoleOutputCP(GetACP());
+    }
+#endif
 
     /*
      * The Process ID.
