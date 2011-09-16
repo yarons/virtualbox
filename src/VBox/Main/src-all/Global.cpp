bd1c4bd9001b47eb49587564c4c03bc7d1e5c401
Frank Mehnert <noreply@oracle.com>
2011-09-16 06:42:57 +0000

    Windows 8: VT-x/AMD-V and I/O APIC
    svn:sync-xref-src-repo-rev: r74058

--- src/VBox/Main/src-all/Global.cpp
+++ src/VBox/Main/src-all/Global.cpp
@@ -1,4 +1,4 @@
-/* $Id: Global.cpp 38755 2011-09-15 08:24:23Z noreply@oracle.com $ */
+/* $Id: Global.cpp 38770 2011-09-16 06:42:57Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -100,7 +100,7 @@ const Global::OSType Global::sOSTypes[SchemaDefs::OSTypeId_COUNT] =
        512,  16, 20 * _1G64, NetworkAdapterType_I82540EM, 0, StorageControllerType_PIIX4, StorageBus_IDE,
         StorageControllerType_IntelAhci, StorageBus_SATA, ChipsetType_PIIX3, AudioControllerType_HDA  },
     { "Windows", "Microsoft Windows", SchemaDefs_OSTypeId_Windows8,        "Windows 8",
-      VBOXOSTYPE_Win8,            VBOXOSHINT_NONE | VBOXOSHINT_USBTABLET,
+      VBOXOSTYPE_Win8,            VBOXOSHINT_NONE | VBOXOSHINT_HWVIRTEX | VBOXOSHINT_IOAPIC | VBOXOSHINT_USBTABLET,
        1536,  16, 20 * _1G64, NetworkAdapterType_I82540EM, 0, StorageControllerType_PIIX4, StorageBus_IDE,
         StorageControllerType_IntelAhci, StorageBus_SATA, ChipsetType_PIIX3, AudioControllerType_HDA  },
     { "Windows", "Microsoft Windows", SchemaDefs_OSTypeId_Windows8_64,     "Windows 8 (64 bit)",
