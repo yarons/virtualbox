3d77ec91a70084ca1bad83fe5ccf3c9cfb7f6709
Frank Mehnert <noreply@oracle.com>
2011-09-16 07:26:32 +0000

    debugging
    svn:sync-xref-src-repo-rev: r74060

--- src/VBox/Additions/WINNT/Graphics/Video/mp/xpdm/VBoxMPDriver.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/xpdm/VBoxMPDriver.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMPDriver.cpp 38635 2011-09-05 13:12:08Z vitali.pelenjow@oracle.com $ */
+/* $Id: VBoxMPDriver.cpp 38771 2011-09-16 07:26:32Z noreply@oracle.com $ */
 
 /** @file
  * VBox XPDM Miniport driver interface functions
@@ -656,6 +656,7 @@ ULONG DriverEntry(IN PVOID Context1, IN PVOID Context2)
     PAGED_CODE();
 
     int irc = RTR0Init(0);
+    LOGREL(("RTR0Init rc = %d\n", irc));
     if (RT_FAILURE(irc))
     {
         LogRel(("VBoxMP::failed to init IPRT (rc=%#x)", irc));
@@ -718,9 +719,10 @@ ULONG DriverEntry(IN PVOID Context1, IN PVOID Context2)
     ULONG rc = VideoPortInitialize(Context1, Context2, &vhwData, NULL);
     if (rc != NO_ERROR)
     {
-        LOG(("VideoPortInitialize failed with %#x", rc));
+        LOGREL(("VideoPortInitialize failed with %#x", rc));
     }
 
+    LOGREL(("Driver Entry leave rc = %d\n", rc));
     LOGF_LEAVE();
     return rc;
 }
