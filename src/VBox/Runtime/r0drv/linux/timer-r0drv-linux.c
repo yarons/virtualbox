fb29dda1a8da01f06954ad2fc07fdd2b2a4cf784
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-10-17 14:51:37 +0000

    timer-r0drv-linux.c: Make 100% sure we ever call del_timer_sync while in atomic or interrupt context.
    svn:sync-xref-src-repo-rev: r74410

--- src/VBox/Runtime/r0drv/linux/timer-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/timer-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: timer-r0drv-linux.c 39004 2011-10-17 14:12:14Z knut.osmundsen@oracle.com $ */
+/* $Id: timer-r0drv-linux.c 39007 2011-10-17 14:51:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Timers, Ring-0 Driver, Linux.
  */
@@ -1405,6 +1405,7 @@ RTDECL(int) RTTimerDestroy(PRTTIMER pTimer)
     AssertPtrReturn(pTimer, VERR_INVALID_HANDLE);
     AssertReturn(pTimer->u32Magic == RTTIMER_MAGIC, VERR_INVALID_HANDLE);
     RTTIMERLNX_LOG(("destroy %p\n", pTimer));
+/** @todo We should invalidate the magic here! */
 
     /*
      * Stop the timer if it's still active, then destroy it if we can.
@@ -1450,7 +1451,14 @@ RTDECL(int) RTTimerDestroy(PRTTIMER pTimer)
     }
 
     if (fCanDestroy)
-        rtTimerLnxDestroyIt(pTimer);
+    {
+        /* For paranoid reasons, defer actually destroying the semaphore when
+           in atomic or interrupt context. */
+        if (in_atomic() || in_interrupt())
+            rtR0LnxWorkqueuePush(&pTimer->DtorWorkqueueItem, rtTimerLnxDestroyDeferred);
+        else
+            rtTimerLnxDestroyIt(pTimer);
+    }
 
     return VINF_SUCCESS;
 }
