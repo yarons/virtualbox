098c0fb34359e935098d13eb9c2ee513f0e2270e
Alexander Eichner <alexander.eichner@oracle.com>
2011-10-20 13:12:30 +0000

    VMDK: Improve error message if a stream optimized image is corrupted
    svn:sync-xref-src-repo-rev: r74474

--- src/VBox/Storage/VMDK.cpp
+++ src/VBox/Storage/VMDK.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMDK.cpp 38876 2011-09-27 09:03:15Z alexander.eichner@oracle.com $ */
+/* $Id: VMDK.cpp 39052 2011-10-20 13:12:30Z alexander.eichner@oracle.com $ */
 /** @file
  * VMDK disk image, core code.
  */
@@ -744,7 +744,11 @@ DECLINLINE(int) vmdkFileInflateSync(PVMDKIMAGE pImage, PVMDKEXTENT pExtent,
         rc = RTZipDecompress(pZip, pvBuf, cbToRead, &cbActuallyRead);
         RTZipDecompDestroy(pZip);
         if (RT_FAILURE(rc))
+        {
+            if (rc == VERR_ZIP_CORRUPTED)
+                rc = vdIfError(pImage->pIfError, rc, RT_SRC_POS, N_("VMDK: Compressed image is corrupted '%s'"), pExtent->pszFullname);
             return rc;
+        }
         if (cbActuallyRead != cbToRead)
             rc = VERR_VD_VMDK_INVALID_FORMAT;
         return rc;
