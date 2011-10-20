caf6fe97ae04ef48f0e4b12f6f42e072fffc5f66
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-10-20 14:53:51 +0000

    DevApic,PDM: Combine the two read functions (MSR/MMIO); renamed debug info handler from lapic -> apic so it matches the device name; misc cleanups.
    svn:sync-xref-src-repo-rev: r74482

--- src/VBox/VMM/VMMAll/PDMAll.cpp
+++ src/VBox/VMM/VMMAll/PDMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMAll.cpp 37475 2011-06-15 16:42:55Z knut.osmundsen@oracle.com $ */
+/* $Id: PDMAll.cpp 39060 2011-10-20 14:53:51Z knut.osmundsen@oracle.com $ */
 /** @file
  * PDM Critical Sections
  */
@@ -335,9 +335,7 @@ VMMDECL(int) PDMApicReadMSR(PVM pVM, VMCPUID iCpu, uint32_t u32Reg, uint64_t *pu
     if (pVM->pdm.s.Apic.CTX_SUFF(pDevIns))
     {
         AssertPtr(pVM->pdm.s.Apic.CTX_SUFF(pfnReadMSR));
-        pdmLock(pVM);
         int rc = pVM->pdm.s.Apic.CTX_SUFF(pfnReadMSR)(pVM->pdm.s.Apic.CTX_SUFF(pDevIns), iCpu, u32Reg, pu64Value);
-        pdmUnlock(pVM);
         return rc;
     }
     return VERR_PDM_NO_APIC_INSTANCE;
