fe1894346b285a1e23612249eb33008a6d34c725
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-10-21 10:21:06 +0000

    build fix
    svn:sync-xref-src-repo-rev: r74493

--- src/VBox/Runtime/common/misc/lockvalidator.cpp
+++ src/VBox/Runtime/common/misc/lockvalidator.cpp
@@ -1,4 +1,4 @@
-/* $Id: lockvalidator.cpp 39070 2011-10-21 09:41:18Z knut.osmundsen@oracle.com $ */
+/* $Id: lockvalidator.cpp 39071 2011-10-21 10:21:06Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Lock Validator.
  */
@@ -1845,7 +1845,7 @@ static uint32_t rtLockValidatorStackDepth(PRTTHREADINT pThread)
 }
 
 
-#ifdef VBOX_STRICT
+#ifdef RT_STRICT
 /**
  * Checks if the stack contains @a pRec.
  *
@@ -1884,7 +1884,7 @@ static bool rtLockValidatorStackContainsRec(PRTTHREADINT pThreadSelf, PRTLOCKVAL
     }
     return false;
 }
-#endif /* VBOX_STRICT */
+#endif /* RT_STRICT */
 
 
 /**
