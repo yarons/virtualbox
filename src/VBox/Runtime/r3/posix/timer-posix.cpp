918e6e39842ca88b0d36f030d01d75f99f59f7cd
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-10-24 14:01:50 +0000

    build fix
    svn:sync-xref-src-repo-rev: r74525

--- src/VBox/Runtime/r3/posix/timer-posix.cpp
+++ src/VBox/Runtime/r3/posix/timer-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: timer-posix.cpp 39091 2011-10-24 13:58:22Z knut.osmundsen@oracle.com $ */
+/* $Id: timer-posix.cpp 39093 2011-10-24 14:01:50Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Timer, POSIX.
  */
@@ -294,11 +294,11 @@ static DECLCALLBACK(int) rttimerThread(RTTHREAD hThreadSelf, void *pvArg)
         {
             ASMAtomicXchgU8(&pTimer->fSuspended, true);
             pTimer->iError = RTErrConvertFromErrno(errno);
-            RTThreadUserSignal(Thread);
+            RTThreadUserSignal(hThreadSelf);
             continue; /* back to suspended mode. */
         }
         pTimer->iError = 0;
-        RTThreadUserSignal(Thread);
+        RTThreadUserSignal(hThreadSelf);
 
         /*
          * Timer Service Loop.
@@ -352,7 +352,7 @@ static DECLCALLBACK(int) rttimerThread(RTTHREAD hThreadSelf, void *pvArg)
         if (!pTimer->fDestroyed)
         {
             pTimer->iError = 0;
-            RTThreadUserSignal(Thread);
+            RTThreadUserSignal(hThreadSelf);
         }
     }
 
@@ -360,7 +360,7 @@ static DECLCALLBACK(int) rttimerThread(RTTHREAD hThreadSelf, void *pvArg)
      * Exit.
      */
     pTimer->iError = 0;
-    RTThreadUserSignal(Thread);
+    RTThreadUserSignal(hThreadSelf);
 
 #else /* IPRT_WITH_POSIX_TIMERS */
 
