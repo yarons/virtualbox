4db4bb9bd7c1f525b2ac2c23d53438075b40f018
Frank Mehnert <noreply@oracle.com>
2011-11-01 12:54:03 +0000

    VBoxManage/doc: typos
    svn:sync-xref-src-repo-rev: r74623

--- src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageHelp.cpp 39119 2011-10-26 13:07:47Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxManageHelp.cpp 39159 2011-11-01 12:54:03Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - help and other message output.
  */
@@ -545,12 +545,12 @@ void printUsage(USAGECATEGORY u64Cmd, PRTSTREAM pStrm)
                      "VBoxManage convertfromraw   <filename> <outputfile>\n"
                      "                            [--format VDI|VMDK|VHD]\n"
                      "                            [--variant Standard,Fixed,Split2G,Stream,ESX]\n"
-                     "                            [--uuid <uuid>\n>"
+                     "                            [--uuid <uuid>\n"
 #ifndef RT_OS_WINDOWS
                      "VBoxManage convertfromraw   stdin <outputfile> <bytes>\n"
                      "                            [--format VDI|VMDK|VHD]\n"
                      "                            [--variant Standard,Fixed,Split2G,Stream,ESX]\n"
-                     "                            [--uuid <uuid>\n>"
+                     "                            [--uuid <uuid>\n"
 #endif
                      "\n");
 
