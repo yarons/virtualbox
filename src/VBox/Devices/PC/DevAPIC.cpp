6e77bffb6d85cd381188eaae2485b912c63db54a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-11-18 15:24:55 +0000

    DevAPIC: file header.
    svn:sync-xref-src-repo-rev: r74966

--- src/VBox/Devices/PC/DevAPIC.cpp
+++ src/VBox/Devices/PC/DevAPIC.cpp
@@ -1,7 +1,6 @@
-/* $Id: DevAPIC.cpp 39369 2011-11-18 15:23:46Z knut.osmundsen@oracle.com $ */
+/* $Id: DevAPIC.cpp 39370 2011-11-18 15:24:55Z knut.osmundsen@oracle.com $ */
 /** @file
  * Advanced Programmable Interrupt Controller (APIC) Device and
- * I/O Advanced Programmable Interrupt Controller (IO-APIC) Device.
  */
 
 /*
