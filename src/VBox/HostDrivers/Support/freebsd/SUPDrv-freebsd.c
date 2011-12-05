0abd2f61e2c9db8b5d530718b8c3bac5975e1e45
Alexander Eichner <alexander.eichner@oracle.com>
2011-12-05 10:19:31 +0000

    Support/FreeBSD: errno and IPRT error code mixup
    svn:sync-xref-src-repo-rev: r75200

--- src/VBox/HostDrivers/Support/freebsd/SUPDrv-freebsd.c
+++ src/VBox/HostDrivers/Support/freebsd/SUPDrv-freebsd.c
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-freebsd.c 39521 2011-12-03 22:59:46Z alexander.eichner@oracle.com $ */
+/* $Id: SUPDrv-freebsd.c 39522 2011-12-05 10:19:31Z alexander.eichner@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - FreeBSD specifics.
  */
@@ -186,7 +186,7 @@ static int VBoxDrvFreeBSDUnload(void)
     Log(("VBoxDrvFreeBSDUnload:\n"));
 
     if (g_cUsers > 0)
-        return EBUSY;
+        return VERR_RESOURCE_BUSY;
 
     /*
      * Reserve what we did in VBoxDrvFreeBSDInit.
