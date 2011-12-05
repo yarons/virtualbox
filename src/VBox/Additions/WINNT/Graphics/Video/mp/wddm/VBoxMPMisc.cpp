f00da25d714472f8caf7bea5cdc6ed9be7d07f9f
Mikhail Sennikovsky <noreply@oracle.com>
2011-12-05 12:49:46 +0000

    wddm: vboxVideoAMgrCtxAllocSubmit fix
    svn:sync-xref-src-repo-rev: r75208

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPMisc.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPMisc.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMPMisc.cpp 39160 2011-11-01 14:35:23Z noreply@oracle.com $ */
+/* $Id: VBoxMPMisc.cpp 39527 2011-12-05 12:49:46Z noreply@oracle.com $ */
 
 /** @file
  * VBox WDDM Miniport driver
@@ -1267,6 +1267,13 @@ static DECLCALLBACK(VOID) vboxVideoAMgrAllocSubmitCompletion(PVBOXMP_DEVEXT pDev
 /* submits a set of chromium uhgsmi buffers to host for processing */
 NTSTATUS vboxVideoAMgrCtxAllocSubmit(PVBOXMP_DEVEXT pDevExt, PVBOXVIDEOCM_ALLOC_CONTEXT pContext, UINT cBuffers, VBOXWDDM_UHGSMI_BUFFER_UI_INFO_ESCAPE *paBuffers)
 {
+    /* ensure we do not overflow the 32bit buffer size value */
+    if (VBOXWDDM_TRAILARRAY_MAXELEMENTSU32(VBOXVDMACMD_CHROMIUM_CMD, aBuffers) < cBuffers)
+    {
+        WARN(("number of buffers passed too big (%d), max is (%d)", cBuffers, VBOXWDDM_TRAILARRAY_MAXELEMENTSU32(VBOXVDMACMD_CHROMIUM_CMD, aBuffers)));
+        return STATUS_INVALID_PARAMETER;
+    }
+
     NTSTATUS Status = STATUS_SUCCESS;
     UINT cbCmd = VBOXVDMACMD_SIZE_FROMBODYSIZE(RT_OFFSETOF(VBOXVDMACMD_CHROMIUM_CMD, aBuffers[cBuffers]));
 
