6980aa60ec80c302021a319c7598d24c9144bf06
Michal Necasek <michal.necasek@oracle.com>
2011-12-09 14:56:37 +0000

    Fixed DMA workaround for AHCI/ATAPI.
    svn:sync-xref-src-repo-rev: r75328

--- src/VBox/Devices/PC/BIOS-new/ahci.c
+++ src/VBox/Devices/PC/BIOS-new/ahci.c
@@ -1,4 +1,4 @@
-/* $Id: ahci.c 39560 2011-12-08 16:41:37Z michal.necasek@oracle.com $ */
+/* $Id: ahci.c 39573 2011-12-09 14:56:37Z michal.necasek@oracle.com $ */
 /** @file
  * AHCI host adapter driver to boot from SATA disks.
  */
@@ -519,10 +519,10 @@ uint16_t ahci_cmd_packet(uint16_t device_id, uint8_t cmdlen, char __far *cmdbuf,
 
     ahci_cmd_data(bios_dsk, ATA_CMD_PACKET);
     VBOXAHCI_DEBUG("%s: transferred %lu bytes\n", __func__, ahci->aCmdHdr[1]);
-#ifdef DMA_WORKAROUND
-    rep_movsw(bios_dsk->drqp.buffer, bios_dsk->drqp.buffer, bios_dsk->drqp.nsect * 2048 / 2);
-#endif
     bios_dsk->drqp.trsfbytes = ahci->aCmdHdr[1];
+#ifdef DMA_WORKAROUND
+    rep_movsw(bios_dsk->drqp.buffer, bios_dsk->drqp.buffer, bios_dsk->drqp.trsfbytes / 2);
+#endif
     return ahci->aCmdHdr[1] == 0 ? 4 : 0;
 //    return 0;   //@todo!!
 }
