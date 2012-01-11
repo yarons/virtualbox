0cc0fb2fc32b0f448d449b094c753968c904a501
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2012-01-11 16:14:02 +0000

    IPRT: disable critical error message boxes on init. Take three.
    svn:sync-xref-src-repo-rev: r75644

--- src/VBox/Runtime/r3/init.cpp
+++ src/VBox/Runtime/r3/init.cpp
@@ -1,4 +1,4 @@
-/* $Id: init.cpp 39752 2012-01-11 16:12:23Z knut.osmundsen@oracle.com $ */
+/* $Id: init.cpp 39753 2012-01-11 16:14:02Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Init Ring-3.
  */
@@ -330,7 +330,8 @@ static int rtR3InitBody(uint32_t fFlags, int cArgs, char ***papszArgs, const cha
      * Disable error popups.
      */
 #ifdef RT_OS_WINDOWS
-    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOOPENFILEERRORBOX);
+    UINT fOldErrMode = SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOOPENFILEERRORBOX);
+    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOOPENFILEERRORBOX | fOldErrMode);
 #elif defined(RT_OS_OS2)
 # error "FIXME"
 #endif
