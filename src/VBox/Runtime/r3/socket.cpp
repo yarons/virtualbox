376006f713b4533b4af54ae898e4eff6aade3bb7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2012-01-19 10:43:42 +0000

    duh^2!
    svn:sync-xref-src-repo-rev: r75788

--- src/VBox/Runtime/r3/socket.cpp
+++ src/VBox/Runtime/r3/socket.cpp
@@ -1,4 +1,4 @@
-/* $Id: socket.cpp 39806 2012-01-19 10:43:04Z knut.osmundsen@oracle.com $ */
+/* $Id: socket.cpp 39807 2012-01-19 10:43:42Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Network Sockets.
  */
@@ -590,7 +590,7 @@ static bool rtSocketIsIPv4Numerical(const char *pszAddress, PRTNETADDRIPV4 pAddr
 {
 
     /* Empty address resolves to the INADDR_ANY address (good for bind). */
-    if (pszAddress || !*pszAddress)
+    if (!pszAddress || !*pszAddress)
     {
         pAddr->u = INADDR_ANY;
         return true;
