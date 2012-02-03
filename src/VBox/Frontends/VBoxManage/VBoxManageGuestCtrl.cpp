f2a2651b0f10658983fdd51620cb6fa16d80d548
Andreas Loeffler <andreas.loeffler@oracle.com>
2012-02-03 16:45:00 +0000

    GuestCtrl: Fixed unknown PIDs after retrieving output.
    svn:sync-xref-src-repo-rev: r76101

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageGuestCtrl.cpp 39843 2012-01-23 18:38:18Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxManageGuestCtrl.cpp 39991 2012-02-03 16:45:00Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxManage - Implementation of guestcontrol command.
  */
@@ -874,9 +874,17 @@ static int handleCtrlExecProgram(ComPtr<IGuest> pGuest, HandlerArg *pArg)
                 ExecuteProcessStatus_T retStatus;
                 ULONG uRetExitCode, uRetFlags;
                 rc = pGuest->GetProcessStatus(uPID, &uRetExitCode, &uRetFlags, &retStatus);
-                if (SUCCEEDED(rc) && fVerbose)
-                    RTPrintf("Exit code=%u (Status=%u [%s], Flags=%u)\n", uRetExitCode, retStatus, ctrlExecProcessStatusToText(retStatus), uRetFlags);
-                rcProc = ctrlExecProcessStatusToExitCode(retStatus, uRetExitCode);
+                if (SUCCEEDED(rc))
+                {
+                    if (fVerbose)
+                        RTPrintf("Exit code=%u (Status=%u [%s], Flags=%u)\n", uRetExitCode, retStatus, ctrlExecProcessStatusToText(retStatus), uRetFlags);
+                    rcProc = ctrlExecProcessStatusToExitCode(retStatus, uRetExitCode);
+                }
+                else
+                {
+                    ctrlPrintError(pGuest, COM_IIDOF(IGuest));
+                    rcProc = RTEXITCODE_FAILURE;
+                }
             }
         }
         else
