f540851445951064cd0e8fc24c903d08aeff90cd
Andreas Loeffler <andreas.loeffler@oracle.com>
2012-02-21 12:43:55 +0000

    VBoxGuest: Initialize VRDP session handling flag.
    svn:sync-xref-src-repo-rev: r76359

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuest.cpp 39890 2012-01-26 19:42:19Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGuest.cpp 40194 2012-02-21 12:43:55Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxGuest - Guest Additions Driver, Common Code.
  */
@@ -709,6 +709,10 @@ int VBoxGuestInitDevExt(PVBOXGUESTDEVEXT pDevExt, uint16_t IOPortBase,
 #endif
     RTListInit(&pDevExt->WokenUpList);
     RTListInit(&pDevExt->FreeList);
+#ifdef
+#ifdef VBOX_WITH_VRDP_SESSION_HANDLING
+    pDevExt->fVRDPEnabled = FALSE;
+#endif
     pDevExt->f32PendingEvents = 0;
     pDevExt->u32MousePosChangedSeq = 0;
     pDevExt->SessionSpinlock = NIL_RTSPINLOCK;
