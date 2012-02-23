aec0cabec222a022a754ed77babc62234d28e763
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2012-02-23 13:13:32 +0000

    Runtime/r0drv/solaris, HostDrivers/VBoxNetFlt/Solaris: Hold mod_lock across ctf_modopen calls.
    svn:sync-xref-src-repo-rev: r76423

--- src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
+++ src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFlt-solaris.c 39208 2011-11-04 16:27:25Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: VBoxNetFlt-solaris.c 40231 2012-02-23 13:13:32Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Solaris Specific Code.
  */
@@ -444,7 +444,9 @@ vboxNetFltSolarisProbeCtf(void)
     if (pModCtl)
     {
         int err;
+        mutex_enter(&mod_lock);
         ctf_file_t *pCtfFile = ctf_modopen(pModCtl->mod_mp, &err);
+        mutex_exit(&mod_lock);
         if (pCtfFile)
         {
             rc = vboxNetFltSolarisCtfGetMemberOffset(pCtfFile, "file_t", "f_vnode", &s_off_vnode);
