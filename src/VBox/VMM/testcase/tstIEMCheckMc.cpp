645f0c181b7edeba6e1f378cb264070bce95a206
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2012-02-24 00:48:47 +0000

    IEM: Implemented FPU instructions 0xd9 0xf3 thru 0xd9 0xff, completing the 0xd9 prefixed instructions.
    svn:sync-xref-src-repo-rev: r76454

--- src/VBox/VMM/testcase/tstIEMCheckMc.cpp
+++ src/VBox/VMM/testcase/tstIEMCheckMc.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstIEMCheckMc.cpp 40243 2012-02-23 22:05:00Z knut.osmundsen@oracle.com $ */
+/* $Id: tstIEMCheckMc.cpp 40244 2012-02-24 00:48:47Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM Testcase - Check the "Microcode".
  */
@@ -184,6 +184,13 @@ IEMOPSHIFTDBLSIZES g_iemAImpl_shrd;
 #define iemAImpl_fsubr_r80_by_r80       NULL
 #define iemAImpl_fdiv_r80_by_r80        NULL
 #define iemAImpl_fdivr_r80_by_r80       NULL
+#define iemAImpl_fprem_r80_by_r80       NULL
+#define iemAImpl_fprem1_r80_by_r80      NULL
+#define iemAImpl_fscale_r80_by_r80      NULL
+
+#define iemAImpl_fpatan_r80_by_r80      NULL
+#define iemAImpl_fyl2xp1_r80_by_r80     NULL
+
 #define iemAImpl_fcom_r80_by_r80        NULL
 #define iemAImpl_fabs_r80               NULL
 #define iemAImpl_fchs_r80               NULL
@@ -191,6 +198,11 @@ IEMOPSHIFTDBLSIZES g_iemAImpl_shrd;
 #define iemAImpl_fxam_r80               NULL
 #define iemAImpl_f2xm1_r80              NULL
 #define iemAImpl_fyl2x_r80              NULL
+#define iemAImpl_fsqrt_r80              NULL
+#define iemAImpl_frndint_r80            NULL
+#define iemAImpl_fsin_r80               NULL
+#define iemAImpl_fcos_r80               NULL
+
 #define iemAImpl_fld1                   NULL
 #define iemAImpl_fldl2t                 NULL
 #define iemAImpl_fldl2e                 NULL
@@ -198,6 +210,10 @@ IEMOPSHIFTDBLSIZES g_iemAImpl_shrd;
 #define iemAImpl_fldlg2                 NULL
 #define iemAImpl_fldln2                 NULL
 #define iemAImpl_fldz                   NULL
+
+#define iemAImpl_fptan_r80_r80          NULL
+#define iemAImpl_fxtract_r80_r80        NULL
+#define iemAImpl_fsincos_r80_r80        NULL
 
 #define iemCImpl_callf                  NULL
 #define iemCImpl_FarJmp                 NULL
