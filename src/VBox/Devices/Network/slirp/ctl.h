a13b260b5f16058c4a60e671e3de1fcffe14e9f2
Vasily Levchenko <noreply@oracle.com>
2012-02-28 19:07:42 +0000

    NAT: CTL_CHECK(x) verify whether address belong to NAT network either.
    svn:sync-xref-src-repo-rev: r76508

--- src/VBox/Devices/Network/slirp/ctl.h
+++ src/VBox/Devices/Network/slirp/ctl.h
@@ -1,4 +1,4 @@
-/* $Id: ctl.h 28800 2010-04-27 08:22:32Z noreply@oracle.com $ */
+/* $Id: ctl.h 40279 2012-02-28 19:07:42Z noreply@oracle.com $ */
 /** @file
  * NAT - IP subnet constants.
  */
@@ -24,4 +24,5 @@
 #define CTL_GUEST       15
 #define CTL_BROADCAST   255
 
-#define CTL_CHECK(x, ctl) (((x) & ~pData->netmask) == (ctl))
+#define CTL_CHECK(x, ctl) (((x) & ~pData->netmask) == (ctl) \
+                           && (((x) & ~pData->netmask) == (pData->special_addr.s_addr & ~pData->netmask)))
