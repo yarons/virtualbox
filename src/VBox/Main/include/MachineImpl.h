bb51e0eb791b45f58c73484aceeb8f3db3811206
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2012-03-12 16:04:28 +0000

    Main/VirtualBox+Machine+Medium: redesign the medium registry save logic, as the local lists in the threads caused inconsistencies with concurrent operations and the return of spurious errors
    svn:sync-xref-src-repo-rev: r76772

--- src/VBox/Main/include/MachineImpl.h
+++ src/VBox/Main/include/MachineImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.h 40418 2012-03-09 22:00:56Z knut.osmundsen@oracle.com $ */
+/* $Id: MachineImpl.h 40432 2012-03-12 16:04:28Z klaus.espenlaub@oracle.com $ */
 /** @file
  * Implementation of IMachine in VBoxSVC - Header.
  */
@@ -840,15 +840,12 @@ protected:
                                settings::StorageController &data);
     HRESULT saveStateSettings(int aFlags);
 
-    void addMediumToRegistry(ComObjPtr<Medium> &pMedium,
-                             GuidList &llRegistriesThatNeedSaving,
-                             Guid *puuid);
+    void addMediumToRegistry(ComObjPtr<Medium> &pMedium);
 
     HRESULT createImplicitDiffs(IProgress *aProgress,
                                 ULONG aWeight,
-                                bool aOnline,
-                                GuidList *pllRegistriesThatNeedSaving);
-    HRESULT deleteImplicitDiffs(GuidList *pllRegistriesThatNeedSaving);
+                                bool aOnline);
+    HRESULT deleteImplicitDiffs();
 
     MediumAttachment* findAttachment(const MediaData::AttachmentList &ll,
                                      IN_BSTR aControllerName,
@@ -861,8 +858,7 @@ protected:
 
     HRESULT detachDevice(MediumAttachment *pAttach,
                          AutoWriteLock &writeLock,
-                         Snapshot *pSnapshot,
-                         GuidList *pllRegistriesThatNeedSaving);
+                         Snapshot *pSnapshot);
 
     HRESULT detachAllMedia(AutoWriteLock &writeLock,
                            Snapshot *pSnapshot,
@@ -936,6 +932,8 @@ protected:
 
     typedef std::list<ComObjPtr<StorageController> > StorageControllerList;
     Backupable<StorageControllerList> mStorageControllers;
+
+    uint64_t                        uRegistryNeedsSaving;
 
     friend class SessionMachine;
     friend class SnapshotMachine;
