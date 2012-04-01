05b2dde0d21045256328b10029cb88e21bbeecd6
Vasily Levchenko <noreply@oracle.com>
2012-04-01 07:56:35 +0000

    VBoxNetFlt: 3.4-rc1 support (VLAN_ETH_ALEN -> ETH_ALEN)
    svn:sync-xref-src-repo-rev: r77207

--- src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
+++ src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFlt-linux.c 39479 2011-11-30 17:07:23Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxNetFlt-linux.c 40733 2012-04-01 07:56:35Z noreply@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Linux Specific Code.
  */
@@ -820,7 +820,11 @@ static int vboxNetFltLinuxPacketHandler(struct sk_buff *pBuf,
         {
             uint8_t *pMac = (uint8_t*)skb_mac_header(pBuf);
             struct vlan_ethhdr *pVHdr = (struct vlan_ethhdr *)(pMac - VLAN_HLEN);
+#  if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 4, 0)
+            memmove(pVHdr, pMac, ETH_ALEN * 2);
+#  else
             memmove(pVHdr, pMac, VLAN_ETH_ALEN * 2);
+#  endif
             pVHdr->h_vlan_proto = RT_H2N_U16(ETH_P_8021Q);
             pVHdr->h_vlan_TCI   = RT_H2N_U16(vlan_tx_tag_get(pBuf));
             pBuf->mac_header   -= VLAN_HLEN;
