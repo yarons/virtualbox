8505101d490aa8a9038b9b422a1967e8251c58f6
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2012-04-08 18:41:46 +0000

    VBoxTpG.cpp: Microsoft Visual C++ support.
    svn:sync-xref-src-repo-rev: r77341

--- include/VBox/VBoxTpG.h
+++ include/VBox/VBoxTpG.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxTpG.h 40603 2012-03-24 13:39:14Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxTpG.h 40826 2012-04-08 18:41:46Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Tracepoint Generator Structures.
  */
@@ -36,6 +36,9 @@ typedef struct VTGPROBELOC
     const char *pszFunction;
     const char *pszFile;
     uint8_t    *pbProbe;
+#if ARCH_BITS == 32 /* Make the structure a multiple of 8. */
+    uint32_t    u32Padding;
+#endif
 } VTGPROBELOC;
 /** Pointer to a probe location. */
 typedef VTGPROBELOC *PVTGPROBELOC;
@@ -50,8 +53,8 @@ typedef VTGPROBELOC *PVTGPROBELOC;
  * Declares a static variable, @a a_VarName, of type VTGPROBELOC in the section
  * indicated by VTG_LOC_SECT.  */
 #if defined(RT_OS_WINDOWS)
-# define VTG_OBJ_SECT       ".VTGObj"
-# define VTG_LOC_SECT       ".VTGPrLc"
+# define VTG_OBJ_SECT       "VTGObj"
+# define VTG_LOC_SECT       "VTGPrLc.Data"
 # ifdef _MSC_VER
 #  define VTG_DECL_VTGPROBELOC(a_VarName) \
     __declspec(allocate(VTG_LOC_SECT)) static VTGPROBELOC a_VarName
