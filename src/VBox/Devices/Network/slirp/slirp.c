1c8c94e3ab7ee8120497b1f0dd22a38fd3d58473
Vasily Levchenko <noreply@oracle.com>
2012-04-09 09:10:45 +0000

    NAT: don't compile ''else'' branch if logging is disabled.
    svn:sync-xref-src-repo-rev: r77351

--- src/VBox/Devices/Network/slirp/slirp.c
+++ src/VBox/Devices/Network/slirp/slirp.c
@@ -1,4 +1,4 @@
-/* $Id: slirp.c 40835 2012-04-09 08:13:57Z noreply@oracle.com $ */
+/* $Id: slirp.c 40836 2012-04-09 09:10:45Z noreply@oracle.com $ */
 /** @file
  * NAT - slirp glue.
  */
@@ -1321,11 +1321,13 @@ void slirp_select_poll(PNATState pData, struct pollfd *polls, int ndfs)
                 ret = soread(pData, so);
                 if (ret > 0)
                     TCP_OUTPUT(pData, sototcpcb(so));
+#ifdef LOG_ENABLED
                 else if (pPrevSo->so_next == so)
                 {
                     Log2(("%R[natsock] errno %d (%s)\n", so, errno, strerror(errno)));
                     break;
                 }
+#endif
             }
             if (pPrevSo->so_next == so)
             {
