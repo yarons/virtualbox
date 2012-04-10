e892cd2d2707112f1155d4a8929ad1ae78ff3cd1
Alexander Eichner <alexander.eichner@oracle.com>
2012-04-10 08:22:56 +0000

    Storage/ATA: Fix CD/DVD passthrough and Session At Once (SAO) recording by saving the CUE sheet and determining the correct sector sizes during recording
    svn:sync-xref-src-repo-rev: r77360

--- src/VBox/Devices/testcase/tstDeviceStructSizeRC.cpp
+++ src/VBox/Devices/testcase/tstDeviceStructSizeRC.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstDeviceStructSizeRC.cpp 40040 2012-02-08 16:49:31Z michal.necasek@oracle.com $ */
+/* $Id: tstDeviceStructSizeRC.cpp 40841 2012-04-10 08:22:56Z alexander.eichner@oracle.com $ */
 /** @file
  * tstDeviceStructSizeGC - Generate structure member and size checks from the RC perspective.
  *
@@ -810,6 +810,8 @@ int main()
     GEN_CHECK_OFF(ATADevState, szInquiryProductId[ATAPI_INQUIRY_PRODUCT_ID_LENGTH]);
     GEN_CHECK_OFF(ATADevState, szInquiryRevision);
     GEN_CHECK_OFF(ATADevState, szInquiryRevision[ATAPI_INQUIRY_REVISION_LENGTH]);
+    GEN_CHECK_OFF(ATADevState, cbCueSheet);
+    GEN_CHECK_OFF(ATADevState, pbCueSheet);
     GEN_CHECK_SIZE(ATATransferRequest);
     GEN_CHECK_OFF(ATATransferRequest, iIf);
     GEN_CHECK_OFF(ATATransferRequest, iBeginTransfer);
