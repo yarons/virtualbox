cbf3d78e74a9793ab15574d1dfcfb9333c46c7b1
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2012-04-10 14:58:32 +0000

    VBoxDispXPDM: do not process unsupported text orders.
    svn:sync-xref-src-repo-rev: r77379

--- src/VBox/Additions/WINNT/Graphics/Video/disp/xpdm/VBoxDispVrdpTxt.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/disp/xpdm/VBoxDispVrdpTxt.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxDispVrdpTxt.cpp 36867 2011-04-28 07:27:03Z noreply@oracle.com $ */
+/* $Id: VBoxDispVrdpTxt.cpp 40853 2012-04-10 14:58:32Z vitali.pelenjow@oracle.com $ */
 
 /** @file
  * VBox XPDM Display driver
@@ -147,7 +147,8 @@ BOOL vrdpReportText(PVBOXDISPDEV pDev, VRDPCLIPRECTS *pClipRects, STROBJ *pstro,
         return FALSE;
     }
 
-    if (   (pstro->flAccel & SO_VERTICAL) != 0
+    /* The driver can get vertical strings with both SO_HORIZONTAL and SO_VERTICAL bits equal to zero. */
+    if (   (pstro->flAccel & SO_HORIZONTAL) == 0
         || (pstro->flAccel & SO_REVERSED) != 0)
     {
         /* Do not support (yet) the vertical and right to left strings.
