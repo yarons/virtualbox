ea59dc4a7a34faa1d286849832f7a0351ae34a8a
Alexander Eichner <alexander.eichner@oracle.com>
2012-04-13 19:38:21 +0000

    Storage/VDI: Make the backend work on big endian architectures
    svn:sync-xref-src-repo-rev: r77457

--- src/VBox/Storage/VDICore.h
+++ src/VBox/Storage/VDICore.h
@@ -1,4 +1,4 @@
-/* $Id: VDICore.h 40713 2012-03-29 16:06:14Z alexander.eichner@oracle.com $ */
+/* $Id: VDICore.h 40906 2012-04-13 19:38:21Z alexander.eichner@oracle.com $ */
 /** @file
  * Virtual Disk Image (VDI), Core Code Header (internal).
  */
@@ -603,6 +603,17 @@ typedef struct VDIASYNCBLOCKALLOC
     /** Block index to allocate. */
     unsigned                uBlock;
 } VDIASYNCBLOCKALLOC, *PVDIASYNCBLOCKALLOC;
+
+/**
+ * Endianess conversion direction.
+ */
+typedef enum VDIECONV
+{
+    /** Host to file endianess. */
+    VDIECONV_H2F = 0,
+    /** File to host endianess. */
+    VDIECONV_F2H
+} VDIECONV;
 
 #endif
 
