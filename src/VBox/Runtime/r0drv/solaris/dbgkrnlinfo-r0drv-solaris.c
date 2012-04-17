fad177ae99fd4d9d6f991e05972cadb8b6276f2e
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2012-04-17 17:38:28 +0000

    Runtime/r0drv/solaris: dbg -> dbgkrnlinfo.
    svn:sync-xref-src-repo-rev: r77524

--- src/VBox/Runtime/r0drv/solaris/dbgkrnlinfo-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/dbgkrnlinfo-r0drv-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: dbg-r0drv-solaris.c 40966 2012-04-17 16:43:28Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: dbgkrnlinfo-r0drv-solaris.c 40969 2012-04-17 17:38:28Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IPRT - Kernel debug information, Ring-0 Driver, Solaris Code.
  */
