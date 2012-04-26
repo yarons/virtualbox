8e143ca9d861f128f0d6de28fbf1d579e5d52b12
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2012-04-26 10:00:30 +0000

    Main: workaround for GUI.
    svn:sync-xref-src-repo-rev: r77634

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 41049 2012-04-25 07:19:53Z vitali.pelenjow@oracle.com $ */
+/* $Id: MachineImpl.cpp 41066 2012-04-26 10:00:30Z vitali.pelenjow@oracle.com $ */
 /** @file
  * Implementation of IMachine in VBoxSVC.
  */
@@ -5839,9 +5839,20 @@ STDMETHODIMP Machine::QuerySavedGuestScreenInfo(ULONG uScreenId,
     int vrc = readSavedGuestScreenInfo(mSSData->strStateFilePath, uScreenId,
                                        &u32OriginX, &u32OriginY, &u32Width, &u32Height, &u16Flags);
     if (RT_FAILURE(vrc))
+    {
+#ifdef RT_OS_WINDOWS
+        /* HACK: GUI sets *pfEnabled to 'true' and expects it to stay so if the API fails.
+         * This works with XPCOM. But Windows COM sets all output parameters to zero.
+         * So just assign fEnable to TRUE again.
+         * The right fix would be to change GUI API wrappers to make sure that parameters
+         * are changed only if API succeeds.
+         */
+        *pfEnabled = TRUE;
+#endif
         return setError(VBOX_E_IPRT_ERROR,
                         tr("Saved guest size is not available (%Rrc)"),
                         vrc);
+    }
 
     *puOriginX = u32OriginX;
     *puOriginY = u32OriginY;
