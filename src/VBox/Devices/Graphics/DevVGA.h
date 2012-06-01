0c10365eafd8cc91b85d8b8336b7388b2691f611
Frank Mehnert <noreply@oracle.com>
2012-06-01 10:37:47 +0000

    build fix
    svn:sync-xref-src-repo-rev: r78299

--- src/VBox/Devices/Graphics/DevVGA.h
+++ src/VBox/Devices/Graphics/DevVGA.h
@@ -1,4 +1,4 @@
-/* $Id: DevVGA.h 41535 2012-06-01 10:17:57Z michal.necasek@oracle.com $ */
+/* $Id: DevVGA.h 41536 2012-06-01 10:37:47Z noreply@oracle.com $ */
 /** @file
  * DevVGA - VBox VGA/VESA device, internal header.
  */
@@ -276,7 +276,7 @@ typedef struct VGAState {
     bool                        fRenderVRAM;
     bool                        Padding1[2];
 
-#if HC_ARCH_BITS == 32
+#if HC_ARCH_BITS == 64
     uint32_t                    Padding2;
 #endif
 
