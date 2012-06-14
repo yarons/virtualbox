04ea9f2770d4029e8d500f1ba569c374c7e3ccf1
Michael Thayer <noreply@oracle.com>
2012-06-14 19:49:31 +0000

    Additions/common/VBoxGuest: fixed copyright dates and licence headers and removed a copyright text that is not relevant to this code.
    svn:sync-xref-src-repo-rev: r78530

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest2.h
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest2.h
@@ -1,10 +1,10 @@
-/* $Id: VBoxGuest2.h 32434 2010-09-12 23:11:51Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGuest2.h 41722 2012-06-14 19:49:31Z noreply@oracle.com $ */
 /** @file
  * VBoxGuest - Guest Additions Driver, bits shared with the windows code.
  */
 
 /*
- * Copyright (C) 2010 Oracle Corporation
+ * Copyright (C) 2010-2012 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -13,6 +13,15 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
  */
 
 #ifndef ___VBoxGuest2_h
