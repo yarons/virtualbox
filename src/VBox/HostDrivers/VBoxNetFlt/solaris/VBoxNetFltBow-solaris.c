37085b80bba1dac7b8e2ec8481b6abb6d26f61bc
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2012-06-14 10:12:57 +0000

    Solaris: license header update for kernel bits.
    svn:sync-xref-src-repo-rev: r78508

--- src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFltBow-solaris.c
+++ src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFltBow-solaris.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFltBow-solaris.c 40444 2012-03-13 13:30:40Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: VBoxNetFltBow-solaris.c 41700 2012-06-14 10:12:57Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Solaris Specific Code.
  */
@@ -13,6 +13,15 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
  */
 
 /*******************************************************************************
