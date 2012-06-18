e6f65edaa2061deaf7f1387ec3860f8b96d162d0
Vasily Levchenko <noreply@oracle.com>
2012-06-18 16:34:50 +0000

    NAT: fix vbox/7459: drop packet if we can't send it because of EAGAIN and Co.
    svn:sync-xref-src-repo-rev: r78638

--- src/VBox/Devices/Network/slirp/udp.c
+++ src/VBox/Devices/Network/slirp/udp.c
@@ -1,4 +1,4 @@
-/* $Id: udp.c 40583 2012-03-23 04:20:59Z noreply@oracle.com $ */
+/* $Id: udp.c 41817 2012-06-18 16:34:50Z noreply@oracle.com $ */
 /** @file
  * NAT - UDP protocol.
  */
@@ -293,14 +293,30 @@ udp_input(PNATState pData, register struct mbuf *m, int iphlen)
         LogRel(("NAT: Error (%s) occurred while setting TTL(%d) attribute "
                 "of IP packet to socket %R[natsock]\n", strerror(errno), ip->ip_ttl, so));
 
-    if (sosendto(pData, so, m) == -1)
+    if (   sosendto(pData, so, m) == -1
+        && (   errno != EAGAIN
+            && errno != EWOULDBLOCK
+            && errno != EINPROGRESS
+            && errno != ENOTCONN))
     {
         m->m_len += iphlen;
         m->m_data -= iphlen;
         *ip = save_ip;
         Log2(("NAT: UDP tx errno = %d (%s) on sent to %RTnaipv4\n",
               errno, strerror(errno), ip->ip_dst));
-        icmp_error(pData, m, ICMP_UNREACH, ICMP_UNREACH_NET, 0, strerror(errno));
+#if 0
+        /* ICMP_SOURCEQUENCH haven't got any effect, the idea here 
+         * inform guest about the exosting NAT resources with assumption that 
+         * that guest reduce traffic. But it doesn't work 
+         */
+        if(    errno == EAGAIN
+            || errno == EWOULDBLOCK
+            || errno == EINPROGRESS
+            || errno == ENOTCONN)
+            icmp_error(pData, m, ICMP_SOURCEQUENCH, 0, 1, strerror(errno));
+        else
+#endif
+            icmp_error(pData, m, ICMP_UNREACH, ICMP_UNREACH_NET, 0, strerror(errno));
         so->so_m = NULL;
         LogFlowFuncLeave();
         return;
