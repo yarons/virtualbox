d665c439aa2c712803af1bd70bfaadaf60834010
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2012-06-22 10:58:47 +0000

    NetShaper: Runtime shaper disabling support (#5582)
    svn:sync-xref-src-repo-rev: r78735

--- src/VBox/Frontends/VBoxManage/VBoxManageBandwidthControl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageBandwidthControl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageBandwidthControl.cpp 41842 2012-06-20 11:46:31Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxManageBandwidthControl.cpp 41882 2012-06-22 10:58:47Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * VBoxManage - The bandwidth control related commands.
  */
@@ -81,8 +81,8 @@ static const char *parseLimit(const char *pcszLimit, LONG64 *pLimit)
         default:
             return "Invalid limit specifier\n";
     }
-    if (*pLimit <= 0)
-        return "Limit must be positive\n";
+    if (*pLimit < 0)
+        return "Limit cannot be negative\n";
     if (*pLimit > INT64_MAX / iMultiplier)
         return "Limit is too big\n";
     *pLimit *= iMultiplier;
