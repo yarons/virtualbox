b7d242fa94f6a676000243bc03c3e3647bfbcf56
Alexander Eichner <alexander.eichner@oracle.com>
2012-07-03 12:59:30 +0000

    Another one
    svn:sync-xref-src-repo-rev: r78881

--- src/VBox/Frontends/VBoxAutostart/VBoxAutostart.cpp
+++ src/VBox/Frontends/VBoxAutostart/VBoxAutostart.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxAutostart.cpp 41999 2012-07-03 12:50:18Z alexander.eichner@oracle.com $ */
+/* $Id: VBoxAutostart.cpp 42001 2012-07-03 12:59:30Z alexander.eichner@oracle.com $ */
 /** @file
  * VBoxAutostart - VirtualBox Autostart service.
  */
@@ -383,10 +383,6 @@ int main(int argc, char *argv[])
                 RTPrintf("%sr%s\n", RTBldCfgVersion(), RTBldCfgRevisionStr());
                 return 0;
 
-            case 'P':
-                pszPidFile = ValueUnion.psz;
-                break;
-
             case 'F':
                 pszLogFile = ValueUnion.psz;
                 break;
@@ -461,7 +457,7 @@ int main(int argc, char *argv[])
             pszLogFile = szLogFile;
         }
 
-        rc = RTProcDaemonizeUsingFork(false /* fNoChDir */, false /* fNoClose */, pszPidFile);
+        rc = RTProcDaemonizeUsingFork(false /* fNoChDir */, false /* fNoClose */, NULL);
         if (RT_FAILURE(rc))
             return RTMsgErrorExit(RTEXITCODE_FAILURE, "failed to daemonize, rc=%Rrc. exiting.", rc);
         /* create release logger, to file */
