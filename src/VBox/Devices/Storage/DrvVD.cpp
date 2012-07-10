43cf6f388883e0bf0428628f51bc12c9b4957e24
Alexander Eichner <alexander.eichner@oracle.com>
2012-07-10 07:41:06 +0000

    Fix error message
    svn:sync-xref-src-repo-rev: r79010

--- src/VBox/Devices/Storage/DrvVD.cpp
+++ src/VBox/Devices/Storage/DrvVD.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvVD.cpp 42071 2012-07-10 07:21:50Z alexander.eichner@oracle.com $ */
+/* $Id: DrvVD.cpp 42073 2012-07-10 07:41:06Z alexander.eichner@oracle.com $ */
 /** @file
  * DrvVD - Generic VBox disk media driver.
  */
@@ -2297,7 +2297,7 @@ static DECLCALLBACK(int) drvvdConstruct(PPDMDRVINS pDrvIns,
             if (RT_FAILURE(rc))
             {
                 rc = PDMDRV_SET_ERROR(pDrvIns, rc,
-                                      N_("DrvVD: Configuration error: Querying \"Discard\" as boolean failed"));
+                                      N_("DrvVD: Configuration error: Querying \"InformAboutZeroBlocks\" as boolean failed"));
                 break;
             }
 
