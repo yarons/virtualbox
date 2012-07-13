9c052f7929ee2d95431d5b350c251bc410a02bf7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2012-07-13 13:59:20 +0000

    Share kBuild template for the VGA and System BIOSes as well as the alternative source generator code.
    svn:sync-xref-src-repo-rev: r79118

--- src/VBox/Devices/BiosCommonCode/support.asm
+++ src/VBox/Devices/BiosCommonCode/support.asm
@@ -1,4 +1,4 @@
-; $Id: support.asm 42059 2012-07-09 13:33:44Z knut.osmundsen@oracle.com $
+; $Id: support.asm 42147 2012-07-13 13:59:20Z knut.osmundsen@oracle.com $
 ;; @file
 ; Compiler support routines.
 ;
