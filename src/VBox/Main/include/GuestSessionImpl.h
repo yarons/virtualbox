bb9cd3192d9e776308e08371085d67ad47c1f2fe
Andreas Loeffler <andreas.loeffler@oracle.com>
2012-08-10 10:19:57 +0000

    Guest Control 2.0: Update.
    svn:sync-xref-src-repo-rev: r79965

--- src/VBox/Main/include/GuestSessionImpl.h
+++ src/VBox/Main/include/GuestSessionImpl.h
@@ -1,5 +1,5 @@
 
-/* $Id: GuestSessionImpl.h 42702 2012-08-09 06:11:32Z noreply@oracle.com $ */
+/* $Id: GuestSessionImpl.h 42749 2012-08-10 10:19:57Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox Main - XXX.
  */
@@ -26,6 +26,8 @@
 #include "GuestDirectoryImpl.h"
 #include "GuestFileImpl.h"
 #include "GuestFsObjInfoImpl.h"
+
+#include <iprt/isofs.h> /* For UpdateAdditions. */
 
 class Guest;
 
@@ -135,6 +137,11 @@ public:
     static int taskThread(RTTHREAD Thread, void *pvUser);
 
 protected:
+
+    int copyFileToGuest(GuestSession *pSession, PRTISOFSFILE pISO,
+                        Utf8Str const &strFileSource, const Utf8Str &strFileDest,
+                        bool fOptional, uint32_t *pcbSize);
+    int runFile(GuestSession *pSession, GuestProcessStartupInfo &procInfo);
 
     /** The (optionally) specified Guest Additions .ISO on the host
      *  which will be used for the updating process. */
