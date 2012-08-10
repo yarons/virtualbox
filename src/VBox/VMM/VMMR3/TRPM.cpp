f9bdaf0bcf1e6a84dcc3c9e8fe16b64f2c8b8065
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2012-08-10 18:26:31 +0000

    TRPM.cpp: Made some unused code build again.
    svn:sync-xref-src-repo-rev: r79981

--- src/VBox/VMM/VMMR3/TRPM.cpp
+++ src/VBox/VMM/VMMR3/TRPM.cpp
@@ -1,4 +1,4 @@
-/* $Id: TRPM.cpp 41965 2012-06-29 02:52:49Z knut.osmundsen@oracle.com $ */
+/* $Id: TRPM.cpp 42762 2012-08-10 18:26:31Z knut.osmundsen@oracle.com $ */
 /** @file
  * TRPM - The Trap Monitor.
  */
@@ -1566,14 +1566,14 @@ VMMR3DECL(int) TRPMR3InjectEvent(PVM pVM, PVMCPU pVCpu, TRPMEVENT enmEvent)
             return HWACCMR3IsActive(pVCpu) ? VINF_EM_RESCHEDULE_HWACC : VINF_EM_RESCHEDULE_REM; /* (Heed the halted state if this is changed!) */
         }
 #else
-        if (HWACCMR3IsActive(pVM))
+        if (HWACCMR3IsActive(pVCpu))
         {
             uint8_t u8Interrupt;
             rc = PDMGetInterrupt(pVCpu, &u8Interrupt);
             Log(("TRPMR3InjectEvent: u8Interrupt=%d (%#x) rc=%Rrc\n", u8Interrupt, u8Interrupt, rc));
             if (RT_SUCCESS(rc))
             {
-                rc = TRPMAssertTrap(pVM, u8Interrupt, TRPM_HARDWARE_INT);
+                rc = TRPMAssertTrap(pVCpu, u8Interrupt, TRPM_HARDWARE_INT);
                 AssertRC(rc);
                 STAM_COUNTER_INC(&pVM->trpm.s.paStatForwardedIRQR3[u8Interrupt]);
                 return VINF_EM_RESCHEDULE_HWACC;
