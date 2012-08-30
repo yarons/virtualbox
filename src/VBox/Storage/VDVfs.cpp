5ef89505c20c910b4f46c0eb4a4d10b23d98c3a4
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2012-08-30 18:36:48 +0000

    CDDL licensing cleanup
    svn:sync-xref-src-repo-rev: r80487

--- src/VBox/Storage/VDVfs.cpp
+++ src/VBox/Storage/VDVfs.cpp
@@ -1,4 +1,4 @@
-/* $Id: VDVfs.cpp 41576 2012-06-05 12:08:04Z alexander.eichner@oracle.com $ */
+/* $Id: VDVfs.cpp 43121 2012-08-30 18:36:48Z klaus.espenlaub@oracle.com $ */
 /** @file
  * Virtual Disk Container implementation. - VFS glue.
  */
@@ -13,15 +13,6 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
- *
- * The contents of this file may alternatively be used under the terms
- * of the Common Development and Distribution License Version 1.0
- * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
- * VirtualBox OSE distribution, in which case the provisions of the
- * CDDL are applicable instead of those of the GPL.
- *
- * You may elect to license modified versions of this file under the
- * terms and conditions of either the GPL or the CDDL or both.
  */
 
 
