891b331e1293fe447f16c939f8adde28d86f9046
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2012-09-06 10:16:14 +0000

    common/string/RTStrIPv6.cpp -> common/net/netaddrstr.cpp.
    svn:sync-xref-src-repo-rev: r80611

--- src/VBox/Runtime/common/net/netaddrstr.cpp
+++ src/VBox/Runtime/common/net/netaddrstr.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTStrIPv6.cpp 43220 2012-09-06 10:13:14Z knut.osmundsen@oracle.com $ */
+/* $Id: netaddrstr.cpp 43221 2012-09-06 10:16:14Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Network Address String Handling.
  */
