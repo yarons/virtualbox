7006aedeeec18636776879a02b6886aae1374dc4
Anshul Makkar <noreply@oracle.com>
2012-09-21 08:12:41 +0000

    Devices/Parallel: Added more debug statements for enhanced logging.. Need to give debug build to user for debugging. Will revert the changes soon.
    svn:sync-xref-src-repo-rev: r80854

--- src/VBox/Devices/Parallel/DevParallel.cpp
+++ src/VBox/Devices/Parallel/DevParallel.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevParallel.cpp 40280 2012-02-28 19:47:00Z knut.osmundsen@oracle.com $ */
+/* $Id: DevParallel.cpp 43383 2012-09-21 08:12:41Z noreply@oracle.com $ */
 /** @file
  * DevParallel - Parallel (Port) Device Emulation.
  *
@@ -474,12 +474,16 @@ PDMBOTHCBDECL(int) parallelIOPortRead(PPDMDEVINS pDevIns, void *pvUser,
 #ifndef IN_RING3
                     rc = VINF_IOM_R3_IOPORT_READ;
 #else
+                    LogFlow(("Read Status \n"));
                     if (RT_LIKELY(pThis->pDrvHostParallelConnector))
                     {
+                        LogFlow(("Call to Read Status \n"));
                         rc = pThis->pDrvHostParallelConnector->pfnReadStatus(pThis->pDrvHostParallelConnector, &pThis->regStatus);
                         AssertRC(rc);
                     }
                     *pu32 = pThis->regStatus;
+
+                    LogFlow(("Call to parallel IRQ . Status=%d\n", *pu32));
                     parallelIrqClear(pThis);
 #endif
                     break;
