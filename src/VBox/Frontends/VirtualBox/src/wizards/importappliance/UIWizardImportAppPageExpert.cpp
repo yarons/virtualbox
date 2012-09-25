e89230ca42c85811736322dfe053d92414be4f45
Sergey Dubov <sergey.dubov@oracle.com>
2012-09-25 10:05:52 +0000

    FE/Qt: 6123: Expert-mode of import-appliance wizard should fetch passed file-path.
    svn:sync-xref-src-repo-rev: r80925

--- src/VBox/Frontends/VirtualBox/src/wizards/importappliance/UIWizardImportAppPageExpert.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/importappliance/UIWizardImportAppPageExpert.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIWizardImportAppPageExpert.cpp 41689 2012-06-13 17:13:36Z sergey.dubov@oracle.com $ */
+/* $Id: UIWizardImportAppPageExpert.cpp 43426 2012-09-25 10:05:52Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -50,6 +50,7 @@ UIWizardImportAppPageExpert::UIWizardImportAppPageExpert(const QString &strFileN
         pMainLayout->addWidget(m_pVMApplianceLabel);
         pMainLayout->addWidget(m_pFileSelector);
         pMainLayout->addWidget(m_pApplianceWidget);
+        m_pFileSelector->setPath(strFileName);
     }
 
     /* Setup connections: */
