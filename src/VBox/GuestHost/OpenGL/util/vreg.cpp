6cc75c818d4bdd2ee72a14e144ea4438cf793c04
Mikhail Sennikovsky <noreply@oracle.com>
2012-11-15 21:31:24 +0000

    build fix
    svn:sync-xref-src-repo-rev: r82106

--- src/VBox/GuestHost/OpenGL/util/vreg.cpp
+++ src/VBox/GuestHost/OpenGL/util/vreg.cpp
@@ -1,4 +1,4 @@
-/* $Id: vreg.cpp 43888 2012-11-15 21:23:50Z noreply@oracle.com $ */
+/* $Id: vreg.cpp 43889 2012-11-15 21:31:24Z noreply@oracle.com $ */
 
 /** @file
  * Visible Regions processing API implementation
@@ -803,12 +803,12 @@ VBOXVREGDECL(void) VBoxVrCompositorTerm(PVBOXVR_COMPOSITOR pCompositor)
     }
 }
 
-static DECLINLINE(void) vboxVrCompositorEntryAdd(PVBOXVR_COMPOSITOR pCompositor, PVBOXVR_COMPOSITOR_ENTRY pEntry)
+DECLINLINE(void) vboxVrCompositorEntryAdd(PVBOXVR_COMPOSITOR pCompositor, PVBOXVR_COMPOSITOR_ENTRY pEntry)
 {
     RTListPrepend(&pCompositor->List, &pEntry->Node);
 }
 
-static DECLINLINE(void) vboxVrCompositorEntryRemove(PVBOXVR_COMPOSITOR pCompositor, PVBOXVR_COMPOSITOR_ENTRY pEntry, PVBOXVR_COMPOSITOR_ENTRY pReplacingEntry)
+DECLINLINE(void) vboxVrCompositorEntryRemove(PVBOXVR_COMPOSITOR pCompositor, PVBOXVR_COMPOSITOR_ENTRY pEntry, PVBOXVR_COMPOSITOR_ENTRY pReplacingEntry)
 {
     RTListNodeRemove(&pEntry->Node);
     if (pCompositor->pfnEntryRemoved)
