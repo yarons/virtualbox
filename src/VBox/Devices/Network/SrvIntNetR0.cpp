129de46d13d166320583363363171a786b971c7a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2012-11-27 15:06:51 +0000

    SrvIntNetR0.cpp: Try shut up warning.
    svn:sync-xref-src-repo-rev: r82333

--- src/VBox/Devices/Network/SrvIntNetR0.cpp
+++ src/VBox/Devices/Network/SrvIntNetR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: SrvIntNetR0.cpp 43387 2012-09-21 09:40:25Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: SrvIntNetR0.cpp 43975 2012-11-27 15:06:51Z knut.osmundsen@oracle.com $ */
 /** @file
  * Internal networking - The ring 0 service.
  */
@@ -3617,7 +3617,7 @@ INTNETR0DECL(int)       IntNetR0IfGetBufferPtrs(INTNETIFHANDLE hIf, PSUPDRVSESSI
 
     intnetR0IfRelease(pIf, pSession);
     LogFlow(("IntNetR0IfGetBufferPtrs: returns %Rrc *ppRing3Buf=%p *ppRing0Buf=%p\n",
-             rc, ppRing3Buf ? *ppRing3Buf : NULL, ppRing0Buf ? *ppRing0Buf : NULL));
+             rc, ppRing3Buf ? *ppRing3Buf : NIL_RTR3PTR, ppRing0Buf ? *ppRing0Buf : NULL));
     return rc;
 }
 
