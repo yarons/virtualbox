81ac0d39a32380ca2f5880235adb145303f28790
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-01-25 18:00:16 +0000

    CSAM: PVM -> PUVM for main interfaces, visibility cleanup.
    svn:sync-xref-src-repo-rev: r83356

--- src/VBox/Main/src-client/MachineDebuggerImpl.cpp
+++ src/VBox/Main/src-client/MachineDebuggerImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineDebuggerImpl.cpp 44375 2013-01-25 12:41:24Z knut.osmundsen@oracle.com $ */
+/* $Id: MachineDebuggerImpl.cpp 44385 2013-01-25 18:00:16Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox IMachineDebugger COM class implementation (VBoxC).
  */
@@ -386,18 +386,12 @@ STDMETHODIMP MachineDebugger::COMSETTER(CSAMEnabled) (BOOL aEnable)
     }
 
     Console::SafeVMPtr ptrVM(mParent);
-    if (FAILED(ptrVM.rc())) return ptrVM.rc();
+    if (FAILED(ptrVM.rc()))
+        return ptrVM.rc();
 
-    int vrc;
-    if (aEnable)
-        vrc = CSAMEnableScanning(ptrVM);
-    else
-        vrc = CSAMDisableScanning(ptrVM);
-
+    int vrc = CSAMR3SetScanningEnabled(ptrVM.rawUVM(), aEnable != FALSE);
     if (RT_FAILURE(vrc))
-    {
-        /** @todo handle error case */
-    }
+        return setError(VBOX_E_VM_ERROR, tr("CSAMR3SetScanningEnabled returned %Rrc"), vrc);
 
     return S_OK;
 }
