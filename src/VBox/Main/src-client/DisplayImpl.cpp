279732bbbbd3b72e6ca92657f5e37359aba0ebfb
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-01-27 23:17:46 +0000

    ConsoleImpl.h/.cpp: changed SafeVMPtrBase to not mess around with pVM, only pUVM.
    svn:sync-xref-src-repo-rev: r83376

--- src/VBox/Main/src-client/DisplayImpl.cpp
+++ src/VBox/Main/src-client/DisplayImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisplayImpl.cpp 44382 2013-01-25 14:14:51Z noreply@oracle.com $ */
+/* $Id: DisplayImpl.cpp 44402 2013-01-27 23:17:46Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -2990,7 +2990,7 @@ void Display::updateDisplayData(void)
     if (!mpDrv)
         return;
 
-#if DEBUG
+#ifdef VBOX_STRICT
     /*
      *  Sanity check. Note that this method may be called on EMT after Console
      *  has started the power down procedure (but before our #drvDestruct() is
@@ -2999,9 +2999,12 @@ void Display::updateDisplayData(void)
      *  build to save some ms (necessary to construct SafeVMPtrQuiet) in this
      *  time-critical method.
      */
-    Console::SafeVMPtrQuiet ptrVM (mParent);
+    Console::SafeVMPtrQuiet ptrVM(mParent);
     if (ptrVM.isOk())
-        Assert(VM_IS_EMT(ptrVM.raw()));
+    {
+        PVM pVM = VMR3GetVM(ptrVM.rawUVM());
+        Assert(VM_IS_EMT(pVM));
+    }
 #endif
 
     /* The method is only relevant to the primary framebuffer. */
