d4e5859164b58f5f14f468bbea939eea0b5c32b1
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2013-01-30 07:58:43 +0000

    BUGZ:6570 Add comment.
    svn:sync-xref-src-repo-rev: r83443

--- src/VBox/Runtime/r0drv/linux/spinlock-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/spinlock-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: spinlock-r0drv-linux.c 44443 2013-01-29 11:14:20Z vadim.galitsyn@oracle.com $ */
+/* $Id: spinlock-r0drv-linux.c 44451 2013-01-30 07:58:43Z vadim.galitsyn@oracle.com $ */
 /** @file
  * IPRT - Spinlocks, Ring-0 Driver, Linux.
  */
@@ -94,6 +94,14 @@ RTDECL(int)  RTSpinlockCreate(PRTSPINLOCK pSpinlock, uint32_t fFlags, const char
     pThis->idAssertCpu  = NIL_RTCPUID;
 #endif
 
+    /*
+       PLEASE DO NOT MODIFY THE NEXT FOUR LINES OF CODE!
+
+       We use this approach in order to split RTSPINLOCK_FLAGS_INTERRUPT_UNSAFE and
+       RTSPINLOCK_FLAGS_INTERRUPT_SAFE spinlocks into separated locking classes when
+       CONFIG_PROVE_LOCKING kernel option is enabled. Using single spin_lock_init()
+       call will trigger kernel warning regarding to incorrect spinlock usage.
+     */
     if (fFlags == RTSPINLOCK_FLAGS_INTERRUPT_UNSAFE)
         spin_lock_init(&pThis->Spinlock);
     else
