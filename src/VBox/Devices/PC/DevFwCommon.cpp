83280a43ec86d465f666164be2c8ef4c45dcdffc
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2013-02-19 13:37:25 +0000

    DMI: update Processor Information table (tyoe 4): do not refer to non-existent Cache Information table (type 7) handles since we do not provide them.
    svn:sync-xref-src-repo-rev: r83857

--- src/VBox/Devices/PC/DevFwCommon.cpp
+++ src/VBox/Devices/PC/DevFwCommon.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevFwCommon.cpp 44529 2013-02-04 15:54:15Z noreply@oracle.com $ */
+/* $Id: DevFwCommon.cpp 44752 2013-02-19 13:37:25Z vadim.galitsyn@oracle.com $ */
 /** @file
  * FwCommon - Shared firmware code (used by DevPcBios & DevEFI).
  */
@@ -771,9 +771,9 @@ int FwCommonPlantDMITable(PPDMDEVINS pDevIns, uint8_t *pTable, unsigned cbMax, P
                                            | RT_BIT(0)  /* CPU enabled */
                                            ;
         pProcessorInf->u8ProcessorUpgrade  = 0x04;   /* ZIF Socket */
-        pProcessorInf->u16L1CacheHandle    = 0x001C;
-        pProcessorInf->u16L2CacheHandle    = 0x001D;
-        pProcessorInf->u16L3CacheHandle    = 0xFFFF; /* unknown */
+        pProcessorInf->u16L1CacheHandle    = 0xFFFF; /* not specified */
+        pProcessorInf->u16L2CacheHandle    = 0xFFFF; /* not specified */
+        pProcessorInf->u16L3CacheHandle    = 0xFFFF; /* not specified */
         pProcessorInf->u8SerialNumber      = 0;      /* not specified */
         pProcessorInf->u8AssetTag          = 0;      /* not specified */
         pProcessorInf->u8PartNumber        = 0;      /* not specified */
