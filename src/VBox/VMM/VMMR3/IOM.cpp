d2600c2913481b8ec4ff1268a6e525940ecb9243
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-02-27 20:22:20 +0000

    IOM: Started on some todos...
    svn:sync-xref-src-repo-rev: r83999

--- src/VBox/VMM/VMMR3/IOM.cpp
+++ src/VBox/VMM/VMMR3/IOM.cpp
@@ -1,4 +1,4 @@
-/* $Id: IOM.cpp 44573 2013-02-06 15:24:36Z knut.osmundsen@oracle.com $ */
+/* $Id: IOM.cpp 44850 2013-02-27 20:22:20Z knut.osmundsen@oracle.com $ */
 /** @file
  * IOM - Input / Output Monitor.
  */
@@ -87,6 +87,13 @@
  * of ways thru PGM.
  *
  */
+
+/** @todo MMIO - simplifying the device end.
+ * - Add a return status for doing DBGFSTOP on access where there are no known
+ *   registers.
+ * -
+ *
+ *   */
 
 
 /*******************************************************************************
