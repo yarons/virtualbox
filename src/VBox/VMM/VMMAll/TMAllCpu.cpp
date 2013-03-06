9883857ca27a9b2d0dee68d2aca532b944db4f45
Michal Necasek <michal.necasek@oracle.com>
2013-03-06 15:08:46 +0000

    TM: Do not return the same value, either.
    svn:sync-xref-src-repo-rev: r84135

--- src/VBox/VMM/VMMAll/TMAllCpu.cpp
+++ src/VBox/VMM/VMMAll/TMAllCpu.cpp
@@ -1,4 +1,4 @@
-/* $Id: TMAllCpu.cpp 44932 2013-03-06 14:49:23Z michal.necasek@oracle.com $ */
+/* $Id: TMAllCpu.cpp 44933 2013-03-06 15:08:46Z michal.necasek@oracle.com $ */
 /** @file
  * TM - Timeout Manager, CPU Time, All Contexts.
  */
@@ -320,15 +320,15 @@ DECLINLINE(uint64_t) tmCpuTickGetInternal(PVMCPU pVCpu, bool fCheckTimers)
         else
             u64 = ASMReadTSC();
 
-        /* Never return a value lower than what the guest has already seen. */
-        if (u64 < pVCpu->tm.s.u64TSCLastSeen)
+        /* Always return a value higher than what the guest has already seen. */
+        if (RT_LIKELY(u64 > pVCpu->tm.s.u64TSCLastSeen))
+            pVCpu->tm.s.u64TSCLastSeen = u64;
+        else
         {
             STAM_COUNTER_INC(&pVM->tm.s.StatTSCUnderflow);
             pVCpu->tm.s.u64TSCLastSeen += 64;   /* @todo choose a good increment here */
             u64 = pVCpu->tm.s.u64TSCLastSeen;
         }
-        else
-            pVCpu->tm.s.u64TSCLastSeen = u64;
     }
     else
         u64 = pVCpu->tm.s.u64TSC;
