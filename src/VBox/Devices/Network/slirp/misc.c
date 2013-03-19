cca4725c9800ef3a2f7907d59bf4b4b5315e0801
Vasily Levchenko <noreply@oracle.com>
2013-03-19 11:14:04 +0000

    NAT: burn fix and blank lines are removed.
    svn:sync-xref-src-repo-rev: r84372

--- src/VBox/Devices/Network/slirp/misc.c
+++ src/VBox/Devices/Network/slirp/misc.c
@@ -1,4 +1,4 @@
-/* $Id: misc.c 45087 2013-03-19 11:10:36Z noreply@oracle.com $ */
+/* $Id: misc.c 45088 2013-03-19 11:14:04Z noreply@oracle.com $ */
 /** @file
  * NAT - helpers.
  */
@@ -98,13 +98,11 @@ fd_nonblock(int fd)
 
     opt = fcntl(fd, F_GETFL, 0);
     opt |= O_NONBLOCK;
-
-
     fcntl(fd, F_SETFL, opt);
 # endif
 #ifndef RT_OS_WINDOWS
     setsockopt(fd, SOL_SOCKET, SO_NOSIGPIPE, &opt, sizeof(int));
-#ednif
+#endif
 }
 
 # if !defined(VBOX_NAT_MEM_DEBUG)
