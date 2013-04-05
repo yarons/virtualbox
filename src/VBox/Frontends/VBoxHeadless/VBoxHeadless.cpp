8fc2cff8e72a12f4e42317900ceee9cb72ad3650
Frank Mehnert <noreply@oracle.com>
2013-04-05 14:07:18 +0000

    forward-ported 84794 (don't filter guest property events by patterns in VBoxHeadless as this affects all clients)
    svn:sync-xref-src-repo-rev: r84796

--- src/VBox/Frontends/VBoxHeadless/VBoxHeadless.cpp
+++ src/VBox/Frontends/VBoxHeadless/VBoxHeadless.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxHeadless.cpp 44158 2012-12-19 10:01:34Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxHeadless.cpp 45373 2013-04-05 14:07:18Z noreply@oracle.com $ */
 /** @file
  * VBoxHeadless - The VirtualBox Headless frontend for running VMs on servers.
  */
@@ -1287,10 +1287,12 @@ extern "C" DECLEXPORT(int) TrustedMain(int argc, char **argv, char **envp)
             com::SafeArray<VBoxEventType_T> eventTypes;
             eventTypes.push_back(VBoxEventType_OnGuestPropertyChanged);
 
-            /* Set the notification pattern to not cause too much load.
-             * Remove (or extend) this pattern when more guest properties need to be handled. */
-            CHECK_ERROR(machine, COMSETTER(GuestPropertyNotificationPatterns)(Bstr("/VirtualBox/GuestInfo/OS/*Logged*").raw()));
-
+            /**
+             * @todo Set the notification pattern to "/VirtualBox/GuestInfo/OS/ *Logged*"
+             *       to not cause too much load. The current API is broken as
+             *       IMachine::GuestPropertyNotificationPatterns() would change the
+             *       filter for _all_ clients. This is not what we want!
+             */
             CHECK_ERROR(es, RegisterListener(vboxListener, ComSafeArrayAsInParam(eventTypes), true));
         }
 
