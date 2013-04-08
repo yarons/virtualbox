e172c7a733fc93e9f9195c1b8859c46ba51a55ac
Frank Mehnert <noreply@oracle.com>
2013-04-08 09:32:00 +0000

    Runtime/http: more error codes
    svn:sync-xref-src-repo-rev: r84824

--- include/iprt/http.h
+++ include/iprt/http.h
@@ -1,4 +1,4 @@
-/* $Id: http.h 45339 2013-04-04 14:54:50Z noreply@oracle.com $ */
+/* $Id: http.h 45398 2013-04-08 09:32:00Z noreply@oracle.com $ */
 /** @file
  * IPRT - Simple HTTP Communication API.
  */
@@ -68,7 +68,8 @@ RTR3DECL(void) RTHttpDestroy(RTHTTP hHttp);
  *
  * @param    hHttp         HTTP interface handle.
  * @param    pcszUrl       URL.
- * @param    ppszResponse  HTTP response.
+ * @param    ppszResponse  HTTP response. It is guaranteed that this string is
+ *                         '\0'-terminated.
  */
 RTR3DECL(int) RTHttpGet(RTHTTP hHttp, const char *pcszUrl, char **ppszResponse);
 
