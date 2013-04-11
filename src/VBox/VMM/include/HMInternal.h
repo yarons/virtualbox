b8f8428c578dbacab52bcc1f0e9550c17301fd88
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-04-11 23:20:33 +0000

    VMM/VMMR0: HM bits.
    svn:sync-xref-src-repo-rev: r84967

--- src/VBox/VMM/include/HMInternal.h
+++ src/VBox/VMM/include/HMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: HMInternal.h 45410 2013-04-08 15:01:48Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMInternal.h 45501 2013-04-11 23:20:33Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM - Internal header file.
  */
@@ -382,8 +382,13 @@ typedef struct HM
         RTHCPHYS                    HCPhysScratch;
         R0PTRTYPE(uint8_t *)        pbScratch;
 #endif
+
+#ifndef VBOX_WITH_OLD_VTX_CODE
+        unsigned                    uFlushTaggedTlb;
+#else
         /** Ring 0 handlers for VT-x. */
         DECLR0CALLBACKMEMBER(void, pfnFlushTaggedTlb, (PVM pVM, PVMCPU pVCpu));
+#endif
 
 #if HC_ARCH_BITS == 32 && defined(VBOX_ENABLE_64_BITS_GUESTS)
         uint32_t                    u32Alignment;
@@ -452,7 +457,7 @@ typedef struct HM
      */
     AVLOU32TREE                     PatchTree;
     uint32_t                        cPatches;
-    HMTPRPATCH                  aPatches[64];
+    HMTPRPATCH                      aPatches[64];
 
     struct
     {
