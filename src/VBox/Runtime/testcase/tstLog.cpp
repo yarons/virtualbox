4d89b16cc3ae9d6d583b28000f87d3c812c8317a
Frank Mehnert <noreply@oracle.com>
2013-04-12 10:12:09 +0000

    Runtime/tstLog: unintended commit
    svn:sync-xref-src-repo-rev: r84986

--- src/VBox/Runtime/testcase/tstLog.cpp
+++ src/VBox/Runtime/testcase/tstLog.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstLog.cpp 45512 2013-04-12 10:10:40Z noreply@oracle.com $ */
+/* $Id: tstLog.cpp 45514 2013-04-12 10:12:09Z noreply@oracle.com $ */
 /** @file
  * IPRT Testcase - Log Formatting.
  */
@@ -30,15 +30,12 @@
 #include <iprt/log.h>
 #include <iprt/initterm.h>
 #include <iprt/err.h>
-#include <iprt/stream.h>
 
 #include <stdio.h>
 
 int main()
 {
     RTR3InitExeNoArguments(0);
-    uint32_t a = 0x80000000, b = 0x80000000, c = 0x80000000;
-    RTPrintf("%d %d", 0x80000000, 0x80000000);
     printf("tstLog: Requires manual inspection of the log output!\n");
     RTLogPrintf("%%Rrc %d: %Rrc\n", VERR_INVALID_PARAMETER, VERR_INVALID_PARAMETER);
     RTLogPrintf("%%Rrs %d: %Rrs\n", VERR_INVALID_PARAMETER, VERR_INVALID_PARAMETER);
