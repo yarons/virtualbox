6fe26ba89a591cbb86251e163355f8a77b0597d8
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2013-04-24 18:57:06 +0000

    build fix
    svn:sync-xref-src-repo-rev: r85263

--- src/VBox/Devices/Network/SrvIntNetR0.cpp
+++ src/VBox/Devices/Network/SrvIntNetR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: SrvIntNetR0.cpp 45716 2013-04-24 18:47:18Z aleksey.ilyushin@oracle.com $ */
+/* $Id: SrvIntNetR0.cpp 45717 2013-04-24 18:57:06Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * Internal networking - The ring 0 service.
  */
@@ -844,12 +844,12 @@ DECLINLINE(PINTNETMACTABENTRY) intnetR0NetworkFindMacAddrEntry(PINTNETNETWORK pN
  */
 DECLINLINE(bool) intnetR0IPv6AddrIsGood(RTNETADDRIPV6 addr)
 {
-    return  !(   (   addr.QWords.qw0 == 0 && addr.QWords.qw1 == 0)                      /* :: */
-              || (   addr.Words.w0 & RT_H2BE_U16(0xff00) == RT_H2BE_U16(0xff00)) /* multicast */
+    return  !(   (   addr.QWords.qw0 == 0 && addr.QWords.qw1 == 0)                       /* :: */
+              || (  (addr.Words.w0 & RT_H2BE_U16(0xff00)) == RT_H2BE_U16(0xff00)) /* multicast */
               || (   addr.Words.w0 == 0 && addr.Words.w1 == 0
                   && addr.Words.w2 == 0 && addr.Words.w3 == 0
                   && addr.Words.w4 == 0 && addr.Words.w5 == 0
-                  && addr.Words.w6 == 0 && addr.Words.w7 == RT_H2BE_U16(0x0001)));     /* ::1 */
+                  && addr.Words.w6 == 0 && addr.Words.w7 == RT_H2BE_U16(0x0001)));      /* ::1 */
 }
 
 
