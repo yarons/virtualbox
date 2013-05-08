8f1eb67d979c6fa652f28ceb36dfd5dff5156f5f
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-05-08 23:44:31 +0000

    VMM: nits.
    svn:sync-xref-src-repo-rev: r85615

--- src/VBox/VMM/VMMR3/HM.cpp
+++ src/VBox/VMM/VMMR3/HM.cpp
@@ -1,4 +1,4 @@
-/* $Id: HM.cpp 45949 2013-05-08 12:37:14Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HM.cpp 45963 2013-05-08 23:44:31Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM - Intel/AMD VM Hardware Support Manager.
  */
@@ -2940,11 +2940,11 @@ VMMR3_INT_DECL(void) HMR3CheckError(PVM pVM, int iStatusCode)
                     LogRel(("HM: Cpu%d ProcCtls2     %#RX32\n", i, pVM->aCpus[i].hm.s.vmx.u32ProcCtls2));
                     LogRel(("HM: Cpu%d EntryCtls     %#RX32\n", i, pVM->aCpus[i].hm.s.vmx.u32EntryCtls));
                     LogRel(("HM: Cpu%d ExitCtls      %#RX32\n", i, pVM->aCpus[i].hm.s.vmx.u32ExitCtls));
-                    LogRel(("HM: Cpu%d MSRBitmapPhys %RHp\n", i, pVM->aCpus[i].hm.s.vmx.HCPhysMsrBitmap));
+                    LogRel(("HM: Cpu%d MSRBitmapPhys %#RHp\n",  i, pVM->aCpus[i].hm.s.vmx.HCPhysMsrBitmap));
 #ifdef VBOX_WITH_AUTO_MSR_LOAD_RESTORE
-                    LogRel(("HM: Cpu%d GuestMSRPhys  %RHp\n", i, pVM->aCpus[i].hm.s.vmx.HCPhysGuestMsr));
-                    LogRel(("HM: Cpu%d HostMsrPhys   %RHp\n", i, pVM->aCpus[i].hm.s.vmx.HCPhysHostMsr));
-                    LogRel(("HM: Cpu%d cGuestMSRs    %u\n",   i, pVM->aCpus[i].hm.s.vmx.cGuestMsrs));
+                    LogRel(("HM: Cpu%d GuestMSRPhys  %#RHp\n",  i, pVM->aCpus[i].hm.s.vmx.HCPhysGuestMsr));
+                    LogRel(("HM: Cpu%d HostMsrPhys   %#RHp\n",  i, pVM->aCpus[i].hm.s.vmx.HCPhysHostMsr));
+                    LogRel(("HM: Cpu%d cGuestMSRs    %u\n",     i, pVM->aCpus[i].hm.s.vmx.cGuestMsrs));
 #endif
                 }
                 /** @todo Log VM-entry event injection control fields
