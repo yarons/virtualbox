cdcab68b32deb219f83d5a94f6446b3be187262c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-05-14 15:28:37 +0000

    build fix
    svn:sync-xref-src-repo-rev: r85746

--- src/VBox/Runtime/common/dbg/dbgcfg.cpp
+++ src/VBox/Runtime/common/dbg/dbgcfg.cpp
@@ -1,4 +1,4 @@
-/* $Id: dbgcfg.cpp 46070 2013-05-14 15:21:57Z knut.osmundsen@oracle.com $ */
+/* $Id: dbgcfg.cpp 46071 2013-05-14 15:28:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Debugging Configuration.
  */
@@ -443,7 +443,7 @@ static bool rtDbgCfgIsFileAndFixCase(char *pszPath, const char *pszFilename, boo
     {
         pszPath[cchPath] = '\0';
         rc = RTPathAppend(pszPath, RTPATH_MAX, pszFilename);
-        AssertRCReturn(rc, rc);
+        AssertRCReturn(rc, false);
         pszPath[strlen(pszPath) - 1] = '_';
 
         if (pfProbablyCompressed)
