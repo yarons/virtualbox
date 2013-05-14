8474769da56ea476a6472d37b61464f3cfe0b344
Mikhail Sennikovsky <noreply@oracle.com>
2013-05-14 12:22:03 +0000

    crOpenGL: remove false assertion
    svn:sync-xref-src-repo-rev: r85733

--- src/VBox/HostServices/SharedOpenGL/crserverlib/server_presenter.cpp
+++ src/VBox/HostServices/SharedOpenGL/crserverlib/server_presenter.cpp
@@ -1,4 +1,4 @@
-/* $Id: server_presenter.cpp 45132 2013-03-21 16:11:28Z noreply@oracle.com $ */
+/* $Id: server_presenter.cpp 46060 2013-05-14 12:22:03Z noreply@oracle.com $ */
 
 /** @file
  * Presenter API
@@ -363,7 +363,6 @@ void* CrHlpGetTexImage(CRContext *pCurCtx, PVBOXVR_TEXTURE pTexture, GLuint idPB
         crStateGetTextureObjectAndImage(pCurCtx, pTexture->target, 0, &pTObj, &pTImg);
 
         GLuint uid = pTObj->hwid;
-        CRASSERT(uid);
         cr_server.head_spu->dispatch_table.BindTexture(pTexture->target, uid);
     }
     else
