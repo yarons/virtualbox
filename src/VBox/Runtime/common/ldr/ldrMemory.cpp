be51ef22c5133cd1ee62c83fd1a117706792723a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-05-14 20:09:44 +0000

    oops.
    svn:sync-xref-src-repo-rev: r85758

--- src/VBox/Runtime/common/ldr/ldrMemory.cpp
+++ src/VBox/Runtime/common/ldr/ldrMemory.cpp
@@ -1,10 +1,10 @@
-/* $Id: ldrMemory.cpp 46081 2013-05-14 20:09:29Z knut.osmundsen@oracle.com $ */
+/* $Id: ldrMemory.cpp 46082 2013-05-14 20:09:44Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Binary Image Loader, The Memory/Debugger Oriented Parts.
  */
 
 /*
- * Copyright (C) 2006-2013Oracle Corporation
+ * Copyright (C) 2006-2013 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
