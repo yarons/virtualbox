1c098f739a4cefbc1868344cee44d9579f8e3a05
Alexander Eichner <alexander.eichner@oracle.com>
2013-05-21 06:30:21 +0000

    warnings
    svn:sync-xref-src-repo-rev: r85896

--- src/VBox/Runtime/common/filesystem/filesystemext.cpp
+++ src/VBox/Runtime/common/filesystem/filesystemext.cpp
@@ -1,4 +1,4 @@
-/* $Id: filesystemext.cpp 41552 2012-06-01 18:03:46Z alexander.eichner@oracle.com $ */
+/* $Id: filesystemext.cpp 46186 2013-05-21 06:30:21Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT Filesystem API (FileSys) - ext2/3 format.
  */
@@ -325,6 +325,8 @@ static DECLCALLBACK(void) rtFsExtDestroy(void *pvThis)
 
 static DECLCALLBACK(int) rtFsExtOpenRoot(void *pvThis, PRTVFSDIR phVfsDir)
 {
+    NOREF(pvThis);
+    NOREF(phVfsDir);
     return VERR_NOT_IMPLEMENTED;
 }
 
