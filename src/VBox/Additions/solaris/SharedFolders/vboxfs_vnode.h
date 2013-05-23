b232b1c387bb2af9510c64a4445fd2a84d58ae6a
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-05-23 14:49:20 +0000

    solaris/SharedFolders: forgot header update.
    svn:sync-xref-src-repo-rev: r85968

--- src/VBox/Additions/solaris/SharedFolders/vboxfs_vnode.h
+++ src/VBox/Additions/solaris/SharedFolders/vboxfs_vnode.h
@@ -1,10 +1,10 @@
-/* $Id: vboxfs_vnode.h 46238 2013-05-23 14:42:14Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: vboxfs_vnode.h 46239 2013-05-23 14:49:20Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VirtualBox File System for Solaris Guests, VNode header.
  */
 
 /*
- * Copyright (C) 2009-2011 Oracle Corporation
+ * Copyright (C) 2009-2013 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
