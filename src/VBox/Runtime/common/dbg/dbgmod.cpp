b728b76f2d72dae1ec34b2ecc5297b135b341420
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-05-26 15:27:31 +0000

    dbgmodcodeview.cpp: More sane segment translation and more stuff.
    svn:sync-xref-src-repo-rev: r86007

--- src/VBox/Runtime/common/dbg/dbgmod.cpp
+++ src/VBox/Runtime/common/dbg/dbgmod.cpp
@@ -1,4 +1,4 @@
-/* $Id: dbgmod.cpp 46266 2013-05-25 19:51:19Z knut.osmundsen@oracle.com $ */
+/* $Id: dbgmod.cpp 46269 2013-05-26 15:27:31Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Debug Module Interpreter.
  */
@@ -279,14 +279,12 @@ static DECLCALLBACK(int) rtDbgModInitOnce(void *pvUser)
         rc = rtDbgModDebugInterpreterRegister(&g_rtDbgModVtDbgNm);
         if (RT_SUCCESS(rc))
             rc = rtDbgModDebugInterpreterRegister(&g_rtDbgModVtDbgDwarf);
-#if 0 /* NOTE!! EXPERIMENTAL, SHOULDN'T BE ENABLED IN SVN YET!! */
-        if (RT_SUCCESS(rc))
-            rc = rtDbgModDebugInterpreterRegister(&g_rtDbgModVtDbgCodeView);
-#endif
 #ifdef RT_OS_WINDOWS
         if (RT_SUCCESS(rc))
             rc = rtDbgModDebugInterpreterRegister(&g_rtDbgModVtDbgDbgHelp);
 #endif
+        if (RT_SUCCESS(rc))
+            rc = rtDbgModDebugInterpreterRegister(&g_rtDbgModVtDbgCodeView);
         if (RT_SUCCESS(rc))
             rc = rtDbgModImageInterpreterRegister(&g_rtDbgModVtImgLdr);
         if (RT_SUCCESS(rc))
