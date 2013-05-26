8fcc4758f3cc7d4cabd819e87507783cc6212b53
Alexander Eichner <alexander.eichner@oracle.com>
2013-05-26 18:02:48 +0000

    Audio/DevIchHda: Implement support for R0 and RC
    svn:sync-xref-src-repo-rev: r86011

--- src/VBox/Devices/testcase/tstDeviceStructSize.cpp
+++ src/VBox/Devices/testcase/tstDeviceStructSize.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstDeviceStructSize.cpp 45796 2013-04-28 22:23:18Z knut.osmundsen@oracle.com $ */
+/* $Id: tstDeviceStructSize.cpp 46272 2013-05-26 18:02:48Z alexander.eichner@oracle.com $ */
 /** @file
  * tstDeviceStructSize - testcase for check structure sizes/alignment
  *                       and to verify that HC and RC uses the same
@@ -96,6 +96,9 @@
 # undef LOG_GROUP
 # include "../Bus/DevPciRaw.cpp"
 #endif
+
+#undef LOG_GROUP
+#include "../Audio/DevIchHda.cpp"
 
 #include <stdio.h>
 
