ac7ca65d2a88c5387c1040e7f0001d568ea433ce
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-05-30 17:27:55 +0000

    rtTestXmlStart: fix.
    svn:sync-xref-src-repo-rev: r86106

--- src/VBox/Runtime/r3/test.cpp
+++ src/VBox/Runtime/r3/test.cpp
@@ -1,4 +1,4 @@
-/* $Id: test.cpp 46317 2013-05-30 08:38:37Z noreply@oracle.com $ */
+/* $Id: test.cpp 46333 2013-05-30 17:27:55Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Testcase Framework.
  */
@@ -774,12 +774,9 @@ static void rtTestXmlStart(PRTTESTINT pTest, const char *pszTest)
     {
         rtTestXmlOutput(pTest, "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
         pTest->eXmlState = RTTESTINT::kXmlPos_ElementEnd;
-        if (!(pTest->fFlags & RTTEST_C_XML_DELAY_TOP_TEST))
-        {
-            pTest->fXmlTopTestDone = true;
-            if (!pTest->fXmlOmitTopTest)
-                rtTestXmlElemStart(pTest, "Test", "name=%RMas", pszTest);
-        }
+        pTest->fXmlTopTestDone = !(pTest->fFlags & RTTEST_C_XML_DELAY_TOP_TEST) || pTest->fXmlOmitTopTest;
+        if (pTest->fXmlTopTestDone && !pTest->fXmlOmitTopTest)
+            rtTestXmlElemStart(pTest, "Test", "name=%RMas", pszTest);
     }
 }
 
@@ -938,7 +935,7 @@ static void rtTestXmlEnd(PRTTESTINT pTest)
          * final timestamp and some certainty that the XML is valid.
          */
         size_t i = pTest->cXmlElements;
-        AssertReturnVoid(i > 0 || pTest->fXmlOmitTopTest);
+        AssertReturnVoid(i > 0 || pTest->fXmlOmitTopTest || !pTest->fXmlTopTestDone);
         while (i-- > 1)
         {
             const char *pszTag = pTest->apszXmlElements[pTest->cXmlElements];
