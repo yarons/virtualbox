b2e4b821fca08f52e9df3e85fe0dcbbb46fce9a4
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-06-03 15:01:02 +0000

    VMM/HM: Comment nits.
    svn:sync-xref-src-repo-rev: r86179

--- src/VBox/VMM/VMMAll/HMAll.cpp
+++ src/VBox/VMM/VMMAll/HMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMAll.cpp 46297 2013-05-28 14:38:48Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMAll.cpp 46363 2013-06-03 15:01:02Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM - All contexts.
  */
@@ -102,7 +102,7 @@ VMM_INT_DECL(int) HMInvalidatePage(PVMCPU pVCpu, RTGCPTR GCVirt)
 }
 
 /**
- * Flushes the guest TLB
+ * Flushes the guest TLB.
  *
  * @returns VBox status code.
  * @param   pVCpu       Pointer to the VMCPU.
@@ -229,7 +229,7 @@ VMM_INT_DECL(int) HMInvalidatePageOnAllVCpus(PVM pVM, RTGCPTR GCPtr)
         else
         {
             hmQueueInvlPage(pVCpu, GCPtr);
-            hmPokeCpuForTlbFlush(pVCpu, false /*fAccountFlushStat*/);
+            hmPokeCpuForTlbFlush(pVCpu, false /* fAccountFlushStat */);
         }
     }
 
@@ -238,7 +238,7 @@ VMM_INT_DECL(int) HMInvalidatePageOnAllVCpus(PVM pVM, RTGCPTR GCPtr)
 
 
 /**
- * Flush the TLBs of all VCPUs
+ * Flush the TLBs of all VCPUs.
  *
  * @returns VBox status code.
  * @param   pVM       Pointer to the VM.
@@ -262,7 +262,7 @@ VMM_INT_DECL(int) HMFlushTLBOnAllVCpus(PVM pVM)
         {
             VMCPU_FF_SET(pVCpu, VMCPU_FF_TLB_FLUSH);
             if (idThisCpu != idCpu)
-                hmPokeCpuForTlbFlush(pVCpu, true /*fAccountFlushStat*/);
+                hmPokeCpuForTlbFlush(pVCpu, true /* fAccountFlushStat */);
         }
     }
 
