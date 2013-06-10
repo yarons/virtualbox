641b9229b38b5252db743bb7f3898d86463b62b8
Frank Mehnert <noreply@oracle.com>
2013-06-10 14:35:37 +0000

    build fix
    svn:sync-xref-src-repo-rev: r86311

--- src/VBox/Main/src-client/DisplayImpl.cpp
+++ src/VBox/Main/src-client/DisplayImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisplayImpl.cpp 46467 2013-06-10 14:27:07Z noreply@oracle.com $ */
+/* $Id: DisplayImpl.cpp 46468 2013-06-10 14:35:37Z noreply@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -2678,7 +2678,7 @@ STDMETHODIMP Display::EnableVideoCaptureScreens(ComSafeArrayIn(BOOL, aScreens))
 #ifdef VBOX_WITH_VPX
     com::SafeArray<LONG> Screens(ComSafeArrayInArg(aScreens));
     for (unsigned i = 0; i < Screens.size(); i++)
-        maVideoRecEnabled[i] = RT_BOOL(aScreens[i]);
+        maVideoRecEnabled[i] = aScreens[i] ? true : false;
     return S_OK;
 #else
     return E_NOTIMPL;
