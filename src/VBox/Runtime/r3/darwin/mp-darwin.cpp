3352e9f6260be60d24715ca5ab4470431274670e
Frank Mehnert <noreply@oracle.com>
2013-06-19 15:41:05 +0000

    Runtime/r3/darwin/mp-darwin: fixed typo
    svn:sync-xref-src-repo-rev: r86559

--- src/VBox/Runtime/r3/darwin/mp-darwin.cpp
+++ src/VBox/Runtime/r3/darwin/mp-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: mp-darwin.cpp 44528 2013-02-04 14:27:54Z noreply@oracle.com $ */
+/* $Id: mp-darwin.cpp 46668 2013-06-19 15:41:05Z noreply@oracle.com $ */
 /** @file
  * IPRT - Multiprocessor, Darwin.
  */
@@ -97,7 +97,7 @@ RTDECL(bool) RTMpIsCpuOnline(RTCPUID idCpu)
     kern_return_t krc = host_processor_info(mach_host_self(),
         PROCESSOR_BASIC_INFO, &nCpus, (processor_info_array_t*)&pinfo, &count);
     AssertReturn (krc == KERN_SUCCESS, true);
-    bool isOnline = idCpu < nCpus ? pinfo[idCpu].running : true;
+    bool isOnline = idCpu < nCpus ? pinfo[idCpu].running : false;
     vm_deallocate(mach_task_self(), (vm_address_t)pinfo, count * sizeof(*pinfo));
     return isOnline;
 #endif
