7bb98c35a1dc3fa2f4e6dcbd895cd2c2b232c097
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-06-25 13:38:45 +0000

    VMM/EM: Commented out debug assertion for single-stepping.
    svn:sync-xref-src-repo-rev: r86711

--- src/VBox/VMM/VMMR3/EM.cpp
+++ src/VBox/VMM/VMMR3/EM.cpp
@@ -1,4 +1,4 @@
-/* $Id: EM.cpp 46486 2013-06-10 22:14:40Z knut.osmundsen@oracle.com $ */
+/* $Id: EM.cpp 46780 2013-06-25 13:38:45Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * EM - Execution Monitor / Manager.
  */
@@ -2239,7 +2239,11 @@ VMMR3_INT_DECL(int) EMR3ExecuteVM(PVM pVM, PVMCPU pVCpu)
                  * Guest debug events.
                  */
                 case VINF_EM_DBG_STEPPED:
+                    /* Commenting this assertion for now as it hinders with single-stepping in new AMD-V code
+                     * (using guest EFLAGS.TF) and returning VINF_EM_DBG_STEPPED in the #DB handler. */
+#if 0
                     AssertMsgFailed(("VINF_EM_DBG_STEPPED cannot be here!"));
+#endif
                 case VINF_EM_DBG_STOP:
                 case VINF_EM_DBG_BREAKPOINT:
                 case VINF_EM_DBG_STEP:
