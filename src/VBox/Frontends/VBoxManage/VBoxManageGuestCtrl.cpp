cb24d399beef46b72a84be89f1b89b25cc88155d
Frank Mehnert <noreply@oracle.com>
2013-07-05 10:38:51 +0000

    VBoxManage/Help: unify help output regarding UUID and VM name
    svn:sync-xref-src-repo-rev: r87017

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageGuestCtrl.cpp 46999 2013-07-05 10:30:22Z noreply@oracle.com $ */
+/* $Id: VBoxManageGuestCtrl.cpp 47000 2013-07-05 10:38:51Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - Implementation of guestcontrol command.
  */
@@ -222,7 +222,7 @@ static int ctrlCopyDirExists(PCOPYCONTEXT pContext, bool bGuest, const char *psz
 void usageGuestControl(PRTSTREAM pStrm, const char *pcszSep1, const char *pcszSep2)
 {
     RTStrmPrintf(pStrm,
-                       "%s guestcontrol %s    <vmname|uuid>\n"
+                       "%s guestcontrol %s    <uuid|vmname>\n"
                  "                            exec[ute]\n"
                  "                            --image <path to program> --username <name>\n"
                  "                            [--passwordfile <file> | --password <password>]\n"
