f1e83b761051908193e2e4456218889c8d1ae359
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2013-07-12 12:27:42 +0000

    initterm-r0drv.cpp: RTR0TermForced must not forget to set g_crtR0Users to zero. Darwin my try _start() a _stop()'ed module more than once if _start fails.
    svn:sync-xref-src-repo-rev: r87209

--- src/VBox/Runtime/r0drv/initterm-r0drv.cpp
+++ src/VBox/Runtime/r0drv/initterm-r0drv.cpp
@@ -1,4 +1,4 @@
-/* $Id: initterm-r0drv.cpp 44528 2013-02-04 14:27:54Z noreply@oracle.com $ */
+/* $Id: initterm-r0drv.cpp 47115 2013-07-12 12:27:42Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Initialization & Termination, R0 Driver, Common.
  */
@@ -64,6 +64,7 @@ static int32_t volatile g_crtR0Users = 0;
 RTR0DECL(int) RTR0Init(unsigned fReserved)
 {
     int rc;
+    uint32_t cNewUsers;
     Assert(fReserved == 0);
     RT_ASSERT_PREEMPTIBLE();
 
@@ -72,8 +73,14 @@ RTR0DECL(int) RTR0Init(unsigned fReserved)
      * We rely on the module loader to ensure that there are no
      * initialization races should two modules share the IPRT.
      */
-    if (ASMAtomicIncS32(&g_crtR0Users) != 1)
-        return VINF_SUCCESS;
+    cNewUsers = ASMAtomicIncS32(&g_crtR0Users);
+    if (cNewUsers != 1)
+    {
+        if (cNewUsers > 1)
+            return VINF_SUCCESS;
+        ASMAtomicDecS32(&g_crtR0Users);
+        return VERR_INTERNAL_ERROR_3;
+    }
 
     rc = rtR0InitNative();
     if (RT_SUCCESS(rc))
@@ -126,6 +133,8 @@ RTR0DECL(void) RTR0Term(void)
     Assert(cNewUsers >= 0);
     if (cNewUsers == 0)
         rtR0Term();
+    else if (cNewUsers < 0)
+        ASMAtomicIncS32(&g_crtR0Users);
 }
 RT_EXPORT_SYMBOL(RTR0Term);
 
@@ -134,7 +143,9 @@ RT_EXPORT_SYMBOL(RTR0Term);
 RTR0DECL(void) RTR0TermForced(void)
 {
     RT_ASSERT_PREEMPTIBLE();
+
     AssertMsg(g_crtR0Users == 1, ("%d\n", g_crtR0Users));
+    ASMAtomicWriteS32(&g_crtR0Users, 0);
 
     rtR0Term();
 }
