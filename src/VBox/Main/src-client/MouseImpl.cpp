0c7b6e742fd5fe009330d0c9ddbf2d385bd5be29
Michael Thayer <noreply@oracle.com>
2013-07-23 18:08:27 +0000

    Main/MouseImpl: properly pass through multi-touch notification.
    svn:sync-xref-src-repo-rev: r87504

--- src/VBox/Main/src-client/MouseImpl.cpp
+++ src/VBox/Main/src-client/MouseImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MouseImpl.cpp 47245 2013-07-19 08:01:57Z noreply@oracle.com $ */
+/* $Id: MouseImpl.cpp 47358 2013-07-23 18:08:27Z noreply@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -224,7 +224,7 @@ STDMETHODIMP Mouse::COMGETTER(MultiTouchSupported) (BOOL *multiTouchSupported)
     AutoCaller autoCaller(this);
     if (FAILED(autoCaller.rc())) return autoCaller.rc();
 
-    *multiTouchSupported = FALSE;
+    *multiTouchSupported = supportsMT();
     return S_OK;
 }
 
@@ -703,6 +703,8 @@ void Mouse::getDeviceCaps(bool *pfAbs, bool *pfRel, bool *pfMT)
         *pfAbs = fAbsDev;
     if (pfRel)
         *pfRel = fRelDev;
+    if (pfMT)
+        *pfMT = fMTDev;
 }
 
 
