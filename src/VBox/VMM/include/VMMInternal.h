b478e8bed5bbe2edd5bc36aa0aad3e2247c5397d
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-07-29 16:35:34 +0000

    VMM/VMMR0: Fix Win-32 alignment.
    svn:sync-xref-src-repo-rev: r87630

--- src/VBox/VMM/include/VMMInternal.h
+++ src/VBox/VMM/include/VMMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VMMInternal.h 47465 2013-07-29 15:04:41Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: VMMInternal.h 47467 2013-07-29 16:35:34Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VMM - Internal header file.
  */
@@ -422,8 +422,9 @@ typedef struct VMMCPU
     R0PTRTYPE(RTTHREADCTX)      hR0ThreadCtx;
 #if HC_ARCH_BITS == 32
     uint32_t                    u32Padding;
-#endif
+#else
     uint64_t                    u64Padding;
+#endif
     /** @} */
 
     /** @name Rendezvous
