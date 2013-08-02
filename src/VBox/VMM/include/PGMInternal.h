f91fac8a495e9877cf34a882a1cdd90c9c01c350
Alexander Eichner <alexander.eichner@oracle.com>
2013-08-02 17:07:39 +0000

    VMM: Fix macro corrupting the HCPhysFN member unintentionally instead of setting the fWrittenToY member (introduced by r71527). Fixes live snapshotting
    svn:sync-xref-src-repo-rev: r87714

--- src/VBox/VMM/include/PGMInternal.h
+++ src/VBox/VMM/include/PGMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: PGMInternal.h 46125 2013-05-16 13:59:37Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMInternal.h 47524 2013-08-02 17:07:39Z alexander.eichner@oracle.com $ */
 /** @file
  * PGM - Internal header file.
  */
@@ -1001,7 +1001,7 @@ typedef PPGMPAGE *PPPGMPAGE;
  * @param   a_pPage     Pointer to the physical guest page tracking structure.
  */
 #define PGM_PAGE_SET_WRITTEN_TO(a_pVM, a_pPage) \
-    do { (a_pPage)->au8[1] |= UINT8_C(0x80); PGM_PAGE_ASSERT_LOCK(a_pVM); } while (0) /// FIXME FIXME
+    do { (a_pPage)->s.fWrittenToY = 1; PGM_PAGE_ASSERT_LOCK(a_pVM); } while (0) /// FIXME FIXME
 
 /**
  * Clears the written-to indicator.
