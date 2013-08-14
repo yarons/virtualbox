eb5bb1d5c4458a0d6cc88bc2785d2f17d1a58386
Alexander Eichner <alexander.eichner@oracle.com>
2013-08-14 10:41:56 +0000

    FE/VBoxManage: Fix error message printing with variable arguments
    svn:sync-xref-src-repo-rev: r87960

--- src/VBox/Frontends/VBoxManage/VBoxManageDisk.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageDisk.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageDisk.cpp 44498 2013-01-31 15:48:05Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxManageDisk.cpp 47720 2013-08-14 10:41:56Z alexander.eichner@oracle.com $ */
 /** @file
  * VBoxManage - The disk related commands.
  */
@@ -47,7 +47,7 @@ using namespace com;
 
 static DECLCALLBACK(void) handleVDError(void *pvUser, int rc, RT_SRC_POS_DECL, const char *pszFormat, va_list va)
 {
-    RTMsgError(pszFormat, va);
+    RTMsgErrorV(pszFormat, va);
     RTMsgError("Error code %Rrc at %s(%u) in function %s", rc, RT_SRC_POS_ARGS);
 }
 
