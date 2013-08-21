a91fd360a31be70f1cf36168a1f3fb2721dcc412
Sergey Dubov <sergey.dubov@oracle.com>
2013-08-21 08:16:23 +0000

    FE/Qt: Popup-center: Popup-pane: Make sure pane is really focused on polish-event if marked to be.
    svn:sync-xref-src-repo-rev: r88262

--- src/VBox/Frontends/VirtualBox/src/widgets/UIPopupPane.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIPopupPane.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIPopupPane.cpp 47759 2013-08-15 12:39:37Z sergey.dubov@oracle.com $ */
+/* $Id: UIPopupPane.cpp 47949 2013-08-21 08:16:23Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -378,6 +378,10 @@ void UIPopupPane::showEvent(QShowEvent *pEvent)
 
 void UIPopupPane::polishEvent(QShowEvent*)
 {
+    /* Focus if marked as 'focused': */
+    if (m_fFocused)
+        setFocus();
+
     /* Emit signal to start *show* animation: */
     emit sigToShow();
 }
