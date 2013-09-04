3aa38c3e395b9a47505aa7a12295436ed1f04d56
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-09-04 13:49:52 +0000

    VMMR3/IEM: STAM description typo.
    svn:sync-xref-src-repo-rev: r88681

--- src/VBox/VMM/VMMR3/IEMR3.cpp
+++ src/VBox/VMM/VMMR3/IEMR3.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMR3.cpp 47548 2013-08-06 03:58:21Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMR3.cpp 48266 2013-09-04 13:49:52Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager.
  */
@@ -53,7 +53,7 @@ VMMR3DECL(int)      IEMR3Init(PVM pVM)
         STAMR3RegisterF(pVM, &pVCpu->iem.s.cInstructions,             STAMTYPE_U32,       STAMVISIBILITY_ALWAYS, STAMUNIT_COUNT,
                         "Instructions interpreted",          "/IEM/CPU%u/cInstructions", idCpu);
         STAMR3RegisterF(pVM, &pVCpu->iem.s.cPotentialExits,           STAMTYPE_U32,       STAMVISIBILITY_ALWAYS, STAMUNIT_COUNT,
-                        "Potential exists",                  "/IEM/CPU%u/cPotentialExits", idCpu);
+                        "Potential exits",                   "/IEM/CPU%u/cPotentialExits", idCpu);
         STAMR3RegisterF(pVM, &pVCpu->iem.s.cRetAspectNotImplemented,  STAMTYPE_U32_RESET, STAMVISIBILITY_ALWAYS, STAMUNIT_COUNT,
                         "VERR_IEM_ASPECT_NOT_IMPLEMENTED",   "/IEM/CPU%u/cRetAspectNotImplemented", idCpu);
         STAMR3RegisterF(pVM, &pVCpu->iem.s.cRetInstrNotImplemented,   STAMTYPE_U32_RESET, STAMVISIBILITY_ALWAYS, STAMUNIT_COUNT,
