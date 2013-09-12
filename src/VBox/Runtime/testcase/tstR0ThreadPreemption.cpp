c41221f2b55698a65c76181e946ab0ad0d9559d6
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-09-12 22:36:08 +0000

    tstR0ThreadPreemption: Try explicitly yielding.
    svn:sync-xref-src-repo-rev: r88921

--- src/VBox/Runtime/testcase/tstR0ThreadPreemption.cpp
+++ src/VBox/Runtime/testcase/tstR0ThreadPreemption.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstR0ThreadPreemption.cpp 47899 2013-08-20 12:24:49Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: tstR0ThreadPreemption.cpp 48463 2013-09-12 22:36:08Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IPRT R0 Testcase - Thread Preemption.
  */
@@ -383,6 +383,7 @@ DECLEXPORT(int) TSTR0ThreadPreemptionSrvReqHandler(PSUPDRVSESSION pSession, uint
 
                 RTThreadSleep(cMsSleepGranularity);
                 cMsSlept += cMsSleepGranularity;
+                RTThreadYield();
             }
 
             if (!ASMAtomicReadBool(&pCtxData->fPreemptingInvoked))
