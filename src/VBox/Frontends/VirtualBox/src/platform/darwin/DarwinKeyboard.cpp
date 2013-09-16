bf7edf31586de133c1d5efd3c1d1f098363f0357
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2013-09-16 17:10:24 +0000

    In addition to r88992: reduce release logging, fix coding style.
    svn:sync-xref-src-repo-rev: r88994

--- src/VBox/Frontends/VirtualBox/src/platform/darwin/DarwinKeyboard.cpp
+++ src/VBox/Frontends/VirtualBox/src/platform/darwin/DarwinKeyboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: DarwinKeyboard.cpp 48496 2013-09-16 16:55:57Z vadim.galitsyn@oracle.com $ */
+/* $Id: DarwinKeyboard.cpp 48498 2013-09-16 17:10:24Z vadim.galitsyn@oracle.com $ */
 /** @file
  * Common GUI Library - Darwin Keyboard routines.
  *
@@ -1213,13 +1213,9 @@ static void darwinLedElementSetValue(IOHIDDeviceRef hidDevice, IOHIDElementRef e
     {
         rc = IOHIDDeviceSetValue(hidDevice, element, valueRef);
         if (rc != kIOReturnSuccess)
-        {
             LogRelFlow(("Warning! Something went wrong in attempt to turn %s HID device led (error %d)!\n", ((fEnabled) ? "on" : "off"), rc));
-        }
         else
-        {
-            LogRelFlow(("Led (%d) is turned %s\n", (int)IOHIDElementGetUsage(element), ((fEnabled) ? "on" : "off")));
-        }
+            Log2(("Led (%d) is turned %s\n", (int)IOHIDElementGetUsage(element), ((fEnabled) ? "on" : "off")));
 
         CFRelease(valueRef);
     }
@@ -1245,22 +1241,16 @@ static void darwinUpdateHostLedDeviceElements(IOHIDDeviceRef hidDevice, CFDictio
             switch (usage)
             {
                 case kHIDUsage_LED_NumLock:
-                {
                     darwinLedElementSetValue(hidDevice, element, fNumLockOn);
                     break;
-                }
 
                 case kHIDUsage_LED_CapsLock:
-                {
                     darwinLedElementSetValue(hidDevice, element, fCapsLockOn);
                     break;
-                }
 
                 case kHIDUsage_LED_ScrollLock:
-                {
                     darwinLedElementSetValue(hidDevice, element, fScrollLockOn);
                     break;
-                }
             }
 
         }
@@ -1329,9 +1319,7 @@ void DarwinUpdateHostLedDevices(bool fNumLockOn, bool fCapsLockOn, bool fScrollL
 
                 rc = IOHIDManagerClose(hidManagerRef, 0);
                 if (rc != kIOReturnSuccess)
-                {
                     LogRelFlow(("Warning! Something went wrong in attempt to close HID device manager!\n"));
-                }
             }
 
             CFRelease(deviceMatchingDictRef);
