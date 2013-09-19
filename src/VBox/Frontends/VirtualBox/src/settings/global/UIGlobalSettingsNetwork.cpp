0567922feb42a24b2adc1e875d95c14503ec86cc
Sergey Dubov <sergey.dubov@oracle.com>
2013-09-19 16:46:04 +0000

    FE/Qt: Global properties / Network page / NAT network tab: IPv6 port forwarding UI: Some additional parsing protection.
    svn:sync-xref-src-repo-rev: r89101

--- src/VBox/Frontends/VirtualBox/src/settings/global/UIGlobalSettingsNetwork.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/global/UIGlobalSettingsNetwork.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGlobalSettingsNetwork.cpp 48549 2013-09-19 16:33:44Z sergey.dubov@oracle.com $ */
+/* $Id: UIGlobalSettingsNetwork.cpp 48551 2013-09-19 16:46:04Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -948,7 +948,7 @@ UIDataNetworkNAT UIGlobalSettingsNetwork::generateDataNetworkNAT(const CNATNetwo
         /* Replace all ':' with ',' first: */
         strIPv6Rule.replace(':', ',');
         /* But replace ',' back with ':' for addresses: */
-        QRegExp re("\\[[^:]+\\]");
+        QRegExp re("\\[[^:\\[\\]]+\\]");
         re.setMinimal(true);
         while (re.indexIn(strIPv6Rule) != -1)
         {
