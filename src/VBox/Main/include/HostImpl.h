80802c488ab0a512804ff39fe7ee624390407bda
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2013-09-20 15:47:37 +0000

    VBoxManage,Main: VideoCaptureDevice -> VideoInputDevice
    svn:sync-xref-src-repo-rev: r89180

--- src/VBox/Main/include/HostImpl.h
+++ src/VBox/Main/include/HostImpl.h
@@ -1,4 +1,4 @@
-/* $Id: HostImpl.h 48578 2013-09-20 10:35:43Z vitali.pelenjow@oracle.com $ */
+/* $Id: HostImpl.h 48607 2013-09-20 15:47:37Z vitali.pelenjow@oracle.com $ */
 /** @file
  * Implementation of IHost.
  */
@@ -77,7 +77,7 @@ public:
     STDMETHOD(COMGETTER(OSVersion))(BSTR *version);
     STDMETHOD(COMGETTER(UTCTime))(LONG64 *aUTCTime);
     STDMETHOD(COMGETTER(Acceleration3DAvailable))(BOOL *aSupported);
-    STDMETHOD(COMGETTER(VideoCaptureDevices))(ComSafeArrayOut(IHostVideoCaptureDevice*, aVideoCaptureDevices));
+    STDMETHOD(COMGETTER(VideoInputDevices))(ComSafeArrayOut(IHostVideoInputDevice*, aVideoInputDevices));
 
     // IHost methods
     STDMETHOD(CreateHostOnlyNetworkInterface)(IHostNetworkInterface **aHostNetworkInterface,
