02d40b16f6abbad4d864618dd81f25e9d1d5f678
Frank Mehnert <noreply@oracle.com>
2013-09-23 10:39:35 +0000

    VMM: fixed help message to point the user where to enable PAE
    svn:sync-xref-src-repo-rev: r89213

--- src/VBox/VMM/VMMR3/PGM.cpp
+++ src/VBox/VMM/VMMR3/PGM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGM.cpp 47786 2013-08-16 08:59:32Z knut.osmundsen@oracle.com $ */
+/* $Id: PGM.cpp 48629 2013-09-23 10:39:35Z noreply@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor. (Mixing stuff here, not good?)
  */
@@ -3579,7 +3579,7 @@ VMMR3DECL(int) PGMR3ChangeMode(PVM pVM, PVMCPU pVCpu, PGMMODE enmGuestMode)
             CPUMGetGuestCpuId(pVCpu, 1, &u32Dummy, &u32Dummy, &u32Dummy, &u32Features);
             if (!(u32Features & X86_CPUID_FEATURE_EDX_PAE))
                 return VMSetRuntimeError(pVM, VMSETRTERR_FLAGS_FATAL, "PAEmode",
-                                         N_("The guest is trying to switch to the PAE mode which is currently disabled by default in VirtualBox. PAE support can be enabled using the VM settings (General/Advanced)"));
+                                         N_("The guest is trying to switch to the PAE mode which is currently disabled by default in VirtualBox. PAE support can be enabled using the VM settings (System/Processor)"));
 
             GCPhysCR3 = CPUMGetGuestCR3(pVCpu) & X86_CR3_PAE_PAGE_MASK;
             rc = PGM_GST_NAME_PAE(Enter)(pVCpu, GCPhysCR3);
