0c492d3e41b67c896238f30ccf11139699c490e9
Vasily Levchenko <noreply@oracle.com>
2013-09-26 14:11:23 +0000

    SessionMachine::init: lockWrite(LOCKVAL_SRC_POS_DECL).
    svn:sync-xref-src-repo-rev: r89342

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 48712 2013-09-26 14:03:38Z noreply@oracle.com $ */
+/* $Id: MachineImpl.cpp 48713 2013-09-26 14:11:23Z noreply@oracle.com $ */
 /** @file
  * Implementation of IMachine in VBoxSVC.
  */
@@ -12782,7 +12782,7 @@ HRESULT SessionMachine::init(Machine *aMachine)
             {
                 aMachine->lockHandle()->unlockWrite();
                 mParent->natNetworkRefInc(name.raw());
-                aMachine->lockHandle()->lockWrite();
+                aMachine->lockHandle()->lockWrite(LOCKVAL_SRC_POS_DECL);
             }
         }
     }
