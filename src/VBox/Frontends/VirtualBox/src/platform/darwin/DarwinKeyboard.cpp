bab265b698ae6dfe90846be726d5dcc19e7186df
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2013-10-04 13:30:32 +0000

    OS X host: leds sync: debugging: enabled leds broadcast only.
    svn:sync-xref-src-repo-rev: r89561

--- src/VBox/Frontends/VirtualBox/src/platform/darwin/DarwinKeyboard.cpp
+++ src/VBox/Frontends/VirtualBox/src/platform/darwin/DarwinKeyboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: DarwinKeyboard.cpp 48825 2013-10-02 16:55:30Z vadim.galitsyn@oracle.com $ */
+/* $Id: DarwinKeyboard.cpp 48887 2013-10-04 13:30:32Z vadim.galitsyn@oracle.com $ */
 /** @file
  * Common GUI Library - Darwin Keyboard routines.
  *
@@ -21,7 +21,7 @@
 *   Header Files                                                               *
 *******************************************************************************/
 #define LOG_GROUP LOG_GROUP_GUI
-//#define VBOX_WITH_KBD_LEDS_SYNC
+#define VBOX_WITH_KBD_LEDS_SYNC
 
 #include "DarwinKeyboard.h"
 #include <iprt/assert.h>
@@ -1514,6 +1514,8 @@ static bool darwinHidDeviceSupported(IOHIDDeviceRef pHidDeviceRef)
 void * DarwinHidDevicesKeepLedsState(void)
 {
 #ifdef VBOX_WITH_KBD_LEDS_SYNC
+    return NULL;
+
     IOReturn         rc;
     VBoxHidsState_t *hidsState;
 
@@ -1625,6 +1627,8 @@ void * DarwinHidDevicesKeepLedsState(void)
 int DarwinHidDevicesApplyAndReleaseLedsState(void *pState)
 {
 #ifdef VBOX_WITH_KBD_LEDS_SYNC
+    return 0;
+
     VBoxHidsState_t *hidsState = (VBoxHidsState_t *)pState;
 
     CFIndex     i;
