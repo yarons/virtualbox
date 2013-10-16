904c195a8b8b014ef0c1b5bbd193eadf8b0bd257
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-10-16 14:34:08 +0000

    Debugger/VBoxDbgConsole: Customized context-menu to toggle colors/fonts.
    svn:sync-xref-src-repo-rev: r89996

--- src/VBox/Debugger/VBoxDbgConsole.h
+++ src/VBox/Debugger/VBoxDbgConsole.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxDbgConsole.h 48946 2013-10-07 21:34:16Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxDbgConsole.h 49142 2013-10-16 14:34:08Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBox Debugger GUI - Console.
  */
@@ -61,12 +61,31 @@ public:
     virtual void appendText(const QString &rStr, bool fClearSelection);
 
 protected:
+    /**
+     * Context menu event.
+     * This adds custom menu items for the output view.
+     *
+     * @param   pEvent      Pointer to the event.
+     */
+    virtual void contextMenuEvent(QContextMenuEvent *pEvent);
+
+
     /** The current line (paragraph) number. */
     unsigned m_uCurLine;
     /** The position in the current line. */
     unsigned m_uCurPos;
     /** The handle to the GUI thread. */
     RTNATIVETHREAD m_hGUIThread;
+    /** Whether the current theme is 'green-on-black'. */
+    bool           m_fGreenOnBlack;
+
+private slots:
+    /**
+     * The "Toggle Theme" context-menu item was triggered.
+     *
+     * Will toggle font and colors of VBoxDbgConsoleOutput.
+     */
+    void         toggleTheme();
 };
 
 
