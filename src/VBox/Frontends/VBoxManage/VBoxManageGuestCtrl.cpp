b3d05f99b3a8b46c1a83e0470610a73ceb221158
Andreas Loeffler <andreas.loeffler@oracle.com>
2013-11-15 13:13:05 +0000

    FE/VBoxManage: GuestCtrl: Removed debug output.
    svn:sync-xref-src-repo-rev: r90662

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageGuestCtrl.cpp 49502 2013-11-15 13:11:13Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxManageGuestCtrl.cpp 49503 2013-11-15 13:13:05Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxManage - Implementation of guestcontrol command.
  */
@@ -720,11 +720,7 @@ static void ctrlUninitVM(PGCTLCMDCTX pCtx, uint32_t uFlags)
             else if (   (pCtx->uFlags & CTLCMDCTX_FLAGS_SESSION_DETACH)
                      && pCtx->fVerbose)
                 RTPrintf("Guest session detached\n");
-#ifdef DEBUG
-            ULONG cRefs = pCtx->pGuestSession->AddRef();
-            RTPrintf("cRefs=%RU32\n", cRefs - 1);
-            pCtx->pGuestSession->Release();
-#endif
+
             pCtx->pGuestSession.setNull();
         }
 
