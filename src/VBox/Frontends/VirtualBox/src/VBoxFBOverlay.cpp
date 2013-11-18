3ce2149d07966f213f1ae59d72186314bd2f5254
Mikhail Sennikovsky <noreply@oracle.com>
2013-11-18 15:41:20 +0000

    vhwa: cleanup fixes
    svn:sync-xref-src-repo-rev: r90713

--- src/VBox/Frontends/VirtualBox/src/VBoxFBOverlay.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxFBOverlay.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxFBOverlay.cpp 49485 2013-11-14 16:18:00Z noreply@oracle.com $ */
+/* $Id: VBoxFBOverlay.cpp 49535 2013-11-18 15:41:20Z noreply@oracle.com $ */
 /** @file
  * VBoxFBOverlay implementation
  */
@@ -2594,6 +2594,7 @@ int VBoxVHWAImage::vhwaSurfaceDestroy(struct VBOXVHWACMD_SURF_DESTROY *pCmd)
     }
     else
     {
+        Assert(pList);
         Assert(pList->size() >= 1);
         if(pList->size() > 1)
         {
