3b689487098a4bdbd69fbbb0474069912929457e
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2013-11-18 10:59:01 +0000

    VMM: Retire VBOX_WITH_AUTO_MSR_LOAD_RESTORE define.
    svn:sync-xref-src-repo-rev: r90697

--- src/VBox/VMM/VMMR3/HM.cpp
+++ src/VBox/VMM/VMMR3/HM.cpp
@@ -1,4 +1,4 @@
-/* $Id: HM.cpp 49520 2013-11-18 09:30:29Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HM.cpp 49523 2013-11-18 10:59:01Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM - Intel/AMD VM Hardware Support Manager.
  */
@@ -2923,11 +2923,9 @@ VMMR3_INT_DECL(void) HMR3CheckError(PVM pVM, int iStatusCode)
                     LogRel(("HM: CPU[%u] EntryCtls        %#RX32\n", i, pVCpu->hm.s.vmx.u32EntryCtls));
                     LogRel(("HM: CPU[%u] ExitCtls         %#RX32\n", i, pVCpu->hm.s.vmx.u32ExitCtls));
                     LogRel(("HM: CPU[%u] HCPhysMsrBitmap  %#RHp\n",  i, pVCpu->hm.s.vmx.HCPhysMsrBitmap));
-#ifdef VBOX_WITH_AUTO_MSR_LOAD_RESTORE
                     LogRel(("HM: CPU[%u] HCPhysGuestMsr   %#RHp\n",  i, pVCpu->hm.s.vmx.HCPhysGuestMsr));
                     LogRel(("HM: CPU[%u] HCPhysHostMsr    %#RHp\n",  i, pVCpu->hm.s.vmx.HCPhysHostMsr));
                     LogRel(("HM: CPU[%u] cMsrs            %u\n",     i, pVCpu->hm.s.vmx.cMsrs));
-#endif
                 }
                 /** @todo Log VM-entry event injection control fields
                  *        VMX_VMCS_CTRL_ENTRY_IRQ_INFO, VMX_VMCS_CTRL_ENTRY_EXCEPTION_ERRCODE
