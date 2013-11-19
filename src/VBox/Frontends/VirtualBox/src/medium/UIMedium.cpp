a31534cdaac43c78808e4f22d95917375d2b17e7
Sergey Dubov <sergey.dubov@oracle.com>
2013-11-19 17:41:16 +0000

    FE/Qt: Medium enumeration mechanism: Support to async CMedium close/delete logic.
    svn:sync-xref-src-repo-rev: r90750

--- src/VBox/Frontends/VirtualBox/src/medium/UIMedium.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/UIMedium.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMedium.cpp 48608 2013-09-20 15:49:23Z noreply@oracle.com $ */
+/* $Id: UIMedium.cpp 49557 2013-11-19 17:41:16Z sergey.dubov@oracle.com $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -79,6 +79,7 @@ UIMedium& UIMedium::operator=(const UIMedium &other)
     m_strLastAccessError = other.lastAccessError();
     m_result = other.result();
 
+    m_strKey = other.key();
     m_strId = other.id();
     m_strName = other.name();
     m_strLocation = other.location();
@@ -167,6 +168,9 @@ void UIMedium::refresh()
     /* Detect basic parameters... */
 
     m_strId = m_medium.isNull() ? nullID() : m_medium.GetId();
+
+    if (m_strKey.isNull() && !m_strId.isNull())
+        m_strKey = m_strId;
 
     m_fHostDrive = m_medium.isNull() ? false : m_medium.GetHostDrive();
 
