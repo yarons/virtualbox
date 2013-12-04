ea94abad8e03a46a996f789c3c82d7f3a71dfdd5
Mikhail Sennikovsky <noreply@oracle.com>
2013-12-04 13:25:00 +0000

    crOpenGL/infrastructure: headers
    svn:sync-xref-src-repo-rev: r91086

--- src/VBox/GuestHost/OpenGL/util/compositor.cpp
+++ src/VBox/GuestHost/OpenGL/util/compositor.cpp
@@ -1,3 +1,20 @@
+/* $Id: compositor.cpp 49773 2013-12-04 13:25:00Z noreply@oracle.com $ */
+
+/** @file
+ * Compositor impl
+ */
+
+/*
+ * Copyright (C) 2013 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
 #include <cr_compositor.h>
 
 #define VBOXVR_SCR_COMPOSITOR_RECTS_UNDEFINED UINT32_MAX
