5c8804756f6fa3d96242c4d1b382b4c322481f9b
Andreas Loeffler <andreas.loeffler@oracle.com>
2014-01-23 11:51:09 +0000

    DnD/VBoxTray: Update.
    svn:sync-xref-src-repo-rev: r91741

--- src/VBox/Additions/WINNT/VBoxTray/VBoxDnDDropSource.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxDnDDropSource.cpp
@@ -1,10 +1,10 @@
-/* $Id: VBoxDnDDropSource.cpp 49947 2013-12-17 08:40:37Z noreply@oracle.com $ */
+/* $Id: VBoxDnDDropSource.cpp 50177 2014-01-23 11:51:09Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxDnDSource.cpp - IDropSource implementation.
  */
 
 /*
- * Copyright (C) 2013 Oracle Corporation
+ * Copyright (C) 2013-2014 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -42,26 +42,6 @@ VBoxDnDDropSource::~VBoxDnDDropSource(void)
     int rc = VbglR3DnDDisconnect(mClientID);
 
     LogFlowFunc(("rc=%Rrc, mRefCount=%RI32\n", rc, mRefCount));
-}
-
-/* static */
-int VBoxDnDDropSource::CreateDropSource(VBoxDnDWnd *pParent, IDropSource **ppDropSource)
-{
-    AssertPtrReturn(pParent, VERR_INVALID_POINTER);
-    AssertPtrReturn(ppDropSource, VERR_INVALID_POINTER);
-
-    int rc;
-    try
-    {
-        *ppDropSource = new VBoxDnDDropSource(pParent);
-        rc = VINF_SUCCESS;
-    }
-    catch(std::bad_alloc &)
-    {
-        rc = VERR_NO_MEMORY;
-    }
-
-    return rc;
 }
 
 /*
