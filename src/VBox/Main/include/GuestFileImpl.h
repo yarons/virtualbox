3bcf360103b3dbd29be62315ebd3600b01f32599
Andrew Hamilton <noreply@oracle.com>
2014-02-19 18:57:56 +0000

    6813 - GuestFsObjInfoImpl.cpp 2nd attempt
    svn:sync-xref-src-repo-rev: r92358

--- src/VBox/Main/include/GuestFileImpl.h
+++ src/VBox/Main/include/GuestFileImpl.h
@@ -1,11 +1,11 @@
 
-/* $Id: GuestFileImpl.h 50502 2014-02-18 21:50:11Z noreply@oracle.com $ */
+/* $Id: GuestFileImpl.h 50515 2014-02-19 18:57:56Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Main - Guest file handling.
  */
 
 /*
- * Copyright (C) 2012-2013 Oracle Corporation
+ * Copyright (C) 2012-2014 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -22,7 +22,6 @@
 #include "VirtualBoxBase.h"
 #include "EventImpl.h"
 
-#include "GuestFsObjInfoImpl.h"
 #include "GuestCtrlImplPrivate.h"
 
 class Console;
