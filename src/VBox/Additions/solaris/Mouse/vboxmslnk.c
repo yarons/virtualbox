ae378ba6075321b6bba14a0a1843aeca35fd6fba
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2014-03-07 13:04:59 +0000

    vboxmslnk: typo.
    svn:sync-xref-src-repo-rev: r92698

--- src/VBox/Additions/solaris/Mouse/vboxmslnk.c
+++ src/VBox/Additions/solaris/Mouse/vboxmslnk.c
@@ -1,4 +1,4 @@
-/* $Id: vboxmslnk.c 50725 2014-03-07 13:03:58Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: vboxmslnk.c 50726 2014-03-07 13:04:59Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VirtualBox Guest Additions Mouse Driver for Solaris: user space loader tool.
  */
@@ -109,7 +109,7 @@ void handleArgs(int argc, char *argv[], int *pfNoLogo)
                "All rights reserved.\n\n");
     if (fShowUsage)
     {
-        printf("Usage:\n  -V|--version  print the tool vesion.\n"
+        printf("Usage:\n  -V|--version  print the tool version.\n"
                "  --nologo      do not display the logo text and only output the connection\n"
                "                ID number needed to disable pointer integration\n"
                "                again.\n"
