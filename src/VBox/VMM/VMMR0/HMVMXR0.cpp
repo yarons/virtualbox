cc7b327178bc3eb405d5f0d3a4129d057c0be4a9
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2014-03-12 15:00:05 +0000

    VMM/HMVMXR0: When only all-context VPID flush is supported by the CPU, avoid reflushing -all- VPIDs for every run after hitting VPID limit.
    It is sufficient to flush all VPIDs once per host CPU when the limit is reached.
    
    
    svn:sync-xref-src-repo-rev: r92759

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 50739 2014-03-11 12:23:28Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXR0.cpp 50749 2014-03-12 15:00:05Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -2039,7 +2039,20 @@ static void hmR0VmxFlushTaggedTlbVpid(PVM pVM, PVMCPU pVCpu, PHMGLOBALCPUINFO pC
         pVCpu->hm.s.cTlbFlushes    = pCpu->cTlbFlushes;
         pVCpu->hm.s.uCurrentAsid   = pCpu->uCurrentAsid;
         if (pCpu->fFlushAsidBeforeUse)
-            hmR0VmxFlushVpid(pVM, pVCpu, pVM->hm.s.vmx.enmFlushVpid, 0 /* GCPtr */);
+        {
+            if (pVM->hm.s.vmx.enmFlushVpid == VMX_FLUSH_VPID_SINGLE_CONTEXT)
+                hmR0VmxFlushVpid(pVM, pVCpu, VMX_FLUSH_VPID_SINGLE_CONTEXT, 0 /* GCPtr */);
+            else if (pVM->hm.s.vmx.enmFlushVpid == VMX_FLUSH_VPID_ALL_CONTEXTS)
+            {
+                hmR0VmxFlushVpid(pVM, pVCpu, VMX_FLUSH_VPID_ALL_CONTEXTS, 0 /* GCPtr */);
+                pCpu->fFlushAsidBeforeUse = false;
+            }
+            else
+            {
+                /* hmR0VmxSetupTaggedTlb() ensures we never get here. Paranoia. */
+                AssertMsgFailed(("Unsupported VPID-flush context type.\n"));
+            }
+        }
     }
     /** @todo We never set VMCPU_FF_TLB_SHOOTDOWN anywhere. See hmQueueInvlPage()
      *        where it is commented out. Support individual entry flushing
