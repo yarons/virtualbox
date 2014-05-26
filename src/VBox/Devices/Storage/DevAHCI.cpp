28ba3dd32bd34544cb2c9eb42186b7703147ddee
Alexander Eichner <alexander.eichner@oracle.com>
2014-05-26 20:29:00 +0000

    Storage/AHCI: Fix the SSM version check for the legacy port reset handling
    svn:sync-xref-src-repo-rev: r93944

--- src/VBox/Devices/Storage/DevAHCI.cpp
+++ src/VBox/Devices/Storage/DevAHCI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevAHCI.cpp 51383 2014-05-23 19:26:10Z alexander.eichner@oracle.com $ */
+/* $Id: DevAHCI.cpp 51409 2014-05-26 20:29:00Z alexander.eichner@oracle.com $ */
 /** @file
  * DevAHCI - AHCI controller device (disk and cdrom).
  *
@@ -82,7 +82,7 @@
 
 /** The current saved state version. */
 #define AHCI_SAVED_STATE_VERSION                        8
-/** The saved state version before changing the port reset logic in an incomptible way. */
+/** The saved state version before changing the port reset logic in an incompatible way. */
 #define AHCI_SAVED_STATE_VERSION_PRE_PORT_RESET_CHANGES 7
 /** Saved state version before the per port hotplug port was added. */
 #define AHCI_SAVED_STATE_VERSION_PRE_HOTPLUG_FLAG       6
@@ -7133,7 +7133,7 @@ static DECLCALLBACK(int) ahciR3LoadExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM, uin
      * Check whether we have to resort to the legacy port reset method to
      * prevent older BIOS versions from failing after a reset.
      */
-    if (uVersion < AHCI_SAVED_STATE_VERSION_PRE_PORT_RESET_CHANGES)
+    if (uVersion <= AHCI_SAVED_STATE_VERSION_PRE_PORT_RESET_CHANGES)
         pThis->fLegacyPortResetMethod = true;
 
     /* Verify config. */
