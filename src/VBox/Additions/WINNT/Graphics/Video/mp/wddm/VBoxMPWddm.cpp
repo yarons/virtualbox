405cc9fcd0a0b4092a68806bcd4d0ddc58f21346
Mikhail Sennikovsky <noreply@oracle.com>
2014-05-30 11:42:14 +0000

    wddm: report better status
    svn:sync-xref-src-repo-rev: r94053

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMPWddm.cpp 51466 2014-05-30 10:07:26Z noreply@oracle.com $ */
+/* $Id: VBoxMPWddm.cpp 51468 2014-05-30 11:42:14Z noreply@oracle.com $ */
 /** @file
  * VBox WDDM Miniport driver
  */
@@ -3629,7 +3629,7 @@ DxgkDdiBuildPagingBufferNew(
             {
                 WARN(("pBuildPagingBuffer->DmaSize(%d) < sizeof VBOXCMDVBVA_PAGING_TRANSFER (%d)", pBuildPagingBuffer->DmaSize , sizeof (VBOXCMDVBVA_PAGING_TRANSFER)));
                 /* @todo: can this actually happen? what status to return? */
-                return STATUS_INVALID_PARAMETER;
+                return STATUS_GRAPHICS_INSUFFICIENT_DMA_BUFFER;
             }
 
             VBOXCMDVBVA_PAGING_TRANSFER *pPaging = (VBOXCMDVBVA_PAGING_TRANSFER*)pBuildPagingBuffer->pDmaBuffer;
