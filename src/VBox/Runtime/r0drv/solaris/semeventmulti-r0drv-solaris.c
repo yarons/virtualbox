18f62e90345acd32285decbbc5de7df04c0bc591
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2014-06-25 04:59:28 +0000

    Runtime/r0drv/solaris: Doxygen bugref comment.
    svn:sync-xref-src-repo-rev: r94506

--- src/VBox/Runtime/r0drv/solaris/semeventmulti-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/semeventmulti-r0drv-solaris.c
@@ -1,10 +1,10 @@
-/* $Id: semeventmulti-r0drv-solaris.c 44528 2013-02-04 14:27:54Z noreply@oracle.com $ */
+/* $Id: semeventmulti-r0drv-solaris.c 51721 2014-06-25 04:59:28Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IPRT - Multiple Release Event Semaphores, Ring-0 Driver, Solaris.
  */
 
 /*
- * Copyright (C) 2006-2012 Oracle Corporation
+ * Copyright (C) 2006-2014 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -214,7 +214,7 @@ RTDECL(int) RTSemEventMultiSignal(RTSEMEVENTMULTI hEventMultiSem)
 
     rtR0SemEventMultiSolRelease(pThis);
 #ifdef DEBUG_ramshankar
-    /** See @bugref{6318} comment#11 */
+    /** See @bugref{6318} comment #11. */
     return VINF_SUCCESS;
 #endif
     RT_ASSERT_PREEMPT_CPUID();
@@ -246,7 +246,7 @@ RTDECL(int) RTSemEventMultiReset(RTSEMEVENTMULTI hEventMultiSem)
     rtR0SemEventMultiSolRelease(pThis);
 
 #ifdef DEBUG_ramshankar
-    /** See @bugref{6318} comment#11 */
+    /** See @bugref{6318} comment #11. */
     return VINF_SUCCESS;
 #endif
     RT_ASSERT_PREEMPT_CPUID();
