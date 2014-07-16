ca152a7100c5dd42a3ee8064dd8aaf3416ec8a8d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2014-07-16 20:19:00 +0000

    build fix
    svn:sync-xref-src-repo-rev: r95064

--- src/VBox/Devices/Storage/DrvVD.cpp
+++ src/VBox/Devices/Storage/DrvVD.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvVD.cpp 52062 2014-07-16 20:17:35Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvVD.cpp 52063 2014-07-16 20:19:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * DrvVD - Generic VBox disk media driver.
  */
@@ -1861,19 +1861,21 @@ static DECLCALLBACK(int) drvvdDiscard(PPDMIMEDIA pInterface, PCRTRANGE paRanges,
 static DECLCALLBACK(int) drvvdIoBufAlloc(PPDMIMEDIA pInterface, size_t cb, void **ppvNew)
 {
     LogFlowFunc(("\n"));
-    int rc = VINF_SUCCESS;
-    void *pvNew = NULL;
+    int rc;
     PVBOXDISK pThis = PDMIMEDIA_2_VBOXDISK(pInterface);
 
     /* Configured encryption requires locked down memory. */
     if (pThis->pCfgCrypto)
-        rc = RTMemSaferAllocZEx(&pvNew, cb, RTMEMSAFER_F_REQUIRE_NOT_PAGABLE);
+        rc = RTMemSaferAllocZEx(ppvNew, cb, RTMEMSAFER_F_REQUIRE_NOT_PAGABLE);
     else
     {
         cb = RT_ALIGN_Z(cb, _4K);
-        pvNew = RTMemPageAlloc(cb);
+        void *pvNew = RTMemPageAlloc(cb);
         if (RT_LIKELY(pvNew))
+        {
             *ppvNew = pvNew;
+            rc = VINF_SUCCESS;
+        }
         else
             rc = VERR_NO_MEMORY;
     }
