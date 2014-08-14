28f4f74f9a388586707c6f0ee1e9e512054c524e
Alexander Eichner <alexander.eichner@oracle.com>
2014-08-14 11:44:04 +0000

    Storage/tstVDIo: Don't run builtin tests if there is not enough free RAM available
    svn:sync-xref-src-repo-rev: r95519

--- src/VBox/Storage/testcase/tstVDIo.cpp
+++ src/VBox/Storage/testcase/tstVDIo.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstVDIo.cpp 52371 2014-08-13 19:00:27Z alexander.eichner@oracle.com $ */
+/* $Id: tstVDIo.cpp 52378 2014-08-14 11:44:04Z alexander.eichner@oracle.com $ */
 /** @file
  *
  * VBox HDD container test utility - I/O replay.
@@ -33,6 +33,7 @@
 #include <iprt/rand.h>
 #include <iprt/critsect.h>
 #include <iprt/test.h>
+#include <iprt/system.h>
 
 #include "VDMemDisk.h"
 #include "VDIoBackend.h"
@@ -2797,6 +2798,20 @@ static void tstVDIoScriptRun(const char *pcszFilename)
  */
 static void tstVDIoRunBuiltinTests(void)
 {
+    /*
+     * We need quite a bit of RAM for the builtin tests. Skip it if there
+     * is not enough free RAM available.
+     */
+    uint64_t cbFree = 0;
+    int rc = RTSystemQueryAvailableRam(&cbFree);
+    if (   RT_FAILURE(rc)
+        || cbFree < (UINT64_C(6) * _1G))
+    {
+        RTStrmPrintf(g_pStdErr, "tstVDIo: fatal error: Failed to query available RAM or not enough available, skipping (rc=%Rrc cbFree=%llu)\n",
+                     rc, cbFree);
+        return;
+    }
+
     for (unsigned i = 0; i < g_cVDIoTests; i++)
     {
         char *pszScript = RTStrDupN((const char *)g_aVDIoTests[i].pch, g_aVDIoTests[i].cb);
