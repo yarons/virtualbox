83969d3e9da4d6ba5a8ede90264238e3ef7918bf
Frank Mehnert <noreply@oracle.com>
2014-08-20 16:34:15 +0000

    Main/EbmlWriter: fixed version string
    svn:sync-xref-src-repo-rev: r95626

--- src/VBox/Main/src-client/EbmlWriter.cpp
+++ src/VBox/Main/src-client/EbmlWriter.cpp
@@ -1,4 +1,4 @@
-/* $Id: EbmlWriter.cpp 52427 2014-08-20 08:59:33Z noreply@oracle.com $ */
+/* $Id: EbmlWriter.cpp 52432 2014-08-20 16:34:15Z noreply@oracle.com $ */
 /** @file
  * EbmlWriter.cpp - EBML writer + WebM container
  */
@@ -103,8 +103,8 @@ void WebMWriter::writeSeekInfo()
     m_uSegmentInfoPos = RTFileTell(m_File);
 
     char szVersion[64];
-    RTStrPrintf(szVersion, sizeof(szVersion), "vpxenc%",
-                m_bDebug ? vpx_codec_version_str() : "");
+    RTStrPrintf(szVersion, sizeof(szVersion), "vpxenc%s",
+                m_bDebug ? "" : vpx_codec_version_str());
 
     m_Ebml.subStart(Info)
           .serializeUnsignedInteger(TimecodeScale, 1000000)
