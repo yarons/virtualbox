4dc1534ad7f64be6796131628a5170b5a53aca94
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2014-10-02 13:44:24 +0000

    MachineImpl: return code mismatch
    svn:sync-xref-src-repo-rev: r96371

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 52861 2014-09-25 12:21:22Z noreply@oracle.com $ */
+/* $Id: MachineImpl.cpp 52933 2014-10-02 13:44:24Z vitali.pelenjow@oracle.com $ */
 /** @file
  * Implementation of IMachine in VBoxSVC.
  */
@@ -8424,18 +8424,18 @@ HRESULT Machine::i_loadMachineDataFromSettings(const settings::MachineConfigFile
                         cbOut,
                         DECODE_STR_MAX);
     com::SafeArray<BYTE> iconByte(cbOut);
-    HRESULT rc = RTBase64Decode(pszStr, iconByte.raw(), cbOut, NULL, NULL);
-    if (FAILED(rc))
+    int vrc = RTBase64Decode(pszStr, iconByte.raw(), cbOut, NULL, NULL);
+    if (RT_FAILURE(vrc))
         return setError(E_FAIL,
-                        tr("Failure to Decode Icon Data. '%s' (%d)"),
+                        tr("Failure to Decode Icon Data. '%s' (%Rrc)"),
                         pszStr,
-                        rc);
+                        vrc);
     mUserData->mIcon.resize(iconByte.size());
     memcpy(&mUserData->mIcon[0], iconByte.raw(), mUserData->mIcon.size());
 
     // look up the object by Id to check it is valid
     ComPtr<IGuestOSType> guestOSType;
-    rc = mParent->GetGuestOSType(Bstr(mUserData->s.strOsType).raw(),
+    HRESULT rc = mParent->GetGuestOSType(Bstr(mUserData->s.strOsType).raw(),
                                          guestOSType.asOutParam());
     if (FAILED(rc)) return rc;
 
@@ -8445,7 +8445,7 @@ HRESULT Machine::i_loadMachineDataFromSettings(const settings::MachineConfigFile
     else
     {
         Utf8Str stateFilePathFull(config.strStateFile);
-        int vrc = i_calculateFullPath(stateFilePathFull, stateFilePathFull);
+        vrc = i_calculateFullPath(stateFilePathFull, stateFilePathFull);
         if (RT_FAILURE(vrc))
             return setError(E_FAIL,
                             tr("Invalid saved state file path '%s' (%Rrc)"),
