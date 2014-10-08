b5647d65905bf3f6e930e9943346003f129b4f17
Sergey Dubov <sergey.dubov@oracle.com>
2014-10-08 16:17:50 +0000

    FE/Qt: Runtime UI rework/cleanup for 7115 (part #7): Small UIConsoleEventHandler rework/cleanup.
    svn:sync-xref-src-repo-rev: r96458

--- src/VBox/Frontends/VirtualBox/src/runtime/UIConsoleEventHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIConsoleEventHandler.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIConsoleEventHandler.cpp 52898 2014-09-30 14:53:05Z sergey.dubov@oracle.com $ */
+/* $Id: UIConsoleEventHandler.cpp 52998 2014-10-08 16:17:50Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIConsoleEventHandler class implementation.
  */
@@ -36,27 +36,26 @@
 
 
 /* static */
-UIConsoleEventHandler *UIConsoleEventHandler::m_pInstance = 0;
+UIConsoleEventHandler *UIConsoleEventHandler::m_spInstance = 0;
 
 /* static */
-UIConsoleEventHandler* UIConsoleEventHandler::instance(UISession *pSession /* = 0 */)
+void UIConsoleEventHandler::create(UISession *pSession)
 {
-    if (!m_pInstance)
+    if (!m_spInstance)
     {
-        m_pInstance = new UIConsoleEventHandler(pSession);
-        m_pInstance->prepare();
+        m_spInstance = new UIConsoleEventHandler(pSession);
+        m_spInstance->prepare();
     }
-    return m_pInstance;
 }
 
 /* static */
 void UIConsoleEventHandler::destroy()
 {
-    if (m_pInstance)
+    if (m_spInstance)
     {
-        m_pInstance->cleanup();
-        delete m_pInstance;
-        m_pInstance = 0;
+        m_spInstance->cleanup();
+        delete m_spInstance;
+        m_spInstance = 0;
     }
 }
 
