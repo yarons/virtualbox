1c2ef7ff40b8d9480a8cf5108a3a40fe193ab33b
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2014-10-16 12:43:22 +0000

    DevVGA: reverted r96537 (wrong solution)
    svn:sync-xref-src-repo-rev: r96588

--- src/VBox/Devices/Graphics/DevVGA_VBVA.cpp
+++ src/VBox/Devices/Graphics/DevVGA_VBVA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA_VBVA.cpp 53044 2014-10-13 14:30:17Z vitali.pelenjow@oracle.com $ */
+/* $Id: DevVGA_VBVA.cpp 53078 2014-10-16 12:43:22Z vitali.pelenjow@oracle.com $ */
 /** @file
  * VirtualBox Video Acceleration (VBVA).
  */
@@ -536,16 +536,6 @@ static int vbvaDisable (unsigned uScreenId, PVGASTATE pVGAState, VBVACONTEXT *pC
     vbvaFlush (pVGAState, pCtx);
 
     VBVAVIEW *pView = &pCtx->aViews[uScreenId];
-
-    if (uScreenId != 0)
-    {
-        /* Disable secondary screens. They only work in VBVA mode. */
-        VBVAINFOSCREEN screen;
-        RT_ZERO(screen);
-        screen.u16Flags = VBVA_SCREEN_F_DISABLED;
-        screen.u32ViewIndex = uScreenId;
-        vbvaResize(pVGAState, pView, &screen);
-    }
 
     if (pView->pVBVA)
     {
