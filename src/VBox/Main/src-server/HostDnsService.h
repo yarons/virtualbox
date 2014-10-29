35a2a0a3b3ae05a12c336bc55c57ca813410a48a
Valery Ushakov <noreply@oracle.com>
2014-10-29 16:26:20 +0000

    Main/HostDnsService: brush up old Windows code.  In general no functional change is intended, but make search list handling follow Windows behavior.
    svn:sync-xref-src-repo-rev: r96703

--- src/VBox/Main/src-server/HostDnsService.h
+++ src/VBox/Main/src-server/HostDnsService.h
@@ -1,4 +1,4 @@
-/* $Id: HostDnsService.h 53159 2014-10-28 14:51:59Z noreply@oracle.com $ */
+/* $Id: HostDnsService.h 53165 2014-10-29 16:26:20Z noreply@oracle.com $ */
 /** @file
  * Host DNS listener.
  */
@@ -157,7 +157,6 @@ class HostDnsServiceWin : public HostDnsMonitor
     virtual int monitorWorker();
 
     private:
-    void strList2List(std::vector<std::string>& lst, char *strLst);
     HRESULT updateInfo();
 
     private:
