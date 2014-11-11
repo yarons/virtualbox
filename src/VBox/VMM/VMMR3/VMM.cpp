3ec173c9f73d52cfb79161daf18e9bf35a7cae42
Frank Mehnert <noreply@oracle.com>
2014-11-11 08:47:46 +0000

    VMM: logging fix
    svn:sync-xref-src-repo-rev: r96879

--- src/VBox/VMM/VMMR3/VMM.cpp
+++ src/VBox/VMM/VMMR3/VMM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMM.cpp 52764 2014-09-16 15:57:03Z knut.osmundsen@oracle.com $ */
+/* $Id: VMM.cpp 53300 2014-11-11 08:47:46Z noreply@oracle.com $ */
 /** @file
  * VMM - The Virtual Machine Monitor Core.
  */
@@ -1340,7 +1340,7 @@ VMMR3_INT_DECL(int) VMMR3RawRunGC(PVM pVM, PVMCPU pVCpu)
  */
 VMMR3_INT_DECL(int) VMMR3HmRunGC(PVM pVM, PVMCPU pVCpu)
 {
-    Log2(("VMMR3HmRunGC: (cs:eip=%04x:%08x)\n", CPUMGetGuestCS(pVCpu), CPUMGetGuestEIP(pVCpu)));
+    Log2(("VMMR3HmRunGC: (cs:rip=%04x:%RX64)\n", CPUMGetGuestCS(pVCpu), CPUMGetGuestRIP(pVCpu)));
 
     for (;;)
     {
@@ -1371,7 +1371,7 @@ VMMR3_INT_DECL(int) VMMR3HmRunGC(PVM pVM, PVMCPU pVCpu)
 #endif /* !LOG_ENABLED */
         if (rc != VINF_VMM_CALL_HOST)
         {
-            Log2(("VMMR3HmRunGC: returns %Rrc (cs:eip=%04x:%08x)\n", rc, CPUMGetGuestCS(pVCpu), CPUMGetGuestEIP(pVCpu)));
+            Log2(("VMMR3HmRunGC: returns %Rrc (cs:rip=%04x:%RX64)\n", rc, CPUMGetGuestCS(pVCpu), CPUMGetGuestRIP(pVCpu)));
             return rc;
         }
         rc = vmmR3ServiceCallRing3Request(pVM, pVCpu);
