a7e1e1079b8d17ec5e16d7e571c8c35fa8a91e82
Michael Thayer <noreply@oracle.com>
2014-11-18 10:41:32 +0000

    Additions/x11/vboxvideo: do not disable the screen on DPMS low power and suppress unneeded events.
    svn:sync-xref-src-repo-rev: r96948

--- src/VBox/Additions/x11/vboxvideo/vboxvideo.c
+++ src/VBox/Additions/x11/vboxvideo/vboxvideo.c
@@ -1,4 +1,4 @@
-/* $Id: vboxvideo.c 53345 2014-11-18 09:55:52Z noreply@oracle.com $ */
+/* $Id: vboxvideo.c 53346 2014-11-18 10:41:32Z noreply@oracle.com $ */
 /** @file
  *
  * Linux Additions X11 graphics driver
@@ -287,8 +287,12 @@ vbox_crtc_dpms(xf86CrtcPtr crtc, int mode)
 {
     VBOXPtr pVBox = VBOXGetRec(crtc->scrn);
     unsigned cDisplay = (uintptr_t)crtc->driver_private;
+    bool fDisabled = (mode == DPMSModeOff);
+
     TRACE_LOG("cDisplay=%u, mode=%i\n", cDisplay, mode);
-    pVBox->pScreens[cDisplay].afDisabled = (mode == DPMSModeOff);
+    if (pVBox->pScreens[cDisplay].afDisabled == fDisabled)
+        return;
+    pVBox->pScreens[cDisplay].afDisabled = fDisabled;
     /* Don't fiddle with the hardware if we are switched
      * to a virtual terminal. */
     if (!crtc->scrn->vtSema) {
