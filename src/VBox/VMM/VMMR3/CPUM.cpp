279e3b29bf2838a4d512070b9eab646d61aad142
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2014-11-24 18:09:36 +0000

    VMM/CPUM: Fix crash while dumping hypervisor leafs
    svn:sync-xref-src-repo-rev: r97029

--- src/VBox/VMM/VMMR3/CPUM.cpp
+++ src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 53202 2014-11-04 15:41:05Z knut.osmundsen@oracle.com $ */
+/* $Id: CPUM.cpp 53391 2014-11-24 18:09:36Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -4288,7 +4288,7 @@ static DECLCALLBACK(void) cpumR3CpuIdInfo(PVM pVM, PCDBGFINFOHLP pHlp, const cha
         if (fGuestHvp)
         {
             pHyperLeafGst = cpumR3CpuIdGetLeaf(pVM->cpum.s.GuestInfo.paCpuIdLeavesR3, pVM->cpum.s.GuestInfo.cCpuIdLeaves,
-                                                       uHyperLeaf, 0 /* uSubLeaf */);
+                                               uHyperLeaf, 0 /* uSubLeaf */);
         }
 
         RT_ZERO(Host);
@@ -4296,7 +4296,7 @@ static DECLCALLBACK(void) cpumR3CpuIdInfo(PVM pVM, PCDBGFINFOHLP pHlp, const cha
             ASMCpuIdExSlow(uHyperLeaf, 0, 0, 0, &Host.eax, &Host.ebx, &Host.ecx, &Host.edx);
 
         CPUMCPUIDLEAF  GuestLeaf;
-        uint32_t const cHyperGstMax = pHyperLeafGst->uEax;
+        uint32_t const cHyperGstMax = pHyperLeafGst ? pHyperLeafGst->uEax : 0;
         uint32_t const cHyperHstMax = Host.eax;
         uint32_t const cHyperMax    = RT_MAX(cHyperHstMax, cHyperGstMax);
         for (uint32_t i = uHyperLeaf; i <= cHyperMax; i++)
