92348a80fe5894754d7fc9f70cfb20e5f7c65742
Frank Mehnert <noreply@oracle.com>
2014-12-10 09:19:40 +0000

    Additions/x11/testcase: warning
    svn:sync-xref-src-repo-rev: r97231

--- src/VBox/Additions/x11/vboxvideo/testcase/tstSetModeXOrg.c
+++ src/VBox/Additions/x11/vboxvideo/testcase/tstSetModeXOrg.c
@@ -1,4 +1,4 @@
-/* $Id: tstSetModeXOrg.c 37423 2011-06-12 18:37:56Z knut.osmundsen@oracle.com $ */
+/* $Id: tstSetModeXOrg.c 53494 2014-12-10 09:19:40Z noreply@oracle.com $ */
 /** @file
  * vboxvideo unit test - modesetting.
  */
@@ -37,11 +37,6 @@ static ScrnInfoPtr pScrns[1] = { &scrnInfo[0] };
 ScrnInfoPtr *xf86Screens = pScrns;
 
 Bool vbox_device_available(VBOXPtr pVBox)
-{
-    return TRUE;
-}
-
-Bool vboxEnableGraphicsCap(VBOXPtr pVBox)
 {
     return TRUE;
 }
