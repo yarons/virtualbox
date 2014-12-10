0583234f3fc0eff88abd6f037bb31cbc3b2bb9cf
Frank Mehnert <noreply@oracle.com>
2014-12-10 09:16:41 +0000

    FE/VBoxManage: std::string::find returns npos not -1 if nothing matches
    svn:sync-xref-src-repo-rev: r97230

--- src/VBox/Frontends/VBoxManage/VBoxManageNATNetwork.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageNATNetwork.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageNATNetwork.cpp 53312 2014-11-12 11:14:34Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxManageNATNetwork.cpp 53493 2014-12-10 09:16:41Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - Implementation of NAT Network command command.
  */
@@ -400,7 +400,7 @@ static int handleOp(HandlerArg *a, OPCODE enmCode, int iStart, int *pcProcessed)
                     LONG lOffset = 0;
                     Bstr bstrAddress;
 
-                    AssertReturn(pos != -1, errorArgument("invalid loopback string"));
+                    AssertReturn(pos != std::string::npos, errorArgument("invalid loopback string"));
 
                     address = it->substr(0, pos);
                     strOffset = it->substr(pos + 1);
