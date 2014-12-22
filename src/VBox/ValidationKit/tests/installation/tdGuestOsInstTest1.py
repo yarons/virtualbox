40602c0ee6519a7daae022261080b428da33ceb4
Frank Mehnert <noreply@oracle.com>
2014-12-22 09:05:31 +0000

    fixed reverted condition
    svn:sync-xref-src-repo-rev: r97361

--- src/VBox/ValidationKit/tests/installation/tdGuestOsInstTest1.py
+++ src/VBox/ValidationKit/tests/installation/tdGuestOsInstTest1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdGuestOsInstTest1.py 53580 2014-12-19 20:57:54Z knut.osmundsen@oracle.com $
+# $Id: tdGuestOsInstTest1.py 53598 2014-12-22 09:05:31Z noreply@oracle.com $
 
 """
 VirtualBox Validation Kit - Guest OS installation tests.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 53580 $"
+__version__ = "$Revision: 53598 $"
 
 
 # Standard Python imports.
@@ -76,7 +76,7 @@ class InstallTestVm(vboxtestvms.TestVm):
 
     def __init__(self, oSet, sVmName, sKind, sInstallIso, sHdCtrlNm, cGbHdd, fFlags):
         vboxtestvms.TestVm.__init__(self, oSet, sVmName, sKind = sKind, sHddControllerType = sHdCtrlNm,
-                                    fRandomPvPMode = (fFlags & self.kfNoWin81Paravirt) != 0);
+                                    fRandomPvPMode = (fFlags & self.kfNoWin81Paravirt) == 0);
         self.sDvdImage    = os.path.join(self.ksIsoPathBase, sInstallIso);
         self.cGbHdd       = cGbHdd;
         self.fInstVmFlags = fFlags;
