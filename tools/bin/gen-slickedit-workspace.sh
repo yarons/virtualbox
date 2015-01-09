1c241647cbfa68bff0cc1f12d71b893a2f739dc6
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-01-09 21:30:48 +0000

    new openssl version.
    svn:sync-xref-src-repo-rev: r97572

--- tools/bin/gen-slickedit-workspace.sh
+++ tools/bin/gen-slickedit-workspace.sh
@@ -1,5 +1,5 @@
 # !kmk_ash
-# $Id: gen-slickedit-workspace.sh 53584 2014-12-20 20:17:01Z knut.osmundsen@oracle.com $
+# $Id: gen-slickedit-workspace.sh 53766 2015-01-09 21:30:48Z knut.osmundsen@oracle.com $
 ## @file
 # Script for generating a SlickEdit workspace.
 #
@@ -963,7 +963,7 @@ my_generate_project "bldprogs"      "src/bldprogs"                          --be
 my_generate_project "zlib"          "src/libs/zlib-1.2.6"                   --begin-incs "include"                                          --end-includes "src/libs/zlib-1.2.6/*.c" "src/libs/zlib-1.2.6/*.h"
 my_generate_project "liblzf"        "src/libs/liblzf-3.4"                   --begin-incs "include"                                          --end-includes "src/libs/liblzf-3.4"
 my_generate_project "libpng"        "src/libs/libpng-1.2.8"                 --begin-incs "include"                                          --end-includes "src/libs/libpng-1.2.8/*.c" "src/libs/libpng-1.2.8/*.h"
-my_generate_project "openssl"       "src/libs/openssl-1.0.1j"               --begin-incs "include" "src/libs/openssl-1.0.1j/crypto"         --end-includes "src/libs/openssl-1.0.1j"
+my_generate_project "openssl"       "src/libs/openssl-1.0.1k"               --begin-incs "include" "src/libs/openssl-1.0.1k/crypto"         --end-includes "src/libs/openssl-1.0.1k"
 my_generate_project "kStuff"        "src/libs/kStuff"                       --begin-incs "include" "src/libs/kStuff/kStuff/include"         --end-includes "src/libs/kStuff"
 
 
