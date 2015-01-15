d68ea075f6b0b3e8ed3a1579a75304fb61ef2b4c
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2015-01-15 12:01:56 +0000

    3D: scalling factor: separate scaling factors along X and Y axes; move out from atomic variables to read-write semaphores.
    svn:sync-xref-src-repo-rev: r97624

--- src/VBox/HostServices/SharedOpenGL/crserverlib/presenter/server_presenter.h
+++ src/VBox/HostServices/SharedOpenGL/crserverlib/presenter/server_presenter.h
@@ -1,4 +1,4 @@
-/* $Id: server_presenter.h 53814 2015-01-15 11:00:51Z vadim.galitsyn@oracle.com $ */
+/* $Id: server_presenter.h 53816 2015-01-15 12:01:56Z vadim.galitsyn@oracle.com $ */
 
 /** @file
  * Presenter API definitions.
@@ -214,8 +214,8 @@ class CrFbWindow
         int SetPosition(int32_t x, int32_t y);
         int SetVisibleRegionsChanged();
         int SetCompositor(const struct VBOXVR_SCR_COMPOSITOR * pCompositor);
-        void SetScaleFactor(GLdouble scaleFactor);
-        GLdouble GetScaleFactor();
+        bool SetScaleFactor(GLdouble scaleFactorW, GLdouble scaleFactorH);
+        bool GetScaleFactor(GLdouble *scaleFactorW, GLdouble *scaleFactorH);
         int UpdateBegin();
         void UpdateEnd();
         uint64_t GetParentId();
@@ -251,7 +251,10 @@ class CrFbWindow
         uint32_t mHeight;
         CR_FBWIN_FLAGS mFlags;
         uint64_t mParentId;
-        volatile uint32_t mScaleFactorStorage;
+
+        RTSEMRW scaleFactorLock;
+        GLdouble mScaleFactorWStorage;
+        GLdouble mScaleFactorHStorage;
 };
 
 
