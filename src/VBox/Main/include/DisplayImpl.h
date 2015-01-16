e7fcc64e1edcde30d05602a0b6917ebdcd66159b
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2015-01-16 10:03:25 +0000

    Host 3D & Main: coding style update.
    svn:sync-xref-src-repo-rev: r97669

--- src/VBox/Main/include/DisplayImpl.h
+++ src/VBox/Main/include/DisplayImpl.h
@@ -1,4 +1,4 @@
-/* $Id: DisplayImpl.h 53850 2015-01-16 09:37:05Z noreply@oracle.com $ */
+/* $Id: DisplayImpl.h 53851 2015-01-16 10:03:25Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -169,7 +169,6 @@ public:
     bool i_handleCrVRecScreenshotBegin(uint32_t uScreen, uint64_t u64TimeStamp);
     void i_handleCrVRecScreenshotEnd(uint32_t uScreen, uint64_t u64TimeStamp);
     void i_handleVRecCompletion();
-    virtual HRESULT notifyScaleFactorChange(ULONG uScreen, ULONG u32ScaleFactorWMultiplied, ULONG u32ScaleFactorHMultiplied);
 #endif
 
     int i_notifyCroglResize(const PVBVAINFOVIEW pView, const PVBVAINFOSCREEN pScreen, void *pvVRAM);
@@ -254,6 +253,9 @@ private:
                                     ULONG aHeight);
     virtual HRESULT querySourceBitmap(ULONG aScreenId,
                                       ComPtr<IDisplaySourceBitmap> &aDisplaySourceBitmap);
+    virtual HRESULT notifyScaleFactorChange(ULONG aScreenId,
+                                            ULONG aScaleFactorWMultiplied,
+                                            ULONG aScaleFactorHMultiplied);
 
     // Wrapped IEventListener properties
 
