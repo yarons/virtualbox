9a40a90a57fa7932fd1e8be42fe2bd4016ca2b1d
Sergey Dubov <sergey.dubov@oracle.com>
2015-02-05 13:23:04 +0000

    FE/Qt: Allow to request normal (windowed) mode through the GUI console argument.
    svn:sync-xref-src-repo-rev: r98035

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGlobal.cpp 53815 2015-01-15 11:15:07Z vadim.galitsyn@oracle.com $ */
+/* $Id: VBoxGlobal.cpp 54081 2015-02-05 13:23:04Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - VBoxGlobal class implementation.
  */
@@ -3980,6 +3980,10 @@ void VBoxGlobal::prepare()
                 m_strPidfile = QString(qApp->argv()[i]);
         }
 #endif /* VBOX_GUI_WITH_PIDFILE */
+        else if (!::strcmp(arg, "-normal") || !::strcmp(arg, "--normal"))
+        {
+            visualStateType = UIVisualStateType_Normal;
+        }
         else if (!::strcmp(arg, "-fullscreen") || !::strcmp(arg, "--fullscreen"))
         {
             visualStateType = UIVisualStateType_Fullscreen;
