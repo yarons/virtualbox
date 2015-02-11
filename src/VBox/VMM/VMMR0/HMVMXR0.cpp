0f7499cf598eb0c1665ad73ca352466685d3fef6
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2015-02-11 17:18:16 +0000

    VMM/HMVMXR0: Fix host hang if VMX-preemption timer is disabled.
    svn:sync-xref-src-repo-rev: r98185

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 54130 2015-02-10 17:01:42Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXR0.cpp 54152 2015-02-11 17:18:16Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -3405,7 +3405,8 @@ DECLINLINE(int) hmR0VmxLoadGuestExitCtls(PVMCPU pVCpu, PCPUMCTX pMixedCtx)
          *        VMX_VMCS_CTRL_EXIT_SAVE_GUEST_PAT_MSR,
          *        VMX_VMCS_CTRL_EXIT_LOAD_HOST_PAT_MSR. */
 
-        if (pVM->hm.s.vmx.Msrs.VmxExit.n.allowed1 & VMX_VMCS_CTRL_EXIT_SAVE_VMX_PREEMPT_TIMER)
+        if (    pVM->hm.s.vmx.fUsePreemptTimer
+            && (pVM->hm.s.vmx.Msrs.VmxExit.n.allowed1 & VMX_VMCS_CTRL_EXIT_SAVE_VMX_PREEMPT_TIMER))
             val |= VMX_VMCS_CTRL_EXIT_SAVE_VMX_PREEMPT_TIMER;
 
         if ((val & zap) != val)
