cbb1aabd2f7de243b7cab614e85d6700f0f5644e
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2015-02-19 16:44:43 +0000

    SupDrv: disable TSC-delta thread, still some issues need sorting.
    svn:sync-xref-src-repo-rev: r98375

--- src/VBox/HostDrivers/Support/SUPDrvInternal.h
+++ src/VBox/HostDrivers/Support/SUPDrvInternal.h
@@ -1,4 +1,4 @@
-/* $Id: SUPDrvInternal.h 54290 2015-02-19 11:16:49Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: SUPDrvInternal.h 54301 2015-02-19 16:44:43Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VirtualBox Support Driver - Internal header.
  */
@@ -230,7 +230,7 @@
 # undef SUPDRV_WITH_MSR_PROBER
 #endif
 
-#if 1
+#if 0
 /**  Use a dedicated kernel thread to service TSC-delta measurement requests.
  *   @todo Test on servers with many CPUs and sockets. */
 #define SUPDRV_USE_TSC_DELTA_THREAD
