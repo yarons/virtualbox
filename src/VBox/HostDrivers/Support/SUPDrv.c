8858d755dd1e76addf674aa9f5dbb328936c5b52
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2015-02-19 20:55:41 +0000

    HostDrivers/Support: should be bool.
    svn:sync-xref-src-repo-rev: r98388

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv.c 54312 2015-02-19 20:41:49Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: SUPDrv.c 54313 2015-02-19 20:55:41Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Common code.
  */
@@ -6002,7 +6002,7 @@ static int supdrvTscDeltaThreadButchered(PSUPDRVDEVEXT pDevExt, bool fSpinlockHe
 static DECLCALLBACK(int) supdrvTscDeltaThread(RTTHREAD hThread, void *pvUser)
 {
     PSUPDRVDEVEXT     pDevExt = (PSUPDRVDEVEXT)pvUser;
-    uint32_t          cTimesMeasured = 0;
+    bool              fInitialMeasurement = true;
     uint32_t          cConsecutiveTimeouts = 0;
     int               rc = VERR_INTERNAL_ERROR_2;
     for (;;)
@@ -6062,10 +6062,11 @@ static DECLCALLBACK(int) supdrvTscDeltaThread(RTTHREAD hThread, void *pvUser)
             case kTscDeltaThreadState_Measuring:
             {
                 cConsecutiveTimeouts = 0;
-                if (!cTimesMeasured++)
+                if (fInitialMeasurement)
                 {
                     int cTries = 8;
                     int cMsWaitPerTry = 10;
+                    fInitialMeasurement = false;
                     do
                     {
                         rc = supdrvMeasureTscDeltas(pDevExt, NULL /* pidxMaster */);
@@ -6083,9 +6084,6 @@ static DECLCALLBACK(int) supdrvTscDeltaThread(RTTHREAD hThread, void *pvUser)
                 {
                     PSUPGLOBALINFOPAGE pGip = pDevExt->pGip;
                     unsigned iCpu;
-
-                    if (cTimesMeasured == UINT32_MAX)
-                        cTimesMeasured = 1;
 
                     /* Measure TSC-deltas only for the CPUs that are in the set. */
                     rc = VINF_SUCCESS;
