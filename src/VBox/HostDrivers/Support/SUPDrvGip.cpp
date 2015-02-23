81f7c32dc310886596b946eb3534ae197f99bd50
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-02-23 10:44:03 +0000

    done.
    svn:sync-xref-src-repo-rev: r98470

--- src/VBox/HostDrivers/Support/SUPDrvGip.cpp
+++ src/VBox/HostDrivers/Support/SUPDrvGip.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPDrvGip.cpp 54375 2015-02-23 10:41:10Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPDrvGip.cpp 54376 2015-02-23 10:44:03Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Common code for GIP.
  */
@@ -3579,12 +3579,9 @@ static int supdrvMeasureTscDeltaOne(PSUPDRVDEVEXT pDevExt, uint32_t idxWorker)
 
     if (RTCpuSetIsMemberByIndex(&pGip->OnlineCpuSet, pGipCpuWorker->iCpuSet))
     {
-        /** @todo we need to check that the master is online...
-         * The old supdrvMeasureTscDeltaCallback code would spin forever. */
         /*
          * Initialize data package for the RTMpOnAll callback.
          */
-        /** @todo this must be allocated, not residing on the stack.   */
         PSUPDRVGIPTSCDELTARGS pArgs = (PSUPDRVGIPTSCDELTARGS)RTMemAllocZ(sizeof(*pArgs));
         if (pArgs)
         {
