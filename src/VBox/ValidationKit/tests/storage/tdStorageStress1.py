cc21c84bf596aa1fdd821ae076ea06c5374db226
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2015-02-24 11:08:06 +0000

    typo
    svn:sync-xref-src-repo-rev: r98540

--- src/VBox/ValidationKit/tests/storage/tdStorageStress1.py
+++ src/VBox/ValidationKit/tests/storage/tdStorageStress1.py
@@ -26,7 +26,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Id: tdStorageStress1.py 54436 2015-02-24 10:58:24Z klaus.espenlaub@oracle.com $"
+__version__ = "$Id: tdStorageStress1.py 54437 2015-02-24 11:08:06Z klaus.espenlaub@oracle.com $"
 
 
 # Standard Python imports.
@@ -354,7 +354,7 @@ class tdStorageStress(vbox.TestDriver):                                      # p
                 listNames.append('LUN');
 
                 if self.fpApiVer >= 4.4:
-                    oHd = oSession.oVBox.createMedium(sDiskFormat, sDiskPath1, vboxcon.AccessMode_ReadWrite, }
+                    oHd = oSession.oVBox.createMedium(sDiskFormat, sDiskPath1, vboxcon.AccessMode_ReadWrite, \
                                                       vboxcon.DeviceType_HardDisk);
                 else:
                     oHd = oSession.oVBox.createHardDisk(sDiskFormat, sDiskPath1);
