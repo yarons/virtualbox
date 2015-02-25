6c60a2c0987d81c8a65163823a0c8dee3dca16c3
Frank Mehnert <noreply@oracle.com>
2015-02-25 09:23:39 +0000

    CPUMR0: fHasThermal was never used
    svn:sync-xref-src-repo-rev: r98581

--- src/VBox/VMM/VMMR0/CPUMR0.cpp
+++ src/VBox/VMM/VMMR0/CPUMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUMR0.cpp 54339 2015-02-20 18:10:12Z knut.osmundsen@oracle.com $ */
+/* $Id: CPUMR0.cpp 54475 2015-02-25 09:23:39Z noreply@oracle.com $ */
 /** @file
  * CPUM - Host Context Ring 0.
  */
@@ -51,8 +51,6 @@ typedef struct CPUMHOSTLAPIC
     bool        fX2Apic;
     /** The APIC version number. */
     uint32_t    uVersion;
-    /** Has APIC_REG_LVT_THMR. Not used. */
-    uint32_t    fHasThermal;
     /** The physical address of the APIC registers. */
     RTHCPHYS    PhysBase;
     /** The memory object entering the physical address. */
@@ -864,7 +862,6 @@ static DECLCALLBACK(void) cpumR0MapLocalApicCpuChecker(RTCPUID idCpu, void *pvUs
     if ((APIC_REG_VERSION_GET_VER(uApicVersion) & 0xF0) == 0x10)
     {
         g_aLApics[iCpu].uVersion    = uApicVersion;
-        g_aLApics[iCpu].fHasThermal = APIC_REG_VERSION_GET_MAX_LVT(uApicVersion) >= 5;
 
 #if 0 /* enable if you need it. */
         if (g_aLApics[iCpu].fX2Apic)
