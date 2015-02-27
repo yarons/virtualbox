fd5a9367676450d923aab83f4f9c70fbf09fdb81
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2015-02-27 12:14:50 +0000

    ValidationKit: excluded wei01-b6ka-3 from network cleanup script
    svn:sync-xref-src-repo-rev: r98681

--- src/VBox/ValidationKit/testdriver/vboxinstaller.py
+++ src/VBox/ValidationKit/testdriver/vboxinstaller.py
@@ -30,7 +30,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 53772 $"
+__version__ = "$Revision: 54548 $"
 
 
 # Standard Python imports.
@@ -716,6 +716,7 @@ class VBoxInstallerTestDriver(TestDriverBase):
         sHostName = socket.getfqdn();
         if    not sHostName.startswith('testboxwin3') \
           and not sHostName.startswith('testboxharp2') \
+          and not sHostName.startswith('wei01-b6ka-3') \
           and utils.getHostOsVersion() in ['8', '8.1', '9', '2008Server', '2008ServerR2', '2012Server']:
             reporter.log('Peforming extra NDIS cleanup...');
             sMagicScript = os.path.abspath(os.path.join(g_ksValidationKitDir, 'testdriver', 'win-vbox-net-uninstall.ps1'));
