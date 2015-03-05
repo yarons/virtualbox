ef2a8dda348b89d60d90fbec45fc96ccee56f0b8
Sergey Dubov <sergey.dubov@oracle.com>
2015-03-05 11:21:53 +0000

    FE/Qt: Re-applying stuff reverted in r98784.
    svn:sync-xref-src-repo-rev: r98791

--- src/VBox/Frontends/VirtualBox/src/runtime/UIFrameBuffer.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIFrameBuffer.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIFrameBuffer.cpp 54637 2015-03-04 22:17:48Z sergey.dubov@oracle.com $ */
+/* $Id: UIFrameBuffer.cpp 54644 2015-03-05 11:21:53Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIFrameBuffer class implementation.
  */
@@ -44,6 +44,13 @@
 # include "CFramebuffer.h"
 # include "CDisplaySourceBitmap.h"
 #endif /* !VBOX_WITH_PRECOMPILED_HEADERS */
+
+/* VirtualBox interface declarations: */
+#ifndef VBOX_WITH_XPCOM
+# include "VirtualBox.h"
+#else /* !VBOX_WITH_XPCOM */
+# include "VirtualBox_XPCOM.h"
+#endif /* VBOX_WITH_XPCOM */
 
 /* Other VBox includes: */
 #include <iprt/critsect.h>
