1dba0d83d99adeabe12d8e7f195b8d63c73d122b
Sergey Dubov <sergey.dubov@oracle.com>
2015-03-13 18:01:57 +0000

    FE/Qt: 7676: Runtime UI: Disk Encryption (DE) support: NLS fix for r98932 and r98933.
    svn:sync-xref-src-repo-rev: r98935

--- src/VBox/Frontends/VirtualBox/src/runtime/UIAddDiskEncryptionPasswordDialog.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIAddDiskEncryptionPasswordDialog.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIAddDiskEncryptionPasswordDialog.cpp 54756 2015-03-13 17:46:26Z sergey.dubov@oracle.com $ */
+/* $Id: UIAddDiskEncryptionPasswordDialog.cpp 54759 2015-03-13 18:01:57Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIAddDiskEncryptionPasswordDialog class implementation.
  */
@@ -273,7 +273,7 @@ QVariant UIEncryptionDataModel::data(const QModelIndex &index, int iRole /* = Qt
             /* We are generating tool-tip here and not in retranslateUi() because of the tricky plural form handling,
              * but be quiet, it's safe enough because the tool-tip being re-acquired every time on mouse-hovering. */
             const QStringList encryptedMediums = m_encryptedMediums.values(m_encryptionPasswords.keys().at(index.row()));
-            return tr("Used by the following %n hard-drive(s):<br>%1",
+            return tr("Used by the following %n hard drive(s):<br>%1",
                       "This text is never used with n == 0. "
                       "Feel free to drop the %n where possible, "
                       "we only included it because of problems with Qt Linguist "
