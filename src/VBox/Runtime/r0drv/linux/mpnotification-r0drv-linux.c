0e6eff070f9b74b9b728c472ce44153b8440c721
Frank Mehnert <noreply@oracle.com>
2015-03-17 11:15:47 +0000

    backed out r98998 and r99000 -- shouldn't be necessary here
    svn:sync-xref-src-repo-rev: r99004

--- src/VBox/Runtime/r0drv/linux/mpnotification-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/mpnotification-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: mpnotification-r0drv-linux.c 54804 2015-03-17 10:46:28Z noreply@oracle.com $ */
+/* $Id: mpnotification-r0drv-linux.c 54808 2015-03-17 11:15:47Z noreply@oracle.com $ */
 /** @file
  * IPRT - Multiprocessor Event Notifications, Ring-0 Driver, Linux.
  */
@@ -153,9 +153,7 @@ static int rtMpNotificationLinuxCallback(struct notifier_block *pNotifierBlock, 
 # if defined(CPU_TASKS_FROZEN) && defined(CPU_ONLINE_FROZEN)
         case CPU_ONLINE_FROZEN:
 # endif
-            stac();
             rtMpNotificationDoCallbacks(RTMPEVENT_ONLINE, idCpu);
-            clac();
             break;
 
 # ifdef CPU_DOWN_PREPARE
@@ -163,9 +161,7 @@ static int rtMpNotificationLinuxCallback(struct notifier_block *pNotifierBlock, 
 #  if defined(CPU_TASKS_FROZEN) && defined(CPU_DOWN_PREPARE_FROZEN)
         case CPU_DOWN_PREPARE_FROZEN:
 #  endif
-            stac();
             rtMpNotificationDoCallbacks(RTMPEVENT_OFFLINE, idCpu);
-            clac();
             break;
 # endif
     }
