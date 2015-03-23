74a838a8b11bf740a18551c1229f615963765d0a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-03-23 00:21:30 +0000

    CPUMGCResumeGuest and CPUMGCResumeGuestV86 has been fed CPUMCPU pointers for a while now? Guess we never noticed because there was usually a zero dword at the start of CPUMCPU until I moved the Hyper (and the host) states to the end. Hyper never used the fpu part of its CPUMCTX structure.
    svn:sync-xref-src-repo-rev: r99114

--- src/VBox/VMM/VMMRC/CPUMRCA.asm
+++ src/VBox/VMM/VMMRC/CPUMRCA.asm
@@ -1,4 +1,4 @@
-; $Id: CPUMRCA.asm 54898 2015-03-22 23:47:07Z knut.osmundsen@oracle.com $
+; $Id: CPUMRCA.asm 54899 2015-03-23 00:21:30Z knut.osmundsen@oracle.com $
 ;; @file
 ; CPUM - Raw-mode Context Assembly Routines.
 ;
@@ -357,7 +357,7 @@ ENDPROC CPUMGCCallV86Code
 ;
 ; This call never returns!
 ;
-; @param    edx     Pointer to CPUM structure.
+; @param    edx     Pointer to CPUMCPU structure.
 ;
 align 16
 BEGINPROC_EXPORTED CPUMGCResumeGuest
@@ -371,8 +371,6 @@ BEGINPROC_EXPORTED CPUMGCResumeGuest
     pop     edx
 %endif
 
-    ; Convert to CPUMCPU pointer
-    add     edx, [edx + CPUM.offCPUMCPU0]
     ;
     ; Setup iretd
     ;
@@ -445,7 +443,7 @@ ENDPROC     CPUMGCResumeGuest
 ;
 ; This call never returns!
 ;
-; @param    edx     Pointer to CPUM structure.
+; @param    edx     Pointer to CPUMCPU structure.
 ;
 align 16
 BEGINPROC_EXPORTED CPUMGCResumeGuestV86
@@ -459,8 +457,6 @@ BEGINPROC_EXPORTED CPUMGCResumeGuestV86
     pop     edx
 %endif
 
-    ; Convert to CPUMCPU pointer
-    add     edx, [edx + CPUM.offCPUMCPU0]
     ;
     ; Setup iretd
     ;
