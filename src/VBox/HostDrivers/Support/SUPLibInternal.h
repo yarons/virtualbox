60c6cdec57e2e58aa0cc834e0e0352c016692ff6
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-03-28 19:57:12 +0000

    supHardNt: Some more memory replacment hacking - take evasive action on failure; flush log file and if possible log volume as we process.
    svn:sync-xref-src-repo-rev: r99266

--- src/VBox/HostDrivers/Support/SUPLibInternal.h
+++ src/VBox/HostDrivers/Support/SUPLibInternal.h
@@ -1,4 +1,4 @@
-/* $Id: SUPLibInternal.h 53002 2014-10-08 23:46:15Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPLibInternal.h 54998 2015-03-28 19:57:12Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Internal header.
  */
@@ -91,6 +91,7 @@
 # define supR3HardenedOpenLog              supR3HardenedStaticOpenLog
 # define supR3HardenedLogV                 supR3HardenedStaticLogV
 # define supR3HardenedLog                  supR3HardenedStaticLog
+# define supR3HardenedLogFlush             supR3HardenedStaticLogFlush
 # define supR3HardenedVerifyAll            supR3HardenedStaticVerifyAll
 # define supR3HardenedVerifyFixedDir       supR3HardenedStaticVerifyFixedDir
 # define supR3HardenedVerifyFixedFile      supR3HardenedStaticVerifyFixedFile
@@ -432,6 +433,11 @@ DECLHIDDEN(void)    supR3HardenedLogV(const char *pszFormat, va_list va);
  * Write to the startup log file.
  */
 DECLHIDDEN(void)    supR3HardenedLog(const char *pszFormat, ...);
+
+/**
+ * Flushes the log file.
+ */
+DECLHIDDEN(void)    supR3HardenedLogFlush(void);
 
 
 DECLHIDDEN(int)     supR3HardenedVerifyAll(bool fFatal, const char *pszProgName);
