6ef9c6cc40ca318192b85f4e6a12c76f88806095
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-03-31 19:34:09 +0000

    oops
    svn:sync-xref-src-repo-rev: r99345

--- src/VBox/VMM/testcase/tstHelp.h
+++ src/VBox/VMM/testcase/tstHelp.h
@@ -1,4 +1,4 @@
-/* $Id: tstHelp.h 55052 2015-03-31 19:09:50Z knut.osmundsen@oracle.com $ */
+/* $Id: tstHelp.h 55056 2015-03-31 19:34:09Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMM testcase - Helper stuff.
  */
@@ -54,7 +54,7 @@ RT_C_DECLS_END
         if (size != sizeof(type)) \
         { \
             printf("error! sizeof(%s): %#x (%d)  Off by %d!! (expected %#x)\n", \
-                   #type, (int)sizeof(type), (int)sizeof(type), (int)(sizeof(type) - size, (int)size)); \
+                   #type, (int)sizeof(type), (int)sizeof(type), (int)(sizeof(type) - size), (int)size); \
             rc++; \
         } \
         else \
