06169ce2995355977a020dff9bd2623222122cbc
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-04-16 15:22:17 +0000

    Need XSave/XRstor in RC too.
    svn:sync-xref-src-repo-rev: r99631

--- src/VBox/Runtime/common/asm/ASMXRstor.asm
+++ src/VBox/Runtime/common/asm/ASMXRstor.asm
@@ -1,6 +1,6 @@
-; $Id: ASMXRstor.asm 55308 2015-04-16 15:01:35Z knut.osmundsen@oracle.com $
+; $Id: ASMXRstor.asm 55311 2015-04-16 15:22:17Z knut.osmundsen@oracle.com $
 ;; @file
-; IPRT - ASMRstor().
+; IPRT - ASMXRstor().
 ;
 
 ;
@@ -39,7 +39,7 @@ BEGINCODE
 ; @param    fMask       The 64-bit state component mask.
 ;                       msc=rdx, gcc=rsi, x86=[esp+8]
 ;
-BEGINPROC_EXPORTED ASMRstor
+BEGINPROC_EXPORTED ASMXRstor
 SEH64_END_PROLOGUE
 %ifdef ASM_CALL64_MSC
         mov     rdx, rdx
@@ -60,5 +60,5 @@ SEH64_END_PROLOGUE
  %error "Undefined arch?"
 %endif
         ret
-ENDPROC ASMRstor
+ENDPROC ASMXRstor
 
