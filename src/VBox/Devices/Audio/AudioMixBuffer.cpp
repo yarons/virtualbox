21e13452b1363e11d8184889d20e4a47354857e8
Michal Necasek <michal.necasek@oracle.com>
2015-04-21 11:34:13 +0000

    Audio: Set nominal volume correctly.
    svn:sync-xref-src-repo-rev: r99684

--- src/VBox/Devices/Audio/AudioMixBuffer.cpp
+++ src/VBox/Devices/Audio/AudioMixBuffer.cpp
@@ -1,4 +1,4 @@
-/* $Id: AudioMixBuffer.cpp 55345 2015-04-20 17:14:00Z michal.necasek@oracle.com $ */
+/* $Id: AudioMixBuffer.cpp 55351 2015-04-21 11:34:13Z michal.necasek@oracle.com $ */
 /** @file
  * VBox audio: Audio mixing buffer for converting reading/writing audio
  *             samples.
@@ -639,8 +639,8 @@ int audioMixBufInit(PPDMAUDIOMIXBUF pMixBuf, const char *pszName, PPDMPCMPROPS p
 
     /* Set initial volume to max. */
     pMixBuf->Volume.fMuted = false;
-    pMixBuf->Volume.uLeft  = 0x7F;
-    pMixBuf->Volume.uRight = 0x7F;
+    pMixBuf->Volume.uLeft  = 0x7FFFFFFF;
+    pMixBuf->Volume.uRight = 0x7FFFFFFF;
 
     /* Prevent division by zero.
      * Do a 1:1 conversion according to AUDIOMIXBUF_S2B_RATIO. */
