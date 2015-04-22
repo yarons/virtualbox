fca4145631cffac18128c31b7683a70827ee6cad
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2015-04-22 15:13:34 +0000

    Log nit.
    svn:sync-xref-src-repo-rev: r99722

--- src/VBox/VMM/VMMR3/GIM.cpp
+++ src/VBox/VMM/VMMR3/GIM.cpp
@@ -1,4 +1,4 @@
-/* $Id: GIM.cpp 55173 2015-04-10 09:22:57Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: GIM.cpp 55383 2015-04-22 15:13:34Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * GIM - Guest Interface Manager.
  */
@@ -117,7 +117,7 @@ VMMR3_INT_DECL(int) GIMR3Init(PVM pVM)
     /*
      * Setup the GIM provider for this VM.
      */
-    LogRel(("GIM: Using provider \"%s\" (Implementation version: %u)\n", szProvider, uVersion));
+    LogRel(("GIM: Using provider '%s' (Implementation version: %u)\n", szProvider, uVersion));
     if (!RTStrCmp(szProvider, "None"))
         pVM->gim.s.enmProviderId = GIMPROVIDERID_NONE;
     else
@@ -142,7 +142,7 @@ VMMR3_INT_DECL(int) GIMR3Init(PVM pVM)
             rc = gimR3KvmInit(pVM);
         }
         else
-            rc = VMR3SetError(pVM->pUVM, VERR_GIM_INVALID_PROVIDER, RT_SRC_POS, "Provider \"%s\" unknown.", szProvider);
+            rc = VMR3SetError(pVM->pUVM, VERR_GIM_INVALID_PROVIDER, RT_SRC_POS, "Provider '%s' unknown.", szProvider);
     }
     return rc;
 }
