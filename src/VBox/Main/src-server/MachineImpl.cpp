501976880cf39cea3a4d579006e0120fdc2177e3
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2015-04-29 10:16:01 +0000

    Main/Snapshot: fix handling of saved state with offline snapshots (looked at wrong machine state variable, regression in previous big code structure change), streamline the code path and fix a crash for early failures to take a snapshot. Eliminate a local variable which should always have the same value as the same flag in the task struct, avoiding inconsistent behavior. Main/Machine: fix sanity check regarding saved state presence when saving the config (resulted in loss of saved state)
    svn:sync-xref-src-repo-rev: r99867

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 55437 2015-04-27 09:35:19Z klaus.espenlaub@oracle.com $ */
+/* $Id: MachineImpl.cpp 55511 2015-04-29 10:16:01Z klaus.espenlaub@oracle.com $ */
 /** @file
  * Implementation of IMachine in VBoxSVC.
  */
@@ -9994,11 +9994,10 @@ void Machine::i_copyMachineDataToSettings(settings::MachineConfigFile &config)
 
     if (    mData->mMachineState == MachineState_Saved
          || mData->mMachineState == MachineState_Restoring
-            // when deleting a snapshot we may or may not have a saved state in the current state,
-            // so let's not assert here please
-         || (    (   mData->mMachineState == MachineState_DeletingSnapshot
-                  || mData->mMachineState == MachineState_DeletingSnapshotOnline
-                  || mData->mMachineState == MachineState_DeletingSnapshotPaused)
+            // when doing certain snapshot operations we may or may not have
+            // a saved state in the current state, so keep everything as is
+         || (    (   mData->mMachineState == MachineState_Snapshotting
+                  || mData->mMachineState == MachineState_DeletingSnapshot)
               && (!mSSData->strStateFilePath.isEmpty())
             )
         )
