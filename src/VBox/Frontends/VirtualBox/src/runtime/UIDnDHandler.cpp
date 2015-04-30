f3e27317b2a01b60633275eb70f46009f99d8763
Sergey Dubov <sergey.dubov@oracle.com>
2015-04-30 11:37:48 +0000

    FE/Qt: DnD: Build-warning fix for r94064.
    svn:sync-xref-src-repo-rev: r99912

--- src/VBox/Frontends/VirtualBox/src/runtime/UIDnDHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIDnDHandler.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIDnDHandler.cpp 55539 2015-04-30 09:48:34Z andreas.loeffler@oracle.com $ */
+/* $Id: UIDnDHandler.cpp 55546 2015-04-30 11:37:48Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIDnDHandler class implementation.
  */
@@ -256,6 +256,7 @@ int UIDnDHandler::dragIsPending(CSession &session, CDnDSource &dndSource,
     else /* No format data from the guest arrived yet. */
         rc = VERR_NO_DATA;
 #else /* !VBOX_WITH_DRAG_AND_DROP_GH */
+    NOREF(dndSource);
     NOREF(session);
     NOREF(screenId);
     NOREF(pParent);
