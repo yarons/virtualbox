f91658b3b7c06b073ee3e6cc66ea5339e35c6d55
Andreas Loeffler <andreas.loeffler@oracle.com>
2015-04-30 10:15:41 +0000

    Tab.
    svn:sync-xref-src-repo-rev: r99906

--- src/VBox/Main/src-client/GuestProcessImpl.cpp
+++ src/VBox/Main/src-client/GuestProcessImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestProcessImpl.cpp 55535 2015-04-30 02:13:56Z knut.osmundsen@oracle.com $ */
+/* $Id: GuestProcessImpl.cpp 55540 2015-04-30 10:15:41Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox Main - Guest process handling.
  */
@@ -1270,10 +1270,10 @@ ProcessWaitResult_T GuestProcess::i_waitFlagsToResultEx(uint32_t fWaitFlags,
                     else
                     {
                         /*
-                             * If ProcessCreateFlag_WaitForProcessStartOnly was specified on process creation the
-                             * caller is not interested in getting further process statuses -- so just don't notify
-                             * anything here anymore and return.
-                             */
+                         * If ProcessCreateFlag_WaitForProcessStartOnly was specified on process creation the
+                         * caller is not interested in getting further process statuses -- so just don't notify
+                         * anything here anymore and return.
+                         */
                         if (uProcFlags & ProcessCreateFlag_WaitForProcessStartOnly)
                             waitResult = ProcessWaitResult_Start;
                     }
@@ -1307,7 +1307,8 @@ ProcessWaitResult_T GuestProcess::i_waitFlagsToResultEx(uint32_t fWaitFlags,
 
     if (newStatus == ProcessStatus_Started)
     {
-        /* Filter out waits which are *not* supported using
+        /**
+         * Filter out waits which are *not* supported using
          * older guest control Guest Additions.
          *
          ** @todo ProcessWaitForFlag_Std* flags are not implemented yet.
