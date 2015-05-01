da4c63363e060cd6b965103b1a04e6704e851be6
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-05-01 10:02:24 +0000

    RTStrCopy is a better choice here.
    svn:sync-xref-src-repo-rev: r99952

--- src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceControlSession.cpp 55578 2015-05-01 01:45:30Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxServiceControlSession.cpp 55579 2015-05-01 10:02:24Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxServiceControlSession - Guest session handling. Also handles
  *                             the forked session processes.
@@ -2349,9 +2349,9 @@ RTEXITCODE VBoxServiceControlSessionForkInit(int argc, char **argv)
         switch (ch)
         {
             case VBOXSERVICESESSIONOPT_LOG_FILE:
-                if (!RTStrPrintf(g_szLogFile, sizeof(g_szLogFile), "%s", ValueUnion.psz))
-                    return RTMsgErrorExit(RTEXITCODE_FAILURE, "Unable to set logfile name to '%s'",
-                                          ValueUnion.psz);
+                rc = RTStrCopy(g_szLogFile, sizeof(g_szLogFile), ValueUnion.psz);
+                if (RT_FAILURE(rc))
+                    return RTMsgErrorExit(RTEXITCODE_FAILURE, "Error copying log file name: %Rrc", rc);
                 break;
 #ifdef DEBUG
             case VBOXSERVICESESSIONOPT_DUMP_STDOUT:
