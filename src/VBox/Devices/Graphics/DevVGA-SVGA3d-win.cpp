0d59c80e33acf2d479173286f4e63c38ba72890c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-05-06 03:08:59 +0000

    SVGA3d: mipmap related patch from trivirt.
    svn:sync-xref-src-repo-rev: r100077

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-win.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA3d-win.cpp 55401 2015-04-23 10:03:17Z noreply@oracle.com $ */
+/* $Id: DevVGA-SVGA3d-win.cpp 55684 2015-05-06 03:08:59Z knut.osmundsen@oracle.com $ */
 /** @file
  * DevVMWare - VMWare SVGA device
  */
@@ -4270,7 +4270,6 @@ int vmsvga3dSetRenderTarget(PVGASTATE pThis, uint32_t cid, SVGA3dRenderTargetTyp
     AssertReturn(pState, VERR_NO_MEMORY);
     AssertReturn(type < SVGA3D_RT_MAX, VERR_INVALID_PARAMETER);
     AssertReturn(target.face == 0, VERR_INVALID_PARAMETER);
-    AssertReturn(target.mipmap == 0, VERR_INVALID_PARAMETER);
 
     Log(("vmsvga3dSetRenderTarget cid=%x type=%x surface id=%x\n", cid, type, target.sid));
 
@@ -4349,6 +4348,7 @@ int vmsvga3dSetRenderTarget(PVGASTATE pThis, uint32_t cid, SVGA3dRenderTargetTyp
     {
     case SVGA3D_RT_DEPTH:
     case SVGA3D_RT_STENCIL:
+        AssertReturn(target.mipmap == 0, VERR_INVALID_PARAMETER);
         if (!pRenderTarget->u.pSurface)
         {
             DWORD cQualityLevels = 0;
@@ -4494,20 +4494,21 @@ int vmsvga3dSetRenderTarget(PVGASTATE pThis, uint32_t cid, SVGA3dRenderTargetTyp
                 PVMSVGA3DSHAREDSURFACE pSharedSurface = vmsvga3dSurfaceGetSharedCopy(pThis, pContext, pRenderTarget);
                 AssertReturn(pSharedSurface, VERR_INTERNAL_ERROR);
 
-                hr = pSharedSurface->u.pTexture->GetSurfaceLevel(0 /* Texture level 0 */,
+                hr = pSharedSurface->u.pTexture->GetSurfaceLevel(target.mipmap,
                                                                  &pSurface);
 
                 fShared = true;
             }
             else
 #endif
-                hr = pRenderTarget->u.pTexture->GetSurfaceLevel(0 /* Texture level 0 */,
+                hr = pRenderTarget->u.pTexture->GetSurfaceLevel(target.mipmap,
                                                                 &pSurface);
 
             AssertMsgReturn(hr == D3D_OK, ("vmsvga3dSetRenderTarget: GetSurfaceLevel failed with %x\n", hr), VERR_INTERNAL_ERROR);
         }
         else
         {
+            AssertReturn(target.mipmap == 0, VERR_INVALID_PARAMETER);
             if (!pRenderTarget->u.pSurface)
             {
                 DWORD cQualityLevels = 0;
