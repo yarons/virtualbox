d23d97a3ad4067c0d2a0ca16dff00ea678b595c2
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-05-16 13:03:11 +0000

    IMachineDebugger: Added a queryOSKernelLog method.
    svn:sync-xref-src-repo-rev: r100342

--- src/VBox/Main/include/MachineDebuggerImpl.h
+++ src/VBox/Main/include/MachineDebuggerImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MachineDebuggerImpl.h 55883 2015-05-16 01:22:15Z knut.osmundsen@oracle.com $ */
+/* $Id: MachineDebuggerImpl.h 55885 2015-05-16 13:03:11Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -106,6 +106,8 @@ private:
                        com::Utf8Str &aPlugInName);
     HRESULT unloadPlugIn(const com::Utf8Str &aName);
     HRESULT detectOS(com::Utf8Str &aOs);
+    HRESULT queryOSKernelLog(ULONG aMaxMessages,
+                             com::Utf8Str &aDmesg);
     HRESULT getRegister(ULONG aCpuId,
                         const com::Utf8Str &aName,
                         com::Utf8Str &aValue);
