64a28c05d0cf9aca5716aeeeebe49f8662df99c5
Frank Mehnert <noreply@oracle.com>
2015-05-21 09:07:06 +0000

    ValidationKit: fixed location of the GA .iso on Solaris
    svn:sync-xref-src-repo-rev: r100505

--- src/VBox/ValidationKit/testdriver/vbox.py
+++ src/VBox/ValidationKit/testdriver/vbox.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: vbox.py 55836 2015-05-12 19:01:41Z klaus.espenlaub@oracle.com $
+# $Id: vbox.py 55995 2015-05-21 09:07:06Z noreply@oracle.com $
 # pylint: disable=C0302
 
 """
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 55836 $"
+__version__ = "$Revision: 55995 $"
 
 
 # Standard Python imports.
@@ -402,6 +402,8 @@ class Build(object): # pylint: disable=R0903
         if self.sOs == 'win' and self.sKind != 'development':
             self.sGuestAdditionsIso = '%s/VBoxGuestAdditions.iso' % (self.sInstallPath,);
         elif self.sOs == 'darwin':
+            self.sGuestAdditionsIso = '%s/VBoxGuestAdditions.iso' % (self.sInstallPath,);
+        elif self.sOs == 'solaris':
             self.sGuestAdditionsIso = '%s/VBoxGuestAdditions.iso' % (self.sInstallPath,);
         else:
             self.sGuestAdditionsIso = '%s/additions/VBoxGuestAdditions.iso' % (self.sInstallPath,);
