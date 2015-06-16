158b8b67458c5531593d8db7fe007138f5f75d86
Alexander Eichner <alexander.eichner@oracle.com>
2015-06-16 13:21:07 +0000

    Main: Style
    svn:sync-xref-src-repo-rev: r101067

--- src/VBox/Main/include/SessionImpl.h
+++ src/VBox/Main/include/SessionImpl.h
@@ -1,4 +1,4 @@
-/* $Id: SessionImpl.h 56448 2015-06-16 13:09:31Z alexander.eichner@oracle.com $ */
+/* $Id: SessionImpl.h 56450 2015-06-16 13:21:07Z alexander.eichner@oracle.com $ */
 /** @file
  * VBox Client Session COM Class definition
  */
@@ -140,7 +140,7 @@ private:
     HRESULT cancelSaveStateWithReason();
 
 
-    HRESULT i_unlockMachine(bool aFinalRelease, bool aFromServer, AutoWriteLock *pLockW);
+    HRESULT i_unlockMachine(bool aFinalRelease, bool aFromServer, AutoWriteLock &aLockW);
 
     SessionState_T mState;
     SessionType_T mType;
