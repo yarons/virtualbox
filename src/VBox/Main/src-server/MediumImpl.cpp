12a1cd5aea91bb28b7906a76506cb1ef140f8a52
Alexander Eichner <alexander.eichner@oracle.com>
2015-06-17 11:09:53 +0000

    Main/Medium: Don't log an error when the medium is not configured for encryption
    svn:sync-xref-src-repo-rev: r101091

--- src/VBox/Main/src-server/MediumImpl.cpp
+++ src/VBox/Main/src-server/MediumImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MediumImpl.cpp 56030 2015-05-22 14:06:29Z noreply@oracle.com $ */
+/* $Id: MediumImpl.cpp 56474 2015-06-17 11:09:53Z alexander.eichner@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -3227,10 +3227,10 @@ HRESULT Medium::getEncryptionSettings(com::Utf8Str &aCipher, com::Utf8Str &aPass
         ComObjPtr<Medium> pBase = i_getBase();
         AutoWriteLock alock(this COMMA_LOCKVAL_SRC_POS);
 
+        /* Check whether encryption is configured for this medium. */
         settings::StringsMap::iterator it = pBase->m->mapProperties.find("CRYPT/KeyStore");
         if (it == pBase->m->mapProperties.end())
-            throw setError(VBOX_E_NOT_SUPPORTED,
-                           tr("The image is not configured for encryption"));
+            throw VBOX_E_NOT_SUPPORTED;
 
 # ifdef VBOX_WITH_EXTPACK
         static const Utf8Str strExtPackPuel("Oracle VM VirtualBox Extension Pack");
