455e7c927d5abcad33aff36145c2ac855efc5db0
Michal Necasek <michal.necasek@oracle.com>
2015-06-18 12:00:19 +0000

    Main/AC97: Propagate codec model setting through CFGM.
    svn:sync-xref-src-repo-rev: r101131

--- src/VBox/Main/src-client/ConsoleImpl2.cpp
+++ src/VBox/Main/src-client/ConsoleImpl2.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl2.cpp 56319 2015-06-09 22:55:53Z knut.osmundsen@oracle.com $ */
+/* $Id: ConsoleImpl2.cpp 56512 2015-06-18 12:00:19Z michal.necasek@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation - VM Configuration Bits.
  *
@@ -2694,6 +2694,8 @@ int Console::i_configConstructorInner(PUVM pUVM, PVM pVM, AutoWriteLock *pAlock)
         {
             AudioControllerType_T audioController;
             hrc = audioAdapter->COMGETTER(AudioController)(&audioController);               H();
+            AudioCodecType_T audioCodec;
+            hrc = audioAdapter->COMGETTER(AudioCodec)(&audioCodec);                         H();
             switch (audioController)
             {
                 case AudioControllerType_AC97:
@@ -2704,6 +2706,15 @@ int Console::i_configConstructorInner(PUVM pUVM, PVM pVM, AutoWriteLock *pAlock)
                     InsertConfigInteger(pInst, "Trusted",          1); /* boolean */
                     hrc = pBusMgr->assignPCIDevice("ichac97", pInst);                       H();
                     InsertConfigNode(pInst,    "Config", &pCfg);
+                    switch (audioCodec)
+                    {
+                        case AudioCodecType_STAC9700:
+                            InsertConfigString(pCfg,   "Codec", "STAC9700");
+                            break;
+                        case AudioCodecType_AD1980:
+                            InsertConfigString(pCfg,   "Codec", "AD1980");
+                            break;
+                    }
                     break;
                 }
                 case AudioControllerType_SB16:
