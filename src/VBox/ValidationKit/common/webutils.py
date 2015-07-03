9ff44e743195a33e395145026f12de485a09e5db
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-07-03 01:41:28 +0000

    better idea for escapeAttrToStr.
    svn:sync-xref-src-repo-rev: r101411

--- src/VBox/ValidationKit/common/webutils.py
+++ src/VBox/ValidationKit/common/webutils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: webutils.py 56295 2015-06-09 14:29:55Z knut.osmundsen@oracle.com $
+# $Id: webutils.py 56762 2015-07-03 01:41:28Z knut.osmundsen@oracle.com $
 
 """
 Common Web Utility Functions.
@@ -26,7 +26,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 56295 $"
+__version__ = "$Revision: 56762 $"
 
 # Standard Python imports.
 import os;
@@ -77,7 +77,9 @@ def escapeAttrToStr(oObject):
     """
     Stringifies the object and hands it to escapeAttr.  May return unicode string.
     """
-    return escapeAttr(oObject);
+    if utils.isString(oObject):
+        return escapeAttr(oObject);
+    return escapeAttr(str(oObject));
 
 def escapeAttrJavaScriptStringDQ(sText):
     """ Escapes a javascript string that is to be emitted between double quotes. """
