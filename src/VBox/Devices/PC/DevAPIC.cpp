e48fc5240331b20739f43116403aac1b71a99111
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2015-07-07 10:04:27 +0000

    DevAPIC: LogRel consistency.
    svn:sync-xref-src-repo-rev: r101487

--- src/VBox/Devices/PC/DevAPIC.cpp
+++ src/VBox/Devices/PC/DevAPIC.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevAPIC.cpp 56292 2015-06-09 14:20:46Z knut.osmundsen@oracle.com $ */
+/* $Id: DevAPIC.cpp 56832 2015-07-07 10:04:27Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * Advanced Programmable Interrupt Controller (APIC) Device.
  *
@@ -1161,7 +1161,7 @@ PDMBOTHCBDECL(int) apicLocalInterrupt(PPDMDEVINS pDevIns, uint8_t u8Pin, uint8_t
             case APIC_DM_FIXED:
             {
                 /** @todo implement APIC_DM_FIXED! */
-                LogRelMax(5, ("delivery type APIC_DM_FIXED not implemented. u8Pin=%d u8Level=%d\n", u8Pin, u8Level));
+                LogRelMax(5, ("APIC: Delivery type APIC_DM_FIXED not implemented. u8Pin=%d u8Level=%d\n", u8Pin, u8Level));
                 return  VINF_SUCCESS;
             }
             case APIC_DM_INIT:
