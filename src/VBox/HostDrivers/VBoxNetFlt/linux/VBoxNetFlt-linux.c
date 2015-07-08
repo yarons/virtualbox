6d710c23430111ae6d7423e45bf6055d5d086b3b
Valery Ushakov <noreply@oracle.com>
2015-07-08 10:34:19 +0000

    VBoxNetFlt/linux: Simplify check for IPv6 link-local addresses that should work with older kernels.
    svn:sync-xref-src-repo-rev: r101514

--- src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
+++ src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFlt-linux.c 56847 2015-07-08 00:48:17Z noreply@oracle.com $ */
+/* $Id: VBoxNetFlt-linux.c 56855 2015-07-08 10:34:19Z noreply@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Linux Specific Code.
  */
@@ -1937,7 +1937,7 @@ static int vboxNetFltLinuxEnumeratorCallback(struct notifier_block *self, unsign
 #endif
         {
             if (   dev != pThis->u.s.pDev
-                && ipv6_addr_src_scope(&ifa->addr) <= IPV6_ADDR_SCOPE_LINKLOCAL)
+                && ipv6_addr_type(&ifa->addr) & (IPV6_ADDR_LINKLOCAL | IPV6_ADDR_LOOPBACK))
                 continue;
 
             Log(("%s: %s: IPv6 addr %RTnaipv6/%u\n",
@@ -2020,7 +2020,7 @@ static int vboxNetFltLinuxNotifierIPv6Callback(struct notifier_block *self, unsi
         vboxNetFltLinuxReleaseNetDev(pThis, pDev);
 
     if (   !fMyDev
-        && ipv6_addr_src_scope(&ifa->addr) <= IPV6_ADDR_SCOPE_LINKLOCAL)
+        && ipv6_addr_type(&ifa->addr) & (IPV6_ADDR_LINKLOCAL | IPV6_ADDR_LOOPBACK))
         return NOTIFY_OK;
 
     if (pThis->pSwitchPort->pfnNotifyHostAddress)
