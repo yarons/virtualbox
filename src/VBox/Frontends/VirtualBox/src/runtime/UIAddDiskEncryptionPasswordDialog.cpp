db9e0d2877bdf9d4cf9010e68c1efc948789d252
Sergey Dubov <sergey.dubov@oracle.com>
2015-07-16 14:31:31 +0000

    FE/Qt: 7676: Encryption support: User-oriented improvement: Focus password table again if password validation failed.
    svn:sync-xref-src-repo-rev: r101663

--- src/VBox/Frontends/VirtualBox/src/runtime/UIAddDiskEncryptionPasswordDialog.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIAddDiskEncryptionPasswordDialog.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIAddDiskEncryptionPasswordDialog.cpp 56953 2015-07-16 14:04:58Z sergey.dubov@oracle.com $ */
+/* $Id: UIAddDiskEncryptionPasswordDialog.cpp 56954 2015-07-16 14:31:31Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIAddDiskEncryptionPasswordDialog class implementation.
  */
@@ -203,6 +203,7 @@ void UIPasswordEditor::keyPressEvent(QKeyEvent *pEvent)
         case Qt::Key_Enter:
         case Qt::Key_Return:
             emit sigEnterKeyTriggered();
+            pEvent->accept();
             break;
         default:
             break;
@@ -456,7 +457,13 @@ void UIAddDiskEncryptionPasswordDialog::accept()
         const QString strMediumId = m_encryptedMediums.values(strPasswordId).first();
         const QString strPassword = m_pTableEncryptionData->encryptionPasswords().value(strPasswordId);
         if (!isPasswordValid(strMediumId, strPassword))
-            return msgCenter().warnAboutInvalidEncryptionPassword(strPasswordId, this);
+        {
+            msgCenter().warnAboutInvalidEncryptionPassword(strPasswordId, this);
+            AssertPtrReturnVoid(m_pTableEncryptionData);
+            m_pTableEncryptionData->setFocus();
+            m_pTableEncryptionData->editFirstIndex();
+            return;
+        }
     }
     /* Call to base-class: */
     QIWithRetranslateUI<QDialog>::accept();
