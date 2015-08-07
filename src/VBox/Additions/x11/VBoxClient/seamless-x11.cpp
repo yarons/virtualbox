27141f3215bc82331b14eef2728d7a0f491a7ea5
Michael Thayer <noreply@oracle.com>
2015-08-07 18:30:43 +0000

    Additions/x11/VBoxClient: fix problems with invisible seamless windows in OpenBox.
    svn:sync-xref-src-repo-rev: r101999

--- src/VBox/Additions/x11/VBoxClient/seamless-x11.cpp
+++ src/VBox/Additions/x11/VBoxClient/seamless-x11.cpp
@@ -1,4 +1,4 @@
-/* $Id: seamless-x11.cpp 55401 2015-04-23 10:03:17Z noreply@oracle.com $ */
+/* $Id: seamless-x11.cpp 57245 2015-08-07 18:30:43Z noreply@oracle.com $ */
 /** @file
  * X11 Seamless mode.
  */
@@ -172,7 +172,11 @@ void SeamlessX11::addClients(const Window hRoot)
         return;
     phChildren = phChildrenRaw;
     for (unsigned i = 0; i < cChildren; ++i)
-        addClientWindow(phChildren[i]);
+        if (!mGuestWindows.find(phChildren[i]))
+        {
+            addClientWindow(phChildren[i]);
+            mChanged = true;
+        }
     XFree(phChildrenRaw);
     LogRelFlowFunc(("returning\n"));
 }
@@ -183,10 +187,8 @@ void SeamlessX11::addClientWindow(const Window hWin)
     LogRelFlowFunc(("\n"));
     XWindowAttributes winAttrib;
     bool fAddWin = true;
-    Window hClient = XmuClientWindow(mDisplay, hWin);
+    Window hClient = 0;
 
-    if (isVirtualRoot(hClient))
-        fAddWin = false;
     if (fAddWin && !XGetWindowAttributes(mDisplay, hWin, &winAttrib))
     {
         LogRelFunc(("VBoxClient: Failed to get the window attributes for window %d\n", hWin));
@@ -194,6 +196,12 @@ void SeamlessX11::addClientWindow(const Window hWin)
     }
     if (fAddWin && (winAttrib.map_state == IsUnmapped))
         fAddWin = false;
+    if (fAddWin)
+    {
+        hClient = XmuClientWindow(mDisplay, hWin);
+        if (isVirtualRoot(hClient))
+            fAddWin = false;
+    }
     XSizeHints dummyHints;
     long dummyLong;
     if (fAddWin && (!XGetWMNormalHints(mDisplay, hClient, &dummyHints,
@@ -303,6 +311,7 @@ void SeamlessX11::nextConfigurationEvent(void)
     }
     mChanged = false;
     XNextEvent(mDisplay, &event);
+    addClients(DefaultRootWindow(mDisplay));
     switch (event.type)
     {
     case ConfigureNotify:
