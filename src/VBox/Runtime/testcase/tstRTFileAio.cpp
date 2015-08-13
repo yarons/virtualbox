8f608af2898b339f49806725937c2efb66eedea8
Frank Mehnert <noreply@oracle.com>
2015-08-13 09:32:58 +0000

    testcase/tstRTFileAio: actually it's VERR_ACCESS_DENIED not VERR_INVALID_PARAMETER on tmpfs
    svn:sync-xref-src-repo-rev: r102073

--- src/VBox/Runtime/testcase/tstRTFileAio.cpp
+++ src/VBox/Runtime/testcase/tstRTFileAio.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstRTFileAio.cpp 57300 2015-08-12 20:23:10Z noreply@oracle.com $ */
+/* $Id: tstRTFileAio.cpp 57311 2015-08-13 09:32:58Z noreply@oracle.com $ */
 /** @file
  * IPRT Testcase - File Async I/O.
  */
@@ -193,7 +193,7 @@ int main()
         rc = RTFileOpen(&hFile, "tstFileAio#1.tst",
                                 RTFILE_O_READWRITE | RTFILE_O_CREATE_REPLACE | RTFILE_O_DENY_NONE | RTFILE_O_ASYNC_IO);
         RTTESTI_CHECK(   rc == VINF_SUCCESS
-                      || (rc == VERR_INVALID_PARAMETER && fAsyncMayFail));
+                      || (rc == VERR_ACCESS_DENIED && fAsyncMayFail));
         if (RT_SUCCESS(rc))
         {
             uint8_t *pbTestBuf = (uint8_t *)RTTestGuardedAllocTail(g_hTest, TSTFILEAIO_BUFFER_SIZE);
