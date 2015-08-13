c5815180d3c8bcffaa1bb922a465a39fd12563ce
Frank Mehnert <noreply@oracle.com>
2015-08-13 08:02:22 +0000

    typo
    svn:sync-xref-src-repo-rev: r102067

--- src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
+++ src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
@@ -1,4 +1,4 @@
-/* $Rev: 57255 $ */
+/* $Rev: 57305 $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Linux specifics.
  */
@@ -667,7 +667,7 @@ static int VBoxDrvLinuxIOCtl(struct inode *pInode, struct file *pFilp, unsigned 
      */
     if (RT_UNLIKELY(g_DevExt.cBadContextCalls > 0))
     {
-        SUPR0Printf("VBoxDrvDarwinIOCtl: EFLAGS.AC=0 detected %u times, refusing all I/O controls!\n", g_DevExt.cBadContextCalls);
+        SUPR0Printf("VBoxDrvLinuxIOCtl: EFLAGS.AC=0 detected %u times, refusing all I/O controls!\n", g_DevExt.cBadContextCalls);
         return ESPIPE;
     }
 
