23f14b1fc6f0ca52ce21f4717cd5d4be5c5d32a4
Frank Mehnert <noreply@oracle.com>
2015-08-18 11:21:12 +0000

    warning
    svn:sync-xref-src-repo-rev: r102182

--- src/VBox/Devices/Audio/DrvAudio.cpp
+++ src/VBox/Devices/Audio/DrvAudio.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvAudio.cpp 57398 2015-08-17 15:33:05Z andreas.loeffler@oracle.com $ */
+/* $Id: DrvAudio.cpp 57417 2015-08-18 11:21:12Z noreply@oracle.com $ */
 /** @file
  * Intermediate audio driver header.
  *
@@ -335,7 +335,6 @@ void drvAudioClearBuf(PPDMPCMPROPS pPCMInfo, void *pvBuf, size_t cbBuf)
 
         case 16:
         {
-            int i;
             uint16_t *p = (uint16_t *)pvBuf;
             int shift = pPCMInfo->cChannels - 1;
             short s = INT16_MAX;
@@ -343,7 +342,7 @@ void drvAudioClearBuf(PPDMPCMPROPS pPCMInfo, void *pvBuf, size_t cbBuf)
             if (pPCMInfo->fSwapEndian)
                 s = RT_BSWAP_U16(s);
 
-            for (i = 0; i < cbBuf << shift; i++)
+            for (unsigned i = 0; i < cbBuf << shift; i++)
                 p[i] = s;
 
             break;
@@ -351,7 +350,6 @@ void drvAudioClearBuf(PPDMPCMPROPS pPCMInfo, void *pvBuf, size_t cbBuf)
 
         case 32:
         {
-            int i;
             uint32_t *p = (uint32_t *)pvBuf;
             int shift = pPCMInfo->cChannels - 1;
             int32_t s = INT32_MAX;
@@ -359,7 +357,7 @@ void drvAudioClearBuf(PPDMPCMPROPS pPCMInfo, void *pvBuf, size_t cbBuf)
             if (pPCMInfo->fSwapEndian)
                 s = RT_BSWAP_U32(s);
 
-            for (i = 0; i < cbBuf << shift; i++)
+            for (unsigned i = 0; i < cbBuf << shift; i++)
                 p[i] = s;
 
             break;
