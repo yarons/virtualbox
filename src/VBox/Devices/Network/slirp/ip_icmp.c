8e7e320863af42495e99f030f1e20834b7943ea4
Valery Ushakov <noreply@oracle.com>
2015-08-19 00:33:17 +0000

    NAT: fix debug log - print IP address, not a pointer to it, and convert ports to host byte order.
    svn:sync-xref-src-repo-rev: r102215

--- src/VBox/Devices/Network/slirp/ip_icmp.c
+++ src/VBox/Devices/Network/slirp/ip_icmp.c
@@ -1,4 +1,4 @@
-/* $Id: ip_icmp.c 56292 2015-06-09 14:20:46Z knut.osmundsen@oracle.com $ */
+/* $Id: ip_icmp.c 57448 2015-08-19 00:33:17Z noreply@oracle.com $ */
 /** @file
  * NAT - IP/ICMP handling.
  */
@@ -350,8 +350,9 @@ icmp_find_original_mbuf(PNATState pData, struct ip *ip)
             }
             for (so = head_socket->so_prev; so != head_socket; so = so->so_prev)
             {
-                /* Should be reaplaced by hash here */
-                Log(("trying:%R[natsock] against %RTnaipv4:%d lport=%d hlport=%d\n", so, &faddr, fport, lport, so->so_hlport));
+                /* Should be replaced by hash here */
+                Log(("trying:%R[natsock] against %RTnaipv4:%d lport=%d hlport=%d\n",
+                     so, faddr.s_addr, ntohs(fport), ntohs(lport), ntohs(so->so_hlport)));
                 if (   so->so_faddr.s_addr == faddr.s_addr
                     && so->so_fport == fport
                     && so->so_hlport == lport)
