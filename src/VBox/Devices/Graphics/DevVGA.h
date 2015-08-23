ffe1b09f5900d89229c4d943ba21334e467ac26d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-08-23 17:29:15 +0000

    DevVGA-SVGA*: Some viewport sanity adjustments and comments.
    svn:sync-xref-src-repo-rev: r102291

--- src/VBox/Devices/Graphics/DevVGA.h
+++ src/VBox/Devices/Graphics/DevVGA.h
@@ -1,4 +1,4 @@
-/* $Id: DevVGA.h 57410 2015-08-18 09:58:29Z klaus.espenlaub@oracle.com $ */
+/* $Id: DevVGA.h 57502 2015-08-23 17:29:15Z knut.osmundsen@oracle.com $ */
 /** @file
  * DevVGA - VBox VGA/VESA device, internal header.
  */
@@ -308,7 +308,9 @@ typedef struct VMSVGAState
     uint32_t                    u32MaxWidth;
     /** Maximum height supported. */
     uint32_t                    u32MaxHeight;
-    /** Viewport rectangle */
+    /** Viewport rectangle, i.e. what's currently visible of the target host
+     *  window.  This is usually (0,0)(uWidth,uHeight), but if the window is
+     *  shrunk and scrolling applied, both the origin and size may differ.  */
     struct
     {
         uint32_t                x;
