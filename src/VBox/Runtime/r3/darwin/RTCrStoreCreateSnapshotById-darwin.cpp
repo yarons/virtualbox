7e83d2ceefba442be86c50b4f01d10869b5275b7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-08-29 00:19:37 +0000

    build fix
    svn:sync-xref-src-repo-rev: r102410

--- src/VBox/Runtime/r3/darwin/RTCrStoreCreateSnapshotById-darwin.cpp
+++ src/VBox/Runtime/r3/darwin/RTCrStoreCreateSnapshotById-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTCrStoreCreateSnapshotById-darwin.cpp 57580 2015-08-29 00:05:48Z knut.osmundsen@oracle.com $ */
+/* $Id: RTCrStoreCreateSnapshotById-darwin.cpp 57582 2015-08-29 00:19:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - RTCrStoreCreateSnapshotById, Darwin.
  */
@@ -34,6 +34,11 @@
 #include <iprt/assert.h>
 #include <iprt/err.h>
 #include <iprt/file.h>
+
+/* HACK ALERT! Shut up those deprecated messages on SecKeychainSearchCreateFromAttributes and SecKeychainSearchCopyNext. */
+#include <CoreFoundation/CoreFoundation.h>
+#undef  DEPRECATED_IN_MAC_OS_X_VERSION_10_7_AND_LATER
+#define DEPRECATED_IN_MAC_OS_X_VERSION_10_7_AND_LATER
 
 #include <Security/Security.h>
 
