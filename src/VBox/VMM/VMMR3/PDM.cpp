f1730f73f73c24d386173b5cc9bae18813b77956
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2015-09-10 10:06:33 +0000

    VMM/PDM: clear driver instance data after destruction, to catch use after free (the audio code used to be buggy, which is fixed for a while now)
    svn:sync-xref-src-repo-rev: r102586

--- src/VBox/VMM/VMMR3/PDM.cpp
+++ src/VBox/VMM/VMMR3/PDM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDM.cpp 57358 2015-08-14 15:16:38Z knut.osmundsen@oracle.com $ */
+/* $Id: PDM.cpp 57683 2015-09-10 10:06:33Z klaus.espenlaub@oracle.com $ */
 /** @file
  * PDM - Pluggable Device Manager.
  */
@@ -610,10 +610,21 @@ static void pdmR3TermLuns(PVM pVM, PPDMLUN pLun, const char *pszDevice, unsigned
             }
             pDrvIns->Internal.s.pDrv->cInstances--;
 
-            TMR3TimerDestroyDriver(pVM, pDrvIns);
+            /* Order of resource freeing like in pdmR3DrvDestroyChain, but
+             * not all need to be done as they are done globally later. */
             //PDMR3QueueDestroyDriver(pVM, pDrvIns);
-            //pdmR3ThreadDestroyDriver(pVM, pDrvIns);
+            TMR3TimerDestroyDriver(pVM, pDrvIns);
             SSMR3DeregisterDriver(pVM, pDrvIns, NULL, 0);
+            //pdmR3ThreadDestroyDriver(pVM, pDrvIns);
+            //DBGFR3InfoDeregisterDriver(pVM, pDrvIns, NULL);
+            //pdmR3CritSectBothDeleteDriver(pVM, pDrvIns);
+            //PDMR3BlkCacheReleaseDriver(pVM, pDrvIns);
+#ifdef VBOX_WITH_PDM_ASYNC_COMPLETION
+            //pdmR3AsyncCompletionTemplateDestroyDriver(pVM, pDrvIns);
+#endif
+
+            /* Clear the driver struture to catch sloppy code. */
+            ASMMemFill32(pDrvIns, RT_OFFSETOF(PDMDRVINS, achInstanceData[pDrvIns->pReg->cbInstance]), 0xdeadd0d0);
 
             pDrvIns = pDrvNext;
         }
