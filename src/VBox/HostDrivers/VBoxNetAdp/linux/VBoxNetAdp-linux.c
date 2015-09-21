1903b4962d0d1b12b3c55b00203b21f5d6079bcb
Frank Mehnert <noreply@oracle.com>
2015-09-21 15:40:31 +0000

    warning
    svn:sync-xref-src-repo-rev: r102793

--- src/VBox/HostDrivers/VBoxNetAdp/linux/VBoxNetAdp-linux.c
+++ src/VBox/HostDrivers/VBoxNetAdp/linux/VBoxNetAdp-linux.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetAdp-linux.c 57839 2015-09-21 15:18:07Z noreply@oracle.com $ */
+/* $Id: VBoxNetAdp-linux.c 57841 2015-09-21 15:40:31Z noreply@oracle.com $ */
 /** @file
  * VBoxNetAdp - Virtual Network Adapter Driver (Host), Linux Specific Code.
  */
@@ -427,7 +427,6 @@ static int __init VBoxNetAdpLinuxInit(void)
  */
 static void __exit VBoxNetAdpLinuxUnload(void)
 {
-    int rc;
     Log(("VBoxNetAdpLinuxUnload\n"));
 
     /*
