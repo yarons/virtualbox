c2628f40ed1843cf7b230d87970a8c31e13c8e0a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-09-26 17:24:17 +0000

    DrvHostSerial.cpp: Should call GetOverlappedResult if ReadFile returns ERROR_IO_PENDING. Observed with Free Virtual Serial Ports v3.20.00.0545 on 64-bit Windows 10.
    svn:sync-xref-src-repo-rev: r102882

--- src/VBox/Devices/Serial/DrvHostSerial.cpp
+++ src/VBox/Devices/Serial/DrvHostSerial.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostSerial.cpp 57358 2015-08-14 15:16:38Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvHostSerial.cpp 57910 2015-09-26 17:24:17Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox stream I/O devices: Host serial driver
  */
@@ -473,8 +473,7 @@ static DECLCALLBACK(int) drvHostSerialSendThread(PPDMDRVINS pDrvIns, PPDMTHREAD 
     while (pThread->enmState == PDMTHREADSTATE_RUNNING)
     {
         int rc = RTSemEventWait(pThis->SendSem, RT_INDEFINITE_WAIT);
-        if (RT_FAILURE(rc))
-            break;
+        AssertRCBreak(rc);
 
         /*
          * Write the character to the host device.
@@ -769,9 +768,20 @@ static DECLCALLBACK(int) drvHostSerialRecvThread(PPDMDRVINS pDrvIns, PPDMTHREAD 
             {
                 if (!ReadFile(pThis->hDeviceFile, abBuffer, sizeof(abBuffer), &dwNumberOfBytesTransferred, &pThis->overlappedRecv))
                 {
-                    rcThread = RTErrConvertFromWin32(GetLastError());
-                    LogRel(("HostSerial#%d: Read failed with error %Rrc; terminating the worker thread.\n", pDrvIns->iInstance, rcThread));
-                    break;
+                    dwRet = GetLastError();
+                    if (dwRet == ERROR_IO_PENDING)
+                    {
+                        if (GetOverlappedResult(pThis->hDeviceFile, &pThis->overlappedRecv, &dwNumberOfBytesTransferred, TRUE))
+                            dwRet = NO_ERROR;
+                        else
+                            dwRet = GetLastError();
+                    }
+                    if (dwRet != NO_ERROR)
+                    {
+                        rcThread = RTErrConvertFromWin32(dwRet);
+                        LogRel(("HostSerial#%d: Read failed with error %Rrc; terminating the worker thread.\n", pDrvIns->iInstance, rcThread));
+                        break;
+                    }
                 }
                 cbRemaining = dwNumberOfBytesTransferred;
             }
