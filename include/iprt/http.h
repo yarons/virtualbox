d26b64e54c1941b56cda9740678f8132a7c879eb
Frank Mehnert <noreply@oracle.com>
2015-10-13 16:21:41 +0000

    fixed prototype
    svn:sync-xref-src-repo-rev: r103373

--- include/iprt/http.h
+++ include/iprt/http.h
@@ -1,4 +1,4 @@
-/* $Id: http.h 58217 2015-10-13 15:36:47Z noreply@oracle.com $ */
+/* $Id: http.h 58219 2015-10-13 16:21:41Z noreply@oracle.com $ */
 /** @file
  * IPRT - Simple HTTP/HTTPS Client API.
  */
@@ -294,7 +294,7 @@ RTR3DECL(int) RTHttpGatherCaCertsInFile(const char *pszCaFile, uint32_t fFlags, 
  * @param   pfnDownloadProgress Progress function to be called. Set it to
  *                          NULL to disable the callback.
  */
-RTR3DECL(int) RTHttpSetDownloadProgressCallback(RTHTTP hHttp, PRTHTTPDOWNLDPROGRCALLBACK pfnDownloadProgress);
+RTR3DECL(int) RTHttpSetDownloadProgressCallback(RTHTTP hHttp, PRTHTTPDOWNLDPROGRCALLBACK pfnDownloadProgress, void *pvUser);
 
 
 /** @} */
