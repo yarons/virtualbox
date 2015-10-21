e1c048d63253e26d34b8fbd9bd167dcb3d921fa5
Michael Thayer <noreply@oracle.com>
2015-10-21 08:59:00 +0000

    Installer/linux (guest): get rid of more auto-generation.
    svn:sync-xref-src-repo-rev: r103554

--- src/VBox/Additions/linux/installer/autorun.sh
+++ src/VBox/Additions/linux/installer/autorun.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: autorun.sh 58324 2015-10-20 08:39:37Z noreply@oracle.com $
+# $Id: autorun.sh 58351 2015-10-21 08:59:00Z noreply@oracle.com $
 #
 # VirtualBox Guest Additions installation script for *nix guests
 #
@@ -18,7 +18,37 @@
 
 PATH=$PATH:/bin:/sbin:/usr/sbin
 
-#include sh-utils.sh
+# Deal with differing "which" semantics
+mywhich() {
+    which "$1" 2>/dev/null | grep -v "no $1"
+}
+
+# Get the name and execute switch for a useful terminal emulator
+#
+# Sets $gxtpath to the emulator path or empty
+# Sets $gxttitle to the "title" switch for that emulator
+# Sets $gxtexec to the "execute" switch for that emulator
+# May clobber $gtx*
+# Calls mywhich
+getxterm() {
+    # gnome-terminal uses -e differently to other emulators
+    for gxti in "konsole --title -e" "gnome-terminal --title -x" "xterm -T -e"; do
+        set $gxti
+        gxtpath="`mywhich $1`"
+        case "$gxtpath" in ?*)
+            gxttitle=$2
+            gxtexec=$3
+            return
+            ;;
+        esac
+    done
+}
+
+# Quotes its argument by inserting '\' in front of every character save
+# for 'A-Za-z0-9/'.  Prints the result to stdout.
+quotify() {
+    echo "$1" | sed -e 's/\([^a-zA-Z0-9/]\)/\\\1/g'
+}
 
 ostype=`uname -s`
 if test "$ostype" != "Linux" && test "$ostype" != "SunOS" ; then
