23c77cd51e0c84d554e7f99424f776f7f594975b
Michael Thayer <noreply@oracle.com>
2015-10-21 11:04:35 +0000

    Installer/linux (guest): fix kernel module re-building looping if the build fails.
    svn:sync-xref-src-repo-rev: r103566

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# Linux Additions kernel module init script ($Revision: 58356 $)
+# Linux Additions kernel module init script ($Revision: 58357 $)
 #
 
 #
@@ -418,10 +418,9 @@ setup()
     if [ "$mod_succ" -eq "0" ]; then
         if running_vboxguest || running_vboxadd; then
             begin "You should restart your guest to make sure the new modules are actually used" console
-        else
-            start
         fi
     fi
+    return "${mod_succ}"
 }
 
 # cleanup_script
@@ -471,7 +470,7 @@ restart)
     restart
     ;;
 setup)
-    setup
+    setup && start
     ;;
 cleanup)
     cleanup
