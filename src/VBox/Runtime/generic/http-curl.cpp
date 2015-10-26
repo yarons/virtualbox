29106fc24263061bb21252954a77c4604c21ff0a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2015-10-26 12:17:33 +0000

    IPRT/http-curl.cpp: Reverted r103649 - comletely wrong fix.
    svn:sync-xref-src-repo-rev: r103665

--- src/VBox/Runtime/generic/http-curl.cpp
+++ src/VBox/Runtime/generic/http-curl.cpp
@@ -1,4 +1,4 @@
-/* $Id: http-curl.cpp 58409 2015-10-26 09:49:48Z andreas.loeffler@oracle.com $ */
+/* $Id: http-curl.cpp 58418 2015-10-26 12:17:33Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - HTTP client API, cURL based.
  */
@@ -705,7 +705,6 @@ static int rtHttpConfigureProxyFromUrl(PRTHTTPINTERNAL pThis, const char *pszPro
 static int rtHttpConfigureProxyForUrlFromEnv(PRTHTTPINTERNAL pThis, const char *pszUrl)
 {
     char szTmp[_1K];
-    szTmp[0] = '\0';
 
     /*
      * First we consult the "no_proxy" / "NO_PROXY" environment variable.
