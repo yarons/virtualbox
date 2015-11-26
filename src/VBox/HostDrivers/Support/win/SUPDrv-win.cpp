3e08ba4ce25d0def19ce3995031b49bd82bbbd90
Michal Necasek <michal.necasek@oracle.com>
2015-11-26 09:18:58 +0000

    Build fix.
    svn:sync-xref-src-repo-rev: r104361

--- src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
+++ src/VBox/HostDrivers/Support/win/SUPDrv-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-win.cpp 58872 2015-11-26 02:17:48Z knut.osmundsen@oracle.com $ */
+/* $Id: SUPDrv-win.cpp 58875 2015-11-26 09:18:58Z michal.necasek@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Windows NT specifics.
  */
@@ -1833,6 +1833,10 @@ void VBOXCALL   supdrvOSLdrNotifyOpened(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE p
     NOREF(pDevExt); NOREF(pImage); NOREF(pszFilename);
 }
 
+void VBOXCALL   supdrvOSLdrNotifyUnloaded(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE pImage)
+{
+    NOREF(pDevExt); NOREF(pImage);
+}
 
 int  VBOXCALL   supdrvOSLdrValidatePointer(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE pImage, void *pv, const uint8_t *pbImageBits)
 {
