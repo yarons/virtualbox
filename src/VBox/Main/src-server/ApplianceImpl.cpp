14aed7fb8eab5d11f58992f86159a840a18cd20a
Frank Mehnert <noreply@oracle.com>
2016-01-06 15:27:45 +0000

    gcc warning
    svn:sync-xref-src-repo-rev: r104906

--- src/VBox/Main/src-server/ApplianceImpl.cpp
+++ src/VBox/Main/src-server/ApplianceImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImpl.cpp 58005 2015-10-02 10:44:29Z valery.portnyagin@oracle.com $ */
+/* $Id: ApplianceImpl.cpp 59268 2016-01-06 15:27:45Z noreply@oracle.com $ */
 /** @file
  * IAppliance and IVirtualSystem COM class implementations.
  */
@@ -761,9 +761,9 @@ HRESULT Appliance::i_initApplianceIONameMap()
     if (!applianceIONameMap.empty())
         return rc;
 
-        applianceIONameMap.insert(std::make_pair(applianceIOTar, applianceIOTarName));
-        applianceIONameMap.insert(std::make_pair(applianceIOFile, applianceIOFileName));
-        applianceIONameMap.insert(std::make_pair(applianceIOSha, applianceIOShaName));
+    applianceIONameMap.insert(std::make_pair(applianceIOTar, applianceIOTarName));
+    applianceIONameMap.insert(std::make_pair(applianceIOFile, applianceIOFileName));
+    applianceIONameMap.insert(std::make_pair(applianceIOSha, applianceIOShaName));
 
     return rc;
 }
