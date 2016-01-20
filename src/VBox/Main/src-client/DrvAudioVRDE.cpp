4054ca62c31e5bd560df44a9d964ae8ae90b8033
Andreas Loeffler <andreas.loeffler@oracle.com>
2016-01-20 14:49:30 +0000

    DrvAudioVRDE.cpp: Fixed wrong casting (PVRDESTREAMIN -> PVRDESTREAMOUT) for drvAudioVRDEControlOut().
    svn:sync-xref-src-repo-rev: r105141

--- src/VBox/Main/src-client/DrvAudioVRDE.cpp
+++ src/VBox/Main/src-client/DrvAudioVRDE.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvAudioVRDE.cpp 58378 2015-10-22 12:46:32Z andreas.loeffler@oracle.com $ */
+/* $Id: DrvAudioVRDE.cpp 59419 2016-01-20 14:49:30Z andreas.loeffler@oracle.com $ */
 /** @file
  * VRDE audio backend for Main.
  */
@@ -303,7 +303,7 @@ static DECLCALLBACK(int) drvAudioVRDEControlOut(PPDMIHOSTAUDIO pInterface, PPDMA
     PDRVAUDIOVRDE pDrv = RT_FROM_MEMBER(pInterface, DRVAUDIOVRDE, IHostAudio);
     AssertPtrReturn(pDrv, VERR_INVALID_POINTER);
 
-    PVRDESTREAMIN pVRDEStrmOut = (PVRDESTREAMIN)pHstStrmOut;
+    PVRDESTREAMOUT pVRDEStrmOut = (PVRDESTREAMOUT)pHstStrmOut;
     AssertPtrReturn(pVRDEStrmOut, VERR_INVALID_POINTER);
 
     LogFlowFunc(("enmStreamCmd=%ld\n", enmStreamCmd));
