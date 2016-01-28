1930e0bb49089099f0865c687894d0017fa6d55c
Alexander Eichner <alexander.eichner@oracle.com>
2016-01-28 14:53:22 +0000

    Fix for the cleanup
    svn:sync-xref-src-repo-rev: r105291

--- src/VBox/Runtime/r3/posix/RTFileSetAllocationSize-posix.cpp
+++ src/VBox/Runtime/r3/posix/RTFileSetAllocationSize-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTFileSetAllocationSize-posix.cpp 59512 2016-01-28 14:41:26Z alexander.eichner@oracle.com $ */
+/* $Id: RTFileSetAllocationSize-posix.cpp 59513 2016-01-28 14:53:22Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT - RTFileSetAllocationSize, linux implementation.
  */
@@ -49,7 +49,7 @@ RTDECL(int) RTFileSetAllocationSize(RTFILE hFile, uint64_t cbSize, uint32_t fFla
 {
     AssertReturn(hFile != NIL_RTFILE, VERR_INVALID_PARAMETER);
     AssertReturn(!(fFlags & ~RTFILE_ALLOC_SIZE_F_VALID), VERR_INVALID_PARAMETER);
-    AssertMsgReturn(sizeof(off_t) >= sizeof(cbSize) &&  RT_HIDWORD(cbSize) == 0,
+    AssertMsgReturn(sizeof(off_t) >= sizeof(cbSize) ||  RT_HIDWORD(cbSize) == 0,
                     ("64-bit filesize not supported! cbSize=%lld\n", cbSize),
                     VERR_NOT_SUPPORTED);
 
