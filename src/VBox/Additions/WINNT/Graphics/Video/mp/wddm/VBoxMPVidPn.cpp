85787df495b7739fe6b9feeccdfad94f6eb0ede0
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2016-01-29 09:07:19 +0000

    WDDM: bugref:8228: display-only driver does not need vsync.
    svn:sync-xref-src-repo-rev: r105298

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPVidPn.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPVidPn.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMPVidPn.cpp 52631 2014-09-05 21:11:11Z noreply@oracle.com $ */
+/* $Id: VBoxMPVidPn.cpp 59518 2016-01-29 09:07:19Z vitali.pelenjow@oracle.com $ */
 
 /** @file
  * VBox WDDM Miniport driver
@@ -127,13 +127,26 @@ static NTSTATUS vboxVidPnPopulateVideoSignalInfo(D3DKMDT_VIDEO_SIGNAL_INFO *pVsi
     pVsi->VideoStandard  = D3DKMDT_VSS_OTHER;
     pVsi->ActiveSize.cx = pResolution->cx;
     pVsi->ActiveSize.cy = pResolution->cy;
-    pVsi->VSyncFreq.Numerator = VSync * 1000;
-    pVsi->VSyncFreq.Denominator = 1000;
-    pVsi->TotalSize.cx = pVsi->ActiveSize.cx;// + VBOXVDPN_C_DISPLAY_HBLANK_SIZE;
-    pVsi->TotalSize.cy = pVsi->ActiveSize.cy;// + VBOXVDPN_C_DISPLAY_VBLANK_SIZE;
-    pVsi->PixelRate = pVsi->TotalSize.cx * pVsi->TotalSize.cy * VSync;
-    pVsi->HSyncFreq.Numerator = (UINT)((pVsi->PixelRate / pVsi->TotalSize.cy) * 1000);
-    pVsi->HSyncFreq.Denominator = 1000;
+    pVsi->TotalSize = pVsi->ActiveSize;
+    if (VBOXWDDM_IS_DISPLAYONLY())
+    {
+        /* VSYNC is not implemented in display-only mode (#8228).
+         * In this case Windows checks that frequencies are not specified.
+         */
+        pVsi->VSyncFreq.Numerator = D3DKMDT_FREQUENCY_NOTSPECIFIED;
+        pVsi->VSyncFreq.Denominator = D3DKMDT_FREQUENCY_NOTSPECIFIED;
+        pVsi->PixelRate = D3DKMDT_FREQUENCY_NOTSPECIFIED;
+        pVsi->HSyncFreq.Numerator = D3DKMDT_FREQUENCY_NOTSPECIFIED;
+        pVsi->HSyncFreq.Denominator = D3DKMDT_FREQUENCY_NOTSPECIFIED;
+    }
+    else
+    {
+        pVsi->VSyncFreq.Numerator = VSync * 1000;
+        pVsi->VSyncFreq.Denominator = 1000;
+        pVsi->PixelRate = pVsi->TotalSize.cx * pVsi->TotalSize.cy * VSync;
+        pVsi->HSyncFreq.Numerator = (UINT)((pVsi->PixelRate / pVsi->TotalSize.cy) * 1000);
+        pVsi->HSyncFreq.Denominator = 1000;
+    }
     pVsi->ScanLineOrdering = D3DDDI_VSSLO_PROGRESSIVE;
 
     return Status;
