2b57bd1df3ab2acc89e6d6b1f0c9207fd9af4f54
Santosh Kumar <noreply@oracle.com>
2016-02-03 16:04:52 +0000

    FE/Qt: bugref:7985: Mac OS X: Runtime UI: UI Session: Adjust the backing-scale factor for cursor for HiDPI screens when unscaled HiDPI output is used.
    svn:sync-xref-src-repo-rev: r105381

--- src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
@@ -1,4 +1,4 @@
-/* $Id: UISession.cpp 59412 2016-01-19 15:08:30Z sergey.dubov@oracle.com $ */
+/* $Id: UISession.cpp 59572 2016-02-03 16:04:52Z noreply@oracle.com $ */
 /** @file
  * VBox Qt GUI - UISession class implementation.
  */
@@ -1721,8 +1721,23 @@ void UISession::setPointerShape(const uchar *pShapeData, bool fHasAlpha,
                            (uint32_t *)image.bits(), uHeight * uWidth * 4);
     }
 
+    /* Create cursor-pixmap from the image: */
+    QPixmap cursorPixmap = QPixmap::fromImage(image);
+# ifdef Q_WS_MAC
+#  ifdef VBOX_GUI_WITH_HIDPI
+    /* Adjust-backing-scale-factor: */
+    // TODO: In case of multi-monitor setup check whether backing-scale factor and cursor are screen specific.
+    /* Get screen-id of main-window: */
+    const ulong uScreenID = machineLogic()->activeMachineWindow()->screenId();
+    /* Get backing-scale-factor: */
+    const double dBackingScaleFactor = frameBuffer(uScreenID)->backingScaleFactor();
+    /* Adjust-backing-scale-factor if necessary: */
+    if (dBackingScaleFactor > 1.0 && frameBuffer(uScreenID)->useUnscaledHiDPIOutput())
+        cursorPixmap.setDevicePixelRatio(dBackingScaleFactor);
+#  endif /* VBOX_GUI_WITH_HIDPI */
+# endif /* Q_WS_MAC */
     /* Set the new cursor: */
-    m_cursor = QCursor(QPixmap::fromImage(image), uXHot, uYHot);
+    m_cursor = QCursor(cursorPixmap, uXHot, uYHot);
     m_fIsValidPointerShapePresent = true;
     NOREF(srcShapePtrScan);
 
