ec8e5579f8fa29bb5997ad0f1f3a3da056766aa9
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2016-03-14 12:41:40 +0000

    Additions/Windows/VBoxTray: useless condition (thanks PVS)
    svn:sync-xref-src-repo-rev: r105995

--- src/VBox/Additions/WINNT/VBoxTray/VBoxDisplay.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxDisplay.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxDisplay.cpp 58307 2015-10-18 23:47:59Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxDisplay.cpp 60020 2016-03-14 12:41:40Z vitali.pelenjow@oracle.com $ */
 /** @file
  * VBoxSeamless - Display notifications.
  */
@@ -653,7 +653,7 @@ static BOOL ResizeDisplayDevice(PVBOXDISPLAYCONTEXT pCtx,
 
         LogFlowFunc(("Request to resize the displa\n"));
         DWORD err = VBoxDispIfResizeModes(&pCtx->pEnv->dispIf, Id, fEnabled, fExtDispSup, paDisplayDevices, paDeviceModes, DevNum);
-        if (err == NO_ERROR || err != ERROR_RETRY)
+        if (err != ERROR_RETRY)
         {
             if (err == NO_ERROR)
                 LogFlowFunc(("VBoxDispIfResizeModes succeeded\n"));
