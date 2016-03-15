1ab65600b9c77de8475cbac8f39f24f592de6186
Frank Mehnert <noreply@oracle.com>
2016-03-15 12:01:40 +0000

    Runtime/VerSetConditionMask: fixed type (thanks PVS)
    svn:sync-xref-src-repo-rev: r106017

--- src/VBox/Runtime/r3/win/vcc100-kernel32-fakes.cpp
+++ src/VBox/Runtime/r3/win/vcc100-kernel32-fakes.cpp
@@ -1,4 +1,4 @@
-/* $Id: vcc100-kernel32-fakes.cpp 57905 2015-09-25 20:39:56Z knut.osmundsen@oracle.com $ */
+/* $Id: vcc100-kernel32-fakes.cpp 60035 2016-03-15 12:01:40Z noreply@oracle.com $ */
 /** @file
  * IPRT - Tricks to make the Visual C++ 2010 CRT work on NT4, W2K and XP.
  */
@@ -412,7 +412,7 @@ ULONGLONG WINAPI VerSetConditionMask(ULONGLONG fConditionMask, DWORD fTypeMask, 
     for (unsigned i = 0; i < 8; i++)
         if (fTypeMask & RT_BIT_32(i))
         {
-            uint64_t fMask  = 0xff << (i*8);
+            uint64_t fMask  = UINT64_C(0xff) << (i*8);
             fConditionMask &= ~fMask;
             fConditionMask |= (uint64_t)bOperator << (i*8);
 
