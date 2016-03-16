0bb97359c9f567da3324e8cc62dbb9beeb6187d9
Alexander Eichner <alexander.eichner@oracle.com>
2016-03-16 09:19:25 +0000

    Storage/LsiLogic: Missing case
    svn:sync-xref-src-repo-rev: r106044

--- src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
+++ src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevLsiLogicSCSI.cpp 60015 2016-03-14 09:37:22Z alexander.eichner@oracle.com $ */
+/* $Id: DevLsiLogicSCSI.cpp 60057 2016-03-16 09:19:25Z alexander.eichner@oracle.com $ */
 /** @file
  * DevLsiLogicSCSI - LsiLogic LSI53c1030 SCSI controller.
  */
@@ -3881,8 +3881,11 @@ static DECLCALLBACK(int) lsilogicR3IsaIOPortWriteStr(PPDMDEVINS pDevIns, void *p
     int rc = vboxscsiWriteString(pDevIns, &pThis->VBoxSCSI, iRegister, pbSrc, pcTransfers, cb);
     if (rc == VERR_MORE_DATA)
     {
-        rc = lsilogicR3PrepareBiosScsiRequest(pThis);
-        AssertRC(rc);
+        ASMAtomicXchgBool(&pThis->fBiosReqPending, true);
+        /* Send a notifier to the PDM queue that there are pending requests. */
+        PPDMQUEUEITEMCORE pItem = PDMQueueAlloc(pThis->CTX_SUFF(pNotificationQueue));
+        AssertMsg(pItem, ("Allocating item for queue failed\n"));
+        PDMQueueInsert(pThis->CTX_SUFF(pNotificationQueue), (PPDMQUEUEITEMCORE)pItem);
     }
     else if (RT_FAILURE(rc))
         AssertMsgFailed(("Writing BIOS register failed %Rrc\n", rc));
