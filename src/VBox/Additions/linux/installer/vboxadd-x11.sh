3c71a9deeb9b6f8a6bbdecdd51af3333656b028a
Frank Mehnert <noreply@oracle.com>
2016-03-18 12:07:46 +0000

    Additions/linux: call vboxadd-x11 during uninstall, otherwise a few files in /etc survive
    svn:sync-xref-src-repo-rev: r106090

--- src/VBox/Additions/linux/installer/vboxadd-x11.sh
+++ src/VBox/Additions/linux/installer/vboxadd-x11.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# Linux Additions X11 setup init script ($Revision: 59962 $)
+# Linux Additions X11 setup init script ($Revision: 60092 $)
 #
 
 #
@@ -476,6 +476,7 @@ EOF
     succ_msg "Window system drivers installed"
 }
 
+# cleanup_script
 cleanup()
 {
     # Restore xorg.conf files as far as possible
