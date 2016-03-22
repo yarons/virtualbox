db9d2a92cfe12a90156f1b530239d798183a80dc
Michael Thayer <noreply@oracle.com>
2016-03-22 19:23:35 +0000

    bugref:8087: Additions/x11: support non-root X server: the Linux kernel sends cursor events to each screen, but we handle them globally.  Keep track of whether the cursor is enabled for each screen, and only disable it globally if it is disabled everywhere.  Also take screen offset into account when reporting cursor position to the host.
    svn:sync-xref-src-repo-rev: r106159

--- src/VBox/Additions/linux/drm/vbox_drv.h
+++ src/VBox/Additions/linux/drm/vbox_drv.h
@@ -1,4 +1,4 @@
-/* $Id: vbox_drv.h 60123 2016-03-21 14:40:17Z noreply@oracle.com $ */
+/* $Id: vbox_drv.h 60141 2016-03-22 19:23:35Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -152,10 +152,7 @@ struct vbox_crtc {
     bool disconnected;
     unsigned crtc_id;
     uint32_t fb_offset;
-    struct drm_gem_object *cursor_bo;
-    uint64_t cursor_addr;
-    int cursor_width, cursor_height;
-    u8 offset_x, offset_y;
+    bool cursor_enabled;
 };
 
 struct vbox_encoder {
