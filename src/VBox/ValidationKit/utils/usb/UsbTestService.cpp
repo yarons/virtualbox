d7ff7a2e2e16ee0ff4036505204f7e8d38a02730
Alexander Eichner <alexander.eichner@oracle.com>
2016-03-31 18:59:42 +0000

    shut up gcc
    svn:sync-xref-src-repo-rev: r106321

--- src/VBox/ValidationKit/utils/usb/UsbTestService.cpp
+++ src/VBox/ValidationKit/utils/usb/UsbTestService.cpp
@@ -1,4 +1,4 @@
-/* $Id: UsbTestService.cpp 60279 2016-03-31 18:57:37Z alexander.eichner@oracle.com $ */
+/* $Id: UsbTestService.cpp 60280 2016-03-31 18:59:42Z alexander.eichner@oracle.com $ */
 /** @file
  * UsbTestService - Remote USB test configuration and execution server.
  */
@@ -533,7 +533,7 @@ static int utsClientReqProcess(PUTSCLIENT pClient)
     /*
      * Read client command packet and process it.
      */
-    PUTSPKTHDR pPktHdr;
+    PUTSPKTHDR pPktHdr = NULL;
     int rc = utsRecvPkt(pClient, &pPktHdr, true /*fAutoRetryOnFailure*/);
     if (RT_FAILURE(rc))
         return rc;
@@ -981,7 +981,7 @@ static RTEXITCODE utsParseArgv(int argc, char **argv, bool *pfExit)
             }
 
             case 'V':
-                RTPrintf("$Revision: 60279 $\n");
+                RTPrintf("$Revision: 60280 $\n");
                 *pfExit = true;
                 return RTEXITCODE_SUCCESS;
 
