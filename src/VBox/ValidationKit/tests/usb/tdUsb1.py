328a598aa5c543010155757f18683fe5a22f82dc
Alexander Eichner <alexander.eichner@oracle.com>
2016-04-19 11:15:23 +0000

    ValidationKit/usb: Move usbgadget2 to usbgadget and rmeove the old version, smaller fixes for pylint
    svn:sync-xref-src-repo-rev: r106700

--- src/VBox/ValidationKit/tests/usb/tdUsb1.py
+++ src/VBox/ValidationKit/tests/usb/tdUsb1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdUsb1.py 60562 2016-04-19 07:50:03Z alexander.eichner@oracle.com $
+# $Id: tdUsb1.py 60567 2016-04-19 11:15:23Z alexander.eichner@oracle.com $
 
 """
 VirtualBox Validation Kit - USB testcase and benchmark.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 60562 $"
+__version__ = "$Revision: 60567 $"
 
 
 # Standard Python imports.
@@ -48,7 +48,7 @@ from testdriver import vbox;
 from testdriver import vboxcon;
 
 # USB gadget control import
-import usbgadget2;
+import usbgadget;
 
 class tdUsbBenchmark(vbox.TestDriver):                                      # pylint: disable=R0902
     """
@@ -307,7 +307,7 @@ class tdUsbBenchmark(vbox.TestDriver):                                      # py
         # Get configured USB test devices from hostname we are running on
         sGadgetHost, uGadgetPort = self.getGadgetParams(self.sHostname, sSpeed);
 
-        oUsbGadget = usbgadget2.UsbGadget();
+        oUsbGadget = usbgadget.UsbGadget();
         reporter.log('Connecting to UTS: ' + sGadgetHost);
         fRc = oUsbGadget.connectTo(30 * 1000, sGadgetHost, uPort = uGadgetPort);
         if fRc is True:
@@ -315,7 +315,7 @@ class tdUsbBenchmark(vbox.TestDriver):                                      # py
             self.oVBox.host.addUSBDeviceSource('USBIP', sGadgetHost, sGadgetHost + (':%s' % oUsbGadget.getUsbIpPort()), [], []);
 
             # Create test device gadget and a filter to attach the device automatically.
-            fRc = oUsbGadget.impersonate(usbgadget2.g_ksGadgetImpersonationTest);
+            fRc = oUsbGadget.impersonate(usbgadget.g_ksGadgetImpersonationTest);
             if fRc is True:
                 iBusId, _ = oUsbGadget.getGadgetBusAndDevId();
                 fRc = oSession.addUsbDeviceFilter('Compliance device', sVendorId = '0525', sProductId = 'a4a0', \
@@ -355,14 +355,14 @@ class tdUsbBenchmark(vbox.TestDriver):                                      # py
         # Get configured USB test devices from hostname we are running on
         sGadgetHost, uGadgetPort = self.getGadgetParams(self.sHostname, sSpeed);
 
-        oUsbGadget = usbgadget2.UsbGadget();
+        oUsbGadget = usbgadget.UsbGadget();
         reporter.log('Connecting to UTS: ' + sGadgetHost);
         fRc = oUsbGadget.connectTo(30 * 1000, sGadgetHost,  uPort = uGadgetPort);
         if fRc is True:
             self.oVBox.host.addUSBDeviceSource('USBIP', sGadgetHost, sGadgetHost + (':%s' % oUsbGadget.getUsbIpPort()), [], []);
 
             # Create test device gadget and a filter to attach the device automatically.
-            fRc = oUsbGadget.impersonate(usbgadget2.g_ksGadgetImpersonationTest);
+            fRc = oUsbGadget.impersonate(usbgadget.g_ksGadgetImpersonationTest);
             if fRc is True:
                 iBusId, _ = oUsbGadget.getGadgetBusAndDevId();
                 fRc = oSession.addUsbDeviceFilter('Compliance device', sVendorId = '0525', sProductId = 'a4a0', \
