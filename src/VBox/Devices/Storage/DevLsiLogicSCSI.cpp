d682c61d77cee206a2beeceba3ddb8de361c4676
Alexander Eichner <alexander.eichner@oracle.com>
2016-04-19 08:28:41 +0000

    Storage/LsiLogic: Fix regression causing writes to the disk to fail
    svn:sync-xref-src-repo-rev: r106695

--- src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
+++ src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevLsiLogicSCSI.cpp 60057 2016-03-16 09:19:25Z alexander.eichner@oracle.com $ */
+/* $Id: DevLsiLogicSCSI.cpp 60565 2016-04-19 08:28:41Z alexander.eichner@oracle.com $ */
 /** @file
  * DevLsiLogicSCSI - LsiLogic LSI53c1030 SCSI controller.
  */
@@ -3890,7 +3890,7 @@ static DECLCALLBACK(int) lsilogicR3IsaIOPortWriteStr(PPDMDEVINS pDevIns, void *p
     else if (RT_FAILURE(rc))
         AssertMsgFailed(("Writing BIOS register failed %Rrc\n", rc));
 
-    return rc;
+    return VINF_SUCCESS;
 }
 
 /**
