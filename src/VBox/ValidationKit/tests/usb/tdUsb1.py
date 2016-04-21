2122eebe0f7e4021a6e8fe990d6ce32448ea5c92
Alexander Eichner <alexander.eichner@oracle.com>
2016-04-21 16:06:58 +0000

    ValidationKit/usb: Format the port as a hexadecimal number because the filter requires it that way
    svn:sync-xref-src-repo-rev: r106793

--- src/VBox/ValidationKit/tests/usb/tdUsb1.py
+++ src/VBox/ValidationKit/tests/usb/tdUsb1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdUsb1.py 60567 2016-04-19 11:15:23Z alexander.eichner@oracle.com $
+# $Id: tdUsb1.py 60634 2016-04-21 16:06:58Z alexander.eichner@oracle.com $
 
 """
 VirtualBox Validation Kit - USB testcase and benchmark.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 60567 $"
+__version__ = "$Revision: 60634 $"
 
 
 # Standard Python imports.
@@ -319,7 +319,7 @@ class tdUsbBenchmark(vbox.TestDriver):                                      # py
             if fRc is True:
                 iBusId, _ = oUsbGadget.getGadgetBusAndDevId();
                 fRc = oSession.addUsbDeviceFilter('Compliance device', sVendorId = '0525', sProductId = 'a4a0', \
-                                                  sPort = str(iBusId));
+                                                  sPort = format(iBusId, 'x'));
                 if fRc is True:
 
                     # Wait a moment to let the USB device appear
