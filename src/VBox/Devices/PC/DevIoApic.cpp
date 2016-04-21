2484d057358158c1395623536cd554641187d8bd
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2016-04-21 16:20:38 +0000

    IOAPIC uses its own log group.
    svn:sync-xref-src-repo-rev: r106794

--- src/VBox/Devices/PC/DevIoApic.cpp
+++ src/VBox/Devices/PC/DevIoApic.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevIoApic.cpp 57358 2015-08-14 15:16:38Z knut.osmundsen@oracle.com $ */
+/* $Id: DevIoApic.cpp 60635 2016-04-21 16:20:38Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * I/O Advanced Programmable Interrupt Controller (IO-APIC) Device.
  */
@@ -42,7 +42,7 @@
 /*********************************************************************************************************************************
 *   Header Files                                                                                                                 *
 *********************************************************************************************************************************/
-#define LOG_GROUP LOG_GROUP_DEV_APIC
+#define LOG_GROUP LOG_GROUP_DEV_IOAPIC
 #include <VBox/vmm/pdmdev.h>
 
 #include <VBox/log.h>
