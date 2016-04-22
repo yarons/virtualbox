9f64fc656147edd143ee37b82984ed5d261f4847
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-04-22 16:12:33 +0000

    iem/smsw: Missed a 286 tweak.
    svn:sync-xref-src-repo-rev: r106827

--- src/VBox/VMM/VMMAll/IEMAllInstructions.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllInstructions.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllInstructions.cpp.h 60559 2016-04-19 03:05:28Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllInstructions.cpp.h 60659 2016-04-22 16:12:33Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Emulation.
  */
@@ -1013,6 +1013,10 @@ FNIEMOP_DEF_1(iemOp_Grp7_smsw, uint8_t, bRm)
         IEM_MC_LOCAL(RTGCPTR,  GCPtrEffDst);
         IEM_MC_CALC_RM_EFF_ADDR(GCPtrEffDst, bRm, 0);
         IEM_MC_FETCH_CR0_U16(u16Tmp);
+#if IEM_CFG_TARGET_CPU == IEMTARGETCPU_DYNAMIC
+        if (pIemCpu->uTargetCpu == IEMTARGETCPU_286)
+            IEM_MC_OR_LOCAL_U16(u16Tmp, 0xfff0); /* Reserved bits observed all set on real hw. */
+#endif
         IEM_MC_STORE_MEM_U16(pIemCpu->iEffSeg, GCPtrEffDst, u16Tmp);
         IEM_MC_ADVANCE_RIP();
         IEM_MC_END();
