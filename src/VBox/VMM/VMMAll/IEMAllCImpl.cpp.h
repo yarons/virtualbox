d35b2c5e743e53add540c0a4ab6b67566293cc9e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-05-02 15:36:04 +0000

    IEM: iret fix
    svn:sync-xref-src-repo-rev: r106991

--- src/VBox/VMM/VMMAll/IEMAllCImpl.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImpl.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllCImpl.cpp.h 60791 2016-05-02 14:52:05Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllCImpl.cpp.h 60792 2016-05-02 15:36:04Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Implementation in C/C++ (code include).
  */
@@ -3115,15 +3115,22 @@ IEM_CIMPL_DEF_1(iemCImpl_iret_prot, IEMMODE, enmEffOpSize)
 
 
     /* Privilege checks. */
+    if (!(DescCS.Legacy.Gen.u4Type & X86_SEL_TYPE_CONF))
+    {
+        if ((uNewCs & X86_SEL_RPL) != DescCS.Legacy.Gen.u2Dpl)
+        {
+            Log(("iret %04x:%08x - RPL != DPL (%d) -> #GP\n", uNewCs, uNewEip, DescCS.Legacy.Gen.u2Dpl));
+            return iemRaiseGeneralProtectionFaultBySelector(pIemCpu, uNewCs);
+        }
+    }
+    else if ((uNewCs & X86_SEL_RPL) < DescCS.Legacy.Gen.u2Dpl)
+    {
+        Log(("iret %04x:%08x - RPL < DPL (%d) -> #GP\n", uNewCs, uNewEip, DescCS.Legacy.Gen.u2Dpl));
+        return iemRaiseGeneralProtectionFaultBySelector(pIemCpu, uNewCs);
+    }
     if ((uNewCs & X86_SEL_RPL) < pIemCpu->uCpl)
     {
         Log(("iret %04x:%08x - RPL < CPL (%d) -> #GP\n", uNewCs, uNewEip, pIemCpu->uCpl));
-        return iemRaiseGeneralProtectionFaultBySelector(pIemCpu, uNewCs);
-    }
-    if (   (DescCS.Legacy.Gen.u4Type & X86_SEL_TYPE_CONF)
-        && (uNewCs & X86_SEL_RPL) < DescCS.Legacy.Gen.u2Dpl)
-    {
-        Log(("iret %04x:%08x - RPL < DPL (%d) -> #GP\n", uNewCs, uNewEip, DescCS.Legacy.Gen.u2Dpl));
         return iemRaiseGeneralProtectionFaultBySelector(pIemCpu, uNewCs);
     }
 
