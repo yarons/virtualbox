5b20d42dc1164546779e6f4849169b570d5590f3
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2016-05-04 09:30:01 +0000

    VMM/APIC: alignment.
    svn:sync-xref-src-repo-rev: r107021

--- src/VBox/VMM/include/APICInternal.h
+++ src/VBox/VMM/include/APICInternal.h
@@ -1,4 +1,4 @@
-/* $Id: APICInternal.h 60804 2016-05-03 14:13:51Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: APICInternal.h 60815 2016-05-04 09:30:01Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * APIC - Advanced Programmable Interrupt Controller.
  */
@@ -419,6 +419,8 @@ typedef struct APICDEV
     PCPDMAPICHLPR3              pApicHlpR3;
     /** The PDM critical section - R3 Ptr. */
     R3PTRTYPE(PPDMCRITSECT)     pCritSectR3;
+    /** Alignment. */
+    R3PTRTYPE(void *)           pvAlignment0;
 
     /** The device instance - R0 Ptr. */
     PPDMDEVINSR0                pDevInsR0;
@@ -426,6 +428,8 @@ typedef struct APICDEV
     PCPDMAPICHLPR0              pApicHlpR0;
     /** The PDM critical section - R0 Ptr. */
     R0PTRTYPE(PPDMCRITSECT)     pCritSectR0;
+    /** Alignment. */
+    R0PTRTYPE(void *)           pvAlignment1;
 
     /** The device instance - RC Ptr. */
     PPDMDEVINSRC                pDevInsRC;
@@ -434,7 +438,7 @@ typedef struct APICDEV
     /** The PDM critical section - RC Ptr. */
     RCPTRTYPE(PPDMCRITSECT) pCritSectRC;
     /** Alignment padding. */
-    RCPTRTYPE(void *)           pvAlignment0;
+    RCPTRTYPE(void *)           pvAlignment2;
 } APICDEV;
 /** Pointer to an APIC device. */
 typedef APICDEV *PAPICDEV;
