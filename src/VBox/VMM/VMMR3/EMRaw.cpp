fdb9497c2c1d046a1a99d2b9ce03d44ea3091b54
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-05-06 12:12:06 +0000

    EM: Use IEM a little more in raw mode.
    svn:sync-xref-src-repo-rev: r107069

--- src/VBox/VMM/VMMR3/EMRaw.cpp
+++ src/VBox/VMM/VMMR3/EMRaw.cpp
@@ -1,4 +1,4 @@
-/* $Id: EMRaw.cpp 58126 2015-10-08 20:59:48Z knut.osmundsen@oracle.com $ */
+/* $Id: EMRaw.cpp 60859 2016-05-06 12:12:06Z knut.osmundsen@oracle.com $ */
 /** @file
  * EM - Execution Monitor / Manager - software virtualization
  */
@@ -365,7 +365,7 @@ static int emR3RawExecuteInstructionWorker(PVM pVM, PVMCPU pVCpu, int rcGC)
      * Use IEM and fallback on REM if the functionality is missing.
      * Once IEM gets mature enough, nothing should ever fall back.
      */
-//#define VBOX_WITH_FIRST_IEM_STEP_B
+#define VBOX_WITH_FIRST_IEM_STEP_B
 #if defined(VBOX_WITH_FIRST_IEM_STEP_B) || !defined(VBOX_WITH_REM)
     Log(("EMINS: %04x:%RGv RSP=%RGv\n", pCtx->cs.Sel, (RTGCPTR)pCtx->rip, (RTGCPTR)pCtx->rsp));
     STAM_PROFILE_START(&pVCpu->em.s.StatIEMEmu, a);
