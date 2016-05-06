b2c8ec4a90f507d25b744cfe56e6ccf596478385
Michael Thayer <noreply@oracle.com>
2016-05-06 16:02:30 +0000

    bugref:8087: Additions/x11: support non-root X server: reduce the mmap offset for the dumb-frame buffer in the kernel graphics driver, as some 32-bit X systems seem not to like 64-bit offsets.
    svn:sync-xref-src-repo-rev: r107076

--- src/VBox/Additions/linux/drm/vbox_drv.h
+++ src/VBox/Additions/linux/drm/vbox_drv.h
@@ -1,4 +1,4 @@
-/* $Id: vbox_drv.h 60352 2016-04-06 11:07:00Z noreply@oracle.com $ */
+/* $Id: vbox_drv.h 60866 2016-05-06 16:02:30Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -266,7 +266,7 @@ extern int vbox_dumb_mmap_offset(struct drm_file *file,
                 uint32_t handle,
                 uint64_t *offset);
 
-#define DRM_FILE_PAGE_OFFSET (0x100000000ULL >> PAGE_SHIFT)
+#define DRM_FILE_PAGE_OFFSET (0x10000000ULL >> PAGE_SHIFT)
 
 int vbox_mm_init(struct vbox_private *vbox);
 void vbox_mm_fini(struct vbox_private *vbox);
