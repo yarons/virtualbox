26351afdc163bd1dc832b8a8ac91cc7d570e1249
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2016-05-20 03:37:00 +0000

    VMM/APIC: nits.
    svn:sync-xref-src-repo-rev: r107342

--- src/VBox/VMM/VMMR3/APIC.cpp
+++ src/VBox/VMM/VMMR3/APIC.cpp
@@ -1,4 +1,4 @@
-/* $Id: APIC.cpp 61072 2016-05-20 02:59:40Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: APIC.cpp 61075 2016-05-20 03:37:00Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * APIC - Advanced Programmable Interrupt Controller.
  */
@@ -283,7 +283,7 @@ VMMR3_INT_DECL(void) APICR3Reset(PVMCPU pVCpu, bool fResetApicBaseMsr)
 
     /*
      * The APIC version register is read-only, so just initialize it here.
-     * It is not clear from the specs, where exactly it is initalized.
+     * It is not clear from the specs, where exactly it is initialized.
      * The version determines the number of LVT entries and size of the APIC ID (8 bits for P4).
      */
     PXAPICPAGE pXApicPage = VMCPU_TO_XAPICPAGE(pVCpu);
