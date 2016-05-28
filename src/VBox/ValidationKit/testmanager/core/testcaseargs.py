569d3225caf7ff74cec59a979597784834403e37
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-05-28 19:24:19 +0000

    TestCaseArgsDataEx: Fixed regression from the TestCaseArgs table change.  initFromDbWithGenIdEx had a hardcoded column count and got the value of the new field instead of the CURRENT_TIMESTAMP value it expected, breaking test case result details.
    svn:sync-xref-src-repo-rev: r107554

--- src/VBox/ValidationKit/testmanager/core/testcaseargs.py
+++ src/VBox/ValidationKit/testmanager/core/testcaseargs.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: testcaseargs.py 61255 2016-05-28 03:52:35Z knut.osmundsen@oracle.com $
+# $Id: testcaseargs.py 61262 2016-05-28 19:24:19Z knut.osmundsen@oracle.com $
 
 """
 Test Manager - Test Case Arguments Variations.
@@ -26,7 +26,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 61255 $"
+__version__ = "$Revision: 61262 $"
 
 
 # Standard python imports.
@@ -209,7 +209,7 @@ class TestCaseArgsDataEx(TestCaseArgsData):
         aoRow = oDb.fetchOne();
         TestCaseArgsData.initFromDbRow(self, aoRow);
 
-        tsNow = aoRow[11];
+        tsNow = aoRow[TestCaseArgsData.kcDbColumns];
         if tsConfigEff is None: tsConfigEff = tsNow;
         if tsRsrcEff is None:   tsRsrcEff   = tsNow;
 
