acf640f40e33f43a7e517610f92f844a85b5a01b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-05-30 15:08:42 +0000

    wuireport.py: width
    svn:sync-xref-src-repo-rev: r107590

--- src/VBox/ValidationKit/testmanager/webui/wuireport.py
+++ src/VBox/ValidationKit/testmanager/webui/wuireport.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: wuireport.py 61297 2016-05-30 15:05:14Z knut.osmundsen@oracle.com $
+# $Id: wuireport.py 61298 2016-05-30 15:08:42Z knut.osmundsen@oracle.com $
 
 """
 Test Manager WUI - Reports.
@@ -26,7 +26,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 61297 $"
+__version__ = "$Revision: 61298 $"
 
 
 # Validation Kit imports.
@@ -584,7 +584,11 @@ class WuiReportTestBoxFailures(WuiReportFailuresWithTotalBase):
         sHtml += u' ';
         sHtml += WuiTestBoxDetailsLink(oTestBox.idTestBox).toHtml();
         sHtml += u'</td>';
-        sHtml += u'<td>%s %s</td>' % (webutils.escapeElem(oTestBox.sOs), webutils.escapeElem(oTestBox.sOsVersion),);
+        sOsAndVer = '%s %s' % (oTestBox.sOs, oTestBox.sOsVersion.strip(),);
+        if len(sOsAndVer) < 22:
+            sHtml += u'<td>%s</td>' % (webutils.escapeElem(sOsAndVer),);
+        else: # wonder if td.title works..
+            sHtml += u'<td title="%s">%s...</td>' % (webutils.escapeAttr(sOsAndVer), webutils.escapeElem(sOsAndVer[:20]));
         sHtml += u'<td>%s</td>'    % (webutils.escapeElem(oTestBox.sCpuArch),);
         sHtml += u'<td>%s</td>'    % (webutils.escapeElem(oTestBox.sCpuVendor),);
         sHtml += u'<td>f=%#x, m=%#x, s=%#x' % (oTestBox.getCpuFamily(), oTestBox.getCpuModel(), oTestBox.getCpuStepping(),)
