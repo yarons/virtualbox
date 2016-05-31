830817e9ffd6c572fabb54d03c8cb6506f116a29
Frank Mehnert <noreply@oracle.com>
2016-05-31 08:39:45 +0000

    force revision update
    svn:sync-xref-src-repo-rev: r107613

--- src/VBox/ValidationKit/testboxscript/testboxscript_real.py
+++ src/VBox/ValidationKit/testboxscript/testboxscript_real.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: testboxscript_real.py 57290 2015-08-12 12:15:57Z knut.osmundsen@oracle.com $
+# $Id: testboxscript_real.py 61319 2016-05-31 08:39:45Z noreply@oracle.com $
 
 """
 TestBox Script - main().
@@ -8,7 +8,7 @@ TestBox Script - main().
 
 __copyright__ = \
 """
-Copyright (C) 2012-2015 Oracle Corporation
+Copyright (C) 2012-2016 Oracle Corporation
 
 This file is part of VirtualBox Open Source Edition (OSE), as
 available from http://www.virtualbox.org. This file is free software;
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 57290 $"
+__version__ = "$Revision: 61319 $"
 
 
 # Standard python imports.
