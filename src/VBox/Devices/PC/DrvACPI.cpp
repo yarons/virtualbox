e460b7700008436bb78be50c846c808f9438dce3
Frank Mehnert <noreply@oracle.com>
2016-06-13 12:40:45 +0000

    Devices/ACPI: when determining the current power consumption, use power_now and only fall back to current_now if the former isn't available. Thanks to Dennis Wassenberg / secunet!
    svn:sync-xref-src-repo-rev: r108033

--- src/VBox/Devices/PC/DrvACPI.cpp
+++ src/VBox/Devices/PC/DrvACPI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvACPI.cpp 61675 2016-06-13 12:39:34Z noreply@oracle.com $ */
+/* $Id: DrvACPI.cpp 61676 2016-06-13 12:40:45Z noreply@oracle.com $ */
 /** @file
  * DrvACPI - ACPI Host Driver.
  */
@@ -643,7 +643,9 @@ static DECLCALLBACK(int) drvACPIPoller(PPDMDRVINS pDrvIns, PPDMTHREAD pThread)
                                     }
                                     RTStrmClose(pStrmStatus);
                                 }
-                                rc = POWER_OPEN(&pStrmStatus, "current_now");
+                                rc = POWER_OPEN(&pStrmStatus, "power_now");
+                                if (RT_FAILURE(rc))
+                                    rc = POWER_OPEN(&pStrmStatus, "current_now");
                                 if (RT_SUCCESS(rc))
                                 {
                                     rc = RTStrmGetLine(pStrmStatus, szLine, sizeof(szLine));
