b549a04afee8c23d3f0551e70a7ba7892f4f4b41
Alexander Eichner <alexander.eichner@oracle.com>
2016-06-13 08:57:36 +0000

    build fix
    svn:sync-xref-src-repo-rev: r108026

--- src/VBox/VMM/VMMR3/DBGFR3Type.cpp
+++ src/VBox/VMM/VMMR3/DBGFR3Type.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFR3Type.cpp 61671 2016-06-13 08:51:05Z alexander.eichner@oracle.com $ */
+/* $Id: DBGFR3Type.cpp 61672 2016-06-13 08:57:36Z alexander.eichner@oracle.com $ */
 /** @file
  * DBGF - Debugger Facility, Type Management.
  */
@@ -1024,8 +1024,8 @@ VMMR3DECL(void) DBGFR3TypeValFree(PDBGFTYPEVAL pVal)
 
     for (uint32_t i = 0; i < pVal->cEntries; i++)
     {
-        PDBGFTYPEVAL pValEntry = &pVal->aEntries[i];
-        PDBGFVALBUF pValBuf = pValEntry->cEntries > 1 ? pValEntry->Buf.pVal : &pValEntry->Buf.Val;
+        PDBGFTYPEVALENTRY pValEntry = &pVal->aEntries[i];
+        PDBGFTYPEVALBUF pValBuf = pValEntry->cEntries > 1 ? pValEntry->Buf.pVal : &pValEntry->Buf.Val;
 
         if (pValEntry->enmType == DBGFTYPEBUILTIN_COMPOUND)
             for (uint32_t iBuf = 0; iBuf < pValEntry->cEntries; iBuf++)
