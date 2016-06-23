29fcd8e758ad1ae261199d6b9e8a3e665eea426d
Sergey Dubov <sergey.dubov@oracle.com>
2016-06-23 15:41:44 +0000

    FE/Qt: bugref:8422: Runtime UI: Full-screen window: Update for r108260.
    svn:sync-xref-src-repo-rev: r108269

--- src/VBox/Frontends/VirtualBox/src/runtime/fullscreen/UIMachineWindowFullscreen.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/fullscreen/UIMachineWindowFullscreen.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineWindowFullscreen.cpp 61850 2016-06-23 12:46:21Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineWindowFullscreen.cpp 61856 2016-06-23 15:41:44Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineWindowFullscreen class implementation.
  */
@@ -210,6 +210,14 @@ void UIMachineWindowFullscreen::sltShowMinimized()
     /* Minimize window and reset it's state to NONE: */
     setWindowState(Qt::WindowNoState);
     windowHandle()->showMinimized();
+
+    /* If there is mini-toolbar: */
+    if (m_pMiniToolBar)
+    {
+        /* Hide mini-toolbar and reset it's state to NONE: */
+        m_pMiniToolBar->setWindowState(Qt::WindowNoState);
+        m_pMiniToolBar->hide();
+    }
 #  endif /* VBOX_WS_X11 */
 # endif /* QT_VERSION >= 0x050000 */
 }
