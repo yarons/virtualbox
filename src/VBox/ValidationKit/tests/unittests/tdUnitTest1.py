28be9963201af2161e00d4141a473be38b81d94c
Frank Mehnert <noreply@oracle.com>
2016-06-23 07:14:41 +0000

    another typo
    svn:sync-xref-src-repo-rev: r108247

--- src/VBox/ValidationKit/tests/unittests/tdUnitTest1.py
+++ src/VBox/ValidationKit/tests/unittests/tdUnitTest1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdUnitTest1.py 61837 2016-06-22 21:35:36Z knut.osmundsen@oracle.com $
+# $Id: tdUnitTest1.py 61840 2016-06-23 07:14:41Z noreply@oracle.com $
 
 """
 VirtualBox Validation Kit - Unit Tests.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 61837 $"
+__version__ = "$Revision: 61840 $"
 
 
 # Standard Python imports.
@@ -315,7 +315,7 @@ class tdUnitTest1(vbox.TestDriver):
         #
         # We need a VBox install (/ build) to test.
         #
-        if False is False:
+        if False is True:
             if not self.importVBoxApi():
                 reporter.error('Unabled to import the VBox Python API.')
                 return False
