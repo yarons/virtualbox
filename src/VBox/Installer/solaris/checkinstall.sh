538beb312dae423bbc9b58bee8c9d83a82abd3dd
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2016-06-28 19:08:35 +0000

    Installer/solaris: need more complex prerequisites for S11+, as S11.0/S11.1 have only the system/library/gcc-45-runtime and the later versions use different names and split it into a C and C++ runtime package.
    svn:sync-xref-src-repo-rev: r108358

--- src/VBox/Installer/solaris/checkinstall.sh
+++ src/VBox/Installer/solaris/checkinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: checkinstall.sh 61909 2016-06-28 08:56:25Z klaus.espenlaub@oracle.com $
+# $Id: checkinstall.sh 61919 2016-06-28 19:08:35Z klaus.espenlaub@oracle.com $
 ## @file
 #
 # VirtualBox checkinstall script for Solaris.
@@ -163,8 +163,8 @@ infoprint "Checking package dependencies..."
 if test -x "$BIN_PKG"; then
     checkdep_ips_either "runtime/python-26" "runtime/python-27"
     checkdep_ips_either "system/library/iconv/utf-8" "system/library/iconv/iconv-core"
-    checkdep_ips "system/library/gcc/gcc-c++-runtime"
-    checkdep_ips "system/library/gcc/gcc-c-runtime"
+    checkdep_ips_either "system/library/gcc/gcc-c++-runtime" "system/library/gcc-45-runtime"
+    checkdep_ips_either "system/library/gcc/gcc-c-runtime" "system/library/gcc-45-runtime"
 else
     PKG_MISSING_IPS="runtime/python-26 system/library/iconv/utf-8 system/library/gcc/gcc-c++-runtime system/library/gcc/gcc-c-runtime"
 fi
