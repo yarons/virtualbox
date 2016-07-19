2b9301981f7b94ee1c3b754aa00757dc981bd5db
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-07-19 14:09:40 +0000

    testdriver/reporter.py: delint
    svn:sync-xref-src-repo-rev: r108858

--- src/VBox/ValidationKit/testdriver/reporter.py
+++ src/VBox/ValidationKit/testdriver/reporter.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: reporter.py 62310 2016-07-19 10:08:40Z knut.osmundsen@oracle.com $
+# $Id: reporter.py 62322 2016-07-19 14:09:40Z knut.osmundsen@oracle.com $
 # pylint: disable=C0302
 
 """
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 62310 $"
+__version__ = "$Revision: 62322 $"
 
 
 # Standard Python imports.
@@ -907,6 +907,7 @@ class RemoteReporter(ReporterBase):
           and cSecsLast >= self.kcSecXmlFlushIdle:
             return self.xmlFlush();
 
+        _ = sDebug;
         return False;
 
     def _xmlWrite(self, asText, fIndent = True):
