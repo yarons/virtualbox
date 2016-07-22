bf83e8390074bb28d71d76231e1e6516b870adea
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-07-22 19:10:00 +0000

    (C) 2016
    svn:sync-xref-src-repo-rev: r109066

--- src/VBox/Devices/Samples/DrvStorageFilter.cpp
+++ src/VBox/Devices/Samples/DrvStorageFilter.cpp
@@ -1,10 +1,10 @@
-/* $Id: DrvStorageFilter.cpp 57358 2015-08-14 15:16:38Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvStorageFilter.cpp 62508 2016-07-22 19:10:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox storage filter driver sample.
  */
 
 /*
- * Copyright (C) 2012-2015 Oracle Corporation
+ * Copyright (C) 2012-2016 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
--- src/VBox/Devices/Samples/VBoxSampleDevice.cpp
+++ src/VBox/Devices/Samples/VBoxSampleDevice.cpp
@@ -1,10 +1,10 @@
-/* $Id: VBoxSampleDevice.cpp 57358 2015-08-14 15:16:38Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxSampleDevice.cpp 62508 2016-07-22 19:10:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Sample Device.
  */
 
 /*
- * Copyright (C) 2009-2015 Oracle Corporation
+ * Copyright (C) 2009-2016 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
