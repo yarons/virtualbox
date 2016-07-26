75ea5eeb2fdbacc4717359ac8167254d4d58ad7b
Santosh Kumar <noreply@oracle.com>
2016-07-26 11:46:43 +0000

    FE/Qt: Session-information window: Scroll information-views per pixel.
    svn:sync-xref-src-repo-rev: r109127

--- src/VBox/Frontends/VirtualBox/src/runtime/information/UIInformationView.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/information/UIInformationView.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIInformationView.cpp 60021 2016-03-14 14:50:57Z noreply@oracle.com $ */
+/* $Id: UIInformationView.cpp 62558 2016-07-26 11:46:43Z noreply@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIInformationView class implementation.
  */
@@ -38,6 +38,8 @@ UIInformationView::UIInformationView(QWidget *pParent)
     m_pTextEdit->setVisible(false);
     /* Set selection mode: */
     setSelectionMode(QAbstractItemView::ExtendedSelection);
+    /* Set scrolling mode to per pixel: */
+    setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
 }
 
 void UIInformationView::updateData(const QModelIndex &topLeft, const QModelIndex &bottomRight)
