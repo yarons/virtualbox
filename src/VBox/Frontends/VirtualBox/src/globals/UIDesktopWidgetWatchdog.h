646f6fb8a57766171eecb59eb61bf7e1a6eccfba
Sergey Dubov <sergey.dubov@oracle.com>
2016-08-04 13:58:32 +0000

    FE/Qt: bugref:8422: Desktop-widget watchdog rework (part 05): Qt5 migration, e.g. QDesktopWidget screen count change signal is no more reliable (at least under XCB), using QApplication screen add/remove signals instead.
    svn:sync-xref-src-repo-rev: r109599

--- src/VBox/Frontends/VirtualBox/src/globals/UIDesktopWidgetWatchdog.h
+++ src/VBox/Frontends/VirtualBox/src/globals/UIDesktopWidgetWatchdog.h
@@ -1,4 +1,4 @@
-/* $Id: UIDesktopWidgetWatchdog.h 62989 2016-08-04 13:50:58Z sergey.dubov@oracle.com $ */
+/* $Id: UIDesktopWidgetWatchdog.h 62990 2016-08-04 13:58:32Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIDesktopWidgetWatchdog class declaration.
  */
@@ -25,6 +25,9 @@
 
 /* Forward declarations: */
 class QDesktopWidget;
+#if QT_VERSION >= 0x050000
+class QScreen;
+#endif /* QT_VERSION >= 0x050000 */
 
 /** QObject extension used as
   * a desktop-widget watchdog aware
@@ -49,8 +52,15 @@ public:
 
 private slots:
 
+#if QT_VERSION < 0x050000
     /** Handles host-screen count change to @a cHostScreenCount. */
     void sltHandleHostScreenCountChanged(int cHostScreenCount);
+#else /* QT_VERSION >= 0x050000 */
+    /** Handles @a pHostScreen adding. */
+    void sltHostScreenAdded(QScreen *pHostScreen);
+    /** Handles @a pHostScreen removing. */
+    void sltHostScreenRemoved(QScreen *pHostScreen);
+#endif /* QT_VERSION >= 0x050000 */
 
     /** Handles resize for the host-screen with @a iHostScreenIndex. */
     void sltHandleHostScreenResized(int iHostScreenIndex);
