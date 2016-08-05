24442d48064f0647bd48d904f922d775f632f98a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-08-05 22:53:36 +0000

    GA/NT/SharedFolders: warnings
    svn:sync-xref-src-repo-rev: r109691

--- src/VBox/Additions/WINNT/SharedFolders/driver/info.c
+++ src/VBox/Additions/WINNT/SharedFolders/driver/info.c
@@ -1,4 +1,4 @@
-/* $Id: info.c 62522 2016-07-22 19:17:25Z knut.osmundsen@oracle.com $ */
+/* $Id: info.c 63077 2016-08-05 22:53:36Z knut.osmundsen@oracle.com $ */
 /** @file
  *
  * VirtualBox Windows Guest Shared Folders
@@ -1470,10 +1470,11 @@ NTSTATUS VBoxMRxSetFileInfo(IN PRX_CONTEXT RxContext)
 
         case FilePositionInformation:
         {
+#ifdef LOG_ENABLED
             PFILE_POSITION_INFORMATION pInfo = (PFILE_POSITION_INFORMATION)pInfoBuffer;
-
             Log(("VBOXSF: MrxSetFileInfo: FilePositionInformation: CurrentByteOffset = 0x%RX64. Unsupported!\n",
                  pInfo->CurrentByteOffset.QuadPart));
+#endif
 
             Status = STATUS_INVALID_PARAMETER;
             break;
@@ -1522,10 +1523,11 @@ NTSTATUS VBoxMRxSetFileInfo(IN PRX_CONTEXT RxContext)
 
         case FileLinkInformation:
         {
+#ifdef LOG_ENABLED
             PFILE_LINK_INFORMATION pInfo = (PFILE_LINK_INFORMATION )pInfoBuffer;
-
             Log(("VBOXSF: MrxSetFileInfo: FileLinkInformation: ReplaceIfExists = %d, RootDirectory = 0x%x = [%.*ls]. Not implemented!\n",
                  pInfo->ReplaceIfExists, pInfo->RootDirectory, pInfo->FileNameLength / sizeof(WCHAR), pInfo->FileName));
+#endif
 
             Status = STATUS_NOT_IMPLEMENTED;
             break;
@@ -1533,10 +1535,11 @@ NTSTATUS VBoxMRxSetFileInfo(IN PRX_CONTEXT RxContext)
 
         case FileRenameInformation:
         {
+#ifdef LOG_ENABLED
             PFILE_RENAME_INFORMATION pInfo = (PFILE_RENAME_INFORMATION)pInfoBuffer;
-
             Log(("VBOXSF: MrxSetFileInfo: FileRenameInformation: ReplaceIfExists = %d, RootDirectory = 0x%x = [%.*ls]\n",
                  pInfo->ReplaceIfExists, pInfo->RootDirectory, pInfo->FileNameLength / sizeof(WCHAR), pInfo->FileName));
+#endif
 
             Status = vbsfRename(RxContext, FileRenameInformation, pInfoBuffer, RxContext->Info.Length);
             break;
@@ -1559,6 +1562,7 @@ end:
 
 NTSTATUS VBoxMRxSetFileInfoAtCleanup(IN PRX_CONTEXT RxContext)
 {
+    RT_NOREF(RxContext);
     Log(("VBOXSF: MRxSetFileInfoAtCleanup\n"));
     return STATUS_SUCCESS;
 }
