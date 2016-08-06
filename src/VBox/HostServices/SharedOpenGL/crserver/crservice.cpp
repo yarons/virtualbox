e69fd96b9c4b645a582a720fd518db7a7d5a6e2e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-08-06 21:19:50 +0000

    warnings
    svn:sync-xref-src-repo-rev: r109736

--- src/VBox/HostServices/SharedOpenGL/crserver/crservice.cpp
+++ src/VBox/HostServices/SharedOpenGL/crserver/crservice.cpp
@@ -1,4 +1,4 @@
-/* $Id: crservice.cpp 63030 2016-08-05 10:46:47Z knut.osmundsen@oracle.com $ */
+/* $Id: crservice.cpp 63120 2016-08-06 21:19:50Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox crOpenGL - Host service entry points.
  */
@@ -1502,9 +1502,9 @@ static DECLCALLBACK(int) svcHostCall(void *, uint32_t u32Function, uint32_t cPar
                     WARN(("svcHostCall: invalid function %d", pCtl->enmType));
                     return VERR_INVALID_PARAMETER;
             }
-            WARN(("should not be here!"));
-            return VERR_INTERNAL_ERROR;
+            /* not reached. */
         }
+
         default:
             if (g_u32fCrHgcmDisabled)
             {
