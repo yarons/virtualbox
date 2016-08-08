dad9c196d35dde481450931026e6b11067239cda
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-08-08 17:03:00 +0000

    build fix
    svn:sync-xref-src-repo-rev: r109804

--- src/VBox/Main/src-client/GuestDnDSourceImpl.cpp
+++ src/VBox/Main/src-client/GuestDnDSourceImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestDnDSourceImpl.cpp 63183 2016-08-08 16:37:26Z knut.osmundsen@oracle.com $ */
+/* $Id: GuestDnDSourceImpl.cpp 63185 2016-08-08 17:03:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation - Guest drag and drop source.
  */
@@ -1065,7 +1065,7 @@ int GuestDnDSource::i_receiveData(PRECVDATACTX pCtx, RTMSINTERVAL msTimeout)
 }
 
 /* static */
-DECLCALLBACK(void) GuestDnDSource::i_receiveDataThreadTask(RecvDataTask *pTask)
+void GuestDnDSource::i_receiveDataThreadTask(RecvDataTask *pTask)
 {
     LogFlowFunc(("pTask=%p\n", pTask));
     AssertPtrReturnVoid(pTask);
