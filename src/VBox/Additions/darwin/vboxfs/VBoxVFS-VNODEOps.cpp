ddcff06a6663d982f7a56cab5c9c284be4a0a5a8
Frank Mehnert <noreply@oracle.com>
2016-08-16 08:44:20 +0000

    fixed headers
    svn:sync-xref-src-repo-rev: r110180

--- src/VBox/Additions/darwin/vboxfs/VBoxVFS-VNODEOps.cpp
+++ src/VBox/Additions/darwin/vboxfs/VBoxVFS-VNODEOps.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxVFS-VNODEOps.cpp 62523 2016-07-22 19:17:44Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxVFS-VNODEOps.cpp 63526 2016-08-16 08:44:20Z noreply@oracle.com $ */
 /** @file
  * VBoxVFS - vnode operations.
  */
@@ -13,15 +13,6 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
- *
- * The contents of this file may alternatively be used under the terms
- * of the Common Development and Distribution License Version 1.0
- * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
- * VirtualBox OSE distribution, in which case the provisions of the
- * CDDL are applicable instead of those of the GPL.
- *
- * You may elect to license modified versions of this file under the
- * terms and conditions of either the GPL or the CDDL or both.
  */
 
 #include <sys/mount.h>
