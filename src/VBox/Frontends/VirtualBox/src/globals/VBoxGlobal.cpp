154df65c1e40a662c7286a19637c404ab3d2ad3f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-09-02 12:42:46 +0000

    revert previous commit (r110452).
    svn:sync-xref-src-repo-rev: r110454

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGlobal.cpp 63692 2016-09-02 12:41:41Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGlobal.cpp 63694 2016-09-02 12:42:46Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Qt GUI - VBoxGlobal class implementation.
  */
@@ -4388,7 +4388,6 @@ void VBoxGlobal::cleanup()
         /* First, make sure we don't use COM any more: */
         m_host.detach();
         m_vbox.detach();
-__debugbreak();
         m_client.detach();
 
         /* There may be UIMedium(s)EnumeratedEvent instances still in the message
