2bbd9ffc436d9631428d420883dddd5a84243294
Alexander Eichner <alexander.eichner@oracle.com>
2016-09-26 17:41:58 +0000

    AHCI: uint32_t vs. size_t issues, hopefully fixes windows buildboxes
    svn:sync-xref-src-repo-rev: r110938

--- src/VBox/Devices/build/VBoxDD.d
+++ src/VBox/Devices/build/VBoxDD.d
@@ -1,4 +1,4 @@
-/* $Id: VBoxDD.d 64016 2016-09-26 15:14:48Z alexander.eichner@oracle.com $ */
+/* $Id: VBoxDD.d 64021 2016-09-26 17:41:58Z alexander.eichner@oracle.com $ */
 /** @file
  * VBoxDD - Static dtrace probes
  */
@@ -22,8 +22,8 @@ provider vboxdd
     probe hgcmcall__completed__emt(void *pvCmd, int rc);
     probe hgcmcall__completed__done(void *pvCmd, unsigned int idFunction, unsigned int idClient, int rc);
 
-    probe ahci__req__submit(void *pvReq, int iTxDir, uint64_t offStart, uint32_t cbXfer);
-    probe ahci__req__completed(void *pvReq, int rcReq, uint64_t offStart, uint32_t cbXfer);
+    probe ahci__req__submit(void *pvReq, int iTxDir, uint64_t offStart, uint64_t cbXfer);
+    probe ahci__req__completed(void *pvReq, int rcReq, uint64_t offStart, uint64_t cbXfer);
 };
 
 #pragma D attributes Evolving/Evolving/Common provider vboxdd provider
