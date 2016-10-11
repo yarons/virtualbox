fc56216f714a601c6cff1e0eaec0245936255e53
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2016-10-11 10:26:18 +0000

    NetFlt/Linux (bugref:8599) One more build fix for r111185
    svn:sync-xref-src-repo-rev: r111193

--- src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
+++ src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFlt-linux.c 64201 2016-10-11 09:17:10Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxNetFlt-linux.c 64202 2016-10-11 10:26:18Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Linux Specific Code.
  */
@@ -816,7 +816,11 @@ DECLINLINE(unsigned) vboxNetFltLinuxGetChecksumStartOffset(struct sk_buff *pBuf)
 #  endif /* LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 18) */
     return pTransportHdr - pBuf->data;
 # else /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 21) */
+#  if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 38)
+    return pBuf->csum_start - skb_headroom(pBuf)
+#  else /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 38) */
     return skb_checksum_start_offset(pBuf);
+#  endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 38) */
 # endif /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 21) */
 }
 
