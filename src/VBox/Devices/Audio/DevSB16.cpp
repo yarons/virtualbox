9d371fac080857e372ac909a1dc9e1f0fda57704
Andreas Loeffler <andreas.loeffler@oracle.com>
2016-10-20 14:23:29 +0000

    Audio: Match to 5.1 in terms of functionality by disabling backend-independent device enumeration and disabling generic backend callback support for now.
    svn:sync-xref-src-repo-rev: r111425

--- src/VBox/Devices/Audio/DevSB16.cpp
+++ src/VBox/Devices/Audio/DevSB16.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevSB16.cpp 63737 2016-09-06 08:43:12Z andreas.loeffler@oracle.com $ */
+/* $Id: DevSB16.cpp 64333 2016-10-20 14:23:29Z andreas.loeffler@oracle.com $ */
 /** @file
  * DevSB16 - VBox SB16 Audio Controller.
  */
@@ -2418,6 +2418,36 @@ static DECLCALLBACK(int) sb16Construct(PPDMDEVINS pDevIns, int iInstance, PCFGMN
     LogFunc(("cLUNs=%RU8, rc=%Rrc\n", uLUN, rc));
 
     sb16ResetLegacy(pThis);
+
+#ifdef VBOX_WITH_AUDIO_SB16_ONETIME_INIT
+    PSB16DRIVER pDrv;
+    RTListForEach(&pThis->lstDrv, pDrv, SB16DRIVER, Node)
+    {
+        /*
+         * Only primary drivers are critical for the VM to run. Everything else
+         * might not worth showing an own error message box in the GUI.
+         */
+        if (!(pDrv->Flags & PDMAUDIODRVFLAGS_PRIMARY))
+            continue;
+
+        PPDMIAUDIOCONNECTOR pCon = pDrv->pConnector;
+        AssertPtr(pCon);
+
+        /** @todo No input streams available for SB16 yet. */
+        bool fValidOut = pCon->pfnStreamGetStatus(pCon, pDrv->Out.pStream) & PDMAUDIOSTRMSTS_FLAG_INITIALIZED;
+        if (!fValidOut)
+        {
+            LogRel(("SB16: Falling back to NULL backend (no sound audible)\n"));
+
+            sb16ResetLegacy(pThis);
+            sb16Reattach(pThis, pDrv, pDrv->uLUN, "NullAudio");
+
+            PDMDevHlpVMSetRuntimeError(pDevIns, 0 /*fFlags*/, "HostAudioNotResponding",
+                N_("No audio devices could be opened. Selecting the NULL audio backend "
+                   "with the consequence that no sound is audible"));
+        }
+    }
+#endif
 
 #ifndef VBOX_WITH_AUDIO_SB16_CALLBACKS
     if (RT_SUCCESS(rc))
