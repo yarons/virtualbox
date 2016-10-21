1fddae2683486c1eeb8c463454cdb34bb84bc3be
Andreas Loeffler <andreas.loeffler@oracle.com>
2016-10-21 10:04:51 +0000

    Audio/AudioMixer.cpp: Do not decrement sinks reference count in audioMixerRemoveSinkInternal().
    svn:sync-xref-src-repo-rev: r111447

--- src/VBox/Devices/Audio/AudioMixer.cpp
+++ src/VBox/Devices/Audio/AudioMixer.cpp
@@ -1,4 +1,4 @@
-/* $Id: AudioMixer.cpp 64048 2016-09-27 13:53:54Z andreas.loeffler@oracle.com $ */
+/* $Id: AudioMixer.cpp 64348 2016-10-21 10:04:51Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBox audio: Mixing routines, mainly used by the various audio device
  *             emulations to achieve proper multiplexing from/to attached
@@ -338,7 +338,6 @@ static int audioMixerRemoveSinkInternal(PAUDIOMIXER pMixer, PAUDMIXSINK pSink)
     /* Remove sink from mixer. */
     RTListNodeRemove(&pSink->Node);
     Assert(pMixer->cSinks);
-    pMixer->cSinks--;
 
     /* Set mixer to NULL so that we know we're not part of any mixer anymore. */
     pSink->pParent = NULL;
@@ -709,9 +708,10 @@ void AudioMixerSinkDestroy(PAUDMIXSINK pSink)
 
     if (pSink->pParent)
     {
-        /* Save sink pointer, as after audioMixerRemoveSinkInternal() the
+        /* Save mixer pointer, as after audioMixerRemoveSinkInternal() the
          * pointer will be gone from the stream. */
         PAUDIOMIXER pMixer = pSink->pParent;
+        AssertPtr(pMixer);
 
         audioMixerRemoveSinkInternal(pMixer, pSink);
 
