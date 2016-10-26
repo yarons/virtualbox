4ef78452f8255b1c5b766ec85302b119f0256d1c
Alexey Strelnikov <noreply@oracle.com>
2016-10-26 15:43:51 +0000

    Added extra audio logging environment var.
    svn:sync-xref-src-repo-rev: r111555

--- src/VBox/ValidationKit/testdriver/vbox.py
+++ src/VBox/ValidationKit/testdriver/vbox.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: vbox.py 64332 2016-10-20 14:03:29Z noreply@oracle.com $
+# $Id: vbox.py 64431 2016-10-26 15:43:51Z noreply@oracle.com $
 # pylint: disable=C0302
 
 """
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 64332 $"
+__version__ = "$Revision: 64431 $"
 
 
 # Standard Python imports.
@@ -2423,6 +2423,8 @@ class TestDriver(base.TestDriver):                                              
             sLogDest = 'file=%s' % sLogFile;
         sEnv = 'VBOX_LOG=%s\nVBOX_LOG_FLAGS=%s\nVBOX_LOG_DEST=%s\nVBOX_RELEASE_LOG_FLAGS=append time' \
              % (self.sLogSessionGroups, self.sLogSessionFlags, sLogDest,);
+        # Extra audio logging
+        sEnv += '\nVBOX_RELEASE_LOG=drv_audio.e.l.l2+drv_host_audio.e.l.l2'
         if sType == 'gui':
             sEnv += '\nVBOX_GUI_DBG_ENABLED=1'
         if asEnv is not None and len(asEnv) > 0:
