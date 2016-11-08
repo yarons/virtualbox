63d464974f6e4e7b344794902e297b6341190614
Sergey Dubov <sergey.dubov@oracle.com>
2016-11-08 18:14:23 +0000

    FE/Qt: bugref:8657: Improve handling of Install GA ISO Image action: GUI should try to mount ISO into free optical drive first (if exist), then to busy drive (otherwise), but never propose force mounting.
    svn:sync-xref-src-repo-rev: r111808

--- src/VBox/Frontends/VirtualBox/src/globals/UIDefs.h
+++ src/VBox/Frontends/VirtualBox/src/globals/UIDefs.h
@@ -1,4 +1,4 @@
-/* $Id: UIDefs.h 62493 2016-07-22 18:44:18Z knut.osmundsen@oracle.com $ */
+/* $Id: UIDefs.h 64604 2016-11-08 18:14:23Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - Global definitions.
  */
@@ -135,5 +135,16 @@ struct StorageSlot
     KStorageBus bus; LONG port; LONG device;
 };
 Q_DECLARE_METATYPE(StorageSlot);
+
+/** Common UI: Storage-slot struct extension with exact controller name. */
+struct ExactStorageSlot : public StorageSlot
+{
+    ExactStorageSlot(const QString &strController,
+                     KStorageBus enmBus, LONG iPort, LONG iDevice)
+        : StorageSlot(enmBus, iPort, iDevice)
+        , controller(strController)
+    {}
+    QString controller;
+};
 
 #endif /* !___UIDefs_h___ */
