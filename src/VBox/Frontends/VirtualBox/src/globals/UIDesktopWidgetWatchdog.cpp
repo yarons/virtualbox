f64276d1ec5d227e5f7f10831b93bcdd8e16be50
Sergey Dubov <sergey.dubov@oracle.com>
2016-11-09 18:30:50 +0000

    FE/Qt: Burn fix for r111839.
    svn:sync-xref-src-repo-rev: r111843

--- src/VBox/Frontends/VirtualBox/src/globals/UIDesktopWidgetWatchdog.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIDesktopWidgetWatchdog.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIDesktopWidgetWatchdog.cpp 64621 2016-11-09 17:55:45Z sergey.dubov@oracle.com $ */
+/* $Id: UIDesktopWidgetWatchdog.cpp 64624 2016-11-09 18:30:50Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIDesktopWidgetWatchdog class implementation.
  */
@@ -34,6 +34,7 @@
 
 /* Other VBox includes: */
 # include <iprt/assert.h>
+# include <VBox/log.h>
 
 #endif /* !VBOX_WITH_PRECOMPILED_HEADERS */
 
