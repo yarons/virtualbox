581806ee13bd663485b15d5b242a14f5ea9479a3
Sergey Dubov <sergey.dubov@oracle.com>
2016-11-10 15:46:51 +0000

    Revert r111868 as it's still required for full-screen mode on Ubuntu 15.10.
    svn:sync-xref-src-repo-rev: r111875

--- src/VBox/Frontends/VirtualBox/src/widgets/UIMiniToolBar.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIMiniToolBar.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMiniToolBar.cpp 64636 2016-11-10 15:19:51Z sergey.dubov@oracle.com $ */
+/* $Id: UIMiniToolBar.cpp 64642 2016-11-10 15:46:51Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMiniToolBar class implementation.
  */
@@ -392,6 +392,16 @@ void UIMiniToolBarPrivate::rebuildShape()
 /* static */
 Qt::WindowFlags UIMiniToolBar::defaultWindowFlags()
 {
+#ifdef VBOX_WS_X11
+    /* Depending on current WM: */
+    switch (vboxGlobal().typeOfWindowManager())
+    {
+        /* Frameless top-level window for Unity, issues with tool window there.. */
+        case X11WMType_Compiz: return Qt::Window | Qt::FramelessWindowHint;
+        default: break;
+    }
+#endif /* VBOX_WS_X11 */
+
     /* Frameless tool window by default: */
     return Qt::Tool | Qt::FramelessWindowHint;
 }
