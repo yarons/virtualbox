9d6fac264b398bc44615565ae58a680e25e49631
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-12-15 15:29:23 +0000

    RTSignTool: Build fixes
    svn:sync-xref-src-repo-rev: r112291

--- src/VBox/Runtime/tools/RTSignTool.cpp
+++ src/VBox/Runtime/tools/RTSignTool.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTSignTool.cpp 64883 2016-12-15 15:26:20Z knut.osmundsen@oracle.com $ */
+/* $Id: RTSignTool.cpp 64884 2016-12-15 15:29:23Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Signing Tool.
  */
@@ -502,7 +502,7 @@ static RTEXITCODE HandleAddNestedExeSignature(int cArgs, char **papszArgs)
     {
         /* Ditto for the destination PKCS#7 signature. */
         SIGNTOOLPKCS7EXE Dst;
-        RTEXITCODE rcExit = SignToolPkcs7Exe_InitFromFile(&Dst, pszSrc, cVerbosity);
+        rcExit = SignToolPkcs7Exe_InitFromFile(&Dst, pszSrc, cVerbosity);
         if (rcExit == RTEXITCODE_SUCCESS)
         {
             /* Add the source to the destination as a nested signature. */
@@ -1805,8 +1805,8 @@ static struct
 const g_aCommands[] =
 {
     { "extract-exe-signer-cert",        HandleExtractExeSignerCert,         HelpExtractExeSignerCert },
-#ifndef IPRT_IN_BUILD_TOOL
     { "add-nested-exe-signature",       HandleAddNestedExeSignature,        HelpAddNestedExeSignature },
+#ifndef IPRT_IN_BUILD_TOOL
     { "verify-exe",                     HandleVerifyExe,                    HelpVerifyExe },
     { "show-exe",                       HandleShowExe,                      HelpShowExe },
 #endif
