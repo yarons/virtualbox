6ea8e7c05e4b9fead225595faf56b8ad0087cd53
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2016-12-16 13:01:20 +0000

    VBoxManage: limit keyboardputscancode 'typing' rate. Simple commands to 'type' ASCII chars.
    svn:sync-xref-src-repo-rev: r112316

--- src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageHelp.cpp 64434 2016-10-27 11:38:06Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxManageHelp.cpp 64906 2016-12-16 13:01:20Z vitali.pelenjow@oracle.com $ */
 /** @file
  * VBoxManage - help and other message output.
  */
@@ -960,6 +960,8 @@ void printUsage(USAGECATEGORY fCategory, uint32_t fSubCategory, PRTSTREAM pStrm)
                      "                            pause|resume|reset|poweroff|savestate|\n"
                      "                            acpipowerbutton|acpisleepbutton|\n"
                      "                            keyboardputscancode <hex> [<hex> ...]|\n"
+                     "                            keyboardputstring <string1> [<string2> ...]|\n"
+                     "                            keyboardputfile <filename>|\n"
                      "                            setlinkstate<1-N> on|off |\n"
 #if defined(VBOX_WITH_NETFLT)
                      "                            nic<1-N> null|nat|bridged|intnet|hostonly|generic|\n"
