c4205661a09a34a5cc0d4dc274969cdf03e0eea4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2016-12-17 02:44:10 +0000

    vsheriff: check for 'Error opening VBoxDrvStub:  STATUS_OBJECT_NAME_NOT_FOUND' in VBoxHardening.log
    svn:sync-xref-src-repo-rev: r112359

--- src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
+++ src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: virtual_test_sheriff.py 64945 2016-12-17 02:42:22Z knut.osmundsen@oracle.com $
+# $Id: virtual_test_sheriff.py 64946 2016-12-17 02:44:10Z knut.osmundsen@oracle.com $
 # pylint: disable=C0301
 
 """
@@ -33,7 +33,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 64945 $"
+__version__ = "$Revision: 64946 $"
 
 
 # Standard python imports
@@ -293,7 +293,7 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
 
         if self.oConfig.sLogFile is not None and len(self.oConfig.sLogFile) > 0:
             self.oLogFile = open(self.oConfig.sLogFile, "a");
-            self.oLogFile.write('VirtualTestSheriff: $Revision: 64945 $ \n');
+            self.oLogFile.write('VirtualTestSheriff: $Revision: 64946 $ \n');
 
 
     def eprint(self, sText):
@@ -520,7 +520,7 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
         for idTestResult, tReason in dReasonForResultId.items():
             oFailureReason = self.oFailureReasonLogic.cachedLookupByNameAndCategory(tReason[1], tReason[0]);
             if oFailureReason is not None:
-                sComment = 'Set by $Revision: 64945 $' # Handy for reverting later.
+                sComment = 'Set by $Revision: 64946 $' # Handy for reverting later.
                 if idTestResult in dCommentForResultId:
                     sComment += ': ' + dCommentForResultId[idTestResult];
 
@@ -747,8 +747,6 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
                             len(sKrnlLog)   if sKrnlLog is not None else 0,
                             len(sVgaText)   if sVgaText is not None else 0,
                             len(sInfoText)  if sInfoText is not None else 0, ));
-            if sVMLog is None:
-                sVMLog = '';
 
             #self.dprint(u'main.log<<<\n%s\n<<<\n' % (sResultLog,));
             #self.dprint(u'vbox.log<<<\n%s\n<<<\n' % (sVMLog,));
@@ -788,7 +786,7 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
             #
             fFoundSomething = False;
             for fStopOnHit, tReason, sNeedle in self.katSimpleMainAndVmLogReasons:
-                if sResultLog.find(sNeedle) > 0 or sVMLog.find(sNeedle) > 0:
+                if sResultLog.find(sNeedle) > 0 or (sVMLog is not None and sVMLog.find(sNeedle) > 0):
                     oCaseFile.noteReasonForId(tReason, oFailedResult.idTestResult);
                     if fStopOnHit:
                         return True;
@@ -825,10 +823,11 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
             #
             # Check for repeated reboots...
             #
-            cResets = sVMLog.count('Changing the VM state from \'RUNNING\' to \'RESETTING\'');
-            if cResets > 10:
-                return oCaseFile.noteReasonForId(self.ktReason_Unknown_Reboot_Loop, oFailedResult.idTestResult,
-                                                 sComment = 'Counted %s reboots' % (cResets,));
+            if sVMLog is not None:
+                cResets = sVMLog.count('Changing the VM state from \'RUNNING\' to \'RESETTING\'');
+                if cResets > 10:
+                    return oCaseFile.noteReasonForId(self.ktReason_Unknown_Reboot_Loop, oFailedResult.idTestResult,
+                                                     sComment = 'Counted %s reboots' % (cResets,));
 
             return fFoundSomething;
 
