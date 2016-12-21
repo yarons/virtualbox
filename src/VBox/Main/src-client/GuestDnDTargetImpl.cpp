3caccec4752c0e2c2af400f4b8716d882be2ac15
Frank Mehnert <noreply@oracle.com>
2016-12-21 12:52:53 +0000

    build fix
    svn:sync-xref-src-repo-rev: r112458

--- src/VBox/Main/src-client/GuestDnDTargetImpl.cpp
+++ src/VBox/Main/src-client/GuestDnDTargetImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestDnDTargetImpl.cpp 64977 2016-12-21 12:50:29Z noreply@oracle.com $ */
+/* $Id: GuestDnDTargetImpl.cpp 64978 2016-12-21 12:52:53Z noreply@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation - Guest drag'n drop target.
  */
@@ -571,6 +571,7 @@ void GuestDnDTarget::i_sendDataThreadTask(SendDataTask *pTask)
         return;
 
     int vrc = pThis->i_sendData(pTask->getCtx(), RT_INDEFINITE_WAIT /* msTimeout */);
+    NOREF(vrc);
 /** @todo
  *
  *  r=bird: What happens with @a vrc?
