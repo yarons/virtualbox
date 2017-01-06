3bbe2f95b03300a33a25c4d8511d0b93344e75ad
Andreas Loeffler <andreas.loeffler@oracle.com>
2017-01-06 09:56:08 +0000

    VideoRec: Register audio driver for video recording.
    svn:sync-xref-src-repo-rev: r112674

--- src/VBox/Main/src-client/VBoxDriversRegister.cpp
+++ src/VBox/Main/src-client/VBoxDriversRegister.cpp
@@ -1,11 +1,11 @@
-/* $Id: VBoxDriversRegister.cpp 61157 2016-05-24 11:47:09Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxDriversRegister.cpp 65171 2017-01-06 09:56:08Z andreas.loeffler@oracle.com $ */
 /** @file
  *
  * Main driver registration.
  */
 
 /*
- * Copyright (C) 2006-2016 Oracle Corporation
+ * Copyright (C) 2006-2017 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -26,6 +26,9 @@
 #include "VMMDev.h"
 #ifdef VBOX_WITH_VRDE_AUDIO
 # include "DrvAudioVRDE.h"
+#endif
+#ifdef VBOX_WITH_AUDIO_VIDEOREC
+# include "DrvAudioVideoRec.h"
 #endif
 #include "Nvram.h"
 #include "UsbWebcamInterface.h"
@@ -71,6 +74,11 @@ extern "C" DECLEXPORT(int) VBoxDriversRegister(PCPDMDRVREGCB pCallbacks, uint32_
         return rc;
 #ifdef VBOX_WITH_VRDE_AUDIO
     rc = pCallbacks->pfnRegister(pCallbacks, &AudioVRDE::DrvReg);
+    if (RT_FAILURE(rc))
+        return rc;
+#endif
+#ifdef VBOX_WITH_AUDIO_VIDEOREC
+    rc = pCallbacks->pfnRegister(pCallbacks, &AudioVideoRec::DrvReg);
     if (RT_FAILURE(rc))
         return rc;
 #endif
