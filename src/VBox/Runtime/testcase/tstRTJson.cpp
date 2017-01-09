6b13a286dc6bdc9a4fd4bc6b05c8aa32c49edcdd
Alexander Eichner <alexander.eichner@oracle.com>
2017-01-09 12:42:54 +0000

    Runtime/tstRTJson: Fix todo
    svn:sync-xref-src-repo-rev: r112708

--- src/VBox/Runtime/testcase/tstRTJson.cpp
+++ src/VBox/Runtime/testcase/tstRTJson.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstRTJson.cpp 62727 2016-07-30 00:20:48Z knut.osmundsen@oracle.com $ */
+/* $Id: tstRTJson.cpp 65202 2017-01-09 12:42:54Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT Testcase - JSON API.
  */
@@ -100,8 +100,9 @@ static void tstBasic(RTTEST hTest)
  */
 static void tstCorrectnessRcForInvalidType(RTTEST hTest, RTJSONVAL hJsonVal, RTJSONVALTYPE enmType)
 {
-#ifndef RT_STRICT /* Enable manually if assertions are enabled or it will assert all over the place for debug builds. */
-/** @todo you can disable assertions and all the noise. See RTAssertSetMayPanic and RTAssertSetQuiet. */
+    bool fSavedMayPanic = RTAssertSetMayPanic(false);
+    bool fSavedQuiet    = RTAssertSetQuiet(true);
+
     if (   enmType != RTJSONVALTYPE_OBJECT
         && enmType != RTJSONVALTYPE_ARRAY)
     {
@@ -139,9 +140,9 @@ static void tstCorrectnessRcForInvalidType(RTTEST hTest, RTJSONVAL hJsonVal, RTJ
         RTTEST_CHECK(hTest, RTJsonValueGetString(hJsonVal) == NULL);
         RTTEST_CHECK_RC(hTest, RTJsonValueQueryString(hJsonVal, &psz), VERR_JSON_VALUE_INVALID_TYPE);
     }
-#else
-    RT_NOREF3(hTest, hJsonVal, enmType);
-#endif
+
+    RTAssertSetMayPanic(fSavedMayPanic);
+    RTAssertSetQuiet(fSavedQuiet);
 }
 
 /**
