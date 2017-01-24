da0b65ce4c568ba7dfa3d119b8f990a9e9b2ea52
Andreas Loeffler <andreas.loeffler@oracle.com>
2017-01-24 10:20:58 +0000

    VideoRec: Renaming.
    svn:sync-xref-src-repo-rev: r113021

--- src/VBox/Main/src-client/VideoRec.h
+++ src/VBox/Main/src-client/VideoRec.h
@@ -1,4 +1,4 @@
-/* $Id: VideoRec.h 65410 2017-01-24 10:06:12Z andreas.loeffler@oracle.com $ */
+/* $Id: VideoRec.h 65412 2017-01-24 10:20:58Z andreas.loeffler@oracle.com $ */
 /** @file
  * Encodes the screen content in VPX format.
  */
@@ -32,11 +32,11 @@ int  VideoRecStreamInit(PVIDEORECCONTEXT pCtx, uint32_t uScreen, const char *psz
                         uint32_t uMaxTime, uint32_t uMaxFileSize, const char *pszOptions);
 
 bool VideoRecIsEnabled(PVIDEORECCONTEXT pCtx);
-int  VideoRecSendAudio(PVIDEORECCONTEXT pCtx, const void *pvData, size_t cbData, uint64_t uTimestampMs);
-int  VideoRecCopyToIntBuf(PVIDEORECCONTEXT pCtx, uint32_t uScreen,
-                          uint32_t x, uint32_t y, uint32_t uPixelFormat, uint32_t uBitsPerPixel,
-                          uint32_t uBytesPerLine, uint32_t uGuestWidth, uint32_t uGuestHeight,
-                          uint8_t *pu8BufferAddress, uint64_t u64TimeStampMs);
+int  VideoRecSendAudioFrame(PVIDEORECCONTEXT pCtx, const void *pvData, size_t cbData, uint64_t uTimestampMs);
+int  VideoRecSendVideoFrame(PVIDEORECCONTEXT pCtx, uint32_t uScreen,
+                            uint32_t x, uint32_t y, uint32_t uPixelFormat, uint32_t uBitsPerPixel,
+                            uint32_t uBytesPerLine, uint32_t uGuestWidth, uint32_t uGuestHeight,
+                            uint8_t *pu8BufferAddress, uint64_t u64TimeStampMs);
 bool VideoRecIsReady(PVIDEORECCONTEXT pCtx, uint32_t uScreen, uint64_t u64TimeStampMs);
 bool VideoRecLimitReached(PVIDEORECCONTEXT pCtx, uint32_t uScreen, uint64_t u64TimeStampMs);
 
