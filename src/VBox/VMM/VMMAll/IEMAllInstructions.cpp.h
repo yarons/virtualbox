aa3d54877be0c6f518c90b7a4cfa4a43f6fd60d3
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-01-29 17:34:57 +0000

    IEM: Enabled cmpxchg16b code.
    svn:sync-xref-src-repo-rev: r113143

--- src/VBox/VMM/VMMAll/IEMAllInstructions.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllInstructions.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllInstructions.cpp.h 65508 2017-01-29 17:33:21Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllInstructions.cpp.h 65509 2017-01-29 17:34:57Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Emulation.
  */
@@ -6832,7 +6832,7 @@ FNIEMOP_DEF_1(iemOp_Grp9_cmpxchg16b_Mdq, uint8_t, bRm)
     IEMOP_MNEMONIC(cmpxchg16b, "cmpxchg16b Mdq");
     if (IEM_GET_GUEST_CPU_FEATURES(pVCpu)->fMovCmpXchg16b)
     {
-#if 1
+#if 0
         RT_NOREF(bRm);
         IEMOP_BITCH_ABOUT_STUB();
         return VERR_IEM_INSTR_NOT_IMPLEMENTED;
