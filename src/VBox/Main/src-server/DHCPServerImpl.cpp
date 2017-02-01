b343beca8aab8e6d37afe9001f419520b3640983
Valery Ushakov <noreply@oracle.com>
2017-02-01 15:07:38 +0000

    Main/DHCPServerImpl: fix Windows #include fallout in r113221.
    svn:sync-xref-src-repo-rev: r113223

--- src/VBox/Main/src-server/DHCPServerImpl.cpp
+++ src/VBox/Main/src-server/DHCPServerImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: DHCPServerImpl.cpp 65569 2017-02-01 14:44:34Z noreply@oracle.com $ */
+/* $Id: DHCPServerImpl.cpp 65571 2017-02-01 15:07:38Z noreply@oracle.com $ */
 
 /** @file
  *
@@ -23,6 +23,7 @@
 #include "AutoCaller.h"
 #include "Logging.h"
 
+#include <iprt/asm.h>
 #include <iprt/net.h>
 #include <iprt/cpp/utils.h>
 
