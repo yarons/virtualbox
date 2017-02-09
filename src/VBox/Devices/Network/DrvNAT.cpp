075b5a59a1993dc2787bbd210528e99513c2d752
Frank Mehnert <noreply@oracle.com>
2017-02-09 14:22:43 +0000

    DrvNAT: missing RTReqQueueDestroy() call
    svn:sync-xref-src-repo-rev: r113408

--- src/VBox/Devices/Network/DrvNAT.cpp
+++ src/VBox/Devices/Network/DrvNAT.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvNAT.cpp 64166 2016-10-06 14:11:46Z noreply@oracle.com $ */
+/* $Id: DrvNAT.cpp 65700 2017-02-09 14:22:43Z noreply@oracle.com $ */
 /** @file
  * DrvNAT - NAT network transport driver.
  */
@@ -1477,6 +1477,9 @@ static DECLCALLBACK(void) drvNATDestruct(PPDMDRVINS pDrvIns)
 
     RTReqQueueDestroy(pThis->hUrgRecvReqQueue);
     pThis->hUrgRecvReqQueue = NIL_RTREQQUEUE;
+
+    RTReqQueueDestroy(pThis->hRecvReqQueue);
+    pThis->hRecvReqQueue = NIL_RTREQQUEUE;
 
     RTSemEventDestroy(pThis->EventRecv);
     pThis->EventRecv = NIL_RTSEMEVENT;
