6214e3c643b135618376cfb0e20133ebd9d9c019
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-02-13 17:55:00 +0000

    SUPHardNt: Fixed harmless typo. Will be relevant if with patch more functions.
    svn:sync-xref-src-repo-rev: r113506

--- src/VBox/HostDrivers/Support/win/SUPR3HardenedMain-win.cpp
+++ src/VBox/HostDrivers/Support/win/SUPR3HardenedMain-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPR3HardenedMain-win.cpp 64532 2016-11-03 14:02:39Z noreply@oracle.com $ */
+/* $Id: SUPR3HardenedMain-win.cpp 65782 2017-02-13 17:55:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Hardened main(), windows bits.
  */
@@ -2571,7 +2571,7 @@ static void supR3HardenedWinInstallHooks(void)
     *(uint32_t *)&g_abSupHardReadWriteExecPage[offExecPage] = RT_ALIGN_32(offExecPage + 4, 8) - (offExecPage + 4);
     offExecPage = RT_ALIGN_32(offExecPage + 4, 8);
     *(uint64_t *)&g_abSupHardReadWriteExecPage[offExecPage] = (uintptr_t)&pbLdrLoadDll[offJmpBack];
-    offExecPage = RT_ALIGN_32(offJmpBack + 8, 16);
+    offExecPage = RT_ALIGN_32(offExecPage + 8, 16);
 
     /* Assemble the LdrLoadDll patch. */
     Assert(offJmpBack >= 12);
@@ -2605,7 +2605,7 @@ static void supR3HardenedWinInstallHooks(void)
     g_abSupHardReadWriteExecPage[offExecPage++] = 0xe9; /* jmp rel32 */
     *(uint32_t *)&g_abSupHardReadWriteExecPage[offExecPage] = (uintptr_t)&pbLdrLoadDll[offJmpBack]
                                                             - (uintptr_t)&g_abSupHardReadWriteExecPage[offExecPage + 4];
-    offExecPage = RT_ALIGN_32(offJmpBack + 4, 16);
+    offExecPage = RT_ALIGN_32(offExecPage + 4, 16);
 
     /* Assemble the LdrLoadDll patch. */
     memcpy(g_abLdrLoadDllPatch, pbLdrLoadDll, sizeof(g_abLdrLoadDllPatch));
