59c32ba5eaa710bade3978932735b32fcefbb17f
Andreas Loeffler <andreas.loeffler@oracle.com>
2017-03-15 10:52:42 +0000

    Additions/VBoxCredProv: Fix for creating the Kerberos logon package. Untested.
    svn:sync-xref-src-repo-rev: r113990

--- src/VBox/Additions/WINNT/VBoxCredProv/VBoxCredProvCredential.h
+++ src/VBox/Additions/WINNT/VBoxCredProv/VBoxCredProvCredential.h
@@ -1,10 +1,10 @@
-/* $Id: VBoxCredProvCredential.h 63476 2016-08-15 14:02:31Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxCredProvCredential.h 66109 2017-03-15 10:52:42Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxCredProvCredential - Class for keeping and handling the passed credentials.
  */
 
 /*
- * Copyright (C) 2012-2016 Oracle Corporation
+ * Copyright (C) 2012-2017 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -94,9 +94,13 @@ public:
 
 protected:
     HRESULT RTUTF16ToUnicode(PUNICODE_STRING pUnicodeDest, PRTUTF16 pwszSource, bool fCopy);
-    HRESULT kerberosLogonInit(KERB_INTERACTIVE_LOGON *pLogonIn,
-                              CREDENTIAL_PROVIDER_USAGE_SCENARIO enmUsage,
-                              PRTUTF16 pwszUser, PRTUTF16 pwszPassword, PRTUTF16 pwszDomain);
+    HRESULT RTUTF16ToUnicodeA(PUNICODE_STRING pUnicodeDest, PRTUTF16 pwszSource);
+    void UnicodeStringFree(PUNICODE_STRING pUnicode);
+
+    HRESULT kerberosLogonCreate(KERB_INTERACTIVE_LOGON *pLogon,
+                                CREDENTIAL_PROVIDER_USAGE_SCENARIO enmUsage,
+                                PRTUTF16 pwszUser, PRTUTF16 pwszPassword, PRTUTF16 pwszDomain);
+    void    kerberosLogonDestroy(KERB_INTERACTIVE_LOGON *pLogon);
     HRESULT kerberosLogonSerialize(const KERB_INTERACTIVE_LOGON *pLogon, PBYTE *ppPackage, DWORD *pcbPackage);
 
 private:
