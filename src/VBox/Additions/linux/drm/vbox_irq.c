5aab76d15347e50bf92725878f10ec32b9f8ed64
Frank Mehnert <noreply@oracle.com>
2017-03-23 12:49:23 +0000

    Additions/linux/drm: fix
    svn:sync-xref-src-repo-rev: r114142

--- src/VBox/Additions/linux/drm/vbox_irq.c
+++ src/VBox/Additions/linux/drm/vbox_irq.c
@@ -1,4 +1,4 @@
-/* $Id: vbox_irq.c 65381 2017-01-20 09:23:53Z noreply@oracle.com $ */
+/* $Id: vbox_irq.c 66224 2017-03-23 12:49:23Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -136,7 +136,7 @@ static void vbox_update_mode_hints(struct vbox_private *vbox)
 
     rc = VBoxHGSMIGetModeHints(&vbox->submit_info, vbox->num_crtcs,
                                vbox->last_mode_hints);
-    if RT_FAILURE(rc) {
+    if (RT_FAILURE(rc)) {
         printk("vboxvideo: VBoxHGSMIGetModeHints failed, rc=%i.\n", rc);
         return;
     }
