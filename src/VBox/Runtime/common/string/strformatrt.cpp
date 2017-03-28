42aa4058d8242fa384c9d0ad6f7e1a8372410240
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-03-28 12:40:02 +0000

    strformatrt.cpp: We shouldn't need 16-bit ring-0 support here AFAIK.
    svn:sync-xref-src-repo-rev: r114224

--- src/VBox/Runtime/common/string/strformatrt.cpp
+++ src/VBox/Runtime/common/string/strformatrt.cpp
@@ -1,4 +1,4 @@
-/* $Id: strformatrt.cpp 66292 2017-03-28 11:33:36Z noreply@oracle.com $ */
+/* $Id: strformatrt.cpp 66299 2017-03-28 12:40:02Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - IPRT String Formatter Extensions.
  */
@@ -485,8 +485,6 @@ DECLHIDDEN(size_t) rtstrFormatRt(PFNRTSTROUTPUT pfnOutput, void *pvArgOutput, co
                     cch += pfnOutput(pvArgOutput, RT_STR_TUPLE("XXXXXXXX"));
 # elif R0_ARCH_BITS == 64
                     cch += pfnOutput(pvArgOutput, RT_STR_TUPLE("XXXXXXXXXXXXXXXX"));
-# elif R0_ARCH_BITS == 16
-                    cch += pfnOutput(pvArgOutput, RT_STR_TUPLE("XXXX"));
 # else
 #  error implement me!
 # endif
