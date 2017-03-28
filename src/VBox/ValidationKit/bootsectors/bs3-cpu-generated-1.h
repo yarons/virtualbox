1e66491e4c9cbd5807daa3063a3738c995aa8054
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-03-28 21:34:58 +0000

    IEM: Implemented movsldup Vdq,Wdq (f3 0f 12)
    svn:sync-xref-src-repo-rev: r114244

--- src/VBox/ValidationKit/bootsectors/bs3-cpu-generated-1.h
+++ src/VBox/ValidationKit/bootsectors/bs3-cpu-generated-1.h
@@ -1,4 +1,4 @@
-/* $Id: bs3-cpu-generated-1.h 66313 2017-03-28 19:28:08Z knut.osmundsen@oracle.com $ */
+/* $Id: bs3-cpu-generated-1.h 66315 2017-03-28 21:34:58Z knut.osmundsen@oracle.com $ */
 /** @file
  * BS3Kit - bs3-cpu-generated-1, common header file.
  */
@@ -47,6 +47,7 @@ typedef enum BS3CG1OP
     BS3CG1OP_Wsd,
     BS3CG1OP_Wps,
     BS3CG1OP_Wpd,
+    BS3CG1OP_Wdq,
 
     BS3CG1OP_Gb,
     BS3CG1OP_Gv,
@@ -57,6 +58,7 @@ typedef enum BS3CG1OP
     BS3CG1OP_Vps,
     BS3CG1OP_Vpd,
     BS3CG1OP_Vq,
+    BS3CG1OP_Vdq,
 
     BS3CG1OP_Ib,
     BS3CG1OP_Iz,
@@ -95,6 +97,7 @@ typedef enum BS3CG1ENC
     BS3CG1ENC_MODRM_Gv_Ma, /**< bound instruction */
     BS3CG1ENC_MODRM_Vq_UqHi,
     BS3CG1ENC_MODRM_Vq_Mq,
+    BS3CG1ENC_MODRM_Vdq_Wdq,
 
     BS3CG1ENC_FIXED,
     BS3CG1ENC_FIXED_AL_Ib,
