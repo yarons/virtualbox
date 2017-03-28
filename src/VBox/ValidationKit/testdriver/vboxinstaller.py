5150b518db57b63aa328bdfafa548e0f388f6563
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2017-03-28 11:56:11 +0000

    testdriver/vboxinstaller.py: there is no vboxnetadp6 driver service on Windows, it uses the same vboxnetadp driver service as the NDIS5 driver but points to a different driver
    svn:sync-xref-src-repo-rev: r114223

--- src/VBox/ValidationKit/testdriver/vboxinstaller.py
+++ src/VBox/ValidationKit/testdriver/vboxinstaller.py
@@ -30,7 +30,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 66291 $"
+__version__ = "$Revision: 66298 $"
 
 
 # Standard Python imports.
@@ -722,7 +722,7 @@ class VBoxInstallerTestDriver(TestDriverBase):
     #
 
     ## VBox windows services we can query the status of.
-    kasWindowsServices = [ 'vboxdrv', 'vboxusbmon', 'vboxnetadp', 'vboxnetflt', 'vboxnetadp6', 'vboxnetlwf' ];
+    kasWindowsServices = [ 'vboxdrv', 'vboxusbmon', 'vboxnetadp', 'vboxnetflt', 'vboxnetlwf' ];
 
     def _installVBoxOnWindows(self):
         """ Installs VBox on Windows."""
