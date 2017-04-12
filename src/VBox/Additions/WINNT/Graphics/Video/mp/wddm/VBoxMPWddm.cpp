b87d6ce35073ff077d890d4e24479bf6b2f3202a
Dmitrii Grigorev <dmitrii.grigorev@oracle.com>
2017-04-12 15:17:58 +0000

    3D: WDDM: Skipping incorrect input data in VBOXESC_DBGPRINT, bugref:8777
    svn:sync-xref-src-repo-rev: r114556

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMPWddm.cpp 66507 2017-04-11 10:15:57Z noreply@oracle.com $ */
+/* $Id: VBoxMPWddm.cpp 66539 2017-04-12 15:17:58Z dmitrii.grigorev@oracle.com $ */
 /** @file
  * VBox WDDM Miniport driver
  */
@@ -5005,11 +5005,13 @@ DxgkDdiEscape(
                 {
                     PVBOXDISPIFESCAPE_DBGPRINT pDbgPrint = (PVBOXDISPIFESCAPE_DBGPRINT)pEscapeHdr;
                     /* ensure the last char is \0*/
-                    *((uint8_t*)pDbgPrint + pEscape->PrivateDriverDataSize - 1) = '\0';
-                    if (g_VBoxLogUm & VBOXWDDM_CFG_LOG_UM_DBGPRINT)
-                        DbgPrint("%s\n", pDbgPrint->aStringBuf);
-                    if (g_VBoxLogUm & VBOXWDDM_CFG_LOG_UM_BACKDOOR)
-                        LOGREL_EXACT(("%s\n", pDbgPrint->aStringBuf));
+                    if (*((uint8_t*)pDbgPrint + pEscape->PrivateDriverDataSize - 1) == '\0')
+                    {
+                        if (g_VBoxLogUm & VBOXWDDM_CFG_LOG_UM_DBGPRINT)
+                            DbgPrint("%s\n", pDbgPrint->aStringBuf);
+                        if (g_VBoxLogUm & VBOXWDDM_CFG_LOG_UM_BACKDOOR)
+                            LOGREL_EXACT(("%s\n", pDbgPrint->aStringBuf));
+                    }
                 }
                 Status = STATUS_SUCCESS;
                 break;
