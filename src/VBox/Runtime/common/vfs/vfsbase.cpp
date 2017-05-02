dfa31fa4c2410c6fad636a5cc679cfe5eb47924c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-05-02 00:07:35 +0000

    vfsbase.cpp: logging fix
    svn:sync-xref-src-repo-rev: r115141

--- src/VBox/Runtime/common/vfs/vfsbase.cpp
+++ src/VBox/Runtime/common/vfs/vfsbase.cpp
@@ -1,4 +1,4 @@
-/* $Id: vfsbase.cpp 66736 2017-05-02 00:01:39Z knut.osmundsen@oracle.com $ */
+/* $Id: vfsbase.cpp 66737 2017-05-02 00:07:35Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Virtual File System, Base.
  */
@@ -1907,7 +1907,7 @@ RTDECL(uint32_t) RTVfsDirRetain(RTVFSDIR hVfsDir)
     AssertPtrReturn(pThis, UINT32_MAX);
     AssertReturn(pThis->uMagic == RTVFSDIR_MAGIC, UINT32_MAX);
     uint32_t cRefs = rtVfsObjRetain(&pThis->Base);
-    LogFlow(("RTVfsDirRetain(%p/%p) -> %#x\n", pThis, pThis->Base.pvThis));
+    LogFlow(("RTVfsDirRetain(%p/%p) -> %#x\n", pThis, pThis->Base.pvThis, cRefs));
     return cRefs;
 }
 
@@ -1923,7 +1923,7 @@ RTDECL(uint32_t) RTVfsDirRelease(RTVFSDIR hVfsDir)
     void *pvThis = pThis->Base.pvThis;
 #endif
     uint32_t cRefs = rtVfsObjRelease(&pThis->Base);
-    LogFlow(("RTVfsDirRelease(%p/%p) -> %#x\n", pThis, pvThis));
+    LogFlow(("RTVfsDirRelease(%p/%p) -> %#x\n", pThis, pvThis, cRefs));
     return cRefs;
 }
 
