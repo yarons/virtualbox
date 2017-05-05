14db9a5d6c34fa543042ea5a208150c2df17f7c8
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-05-05 08:13:01 +0000

    testboxscript: solaris setup fix
    svn:sync-xref-src-repo-rev: r115220

--- src/VBox/ValidationKit/testboxscript/solaris/setup-routines.sh
+++ src/VBox/ValidationKit/testboxscript/solaris/setup-routines.sh
@@ -1,4 +1,4 @@
-# $Id: setup-routines.sh 66800 2017-05-04 23:55:34Z knut.osmundsen@oracle.com $
+# $Id: setup-routines.sh 66803 2017-05-05 08:13:01Z knut.osmundsen@oracle.com $
 ## @file
 # VirtualBox Validation Kit - TestBoxScript Service Setup on Solaris.
 #
@@ -130,7 +130,7 @@ os_load_config() {
                  | ${MY_SED} \
                    -e 's/^.*testboxscript -d -m *//' \
                 `;
-        eval common_testboxscript_args_to_config `echo "${XMLARGS}" | sed -e "s/<string>/'/g" -e "s/<\/string>/'/g" `;
+        eval common_testboxscript_args_to_config ${XMLARGS}
     fi
 }
 
