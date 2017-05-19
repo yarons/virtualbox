a1610209d4f00fc09c16efbfd9ffeb8a6041716a
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2017-05-19 13:30:38 +0000

    VMM/HMSVMR0: Re-enable the IEM event reflection logic.
    svn:sync-xref-src-repo-rev: r115531

--- src/VBox/VMM/VMMR0/HMSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HMSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMSVMR0.cpp 66982 2017-05-19 13:24:50Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMSVMR0.cpp 66983 2017-05-19 13:30:38Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM SVM (AMD-V) - Host Context Ring-0.
  */
@@ -35,7 +35,7 @@
 #include "HMSVMR0.h"
 #include "dtrace/VBoxVMM.h"
 
-//#define HMSVM_USE_IEM_EVENT_REFLECTION
+#define HMSVM_USE_IEM_EVENT_REFLECTION
 #ifdef DEBUG_ramshankar
 # define HMSVM_SYNC_FULL_GUEST_STATE
 # define HMSVM_ALWAYS_TRAP_ALL_XCPTS
