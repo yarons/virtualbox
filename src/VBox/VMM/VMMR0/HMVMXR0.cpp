ab684e549e5c1bec2d0b244283651669bb1a685c
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2017-05-23 06:37:26 +0000

    VMM/HMVMXR0: If event delivery causes an EPT misconfig/APIC access, don't re-execute the instruction using IEM, instead re-inject the original event.
    svn:sync-xref-src-repo-rev: r115595

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 67024 2017-05-23 06:35:38Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXR0.cpp 67025 2017-05-23 06:37:26Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -12979,8 +12979,7 @@ HMVMX_EXIT_DECL hmR0VmxExitApicAccess(PVMCPU pVCpu, PCPUMCTX pMixedCtx, PVMXTRAN
         if (RT_UNLIKELY(pVCpu->hm.s.Event.fPending))
         {
             STAM_COUNTER_INC(&pVCpu->hm.s.StatInjectPendingInterpret);
-            /** @todo return VINF_EM_RAW_INJECT_TRPM_EVENT? */
-            return VERR_EM_INTERPRETER;
+            return VINF_EM_RAW_INJECT_TRPM_EVENT;
         }
     }
     else
@@ -13157,8 +13156,7 @@ HMVMX_EXIT_DECL hmR0VmxExitEptMisconfig(PVMCPU pVCpu, PCPUMCTX pMixedCtx, PVMXTR
         if (RT_UNLIKELY(pVCpu->hm.s.Event.fPending))
         {
             STAM_COUNTER_INC(&pVCpu->hm.s.StatInjectPendingInterpret);
-            /** @todo return VINF_EM_RAW_INJECT_TRPM_EVENT? */
-            return VERR_EM_INTERPRETER;
+            return VINF_EM_RAW_INJECT_TRPM_EVENT;
         }
     }
     else
