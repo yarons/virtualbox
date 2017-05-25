2c1824b92df65c6158f7bdd8f93ebc44bb3d0936
Sergey Dubov <sergey.dubov@oracle.com>
2017-05-25 15:14:50 +0000

    FE/Qt: Selector UI: Tools pane: Few adjustments for r115731.
    svn:sync-xref-src-repo-rev: r115753

--- src/VBox/Frontends/VirtualBox/src/selector/UIToolsPane.h
+++ src/VBox/Frontends/VirtualBox/src/selector/UIToolsPane.h
@@ -1,4 +1,4 @@
-/* $Id: UIToolsPane.h 67068 2017-05-24 17:47:44Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsPane.h 67090 2017-05-25 15:14:50Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsPane class declaration.
  */
@@ -88,8 +88,8 @@ private:
     /** Prepares menu. */
     void prepareMenu();
 
-    /** Adds corresponding tab-bar tab. */
-    void addTabBarTab(ToolsType enmType, bool fCloseable);
+    /** Activates corresponding tab-bar tab, adds new if necessary. */
+    void activateTabBarTab(ToolsType enmType, bool fCloseable);
 
     /** Holds the main layout isntance. */
     QVBoxLayout               *m_pLayoutMain;
