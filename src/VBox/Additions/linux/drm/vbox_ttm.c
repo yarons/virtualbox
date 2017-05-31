629c4d6458a4a45bec4ea87c5f9e758e91f4d9dd
Michael Thayer <noreply@oracle.com>
2017-05-31 14:05:16 +0000

    bugref:8524: Additions/linux: play nicely with distribution-installed Additions The vboxvideo causes a NULL pointer dereference inside the kernel on kernel 4.10+ when ttm needs to make room in the video memory and tries to evict things.
    This is caused by upstream kernel commit a2ab19fed9d1 ("drm/ttm: make
    eviction decision a driver callback v2") introducing a new
    eviction_valuable callback which the vboxvideo driver does not define:
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit?id=a2ab19fed9d1dc5a7a2ced44f4b289885c522a8f
    
    This commit adds a definition for this callback, fixing the crash.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Further updated to add a definition for the io_mem_pfn callback, added in
    upstream kernel commit ea642c32 ("drm/ttm: add io_mem_pfn callback").
    
    svn:sync-xref-src-repo-rev: r115850

--- src/VBox/Additions/linux/drm/vbox_ttm.c
+++ src/VBox/Additions/linux/drm/vbox_ttm.c
@@ -1,4 +1,4 @@
-/* $Id: vbox_ttm.c 66544 2017-04-12 17:02:30Z noreply@oracle.com $ */
+/* $Id: vbox_ttm.c 67174 2017-05-31 14:05:16Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -262,11 +262,17 @@ struct ttm_bo_driver vbox_bo_driver = {
     .ttm_tt_populate = vbox_ttm_tt_populate,
     .ttm_tt_unpopulate = vbox_ttm_tt_unpopulate,
     .init_mem_type = vbox_bo_init_mem_type,
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
+    .eviction_valuable = ttm_bo_eviction_valuable,
+#endif
     .evict_flags = vbox_bo_evict_flags,
     .move = vbox_bo_move,
     .verify_access = vbox_bo_verify_access,
     .io_mem_reserve = &vbox_ttm_io_mem_reserve,
     .io_mem_free = &vbox_ttm_io_mem_free,
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+    .io_mem_pfn = ttm_bo_default_io_mem_pfn,
+#endif
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
     .lru_tail = &ttm_bo_default_lru_tail,
     .swap_lru_tail = &ttm_bo_default_swap_lru_tail,
