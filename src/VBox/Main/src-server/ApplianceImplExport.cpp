8644a2bf971c932c0e969e6eef201b2398cbe8da
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-06-01 13:37:09 +0000

    Main: Appliance: todo
    svn:sync-xref-src-repo-rev: r115891

--- src/VBox/Main/src-server/ApplianceImplExport.cpp
+++ src/VBox/Main/src-server/ApplianceImplExport.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImplExport.cpp 67208 2017-06-01 13:32:35Z knut.osmundsen@oracle.com $ */
+/* $Id: ApplianceImplExport.cpp 67209 2017-06-01 13:37:09Z knut.osmundsen@oracle.com $ */
 /** @file
  * IAppliance and IVirtualSystem COM class implementations.
  */
@@ -2301,9 +2301,12 @@ HRESULT Appliance::i_writeFSOPC(TaskOVF *pTask, AutoWriteLockBase &writeLock)
             if (RT_FAILURE(vrc))
                 throw setErrorVrc(vrc, tr("Failed to create '%s' (%Rrc)"), strTarballPath.c_str(), vrc);
 
-            RTVFSIOSTREAM hVfsIosGzip;
+            RTVFSIOSTREAM hVfsIosGzip = NIL_RTVFSIOSTREAM;
             vrc = RTZipGzipCompressIoStream(hVfsIosFile, 0 /*fFlags*/, 6 /*uLevel*/, &hVfsIosGzip);
             RTVfsIoStrmRelease(hVfsIosFile);
+
+            /** @todo insert I/O thread here between gzip and the tar creator. Needs
+             *        implementing. */
 
             RTVFSFSSTREAM hVfsFssTar = NIL_RTVFSFSSTREAM;
             if (RT_SUCCESS(vrc))
