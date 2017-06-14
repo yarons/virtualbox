f29a6fb64d2a1de5d700520e9e239d508d1ba522
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-06-14 12:59:37 +0000

    IPRT: More ISO maker code.
    svn:sync-xref-src-repo-rev: r116124

--- src/VBox/Runtime/common/fs/isomakercmd.cpp
+++ src/VBox/Runtime/common/fs/isomakercmd.cpp
@@ -1,4 +1,4 @@
-/* $Id: isomakercmd.cpp 67381 2017-06-13 19:10:51Z knut.osmundsen@oracle.com $ */
+/* $Id: isomakercmd.cpp 67399 2017-06-14 12:59:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - ISO Image Maker Command.
  */
@@ -896,6 +896,13 @@ static int rtFsIsoMakerCmdOptNameSetup(PRTFSISOMAKERCMDOPTS pOpts, const char *p
         if (iNameSpecifier >= RT_ELEMENTS(pOpts->afNameSpecifiers))
             return rtFsIsoMakerCmdSyntaxError(pOpts, "too many name specifiers (max %d)", RT_ELEMENTS(pOpts->afNameSpecifiers));
         pOpts->afNameSpecifiers[iNameSpecifier] = fNameSpecifier;
+        iNameSpecifier++;
+
+        /*
+         * Next, if any.
+         */
+        if (pszSpec[offSpec] == ',')
+            offSpec++;
     } while (pszSpec[offSpec] != '\0');
 
     pOpts->cNameSpecifiers = iNameSpecifier;
@@ -1056,7 +1063,7 @@ static int rtFsIsoMakerCmdAddSomething(PRTFSISOMAKERCMDOPTS pOpts, const char *p
             }
         }
 
-        for (uint32_t iDst = cParsedNames; iDst < pOpts->cNameSpecifiers; iDst++)
+        for (uint32_t iDst = iSrc + 1; iDst < pOpts->cNameSpecifiers; iDst++)
             aParsedNames[iDst] = aParsedNames[iSrc];
         cParsedNames = pOpts->cNameSpecifiers + 1;
     }
