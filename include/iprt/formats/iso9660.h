f64c33e319f540d3bd9e81d8f78095754138f7a8
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-06-16 13:14:39 +0000

    IPRT: More ISO maker code (booting related).
    svn:sync-xref-src-repo-rev: r116168

--- include/iprt/formats/iso9660.h
+++ include/iprt/formats/iso9660.h
@@ -1,4 +1,4 @@
-/* $Id: iso9660.h 67426 2017-06-15 14:18:16Z knut.osmundsen@oracle.com $ */
+/* $Id: iso9660.h 67437 2017-06-16 13:14:39Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT, ISO 9660 File System
  */
@@ -876,7 +876,7 @@ typedef ISO9660ELTORITOSECTIONENTRYEXT const *PCISO9660ELTORITOSECTIONENTRYEXT;
 /**
  * Boot information table used by isolinux and GRUB2 El Torito boot files.
  */
-typedef struct ISO9960SYSLINUXINFOTABLE
+typedef struct ISO9660SYSLINUXINFOTABLE
 {
     /** 0x00/0x08: Offset of the primary volume descriptor (block offset). */
     uint32_t            offPrimaryVolDesc;
@@ -890,12 +890,12 @@ typedef struct ISO9960SYSLINUXINFOTABLE
     uint32_t            uChecksum;
     /** 0x10/0x18: Reserved for future fun. */
     uint32_t            auReserved[10];
-} ISO9960SYSLINUXINFOTABLE;
-AssertCompileSize(ISO9960SYSLINUXINFOTABLE, 56);
+} ISO9660SYSLINUXINFOTABLE;
+AssertCompileSize(ISO9660SYSLINUXINFOTABLE, 56);
 /** Pointer to a syslinux boot information table.   */
-typedef ISO9960SYSLINUXINFOTABLE *PISO9960SYSLINUXINFOTABLE;
+typedef ISO9660SYSLINUXINFOTABLE *PISO9660SYSLINUXINFOTABLE;
 /** Pointer to a const syslinux boot information table.   */
-typedef ISO9960SYSLINUXINFOTABLE const *PCISO9960SYSLINUXINFOTABLE;
+typedef ISO9660SYSLINUXINFOTABLE const *PCISO9660SYSLINUXINFOTABLE;
 
 /** @} */
 
