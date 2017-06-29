3d67cda104899e6a29e82be3946f445ef27d4242
Andreas Loeffler <andreas.loeffler@oracle.com>
2017-06-29 11:50:06 +0000

    Audio/DrvAudioCommon.cpp: Some more assertions for DrvAudioHlpPCMPropsAreValid() + DrvAudioHlpStreamCfgIsValid().
    svn:sync-xref-src-repo-rev: r116517

--- src/VBox/Devices/Audio/DrvAudioCommon.cpp
+++ src/VBox/Devices/Audio/DrvAudioCommon.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvAudioCommon.cpp 67606 2017-06-26 12:41:12Z andreas.loeffler@oracle.com $ */
+/* $Id: DrvAudioCommon.cpp 67689 2017-06-29 11:50:06Z andreas.loeffler@oracle.com $ */
 /** @file
  * Intermedia audio driver, common routines. These are also used
  * in the drivers which are bound to Main, e.g. the VRDE or the
@@ -795,6 +795,8 @@ bool DrvAudioHlpPCMPropsAreValid(const PPDMAUDIOPCMPROPS pProps)
     bool fValid = (   pProps->cChannels >= 1
                    && pProps->cChannels <= 8);
 
+    AssertMsg(fValid, ("Channel count (%RU8) invalid\n", pProps->cChannels));
+
     if (fValid)
     {
         switch (pProps->cBits)
@@ -808,15 +810,21 @@ bool DrvAudioHlpPCMPropsAreValid(const PPDMAUDIOPCMPROPS pProps)
                 fValid = false;
                 break;
         }
+
+        AssertMsg(fValid, ("Bits (%RU8) invalid\n", pProps->cBits));
     }
 
     if (!fValid)
         return false;
 
     fValid &= pProps->uHz > 0;
+    AssertMsg(fValid, ("Hz (%RU32) invalid\n", pProps->uHz));
+
     fValid &= pProps->cShift == PDMAUDIOPCMPROPS_MAKE_SHIFT_PARMS(pProps->cBits, pProps->cChannels);
+    AssertMsg(fValid, ("Shift (%RU8) invalid\n", pProps->cShift));
 
     fValid &= pProps->fSwapEndian == false; /** @todo Handling Big Endian audio data is not supported yet. */
+    AssertMsg(fValid, ("Swap endian (%RTbool) invalid\n", pProps->fSwapEndian));
 
     return fValid;
 }
@@ -878,6 +886,8 @@ bool DrvAudioHlpStreamCfgIsValid(const PPDMAUDIOSTREAMCFG pCfg)
 
     bool fValid = (   pCfg->enmDir == PDMAUDIODIR_IN
                    || pCfg->enmDir == PDMAUDIODIR_OUT);
+
+    AssertMsg(fValid, ("Stream direction not set / invalid\n"));
 
     if (fValid)
         fValid = DrvAudioHlpPCMPropsAreValid(&pCfg->Props);
