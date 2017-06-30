bbac3093818287e389f925b8abc2f10e0e453ca5
Frank Mehnert <noreply@oracle.com>
2017-06-30 15:46:07 +0000

    AudioMixBuffer: comment
    svn:sync-xref-src-repo-rev: r116613

--- src/VBox/Devices/Audio/AudioMixBuffer.cpp
+++ src/VBox/Devices/Audio/AudioMixBuffer.cpp
@@ -1,4 +1,4 @@
-/* $Id: AudioMixBuffer.cpp 67741 2017-06-30 15:42:33Z noreply@oracle.com $ */
+/* $Id: AudioMixBuffer.cpp 67742 2017-06-30 15:46:07Z noreply@oracle.com $ */
 /** @file
  * VBox audio: Audio mixing buffer for converting reading/writing audio
  *             samples.
@@ -447,6 +447,7 @@ static int audioMixBufAlloc(PPDMAUDIOMIXBUF pMixBuf, uint32_t cSamples)
     /* Clips a specific output value to a single sample value. */ \
     DECLCALLBACK(int64_t) audioMixBufClipFrom##_aName(_aType aVal) \
     { \
+        /* left shifting of signed values is not defined, therefore the intermediate uint64_t cast */ \
         if (_aSigned) \
             return (int64_t) (((uint64_t) ((int64_t) aVal                     )) << (32 - _aShift)); \
         return     (int64_t) (((uint64_t) ((int64_t) aVal - ((_aMax >> 1) + 1))) << (32 - _aShift)); \
