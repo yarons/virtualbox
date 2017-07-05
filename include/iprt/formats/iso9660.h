d2f1c50942216b542a9c4b2e1924acf2b6ee2fa9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-07-05 12:29:35 +0000

    isomaker: Added API for setting the rock ridge name and adjusted RTFsIsoMakerAddUnnamedDir to take additional attributes.
    svn:sync-xref-src-repo-rev: r116716

--- include/iprt/formats/iso9660.h
+++ include/iprt/formats/iso9660.h
@@ -1,4 +1,4 @@
-/* $Id: iso9660.h 67790 2017-07-05 11:24:53Z knut.osmundsen@oracle.com $ */
+/* $Id: iso9660.h 67794 2017-07-05 12:29:35Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT, ISO 9660 File System
  */
@@ -1236,7 +1236,7 @@ typedef ISO9660RRIPSL const *PCISO9660RRIPSL;
 #define ISO9660RRIP_SL_C_MOUNT_POINT    UINT8_C(0x10)
 /** Reserved / historically was uname network node name. */
 #define ISO9660RRIP_SL_C_UNAME          UINT8_C(0x20)
-/** Reserved mask (includes historically bits). */
+/** Reserved mask (considers historically bits reserved). */
 #define ISO9660RRIP_SL_C_RESERVED_MASK  UINT8_C(0xf0)
 /** @} */
 
@@ -1272,6 +1272,8 @@ typedef ISO9660RRIPNM const *PCISO9660RRIPNM;
 #define ISO9660RRIP_NM_F_PARENT         UINT8_C(0x04)
 /** Reserved / historically was uname network node name. */
 #define ISO9660RRIP_NM_F_UNAME          UINT8_C(0x20)
+/** Reserved mask (considers historically bits reserved). */
+#define ISO9660RRIP_NM_F_RESERVED_MASK  UINT8_C(0xf8)
 /** @} */
 
 
