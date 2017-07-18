ed7fd63cbd2ec0bddf1ddbd4f4fc1a9ec3e23ac1
Sergey Dubov <sergey.dubov@oracle.com>
2017-07-18 11:09:52 +0000

    FE/Qt: bugref:8400: Virtual Media Manager: Details widget: Forgot to update tool-tip in r116993.
    svn:sync-xref-src-repo-rev: r117035

--- src/VBox/Frontends/VirtualBox/src/widgets/UIMediumSizeEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIMediumSizeEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMediumSizeEditor.cpp 67985 2017-07-17 09:11:52Z sergey.dubov@oracle.com $ */
+/* $Id: UIMediumSizeEditor.cpp 68017 2017-07-18 11:09:52Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMediumSizeEditor class implementation.
  */
@@ -54,6 +54,7 @@ void UIMediumSizeEditor::setMediumSize(qulonglong uSize)
 {
     /* Remember the new size: */
     m_uSize = uSize;
+
     /* And assign it to the slider & editor: */
     m_pSlider->blockSignals(true);
     m_pSlider->setValue(sizeMBToSlider(m_uSize, m_iSliderScale));
@@ -61,6 +62,9 @@ void UIMediumSizeEditor::setMediumSize(qulonglong uSize)
     m_pEditor->blockSignals(true);
     m_pEditor->setText(vboxGlobal().formatSize(m_uSize));
     m_pEditor->blockSignals(false);
+
+    /* Update the tool-tips: */
+    updateSizeToolTips(m_uSize);
 }
 
 void UIMediumSizeEditor::retranslateUi()
