58137a9fddc822c3a068d51caa34f7e2f612773d
Frank Mehnert <noreply@oracle.com>
2017-07-31 07:12:15 +0000

    vsheriff: detect if the driver cannot be unloaded on Linux
    svn:sync-xref-src-repo-rev: r117275

--- src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
+++ src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: virtual_test_sheriff.py 68142 2017-07-27 16:20:00Z noreply@oracle.com $
+# $Id: virtual_test_sheriff.py 68182 2017-07-31 07:12:15Z noreply@oracle.com $
 # pylint: disable=C0301
 
 """
@@ -33,7 +33,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 68142 $"
+__version__ = "$Revision: 68182 $"
 
 
 # Standard python imports
@@ -293,7 +293,7 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
 
         if self.oConfig.sLogFile:
             self.oLogFile = open(self.oConfig.sLogFile, "a");
-            self.oLogFile.write('VirtualTestSheriff: $Revision: 68142 $ \n');
+            self.oLogFile.write('VirtualTestSheriff: $Revision: 68182 $ \n');
 
 
     def eprint(self, sText):
@@ -559,7 +559,7 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
         for idTestResult, tReason in dReasonForResultId.items():
             oFailureReason = self.getFailureReason(tReason);
             if oFailureReason is not None:
-                sComment = 'Set by $Revision: 68142 $' # Handy for reverting later.
+                sComment = 'Set by $Revision: 68182 $' # Handy for reverting later.
                 if idTestResult in dCommentForResultId:
                     sComment += ': ' + dCommentForResultId[idTestResult];
 
@@ -674,6 +674,8 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
               'This system is not currently set up to build kernel modules' ),
             ( True, ktReason_Host_DriverNotCompilable,
               'This system is currently not set up to build kernel modules' ),
+            ( True, ktReason_Host_DriverNotUnloading,
+              'Cannot unload module vboxdrv'),
         ],
     };
 
