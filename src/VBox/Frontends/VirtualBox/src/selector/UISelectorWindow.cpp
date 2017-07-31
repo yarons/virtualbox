929eff092e8e0332efd20926c6863bda1d559959
Sergey Dubov <sergey.dubov@oracle.com>
2017-07-31 13:59:12 +0000

    FE/Qt: bugref:8900: Selector UI: Make sure Details and Snapshots Machine tools ares opened at startup for now (until we are saving/restoring what was actually opened).
    svn:sync-xref-src-repo-rev: r117302

--- src/VBox/Frontends/VirtualBox/src/selector/UISelectorWindow.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/UISelectorWindow.cpp
@@ -1,4 +1,4 @@
-/* $Id: UISelectorWindow.cpp 68200 2017-07-31 12:41:10Z sergey.dubov@oracle.com $ */
+/* $Id: UISelectorWindow.cpp 68204 2017-07-31 13:59:12Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UISelectorWindow class implementation.
  */
@@ -1278,8 +1278,9 @@ void UISelectorWindow::prepare()
     prepareWidgets();
     prepareConnections();
 
-    /* Make sure at least 'Details' Machine tool is opened at startup: */
+    /* Make sure 'Details' and 'Snapshots' Machine tools ares opened at startup for now: */
     actionPool()->action(UIActionIndexST_M_Tools_M_Machine_Details)->trigger();
+    actionPool()->action(UIActionIndexST_M_Tools_M_Machine_Snapshots)->trigger();
 
     /* Load settings: */
     loadSettings();
