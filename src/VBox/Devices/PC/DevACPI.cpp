3dfc9e46d02d0992de28f0a4ea512a5d21ecb4be
Frank Mehnert <noreply@oracle.com>
2017-08-02 16:38:51 +0000

    DevACPI: disable the test for now
    svn:sync-xref-src-repo-rev: r117372

--- src/VBox/Devices/PC/DevACPI.cpp
+++ src/VBox/Devices/PC/DevACPI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevACPI.cpp 68249 2017-08-02 14:45:18Z noreply@oracle.com $ */
+/* $Id: DevACPI.cpp 68259 2017-08-02 16:38:51Z noreply@oracle.com $ */
 /** @file
  * DevACPI - Advanced Configuration and Power Interface (ACPI) Device.
  */
@@ -1761,6 +1761,7 @@ PDMBOTHCBDECL(int) acpiPMTmrRead(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port
     DBGFTRACE_PDM_U64_TAG(pDevIns, u64Now, "acpi");
     Log(("acpi: acpiPMTmrRead -> %#x\n", *pu32));
 
+#if 0
     /** @todo temporary: sanity check against running backwards */
     uint32_t uOld = ASMAtomicXchgU32(&pThis->uPmTimeOld, *pu32);
     if (*pu32 - uOld >= 0x10000000)
@@ -1773,6 +1774,7 @@ PDMBOTHCBDECL(int) acpiPMTmrRead(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port
         AssertReleaseMsgFailed(("acpiPMTmrRead: old=%08RX32, current=%08RX32\n", uOld, *pu32));
 #endif
     }
+#endif
 
     NOREF(pvUser); NOREF(Port);
     return rc;
