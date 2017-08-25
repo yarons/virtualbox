a1b0ac6d86203a40bc1bd5e41bf45d802dc9c609
Michal Necasek <michal.necasek@oracle.com>
2017-08-25 13:16:30 +0000

    Typos.
    svn:sync-xref-src-repo-rev: r117693

--- src/VBox/VMM/VMMAll/CPUMAllMsrs.cpp
+++ src/VBox/VMM/VMMAll/CPUMAllMsrs.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUMAllMsrs.cpp 66581 2017-04-17 03:00:00Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: CPUMAllMsrs.cpp 68531 2017-08-25 13:16:30Z michal.necasek@oracle.com $ */
 /** @file
  * CPUM - CPU MSR Registers.
  */
@@ -1851,13 +1851,13 @@ static DECLCALLBACK(VBOXSTRICTRC) cpumMsrWr_IntelPkgCStConfigControl(PVMCPU pVCp
 
     if (pVCpu->cpum.s.GuestMsrs.msr.PkgCStateCfgCtrl & RT_BIT_64(15))
     {
-        Log(("CPUM: WRMDR %#x (%s), %#llx: Write protected -> #GP\n", idMsr, pRange->szName, uValue));
+        Log(("CPUM: WRMSR %#x (%s), %#llx: Write protected -> #GP\n", idMsr, pRange->szName, uValue));
         return VERR_CPUM_RAISE_GP_0;
     }
 #if 0 /** @todo check what real (old) hardware does. */
     if ((uValue & 7) >= 5)
     {
-        Log(("CPUM: WRMDR %#x (%s), %#llx: Invalid limit (%d) -> #GP\n", idMsr, pRange->szName, uValue, (uint32_t)(uValue & 7)));
+        Log(("CPUM: WRMSR %#x (%s), %#llx: Invalid limit (%d) -> #GP\n", idMsr, pRange->szName, uValue, (uint32_t)(uValue & 7)));
         return VERR_CPUM_RAISE_GP_0;
     }
 #endif
