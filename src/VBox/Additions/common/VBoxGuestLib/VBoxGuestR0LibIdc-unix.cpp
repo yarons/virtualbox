aba0387774beae5dce05f7be9e06d618565c3a5c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-08-31 12:10:30 +0000

    merging vbglioc r117732: solaris adjustments.
    svn:sync-xref-src-repo-rev: r117784

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR0LibIdc-unix.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR0LibIdc-unix.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR0LibIdc-unix.cpp 68561 2017-08-31 12:10:14Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGuestR0LibIdc-unix.cpp 68567 2017-08-31 12:10:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxGuestLib - Ring-0 Support Library for VBoxGuest, IDC, UNIX-like OSes.
  */
@@ -34,13 +34,13 @@
 
 int VBOXCALL vbglR0IdcNativeOpen(PVBGLIDCHANDLE pHandle, PVBGLIOCIDCCONNECT pReq)
 {
-    return VBoxGuestIDC(NULL /*pvSession*/, VBGL_IOCTL_IDC_CONNECT, pReq, sizeof(*pReq));
+    return VBoxGuestIDC(NULL /*pvSession*/, VBGL_IOCTL_IDC_CONNECT, &pReq->Hdr, sizeof(*pReq));
 }
 
 
 int VBOXCALL vbglR0IdcNativeClose(PVBGLIDCHANDLE pHandle, PVBGLIOCIDCDISCONNECT pReq)
 {
-    return VBoxGuestIDC(pHandle->s.pvSession, VBGL_IOCTL_IDC_DISCONNECT, pReq, sizeof(*pReq));
+    return VBoxGuestIDC(pHandle->s.pvSession, VBGL_IOCTL_IDC_DISCONNECT, &pReq->Hdr, sizeof(*pReq));
 }
 
 
