059603da6f70778f9f1fccc9a756e1d1ad803cca
Alexander Eichner <alexander.eichner@oracle.com>
2017-09-07 16:14:52 +0000

    doxygen fix
    svn:sync-xref-src-repo-rev: r117933

--- src/VBox/Devices/Serial/DrvChar.cpp
+++ src/VBox/Devices/Serial/DrvChar.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvChar.cpp 68699 2017-09-07 15:12:54Z alexander.eichner@oracle.com $ */
+/* $Id: DrvChar.cpp 68702 2017-09-07 16:14:52Z alexander.eichner@oracle.com $ */
 /** @file
  * Driver that adapts PDMISTREAM into PDMICHARCONNECTOR / PDMICHARPORT.
  *
@@ -153,9 +153,9 @@ static DECLCALLBACK(int) drvCharSetParameters(PPDMICHARCONNECTOR pInterface, uns
 /**
  * Send thread loop - pushes data down thru the driver chain.
  *
- * @returns 0 on success.
- * @param   hThreadSelf Thread handle to this thread.
- * @param   pvUser      User argument.
+ * @returns VBox status code.
+ * @param   pDrvIns     The char driver instance.
+ * @param   pThread     The worker thread.
  */
 static DECLCALLBACK(int) drvCharIoLoop(PPDMDRVINS pDrvIns, PPDMTHREAD pThread)
 {
