813fe0aacfdc9856cbbdf1122fcdd6368cbcf01c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-09-25 13:17:02 +0000

    VMMR3EmtRendezvous: Pretend we're a single CPU config when called during vmR3Destroy after the EMTs have started terminating. [alignment fix]
    svn:sync-xref-src-repo-rev: r118108

--- src/VBox/VMM/include/VMInternal.h
+++ src/VBox/VMM/include/VMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VMInternal.h 68853 2017-09-25 13:10:41Z knut.osmundsen@oracle.com $ */
+/* $Id: VMInternal.h 68854 2017-09-25 13:17:02Z knut.osmundsen@oracle.com $ */
 /** @file
  * VM - Internal header file.
  */
@@ -195,6 +195,9 @@ typedef struct VMINTUSERPERVM
     volatile uint32_t               cActiveEmts;
 
 # ifdef VBOX_WITH_STATISTICS
+#  if HC_ARCH_BITS == 32
+    uint32_t                        uPadding;
+#  endif
     /** Number of VMR3ReqAlloc returning a new packet. */
     STAMCOUNTER                     StatReqAllocNew;
     /** Number of VMR3ReqAlloc causing races. */
