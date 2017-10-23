1c798f9980c64cad238e6d5e9a93f75f9b60bb09
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-10-23 18:13:56 +0000

    include/VBox/: A couple of license adjustments and other scm cleanups.
    svn:sync-xref-src-repo-rev: r118492

--- include/VBox/ata.h
+++ include/VBox/ata.h
@@ -1,4 +1,4 @@
-/* $Id: ata.h 69107 2017-10-17 10:53:48Z knut.osmundsen@oracle.com $ */
+/* $Id: ata.h 69181 2017-10-23 18:13:56Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox storage devices: ATA/ATAPI declarations
  */
@@ -13,6 +13,15 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
  */
 
 #ifndef ___VBox_ata_h
