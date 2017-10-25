b6fd6e10fd31b24e27adab0768f18ec42c57faef
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-10-25 13:39:29 +0000

    scm: acpi sources
    svn:sync-xref-src-repo-rev: r118623

--- src/bldprogs/scm.cpp
+++ src/bldprogs/scm.cpp
@@ -1,4 +1,4 @@
-/* $Id: scm.cpp 69295 2017-10-25 12:26:19Z knut.osmundsen@oracle.com $ */
+/* $Id: scm.cpp 69302 2017-10-25 13:39:29Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase / Tool - Source Code Massager.
  */
@@ -305,6 +305,17 @@ static PFNSCMREWRITER const g_aRewritersFor_DTrace[] =
     rewrite_Copyright_CstyleComment,
 };
 
+static PFNSCMREWRITER const g_aRewritersFor_DSL[] =
+{
+    rewrite_ForceNativeEol,
+    rewrite_ExpandTabs,
+    rewrite_StripTrailingBlanks,
+    rewrite_AdjustTrailingLines,
+    rewrite_SvnNoExecutable,
+    rewrite_SvnKeywords,
+    rewrite_Copyright_CstyleComment,
+};
+
 static PFNSCMREWRITER const g_aRewritersFor_ASM[] =
 {
     rewrite_ForceNativeEol,
@@ -451,9 +462,10 @@ static SCMCFGENTRY const g_aConfigs[] =
     SCM_CFG_ENTRY(g_aRewritersFor_C_and_CPP,        false, "*.c|*.cpp|*.C|*.CPP|*.cxx|*.cc|*.m|*.mm" ),
     SCM_CFG_ENTRY(g_aRewritersFor_H_and_HPP,        false, "*.h|*.hpp" ),
     SCM_CFG_ENTRY(g_aRewritersFor_RC,               false, "*.rc" ),
-    SCM_CFG_ENTRY(g_aRewritersFor_DTrace,           false, "*.d" ),
     SCM_CFG_ENTRY(g_aRewritersFor_ASM,              false, "*.asm|*.mac|*.inc" ),
+    SCM_CFG_ENTRY(g_aRewritersFor_DTrace,           false, "*.d" ),
     SCM_CFG_ENTRY(g_aRewritersFor_DEF,              false, "*.def" ),
+    SCM_CFG_ENTRY(g_aRewritersFor_DSL,              false, "*.dsl" ),
     SCM_CFG_ENTRY(g_aRewritersFor_ShellScripts,     false, "*.sh|configure" ),
     SCM_CFG_ENTRY(g_aRewritersFor_BatchFiles,       false, "*.bat|*.cmd|*.btm" ),
     SCM_CFG_ENTRY(g_aRewritersFor_BasicScripts,     false, "*.vbs|*.vb" ),
@@ -2102,7 +2114,7 @@ int main(int argc, char **argv)
             case 'V':
             {
                 /* The following is assuming that svn does it's job here. */
-                static const char s_szRev[] = "$Revision: 69295 $";
+                static const char s_szRev[] = "$Revision: 69302 $";
                 const char *psz = RTStrStripL(strchr(s_szRev, ' '));
                 RTPrintf("r%.*s\n", strchr(psz, ' ') - psz, psz);
                 return 0;
