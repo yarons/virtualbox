c28d58159016f62bf2f0f34368fecb50c6316c7f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-10-25 00:11:02 +0000

    VBoxSDL: scm updates
    svn:sync-xref-src-repo-rev: r118572

--- src/VBox/Frontends/VBoxSDL/Framebuffer-darwin.m
+++ src/VBox/Frontends/VBoxSDL/Framebuffer-darwin.m
@@ -1,3 +1,19 @@
+/* $Id: Framebuffer-darwin.m 69256 2017-10-25 00:11:02Z knut.osmundsen@oracle.com $ */
+/** @file
+ * VBoxSDL - Darwin Cocoa helper functions.
+ */
+
+/*
+ * Copyright (C) 2006-2016 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
 
 
 /*********************************************************************************************************************************
