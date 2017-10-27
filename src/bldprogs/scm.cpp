139d5873ccfe85133a158ae128747369db029c3a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-10-27 09:28:52 +0000

    scm: wix files
    svn:sync-xref-src-repo-rev: r118740

--- src/bldprogs/scm.cpp
+++ src/bldprogs/scm.cpp
@@ -1,4 +1,4 @@
-/* $Id: scm.cpp 69380 2017-10-26 16:38:03Z knut.osmundsen@oracle.com $ */
+/* $Id: scm.cpp 69411 2017-10-27 09:28:52Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase / Tool - Source Code Massager.
  */
@@ -456,6 +456,17 @@ static PFNSCMREWRITER const g_aRewritersFor_Xml[] =
     /** @todo copyright is in an XML comment. */
 };
 
+static PFNSCMREWRITER const g_aRewritersFor_Wix[] =
+{
+    rewrite_ForceNativeEol,
+    rewrite_ExpandTabs,
+    rewrite_StripTrailingBlanks,
+    rewrite_AdjustTrailingLines,
+    rewrite_SvnNoExecutable,
+    rewrite_SvnKeywords,
+    /** @todo copyright is in an XML comment. */
+};
+
 static PFNSCMREWRITER const g_aRewritersFor_QtProject[] =
 {
     rewrite_ForceNativeEol,
@@ -534,9 +545,10 @@ static SCMCFGENTRY const g_aConfigs[] =
     SCM_CFG_ENTRY(g_aRewritersFor_DriverInfFiles,   false, "*.inf" ),
     SCM_CFG_ENTRY(g_aRewritersFor_NsisFiles,        false, "*.nsh|*.nsi" ),
     SCM_CFG_ENTRY(g_aRewritersFor_ScmSettings,      false, "*.scm-settings" ),
-    SCM_CFG_ENTRY(g_aRewritersFor_Images,           true,  "*.png|*.bmp|*.jpg|*.pnm|*.ico|*.icns" ),
+    SCM_CFG_ENTRY(g_aRewritersFor_Images,           true,  "*.png|*.bmp|*.jpg|*.pnm|*.ico|*.icns|*.tiff|*.tif|*.xcf" ),
     SCM_CFG_ENTRY(g_aRewritersFor_Xslt,             false, "*.xsl" ),
     SCM_CFG_ENTRY(g_aRewritersFor_Xml,              false, "*.xml" ),
+    SCM_CFG_ENTRY(g_aRewritersFor_Wix,              false, "*.wxi|*.wxs|*.wxl" ),
     SCM_CFG_ENTRY(g_aRewritersFor_QtProject,        false, "*.pro" ),
     SCM_CFG_ENTRY(g_aRewritersFor_QtResourceFiles,  false, "*.qrc" ),
     SCM_CFG_ENTRY(g_aRewritersFor_QtTranslations,   false, "*.ts" ),
@@ -2234,7 +2246,7 @@ int main(int argc, char **argv)
             case 'V':
             {
                 /* The following is assuming that svn does it's job here. */
-                static const char s_szRev[] = "$Revision: 69380 $";
+                static const char s_szRev[] = "$Revision: 69411 $";
                 const char *psz = RTStrStripL(strchr(s_szRev, ' '));
                 RTPrintf("r%.*s\n", strchr(psz, ' ') - psz, psz);
                 return 0;
