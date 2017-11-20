5db129b64cf939500029a789a9da1af9de77500c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-11-20 18:41:33 +0000

    gen-sql-comment.py: Another shot at output stuff that makes scm happy.
    svn:sync-xref-src-repo-rev: r119185

--- src/VBox/ValidationKit/testmanager/db/gen-sql-comments.py
+++ src/VBox/ValidationKit/testmanager/db/gen-sql-comments.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: gen-sql-comments.py 69778 2017-11-20 17:57:01Z knut.osmundsen@oracle.com $
+# $Id: gen-sql-comments.py 69781 2017-11-20 18:41:33Z knut.osmundsen@oracle.com $
 
 """
 Converts doxygen style comments in SQL script to COMMENT ON statements.
@@ -70,7 +70,7 @@ class SqlDox(object):
 
     def commitComment2(self, sSqlObj):
         if self.sComment is not None and sSqlObj is not None:
-            print("COMMENT ON %s IS\n  '%s';\n\n" % (sSqlObj, self.quoteSqlString(self.sComment.strip())));
+            print("COMMENT ON %s IS\n  '%s';\n" % (sSqlObj, self.quoteSqlString(self.sComment.strip())));
         self.resetComment();
         return True;
 
