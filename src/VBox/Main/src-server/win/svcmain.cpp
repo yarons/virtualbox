2f3c4bdfbec217e653de2db2e982d62f2a95ffcf
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-11-22 11:42:14 +0000

    svcmain.cpp: Only call IVirtualBoxSDS::deregisterVBoxSVC from the chosen VBoxSVC. Avoid calling it during com shutdown (just to be on the safe side).
    svn:sync-xref-src-repo-rev: r119224

--- src/VBox/Main/src-server/win/svcmain.cpp
+++ src/VBox/Main/src-server/win/svcmain.cpp
@@ -1,4 +1,4 @@
-/* $Id: svcmain.cpp 69783 2017-11-20 18:58:25Z knut.osmundsen@oracle.com $ */
+/* $Id: svcmain.cpp 69803 2017-11-22 11:42:14Z knut.osmundsen@oracle.com $ */
 /** @file
  * SVCMAIN - COM out-of-proc server main entry
  */
@@ -78,8 +78,14 @@ CExeModule *g_pModule     = NULL;
 HWND        g_hMainWindow = NULL;
 HINSTANCE   g_hInstance   = NULL;
 #ifdef VBOX_WITH_SDS
-/** This is set if we're connected to SDS and should discount a server lock
- * that it is holding when deciding whether we're idle or not. */
+/** This is set if we're connected to SDS.
+ *
+ * It means that we should discount a server lock that it is holding when
+ * deciding whether we're idle or not.
+ *
+ * Also, when set we deregister with SDS during class factory destruction.  We
+ * exploit this to prevent attempts to deregister during or after COM shutdown.
+ */
 bool        g_fRegisteredWithVBoxSDS = false;
 #endif
 
@@ -219,8 +225,10 @@ public:
             m_pObj = NULL;
         }
 
-        /** @todo Need to check if this is okay wrt COM termination. */
-        i_deregisterWithSds();
+        /* We usually get here during g_pModule->Term() via CoRevokeClassObjec, so COM
+           probably working well enough to talk to SDS when we get here. */
+        if (g_fRegisteredWithVBoxSDS)
+            i_deregisterWithSds();
     }
 
     // IClassFactory
@@ -322,7 +330,7 @@ HRESULT VirtualBoxClassFactory::i_registerWithSds(IUnknown **ppOtherVirtualBox)
         hrc = m_ptrVirtualBoxSDS->RegisterVBoxSVC(m_pVBoxSVC, GetCurrentProcessId(), ppOtherVirtualBox);
         if (SUCCEEDED(hrc))
         {
-            g_fRegisteredWithVBoxSDS = true;
+            g_fRegisteredWithVBoxSDS = !*ppOtherVirtualBox;
             return hrc;
         }
         m_pVBoxSVC->Release();
@@ -900,6 +908,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpC
 
     g_pModule->Term();
 
+#ifdef VBOX_WITH_SDS
+    g_fRegisteredWithVBoxSDS = false; /* Don't trust COM LPC to work right from now on.  */
+#endif
     com::Shutdown();
 
     if(g_pModule)
