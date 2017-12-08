a236f6cf512be3837bf2a54d891d52bb3217602f
Andreas Loeffler <andreas.loeffler@oracle.com>
2017-12-08 17:23:10 +0000

    Fixed typo: Occurance -> Occurrence.
    svn:sync-xref-src-repo-rev: r119576

--- src/VBox/Main/src-client/DisplayImpl.cpp
+++ src/VBox/Main/src-client/DisplayImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisplayImpl.cpp 70036 2017-12-08 16:52:18Z andreas.loeffler@oracle.com $ */
+/* $Id: DisplayImpl.cpp 70039 2017-12-08 17:23:10Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -2682,7 +2682,7 @@ DECLCALLBACK(int) Display::i_videoRecConfigure(Display *pThis, PVIDEORECCFG pCfg
         {
             if (fAttachDetach)
                 vrc2 = PDMR3DriverDetach(ptrVM.rawUVM(), strAudioDev.c_str(), uInstance, uLUN, "AUDIO",
-                                         0 /* iOccurance */, 0 /* fFlags */);
+                                         0 /* iOccurrence */, 0 /* fFlags */);
 
             if (RT_SUCCESS(vrc2))
             {
