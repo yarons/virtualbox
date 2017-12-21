bbb377c2219aed51425eefc2fa878ef7ffa09b7f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-12-21 12:26:39 +0000

    RTLdrCheckImports.cpp: Build fix.
    svn:sync-xref-src-repo-rev: r119866

--- src/VBox/Runtime/tools/RTLdrCheckImports.cpp
+++ src/VBox/Runtime/tools/RTLdrCheckImports.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTLdrCheckImports.cpp 70256 2017-12-21 06:13:13Z knut.osmundsen@oracle.com $ */
+/* $Id: RTLdrCheckImports.cpp 70271 2017-12-21 12:26:39Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Module dependency checker.
  */
@@ -96,7 +96,7 @@ typedef struct RTCHECKIMPORTSTATE
     /** Number modules. */
     uint32_t                cImports;
     /** Import modules. */
-    RTCHECKIMPORTMODULE     aImports[];
+    RTCHECKIMPORTMODULE     aImports[RT_FLEXIBLE_ARRAY];
 } RTCHECKIMPORTSTATE;
 /** Pointer to the import checker state. */
 typedef RTCHECKIMPORTSTATE *PRTCHECKIMPORTSTATE;
