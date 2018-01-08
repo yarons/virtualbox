0e24629b4204dac35194efce7b2ded635d28f182
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-01-08 20:04:54 +0000

    FE/Qt: bugref:9072 The first attempt on logviewer's bookmark panel
    svn:sync-xref-src-repo-rev: r120101

--- src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMediumManager.cpp 69821 2017-11-24 12:01:19Z sergey.dubov@oracle.com $ */
+/* $Id: UIMediumManager.cpp 70487 2018-01-08 20:04:54Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMediumManager class implementation.
  */
@@ -2467,4 +2467,3 @@ UIMediumManagerWidget *UIMediumManager::widget()
 {
     return qobject_cast<UIMediumManagerWidget*>(QIManagerDialog::widget());
 }
-
