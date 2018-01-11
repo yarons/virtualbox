42a3fd4be10305231768c41b2fa587af6f1e10fe
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-01-11 15:10:32 +0000

    FE/Qt: Add Q_UNUSED macro for not used arguments
    svn:sync-xref-src-repo-rev: r120215

--- src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerWidget.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVMLogViewerWidget.cpp 70539 2018-01-11 14:11:16Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMLogViewerWidget.cpp 70542 2018-01-11 15:10:32Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMLogViewer class implementation.
  */
@@ -255,6 +255,8 @@ void UIVMLogViewerWidget::sltSearchResultHighLigting()
 
 void UIVMLogViewerWidget::sltTabIndexChange(int tabIndex)
 {
+    Q_UNUSED(tabIndex);
+
     // if (m_iCurrentTabIndex == tabIndex)
     //     return;
 
@@ -292,6 +294,7 @@ void UIVMLogViewerWidget::sltCreateBookmarkAtCurrent()
 
 void UIVMLogViewerWidget::sltCreateBookmarkAtLine(LogBookmark bookmark)
 {
+    Q_UNUSED(bookmark);
     // QVector<LogBookmark> *pBookmarkVector = currentBookmarkVector();
     // if (!pBookmarkVector)
     //     return;
