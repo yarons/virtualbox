a9e5e781a7bb7ccc679219874c2588d5366f9273
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-01-15 16:35:07 +0000

    Audio/HDA: Fixes for Windows guest surround setups.
    svn:sync-xref-src-repo-rev: r120302

--- src/VBox/Devices/Audio/HDAStream.h
+++ src/VBox/Devices/Audio/HDAStream.h
@@ -1,4 +1,4 @@
-/* $Id: HDAStream.h 70248 2017-12-20 18:12:25Z andreas.loeffler@oracle.com $ */
+/* $Id: HDAStream.h 70594 2018-01-15 16:35:07Z andreas.loeffler@oracle.com $ */
 /** @file
  * HDAStream.h - Stream functions for HD Audio.
  */
@@ -170,8 +170,13 @@ typedef struct HDASTREAMSTATE
     /** List of DMA handlers. */
     RTLISTANCHORR3          lstDMAHandlers;
 #endif
+    /** How much DMA data from a previous transfer is left to be processed (in bytes).
+     *  This can happen if the stream's frame size is bigger (e.g. 16 bytes) than
+     *  the current DMA FIFO can hold (e.g. 10 bytes). Mostly needed for more complex
+     *  stuff like interleaved surround streams. */
+    uint16_t                cbDMALeft;
     /** Unused, padding. */
-    uint8_t                 Padding3[3];
+    uint8_t                 Padding3;
 } HDASTREAMSTATE, *PHDASTREAMSTATE;
 
 /**
