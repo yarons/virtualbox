93b17137a2a2379c0d66ec2223b5df0de4da3212
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-02-05 10:58:07 +0000

    VMM/IEM: Build fix.
    svn:sync-xref-src-repo-rev: r120688

--- src/VBox/VMM/VMMAll/IEMAll.cpp
+++ src/VBox/VMM/VMMAll/IEMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAll.cpp 70861 2018-02-05 10:49:59Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: IEMAll.cpp 70862 2018-02-05 10:58:07Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager - All Contexts.
  */
@@ -15447,14 +15447,6 @@ VMMDECL(VBOXSTRICTRC) IEMExecLots(PVMCPU pVCpu, uint32_t *pcInstructions)
             if (pVCpu->iem.s.cActiveMappings > 0)
                 iemMemRollback(pVCpu);
             pVCpu->iem.s.cLongJumps++;
-#  ifdef VBOX_WITH_NESTED_HWVIRT
-            /*
-             * When a nested-guest causes an exception intercept when fetching memory
-             * (e.g. IEM_MC_FETCH_MEM_U16) as part of instruction execution, we need this
-             * to fix-up VINF_SVM_VMEXIT on the longjmp way out, otherwise we will guru.
-             */
-            rcStrict = iemExecStatusCodeFiddling(pVCpu, rcStrict);
-#  endif
         }
         pVCpu->iem.s.CTX_SUFF(pJmpBuf) = pSavedJmpBuf;
 # endif
