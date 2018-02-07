815fe66929287259acfb0b27c5de3abac97e31f5
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-02-07 14:08:47 +0000

    IPRT: dir.c,dir-posix.cpp,direnum-nt.cpp: Implemented RTDIR_F_NO_FOLLOW on posix, adjusted NT implementation and documented the VERR_IS_A_SYMLINK return code for the relevant APIs. [build fix]
    svn:sync-xref-src-repo-rev: r120718

--- src/VBox/Runtime/r3/posix/dir-posix.cpp
+++ src/VBox/Runtime/r3/posix/dir-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: dir-posix.cpp 70890 2018-02-07 14:05:43Z knut.osmundsen@oracle.com $ */
+/* $Id: dir-posix.cpp 70892 2018-02-07 14:08:47Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Directory manipulation, POSIX.
  */
@@ -277,7 +277,7 @@ int rtDirNativeOpen(PRTDIRINTERNAL pDir, char *pszPathBuf, uintptr_t hRelativeDi
             { /* likely */ }
             else
             {
-                pfnFdOpenDir = (PFNFDOPENDIR)dlsym(RTLD_DEFAULT, "fdopendir");
+                pfnFdOpenDir = (PFNFDOPENDIR)(uintptr_t)dlsym(RTLD_DEFAULT, "fdopendir");
                 s_pfnFdOpenDir = pfnFdOpenDir;
                 ASMAtomicWriteBool(&s_fInitalized, true);
             }
