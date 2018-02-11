a03718b3f8a84402c35e2ed50c7f2be57e8bbae6
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-02-11 21:30:58 +0000

    Build fix.
    svn:sync-xref-src-repo-rev: r120811

--- src/VBox/Devices/Audio/DevHDA.cpp
+++ src/VBox/Devices/Audio/DevHDA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevHDA.cpp 70965 2018-02-11 21:27:54Z andreas.loeffler@oracle.com $ */
+/* $Id: DevHDA.cpp 70966 2018-02-11 21:30:58Z andreas.loeffler@oracle.com $ */
 /** @file
  * DevHDA.cpp - VBox Intel HD Audio Controller.
  *
@@ -276,7 +276,6 @@ static int                        hdaRemoveStream(PHDASTATE pThis, PPDMAUDIOSTRE
 # ifdef HDA_USE_DMA_ACCESS_HANDLER
 static DECLCALLBACK(VBOXSTRICTRC) hdaDMAAccessHandler(PVM pVM, PVMCPU pVCpu, RTGCPHYS GCPhys, void *pvPhys, void *pvBuf, size_t cbBuf, PGMACCESSTYPE enmAccessType, PGMACCESSORIGIN enmOrigin, void *pvUser);
 # endif
-static void                       hdaDoTransfers(PHDASTATE pThis);
 #endif /* IN_RING3 */
 /** @} */
 
