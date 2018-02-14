4aa528758e084ee69875517245b75b944c077e1e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-02-14 18:42:52 +0000

    Main,Installer: Made unattended installation work for rhel3 and friends.
    svn:sync-xref-src-repo-rev: r120878

--- src/VBox/Main/include/UnattendedInstaller.h
+++ src/VBox/Main/include/UnattendedInstaller.h
@@ -1,4 +1,4 @@
-/* $Id: UnattendedInstaller.h 71011 2018-02-14 16:11:18Z knut.osmundsen@oracle.com $ */
+/* $Id: UnattendedInstaller.h 71018 2018-02-14 18:42:52Z knut.osmundsen@oracle.com $ */
 /** @file
  * UnattendedInstaller class header
  */
@@ -527,13 +527,24 @@ public:
 
 
 /**
- * RHEL 5 installer (same as RHEL 6 & 7, except for the kickstart template).
+ * RHEL 4 installer (same as RHEL 6 & 7, except for the kickstart template).
  */
 class UnattendedRhel4Installer : public UnattendedRhel6And7Installer
 {
 public:
     UnattendedRhel4Installer(Unattended *pParent) : UnattendedRhel6And7Installer(pParent, "rhel4_ks.cfg") {}
     ~UnattendedRhel4Installer() {}
+};
+
+
+/**
+ * RHEL 3 installer (same as RHEL 6 & 7, except for the kickstart template).
+ */
+class UnattendedRhel3Installer : public UnattendedRhel6And7Installer
+{
+public:
+    UnattendedRhel3Installer(Unattended *pParent) : UnattendedRhel6And7Installer(pParent, "rhel3_ks.cfg") {}
+    ~UnattendedRhel3Installer() {}
 };
 
 
