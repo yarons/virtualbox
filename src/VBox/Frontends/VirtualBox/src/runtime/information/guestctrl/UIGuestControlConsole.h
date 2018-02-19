bc39f4a994e8dd0821e496ebd9f93b096cfbc079
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-02-19 12:16:09 +0000

    FE/Qt: bugref:6699 Adding a context menu to tree widget to close a session of terminate a process
    svn:sync-xref-src-repo-rev: r120912

--- src/VBox/Frontends/VirtualBox/src/runtime/information/guestctrl/UIGuestControlConsole.h
+++ src/VBox/Frontends/VirtualBox/src/runtime/information/guestctrl/UIGuestControlConsole.h
@@ -1,4 +1,4 @@
-/* $Id: UIGuestControlConsole.h 71025 2018-02-15 14:11:16Z klaus.espenlaub@oracle.com $ */
+/* $Id: UIGuestControlConsole.h 71049 2018-02-19 12:16:09Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIGuestControlConsole class declaration.
  */
@@ -21,18 +21,20 @@
 /* Qt includes: */
 # include <QPlainTextEdit>
 
+/** QPlainTextEdit extension to provide a simple terminal like widget. */
 class UIGuestControlConsole : public QPlainTextEdit
 {
 
     Q_OBJECT;
 
 signals:
-
+    /* This is emitted when return key is pressed */
     void commandEntered(QString command);
 
 public:
 
     UIGuestControlConsole(QWidget* parent = 0);
+    /* @p strOutput is displayed in the console */
     void putOutput(const QString &strOutput);
 
 protected:
@@ -46,23 +48,20 @@ private slots:
 
 
 private:
+
     typedef QVector<QString> CommandHistory;
-    int      currentLineNumber() const;
-    void     reset();
-    void     startNextLine();
+    void        reset();
+    void        startNextLine();
     /** Return the text of the curent line */
-    QString  getCommandString();
+    QString     getCommandString();
     /** Replaces the content of the last line with m_strPromt + @p stringNewContent */
-    void     replaceLineContent(const QString &stringNewContent);
+    void        replaceLineContent(const QString &stringNewContent);
     /** Get next/prev command from history. Return @p originalString if history is empty. */
     QString     getNextCommandFromHistory(const QString &originalString = QString());
     QString     getPreviousCommandFromHistory(const QString &originalString = QString());
-    void        printCommandHistory() const;
 
-    /** Stores the line number the edit cursor */
-    unsigned m_uLineNumber;
-    const QString m_strGreet;
-    const QString m_strPrompt;
+    const QString  m_strGreet;
+    const QString  m_strPrompt;
 
     /* A vector of entered commands */
     CommandHistory m_tCommandHistory;
