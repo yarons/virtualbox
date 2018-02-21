130e392e262af2af152a7e2ce4a62b1872442ead
Dmitrii Grigorev <dmitrii.grigorev@oracle.com>
2018-02-21 13:34:03 +0000

    WDDM/VBoxTray: bugref:8444: Fixed compilation error caused by typo
    svn:sync-xref-src-repo-rev: r120950

--- src/VBox/Additions/WINNT/VBoxTray/VBoxDispIf.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxDispIf.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxDispIf.cpp 71085 2018-02-21 13:24:04Z dmitrii.grigorev@oracle.com $ */
+/* $Id: VBoxDispIf.cpp 71086 2018-02-21 13:34:03Z dmitrii.grigorev@oracle.com $ */
 /** @file
  * VBoxTray - Display Settings Interface abstraction for XPDM & WDDM
  */
@@ -1511,7 +1511,7 @@ DWORD vboxDispIfResizeModesWDDM(PCVBOXDISPIF const pIf, UINT iChangedMode, BOOL 
     /* Whether the current display is already or should be enabled. */
     BOOL fChangedEnable = fEnable || RT_BOOL(paDisplayDevices[iChangedMode].StateFlags & DISPLAY_DEVICE_ACTIVE);
 
-    if (fCurrentEnable)
+    if (fChangedEnable)
     {
         RTRECTSIZE Size;
 
