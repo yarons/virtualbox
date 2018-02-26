9e0371c8795d2afcf70ecc4f3320d864bfbae600
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-02-26 10:33:45 +0000

    VBoxService/Toolbox: Bumped copyright.
    svn:sync-xref-src-repo-rev: r121000

--- src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
@@ -1,10 +1,10 @@
-/* $Id: VBoxServiceToolBox.cpp 71120 2018-02-26 10:31:13Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxServiceToolBox.cpp 71121 2018-02-26 10:33:45Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxServiceToolbox - Internal (BusyBox-like) toolbox.
  */
 
 /*
- * Copyright (C) 2012-2017 Oracle Corporation
+ * Copyright (C) 2012-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
