ab69efc26d7a784a887a8a763ed199a9f4d64767
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-03-05 17:46:54 +0000

    FE/Qt bugref::6699 Windows build fix
    svn:sync-xref-src-repo-rev: r121118

--- src/VBox/Frontends/VirtualBox/src/runtime/information/guestctrl/UIGuestControlFileTable.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/information/guestctrl/UIGuestControlFileTable.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGuestControlFileTable.cpp 71208 2018-03-05 17:39:39Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIGuestControlFileTable.cpp 71209 2018-03-05 17:46:54Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIGuestControlFileTable class implementation.
  */
@@ -875,9 +875,9 @@ void UIHostFileTable::readDirectory(const QString& strPath, UIFileTableItem *par
     updateCurrentLocationEdit(strPath);
 }
 
-void UIHostFileTable::deleteByItem(UIFileTableItem */*item */)
+void UIHostFileTable::deleteByItem(UIFileTableItem *item)
 {
-
+    Q_UNUSED(item);
 }
 
 #include "UIGuestControlFileTable.moc"
