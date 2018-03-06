2fcdc0e706f687c1e669408dfebc8e234fce6b78
Valery Portnyagin <valery.portnyagin@oracle.com>
2018-03-06 10:00:14 +0000

    bugref:8345. Try to fix the length of the string.
    svn:sync-xref-src-repo-rev: r121139

--- src/VBox/ValidationKit/tests/api/tdApi1.py
+++ src/VBox/ValidationKit/tests/api/tdApi1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdApi1.py 71193 2018-03-05 10:27:41Z valery.portnyagin@oracle.com $
+# $Id: tdApi1.py 71229 2018-03-06 10:00:14Z valery.portnyagin@oracle.com $
 
 """
 VirtualBox Validation Kit - API Test wrapper #1 combining all API sub-tests
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 71193 $"
+__version__ = "$Revision: 71229 $"
 
 
 # Standard Python imports.
@@ -84,5 +84,6 @@ if __name__ == '__main__':
     from tdMoveMedium1 import SubTstDrvMoveMedium1
     from tdTreeDepth1 import SubTstDrvTreeDepth1
     from tdMoveVM import SubTstDrvMoveVM
-    sys.exit(tdApi1([SubTstDrvPython1, SubTstDrvAppliance1, SubTstDrvMoveMedium1, SubTstDrvTreeDepth1, SubTstDrvMoveVM]).main(sys.argv))
+    sys.exit(tdApi1([SubTstDrvPython1, SubTstDrvAppliance1, SubTstDrvMoveMedium1, 
+                     SubTstDrvTreeDepth1, SubTstDrvMoveVM]).main(sys.argv))
 
