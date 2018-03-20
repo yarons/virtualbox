2ecc7025ae060060dd764b464960dd67d03daf8c
Sergey Dubov <sergey.dubov@oracle.com>
2018-03-20 11:22:11 +0000

    FE/Qt: bugref:9049: Small cleanup for QIArrowButtonPress.
    svn:sync-xref-src-repo-rev: r121358

--- src/VBox/Frontends/VirtualBox/src/extensions/QIArrowButtonPress.h
+++ src/VBox/Frontends/VirtualBox/src/extensions/QIArrowButtonPress.h
@@ -1,10 +1,10 @@
-/* $Id: QIArrowButtonPress.h 69500 2017-10-28 15:14:05Z knut.osmundsen@oracle.com $ */
+/* $Id: QIArrowButtonPress.h 71393 2018-03-20 11:22:11Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - QIArrowButtonPress class declaration.
  */
 
 /*
- * Copyright (C) 2006-2017 Oracle Corporation
+ * Copyright (C) 2006-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -34,22 +34,23 @@ public:
     /** Button types. */
     enum ButtonType { ButtonType_Back, ButtonType_Next };
 
-    /** Constructor, passes @a pParent to the QIRichToolButton constructor.
-      * @param buttonType is used to define which type of the button it is. */
-    QIArrowButtonPress(ButtonType buttonType, QWidget *pParent = 0);
+    /** Constructs button passing @a pParent to the base-class.
+      * @param  enmButtonType  Brings which type of the button it is. */
+    QIArrowButtonPress(ButtonType enmButtonType, QWidget *pParent = 0);
 
 protected:
 
-    /** Retranslation routine. */
-    virtual void retranslateUi();
+    /** Handles translation event. */
+    virtual void retranslateUi() /* override */;
 
-    /** Key-press-event handler. */
-    virtual void keyPressEvent(QKeyEvent *pEvent);
+    /** Handles key-press @a pEvent. */
+    virtual void keyPressEvent(QKeyEvent *pEvent) /* override */;
 
 private:
 
     /** Holds the button-type. */
-    ButtonType m_buttonType;
+    ButtonType m_enmButtonType;
 };
 
 #endif /* !___QIArrowButtonPress_h___ */
+
