b67b13e78b3d89f3c6f0f01ee1d116233bb350ea
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-04-12 12:23:29 +0000

    Guest Control/Main: More bugfixes for copyTo/copyFrom tasks, along with test cases.
    svn:sync-xref-src-repo-rev: r122055

--- src/VBox/Main/include/GuestSessionImplTasks.h
+++ src/VBox/Main/include/GuestSessionImplTasks.h
@@ -1,4 +1,4 @@
-/* $Id: GuestSessionImplTasks.h 71824 2018-04-11 12:37:42Z andreas.loeffler@oracle.com $ */
+/* $Id: GuestSessionImplTasks.h 71847 2018-04-12 12:23:29Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox Main - Guest session tasks header.
  */
@@ -115,6 +115,8 @@ protected:
     /** Progress object for getting updated when running
      *  asynchronously. Optional. */
     ComObjPtr<Progress>     mProgress;
+    /** The guest's path style (depending on the guest OS type set). */
+    uint32_t                mfPathStyle;
 };
 
 /**
@@ -143,18 +145,21 @@ class GuestSessionCopyTask : public GuestSessionTask
 public:
 
     GuestSessionCopyTask(GuestSession *pSession)
-        : GuestSessionTask(pSession) { RT_ZERO(u); }
+        : GuestSessionTask(pSession)
+    {
+        RT_ZERO(u);
+    }
 
     virtual ~GuestSessionCopyTask() { }
 
 protected:
 
     /** Source to copy from. */
-    Utf8Str mSource;
+    Utf8Str  mSource;
     /** Destination to copy to. */
-    Utf8Str mDest;
+    Utf8Str  mDest;
     /** Filter (wildcard-style) when copying directories. */
-    Utf8Str mFilter;
+    Utf8Str  mFilter;
 
     union
     {
