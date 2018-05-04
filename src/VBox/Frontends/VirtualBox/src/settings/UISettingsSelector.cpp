3086196370b3d60f15ba5e5a6bf86a909bc52a93
Sergey Dubov <sergey.dubov@oracle.com>
2018-05-04 08:27:54 +0000

    FE/Qt: bugref:9049: Tiny cleanup for UISettingsSelector classes and move them to VBoxGlobal library.
    svn:sync-xref-src-repo-rev: r122494

--- src/VBox/Frontends/VirtualBox/src/settings/UISettingsSelector.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/UISettingsSelector.cpp
@@ -1,10 +1,10 @@
-/* $Id: UISettingsSelector.cpp 69500 2017-10-28 15:14:05Z knut.osmundsen@oracle.com $ */
+/* $Id: UISettingsSelector.cpp 72102 2018-05-04 08:27:54Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UISettingsSelector class implementation.
  */
 
 /*
- * Copyright (C) 2008-2017 Oracle Corporation
+ * Copyright (C) 2008-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -815,4 +815,3 @@ UISelectorActionItem *UISettingsSelectorToolBar::findActionItemByTabWidget(QTabW
     return pResult;
 
 }
-
