050c202693f8d9f90a75367dad3c17a4e661ed97
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2018-05-07 14:29:41 +0000

    build fix
    svn:sync-xref-src-repo-rev: r122539

--- src/VBox/Runtime/common/time/time.cpp
+++ src/VBox/Runtime/common/time/time.cpp
@@ -1,4 +1,4 @@
-/* $Id: time.cpp 72140 2018-05-07 14:21:31Z klaus.espenlaub@oracle.com $ */
+/* $Id: time.cpp 72141 2018-05-07 14:29:41Z klaus.espenlaub@oracle.com $ */
 /** @file
  * IPRT - Time.
  */
@@ -1100,7 +1100,7 @@ static PRTTIME rtTimeConvertToZulu(PRTTIME pTime)
  * @returns NULL if the data is invalid.
  * @param   pTime       The time structure to convert.
  */
-PRTTIME RTTimeConvertToZulu(PRTTIME pTime)
+RTDECL(PRTTIME) RTTimeConvertToZulu(PRTTIME pTime)
 {
     /*
      * Validate that we've got the minimum of stuff handy.
@@ -1110,6 +1110,7 @@ PRTTIME RTTimeConvertToZulu(PRTTIME pTime)
 
     return rtTimeConvertToZulu(rtTimeNormalizeInternal(pTime));
 }
+RT_EXPORT_SYMBOL(RTTimeConvertToZulu);
 
 
 /**
