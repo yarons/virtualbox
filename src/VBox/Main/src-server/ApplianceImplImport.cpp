0c358b19b5b6473bd451729dec1eb8aac818cac9
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2018-05-14 13:22:22 +0000

    build fix
    svn:sync-xref-src-repo-rev: r122640

--- src/VBox/Main/src-server/ApplianceImplImport.cpp
+++ src/VBox/Main/src-server/ApplianceImplImport.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImplImport.cpp 72200 2018-05-14 13:13:52Z klaus.espenlaub@oracle.com $ */
+/* $Id: ApplianceImplImport.cpp 72201 2018-05-14 13:22:22Z klaus.espenlaub@oracle.com $ */
 /** @file
  * IAppliance and IVirtualSystem COM class implementations.
  */
@@ -2150,7 +2150,9 @@ HRESULT Appliance::i_importDoIt(TaskOVF *pTask, AutoWriteLockBase &rWriteLock, R
             /* If everything was successful so far check if some extension
              * pack wants to do file sanity checking. */
             if (SUCCEEDED(hrc))
+            {
                 /** @todo */;
+            }
         }
         catch (HRESULT hrcXcpt)
         {
