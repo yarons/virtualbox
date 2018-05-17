dbf0d460e71d51e8635df122018da2b48ec40d62
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-05-17 11:38:01 +0000

    FE/Qt bugref:6769: Dont set content margins in UIVMNamePathSelector
    svn:sync-xref-src-repo-rev: r122681

--- src/VBox/Frontends/VirtualBox/src/widgets/UIVMNamePathSelector.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIVMNamePathSelector.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVMNamePathSelector.cpp 72228 2018-05-17 08:18:56Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMNamePathSelector.cpp 72235 2018-05-17 11:38:01Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMNamePathSelector class implementation.
  */
@@ -127,11 +127,8 @@ void UIVMNamePathSelector::prepareWidgets()
         return;
             /* Configure layout: */
 #ifdef VBOX_WS_MAC
-            m_pMainLayout->setContentsMargins(0, 0, 0, 0);
             m_pMainLayout->setSpacing(0);
 #else
-            m_pMainLayout->setContentsMargins(qApp->style()->pixelMetric(QStyle::PM_LayoutLeftMargin) / 2, 0,
-                                                 qApp->style()->pixelMetric(QStyle::PM_LayoutRightMargin) / 2, 0);
             m_pMainLayout->setSpacing(qApp->style()->pixelMetric(QStyle::PM_LayoutHorizontalSpacing) / 2);
 #endif
     setLayout(m_pMainLayout);
