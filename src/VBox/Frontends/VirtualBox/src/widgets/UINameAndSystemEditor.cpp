3cc2dc0e90d4ff3ffb32c844134c5838ef56c4df
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-05-24 08:31:14 +0000

    FE/Qt: ​bugref:6769 In the new vm wizard making the path/name fields extend to 3rd column
    svn:sync-xref-src-repo-rev: r122766

--- src/VBox/Frontends/VirtualBox/src/widgets/UINameAndSystemEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UINameAndSystemEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINameAndSystemEditor.cpp 72288 2018-05-22 14:29:53Z serkan.bayraktar@oracle.com $ */
+/* $Id: UINameAndSystemEditor.cpp 72313 2018-05-24 08:31:14Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UINameAndSystemEditor class implementation.
  */
@@ -240,7 +240,7 @@ void UINameAndSystemEditor::prepareWidgets()
         m_pNameLineEdit = new QILineEdit;
         if (m_pNameLineEdit)
         {
-            pMainLayout->addWidget(m_pNameLineEdit, 0, 1, 1, 1);
+            pMainLayout->addWidget(m_pNameLineEdit, 0, 1, 1, 2);
         }
 
         m_pPathLabel = new QLabel;
@@ -254,7 +254,7 @@ void UINameAndSystemEditor::prepareWidgets()
         m_pPathSelector = new UIFilePathSelector;
         if (m_pPathSelector)
         {
-            pMainLayout->addWidget(m_pPathSelector, 1, 1, 1, 1);
+            pMainLayout->addWidget(m_pPathSelector, 1, 1, 1, 2);
             QString strDefaultMachineFolder = vboxGlobal().virtualBox().GetSystemProperties().GetDefaultMachineFolder();
             m_pPathSelector->setPath(strDefaultMachineFolder);
             m_pPathSelector->setDefaultPath(strDefaultMachineFolder);
