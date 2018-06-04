1599247065dfe45a5a7cb62c3ca6d5c1ada28846
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-06-04 08:01:05 +0000

    FE/Qt bugref:9183 Showing the differencing disks as children of the mard drives in medium selection widget
    svn:sync-xref-src-repo-rev: r122895

--- src/VBox/Frontends/VirtualBox/src/medium/UIMediumSelector.h
+++ src/VBox/Frontends/VirtualBox/src/medium/UIMediumSelector.h
@@ -1,4 +1,4 @@
-/* $Id: UIMediumSelector.h 72410 2018-06-01 12:48:18Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIMediumSelector.h 72423 2018-06-04 08:01:05Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMediumSelector class declaration.
  */
@@ -87,6 +87,8 @@ private:
     void updateOkButton();
     UIMediumItem* addTreeItem(const UIMedium &medium, QITreeWidgetItem *pParent);
     void restoreSelection(const QStringList &selectedMediums, QVector<UIMediumItem*> &mediumList);
+    UIMediumItem* createHardDiskItem(const UIMedium &medium);
+    UIMediumItem* searchItem(const QTreeWidgetItem *pParent, const QString &mediumId);
 
     QVBoxLayout       *m_pMainLayout;
     QITreeWidget      *m_pTreeWidget;
