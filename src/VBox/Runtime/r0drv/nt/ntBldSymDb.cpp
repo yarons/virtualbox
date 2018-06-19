3f60ee9044a92708a0a7fa6048cb984aaca431df
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-06-19 14:23:17 +0000

    Windows 17134/64 sym update.
    svn:sync-xref-src-repo-rev: r123112

--- src/VBox/Runtime/r0drv/nt/ntBldSymDb.cpp
+++ src/VBox/Runtime/r0drv/nt/ntBldSymDb.cpp
@@ -1,4 +1,4 @@
-﻿/* $Id: ntBldSymDb.cpp 69753 2017-11-19 14:27:58Z knut.osmundsen@oracle.com $ */
+﻿/* $Id: ntBldSymDb.cpp 72614 2018-06-19 14:23:17Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - RTDirCreateUniqueNumbered, generic implementation.
  */
@@ -796,6 +796,8 @@ static RTEXITCODE FigurePdbVersionInfo(const char *pszPdb, PRTNTSDBOSVER pVerInf
             { RT_STR_TUPLE("Windows_Win8.9200"),                6, 2, 0, 9200 }, /* RTM */
             { RT_STR_TUPLE("en_windows_8_1"),                   6, 3, 0, 9600 }, /* RTM */
             { RT_STR_TUPLE("en_windows_10_symbols_"),          10, 0, 0,10240 }, /* RTM */
+            { RT_STR_TUPLE("en_windows_10_symbols_"),          10, 0, 0,10240 }, /* RTM */
+            { RT_STR_TUPLE("en_windows_10_17134_"),            10, 0, 0,17134 }, /* 1803 */
         };
 
         const char *pszComp  = u.Split.apszComps[i];
@@ -1152,7 +1154,7 @@ int main(int argc, char **argv)
                 break;
 
             case 'V':
-                RTPrintf("$Revision: 69753 $");
+                RTPrintf("$Revision: 72614 $");
                 break;
 
             case 'h':
