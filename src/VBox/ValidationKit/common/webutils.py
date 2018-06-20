211df6d1539ee86f0cba06c22c54515daa536778
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-06-20 15:44:09 +0000

    webutils.py: Fixed the fNoProxies parameter for downloadFile(). [pylint]
    svn:sync-xref-src-repo-rev: r123136

--- src/VBox/ValidationKit/common/webutils.py
+++ src/VBox/ValidationKit/common/webutils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: webutils.py 72632 2018-06-20 15:42:03Z knut.osmundsen@oracle.com $
+# $Id: webutils.py 72633 2018-06-20 15:44:09Z knut.osmundsen@oracle.com $
 
 """
 Common Web Utility Functions.
@@ -26,7 +26,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 72632 $"
+__version__ = "$Revision: 72633 $"
 
 # Standard Python imports.
 import os;
@@ -163,8 +163,8 @@ def downloadFile(sUrlFile, sDstFile, sLocalPrefix, fnLog, fnError = None, fNoPro
             elif sys.version_info[0] < 3:
                 oSrc = urllib_urlopen(sUrlFile, proxies = dict());
             else:
-                oProxyHandler = urllib.request.ProxyHandler(proxies = dict()); # p?ylint: disable=???
-                oOpener = urllib.request.build_opener(oProxyHandler)           # p?ylint: disable=???
+                oProxyHandler = urllib.request.ProxyHandler(proxies = dict()); # pylint: disable=no-member
+                oOpener = urllib.request.build_opener(oProxyHandler)           # pylint: disable=no-member
                 oSrc = oOpener.open(sUrlFile);
             oDst = utils.openNoInherit(sDstFile, 'wb');
             oDst.write(oSrc.read());
