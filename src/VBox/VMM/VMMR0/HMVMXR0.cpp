e6bdeefefaea5eca8ff1f2b0bb7d9b8cc6d1d6ad
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-06-21 16:07:23 +0000

    VMM/HMVMXR0: Build fix.
    svn:sync-xref-src-repo-rev: r123152

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 72643 2018-06-21 16:02:03Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXR0.cpp 72644 2018-06-21 16:07:23Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -7083,9 +7083,9 @@ static int hmR0VmxImportGuestState(PVMCPU pVCpu, PCPUMCTX pCtx, uint64_t fWhat)
     /* RFLAGS and interruptibility-state required while re-evaluating interrupt injection, see hmR0VmxGetGuestIntrState(). */
     if (fWhat & (CPUMCTX_EXTRN_RFLAGS | CPUMCTX_EXTRN_HM_VMX_INT_STATE))
     {
-        rc = VMXReadVmcsGstN(VMX_VMCS_GUEST_RFLAGS, &u64Val);
+        rc = VMXReadVmcs32(VMX_VMCS_GUEST_RFLAGS, &u32Val);
         AssertRCReturn(rc, rc);
-        pCtx->eflags.u32 = u64Val;
+        pCtx->eflags.u32 = u32Val;
         /* Restore eflags for real-on-v86-mode hack. */
         if (pVCpu->hm.s.vmx.RealMode.fRealOnV86Active)
         {
