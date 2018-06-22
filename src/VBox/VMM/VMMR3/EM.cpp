2a35466b5665b4b24069270ea0a3bac15f7efd77
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-06-22 11:16:33 +0000

    EM: Some tuning for w2ksp4 on SVM. bugref:9198
    svn:sync-xref-src-repo-rev: r123169

--- src/VBox/VMM/VMMR3/EM.cpp
+++ src/VBox/VMM/VMMR3/EM.cpp
@@ -1,4 +1,4 @@
-/* $Id: EM.cpp 72657 2018-06-22 11:05:11Z knut.osmundsen@oracle.com $ */
+/* $Id: EM.cpp 72658 2018-06-22 11:16:33Z knut.osmundsen@oracle.com $ */
 /** @file
  * EM - Execution Monitor / Manager.
  */
@@ -184,13 +184,13 @@ VMMR3_INT_DECL(int) EMR3Init(PVM pVM)
         pVM->aCpus[i].em.s.fExitOptimizationEnabledR0PreemptDisabled = fExitOptimizationEnabledR0PreemptDisabled;
 
         pVM->aCpus[i].em.s.cHistoryExecMaxInstructions               = 8192;
-        pVM->aCpus[i].em.s.cHistoryProbeMinInstructions              = 48;
+        pVM->aCpus[i].em.s.cHistoryProbeMinInstructions              = 75;
         pVM->aCpus[i].em.s.cHistoryProbeMaxInstructionsWithoutExit   = 24;
 #ifdef RT_OS_WINDOWS
         if (VM_IS_NEM_ENABLED(pVM))
         {
             pVM->aCpus[i].em.s.cHistoryProbeMaxInstructionsWithoutExit = 32;
-            pVM->aCpus[i].em.s.cHistoryProbeMinInstructions            = 64;
+            pVM->aCpus[i].em.s.cHistoryProbeMinInstructions            = 99;
         }
 #endif
     }
