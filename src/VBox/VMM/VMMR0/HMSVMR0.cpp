21b802c1f8a246be22f3b8c1a698ec671b1cfa1e
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-06-29 08:57:48 +0000

    VMM: Extend HM changed flags. ​​​​​bugref:9193 [build fix]
    svn:sync-xref-src-repo-rev: r123294

--- src/VBox/VMM/VMMR0/HMSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HMSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMSVMR0.cpp 72744 2018-06-29 07:36:19Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMSVMR0.cpp 72758 2018-06-29 08:57:48Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM SVM (AMD-V) - Host Context Ring-0.
  */
@@ -518,7 +518,7 @@ static void hmR0SvmLogState(PVMCPU pVCpu, PCSVMVMCB pVmcb, const char *pszPrefix
         Log4(("%s: br_from=%#RX64 br_to=%#RX64 lastxcpt_from=%#RX64 lastxcpt_to=%#RX64\n", pszPrefix, pVmcbGuest->u64BR_FROM,
               pVmcbGuest->u64BR_TO, pVmcbGuest->u64LASTEXCPFROM, pVmcbGuest->u64LASTEXCPTO));
     }
-    NOREF(pVmcbGuest);
+    NOREF(pVmcbGuest); NOREF(pCtx);
 }
 #endif  /* VBOX_STRICT */
 
