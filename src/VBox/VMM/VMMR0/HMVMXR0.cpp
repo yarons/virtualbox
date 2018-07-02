ffaea751b1b224858f474e6083cd9411d1cd48ba
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-07-02 09:15:09 +0000

    VMM/HMVMXR0: Use atomic Uo when writing to fCtxChanged in preemptible contexts.
    svn:sync-xref-src-repo-rev: r123332

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 72789 2018-07-02 09:13:58Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXR0.cpp 72790 2018-07-02 09:15:09Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -8158,7 +8158,7 @@ static VBOXSTRICTRC hmR0VmxExportGuestStateOptimal(PVM pVM, PVMCPU pVCpu, PCCPUM
     Assert(VMMR0IsLogFlushDisabled(pVCpu));
 
 #ifdef HMVMX_ALWAYS_SYNC_FULL_GUEST_STATE
-    pVCpu->hm.s.fCtxChanged |= HM_CHANGED_ALL_GUEST;
+    ASMAtomicUoOrU64(&pVCpu->hm.s.fCtxChanged, HM_CHANGED_ALL_GUEST);
 #endif
 
     /*
