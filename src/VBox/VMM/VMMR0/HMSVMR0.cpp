3cb07fd5cd9969c212c81f9895a57ad5b1b93c57
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-07-08 05:57:32 +0000

    VMM/HMSVMR0: vmload instruction modifies Kernel-GS-Base MSR as well as SYSCALL MSRs.
    svn:sync-xref-src-repo-rev: r123516

--- src/VBox/VMM/VMMR0/HMSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HMSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMSVMR0.cpp 72963 2018-07-08 05:56:09Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMSVMR0.cpp 72964 2018-07-08 05:57:32Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM SVM (AMD-V) - Host Context Ring-0.
  */
@@ -7785,6 +7785,7 @@ HMSVM_EXIT_DECL hmR0SvmExitVmload(PVMCPU pVCpu, PCPUMCTX pCtx, PSVMTRANSIENT pSv
     {
         ASMAtomicUoOrU64(&pVCpu->hm.s.fCtxChanged, HM_CHANGED_GUEST_FS              | HM_CHANGED_GUEST_GS
                                                  | HM_CHANGED_GUEST_TR              | HM_CHANGED_GUEST_LDTR
+                                                 | HM_CHANGED_GUEST_KERNEL_GS_BASE  | HM_CHANGED_GUEST_SYSCALL_MSRS
                                                  | HM_CHANGED_GUEST_SYSENTER_MSR_MASK);
     }
     return VBOXSTRICTRC_VAL(rcStrict);
