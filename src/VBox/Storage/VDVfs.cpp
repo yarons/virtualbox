2aad1aadaaa8666b208017e21abf67873571eba8
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-07-09 07:43:53 +0000

    VDVfs.cpp: Fixed off by one issue in vdVfsFile_Write.
    svn:sync-xref-src-repo-rev: r123549

--- src/VBox/Storage/VDVfs.cpp
+++ src/VBox/Storage/VDVfs.cpp
@@ -1,4 +1,4 @@
-/* $Id: VDVfs.cpp 70307 2017-12-22 08:37:01Z knut.osmundsen@oracle.com $ */
+/* $Id: VDVfs.cpp 72997 2018-07-09 07:43:53Z knut.osmundsen@oracle.com $ */
 /** @file
  * Virtual Disk Container implementation. - VFS glue.
  */
@@ -361,7 +361,7 @@ static DECLCALLBACK(int) vdVfsFile_Write(void *pvThis, RTFOFF off, PCRTSGBUF pSg
     }
 
     size_t cbLeftToWrite;
-    if (offUnsigned + pSgBuf->paSegs[0].cbSeg < cbImage)
+    if (offUnsigned + pSgBuf->paSegs[0].cbSeg <= cbImage)
     {
         cbLeftToWrite = pSgBuf->paSegs[0].cbSeg;
         if (pcbWritten)
