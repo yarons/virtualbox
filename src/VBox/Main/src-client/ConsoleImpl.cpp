f1af4e9f2d3df64311cc4d92c38fd6d73e402f2a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-07-09 11:20:34 +0000

    Main: Use setErrorBoth when we've got a VBox status code handy. (The COM status codes aren't too specfic and this may help us decode error messages and provide an alternative to strstr for API clients. setErrorBoth isn't new, btw.) [build fix]
    svn:sync-xref-src-repo-rev: r123557

--- src/VBox/Main/src-client/ConsoleImpl.cpp
+++ src/VBox/Main/src-client/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 73003 2018-07-09 11:09:32Z knut.osmundsen@oracle.com $ */
+/* $Id: ConsoleImpl.cpp 73005 2018-07-09 11:20:34Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation
  */
@@ -9337,7 +9337,7 @@ HRESULT Console::i_attachToTapInterface(INetworkAdapter *networkAdapter)
     if (RT_FAILURE(vrc) && SUCCEEDED(rc))
     {
         LogRel(("General failure attaching to host interface\n"));
-        rc = setError(E_FAIL, vrc, tr("General failure attaching to host interface"));
+        rc = setErrorBoth(E_FAIL, vrc, tr("General failure attaching to host interface"));
     }
     LogFlowThisFunc(("rc=%Rhrc\n", rc));
     return rc;
