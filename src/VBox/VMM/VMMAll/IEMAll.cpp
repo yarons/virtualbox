30699072adf69b21de811f2f1f4af2302b42e148
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-07-10 10:00:26 +0000

    VMM/IEM: IEMExecDecodedMovCRxRead require CR3, CR4 and APIC TPR in addition to IEM_CPUMCTX_EXTRN_EXEC_DECODED_NO_MEM_MASK. CR2 is fetched on demand.
    svn:sync-xref-src-repo-rev: r123578

--- src/VBox/VMM/VMMAll/IEMAll.cpp
+++ src/VBox/VMM/VMMAll/IEMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAll.cpp 73024 2018-07-10 09:59:24Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: IEMAll.cpp 73026 2018-07-10 10:00:26Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager - All Contexts.
  */
@@ -14837,6 +14837,8 @@ VMM_INT_DECL(VBOXSTRICTRC) IEMExecDecodedMovCRxWrite(PVMCPU pVCpu, uint8_t cbIns
 VMM_INT_DECL(VBOXSTRICTRC) IEMExecDecodedMovCRxRead(PVMCPU pVCpu, uint8_t cbInstr, uint8_t iGReg, uint8_t iCrReg)
 {
     IEMEXEC_ASSERT_INSTR_LEN_RETURN(cbInstr, 2);
+    IEM_CTX_ASSERT(pVCpu, IEM_CPUMCTX_EXTRN_EXEC_DECODED_NO_MEM_MASK | CPUMCTX_EXTRN_CR3 | CPUMCTX_EXTRN_CR4
+                        | CPUMCTX_EXTRN_APIC_TPR);
     Assert(iCrReg < 16);
     Assert(iGReg < 16);
 
