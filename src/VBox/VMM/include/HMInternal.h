2cba831011f02f339a7d32071d8cfedc486e26a7
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-07-10 04:38:01 +0000

    VMM/HMSVMR0: Fallback to IEMExecOne() on certain VM-exits on CPUs that do not provide the instruction length. Fixes setting HM_CHANGED_XXX flags on VINF_IEM_RAISED_XCPT on some VM-exits. Fixes an incorrect assertion in hmR0SvmCallRing3Callback().
    svn:sync-xref-src-repo-rev: r123567

--- src/VBox/VMM/include/HMInternal.h
+++ src/VBox/VMM/include/HMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: HMInternal.h 72988 2018-07-09 02:27:49Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMInternal.h 73015 2018-07-10 04:38:01Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM - Internal header file.
  */
@@ -156,7 +156,7 @@ RT_C_DECLS_BEGIN
                                                   | HM_CHANGED_KEEPER_STATE_MASK)
 
 /** Mask of what state might have changed when IEM raised an exception.
-  This is a combination IEM_CPUMCTX_EXTRN_MUST_MASK and IEM_CPUMCTX_EXTRN_XCPT_MASK. */
+ *  This is a based on IEM_CPUMCTX_EXTRN_XCPT_MASK. */
 #define HM_CHANGED_XCPT_RAISED_MASK              (  HM_CHANGED_GUEST_GPRS_MASK  \
                                                   | HM_CHANGED_GUEST_RIP        \
                                                   | HM_CHANGED_GUEST_RFLAGS     \
