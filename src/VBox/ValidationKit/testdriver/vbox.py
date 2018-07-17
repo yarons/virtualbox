9c11d36984f57a737811557b95add769eeb184f1
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-07-17 09:29:17 +0000

    ValidationKit/testdriver: Include 'exits' to the info dump.
    svn:sync-xref-src-repo-rev: r123773

--- src/VBox/ValidationKit/testdriver/vbox.py
+++ src/VBox/ValidationKit/testdriver/vbox.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: vbox.py 72732 2018-06-29 06:53:30Z knut.osmundsen@oracle.com $
+# $Id: vbox.py 73171 2018-07-17 09:29:17Z ramshankar.venkataraman@oracle.com $
 # pylint: disable=C0302
 
 """
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 72732 $"
+__version__ = "$Revision: 73171 $"
 
 
 # Standard Python imports.
@@ -2817,6 +2817,7 @@ class TestDriver(base.TestDriver):                                              
                                      ('fflags', ''),
                                      ('cpumguest', 'verbose all'),
                                      ('cpumguestinstr', 'symbol all'),
+                                     ('exits', ''),
                                      ('pic', ''),
                                      ('apic', ''),
                                      ('apiclvt', ''),
