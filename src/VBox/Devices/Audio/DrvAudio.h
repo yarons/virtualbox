aa3a0ae193990d040db6c69bb2f018c4f313c3f7
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-07-18 15:00:47 +0000

    Audio/DrvAudio: Don't use float / size_t in DrvAudioHlpBytesToMs() and DrvAudioHlpFramesToMs().
    svn:sync-xref-src-repo-rev: r123813

--- src/VBox/Devices/Audio/DrvAudio.h
+++ src/VBox/Devices/Audio/DrvAudio.h
@@ -1,4 +1,4 @@
-/* $Id: DrvAudio.h 73207 2018-07-18 14:56:36Z andreas.loeffler@oracle.com $ */
+/* $Id: DrvAudio.h 73208 2018-07-18 15:00:47Z andreas.loeffler@oracle.com $ */
 /** @file
  * Intermediate audio driver header.
  */
@@ -154,9 +154,9 @@ void DrvAudioHlpClearBuf(const PPDMAUDIOPCMPROPS pPCMInfo, void *pvBuf, size_t c
 uint32_t DrvAudioHlpCalcBitrate(uint8_t cBits, uint32_t uHz, uint8_t cChannels);
 uint32_t DrvAudioHlpCalcBitrate(const PPDMAUDIOPCMPROPS pProps);
 uint32_t DrvAudioHlpBytesToFrames(const PPDMAUDIOPCMPROPS pProps, uint32_t cbBytes);
-uint64_t DrvAudioHlpBytesToMs(const PPDMAUDIOPCMPROPS pProps, size_t cbBytes);
+uint64_t DrvAudioHlpBytesToMs(const PPDMAUDIOPCMPROPS pProps, uint32_t cbBytes);
 uint32_t DrvAudioHlpFramesToBytes(const PPDMAUDIOPCMPROPS pProps, uint32_t cFrames);
-float DrvAudioHlpFramesToMs(const PPDMAUDIOPCMPROPS pProps, size_t cFrames);
+uint64_t DrvAudioHlpFramesToMs(const PPDMAUDIOPCMPROPS pProps, uint32_t cFrames);
 uint32_t DrvAudioHlpMsToBytes(const PPDMAUDIOPCMPROPS pProps, uint32_t uMs);
 uint32_t DrvAudioHlpMsToFrames(const PPDMAUDIOPCMPROPS pProps, uint32_t uMs);
 bool DrvAudioHlpPCMPropsAreEqual(const PPDMAUDIOPCMPROPS pPCMProps1, const PPDMAUDIOPCMPROPS pPCMProps2);
