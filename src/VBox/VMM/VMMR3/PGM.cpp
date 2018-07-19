f51eecc38bde3c4b253997c9e718a4e0166078d6
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-07-19 15:57:49 +0000

    PGM: Working on eliminating PGMMODEDATA and the corresponding PGMCPU section so we can do mode switching in ring-0. This second part deals with shadow paging pointers and expands PGM_TYPE_NESTED & PGMMODE_NESTED into 32BIT, PAE and AMD64 variants to better map to reality at the expense of a little bit of more code.  bugref:9044 [build fix]
    svn:sync-xref-src-repo-rev: r123853

--- src/VBox/VMM/VMMR3/PGM.cpp
+++ src/VBox/VMM/VMMR3/PGM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGM.cpp 73246 2018-07-19 15:51:20Z knut.osmundsen@oracle.com $ */
+/* $Id: PGM.cpp 73247 2018-07-19 15:57:49Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor. (Mixing stuff here, not good?)
  */
@@ -3821,8 +3821,8 @@ VMMR3DECL(int) PGMR3ChangeMode(PVM pVM, PVMCPU pVCpu, PGMMODE enmGuestMode)
     /*
      * Calc the shadow mode and switcher.
      */
-    VMMSWITCHER enmSwitcher;
-    PGMMODE     enmShadowMode;
+    VMMSWITCHER enmSwitcher   = VMMSWITCHER_INVALID;
+    PGMMODE     enmShadowMode = PGMMODE_INVALID;
     enmShadowMode = pgmR3CalcShadowMode(pVM, enmGuestMode, pVM->pgm.s.enmHostMode, pVCpu->pgm.s.enmShadowMode, &enmSwitcher);
 
 #ifdef VBOX_WITH_RAW_MODE
