d0b656009f59d6fc49fddd3de2bbb0a9afb56251
Sergey Dubov <sergey.dubov@oracle.com>
2018-07-23 13:49:09 +0000

    Main: bugref:9152: A bit of fixes for hardcoded json string to improve NLS translation.
    svn:sync-xref-src-repo-rev: r123930

--- src/VBox/Main/src-server/CloudClientImpl.cpp
+++ src/VBox/Main/src-server/CloudClientImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: CloudClientImpl.cpp 73314 2018-07-23 11:11:54Z sergey.dubov@oracle.com $*/
+/* $Id: CloudClientImpl.cpp 73321 2018-07-23 13:49:09Z sergey.dubov@oracle.com $*/
 /** @file
  * ICloudClient  COM class implementations.
  */
@@ -386,7 +386,7 @@ static Utf8Str strExportParametersToOCI = "{\n"
 "\t""\t""\"type\": 29,\n"
 "\t""\t""\"min\": 10,\n"
 "\t""\t""\"max\": 50,\n"
-"\t""\t""\"unit\": \"Gb\"\n"
+"\t""\t""\"unit\": \"GB\"\n"
 "\t""},\n"
 "\t""\"Bucket\": {\n"
 "\t""\t""\"type\": 30,\n"
