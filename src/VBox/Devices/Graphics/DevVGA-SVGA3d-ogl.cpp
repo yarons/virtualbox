8db68609cb72a21c56cbd721fc5be21c5f4d555f
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2018-07-26 08:37:41 +0000

    DevVGA-SVGA3d-ogl.cpp: cleaned up loading OpenGL functions (mac build fix).
    svn:sync-xref-src-repo-rev: r123979

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-ogl.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-ogl.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA3d-ogl.cpp 73363 2018-07-26 08:33:24Z vitali.pelenjow@oracle.com $ */
+/* $Id: DevVGA-SVGA3d-ogl.cpp 73364 2018-07-26 08:37:41Z vitali.pelenjow@oracle.com $ */
 /** @file
  * DevVMWare - VMWare SVGA device
  */
@@ -730,13 +730,13 @@ static int vmsvga3dLoadGLFunctions(PVMSVGA3DSTATE pState)
     }
     else if (vmsvga3dCheckGLExtension(pState, 0.0f, " GL_ARB_draw_instanced "))
     {
-        GLGETPROC_(PFNGLDRAWARRAYSINSTANCEDARBPROC              , glDrawArraysInstanced,   "ARB");
-        GLGETPROC_(PFNGLDRAWELEMENTSINSTANCEDARBPROC            , glDrawElementsInstanced, "ARB");
+        GLGETPROC_(PFNGLDRAWARRAYSINSTANCEDPROC                 , glDrawArraysInstanced,   "ARB");
+        GLGETPROC_(PFNGLDRAWELEMENTSINSTANCEDPROC               , glDrawElementsInstanced, "ARB");
     }
     else if (vmsvga3dCheckGLExtension(pState, 0.0f, " GL_EXT_draw_instanced "))
     {
-        GLGETPROC_(PFNGLDRAWARRAYSINSTANCEDEXTPROC              , glDrawArraysInstanced,   "EXT");
-        GLGETPROC_(PFNGLDRAWELEMENTSINSTANCEDEXTPROC            , glDrawElementsInstanced, "EXT");
+        GLGETPROC_(PFNGLDRAWARRAYSINSTANCEDPROC                 , glDrawArraysInstanced,   "EXT");
+        GLGETPROC_(PFNGLDRAWELEMENTSINSTANCEDPROC               , glDrawElementsInstanced, "EXT");
     }
 
     /* OpenGL 3.2 core, GL_ARB_draw_elements_base_vertex. Same functions names in the ARB and core specs. */
