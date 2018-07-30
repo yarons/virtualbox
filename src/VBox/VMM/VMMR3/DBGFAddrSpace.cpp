d04bbb950eaeb97e7cd9a95c8fb69967e9302f1d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-07-30 15:25:28 +0000

    DBGF: Unwinding PE/AMD64, considered sketches for generic unwinding using unwind info.
    svn:sync-xref-src-repo-rev: r124015

--- src/VBox/VMM/VMMR3/DBGFAddrSpace.cpp
+++ src/VBox/VMM/VMMR3/DBGFAddrSpace.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFAddrSpace.cpp 70948 2018-02-10 15:38:12Z knut.osmundsen@oracle.com $ */
+/* $Id: DBGFAddrSpace.cpp 73397 2018-07-30 15:25:28Z knut.osmundsen@oracle.com $ */
 /** @file
  * DBGF - Debugger Facility, Address Space Management.
  */
@@ -543,7 +543,7 @@ VMMR3DECL(int) DBGFR3AsSetAlias(PUVM pUVM, RTDBGAS hAlias, RTDBGAS hAliasFor)
         return VERR_INVALID_HANDLE;
 
     /*
-     * Make sure the handle has is already in the database.
+     * Make sure the handle is already in the database.
      */
     int rc = VERR_NOT_FOUND;
     DBGF_AS_DB_LOCK_WRITE(pUVM);
@@ -558,6 +558,8 @@ VMMR3DECL(int) DBGFR3AsSetAlias(PUVM pUVM, RTDBGAS hAlias, RTDBGAS hAliasFor)
         Assert(cRefs > 0); Assert(cRefs != UINT32_MAX); NOREF(cRefs);
         rc = VINF_SUCCESS;
     }
+    else
+        RTDbgAsRelease(hRealAliasFor);
     DBGF_AS_DB_UNLOCK_WRITE(pUVM);
 
     return rc;
@@ -1210,6 +1212,7 @@ VMMR3DECL(int) DBGFR3AsSymbolByAddr(PUVM pUVM, RTDBGAS hDbgAs, PCDBGFADDRESS pAd
             RTDbgModRelease(hMod);
     }
 
+    RTDbgAsRelease(hRealAS);
     return rc;
 }
 
@@ -1296,6 +1299,7 @@ VMMR3DECL(int) DBGFR3AsSymbolByName(PUVM pUVM, RTDBGAS hDbgAs, const char *pszSy
             RTDbgModRelease(hMod);
     }
 
+    RTDbgAsRelease(hRealAS);
     return rc;
 }
 
@@ -1333,7 +1337,10 @@ VMMR3DECL(int)          DBGFR3AsLineByAddr(PUVM pUVM, RTDBGAS hDbgAs, PCDBGFADDR
     /*
      * Do the lookup.
      */
-    return RTDbgAsLineByAddr(hRealAS, pAddress->FlatPtr, poffDisp, pLine, phMod);
+    int rc = RTDbgAsLineByAddr(hRealAS, pAddress->FlatPtr, poffDisp, pLine, phMod);
+
+    RTDbgAsRelease(hRealAS);
+    return rc;
 }
 
 
