426a255039ac110ef661f9176f8007ffcd750de9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-08-01 11:18:54 +0000

    DBGPlugInNt: doxygen fix
    svn:sync-xref-src-repo-rev: r124041

--- src/VBox/Debugger/DBGPlugInWinNt.cpp
+++ src/VBox/Debugger/DBGPlugInWinNt.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGPlugInWinNt.cpp 73418 2018-08-01 11:10:29Z knut.osmundsen@oracle.com $ */
+/* $Id: DBGPlugInWinNt.cpp 73419 2018-08-01 11:18:54Z knut.osmundsen@oracle.com $ */
 /** @file
  * DBGPlugInWindows - Debugger and Guest OS Digger Plugin For Windows NT.
  */
@@ -299,9 +299,6 @@ static const RTUTF16 g_wszKernelNames[][WINNT_KERNEL_BASE_NAME_LEN + 1] =
  * @param   pszName         The image name.
  * @param   pImageAddr      The image address.
  * @param   cbImage         The size of the image.
- * @param   pbBuf           Scratch buffer containing the first
- *                          RT_MIN(cbBuf, cbImage) bytes of the image.
- * @param   cbBuf           The scratch buffer size.
  */
 static void dbgDiggerWinNtProcessImage(PDBGDIGGERWINNT pThis, PUVM pUVM, const char *pszName,
                                        PCDBGFADDRESS pImageAddr, uint32_t cbImage)
