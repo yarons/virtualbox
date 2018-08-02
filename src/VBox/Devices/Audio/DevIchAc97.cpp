8ca91a4f8c609423c553b14dc4585b68112c9a19
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-08-02 12:37:33 +0000

    Audio: When setting the recording source, check if the selected driver has at least one input source available, otherwise skip to the next one.
    svn:sync-xref-src-repo-rev: r124079

--- src/VBox/Devices/Audio/DevIchAc97.cpp
+++ src/VBox/Devices/Audio/DevIchAc97.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevIchAc97.cpp 73403 2018-07-31 08:59:13Z andreas.loeffler@oracle.com $ */
+/* $Id: DevIchAc97.cpp 73452 2018-08-02 12:37:33Z andreas.loeffler@oracle.com $ */
 /** @file
  * DevIchAc97 - VBox ICH AC97 Audio Controller.
  */
@@ -1570,9 +1570,18 @@ static int ichac97R3MixerAddDrvStream(PAC97STATE pThis, PAUDMIXSINK pMixSink, PP
                  * @todo Make the recording source dynamic (CFGM?). */
                 if (pStreamCfg->enmDir == PDMAUDIODIR_IN)
                 {
-                    rc = AudioMixerSinkSetRecordingSource(pMixSink, pMixStrm);
-                    LogFlowFunc(("LUN#%RU8: Recording source is now \"%s\", rc=%Rrc\n", pDrv->uLUN, pStreamCfg->szName, rc));
-                    LogRel2(("HDA: Set recording source to '%s'\n", pStreamCfg->szName));
+                    PDMAUDIOBACKENDCFG Cfg;
+                    rc = pDrv->pConnector->pfnGetConfig(pDrv->pConnector, &Cfg);
+                    if (   RT_SUCCESS(rc)
+                        && Cfg.cMaxStreamsIn) /* At least one input source available? */
+                    {
+                        rc = AudioMixerSinkSetRecordingSource(pMixSink, pMixStrm);
+                        LogFlowFunc(("LUN#%RU8: Recording source is now '%s', rc=%Rrc\n", pDrv->uLUN, pStreamCfg->szName, rc));
+                        LogRel2(("AC97: Set recording source to '%s'\n", pStreamCfg->szName));
+                    }
+                    else if (RT_FAILURE(rc))
+                        LogFunc(("LUN#%RU8: Unable to retrieve backend configuratio for '%s', rc=%Rrc\n",
+                                 pDrv->uLUN, pStreamCfg->szName, rc));
                 }
             }
         }
