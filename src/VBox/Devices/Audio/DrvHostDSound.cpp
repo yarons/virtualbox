6c741e3dd6119f26586a5954ad468b39b1e21aff
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-08-06 16:48:31 +0000

    Audio/DSound: Check if the critical section is valid on stream destruction.
    svn:sync-xref-src-repo-rev: r124159

--- src/VBox/Devices/Audio/DrvHostDSound.cpp
+++ src/VBox/Devices/Audio/DrvHostDSound.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostDSound.cpp 73529 2018-08-06 16:26:43Z andreas.loeffler@oracle.com $ */
+/* $Id: DrvHostDSound.cpp 73530 2018-08-06 16:48:31Z andreas.loeffler@oracle.com $ */
 /** @file
  * Windows host backend driver using DirectSound.
  */
@@ -2211,7 +2211,8 @@ static DECLCALLBACK(int) drvHostDSoundStreamDestroy(PPDMIHOSTAUDIO pInterface, P
 
     if (RT_SUCCESS(rc))
     {
-        rc = RTCritSectDelete(&pStreamDS->CritSect);
+        if (RTCritSectIsInitialized(&pStreamDS->CritSect))
+            rc = RTCritSectDelete(&pStreamDS->CritSect);
     }
 
     return rc;
