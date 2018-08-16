7247c56f4f60bb4a7cfa2fe305b791612690900a
Sergey Dubov <sergey.dubov@oracle.com>
2018-08-16 16:14:06 +0000

    build fix for r124396
    svn:sync-xref-src-repo-rev: r124397

--- src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolSelector.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolSelector.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIActionPoolSelector.cpp 73717 2018-08-16 16:00:08Z sergey.dubov@oracle.com $ */
+/* $Id: UIActionPoolSelector.cpp 73718 2018-08-16 16:14:06Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIActionPoolSelector class implementation.
  */
@@ -2224,7 +2224,6 @@ void UIActionPoolSelector::updateMenuMedium()
 {
     /* Get corresponding menu: */
     UIMenu *pMenu = action(UIActionIndexST_M_Medium)->menu();
-    AssertPtrReturnVoid(pMenu);
     /* Clear contents: */
     pMenu->clear();
 
