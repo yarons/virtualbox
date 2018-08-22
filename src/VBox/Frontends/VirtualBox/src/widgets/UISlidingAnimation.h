bee8c7eda3fd4794e3b175167076a3cf7ff4e9f2
Sergey Dubov <sergey.dubov@oracle.com>
2018-08-22 18:18:34 +0000

    FE/Qt: UISlidingAnimation: Notify about animation completion only when there really was an animation.
    svn:sync-xref-src-repo-rev: r124537

--- src/VBox/Frontends/VirtualBox/src/widgets/UISlidingAnimation.h
+++ src/VBox/Frontends/VirtualBox/src/widgets/UISlidingAnimation.h
@@ -1,4 +1,4 @@
-/* $Id: UISlidingAnimation.h 73844 2018-08-22 18:16:31Z sergey.dubov@oracle.com $ */
+/* $Id: UISlidingAnimation.h 73845 2018-08-22 18:18:34Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UISlidingAnimation class declaration.
  */
@@ -71,9 +71,9 @@ public:
 private slots:
 
     /** Handles entering for 'Start' state. */
-    void sltHandleStateEnteredStart() { emit sigAnimationComplete(SlidingDirection_Reverse); }
+    void sltHandleStateEnteredStart();
     /** Handles entering for 'Final' state. */
-    void sltHandleStateEnteredFinal() { emit sigAnimationComplete(SlidingDirection_Forward); }
+    void sltHandleStateEnteredFinal();
 
 private:
 
@@ -95,6 +95,8 @@ private:
     bool             m_fReverse;
     /** Holds the animation instance. */
     UIAnimation     *m_pAnimation;
+    /** Holds whether animation is in progress. */
+    bool             m_fIsInProgress;
     /** Holds sub-window start-geometry. */
     QRect            m_startWidgetGeometry;
     /** Holds sub-window final-geometry. */
