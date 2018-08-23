e0c7d07a1439a87a7d91860ef40d81eaedb1a00b
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2018-08-23 08:52:29 +0000

    Main/CloudClient+Progress+ExtPack: Make use of the extpack helper functions for dealing with IProgress objects, and remove the remaining temporary hacks which used to be required for the "cloud provider in extpack" case.
    svn:sync-xref-src-repo-rev: r124546

--- src/VBox/Main/src-all/ProgressImpl.cpp
+++ src/VBox/Main/src-all/ProgressImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ProgressImpl.cpp 73824 2018-08-22 12:31:25Z klaus.espenlaub@oracle.com $ */
+/* $Id: ProgressImpl.cpp 73853 2018-08-23 08:52:29Z klaus.espenlaub@oracle.com $ */
 /** @file
  *
  * VirtualBox Progress COM class implementation
@@ -207,7 +207,7 @@ HRESULT Progress::init(
 
     unconst(mId).create();
 
-#if !defined(VBOX_COM_INPROC) && !defined(VBOX_WITH_CLOUD_PROVIDERS_IN_EXTPACK)
+#if !defined(VBOX_COM_INPROC)
     /* add to the global collection of progress operations (note: after
      * creating mId) */
     mParent->i_addProgress(this);
@@ -326,7 +326,7 @@ void Progress::uninit()
     /* release initiator (effective only if mInitiator has been assigned in init()) */
     unconst(mInitiator).setNull();
 
-#if !defined(VBOX_COM_INPROC) && !defined(VBOX_WITH_CLOUD_PROVIDERS_IN_EXTPACK)
+#if !defined(VBOX_COM_INPROC)
     if (mParent)
     {
         /* remove the added progress on failure to complete the initialization */
@@ -470,7 +470,7 @@ HRESULT Progress::i_notifyCompleteEI(HRESULT aResultCode, const ComPtr<IVirtualB
     }
     mErrorInfo = aErrorInfo;
 
-#if !defined(VBOX_COM_INPROC) && !defined(VBOX_WITH_CLOUD_PROVIDERS_IN_EXTPACK)
+#if !defined(VBOX_COM_INPROC)
     /* remove from the global collection of pending progress operations */
     if (mParent)
         mParent->i_removeProgress(mId.ref());
