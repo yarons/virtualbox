2b29b39c09e2365e463730c4541315c9c559d866
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-08-29 15:24:53 +0000

    IPRT/rest: Added RTJsonIteratorBeginArray and RTJsonIteratorBeginObject for more accurate JSON decoding.  Early RTCRestArray implementation.  bugref:9167 [build fix]
    svn:sync-xref-src-repo-rev: r124689

--- src/VBox/Runtime/common/rest/RTCRestArrayBase.cpp
+++ src/VBox/Runtime/common/rest/RTCRestArrayBase.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTCRestArrayBase.cpp 73958 2018-08-29 15:21:46Z knut.osmundsen@oracle.com $ */
+/* $Id: RTCRestArrayBase.cpp 73960 2018-08-29 15:24:53Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - C++ REST, RTCRestArrayBase implementation.
  */
@@ -290,7 +290,7 @@ int RTCRestArrayBase::copyArrayWorker(RTCRestArrayBase const &a_rThat, bool fThr
         {
             for (size_t i = 0; i < a_rThat.m_cElements; i++)
             {
-                int rc = insertCopyWorker(i, *a_rThat.m_papElements[i], false);
+                rc = insertCopyWorker(i, *a_rThat.m_papElements[i], false);
                 if (RT_SUCCESS(rc))
                 { /* likely */ }
                 else if (fThrow)
@@ -300,7 +300,7 @@ int RTCRestArrayBase::copyArrayWorker(RTCRestArrayBase const &a_rThat, bool fThr
             }
         }
     }
-    return VINF_SUCCESS;
+    return rc;
 }
 
 
