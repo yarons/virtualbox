9c410a0701bee752b3a15d01af17a0f36c1a7fec
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-08-31 20:48:40 +0000

    ValidationKit/tdMoveMedium1.py: Try to fix burns.
    svn:sync-xref-src-repo-rev: r124758

--- src/VBox/ValidationKit/tests/api/tdMoveMedium1.py
+++ src/VBox/ValidationKit/tests/api/tdMoveMedium1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdMoveMedium1.py 74003 2018-08-31 14:47:37Z noreply@oracle.com $
+# $Id: tdMoveMedium1.py 74012 2018-08-31 20:48:40Z andreas.loeffler@oracle.com $
 
 """
 VirtualBox Validation Kit - Medium Move Test #1
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 74003 $"
+__version__ = "$Revision: 74012 $"
 
 
 # Standard Python imports.
@@ -204,6 +204,6 @@ class SubTstDrvMoveMedium1(base.SubTestDriverBase):
 
 if __name__ == '__main__':
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    from tdApi1 import tdApi1
+    from tests.api.tdApi1 import tdApi1
     sys.exit(tdApi1([SubTstDrvMoveMedium1]).main(sys.argv))
 
