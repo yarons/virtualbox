0b6ed8a06bd488af628d3c7c2ea59c726ac1f4e2
Sergey Dubov <sergey.dubov@oracle.com>
2018-08-31 10:52:13 +0000

    FE/Qt: VirtualBox Manager UI: Chooser pane re-styling: Group item: No need for child vertical margin as well.
    svn:sync-xref-src-repo-rev: r124741

--- src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIChooserItemGroup.cpp 73997 2018-08-31 10:45:01Z sergey.dubov@oracle.com $ */
+/* $Id: UIChooserItemGroup.cpp 73999 2018-08-31 10:52:13Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIChooserItemGroup class implementation.
  */
@@ -829,7 +829,7 @@ void UIChooserItemGroup::updateLayout()
         }
 
         /* Prepare body indent: */
-        iPreviousVerticalIndent = 3 * iVerticalMargin + iFullHeaderHeight;
+        iPreviousVerticalIndent = 2 * iVerticalMargin + iFullHeaderHeight;
     }
 
     /* No body for closed group: */
@@ -1468,8 +1468,6 @@ int UIChooserItemGroup::minimumHeightHintForGroup(bool fGroupOpened) const
         /* But if group-item is opened: */
         if (fGroupOpened)
         {
-            /* We should take into spacing between header and body: */
-            iProposedHeight += iVerticalMargin;
             /* And every existing child height: */
             foreach (UIChooserItem *pItem, items())
                 iProposedHeight += pItem->minimumHeightHint();
