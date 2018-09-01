4403aaa55a5c38e9669641694b1d622c25c0b0de
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-09-01 05:29:02 +0000

    VMM/IEM: Nested VMX: bugref:9180 VMLAUNCH skeleton.
    svn:sync-xref-src-repo-rev: r124763

--- src/VBox/VMM/VMMAll/IEMAllInstructionsTwoByte0f.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllInstructionsTwoByte0f.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllInstructionsTwoByte0f.cpp.h 74015 2018-09-01 04:20:29Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: IEMAllInstructionsTwoByte0f.cpp.h 74017 2018-09-01 05:29:02Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Emulation.
  *
@@ -251,6 +251,8 @@ FNIEMOP_DEF(iemOp_Grp7_vmcall)
 FNIEMOP_DEF(iemOp_Grp7_vmlaunch)
 {
     IEMOP_MNEMONIC(vmlaunch, "vmlaunch");
+    IEMOP_HLP_IN_VMX_OPERATION("vmlaunch", kVmxVInstrDiag_Vmlaunch);
+    IEMOP_HLP_VMX_INSTR("vmalunch", kVmxVInstrDiag_Vmlaunch);
     IEMOP_HLP_DONE_DECODING();
     return IEM_MC_DEFER_TO_CIMPL_0(iemCImpl_vmlaunch);
 }
@@ -276,6 +278,8 @@ FNIEMOP_DEF(iemOp_Grp7_vmresume)
 FNIEMOP_DEF(iemOp_Grp7_vmxoff)
 {
     IEMOP_MNEMONIC(vmxoff, "vmxoff");
+    IEMOP_HLP_IN_VMX_OPERATION("vmxoff", kVmxVInstrDiag_Vmxoff);
+    IEMOP_HLP_VMX_INSTR("vmxoff", kVmxVInstrDiag_Vmxoff);
     IEMOP_HLP_DONE_DECODING();
     return IEM_MC_DEFER_TO_CIMPL_0(iemCImpl_vmxoff);
 }
