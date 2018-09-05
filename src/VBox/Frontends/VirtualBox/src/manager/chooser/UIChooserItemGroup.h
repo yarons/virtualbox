69544f2686cfb7795c4be9954e182896b0482cf6
Sergey Dubov <sergey.dubov@oracle.com>
2018-09-05 11:40:11 +0000

    FE/Qt: VirtualBox Manager UI: Chooser pane re-styling: Group item: Hovering animation cleanup as well (s.a. r124840).
    svn:sync-xref-src-repo-rev: r124841

--- src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.h
+++ src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.h
@@ -1,4 +1,4 @@
-/* $Id: UIChooserItemGroup.h 74002 2018-08-31 11:19:49Z sergey.dubov@oracle.com $ */
+/* $Id: UIChooserItemGroup.h 74079 2018-09-05 11:40:11Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIChooserItemGroup class declaration.
  */
@@ -262,8 +262,8 @@ private:
         /** Returns whether group is main-root. */
         bool isMainRoot() const { return m_fMainRoot; }
 
-        /** Returns blackout item darkness. */
-        int blackoutDarkness() const { return m_iBlackoutDarkness; }
+        /** Returns item's header darkness. */
+        int headerDarkness() const { return m_iHeaderDarkness; }
 
         /** Defines @a iAdditionalHeight. */
         void setAdditionalHeight(int iAdditionalHeight);
@@ -323,10 +323,8 @@ private:
 
         /** Holds aditional height. */
         int  m_iAdditionalHeight;
-        /** Holds corner radious. */
-        int  m_iCornerRadius;
-        /** Holds blackout item darkness. */
-        int  m_iBlackoutDarkness;
+        /** Holds the header darkness. */
+        int  m_iHeaderDarkness;
 
         /** Holds the cached name. */
         QString m_strName;
