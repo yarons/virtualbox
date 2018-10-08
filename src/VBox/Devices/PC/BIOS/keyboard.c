c3a6df8e0f20ee6cec57458d3ea7e43808d6496b
Michal Necasek <michal.necasek@oracle.com>
2018-10-08 09:57:32 +0000

    BIOS: Test VBOX_BIOS_CPU instead of BX_CPU.
    svn:sync-xref-src-repo-rev: r125559

--- src/VBox/Devices/PC/BIOS/keyboard.c
+++ src/VBox/Devices/PC/BIOS/keyboard.c
@@ -1,4 +1,4 @@
-/* $Id: keyboard.c 69501 2017-10-28 16:12:47Z knut.osmundsen@oracle.com $ */
+/* $Id: keyboard.c 74665 2018-10-08 09:57:32Z michal.necasek@oracle.com $ */
 /** @file
  * PC BIOS - ???
  */
@@ -340,12 +340,12 @@ unsigned int enqueue_key(uint8_t scan_code, uint8_t ascii_code)
 {
     uint16_t    buffer_start, buffer_end, buffer_head, buffer_tail, temp_tail;
 
-#if BX_CPU < 2
-    buffer_start = 0x001E;
-    buffer_end   = 0x003E;
-#else
+#if VBOX_BIOS_CPU >= 80286
     buffer_start = read_word(0x0040, 0x0080);
     buffer_end   = read_word(0x0040, 0x0082);
+#else
+    buffer_start = 0x001E;
+    buffer_end   = 0x003E;
 #endif
 
     buffer_head = read_word(0x0040, 0x001A);
@@ -616,12 +616,12 @@ unsigned int dequeue_key(uint8_t __far *scan_code, uint8_t __far *ascii_code, un
     uint16_t    buffer_start, buffer_end, buffer_head, buffer_tail;
     uint8_t     acode, scode;
 
-#if BX_CPU < 2
-    buffer_start = 0x001E;
-    buffer_end   = 0x003E;
-#else
+#if VBOX_BIOS_CPU >= 80286
     buffer_start = read_word(0x0040, 0x0080);
     buffer_end   = read_word(0x0040, 0x0082);
+#else
+    buffer_start = 0x001E;
+    buffer_end   = 0x003E;
 #endif
 
     buffer_head = read_word(0x0040, 0x001a);
