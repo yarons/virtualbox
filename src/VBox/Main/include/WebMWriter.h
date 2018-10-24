9fe91088d00e02d5ca0acd278e9fe24808d9ed57
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-10-24 11:50:41 +0000

    VideoRec/Main: Make sure to initialize cluster count in WebMSegment.
    svn:sync-xref-src-repo-rev: r126074

--- src/VBox/Main/include/WebMWriter.h
+++ src/VBox/Main/include/WebMWriter.h
@@ -1,4 +1,4 @@
-/* $Id: WebMWriter.h 75032 2018-10-24 11:49:04Z andreas.loeffler@oracle.com $ */
+/* $Id: WebMWriter.h 75033 2018-10-24 11:50:41Z andreas.loeffler@oracle.com $ */
 /** @file
  * WebMWriter.h - WebM container handling.
  */
@@ -381,6 +381,7 @@ public:
             , offSeekInfo(0)
             , offTracks(0)
             , offCues(0)
+            , cClusters(0)
         {
             uTimecodeScaleFactor = VBOX_WEBM_TIMECODESCALE_FACTOR_MS;
 
