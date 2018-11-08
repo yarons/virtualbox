ea6c63eaedefa42008a91362a01e462e32956120
Michael Thayer <noreply@oracle.com>
2018-11-08 15:29:56 +0000

    Additions/linux: disable 3D for guests using Wayland. ticketref:18116: Linux guests with X.Org Server 1.20 and 3D enabled fail... Our 3D pass-through on Linux guests was designed for an X11 world where GLX was the only way of accessing OpenGL.  It does not work with Wayland clients or compositors, and can actively cause problems for EGL with OpenGL.  Case in point, guests with X.Org Server 1.20 installed do not boot to desktop with 3D enabled.  Rather than working around that for the few remaining GLX applications on the desktop, just disable 3D when Wayland is in use.
    svn:sync-xref-src-repo-rev: r126443

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-# $Id: vboxadd.sh 74964 2018-10-22 08:57:20Z noreply@oracle.com $
+# $Id: vboxadd.sh 75323 2018-11-08 15:29:56Z noreply@oracle.com $
 ## @file
-# Linux Additions kernel module init script ($Revision: 74964 $)
+# Linux Additions kernel module init script ($Revision: 75323 $)
 #
 
 #
@@ -269,6 +269,20 @@ start()
     rm -f /etc/ld.so.conf.d/00vboxvideo.conf
     rm -Rf /var/lib/VBoxGuestAdditions/lib
     if /usr/bin/VBoxClient --check3d 2>/dev/null; then
+        setup_gl=true;
+    else
+        unset setup_gl
+    fi
+    # Disable 3D if Xwayland is found, except on Ubuntu 18.04.
+    if type Xwayland >/dev/null 2>&1; then
+        unset PRETTY_NAME
+        . /etc/os-release 2>/dev/null
+        case "${PRETTY_NAME}" in
+            "Ubuntu 18.04"*) ;;
+            *) unset setup_gl ;;
+        esac
+    fi
+    if test -n "${setup_gl}"; then
         mkdir -p /var/lib/VBoxGuestAdditions/lib
         ln -sf "${INSTALL_DIR}/lib/VBoxOGL.so" /var/lib/VBoxGuestAdditions/lib/libGL.so.1
         # SELinux for the OpenGL libraries, so that gdm can load them during the
