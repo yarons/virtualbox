397e3e5ec588913bf2d6c10191ea6151b369ee95
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-11-16 16:23:00 +0000

    VMMDev/HGCM: Eliminated the unlock-delay hack (from 3.0) and implemented lock based approach for HGCM request (disabled). bugref:9172 [build fix]
    svn:sync-xref-src-repo-rev: r126681

--- src/VBox/Devices/VMMDev/VMMDev.cpp
+++ src/VBox/Devices/VMMDev/VMMDev.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMMDev.cpp 75520 2018-11-16 16:06:57Z knut.osmundsen@oracle.com $ */
+/* $Id: VMMDev.cpp 75523 2018-11-16 16:23:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMMDev - Guest <-> VMM/Host communication device.
  */
@@ -2930,12 +2930,12 @@ static DECLCALLBACK(int) vmmdevRequestHandler(PPDMDEVINS pDevIns, void *pvUser, 
             {
                 memcpy(pRequestHeader, &requestHeader, sizeof(VMMDevRequestHeader));
 
-                VMMDEVREQLOCK   Lock   = { NULL, { NULL, NULL } };
+                VMMDEVREQLOCK   Lock   = { NULL, { 0, NULL } };
                 PVMMDEVREQLOCK  pLock  = NULL;
                 size_t          cbLeft = requestHeader.size - sizeof(VMMDevRequestHeader);
                 if (cbLeft)
                 {
-#if 1
+#if 0
                     RT_NOREF_PV(Lock);
 #else
                     if (   (   requestHeader.requestType == VMMDevReq_HGCMCall32
