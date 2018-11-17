a534f744beb9a11544cdfb7af4f221b81e5eef2c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-11-17 03:56:22 +0000

    os2/VBoxSFFile.cpp: Fixed leak.
    svn:sync-xref-src-repo-rev: r126701

--- src/VBox/Additions/os2/VBoxSF/VBoxSFFile.cpp
+++ src/VBox/Additions/os2/VBoxSF/VBoxSFFile.cpp
@@ -1,4 +1,4 @@
-/** $Id: VBoxSFFile.cpp 75461 2018-11-14 19:51:23Z knut.osmundsen@oracle.com $ */
+/** $Id: VBoxSFFile.cpp 75543 2018-11-17 03:56:22Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxSF - OS/2 Shared Folders, the file level IFS EPs.
  */
@@ -487,7 +487,7 @@ vboxSfOs2QueryFileInfo(PVBOXSFFOLDER pFolder, PSFFSI pSfFsi, PVBOXSFSYFI pSfFsd,
         uint32_t cbObjInfo = sizeof(*pObjInfo);
 
         int vrc = VbglR0SfFsInfo(&g_SfClient, &pFolder->hHostFolder, pSfFsd->hHostFile,
-                                SHFL_INFO_FILE | SHFL_INFO_GET, &cbObjInfo, (PSHFLDIRINFO)pObjInfo);
+                                 SHFL_INFO_FILE | SHFL_INFO_GET, &cbObjInfo, (PSHFLDIRINFO)pObjInfo);
         if (RT_SUCCESS(vrc))
         {
             rc = vboxSfOs2FileStatusFromObjInfo(pbData, cbData, uLevel, pObjInfo);
@@ -508,6 +508,7 @@ vboxSfOs2QueryFileInfo(PVBOXSFFOLDER pFolder, PSFFSI pSfFsi, PVBOXSFSYFI pSfFsd,
             Log(("vboxSfOs2QueryFileInfo: VbglR0SfFsInfo failed: %Rrc\n", vrc));
             rc = vboxSfOs2ConvertStatusToOs2(vrc, ERROR_GEN_FAILURE);
         }
+        VbglR0PhysHeapFree(pObjInfo);
     }
     else
         rc = ERROR_NOT_ENOUGH_MEMORY;
