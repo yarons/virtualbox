a639af0cf92888218e188e0054d6df420899ece9
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-11-21 10:49:32 +0000

    VMM/IEM: Nested VMX: bugref:9180 Documented a hack that's no longer necessary and added a couple of flower boxes.
    svn:sync-xref-src-repo-rev: r126816

--- src/VBox/VMM/VMMAll/IEMAllCImplSvmInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplSvmInstr.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllCImplSvmInstr.cpp.h 75611 2018-11-20 11:20:25Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: IEMAllCImplSvmInstr.cpp.h 75638 2018-11-21 10:49:32Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IEM - AMD-V (Secure Virtual Machine) instruction implementation.
  */
@@ -16,6 +16,9 @@
  */
 
 
+/*********************************************************************************************************************************
+*   Defined Constants And Macros                                                                                                 *
+*********************************************************************************************************************************/
 #ifdef VBOX_WITH_NESTED_HWVIRT_SVM
 /**
  * Check the common SVM instruction preconditions.
