4fe978b03312a0b1ad5ac98f449b020938ce6830
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-11-22 21:25:35 +0000

    darwin/VBoxSF: Updates.
    svn:sync-xref-src-repo-rev: r126871

--- src/VBox/Additions/darwin/VBoxSF/VBoxSFInternal.h
+++ src/VBox/Additions/darwin/VBoxSF/VBoxSFInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxSFInternal.h 75675 2018-11-22 21:16:48Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxSFInternal.h 75676 2018-11-22 21:25:35Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxSF - Darwin Shared Folders, internal header.
  */
@@ -102,7 +102,7 @@ bool    vboxSfDwnConnect(void);
 vnode_t vboxSfDwnVnAlloc(mount_t pMount, enum vtype enmType, vnode_t pParent, uint64_t cbFile);
 
 
-
+#if 0 /* old code, rewriting. */
 
 /**
  * Helper function to create XNU VFS vnode object.
@@ -239,7 +239,8 @@ extern mode_t vboxvfs_h2g_mode_inernal(RTFMODE fHostMode);
 extern uint32_t vboxvfs_g2h_mode_inernal(mode_t fGuestMode);
 
 extern SHFLSTRING *vboxvfs_construct_shflstring(const char *pszName, size_t cchName);
-
+#endif
 
 #endif
+
 
