f4f8df81133248989a3a4ce4b4c701726a01e12d
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-11-26 08:39:43 +0000

    VBoxServiceControlSession.cpp: Bail out in vgsvcGstCtrlSessionSpawnWorker() when we're unable to connect to the host service.
    svn:sync-xref-src-repo-rev: r126926

--- src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceControlSession.cpp 75720 2018-11-25 18:37:51Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxServiceControlSession.cpp 75727 2018-11-26 08:39:43Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxServiceControlSession - Guest session handling. Also handles the spawned session processes.
  */
@@ -1335,16 +1335,10 @@ static RTEXITCODE vgsvcGstCtrlSessionSpawnWorker(PVBOXSERVICECTRLSESSION pSessio
         VGSvcVerbose(1, "Using client ID=%RU32\n", uClientID);
     }
     else
-/** @todo r=bird: This should return to caller, shouldn't it it?!?
- * Because VbglR3GuestCtrlSessionNotify() below is now using an uninitialized
- * uClientID, right?  If you check VbglR3GuestCtrlConnect, you can clearly see
- * that it will not be set, except on success.  So, this is a terrible idea!
- *
- * Did this happen because of a weird desire to have a single return statement?
- * Or was it some other confusion wrt program flow/state?  Indentation fright?
- * Please figure out why and try not to do it again. :-)
- */
+    {
         VGSvcError("Error connecting to guest control service, rc=%Rrc\n", rc);
+        return RTEXITCODE_FAILURE;
+    }
 
     /* Report started status. */
     VBGLR3GUESTCTRLCMDCTX ctx = { uClientID, VBOX_GUESTCTRL_CONTEXTID_MAKE_SESSION(pSession->StartupInfo.uSessionID) };
