45e9ff49ba8cb0a4baa0659045386cc483faae5d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-11-29 01:55:12 +0000

    DevHDA: No more assertions for me, thank you.
    svn:sync-xref-src-repo-rev: r127005

--- src/VBox/Devices/Audio/DevHDA.h
+++ src/VBox/Devices/Audio/DevHDA.h
@@ -1,4 +1,4 @@
-/* $Id: DevHDA.h 75606 2018-11-20 10:26:04Z andreas.loeffler@oracle.com $ */
+/* $Id: DevHDA.h 75802 2018-11-29 01:55:12Z knut.osmundsen@oracle.com $ */
 /** @file
  * DevHDA.h - VBox Intel HD Audio Controller.
  */
@@ -176,13 +176,6 @@ typedef struct HDASTATE
 #endif
     /** Last updated wall clock (WALCLK) counter. */
     uint64_t                           u64WalClk;
-#ifdef VBOX_STRICT
-    /** Wall clock (WALCLK) stale count.
-     *  This indicates the number of set wall clock
-     *  values which did not actually move the counter forward (stale). */
-    uint8_t                            u8WalClkStaleCnt;
-    uint8_t                            au8Padding2[7];
-#endif
     /** Response Interrupt Count (RINTCNT). */
     uint16_t                           u16RespIntCnt;
     /** Position adjustment (in audio frames).
@@ -198,7 +191,15 @@ typedef struct HDASTATE
     uint16_t                           cPosAdjustFrames;
     /** Whether the position adjustment is enabled or not. */
     bool                               fPosAdjustEnabled;
+#ifdef VBOX_STRICT
+    /** Wall clock (WALCLK) stale count.
+     *  This indicates the number of set wall clock
+     *  values which did not actually move the counter forward (stale). */
+    uint8_t                            u8WalClkStaleCnt;
+    uint8_t                            Padding1[2];
+#else
     uint8_t                            Padding1[3];
+#endif
     /** Current IRQ level. */
     uint8_t                            u8IRQL;
     /** The device timer Hz rate. Defaults to HDA_TIMER_HZ_DEFAULT. */
