c4f22a87dc7b8aa6d88a32886cb946c31a244c7d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-11-29 02:22:25 +0000

    VBoxService: Started using the new PEEK and GET message functions. bugref:9313
    svn:sync-xref-src-repo-rev: r127007

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibGuestCtrl.cpp
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibGuestCtrl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR3LibGuestCtrl.cpp 75803 2018-11-29 02:22:10Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGuestR3LibGuestCtrl.cpp 75804 2018-11-29 02:22:25Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxGuestR3Lib - Ring-3 Support Library for VirtualBox guest additions, guest control.
  */
@@ -194,7 +194,7 @@ DECL_NO_INLINE(static, bool) vbglR3GuestCtrlDetectPeekGetCancelSupport(uint32_t 
         rc = VbglR3HGCMCall(&PeekCall.Hdr, sizeof(PeekCall));
     } while (rc == VERR_INTERRUPTED);
 
-    LogRel(("vbglR3GuestCtrlDetectPeekGetCancelSupport: rc=%Rrc %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x\n",
+    LogRel2(("vbglR3GuestCtrlDetectPeekGetCancelSupport: rc=%Rrc %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x %#x\n",
              rc, PeekCall.idMsg.u.value32,     PeekCall.cParams.u.value32,
              PeekCall.acbParams[ 0].u.value32, PeekCall.acbParams[ 1].u.value32,
              PeekCall.acbParams[ 2].u.value32, PeekCall.acbParams[ 3].u.value32,
