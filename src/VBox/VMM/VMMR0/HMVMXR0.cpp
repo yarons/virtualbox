7d27ffa2512189cf13d965e0baab9f9a9524bc69
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-11-30 06:38:37 +0000

    VMM/HMVMXR0: Typo in commented code.
    svn:sync-xref-src-repo-rev: r127029

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 75822 2018-11-29 17:37:35Z knut.osmundsen@oracle.com $ */
+/* $Id: HMVMXR0.cpp 75826 2018-11-30 06:38:37Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -2629,7 +2629,7 @@ static int hmR0VmxSetupMiscCtls(PVMCPU pVCpu)
 #if 0
     /* Setup debug controls */
     rc  = VMXWriteVmcs64(VMX_VMCS64_GUEST_DEBUGCTL_FULL, 0);
-    rc |= VMXWriteVmcs32(VMX_VMCS_GUEST_PENDING_DEBUG_EXCEPTIONS, 0);
+    rc |= VMXWriteVmcs32(VMX_VMCS_GUEST_PENDING_DEBUG_XCPTS, 0);
     AssertRCReturn(rc, rc);
 #endif
 
@@ -5435,7 +5435,7 @@ static int hmR0VmxInitVmcsReadCache(PVMCPU pVCpu)
     VMXLOCAL_INIT_READ_CACHE_FIELD(pCache, VMX_VMCS_GUEST_RIP);
 #if 0
     /* Unused natural width guest-state fields. */
-    VMXLOCAL_INIT_READ_CACHE_FIELD(pCache, VMX_VMCS_GUEST_PENDING_DEBUG_EXCEPTIONS);
+    VMXLOCAL_INIT_READ_CACHE_FIELD(pCache, VMX_VMCS_GUEST_PENDING_DEBUG_XCPTS);
     VMXLOCAL_INIT_READ_CACHE_FIELD(pCache, VMX_VMCS_GUEST_CR3); /* Handled in Nested Paging case */
 #endif
     VMXLOCAL_INIT_READ_CACHE_FIELD(pCache, VMX_VMCS_GUEST_SYSENTER_ESP);
