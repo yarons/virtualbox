80461f0d3992c671b2d320b335de0521a9c8dded
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-12-03 23:09:03 +0000

    VBoxService: scm fix
    svn:sync-xref-src-repo-rev: r127135

--- src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceToolBox.cpp 75925 2018-12-03 20:45:48Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxServiceToolBox.cpp 75929 2018-12-03 23:09:03Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxServiceToolbox - Internal (BusyBox-like) toolbox.
  */
@@ -949,7 +949,7 @@ static int vgsvcToolboxLsHandleDir(const char *pszDir, uint32_t fFlags, uint32_t
                     case RTFS_TYPE_DIRECTORY:
                     {
                         const char *pszName = pNodeIt->dirEntry.szName;
-                        if (   !RTStrICmp(pszName, ".")  /** @todo r=bird: Please do explain what the upper/lower casing of  '.' is! I'm really curious. */  
+                        if (   !RTStrICmp(pszName, ".")  /** @todo r=bird: Please do explain what the upper/lower casing of  '.' is! I'm really curious. */
                             || !RTStrICmp(pszName, "..")) /** @todo r=bird: There is a RTDir API for checking these. Use it! */
                         {
                             /* Skip dot directories. */
