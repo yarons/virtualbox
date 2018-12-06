751b113d41c466b2df7beaf98a0104e479a2cded
Andreas Loeffler <andreas.loeffler@oracle.com>
2018-12-06 15:14:02 +0000

    Audio/HDA: Cleaned up setting the stream registers at one defined place (hdaR3StreamInit()).
    svn:sync-xref-src-repo-rev: r127242

--- src/VBox/Devices/Audio/HDAStream.h
+++ src/VBox/Devices/Audio/HDAStream.h
@@ -1,4 +1,4 @@
-/* $Id: HDAStream.h 75983 2018-12-05 16:35:32Z andreas.loeffler@oracle.com $ */
+/* $Id: HDAStream.h 76018 2018-12-06 15:14:02Z andreas.loeffler@oracle.com $ */
 /** @file
  * HDAStream.h - Stream functions for HD Audio.
  */
@@ -200,20 +200,26 @@ typedef struct HDASTREAM
      *  For a stereo stream, this is u8Channel + 1. */
     uint8_t                  u8Channel;
     uint8_t                  Padding0[6];
-    /** DMA base address (SDnBDPU - SDnBDPL). */
+    /** DMA base address (SDnBDPU - SDnBDPL).
+     *  Will be updated in hdaR3StreamInit(). */
     uint64_t                 u64BDLBase;
     /** Cyclic Buffer Length (SDnCBL).
-     *  Represents the size of the ring buffer. */
+     *  Represents the size of the ring buffer.
+     *  Will be updated in hdaR3StreamInit(). */
     uint32_t                 u32CBL;
-    /** Format (SDnFMT). */
+    /** Format (SDnFMT).
+     *  Will be updated in hdaR3StreamInit(). */
     uint16_t                 u16FMT;
     /** FIFO Size (FIFOS).
      *  Maximum number of bytes that may have been DMA'd into
-     *  memory but not yet transmitted on the link. */
+     *  memory but not yet transmitted on the link.
+     *
+     *  Will be updated in hdaR3StreamInit(). */
     uint16_t                 u16FIFOS;
     /** FIFO Watermark. */
     uint16_t                 u16FIFOW;
-    /** Last Valid Index (SDnLVI). */
+    /** Last Valid Index (SDnLVI).
+     *  Will be updated in hdaR3StreamInit(). */
     uint16_t                 u16LVI;
     uint16_t                 Padding1[2];
     /** Pointer to the HDA state this stream is attached to. */
