e43db0c46ff14cb8005759440e51c20e9d6eb98f
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2018-12-06 11:15:00 +0000

    VMM: Nested VMX/SVM: Fix Only-in-IEM execution macro define typo.
    svn:sync-xref-src-repo-rev: r127233

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 75956 2018-12-05 05:54:56Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXR0.cpp 76009 2018-12-06 11:15:00Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -8438,8 +8438,9 @@ static VBOXSTRICTRC hmR0VmxPreRunGuest(PVMCPU pVCpu, PVMXTRANSIENT pVmxTransient
 {
     Assert(VMMRZCallRing3IsEnabled(pVCpu));
 
-#ifdef VBOX_WITH_NESTED_HWVIRT_VMX_ONLY_IN_IEM
-    Log2(("hmR0SvmPreRunGuest: Rescheduling to IEM due to nested-hwvirt or forced IEM exec -> VINF_EM_RESCHEDULE_REM\n"));
+#ifdef VBOX_WITH_NESTED_HWVIRT_ONLY_IN_IEM
+    Log2(("hmR0VmxPreRunGuest: Rescheduling to IEM due to nested-hwvirt or forced IEM exec -> VINF_EM_RESCHEDULE_REM\n"));
+    RT_NOREF3(pVCpu, pVmxTransient, fStepping);
     return VINF_EM_RESCHEDULE_REM;
 #endif
 
