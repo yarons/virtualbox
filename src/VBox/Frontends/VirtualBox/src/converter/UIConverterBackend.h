2525caaed408747730a2d79d6a39cc090349938a
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-12-07 13:52:59 +0000

    FE/Qt: bugref:9012. Fix for r127272.
    svn:sync-xref-src-repo-rev: r127295

--- src/VBox/Frontends/VirtualBox/src/converter/UIConverterBackend.h
+++ src/VBox/Frontends/VirtualBox/src/converter/UIConverterBackend.h
@@ -1,4 +1,4 @@
-/* $Id: UIConverterBackend.h 76044 2018-12-07 09:50:51Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIConverterBackend.h 76058 2018-12-07 13:52:59Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIConverterBackend declaration.
  */
@@ -260,8 +260,7 @@ template<> SHARED_LIBRARY_STUFF QString toString(const UIMediumFormat &enmUIMedi
 template<> SHARED_LIBRARY_STUFF QString toInternalString(const UIMediumFormat &enmUIMediumFormat);
 template<> SHARED_LIBRARY_STUFF UIMediumFormat fromInternalString<UIMediumFormat>(const QString &strUIMediumFormat);
 template<> SHARED_LIBRARY_STUFF QString toString(const UISettingsDefs::RecordingMode &enmRecordingMode);
-template<> SHARED_LIBRARY_STUFF QString toInternalString(const UISettingsDefs::RecordingMode &enmRecordingMode);
-template<> SHARED_LIBRARY_STUFF UISettingsDefs::RecordingMode fromInternalString<UISettingsDefs::RecordingMode>(const QString &enmRecordingMode);
+template<> SHARED_LIBRARY_STUFF UISettingsDefs::RecordingMode fromString<UISettingsDefs::RecordingMode>(const QString &enmRecordingMode);
 
 
 /* Declare COM conversion specializations: */
