525aec4f85f2314c15421b5faf79036f920329c6
Michael Thayer <noreply@oracle.com>
2018-12-08 13:00:59 +0000

    Linux installers: use systemctl daemon-reload, not daemon-reexec. bugref:3809: Linux installer maintenance daemon-reexec is the debugging version of daemon-reload.
    svn:sync-xref-src-repo-rev: r127308

--- src/VBox/Installer/linux/routines.sh
+++ src/VBox/Installer/linux/routines.sh
@@ -1,3 +1,4 @@
+# $Id: routines.sh 76068 2018-12-08 13:00:59Z noreply@oracle.com $
 # Oracle VM VirtualBox
 # VirtualBox installer shell routines
 #
@@ -231,7 +232,7 @@ remove_init_script()
 finish_init_script_install()
 {
     if use_systemd; then
-        systemctl daemon-reexec
+        systemctl daemon-reload
     fi
 }
 
