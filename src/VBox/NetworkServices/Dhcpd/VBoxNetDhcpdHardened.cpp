19265ee320b62fec66738411a9e28128c547c08e
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2018-12-11 16:43:14 +0000

    Main/DHCPServer: (bugref:9288) Fix for hardened builds (use VBoxNetDHCP instead of VBoxNetDhcpd).
    svn:sync-xref-src-repo-rev: r127414

--- src/VBox/NetworkServices/Dhcpd/VBoxNetDhcpdHardened.cpp
+++ src/VBox/NetworkServices/Dhcpd/VBoxNetDhcpdHardened.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetDhcpdHardened.cpp 75648 2018-11-21 18:02:38Z aleksey.ilyushin@oracle.com $ */
+/* $Id: VBoxNetDhcpdHardened.cpp 76166 2018-12-11 16:43:14Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * VBoxNetDhcpd - Hardened main().
  */
@@ -20,6 +20,6 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    return SUPR3HardenedMain("VBoxNetDhcpd", 0 /* fFlags */, argc, argv, envp);
+    return SUPR3HardenedMain("VBoxNetDHCP", 0 /* fFlags */, argc, argv, envp);
 }
 
