5a8285865b33da7d9ce25a4271e9017fd40c1a51
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-12-13 11:12:11 +0000

    FE\Qt: Check a medium's uuid before trying to attach it to a vm
    svn:sync-xref-src-repo-rev: r127465

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsStorage.cpp 75006 2018-10-23 15:48:45Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIMachineSettingsStorage.cpp 76205 2018-12-13 11:12:11Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineSettingsStorage class implementation.
  */
@@ -3842,7 +3842,7 @@ void UIMachineSettingsStorage::addAttachmentWrapper(KDeviceType enmDevice)
         default: break; /* Shut up, MSC! */
     }
 
-    if (!fCancelled)
+    if (!fCancelled && !uMediumId.isNull())
     {
         m_pModelStorage->addAttachment(QUuid(m_pModelStorage->data(index, StorageModel::R_ItemId).toString()), enmDevice, uMediumId);
         m_pModelStorage->sort();
