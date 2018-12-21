2206e628ff1e21fd1fb628c87366110b4fa3ec10
Michal Necasek <michal.necasek@oracle.com>
2018-12-21 13:40:40 +0000

    Whitespace.
    svn:sync-xref-src-repo-rev: r127632

--- src/VBox/Devices/USB/linux/USBProxyDevice-linux.cpp
+++ src/VBox/Devices/USB/linux/USBProxyDevice-linux.cpp
@@ -1,4 +1,4 @@
-/* $Id: USBProxyDevice-linux.cpp 76331 2018-12-21 10:25:11Z michal.necasek@oracle.com $ */
+/* $Id: USBProxyDevice-linux.cpp 76337 2018-12-21 13:40:40Z michal.necasek@oracle.com $ */
 /** @file
  * USB device proxy - the Linux backend.
  */
@@ -1640,7 +1640,7 @@ static VUSBSTATUS vusbProxyLinuxStatusToVUsbStatus(int iStatus)
         case -ESHUTDOWN:
             Log(("vusbProxyLinuxStatusToVUsbStatus: SHUTDOWN!!\n"));
             return VUSBSTATUS_STALL;
-        
+
         case -ENOENT:
             Log(("vusbProxyLinuxStatusToVUsbStatus: ENOENT!!\n"));
             return VUSBSTATUS_STALL;
