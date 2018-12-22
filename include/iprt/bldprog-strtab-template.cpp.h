9be07627b925e5ffb9748e1064d4fb10b7eb73cd
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-12-22 04:45:33 +0000

    iprt/bldprog-strtab-template.cpp.h: Header build fix.
    svn:sync-xref-src-repo-rev: r127672

--- include/iprt/bldprog-strtab-template.cpp.h
+++ include/iprt/bldprog-strtab-template.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: bldprog-strtab-template.cpp.h 76373 2018-12-22 04:43:51Z knut.osmundsen@oracle.com $ */
+/* $Id: bldprog-strtab-template.cpp.h 76374 2018-12-22 04:45:33Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Build Program - String Table Generator.
  */
@@ -157,7 +157,7 @@ typedef struct BLDPROGSTRTAB
 } BLDPROGSTRTAB;
 typedef BLDPROGSTRTAB *PBLDPROGSTRTAB;
 
-#if RT_CLANG_PREREQ(4, 0) || RT_GCC_PREREQ(4, 6)
+#if RT_CLANG_PREREQ(4, 0) || RT_GNUC_PREREQ(4, 6)
 #  pragma GCC diagnostic push
 #  pragma GCC diagnostic ignored "-Wunused-function"
 #endif
@@ -1038,7 +1038,7 @@ static void BldProgStrTab_WriteStringTable(PBLDPROGSTRTAB pThis, FILE *pOut,
 #endif
 }
 
-#if RT_CLANG_PREREQ(4, 0) || RT_GCC_PREREQ(4, 6)
+#if RT_CLANG_PREREQ(4, 0) || RT_GNUC_PREREQ(4, 6)
 #  pragma GCC diagnostic pop
 #endif
 
