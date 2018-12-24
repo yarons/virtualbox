49cdb6c4f87cf9e6d04320733ff41afb95c2e0d4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-12-24 15:10:04 +0000

    Happy scm.
    svn:sync-xref-src-repo-rev: r127740

--- src/VBox/Main/testcase/tstGuestCtrlParseBuffer.cpp
+++ src/VBox/Main/testcase/tstGuestCtrlParseBuffer.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstGuestCtrlParseBuffer.cpp 76403 2018-12-23 15:19:41Z knut.osmundsen@oracle.com $ */
+/* $Id: tstGuestCtrlParseBuffer.cpp 76439 2018-12-24 15:10:04Z knut.osmundsen@oracle.com $ */
 /** @file
  * Output stream parsing test cases.
  */
@@ -34,6 +34,7 @@ using namespace com;
 #ifndef BYTE
 # define BYTE uint8_t
 #endif
+
 
 /*********************************************************************************************************************************
 *   Defined Constants And Macros                                                                                                 *
