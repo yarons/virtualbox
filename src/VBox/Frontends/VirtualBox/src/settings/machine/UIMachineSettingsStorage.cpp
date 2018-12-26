cd85174b17cad31b2b02b8322b0072a068dcf615
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2018-12-26 10:38:23 +0000

    FE/Qt: bugref:9346. Allow creating an optical drive during vm storage attachment.
    svn:sync-xref-src-repo-rev: r127785

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsStorage.cpp 76205 2018-12-13 11:12:11Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIMachineSettingsStorage.cpp 76484 2018-12-26 10:38:23Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineSettingsStorage class implementation.
  */
@@ -3804,6 +3804,7 @@ void UIMachineSettingsStorage::addAttachmentWrapper(KDeviceType enmDevice)
     const QString strMachineFolder(QFileInfo(m_strMachineSettingsFilePath).absolutePath());
 
     bool fCancelled = false;
+    bool fCreateEmpty = false;
     QUuid uMediumId;
     switch (enmDevice)
     {
@@ -3825,6 +3826,9 @@ void UIMachineSettingsStorage::addAttachmentWrapper(KDeviceType enmDevice)
             if (iAnswer == AlertButton_Choice2)
                 uMediumId = vboxGlobal().openMediumSelectorDialog(this, UIMediumDeviceType_DVD,
                                                                   m_strMachineName, m_strMachineSettingsFilePath);
+            /* For optical medium we allow creating an empty drive: */
+            else if (iAnswer == AlertButton_Choice1)
+                fCreateEmpty = true;
             else if (iAnswer == AlertButton_Cancel)
                 fCancelled = true;
             break;
@@ -3842,7 +3846,7 @@ void UIMachineSettingsStorage::addAttachmentWrapper(KDeviceType enmDevice)
         default: break; /* Shut up, MSC! */
     }
 
-    if (!fCancelled && !uMediumId.isNull())
+    if (!fCancelled && (!uMediumId.isNull() || fCreateEmpty))
     {
         m_pModelStorage->addAttachment(QUuid(m_pModelStorage->data(index, StorageModel::R_ItemId).toString()), enmDevice, uMediumId);
         m_pModelStorage->sort();
