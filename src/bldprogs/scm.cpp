397c1c5059f1b111515ac33bb73af859a028bf42
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-12-30 06:44:40 +0000

    scm: Enabled --fix-header-guards by default.  bugref:9344
    svn:sync-xref-src-repo-rev: r127849

--- src/bldprogs/scm.cpp
+++ src/bldprogs/scm.cpp
@@ -1,4 +1,4 @@
-/* $Id: scm.cpp 76512 2018-12-30 05:14:08Z knut.osmundsen@oracle.com $ */
+/* $Id: scm.cpp 76547 2018-12-30 06:44:40Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase / Tool - Source Code Massager.
  */
@@ -178,7 +178,7 @@ static SCMSETTINGSBASE const g_Defaults =
     /* .fStripTrailingLines = */                    true,
     /* .fFixFlowerBoxMarkers = */                   true,
     /* .cMinBlankLinesBeforeFlowerBoxMakers = */    2,
-    /* .fFixHeaderGuards = */                       false, /** @todo fFixHeaderGuards = true */
+    /* .fFixHeaderGuards = */                       true,
     /* .fPragmaOnce = */                            true,
     /* .fFixTodos = */                              true,
     /* .fFixErrH = */                               true,
@@ -2862,7 +2862,7 @@ int main(int argc, char **argv)
             case 'V':
             {
                 /* The following is assuming that svn does it's job here. */
-                static const char s_szRev[] = "$Revision: 76512 $";
+                static const char s_szRev[] = "$Revision: 76547 $";
                 const char *psz = RTStrStripL(strchr(s_szRev, ' '));
                 RTPrintf("r%.*s\n", strchr(psz, ' ') - psz, psz);
                 return 0;
