6b45fab97f8eb02cd289d479430a0d0a360ba5f7
Michael Thayer <noreply@oracle.com>
2019-01-17 13:53:59 +0000

    drivers/linux: adjust for new access_ok signature. bugref:4567: Linux kernel driver maintenance. The API for the access_ok() macro has changed in 5.0 to remove the first argument.  Contributed by Kyle Laker under the terms of the MIT licence. Verified by Valdis Kletnieks.  Thank you.
    svn:sync-xref-src-repo-rev: r128219

--- src/VBox/Runtime/r0drv/linux/memuserkernel-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/memuserkernel-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: memuserkernel-r0drv-linux.c 76553 2019-01-01 01:45:53Z knut.osmundsen@oracle.com $ */
+/* $Id: memuserkernel-r0drv-linux.c 76859 2019-01-17 13:53:59Z noreply@oracle.com $ */
 /** @file
  * IPRT - User & Kernel Memory, Ring-0 Driver, Linux.
  */
@@ -66,7 +66,11 @@ RT_EXPORT_SYMBOL(RTR0MemUserCopyTo);
 RTR0DECL(bool) RTR0MemUserIsValidAddr(RTR3PTR R3Ptr)
 {
     IPRT_LINUX_SAVE_EFL_AC();
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+    bool fRc = access_ok((void *)R3Ptr, 1);
+#else
     bool fRc = access_ok(VERIFY_READ, (void *)R3Ptr, 1);
+#endif
     IPRT_LINUX_RESTORE_EFL_AC();
     return fRc;
 }
@@ -82,7 +86,11 @@ RTR0DECL(bool) RTR0MemKernelIsValidAddr(void *pv)
     return (uintptr_t)pv >= PAGE_OFFSET;
 #else
 # error "PORT ME"
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+    return !access_ok(pv, 1);
+#else
     return !access_ok(VERIFY_READ, pv, 1);
+#endif /* LINUX_VERSION_CODE */
 #endif
 }
 RT_EXPORT_SYMBOL(RTR0MemKernelIsValidAddr);
