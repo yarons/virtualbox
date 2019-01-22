7040380c865052bd80d5ade3918e10ea6b82d614
Michael Thayer <noreply@oracle.com>
2019-01-22 16:51:28 +0000

    Reverted previous commit: merged too much.
    svn:sync-xref-src-repo-rev: r128316

--- src/VBox/Additions/linux/drm/vbox_main.c
+++ src/VBox/Additions/linux/drm/vbox_main.c
@@ -1,4 +1,4 @@
-/* $Id: vbox_main.c 76938 2019-01-22 16:50:18Z noreply@oracle.com $ */
+/* $Id: vbox_main.c 76939 2019-01-22 16:51:28Z noreply@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -67,7 +67,7 @@ void vbox_enable_accel(struct vbox_private *vbox)
 		if (vbox->vbva_info[i].pVBVA)
 			continue;
 
-		vbva = (void __force *)vbox->vbva_buffers + i * VBVA_MIN_BUFFER_SIZE;
+		vbva = (void *)vbox->vbva_buffers + i * VBVA_MIN_BUFFER_SIZE;
 		if (!VBoxVBVAEnable(&vbox->vbva_info[i],
 				 vbox->guest_pool, vbva, i)) {
 			/* very old host or driver error. */
