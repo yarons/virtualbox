fd55660c48033ca5e8320a29eb737d3d9a40cc7d
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-01-24 08:54:53 +0000

    Shared Clipboard/HostServices: Renamed VBoxSharedClipboard.cpp to VBoxSharedClipboardSvc.cpp and VBoxSharedClipboard.rc to VBoxSharedClipboardSvc.rc.
    svn:sync-xref-src-repo-rev: r128354

--- src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc.cpp
+++ src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc.cpp
@@ -1,4 +1,4 @@
-/* $Id: service.cpp 76553 2019-01-01 01:45:53Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxSharedClipboardSvc.cpp 76970 2019-01-24 08:54:53Z andreas.loeffler@oracle.com $ */
 /** @file
  * Shared Clipboard Service - Host service entry points.
  */
