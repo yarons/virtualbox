23224bca6125dec99ab0fdab2484157d05276153
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-01-24 08:38:26 +0000

    Guest Control/HostServices: Renamed service.cpp to VBoxGuestControlSvc.cpp
    svn:sync-xref-src-repo-rev: r128347

--- src/VBox/HostServices/GuestControl/VBoxGuestControlSvc.cpp
+++ src/VBox/HostServices/GuestControl/VBoxGuestControlSvc.cpp
@@ -1,4 +1,4 @@
-/* $Id: service.cpp 76962 2019-01-24 08:35:56Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxGuestControlSvc.cpp 76963 2019-01-24 08:38:26Z andreas.loeffler@oracle.com $ */
 /** @file
  * Guest Control Service: Controlling the guest.
  */
