4b73e1e4e89086e3a4709eac3b74e81706fcf8e5
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2019-02-04 17:52:47 +0000

    FE/Qt: Removing source medium selector from the clone medium wizard(s)
    svn:sync-xref-src-repo-rev: r128629

--- src/VBox/Frontends/VirtualBox/src/wizards/clonevd/UIWizardCloneVD.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/clonevd/UIWizardCloneVD.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardCloneVD.h 76581 2019-01-01 06:24:57Z knut.osmundsen@oracle.com $ */
+/* $Id: UIWizardCloneVD.h 77154 2019-02-04 17:52:47Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardCloneVD class declaration.
  */
@@ -41,8 +41,7 @@ public:
     {
         Page1,
         Page2,
-        Page3,
-        Page4
+        Page3
     };
 
     /** Expert Page IDs. */
@@ -55,7 +54,7 @@ public:
     UIWizardCloneVD(QWidget *pParent, const CMedium &comSourceVirtualDisk);
 
     /** Returns source virtual-disk. */
-    CMedium sourceVirtualDisk() const { return m_comSourceVirtualDisk; }
+    const CMedium &sourceVirtualDisk() const { return m_comSourceVirtualDisk; }
     /** Returns target virtual-disk. */
     CMedium targetVirtualDisk() const { return m_comTargetVirtualDisk; }
 
@@ -83,4 +82,3 @@ private:
 };
 
 #endif /* !FEQT_INCLUDED_SRC_wizards_clonevd_UIWizardCloneVD_h */
-
