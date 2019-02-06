832bcc8cc2d0c5c7ac4dd9ed0473a7d46580e497
Alexander Eichner <alexander.eichner@oracle.com>
2019-02-06 18:31:48 +0000

    AHCI: Revert last memory leak fix. PDMDevHlpDriverAttach() takes over ownership of the pszDesc pointer
    svn:sync-xref-src-repo-rev: r128672

--- src/VBox/Devices/Storage/DevAHCI.cpp
+++ src/VBox/Devices/Storage/DevAHCI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevAHCI.cpp 77182 2019-02-06 18:24:30Z alexander.eichner@oracle.com $ */
+/* $Id: DevAHCI.cpp 77183 2019-02-06 18:31:48Z alexander.eichner@oracle.com $ */
 /** @file
  * DevAHCI - AHCI controller device (disk and cdrom).
  *
@@ -6186,9 +6186,6 @@ static DECLCALLBACK(int) ahciR3Construct(PPDMDEVINS pDevIns, int iInstance, PCFG
         else
             return PDMDevHlpVMSetError(pDevIns, rc, RT_SRC_POS,
                                        N_("AHCI: Failed to attach drive to %s"), pszName);
-
-        RTStrFree(pszName);
-        pszName = NULL;
     }
 
     /*
