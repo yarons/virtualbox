945f0d5dd5906c86b77f6e9b3f5e6ec880515cdf
Michael Thayer <noreply@oracle.com>
2019-02-07 10:23:12 +0000

    Devices/DevVGA-SVGA: make FIFO polling thread sleep when idle. bugref:9376: Complete hardware cursor implementation in VMSVGA. Fix an unused parameter warning from the previous commit when built in release mode.
    svn:sync-xref-src-repo-rev: r128685

--- src/VBox/Devices/Graphics/DevVGA-SVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA.cpp 77193 2019-02-07 10:06:19Z noreply@oracle.com $ */
+/* $Id: DevVGA-SVGA.cpp 77194 2019-02-07 10:23:12Z noreply@oracle.com $ */
 /** @file
  * VMware SVGA device.
  *
@@ -2375,14 +2375,15 @@ vmsvgaR3FIFOAccessHandler(PVM pVM, PVMCPU pVCpu, RTGCPHYS GCPhys, void *pvPhys, 
     PVGASTATE   pThis = (PVGASTATE)pvUser;
     int         rc;
     Assert(pThis);
-    Assert(GCPhys >= pThis->svga.GCPhysFIFO);
     NOREF(pVCpu); NOREF(pvPhys); NOREF(pvBuf); NOREF(cbBuf); NOREF(enmOrigin);
 
     SUPSemEventSignal(pThis->svga.pSupDrvSession, pThis->svga.FIFORequestSem);
 # ifdef DEBUG_FIFO_ACCESS
+    Assert(GCPhys >= pThis->svga.GCPhysFIFO);
     rc = vmsvgaDebugFIFOAccess(pVM, pThis, GCPhys, enmAccessType == PGMACCESSTYPE_WRITE);
 # else
     NOREF(enmAccessType);
+    NOREF(GCPhys);
 // #  ifdef DEBUG
     /* Invariant: the access handler should never trigger twice within a certain
      * time span; calling it 500ms here for simplicity. */
