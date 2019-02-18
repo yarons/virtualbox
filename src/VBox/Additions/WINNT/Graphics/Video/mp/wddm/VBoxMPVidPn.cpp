357f68cf6c140798493ea39deaaab2fff82842e0
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-02-18 09:08:59 +0000

    Additions/WDDM: Return TRUE in VBoxVidPnIsSupported() if hVidPn is NULL; the display adapter can always be configured to display nothing.
    svn:sync-xref-src-repo-rev: r128876

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPVidPn.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPVidPn.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMPVidPn.cpp 76553 2019-01-01 01:45:53Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxMPVidPn.cpp 77342 2019-02-18 09:08:59Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBox WDDM Miniport driver
  */
@@ -1688,6 +1688,14 @@ static BOOLEAN vboxVidPnIsPathSupported(PVBOXMP_DEVEXT pDevExt, const D3DKMDT_VI
 
 NTSTATUS VBoxVidPnIsSupported(PVBOXMP_DEVEXT pDevExt, D3DKMDT_HVIDPN hVidPn, BOOLEAN *pfSupported)
 {
+    /* According Microsoft Docs we must return pfSupported = TRUE here if hVidPn is NULL, as
+     * the display adapter can always be configured to display nothing. */
+    if (hVidPn == NULL)
+    {
+        *pfSupported = TRUE;
+        return STATUS_SUCCESS;
+    }
+
     *pfSupported = FALSE;
 
     const DXGK_VIDPN_INTERFACE* pVidPnInterface = NULL;
