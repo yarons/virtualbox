bdbd9634dec6fd6dce6c555fca1b819f2a69d413
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-02-21 18:35:09 +0000

    Guest Control/Validation Kit: Use an array for file.write() calls.
    svn:sync-xref-src-repo-rev: r128974

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 77384 $"
+__version__ = "$Revision: 77413 $"
 
 # Disable bitching about too many arguments per function.
 # pylint: disable=R0913
@@ -43,6 +43,7 @@ __version__ = "$Revision: 77384 $"
 # pylint: disable=R0903
 
 # Standard Python imports.
+from array import array
 import errno
 import os
 import random
@@ -3057,7 +3058,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
                     reporter.log("File '%s' opened" % curTest.sFile);
                     ## @todo Split this up in 64K writes. Later.
                     ## @todo Test timeouts.
-                    cBytesWritten = curFile.write(curTest.aBuf, 30 * 1000);
+                    cBytesWritten = curFile.write(array('b', curTest.aBuf), 30 * 1000);
                     if    curRes.cbProcessed > 0 \
                       and curRes.cbProcessed != cBytesWritten:
                         reporter.error('Test #%d failed: Written buffer length does not match: Got %d, expected %d' \
