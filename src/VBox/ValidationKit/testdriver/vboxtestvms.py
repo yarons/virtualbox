2344b37930a4b3d1a4a8e689e6bc006e1c18fb83
Alexey Strelnikov <noreply@oracle.com>
2019-02-28 09:27:20 +0000

    Updated ubuntu image to 18.04.02
    svn:sync-xref-src-repo-rev: r129089

--- src/VBox/ValidationKit/testdriver/vboxtestvms.py
+++ src/VBox/ValidationKit/testdriver/vboxtestvms.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: vboxtestvms.py 77491 2019-02-27 20:32:42Z noreply@oracle.com $
+# $Id: vboxtestvms.py 77497 2019-02-28 09:27:20Z noreply@oracle.com $
 
 """
 VirtualBox Test VMs
@@ -26,7 +26,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 77491 $"
+__version__ = "$Revision: 77497 $"
 
 # Standard Python imports.
 import copy;
@@ -1070,7 +1070,7 @@ class TestVmManager(object):
                sKind = 'RedHat', acCpusSup = range(1, 33), fIoApic = True, sNic0AttachType = 'nat'),
         TestVm('tst-arch',                  kfGrpStandard,        sHd = '4.2/usb/tst-arch.vdi',
                sKind = 'ArchLinux_64', acCpusSup = range(1, 33), fIoApic = True, sNic0AttachType = 'nat'),
-        TestVm('tst-ubuntu-180401-64',   kfGrpStdSmoke,        sHd = '4.2/ubuntu-180401/t-ubuntu-180401-64.vdi',
+        TestVm('tst-ubuntu-1804-64',   kfGrpStdSmoke,        sHd = '4.2/ubuntu-1804/t-ubuntu-1804-64.vdi',
                sKind = 'Ubuntu_64', acCpusSup = range(1, 33), fIoApic = True),
         TestVm('tst-ol76-64',   kfGrpStdSmoke,        sHd = '4.2/ol76/t-ol76-64.vdi',
                sKind = 'Oracle_64', acCpusSup = range(1, 33), fIoApic = True),
