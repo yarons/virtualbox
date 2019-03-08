2230913999be3b14d0463570568f55e6383f8474
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2019-03-08 10:35:00 +0000

    VMM/EM: Nested VMX: bugref:9180 Fix calling IEMExecVmxVmexitApicWrite.
    svn:sync-xref-src-repo-rev: r129239

--- src/VBox/VMM/VMMR3/EM.cpp
+++ src/VBox/VMM/VMMR3/EM.cpp
@@ -1,4 +1,4 @@
-/* $Id: EM.cpp 77610 2019-03-08 10:31:35Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: EM.cpp 77611 2019-03-08 10:35:00Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * EM - Execution Monitor / Manager.
  */
@@ -2144,8 +2144,9 @@ int emR3ForcedActions(PVM pVM, PVMCPU pVCpu, int rc)
          */
         if (VMCPU_FF_IS_SET(pVCpu, VMCPU_FF_VMX_APIC_WRITE))
         {
-            rc = VBOXSTRICTRC_VAL(IEMExecVmxVmexitApicWrite(pVCpu));
-            Assert(rc != VINF_VMX_INTERCEPT_NOT_ACTIVE);
+            rc2 = VBOXSTRICTRC_VAL(IEMExecVmxVmexitApicWrite(pVCpu));
+            Assert(rc2 != VINF_VMX_INTERCEPT_NOT_ACTIVE);
+            UPDATE_RC();
         }
 
         /*
