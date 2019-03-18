53fbc13f5de94c567fb0ccb6742c83ee38fb6f81
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2019-03-18 10:59:53 +0000

    FE/Qt: bugref:9378. Selecting the matched item after search.
    svn:sync-xref-src-repo-rev: r129409

--- src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserModel.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserModel.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIChooserModel.cpp 77723 2019-03-15 12:52:07Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIChooserModel.cpp 77748 2019-03-18 10:59:53Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIChooserModel class implementation.
  */
@@ -531,6 +531,7 @@ void UIChooserModel::scrollToSearchResult(bool fIsNext)
         if (pItem)
         {
             pItem->makeSureItsVisible();
+            setCurrentItem(pItem);
         }
     }
 
