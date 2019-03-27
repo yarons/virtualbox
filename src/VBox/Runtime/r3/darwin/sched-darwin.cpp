e6d98193a5ff35dcfe76f7f5514f995fb11c9da3
Alexey Diachenko <noreply@oracle.com>
2019-03-27 12:20:08 +0000

    Main: bugref:7929: Fixed comilation issues
    svn:sync-xref-src-repo-rev: r129627

--- src/VBox/Runtime/r3/darwin/sched-darwin.cpp
+++ src/VBox/Runtime/r3/darwin/sched-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: sched-darwin.cpp 77910 2019-03-27 11:33:01Z noreply@oracle.com $ */
+/* $Id: sched-darwin.cpp 77914 2019-03-27 12:20:08Z noreply@oracle.com $ */
 /** @file
  * IPRT - Scheduling, Darwin.
  */
@@ -232,7 +232,7 @@ static int rtSchedDarwinGetBasePriority(PRTTHREADINT pThread)
     struct policy_timeshare_info TSInfo = {0,0,0,0,0};
     kern_return_t krc = thread_info(!pThread ? mach_thread_self() : pthread_mach_thread_np((pthread_t)pThread->Core.Key),
                                     THREAD_SCHED_TIMESHARE_INFO, (thread_info_t)&TSInfo, &Count);
-    Assert(krc == KERN_SUCCESS);
+    Assert(krc == KERN_SUCCESS); NOREF(krc);
 
     return TSInfo.base_priority;
 }
