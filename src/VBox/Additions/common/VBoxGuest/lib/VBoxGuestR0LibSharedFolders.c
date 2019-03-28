d653c701c9b8ffa67d186b0012ab39046ae4bfd8
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-03-28 15:56:44 +0000

    linux/vboxsf: Reimplemented vbsf_splice_read to make both sendfile and splice() work more correctly on 2.6.23 - 2.6.30.  bugref:9172
    svn:sync-xref-src-repo-rev: r129659

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR0LibSharedFolders.c
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR0LibSharedFolders.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR0LibSharedFolders.c 77862 2019-03-24 02:48:08Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGuestR0LibSharedFolders.c 77939 2019-03-28 15:56:44Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxGuestR0LibSharedFolders - Ring 0 Shared Folders calls.
  */
@@ -308,8 +308,6 @@ DECLVBGL(int) VbglR0SfRename(PVBGLSFCLIENT pClient, PVBGLSFMAP pMap, PSHFLSTRING
     return rc;
 }
 
-#endif /* !RT_OS_LINUX */
-
 DECLVBGL(int) VbglR0SfRead(PVBGLSFCLIENT pClient, PVBGLSFMAP pMap, SHFLHANDLE hFile,
                            uint64_t offset, uint32_t *pcbBuffer, uint8_t *pBuffer, bool fLocked)
 {
@@ -340,8 +338,6 @@ DECLVBGL(int) VbglR0SfRead(PVBGLSFCLIENT pClient, PVBGLSFMAP pMap, SHFLHANDLE hF
     }
     return rc;
 }
-
-#if !defined(RT_OS_LINUX)
 
 DECLVBGL(int) VbglR0SfReadPageList(PVBGLSFCLIENT pClient, PVBGLSFMAP pMap, SHFLHANDLE hFile, uint64_t offset, uint32_t *pcbBuffer,
                                    uint16_t offFirstPage, uint16_t cPages, RTGCPHYS64 *paPages)
