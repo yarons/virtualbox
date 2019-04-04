a52f3be90c6979a0bc576a12a2641d25f76bebaa
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-04-04 11:36:37 +0000

    Main/HostDnsService: HostDnsServiceBase::uninit() is also virtual.
    svn:sync-xref-src-repo-rev: r129785

--- src/VBox/Main/src-server/HostDnsService.h
+++ src/VBox/Main/src-server/HostDnsService.h
@@ -1,4 +1,4 @@
-/* $Id: HostDnsService.h 77995 2019-04-03 15:29:37Z andreas.loeffler@oracle.com $ */
+/* $Id: HostDnsService.h 78012 2019-04-04 11:36:37Z andreas.loeffler@oracle.com $ */
 /** @file
  * Host DNS listener.
  */
@@ -65,8 +65,7 @@ public:
     /* @note: method will wait till client call
        HostDnsService::monitorThreadInitializationDone() */
     virtual HRESULT init(HostDnsMonitorProxy *pProxy);
-
-    void uninit(void);
+    virtual void uninit(void);
 
 protected:
 
