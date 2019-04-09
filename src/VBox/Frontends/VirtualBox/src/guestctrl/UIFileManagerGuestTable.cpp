1621a8fb1931ac2fe44cfae49e9e04a44fe8ab40
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2019-04-09 11:19:30 +0000

    FE/Qt: bugref:6699. Using a shorter QVector initilization.
    svn:sync-xref-src-repo-rev: r129891

--- src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerGuestTable.cpp
+++ src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerGuestTable.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIFileManagerGuestTable.cpp 78054 2019-04-09 10:48:33Z andreas.loeffler@oracle.com $ */
+/* $Id: UIFileManagerGuestTable.cpp 78056 2019-04-09 11:19:30Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIFileManagerGuestTable class implementation.
  */
@@ -234,9 +234,7 @@ void UIFileManagerGuestTable::deleteByItem(UICustomFileSystemItem *item)
 
     if (item->isDirectory())
     {
-        QVector<KDirectoryRemoveRecFlag> aFlags;
-        aFlags.push_back(KDirectoryRemoveRecFlag_ContentAndDir);
-
+        QVector<KDirectoryRemoveRecFlag> aFlags(1, KDirectoryRemoveRecFlag_ContentAndDir);
         m_comGuestSession.DirectoryRemoveRecursive(item->path(), aFlags);
     }
     else
@@ -260,9 +258,7 @@ void UIFileManagerGuestTable::deleteByPath(const QStringList &pathList)
         }
         else if (eType == KFsObjType_Directory)
         {
-            QVector<KDirectoryRemoveRecFlag> aFlags;
-            aFlags.push_back(KDirectoryRemoveRecFlag_ContentAndDir);
-
+            QVector<KDirectoryRemoveRecFlag> aFlags(1, KDirectoryRemoveRecFlag_ContentAndDir);
             m_comGuestSession.DirectoryRemoveRecursive(strPath, aFlags);
         }
 
