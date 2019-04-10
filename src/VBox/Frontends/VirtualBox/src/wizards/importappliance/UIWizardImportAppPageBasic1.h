6644f82ad95ed5246cf2ce30273fa8fd389eab07
Sergey Dubov <sergey.dubov@oracle.com>
2019-04-10 15:12:14 +0000

    FE/Qt: bugref:9434: Import Appliance wizard: 1st basic page: Integrate hidden for now cloud account machine instance control.
    svn:sync-xref-src-repo-rev: r129950

--- src/VBox/Frontends/VirtualBox/src/wizards/importappliance/UIWizardImportAppPageBasic1.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/importappliance/UIWizardImportAppPageBasic1.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardImportAppPageBasic1.h 78082 2019-04-10 11:56:38Z sergey.dubov@oracle.com $ */
+/* $Id: UIWizardImportAppPageBasic1.h 78094 2019-04-10 15:12:14Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardImportAppPageBasic1 class declaration.
  */
@@ -33,6 +33,7 @@
 
 /* Forward declarations: */
 class QLabel;
+class QListWidget;
 class QTableWidget;
 class QStackedLayout;
 class QIComboBox;
@@ -69,6 +70,8 @@ protected:
     void populateAccounts();
     /** Populates account properties. */
     void populateAccountProperties();
+    /** Populates account instances. */
+    void populateAccountInstances();
 
     /** Updates page appearance. */
     virtual void updatePageAppearance();
@@ -127,6 +130,10 @@ protected:
     QIToolButton *m_pAccountToolButton;
     /** Holds the account property table instance. */
     QTableWidget *m_pAccountPropertyTable;
+    /** Holds the account instance label instance. */
+    QLabel       *m_pAccountInstanceLabel;
+    /** Holds the account instance list instance. */
+    QListWidget  *m_pAccountInstanceList;
 };
 
 /** UIWizardPage extension for 1st page of the Import Appliance wizard, extends UIWizardImportAppPage1 as well. */
