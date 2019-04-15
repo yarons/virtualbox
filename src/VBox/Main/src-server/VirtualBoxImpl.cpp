c102f7aabc732c652233fc758a7a5bf80fe06b67
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2019-04-15 14:29:49 +0000

    Main: Machine+VirtualBox: slightly prettier fix to bugref:9075, including comment adjustments
    svn:sync-xref-src-repo-rev: r130020

--- src/VBox/Main/src-server/VirtualBoxImpl.cpp
+++ src/VBox/Main/src-server/VirtualBoxImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxImpl.cpp 78122 2019-04-15 09:33:32Z noreply@oracle.com $ */
+/* $Id: VirtualBoxImpl.cpp 78124 2019-04-15 14:29:49Z klaus.espenlaub@oracle.com $ */
 /** @file
  * Implementation of IVirtualBox in VBoxSVC.
  */
@@ -125,6 +125,8 @@ std::map<com::Utf8Str, int> VirtualBox::sNatNetworkNameToRefCount;
 
 // static leaked (todo: find better place to free it.)
 RWLockHandle *VirtualBox::spMtxNatNetworkNameToRefCountLock;
+
+
 ////////////////////////////////////////////////////////////////////////////////
 //
 // CallbackEvent class
@@ -2141,14 +2143,14 @@ HRESULT VirtualBox::setExtraData(const com::Utf8Str &aKey,
     Utf8Str strOldValue;            // empty
     HRESULT rc = S_OK;
 
-    /* Because non-ASCII characters in aKey have caused problems in the settings
+    /* Because control characters in aKey have caused problems in the settings
      * they are rejected unless the key should be deleted. */
     if (!strValue.isEmpty())
     {
         for (size_t i = 0; i < strKey.length(); ++i)
         {
             char ch = strKey[i];
-            if (RT_C_IS_CNTRL(ch))
+            if (RTLocCIsCntrl(ch))
                 return E_INVALIDARG;
         }
     }
