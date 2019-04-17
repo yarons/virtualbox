967245d40e46f46886664da1f63e6c9c8280408e
Sergey Dubov <sergey.dubov@oracle.com>
2019-04-17 17:31:19 +0000

    FE/Qt: bugref:9434: Import Appliance wizard: Form Editor widget: Value change support.
    svn:sync-xref-src-repo-rev: r130093

--- src/VBox/Frontends/VirtualBox/src/globals/UIMessageCenter.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIMessageCenter.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMessageCenter.cpp 78166 2019-04-17 14:46:26Z sergey.dubov@oracle.com $ */
+/* $Id: UIMessageCenter.cpp 78177 2019-04-17 17:31:19Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMessageCenter class implementation.
  */
@@ -52,6 +52,8 @@
 #endif
 
 /* COM includes: */
+#include "CBooleanFormValue.h"
+#include "CChoiceFormValue.h"
 #include "CCloudClient.h"
 #include "CCloudProfile.h"
 #include "CCloudProvider.h"
@@ -63,6 +65,7 @@
 #include "CSharedFolder.h"
 #include "CSnapshot.h"
 #include "CStorageController.h"
+#include "CStringFormValue.h"
 #include "CConsole.h"
 #include "CMachine.h"
 #include "CSystemProperties.h"
@@ -1686,6 +1689,34 @@ bool UIMessageCenter::confirmCloudProfilesImport(QWidget *pParent /* = 0 */) con
                           tr("Import") /* ok button text */,
                           QString() /* cancel button text */,
                           false /* ok button by default? */);
+}
+
+void UIMessageCenter::cannotAssignFormValue(const CBooleanFormValue &comValue, QWidget *pParent /* = 0 */) const
+{
+    error(pParent, MessageType_Error,
+          tr("Failed to assign form value."),
+          UIErrorString::formatErrorInfo(comValue));
+}
+
+void UIMessageCenter::cannotAssignFormValue(const CStringFormValue &comValue, QWidget *pParent /* = 0 */) const
+{
+    error(pParent, MessageType_Error,
+          tr("Failed to assign form value."),
+          UIErrorString::formatErrorInfo(comValue));
+}
+
+void UIMessageCenter::cannotAssignFormValue(const CChoiceFormValue &comValue, QWidget *pParent /* = 0 */) const
+{
+    error(pParent, MessageType_Error,
+          tr("Failed to assign form value."),
+          UIErrorString::formatErrorInfo(comValue));
+}
+
+void UIMessageCenter::cannotAssignFormValue(const CProgress &comProgress, QWidget *pParent /* = 0 */) const
+{
+    error(pParent, MessageType_Error,
+          tr("Failed to assign form value."),
+          UIErrorString::formatErrorInfo(comProgress));
 }
 
 bool UIMessageCenter::confirmHardDisklessMachine(QWidget *pParent /* = 0*/) const
