5153365cd08353261684541781ebc36c6447bc50
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-04-26 08:29:34 +0000

    IPRT/nt: Implemented offSub & cbSub in the NT versions of rtR0MemObjNativeMapKernel and rtR0MemObjNativeMapUser. Untested+unused, but seems not to interfer with existing usage. [doxyfix] bugref:9217
    svn:sync-xref-src-repo-rev: r130280

--- src/VBox/Runtime/r0drv/nt/memobj-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/memobj-r0drv-nt.cpp
@@ -1,4 +1,4 @@
-/* $Id: memobj-r0drv-nt.cpp 78278 2019-04-24 16:03:59Z knut.osmundsen@oracle.com $ */
+/* $Id: memobj-r0drv-nt.cpp 78314 2019-04-26 08:29:34Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Ring-0 Memory Objects, NT.
  */
@@ -709,6 +709,10 @@ DECLHIDDEN(int) rtR0MemObjNativeReserveUser(PPRTR0MEMOBJINTERNAL ppMem, RTR3PTR 
  * @param   fProt       The desired page protection for the mapping.
  * @param   R0Process   If NIL_RTR0PROCESS map into system (kernel) memory.
  *                      If not nil, it's the current process.
+ * @param   offSub      Offset into @a pMemToMap to start mapping.
+ * @param   cbSub       The number of bytes to map from @a pMapToMem.  0 if
+ *                      we're to map everything. Non-zero if @a offSub is
+ *                      non-zero.
  */
 static int rtR0MemObjNtMap(PPRTR0MEMOBJINTERNAL ppMem, RTR0MEMOBJ pMemToMap, void *pvFixed, size_t uAlignment,
                            unsigned fProt, RTR0PROCESS R0Process, size_t offSub, size_t cbSub)
