8d6116e59421c31bf40f2c4784c891211a997ecd
Sergey Dubov <sergey.dubov@oracle.com>
2019-05-14 16:12:59 +0000

    FE/Qt: bugref:4551: Icon enum values for SAS controller forgotten in r56277.
    svn:sync-xref-src-repo-rev: r130558

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsStorage.cpp 78517 2019-05-14 15:52:41Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineSettingsStorage.cpp 78518 2019-05-14 16:12:59Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineSettingsStorage class implementation.
  */
@@ -316,6 +316,9 @@ UIIconPoolStorageSettings::UIIconPoolStorageSettings()
     m_names.insert(SCSIControllerNormal,     ":/scsi_16px.png");
     m_names.insert(SCSIControllerExpand,     ":/scsi_expand_16px.png");
     m_names.insert(SCSIControllerCollapse,   ":/scsi_collapse_16px.png");
+    m_names.insert(SASControllerNormal,      ":/sata_16px.png");
+    m_names.insert(SASControllerExpand,      ":/sata_expand_16px.png");
+    m_names.insert(SASControllerCollapse,    ":/sata_collapse_16px.png");
     m_names.insert(USBControllerNormal,      ":/usb_16px.png");
     m_names.insert(USBControllerExpand,      ":/usb_expand_16px.png");
     m_names.insert(USBControllerCollapse,    ":/usb_collapse_16px.png");
@@ -398,7 +401,7 @@ AbstractControllerType::AbstractControllerType (KStorageBus aBusType, KStorageCo
                 mPixmaps [i] = (PixmapType)(FloppyControllerNormal + i);
                 break;
             case KStorageBus_SAS:
-                mPixmaps [i] = (PixmapType)(SATAControllerNormal + i);
+                mPixmaps [i] = (PixmapType)(SASControllerNormal + i);
                 break;
             case KStorageBus_USB:
                 mPixmaps [i] = (PixmapType)(USBControllerNormal + i);
--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.h
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.h
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsStorage.h 78517 2019-05-14 15:52:41Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineSettingsStorage.h 78518 2019-05-14 16:12:59Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineSettingsStorage class declaration.
  */
@@ -89,6 +89,9 @@ enum PixmapType
     SCSIControllerNormal,
     SCSIControllerExpand,
     SCSIControllerCollapse,
+    SASControllerNormal,
+    SASControllerExpand,
+    SASControllerCollapse,
     USBControllerNormal,
     USBControllerExpand,
     USBControllerCollapse,
