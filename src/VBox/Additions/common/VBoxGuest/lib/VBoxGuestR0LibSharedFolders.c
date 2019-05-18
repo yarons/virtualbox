b6e8a154f0fe565694f3fb709fb5d1b1e87a1034
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-05-18 02:25:33 +0000

    winnt/vboxsf: Converted VBoxMRxRead the new HGCM IDC interface, only keeping the page list variant.  bugref:9172
    svn:sync-xref-src-repo-rev: r130631

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR0LibSharedFolders.c
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR0LibSharedFolders.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR0LibSharedFolders.c 78569 2019-05-17 14:11:07Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGuestR0LibSharedFolders.c 78576 2019-05-18 02:25:33Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxGuestR0LibSharedFolders - Ring 0 Shared Folders calls.
  */
@@ -315,8 +315,6 @@ DECLVBGL(int) VbglR0SfRename(PVBGLSFCLIENT pClient, PVBGLSFMAP pMap, PSHFLSTRING
     return rc;
 }
 
-# endif /* !RT_OS_WINDOWS */
-
 DECLVBGL(int) VbglR0SfRead(PVBGLSFCLIENT pClient, PVBGLSFMAP pMap, SHFLHANDLE hFile,
                            uint64_t offset, uint32_t *pcbBuffer, uint8_t *pBuffer, bool fLocked)
 {
@@ -393,6 +391,8 @@ DECLVBGL(int) VbglR0SfReadPageList(PVBGLSFCLIENT pClient, PVBGLSFMAP pMap, SHFLH
     RTMemTmpFree(pData);
     return rc;
 }
+
+# endif /* !RT_OS_WINDOWS */
 
 DECLVBGL(int) VbglR0SfWrite(PVBGLSFCLIENT pClient, PVBGLSFMAP pMap, SHFLHANDLE hFile,
                             uint64_t offset, uint32_t *pcbBuffer, uint8_t *pBuffer, bool fLocked)
