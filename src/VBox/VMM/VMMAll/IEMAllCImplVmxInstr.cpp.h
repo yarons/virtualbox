cb0ecc851a126c4521bb8c67d55ef508910fac55
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2019-05-22 10:48:06 +0000

    VMM/IEM: Nested VMX: bugref:9180 PAUSE instruction doesn't pass extra info other than the length.
    svn:sync-xref-src-repo-rev: r130722

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllCImplVmxInstr.cpp.h 78650 2019-05-22 09:41:16Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: IEMAllCImplVmxInstr.cpp.h 78654 2019-05-22 10:48:06Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IEM - VT-x instruction implementation.
  */
@@ -3694,13 +3694,7 @@ IEM_STATIC VBOXSTRICTRC iemVmxVmexitInstrPause(PVMCPU pVCpu, uint8_t cbInstr)
     }
 
     if (fIntercept)
-    {
-        VMXVEXITINFO ExitInfo;
-        RT_ZERO(ExitInfo);
-        ExitInfo.uReason = VMX_EXIT_PAUSE;
-        ExitInfo.cbInstr = cbInstr;
-        return iemVmxVmexitInstrWithInfo(pVCpu, &ExitInfo);
-    }
+        return iemVmxVmexitInstr(pVCpu, VMX_EXIT_PAUSE, cbInstr);
 
     return VINF_VMX_INTERCEPT_NOT_ACTIVE;
 }
