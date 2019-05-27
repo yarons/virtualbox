eb736321e049f3d7bb763d25e77b2baffccbdf14
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-05-27 11:32:49 +0000

    DnD/DnDURIList: Use delete instead of RTMemFree() in DnDURIList::Clear() and DnDURIList::RemoveFirst().
    svn:sync-xref-src-repo-rev: r130872

--- src/VBox/GuestHost/DragAndDrop/DnDURIList.cpp
+++ src/VBox/GuestHost/DragAndDrop/DnDURIList.cpp
@@ -1,4 +1,4 @@
-/* $Id: DnDURIList.cpp 78021 2019-04-05 08:25:19Z andreas.loeffler@oracle.com $ */
+/* $Id: DnDURIList.cpp 78783 2019-05-27 11:32:49Z andreas.loeffler@oracle.com $ */
 /** @file
  * DnD - URI list class.
  */
@@ -440,7 +440,7 @@ void DnDURIList::Clear(void)
     {
         DnDURIObject *pCurObj = m_lstTree.at(i);
         AssertPtr(pCurObj);
-        RTMemFree(pCurObj);
+        delete pCurObj;
     }
     m_lstTree.clear();
 
@@ -461,7 +461,7 @@ void DnDURIList::RemoveFirst(void)
     m_cbTotal -= cbSize; /* Adjust total size. */
 
     pCurObj->Close();
-    RTMemFree(pCurObj);
+    delete pCurObj;
 
     m_lstTree.removeFirst();
 }
