54c954b2be8de94c82add71eceec65b49483cd9a
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2019-05-30 05:40:08 +0000

    VMM/CPUM: Nested VMX: bugref:9180 Nit.
    svn:sync-xref-src-repo-rev: r130980

--- src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
+++ src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUMAllRegs.cpp 78867 2019-05-30 05:39:12Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: CPUMAllRegs.cpp 78868 2019-05-30 05:40:08Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * CPUM - CPU Monitor(/Manager) - Getters and Setters.
  */
@@ -3012,9 +3012,11 @@ VMM_INT_DECL(uint64_t) CPUMRemoveNestedGuestTscOffset(PCVMCPU pVCpu, uint64_t uT
     PCCPUMCTX pCtx = &pVCpu->cpum.s.Guest;
     if (CPUMIsGuestInVmxNonRootMode(pCtx))
     {
-        PCVMXVVMCS pVmcs = pCtx->hwvirt.vmx.CTX_SUFF(pVmcs);
         if (CPUMIsGuestVmxProcCtlsSet(pVCpu, pCtx, VMX_PROC_CTLS_USE_TSC_OFFSETTING))
+        {
+            PCVMXVVMCS pVmcs = pCtx->hwvirt.vmx.CTX_SUFF(pVmcs);
             return uTicks - pVmcs->u64TscOffset.u;
+        }
         return uTicks;
     }
 
