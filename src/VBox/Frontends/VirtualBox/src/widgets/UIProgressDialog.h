853428bb1766c5f31e1b752d444287fdda4e3391
Sergey Dubov <sergey.dubov@oracle.com>
2019-06-04 14:59:37 +0000

    FE/Qt: bugref:9434: UIProgressDialog: Cache current operation weight among other progress parameters; Use agreement implemented in r131088 and r131090 to reflect infinite progress animation if the only operation weight is equal to 1.
    svn:sync-xref-src-repo-rev: r131095

--- src/VBox/Frontends/VirtualBox/src/widgets/UIProgressDialog.h
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIProgressDialog.h
@@ -1,4 +1,4 @@
-/* $Id: UIProgressDialog.h 76581 2019-01-01 06:24:57Z knut.osmundsen@oracle.com $ */
+/* $Id: UIProgressDialog.h 78969 2019-06-04 14:59:37Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIProgressDialog class declaration.
  */
@@ -154,6 +154,8 @@ private:
     const ulong  m_cOperations;
     /** Holds the number of current operation. */
     ulong        m_uCurrentOperation;
+    /** Holds the weight of current operation. */
+    ulong        m_uCurrentOperationWeight;
     /** Holds whether progress cancel is enabled. */
     bool         m_fCancelEnabled;
     /** Holds whether the progress has ended. */
