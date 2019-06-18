b93b9d177bb870b66874c2d03d3c78e2e01c6e7b
Sergey Dubov <sergey.dubov@oracle.com>
2019-06-18 09:52:35 +0000

    FE/Qt: bugref:7127: Reverting r131365 since it's too early.
    svn:sync-xref-src-repo-rev: r131380

--- src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMediumManager.cpp 79192 2019-06-17 17:02:14Z sergey.dubov@oracle.com $ */
+/* $Id: UIMediumManager.cpp 79204 2019-06-18 09:52:35Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMediumManager class implementation.
  */
@@ -693,7 +693,6 @@ void UIMediumManagerWidget::prepare()
     /* Apply language settings: */
     retranslateUi();
 
-#ifndef VBOX_GUI_WITH_NEW_MEDIA_EVENTS
     /* Start medium-enumeration (if necessary): */
     if (!vboxGlobal().isMediumEnumerationInProgress())
         vboxGlobal().startMediumEnumeration();
@@ -707,14 +706,6 @@ void UIMediumManagerWidget::prepare()
         if (!vboxGlobal().isMediumEnumerationInProgress())
             sltHandleMediumEnumerationFinish();
     }
-#else
-    /* Emulate medium-enumeration start: */
-    sltHandleMediumEnumerationStart();
-
-    /* Emulate medium-enumeration finish (if necessary): */
-    if (!vboxGlobal().isMediumEnumerationInProgress())
-        sltHandleMediumEnumerationFinish();
-#endif
 }
 
 void UIMediumManagerWidget::prepareConnections()
