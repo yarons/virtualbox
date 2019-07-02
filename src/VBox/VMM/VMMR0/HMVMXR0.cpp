da0db480f44faa6f744ca156042191cfb0703c70
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2019-07-02 17:13:06 +0000

    VMM/HMVMXR0: Nested VMX: bugref:9180 Comment correction, we only update the host-context and shared state (not the rest).
    svn:sync-xref-src-repo-rev: r131742

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 79477 2019-07-02 17:07:56Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXR0.cpp 79478 2019-07-02 17:13:06Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -1381,10 +1381,9 @@ static int hmR0VmxSwitchToGstOrNstGstVmcs(PVMCPU pVCpu, bool fSwitchToNstGstVmcs
         ASMSetFlags(fEFlags);
 
         /*
-         * If we are switching to a VMCS that was executed on a different host CPU or was never
-         * executed before, flag that we need to export the host state before executing
-         * guest/nested-guest code using hardware-assisted VMX in addition to exporting the
-         * guest/nested-guest state.
+         * If we are switching to a VMCS that was executed on a different host CPU or was
+         * never executed before, flag that we need to export the host state before executing
+         * guest/nested-guest code using hardware-assisted VMX.
          */
         if (pVmcsInfoTo->idHostCpu == RTMpCpuId())
         { /* likely */ }
