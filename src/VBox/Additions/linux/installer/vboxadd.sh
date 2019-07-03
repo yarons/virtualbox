cbd38509a6a0de5ab8263ef517f11d23399ad760
Michael Thayer <noreply@oracle.com>
2019-07-03 16:46:58 +0000

    Additions/linux: make sure udev rules are used. bugref:9138: Additions kmod RPMs: support uek5. Call udevadm control --reload to make sure the new udev rules are used after they are installed.
    svn:sync-xref-src-repo-rev: r131779

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-# $Id: vboxadd.sh 79438 2019-07-01 14:08:08Z noreply@oracle.com $
+# $Id: vboxadd.sh 79510 2019-07-03 16:46:58Z noreply@oracle.com $
 ## @file
-# Linux Additions kernel module init script ($Revision: 79438 $)
+# Linux Additions kernel module init script ($Revision: 79510 $)
 #
 
 #
@@ -354,6 +354,9 @@ create_udev_rule()
         ## @todo 60-vboxadd.rules -> 60-vboxguest.rules ?
         echo "KERNEL=${udev_fix}\"vboxguest\", NAME=\"vboxguest\", OWNER=\"vboxadd\", MODE=\"0660\"" > /etc/udev/rules.d/60-vboxadd.rules
         echo "KERNEL=${udev_fix}\"vboxuser\", NAME=\"vboxuser\", OWNER=\"vboxadd\", MODE=\"0666\"" >> /etc/udev/rules.d/60-vboxadd.rules
+        # Make sure the new rule is noticed.
+        udevadm control --reload 2>&1 || true
+        udevcontrol reload_rules 2>&1 || true
     fi
 }
 
@@ -462,6 +465,8 @@ cleanup()
     fi
     rm -f /sbin/mount.vboxsf 2>/dev/null
     rm -f /etc/udev/rules.d/60-vboxadd.rules 2>/dev/null
+    udevadm control --reload 2>&1 || true
+    udevcontrol reload_rules 2>&1 || true
 }
 
 start()
