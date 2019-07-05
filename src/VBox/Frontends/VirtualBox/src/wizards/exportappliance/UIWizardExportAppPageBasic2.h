f1f98c1c379de17604882b7b7b9124b853b61844
Sergey Dubov <sergey.dubov@oracle.com>
2019-07-05 10:18:48 +0000

    FE/Qt: bugref:9484: Export Appliance wizard: 2nd basic page: Adding radio-button options for 2 phase'd export, s.a. bugref:9489.
    svn:sync-xref-src-repo-rev: r131837

--- src/VBox/Frontends/VirtualBox/src/wizards/exportappliance/UIWizardExportAppPageBasic2.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/exportappliance/UIWizardExportAppPageBasic2.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardExportAppPageBasic2.h 79542 2019-07-05 07:41:33Z sergey.dubov@oracle.com $ */
+/* $Id: UIWizardExportAppPageBasic2.h 79552 2019-07-05 10:18:48Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardExportAppPageBasic2 class declaration.
  */
@@ -42,6 +42,7 @@ class QComboBox;
 class QGridLayout;
 class QLabel;
 class QLineEdit;
+class QRadioButton;
 class QStackedWidget;
 class QTableWidget;
 class QIRichTextLabel;
@@ -219,6 +220,15 @@ protected:
     QIToolButton *m_pAccountToolButton;
     /** Holds the account property table instance. */
     QTableWidget *m_pAccountPropertyTable;
+
+    /** Holds the machine label instance. */
+    QLabel       *m_pMachineLabel;
+    /** Holds the ask then export radio button instance. */
+    QRadioButton *m_pRadioAskThenExport;
+    /** Holds the export then ask radio button instance. */
+    QRadioButton *m_pRadioExportThenAsk;
+    /** Holds the don't ask radio button instance. */
+    QRadioButton *m_pRadioDoNotAsk;
 };
 
 
