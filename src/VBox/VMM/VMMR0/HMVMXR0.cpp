aa1c52b75236f534f062f8300dfb7e00f64eca0c
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2019-07-07 15:27:41 +0000

    VMM/HMVMXR0: Nested VMX: bugref:9180 Nit.
    svn:sync-xref-src-repo-rev: r131868

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXR0.cpp 79577 2019-07-07 15:09:59Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXR0.cpp 79578 2019-07-07 15:27:41Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (Intel VT-x) - Host Context Ring-0.
  */
@@ -17095,13 +17095,13 @@ HMVMX_EXIT_DECL hmR0VmxExitVmreadVmwriteNested(PVMCPU pVCpu, PVMXTRANSIENT pVmxT
 
     uint8_t const iGReg = pVmxTransient->ExitInstrInfo.VmreadVmwrite.iReg2;
     Assert(iGReg < RT_ELEMENTS(pVCpu->cpum.GstCtx.aGRegs));
-    uint64_t u64FieldEnc = pVCpu->cpum.GstCtx.aGRegs[iGReg].u64;
+    uint64_t u64VmcsField = pVCpu->cpum.GstCtx.aGRegs[iGReg].u64;
 
     HMVMX_CPUMCTX_ASSERT(pVCpu, CPUMCTX_EXTRN_EFER);
     if (!CPUMIsGuestInLongModeEx(&pVCpu->cpum.GstCtx))
-        u64FieldEnc &= UINT64_C(0xffffffff);
+        u64VmcsField &= UINT64_C(0xffffffff);
 
-    if (CPUMIsGuestVmxVmreadVmwriteInterceptSet(pVCpu, pVmxTransient->uExitReason, u64FieldEnc))
+    if (CPUMIsGuestVmxVmreadVmwriteInterceptSet(pVCpu, pVmxTransient->uExitReason, u64VmcsField))
     {
         rc |= hmR0VmxReadExitInstrLenVmcs(pVmxTransient);
         rc |= hmR0VmxReadExitQualVmcs(pVCpu, pVmxTransient);
