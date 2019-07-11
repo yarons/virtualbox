4f73b6e8821dfc095e5f2d7359659a04d34052f6
Sergey Dubov <sergey.dubov@oracle.com>
2019-07-11 07:27:15 +0000

    FE/Qt: bugref:9495: New Cloud VM wizard: 2nd basic page: Forgot something more in r131870 (s.a. r131894).
    svn:sync-xref-src-repo-rev: r132012

--- src/VBox/Frontends/VirtualBox/src/wizards/newcloudvm/UIWizardNewCloudVMPageBasic2.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/newcloudvm/UIWizardNewCloudVMPageBasic2.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewCloudVMPageBasic2.cpp 79587 2019-07-08 10:01:12Z sergey.dubov@oracle.com $ */
+/* $Id: UIWizardNewCloudVMPageBasic2.cpp 79683 2019-07-11 07:27:15Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewCloudVMPageBasic2 class implementation.
  */
@@ -176,4 +176,5 @@ void UIWizardNewCloudVMPageBasic2::sltInitShortWizardForm()
 
     /* Refresh form properties table: */
     refreshFormPropertiesTable();
+    emit completeChanged();
 }
