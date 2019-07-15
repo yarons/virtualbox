4f88fdfbccfb2b2ab7abc8db278bf0453f046cc4
Valery Ushakov <noreply@oracle.com>
2019-07-15 15:32:29 +0000

    VBoxManage: annotate errorArgument() with RT_IPRT_FORMAT_ATTR.
    svn:sync-xref-src-repo-rev: r132163

--- src/VBox/Frontends/VBoxManage/VBoxManage.h
+++ src/VBox/Frontends/VBoxManage/VBoxManage.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxManage.h 79611 2019-07-08 23:33:59Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxManage.h 79796 2019-07-15 15:32:29Z noreply@oracle.com $ */
 /** @file
  * VBoxManage - VirtualBox command-line interface, internal header file.
  */
@@ -193,7 +193,7 @@ RTEXITCODE errorSyntax(USAGECATEGORY enmCommand, const char *pszFormat, ...);
 RTEXITCODE errorSyntaxEx(USAGECATEGORY enmCommand, uint64_t fSubcommandScope, const char *pszFormat, ...);
 RTEXITCODE errorGetOpt(USAGECATEGORY enmCommand, int rc, union RTGETOPTUNION const *pValueUnion);
 RTEXITCODE errorGetOptEx(USAGECATEGORY enmCommand, uint64_t fSubcommandScope, int rc, union RTGETOPTUNION const *pValueUnion);
-RTEXITCODE errorArgument(const char *pszFormat, ...);
+RTEXITCODE errorArgument(const char *pszFormat, ...) RT_IPRT_FORMAT_ATTR(1, 2);
 
 void printUsageInternal(USAGECATEGORY enmCommand, PRTSTREAM pStrm);
 
