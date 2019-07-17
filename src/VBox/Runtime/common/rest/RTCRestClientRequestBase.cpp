00906d91aa95cfdf8c0c29a249c779ed1b6a48f0
Valery Ushakov <noreply@oracle.com>
2019-07-17 15:12:00 +0000

    IPRT/rest: doQueryParameters - use %RMpf instead of %RMpq.  Plus sings must be encoded, otherwise they will be misinterpreted as spaces by the peer.
    svn:sync-xref-src-repo-rev: r132223

--- src/VBox/Runtime/common/rest/RTCRestClientRequestBase.cpp
+++ src/VBox/Runtime/common/rest/RTCRestClientRequestBase.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTCRestClientRequestBase.cpp 76553 2019-01-01 01:45:53Z knut.osmundsen@oracle.com $ */
+/* $Id: RTCRestClientRequestBase.cpp 79837 2019-07-17 15:12:00Z noreply@oracle.com $ */
 /** @file
  * IPRT - C++ REST, RTCRestClientRequestBase implementation.
  */
@@ -152,7 +152,7 @@ int RTCRestClientRequestBase::doQueryParameters(RTCString *a_pStrQuery, QUERYPAR
                 int rc = a_papQueryParamObjs[i]->toString(&strTmpVal, a_paQueryParams[i].fFlags);
                 AssertRCReturn(rc, rc);
 
-                rc = a_pStrQuery->appendPrintfNoThrow("%c%RMpq=%RMpq", chSep, a_paQueryParams[i].pszName, strTmpVal.c_str());
+                rc = a_pStrQuery->appendPrintfNoThrow("%c%RMpf=%RMpf", chSep, a_paQueryParams[i].pszName, strTmpVal.c_str());
                 AssertRCReturn(rc, rc);
 
                 chSep = '&';
@@ -171,7 +171,7 @@ int RTCRestClientRequestBase::doQueryParameters(RTCString *a_pStrQuery, QUERYPAR
                     int rc = pObj->toString(&strTmpVal, a_paQueryParams[i].fFlags & ~RTCRestObjectBase::kCollectionFormat_Mask);
                     AssertRCReturn(rc, rc);
 
-                    rc = a_pStrQuery->appendPrintfNoThrow("%c%RMpq=%RMpq", chSep, a_paQueryParams[i].pszName, strTmpVal.c_str());
+                    rc = a_pStrQuery->appendPrintfNoThrow("%c%RMpf=%RMpf", chSep, a_paQueryParams[i].pszName, strTmpVal.c_str());
                     AssertRCReturn(rc, rc);
 
                     chSep = '&';
