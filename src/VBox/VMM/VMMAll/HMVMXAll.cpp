90de298fe21d9e69b8b00750a91eefd6ea99db8b
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2019-07-19 09:15:09 +0000

    VMM/HM: Nested VMX: bugref:9180 We need to flush the TLB when the VMX APIC-access page address changes (when we switch to/from guest/nested-guest).
    svn:sync-xref-src-repo-rev: r132276

--- src/VBox/VMM/VMMAll/HMVMXAll.cpp
+++ src/VBox/VMM/VMMAll/HMVMXAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXAll.cpp 79715 2019-07-12 05:59:51Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMVMXAll.cpp 79874 2019-07-19 09:15:09Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM VMX (VT-x) - All contexts.
  */
@@ -1324,6 +1324,13 @@ VMM_INT_DECL(void) HMNotifyVmxNstGstVmexit(PVMCPU pVCpu)
      * VMCS controls on the next nested-guest VM-entry.
      */
     pVCpu->hm.s.vmx.fMergedNstGstCtls = false;
+
+    /*
+     * Flush the TLB before entering the outer guest execution (mainly required since the
+     * APIC-access guest-physical address would have changed and probably more things in
+     * the future).
+     */
+    pVCpu->hm.s.vmx.fSwitchedNstGstFlushTlb = true;
 }
 
 
--- src/VBox/VMM/include/HMInternal.h
+++ src/VBox/VMM/include/HMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: HMInternal.h 79873 2019-07-19 09:07:21Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMInternal.h 79874 2019-07-19 09:15:09Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM - Internal header file.
  */
@@ -993,8 +993,11 @@ typedef struct HMCPU
             bool                        fMergedNstGstCtls;
             /** Whether the nested-guest VMCS has been copied to the shadow VMCS. */
             bool                        fCopiedNstGstToShadowVmcs;
+            /** Whether flushing the TLB is required due to switching to/from the
+             *  nested-geust. */
+            bool                        fSwitchedNstGstFlushTlb;
             /** Alignment. */
-            bool                        afAlignment0[5];
+            bool                        afAlignment0[4];
             /** Cached guest APIC-base MSR for identifying when to map the APIC-access page. */
             uint64_t                    u64GstMsrApicBase;
             /** VMCS cache for batched vmread/vmwrites. */
