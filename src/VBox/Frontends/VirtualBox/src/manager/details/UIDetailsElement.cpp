0d7cdabdfd8531df96d05ab63f6885ba9a749509
Sergey Dubov <sergey.dubov@oracle.com>
2019-08-14 18:13:33 +0000

    FE/Qt: bugref:7720: UIDetailsElement: Bug-fix for r132660, attachment type change should be accessible even when VM is in running/saved states.
    svn:sync-xref-src-repo-rev: r132735

--- src/VBox/Frontends/VirtualBox/src/manager/details/UIDetailsElement.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/details/UIDetailsElement.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIDetailsElement.cpp 80215 2019-08-09 17:08:08Z sergey.dubov@oracle.com $ */
+/* $Id: UIDetailsElement.cpp 80280 2019-08-14 18:13:33Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIDetailsElement class implementation.
  */
@@ -180,7 +180,7 @@ void UIDetailsElement::updateAppearance()
     m_pTextPane->setAnchorRoleRestricted("#attach", cal != ConfigurationAccessLevel_Full);
     m_pTextPane->setAnchorRoleRestricted("#audio_host_driver_type", cal != ConfigurationAccessLevel_Full);
     m_pTextPane->setAnchorRoleRestricted("#audio_controller_type", cal != ConfigurationAccessLevel_Full);
-    m_pTextPane->setAnchorRoleRestricted("#network_attachment_type", cal != ConfigurationAccessLevel_Full);
+    m_pTextPane->setAnchorRoleRestricted("#network_attachment_type", cal == ConfigurationAccessLevel_Null);
     m_pTextPane->setAnchorRoleRestricted("#usb_controller_type", cal != ConfigurationAccessLevel_Full);
 #ifndef VBOX_WS_MAC
     m_pTextPane->setAnchorRoleRestricted("#menu_bar", cal == ConfigurationAccessLevel_Null);
