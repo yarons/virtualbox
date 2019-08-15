a3d250fe507b70c6f54b78f5435ea5d7ed873464
Paul Klissner <noreply@oracle.com>
2019-08-15 18:45:06 +0000

    Storage/DevVirtioSCSI.cpp: Implemented interrupt handling and check/improved nuanced spec'd handling of EVENT_IDX mode, notification, device reset, added client/framework interface and other cleanup (see bugref:9440 Comment 51)
    svn:sync-xref-src-repo-rev: r132764

--- src/VBox/Devices/VirtIO/Virtio_1_0.h
+++ src/VBox/Devices/VirtIO/Virtio_1_0.h
@@ -1,4 +1,4 @@
-/* $Id: Virtio_1_0.h 80240 2019-08-12 23:45:40Z noreply@oracle.com $ */
+/* $Id: Virtio_1_0.h 80306 2019-08-15 18:45:06Z noreply@oracle.com $ */
 /** @file
  * Virtio_1_0.h - Virtio Declarations
  */
@@ -146,15 +146,14 @@ typedef struct VIRTIOCALLBACKS
 /**
  * API to for VirtIO client below this point.
  */
-int           virtioReset        (VIRTIOHANDLE hVirtio);
-bool          virtioQueueInit    (VIRTIOHANDLE hVirtio, uint16_t qIdx, const char *pcszName);
+bool          virtioQueueAttach  (VIRTIOHANDLE hVirtio, uint16_t qIdx, const char *pcszName);
 const char *  virtioQueueGetName (VIRTIOHANDLE hVirtio, uint16_t qIdx);
+bool          virtioQueuePeek    (VIRTIOHANDLE hVirtio, uint16_t qIdx, PVIRTQ_BUF_VECTOR_T pBufVec);
 bool          virtioQueueGet     (VIRTIOHANDLE hVirtio, uint16_t qIdx, PVIRTQ_BUF_VECTOR_T pBufVec, bool fRemove);
 void          virtioQueuePut     (VIRTIOHANDLE hVirtio, uint16_t qIdx, PVIRTQ_BUF_VECTOR_T pBufVec, uint32_t cb);
-bool          virtioQueuePeek    (VIRTIOHANDLE hVirtio, uint16_t qIdx, PVIRTQ_BUF_VECTOR_T pBufVec);
 void          virtioQueueSync    (VIRTIOHANDLE hVirtio, uint16_t qIdx);
 bool          virtioQueueIsEmpty (VIRTIOHANDLE hVirtio, uint16_t qIdx);
-int           virtioGetNumQueues (VIRTIOHANDLE hVirtio);
+void          virtioResetAll     (VIRTIOHANDLE hVirtio);
 
 /** CLIENT MUST CALL ON RELOCATE CALLBACK! */
 void          virtioRelocate     (PPDMDEVINS pDevIns, RTGCINTPTR offDelta);
@@ -166,7 +165,6 @@ void          virtioRelocate     (PPDMDEVINS pDevIns, RTGCINTPTR offDelta);
  * @param   pVirtio                  Device State
  * @param   pPciParams               Values to populate industry standard PCI Configuration Space data structure
  * @param   pcszInstance             Device instance name
- * @param   uNumQueues               Number of Virtio Queues created by consumer (driver)
  * @param   uDevSpecificFeatures     VirtIO device-specific features offered by client
  * @param   devCapReadCallback       Client handler to call upon guest read to device specific capabilities.
  * @param   devCapWriteCallback      Client handler to call upon guest write to device specific capabilities.
