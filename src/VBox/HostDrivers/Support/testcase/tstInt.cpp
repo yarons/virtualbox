99b45aa9f3ddd14a1b8ace8d96075463ce6da890
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-08-16 20:07:11 +0000

    tstInt: Eliminating the VBOX_BUGREF_9217 preprocessor macro.  bugref:9217
    svn:sync-xref-src-repo-rev: r132792

--- src/VBox/HostDrivers/Support/testcase/tstInt.cpp
+++ src/VBox/HostDrivers/Support/testcase/tstInt.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstInt.cpp 80305 2019-08-15 17:35:00Z knut.osmundsen@oracle.com $ */
+/* $Id: tstInt.cpp 80332 2019-08-16 20:07:11Z knut.osmundsen@oracle.com $ */
 /** @file
  * SUP Testcase - Test the interrupt gate feature of the support library.
  */
@@ -94,16 +94,9 @@ int main(int argc, char **argv)
             {
                 PVM pVM = CreateVMReq.pVMR3;
                 AssertRelease(VALID_PTR(pVM));
-#ifdef VBOX_BUGREF_9217
                 AssertRelease(pVM->pVMR0ForCall == CreateVMReq.pVMR0);
-#else
-                AssertRelease(pVM->pVMR0 == CreateVMReq.pVMR0);
-#endif
                 AssertRelease(pVM->pSession == pSession);
                 AssertRelease(pVM->cCpus == 1);
-#ifdef VBOX_WITH_RAW_MODE
-                AssertRelease(pVM->offVMCPU == RT_UOFFSETOF(VM, aCpus));
-#endif
                 pVM->enmVMState = VMSTATE_CREATED;
                 PVMR0 const pVMR0 = CreateVMReq.pVMR0;
 
