6a49637dc1cafe576147c1510b64b70170ceaa12
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2019-08-21 14:51:02 +0000

    com/array.h: handling empty base elements better (hopefully will eliminate static code analysis issues) by using a const array as the representation.
    svn:sync-xref-src-repo-rev: r132838

--- src/VBox/Main/glue/com.cpp
+++ src/VBox/Main/glue/com.cpp
@@ -1,4 +1,4 @@
-/* $Id: com.cpp 76553 2019-01-01 01:45:53Z knut.osmundsen@oracle.com $ */
+/* $Id: com.cpp 80373 2019-08-21 14:51:02Z klaus.espenlaub@oracle.com $ */
 /** @file
  * MS COM / XPCOM Abstraction Layer
  */
@@ -57,13 +57,7 @@ namespace com
 /* static */
 const Guid Guid::Empty; /* default ctor is OK */
 
-#if defined (VBOX_WITH_XPCOM)
-
-/* static */
-const nsID *SafeGUIDArray::nsIDRef::Empty = (const nsID *)Guid::Empty.raw();
-
-#endif /* (VBOX_WITH_XPCOM) */
-
+const char Zeroes[16] = {0, };
 
 
 void GetInterfaceNameByIID(const GUID &aIID, BSTR *aName)
