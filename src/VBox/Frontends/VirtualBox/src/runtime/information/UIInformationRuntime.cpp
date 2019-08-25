f5abcfe53febee36a0683aeba4fad566a909df4f
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2019-08-25 14:56:06 +0000

    FE/Qt: bugref:9510: Putting back the title icon
    svn:sync-xref-src-repo-rev: r132890

--- src/VBox/Frontends/VirtualBox/src/runtime/information/UIInformationRuntime.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/information/UIInformationRuntime.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIInformationRuntime.cpp 80415 2019-08-25 14:44:53Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIInformationRuntime.cpp 80416 2019-08-25 14:56:06Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIInformationRuntime class implementation.
  */
@@ -30,6 +30,7 @@
 #include "UICommon.h"
 #include "UIConverter.h"
 #include "UIExtraDataManager.h"
+#include "UIIconPool.h"
 #include "UIInformationRuntime.h"
 #include "UISession.h"
 
@@ -200,7 +201,10 @@ UIRuntimeInfoWidget::UIRuntimeInfoWidget(QWidget *pParent, const CMachine &machi
 
     retranslateUi();
 
-    QTableWidgetItem *pTitleItem = new QTableWidgetItem(m_strTableTitle);
+    //QString(":/state_running_16px.png");
+    //setItem(0, 0, new QTableWidgetItem(, ""));
+
+    QTableWidgetItem *pTitleItem = new QTableWidgetItem(UIIconPool::iconSet(":/state_running_16px.png"), m_strTableTitle);
     QFont titleFont(font());
     titleFont.setBold(true);
     pTitleItem->setFont(titleFont);
@@ -578,7 +582,7 @@ void UIChart::paintEvent(QPaintEvent *pEvent)
         painter.setPen(QColor(20, 20, 20, 180));
         QFont font = painter.font();
         font.setBold(true);
-        /** @todo: make this size dynamic. aka. autoscale the font. */
+        /** @todo make this size dynamic. aka. autoscale the font. */
         font.setPixelSize(16);
         painter.setFont(font);
         painter.drawText(2 * m_iMarginLeft, 15 * m_iMarginTop, m_strGAWarning);
