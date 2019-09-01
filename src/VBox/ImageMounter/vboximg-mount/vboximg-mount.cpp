28e966ceaf084f179ef350944505f29bf830b5e9
Alexander Eichner <alexander.eichner@oracle.com>
2019-09-01 13:56:52 +0000

    vboximg-mount: macOS build fix
    svn:sync-xref-src-repo-rev: r133033

--- src/VBox/ImageMounter/vboximg-mount/vboximg-mount.cpp
+++ src/VBox/ImageMounter/vboximg-mount/vboximg-mount.cpp
@@ -1,4 +1,4 @@
-/* $Id: vboximg-mount.cpp 80524 2019-09-01 13:17:00Z alexander.eichner@oracle.com $ */
+/* $Id: vboximg-mount.cpp 80526 2019-09-01 13:56:52Z alexander.eichner@oracle.com $ */
 /** @file
  * vboximg-mount - Disk Image Flattening FUSE Program.
  */
@@ -584,10 +584,16 @@ vboximgOp_getattr(const char *pszPath, struct stat *stbuf)
                 stbuf->st_uid   = 0;
                 stbuf->st_gid   = 0;
 
+#ifdef RT_OS_DARWIN
+                RTTimeSpecGetTimespec(&ObjInfo.AccessTime, &stbuf->st_atimespec);
+                RTTimeSpecGetTimespec(&ObjInfo.ModificationTime, &stbuf->st_mtimespec);
+                RTTimeSpecGetTimespec(&ObjInfo.ChangeTime, &stbuf->st_ctimespec);
+                RTTimeSpecGetTimespec(&ObjInfo.BirthTime, &stbuf->st_birthtimespec);
+#else
                 RTTimeSpecGetTimespec(&ObjInfo.AccessTime, &stbuf->st_atim);
                 RTTimeSpecGetTimespec(&ObjInfo.ModificationTime, &stbuf->st_mtim);
-                RTTimeSpecGetTimespec(&ObjInfo.ChangeTime, &stbuf->st_ctim);
-                /*RTTimeSpecGetTimespec(&ObjInfo.BirthTime, &stbuf->st_birthtime);*/ /* Not existing on Linux. */
+                RTTimeSpecGetTimespec(&ObjInfo.ChangeTime, &stbuf->st_ctim); 
+#endif
 
                 switch (ObjInfo.Attr.fMode & RTFS_TYPE_MASK)
                 {
