d9ce494a79464f0ac5d87f936a94c9780729cd51
Alexander Eichner <alexander.eichner@oracle.com>
2019-09-02 10:34:24 +0000

    VMM/PDMR0Device: Disable part of the assertion temporarily to unbreak starting VMs in release mode with certain VM configs, bugref:9218
    svn:sync-xref-src-repo-rev: r133051

--- src/VBox/VMM/VMMR0/PDMR0Device.cpp
+++ src/VBox/VMM/VMMR0/PDMR0Device.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMR0Device.cpp 80531 2019-09-01 23:03:34Z knut.osmundsen@oracle.com $ */
+/* $Id: PDMR0Device.cpp 80544 2019-09-02 10:34:24Z alexander.eichner@oracle.com $ */
 /** @file
  * PDM - Pluggable Device and Driver Manager, R0 Device parts.
  */
@@ -1599,7 +1599,7 @@ VMMR0_INT_DECL(int) PDMR0DeviceCompatSetCritSectReqHandler(PGVM pGVM, PPDMDEVICE
     {
         size_t offCritSect = pReq->pCritSectR3 - pDevIns->pDevInsForR3R0->pvInstanceDataR3;
         AssertLogRelMsgReturn(   offCritSect                     <  pDevIns->pReg->cbInstanceShared
-                              && offCritSect + sizeof(PDMPCIDEV) <= pDevIns->pReg->cbInstanceShared,
+                              /*&& offCritSect + sizeof(PDMPCIDEV) <= pDevIns->pReg->cbInstanceShared*/,
                               ("offCritSect=%p pCritSectR3=%p cbInstanceShared=%#x (%s)\n",
                                offCritSect, pReq->pCritSectR3, pDevIns->pReg->cbInstanceShared, pDevIns->pReg->szName),
                               VERR_INVALID_POINTER);
