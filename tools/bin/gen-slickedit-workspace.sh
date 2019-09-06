c666a735eca39e0a078bfbcdcdde8f0ccebcc803
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-09-06 20:10:04 +0000

    gen-slickedit-workspace.sh: Ignore DOS-style backup files.
    svn:sync-xref-src-repo-rev: r133173

--- tools/bin/gen-slickedit-workspace.sh
+++ tools/bin/gen-slickedit-workspace.sh
@@ -1,5 +1,5 @@
 # !kmk_ash
-# $Id: gen-slickedit-workspace.sh 80629 2019-09-06 10:32:03Z andreas.loeffler@oracle.com $
+# $Id: gen-slickedit-workspace.sh 80642 2019-09-06 20:10:04Z knut.osmundsen@oracle.com $
 ## @file
 # Script for generating a SlickEdit workspace.
 #
@@ -170,7 +170,7 @@ my_wildcard()
     else
         MY_FOLDER="$1-All.lst"
     fi
-    EXCLUDES="*.log;*.kup;*~;*.pyc;*.exe;*.sys;*.dll;*.o;*.obj;*.lib;*.a;*.ko;*.class;*.cvsignore;*.done;*.project;*.actionScriptProperties;*.scm-settings;*.svnpatch.rej;*.svn-base;.svn/*;*.gitignore;*.gitattributes;*.gitmodules;*.swagger-codegen-ignore"
+    EXCLUDES="*.log;*.kup;*~;*.bak;*.bak?;*.pyc;*.exe;*.sys;*.dll;*.o;*.obj;*.lib;*.a;*.ko;*.class;*.cvsignore;*.done;*.project;*.actionScriptProperties;*.scm-settings;*.svnpatch.rej;*.svn-base;.svn/*;*.gitignore;*.gitattributes;*.gitmodules;*.swagger-codegen-ignore"
     echo '        <F N="'"${2}"'/*" Recurse="1" Excludes="'"${EXCLUDES}"'"/>' >> "${MY_FOLDER}"
 }
 
