0dbd3a04d7a93a0f5bf57914df0589e961d7a68d
Alexander Eichner <alexander.eichner@oracle.com>
2019-09-13 11:39:41 +0000

    vboximg-mount: scm fixes
    svn:sync-xref-src-repo-rev: r133341

--- src/VBox/ImageMounter/vboximg-mount/fuse.cpp
+++ src/VBox/ImageMounter/vboximg-mount/fuse.cpp
@@ -1,4 +1,4 @@
-/* $Id: fuse.cpp 80774 2019-09-13 11:17:18Z alexander.eichner@oracle.com $ */
+/* $Id: fuse.cpp 80776 2019-09-13 11:39:41Z alexander.eichner@oracle.com $ */
 /** @file
  *
  * Module to dynamically load libfuse and load all symbols
@@ -15,15 +15,6 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
- *
- * The contents of this file may alternatively be used under the terms
- * of the Common Development and Distribution License Version 1.0
- * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
- * VirtualBox OSE distribution, in which case the provisions of the
- * CDDL are applicable instead of those of the GPL.
- *
- * You may elect to license modified versions of this file under the
- * terms and conditions of either the GPL or the CDDL or both.
  */
 
 #define LOG_GROUP RTLOGGROUP_DEFAULT
