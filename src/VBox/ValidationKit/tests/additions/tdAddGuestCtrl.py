767acef42aaab118cea708b328bc481e13759bb7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-09-18 11:15:58 +0000

    ValKit/tdAddGuestCtrl.py: env check fixes. [copy&past fix]
    svn:sync-xref-src-repo-rev: r133457

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 80880 $"
+__version__ = "$Revision: 80882 $"
 
 # Standard Python imports.
 import errno
@@ -2235,7 +2235,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
         elif self.oTstDrv.fpApiVer >= 6.1 and oTestVm.isWindows():
             aoTests.append(tdTestSessionEx([tdStepSessionSetEnv('=D:', 'D:/tmp'),
                                             tdStepSessionCheckEnv(['=D:=D:/tmp',]),
-                                            tdStepSessionBulkEnv([['=D:=D:/temp', '=FOO', 'foo=bar']),
+                                            tdStepSessionBulkEnv(['=D:=D:/temp', '=FOO', 'foo=bar']),
                                             tdStepSessionCheckEnv(['=D:=D:/temp', '=FOO', 'foo=bar']),
                                             tdStepSessionUnsetEnv('=D:'),
                                             tdStepSessionCheckEnv(['=FOO', 'foo=bar']),
