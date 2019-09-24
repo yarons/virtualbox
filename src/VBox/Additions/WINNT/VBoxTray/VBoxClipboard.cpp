e2efe18ce82c88efd7adf04247f6a18aa013ab21
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-09-24 11:32:11 +0000

    Shared Clipboard/VBoxTray: Only report formats if we actually *have* formats to report.
    svn:sync-xref-src-repo-rev: r133555

--- src/VBox/Additions/WINNT/VBoxTray/VBoxClipboard.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxClipboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxClipboard.cpp 80965 2019-09-24 09:49:34Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxClipboard.cpp 80967 2019-09-24 11:32:11Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxClipboard - Shared clipboard, Windows Guest Implementation.
  */
@@ -481,7 +481,7 @@ static LRESULT vboxClipboardWinProcessMsg(PSHCLCONTEXT pCtx, HWND hwnd, UINT msg
 
        case WM_DRAWCLIPBOARD:
        {
-           LogFlowFunc(("WM_DRAWCLIPBOARD, hwnd %p\n", pWinCtx->hWnd));
+           LogFlowFunc(("WM_DRAWCLIPBOARD: hwnd %p\n", pWinCtx->hWnd));
 
            if (GetClipboardOwner() != hwnd)
            {
@@ -489,8 +489,11 @@ static LRESULT vboxClipboardWinProcessMsg(PSHCLCONTEXT pCtx, HWND hwnd, UINT msg
                /* WM_DRAWCLIPBOARD always expects a return code of 0, so don't change "rc" here. */
                SHCLFORMATDATA Formats;
                int rc = SharedClipboardWinGetFormats(pWinCtx, &Formats);
-               if (RT_SUCCESS(rc))
+               if (   RT_SUCCESS(rc)
+                   && Formats.uFormats != VBOX_SHCL_FMT_NONE)
+               {
                    rc = VbglR3ClipboardFormatsSend(&pCtx->CmdCtx, &Formats);
+               }
            }
 
            lresultRc = SharedClipboardWinChainPassToNext(pWinCtx, msg, wParam, lParam);
