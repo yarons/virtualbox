f2ccac6f4c5aa8cd9626929fa16cf0ddd4e1b49f
Sergey Dubov <sergey.dubov@oracle.com>
2019-10-02 14:13:29 +0000

    FE/Qt: bugref:9582: VM settings / Storage page: HUGE cleanup related to coding-style fixes.
    svn:sync-xref-src-repo-rev: r133712

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.h
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.h
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsStorage.h 81099 2019-10-02 13:20:26Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineSettingsStorage.h 81101 2019-10-02 14:13:29Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineSettingsStorage class declaration.
  */
@@ -51,14 +51,14 @@ signals:
 public:
 
     /** Holds the controller mime-type for the D&D system. */
-    static const QString s_strControllerMimeType;
+    static const QString  s_strControllerMimeType;
     /** Holds the attachment mime-type for the D&D system. */
-    static const QString s_strAttachmentMimeType;
+    static const QString  s_strAttachmentMimeType;
 
     /** Constructs Storage settings page. */
     UIMachineSettingsStorage();
     /** Destructs Storage settings page. */
-    ~UIMachineSettingsStorage();
+    virtual ~UIMachineSettingsStorage() /* override */;
 
     /** Defines chipset @a enmType. */
     void setChipsetType(KChipsetType enmType);
@@ -246,7 +246,7 @@ private:
     bool isAttachmentCouldBeUpdated(const UISettingsCacheMachineStorageAttachment &attachmentCache) const;
 
     /** Holds the machine ID. */
-    QUuid  m_uMachineId;
+    QUuid    m_uMachineId;
     /** Holds the machine settings file-path. */
     QString  m_strMachineSettingsFilePath;
     /** Holds the machine settings file-path. */
