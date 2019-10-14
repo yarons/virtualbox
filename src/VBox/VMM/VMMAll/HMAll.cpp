b4bf2d091fe0b14bd649e80fa8e73d015c71bd0b
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2019-10-14 08:46:35 +0000

    VMM: Log RDPRU feature bit, VMCB intercept bit and VMEXIT code.
    svn:sync-xref-src-repo-rev: r133936

--- src/VBox/VMM/VMMAll/HMAll.cpp
+++ src/VBox/VMM/VMMAll/HMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMAll.cpp 80630 2019-09-06 11:03:24Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: HMAll.cpp 81242 2019-10-14 08:46:35Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * HM - All contexts.
  */
@@ -271,9 +271,10 @@ static const char * const g_apszSvmExitReasons[MAX_EXITREASON_STAT] =
     EXIT_REASON(SVM_EXIT_MWAIT        ,  139, "MWAIT instruction."),
     EXIT_REASON(SVM_EXIT_MWAIT_ARMED  ,  140, "MWAIT instruction when armed."),
     EXIT_REASON(SVM_EXIT_XSETBV       ,  141, "XSETBV instruction."),
+    EXIT_REASON(SVM_EXIT_RDPRU        ,  142, "RDPRU instruction."),
 };
 /** Array index of the last valid AMD-V exit reason. */
-#define MAX_EXITREASON_AMDV              141
+#define MAX_EXITREASON_AMDV              142
 
 /** Special exit reasons not covered in the array above. */
 #define SVM_EXIT_REASON_NPF                  EXIT_REASON(SVM_EXIT_NPF                , 1024, "Nested Page Fault.")
--- src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
+++ src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUMR3CpuId.cpp 81241 2019-10-14 08:26:50Z michal.necasek@oracle.com $ */
+/* $Id: CPUMR3CpuId.cpp 81242 2019-10-14 08:46:35Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * CPUM - CPU ID part.
  */
@@ -6346,6 +6346,7 @@ static DBGFREGSUBFIELD const g_aExtLeaf8EbxSubFields[] =
     DBGFREGSUBFIELD_RO("CLZERO\0"       "Clear zero instruction (cacheline)",            0, 1, 0),
     DBGFREGSUBFIELD_RO("IRPerf\0"       "Instructions retired count support",            1, 1, 0),
     DBGFREGSUBFIELD_RO("XSaveErPtr\0"   "Save/restore error pointers (FXSAVE/RSTOR*)",   2, 1, 0),
+    DBGFREGSUBFIELD_RO("RDPRU\0"        "RDPRU instruction",                             4, 1, 0),
     DBGFREGSUBFIELD_RO("IBPB\0"         "Supports the IBPB command in IA32_PRED_CMD",   12, 1, 0),
     DBGFREGSUBFIELD_TERMINATOR()
 };
