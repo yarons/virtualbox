85f3b98bf212f10837565094c5a4c47c5e10950b
Andreas Loeffler <andreas.loeffler@oracle.com>
2019-10-17 14:24:34 +0000

    Shared Clipboard/Transfers: Build fix.
    svn:sync-xref-src-repo-rev: r134029

--- src/VBox/GuestHost/SharedClipboard/clipboard-win.cpp
+++ src/VBox/GuestHost/SharedClipboard/clipboard-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: clipboard-win.cpp 81317 2019-10-17 14:21:45Z andreas.loeffler@oracle.com $ */
+/* $Id: clipboard-win.cpp 81319 2019-10-17 14:24:34Z andreas.loeffler@oracle.com $ */
 /** @file
  * Shared Clipboard: Windows-specific functions for clipboard handling.
  */
@@ -1144,7 +1144,7 @@ int SharedClipboardWinDropFilesToStringList(DROPFILES *pDropFiles, char **papszL
         {
             LogFlowFunc(("\tFile: %s (cchFile=%RU16)\n", pszFileUtf8, cchFileUtf8));
 
-            LogRel2((("Shared Clipboard: Adding file '%s' to transfer\n", pszFileUtf8));
+            LogRel2(("Shared Clipboard: Adding file '%s' to transfer\n", pszFileUtf8));
 
             rc = RTStrAAppendExN(&pszFiles, 1 /* cPairs */, pszFileUtf8, strlen(pszFileUtf8));
             cchFiles += (uint32_t)strlen(pszFileUtf8);
