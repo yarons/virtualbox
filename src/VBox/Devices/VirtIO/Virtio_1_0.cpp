7c7dd94d6592de158e964712e3e91c30322db794
Paul Klissner <noreply@oracle.com>
2019-10-18 10:55:57 +0000

    Storage/DevVirtioSCSI.cpp: Some improvements to failures related to bad targts, including control queue responses). Not sure if that helps the EFI problem Klaus was seeing.
    svn:sync-xref-src-repo-rev: r134061

--- src/VBox/Devices/VirtIO/Virtio_1_0.cpp
+++ src/VBox/Devices/VirtIO/Virtio_1_0.cpp
@@ -1,4 +1,4 @@
-/* $Id: Virtio_1_0.cpp 81300 2019-10-17 05:51:54Z noreply@oracle.com $ */
+/* $Id: Virtio_1_0.cpp 81348 2019-10-18 10:55:57Z noreply@oracle.com $ */
 /** @file
  * Virtio_1_0 - Virtio Common (PCI, feature & config mgt, queue mgt & proxy, notification mgt)
  */
@@ -1212,14 +1212,14 @@ int   virtioConstruct(PPDMDEVINS             pDevIns,
         if (RT_FAILURE(rc))
         {
             /* See PDMDevHlp.cpp:pdmR3DevHlp_PCIRegisterMsi */
-            Log(("Failed to configure MSI-X (%Rrc). Reverting to INTx\n", rc));
+            LogFunc(("Failed to configure MSI-X (%Rrc). Reverting to INTx\n", rc));
             pVirtio->fMsiSupport = false;
         }
         else
-            Log(("Using MSI-X for guest driver notification\n"));
+            Log2Func(("Using MSI-X for guest driver notification\n"));
     }
     else
-        Log(("MSI-X not available for VBox, using INTx notification\n"));
+        LogFunc(("MSI-X not available for VBox, using INTx notification\n"));
 
 
     /* Set offset to first capability and enable PCI dev capabilities */
