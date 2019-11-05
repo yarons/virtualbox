e2cc73d54da1a8153f3bed74e719676949a38f7f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-11-05 16:45:26 +0000

    Virtio_1_0,DevVirtioScsi: Converting to new PDM model - mostly done, I think. (untested) bugref:9218
    svn:sync-xref-src-repo-rev: r134437

--- src/VBox/Devices/VirtIO/Virtio_1_0.h
+++ src/VBox/Devices/VirtIO/Virtio_1_0.h
@@ -1,4 +1,4 @@
-/* $Id: Virtio_1_0.h 81675 2019-11-05 16:13:34Z knut.osmundsen@oracle.com $ */
+/* $Id: Virtio_1_0.h 81677 2019-11-05 16:45:26Z knut.osmundsen@oracle.com $ */
 /** @file
  * Virtio_1_0.h - Virtio Declarations
  */
@@ -450,6 +450,7 @@ void virtioR3PropagateResumeNotification(PPDMDEVINS pDevIns, PVIRTIOSTATE pVirti
 void virtioR3Term(PPDMDEVINS pDevIns, PVIRTIOSTATE pVirtio, PVIRTIOSTATECC pVirtioCC);
 int  virtioR3Init(PPDMDEVINS pDevIns, PVIRTIOSTATE pVirtio, PVIRTIOSTATECC pVirtioCC, PVIRTIOPCIPARAMS pPciParams,
                   const char *pcszInstance, uint64_t fDevSpecificFeatures, void *pvDevSpecificCfg, uint16_t cbDevSpecificCfg);
+int  virtioRZInit(PPDMDEVINS pDevIns, PVIRTIOSTATE pVirtio, PVIRTIOSTATECC pVirtioCC);
 
 void virtioLogMappedIoValue(const char *pszFunc, const char *pszMember, uint32_t uMemberSize,
                             const void *pv, uint32_t cb, uint32_t uOffset,
