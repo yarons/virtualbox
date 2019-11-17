2a13b4023223619acbf841f309746359d592f12c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-11-17 20:43:45 +0000

    PDM: logging fixes. bugref:9218
    svn:sync-xref-src-repo-rev: r134713

--- src/VBox/VMM/VMMR0/PDMR0Device.cpp
+++ src/VBox/VMM/VMMR0/PDMR0Device.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDMR0Device.cpp 81909 2019-11-17 18:23:56Z knut.osmundsen@oracle.com $ */
+/* $Id: PDMR0Device.cpp 81916 2019-11-17 20:43:45Z knut.osmundsen@oracle.com $ */
 /** @file
  * PDM - Pluggable Device and Driver Manager, R0 Device parts.
  */
@@ -1100,8 +1100,8 @@ static DECLCALLBACK(int) pdmR0DevHlp_PCIBusSetUpContext(PPDMDEVINS pDevIns, PPDM
 static DECLCALLBACK(int) pdmR0DevHlp_PICSetUpContext(PPDMDEVINS pDevIns, PPDMPICREG pPicReg, PCPDMPICHLP *ppPicHlp)
 {
     PDMDEV_ASSERT_DEVINS(pDevIns);
-    LogFlow(("pdmR0DevHlp_PICSetUpContext: caller='%s'/%d: pPicReg=%p:{.u32Version=%#x, .pfnSetIrqR3=%p, .pfnGetInterruptR3=%p, .pszGetIrqRC=%p:{%s}, .pszGetInterruptRC=%p:{%s}, .pszGetIrqR0=%p:{%s}, .pszGetInterruptR0=%p:{%s} } ppPicHlp=%p\n",
-             pDevIns->pReg->szName, pDevIns->iInstance, pPicReg, pPicReg->u32Version, pPicReg->pfnSetIrq, pPicReg->pfnGetInterrupt, ppPicHlp));
+    LogFlow(("pdmR0DevHlp_PICSetUpContext: caller='%s'/%d: pPicReg=%p:{.u32Version=%#x, .pfnSetIrq=%p, .pfnGetInterrupt=%p, .u32TheEnd=%#x } ppPicHlp=%p\n",
+             pDevIns->pReg->szName, pDevIns->iInstance, pPicReg, pPicReg->u32Version, pPicReg->pfnSetIrq, pPicReg->pfnGetInterrupt, pPicReg->u32TheEnd, ppPicHlp));
     PGVM pGVM = pDevIns->Internal.s.pGVM;
 
     /*
