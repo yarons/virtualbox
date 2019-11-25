bc037743806531470d6adcc86ccc9f9e86e83f77
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-11-25 17:26:34 +0000

    DevFdc: Saved state fix - forgot to increase FDC_SAVESTATE_CURRENT. duh!
    svn:sync-xref-src-repo-rev: r135001

--- src/VBox/Devices/Storage/DevFdc.cpp
+++ src/VBox/Devices/Storage/DevFdc.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevFdc.cpp 82177 2019-11-25 13:54:41Z knut.osmundsen@oracle.com $ */
+/* $Id: DevFdc.cpp 82188 2019-11-25 17:26:34Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox storage devices - Floppy disk controller
  */
@@ -61,7 +61,7 @@
 *********************************************************************************************************************************/
 /** @name FDC saved state versions
  * @{ */
-#define FDC_SAVESTATE_CURRENT   2       /**< Current version. */
+#define FDC_SAVESTATE_CURRENT   3       /**< Current version. */
 #define FDC_SAVESTATE_PRE_DELAY 2       /**< Pre IRQDelay. */
 #define FDC_SAVESTATE_OLD       1       /**< The original saved state. */
 /** @}*/
