a0e1d4d591e2f441e3db10de63f207f4d6c816c3
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2019-11-25 18:08:10 +0000

    Main/RecordingSettings: move monitor count query to place before taking the object lock (the recording settings one is much higher lock order than the Machine one)
    svn:sync-xref-src-repo-rev: r135005

--- src/VBox/Main/include/RecordingSettingsImpl.h
+++ src/VBox/Main/include/RecordingSettingsImpl.h
@@ -1,4 +1,4 @@
-/* $Id: RecordingSettingsImpl.h 78068 2019-04-10 09:38:27Z andreas.loeffler@oracle.com $ */
+/* $Id: RecordingSettingsImpl.h 82192 2019-11-25 18:08:10Z klaus.espenlaub@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation - Machine recording screen settings.
  */
@@ -66,7 +66,7 @@ private:
     typedef std::map <uint32_t, ComObjPtr<RecordingScreenSettings> > RecordScreenSettingsMap;
 
     void i_reset(void);
-    int i_syncToMachineDisplays(void);
+    int i_syncToMachineDisplays(uint32_t cMonitors);
     int i_createScreenObj(RecordScreenSettingsMap &screenSettingsMap, uint32_t uScreenId, const settings::RecordingScreenSettings &data);
     int i_destroyScreenObj(RecordScreenSettingsMap &screenSettingsMap, uint32_t uScreenId);
     int i_destroyAllScreenObj(RecordScreenSettingsMap &screenSettingsMap);
