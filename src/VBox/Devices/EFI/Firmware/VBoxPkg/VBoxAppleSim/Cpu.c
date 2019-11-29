3fe570c9f219eed2d63b8d6453c9f18fde2f9fec
Michal Necasek <michal.necasek@oracle.com>
2019-11-29 15:07:28 +0000

    EFI: Set DevicePathsSupported in DataHub (see bugref:6930).
    svn:sync-xref-src-repo-rev: r135109

--- src/VBox/Devices/EFI/Firmware/VBoxPkg/VBoxAppleSim/Cpu.c
+++ src/VBox/Devices/EFI/Firmware/VBoxPkg/VBoxAppleSim/Cpu.c
@@ -1,4 +1,4 @@
-/* $Id: Cpu.c 80924 2019-09-20 13:08:32Z alexander.eichner@oracle.com $ */
+/* $Id: Cpu.c 82285 2019-11-29 15:07:28Z michal.necasek@oracle.com $ */
 /** @file
  * Cpu.c - VirtualBox CPU descriptors
  */
@@ -103,6 +103,7 @@ CpuUpdateDataHub(EFI_BOOT_SERVICES * bs,
     EFI_STATUS                  Status;
     EFI_DATA_HUB_PROTOCOL       *DataHub;
     MAGIC_HUB_DATA              *MagicData;
+    UINT32                      Supported = 1;
     //
     // Locate DataHub protocol.
     //
@@ -122,6 +123,12 @@ CpuUpdateDataHub(EFI_BOOT_SERVICES * bs,
     LogData(DataHub, MagicData, L"FSBFrequency", &FSBFrequency, sizeof(FSBFrequency));
     LogData(DataHub, MagicData, L"TSCFrequency", &TSCFrequency, sizeof(TSCFrequency));
     LogData(DataHub, MagicData, L"CPUFrequency", &CPUFrequency, sizeof(CPUFrequency));
+
+    // The following is required for OS X to construct a SATA boot path. UEFI 2.0 (published
+    // in Jan 2006, same time as the first Intel Macs) did not standardize SATA device paths;
+    // if DevicePathsSupported is not set, OS X will create ATA boot paths which will fail
+    // to boot
+    LogData(DataHub, MagicData, L"DevicePathsSupported", &Supported, sizeof(Supported));
 
     FreePool (MagicData);
 
