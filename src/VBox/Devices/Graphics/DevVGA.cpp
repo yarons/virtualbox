4e17caaa52613f1d962b458a08aa11d2d0e722b8
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-11-29 13:10:02 +0000

    DevVGA: vgaR3RegisterVRAMHandler must pass pDevIns to the handlers, not pThis. Regression from last refactoring. [grr] bugref:9218
    svn:sync-xref-src-repo-rev: r135107

--- src/VBox/Devices/Graphics/DevVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA.cpp 82282 2019-11-29 12:40:59Z knut.osmundsen@oracle.com $ */
+/* $Id: DevVGA.cpp 82283 2019-11-29 13:10:02Z knut.osmundsen@oracle.com $ */
 /** @file
  * DevVGA - VBox VGA/VESA device.
  */
@@ -5587,8 +5587,8 @@ int vgaR3RegisterVRAMHandler(PPDMDEVINS pDevIns, PVGASTATE pThis, uint64_t cbFra
     Assert(pThis->GCPhysVRAM);
     int rc = PGMHandlerPhysicalRegister(PDMDevHlpGetVM(pDevIns),
                                         pThis->GCPhysVRAM, pThis->GCPhysVRAM + (cbFrameBuffer - 1),
-                                        pThis->hLfbAccessHandlerType, pDevIns, pDevIns->pvInstanceDataR0,
-                                        pDevIns->pvInstanceDataRC, "VGA LFB");
+                                        pThis->hLfbAccessHandlerType, pDevIns, pDevIns->pDevInsR0RemoveMe,
+                                        pDevIns->pDevInsForRC, "VGA LFB");
 
     AssertRC(rc);
     return rc;
