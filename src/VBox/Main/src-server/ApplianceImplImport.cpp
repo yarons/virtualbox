e2b8e02fc96d1f547db6a88ff2d1c4d281653c04
Valery Portnyagin <valery.portnyagin@oracle.com>
2019-12-03 10:22:30 +0000

    bugref:9416. Cloud import: replace the existing files in the folder where new VM is created during action.
    svn:sync-xref-src-repo-rev: r135161

--- src/VBox/Main/src-server/ApplianceImplImport.cpp
+++ src/VBox/Main/src-server/ApplianceImplImport.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImplImport.cpp 81964 2019-11-18 20:42:02Z klaus.espenlaub@oracle.com $ */
+/* $Id: ApplianceImplImport.cpp 82337 2019-12-03 10:22:30Z valery.portnyagin@oracle.com $ */
 /** @file
  * IAppliance and IVirtualSystem COM class implementations.
  */
@@ -1818,7 +1818,7 @@ HRESULT Appliance::i_importCloudImpl(TaskCloud *pTask)
 
                             /* Write the file on the disk */
                             vrc = RTVfsFileOpenNormal(strAbsDstPath.c_str(),
-                                                      RTFILE_O_WRITE | RTFILE_O_DENY_ALL | RTFILE_O_CREATE,
+                                                      RTFILE_O_WRITE | RTFILE_O_DENY_ALL | RTFILE_O_CREATE_REPLACE,
                                                       &hVfsDstFile);
                             if (RT_FAILURE(vrc))
                                 throw  setErrorVrc(vrc, tr("Could not create the file '%s' (%Rrc)"), strAbsDstPath.c_str(), vrc);
