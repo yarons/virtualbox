4022d1c6d59c7ffd49c40a35757e9fc964817f26
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2019-12-03 19:15:02 +0000

    Dev/E1000: Fixed regression introduced by r135174
    svn:sync-xref-src-repo-rev: r135184

--- src/VBox/Devices/Network/DevE1000Phy.cpp
+++ src/VBox/Devices/Network/DevE1000Phy.cpp
@@ -1,4 +1,4 @@
-/** $Id: DevE1000Phy.cpp 82347 2019-12-03 16:32:17Z aleksey.ilyushin@oracle.com $ */
+/** $Id: DevE1000Phy.cpp 82354 2019-12-03 19:15:02Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * DevE1000Phy - Intel 82540EM Ethernet Controller Internal PHY Emulation.
  *
@@ -258,7 +258,7 @@ uint16_t Phy::readRegister(PPHY pPhy, uint32_t u32Address, PPDMDEVINS pDevIns)
     int      index = Phy::lookupRegister(u32Address);
     uint16_t u16   = 0;
 
-    if (index > 0)
+    if (index >= 0)
     {
         u16 = s_regMap[index].pfnRead(pPhy, (uint32_t)index, pDevIns);
         PhyLog(("PHY#%d At %02d read  %04X      from %s (%s)\n",
@@ -283,7 +283,7 @@ void Phy::writeRegister(PPHY pPhy, uint32_t u32Address, uint16_t u16Value, PPDMD
 {
     int index = Phy::lookupRegister(u32Address);
 
-    if (index > 0)
+    if (index >= 0)
     {
         PhyLog(("PHY#%d At %02d write      %04X  to  %s (%s)\n",
                 pPhy->iInstance, s_regMap[index].u32Address, u16Value,
