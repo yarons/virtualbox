604f72f5aa21525465f3a5959e677d2020372f64
Sergey Dubov <sergey.dubov@oracle.com>
2019-12-06 19:54:04 +0000

    FE/Qt: One more NLS fix in one of wizards.
    svn:sync-xref-src-repo-rev: r135325

--- src/VBox/Frontends/VirtualBox/src/wizards/importappliance/UIWizardImportAppPageBasic1.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/importappliance/UIWizardImportAppPageBasic1.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIWizardImportAppPageBasic1.cpp 82471 2019-12-06 18:34:00Z sergey.dubov@oracle.com $ */
+/* $Id: UIWizardImportAppPageBasic1.cpp 82473 2019-12-06 19:54:04Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardImportAppPageBasic1 class implementation.
  */
@@ -816,7 +816,7 @@ void UIWizardImportAppPageBasic1::retranslateUi()
 
     /* Translate main label: */
     m_pLabelMain->setText(UIWizardImportApp::tr("Please choose the source to import appliance from.  This can be a "
-                                                "local file system to import OVF archive or one of known  cloud "
+                                                "local file system to import OVF archive or one of known cloud "
                                                 "service providers to import cloud VM from."));
 
     /* Translate source label: */
