9f17a6cba2fb1781a3b860e75299b54f5bff5f38
Sergey Dubov <sergey.dubov@oracle.com>
2019-12-12 08:36:15 +0000

    FE/Qt: bugref:9611: UINetworkAttachmentEditor: Get rid of hardcoded network attachment types, instead acquire these types through CSystemProperties interface.
    svn:sync-xref-src-repo-rev: r135437

--- src/VBox/Frontends/VirtualBox/src/widgets/UINetworkAttachmentEditor.h
+++ src/VBox/Frontends/VirtualBox/src/widgets/UINetworkAttachmentEditor.h
@@ -1,4 +1,4 @@
-/* $Id: UINetworkAttachmentEditor.h 81422 2019-10-21 18:04:10Z aleksey.ilyushin@oracle.com $ */
+/* $Id: UINetworkAttachmentEditor.h 82563 2019-12-12 08:36:15Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UINetworkAttachmentEditor class declaration.
  */
@@ -132,6 +132,9 @@ private:
     QMap<KNetworkAttachmentType, QStringList>  m_names;
     /** Holds the map of current names. */
     QMap<KNetworkAttachmentType, QString>      m_name;
+
+    /** Holds the requested type. */
+    KNetworkAttachmentType  m_enmType;
 
     /** Holds the type label instance. */
     QLabel     *m_pLabelType;
