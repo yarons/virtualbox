6c428b6542a15157b6757bf59a0126f81331c75e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2019-12-17 05:06:29 +0000

    AudioMixer: Attempt to fix another regression from r135459.  bugref:9625
    svn:sync-xref-src-repo-rev: r135488

--- src/VBox/Devices/Audio/AudioMixer.cpp
+++ src/VBox/Devices/Audio/AudioMixer.cpp
@@ -1,4 +1,4 @@
-/* $Id: AudioMixer.cpp 82598 2019-12-16 18:48:48Z andreas.loeffler@oracle.com $ */
+/* $Id: AudioMixer.cpp 82602 2019-12-17 05:06:29Z knut.osmundsen@oracle.com $ */
 /** @file
  * Audio mixing routines for multiplexing audio sources in device emulations.
  *
@@ -1549,10 +1549,8 @@ static int audioMixerSinkSetRecSourceInternal(PAUDMIXSINK pSink, PAUDMIXSTREAM p
             AssertPtr(pStream->pConn);
             rc = pStream->pConn->pfnEnable(pStream->pConn, PDMAUDIODIR_IN, true /* Enable */);
             if (RT_SUCCESS(rc))
-            {
                 pSink->In.pStreamRecSource = pStream;
-            }
-            else if (pSink->In.pStreamRecSource->pConn) /* Stay with the current recording source (if any) and re-enable it. */
+            else if (pSink->In.pStreamRecSource) /* Stay with the current recording source (if any) and re-enable it. */
             {
                 const PPDMIAUDIOCONNECTOR pConn = pSink->In.pStreamRecSource->pConn;
                 AssertPtr(pConn);
