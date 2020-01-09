5e1163c64aa12cbabe1a7d695037402d9a19f37c
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2020-01-09 10:05:26 +0000

    Dev/E1000: (bugref:9615) GSO and VLAN buffer allocation fixes.
    svn:sync-xref-src-repo-rev: r135602

--- src/VBox/Devices/Network/DevE1000.cpp
+++ src/VBox/Devices/Network/DevE1000.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevE1000.cpp 82368 2019-12-04 09:47:53Z aleksey.ilyushin@oracle.com $ */
+/* $Id: DevE1000.cpp 82685 2020-01-09 10:05:26Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * DevE1000 - Intel 82540EM Ethernet Controller Emulation.
  *
@@ -3848,6 +3848,14 @@ DECLINLINE(int) e1kXmitAllocBuf(PE1KSTATE pThis, PE1KSTATECC pThisCC, bool fGso)
             /* Zero packet, no need for the buffer */
             return VINF_SUCCESS;
         }
+        if (fGso && pThis->GsoCtx.u8Type == PDMNETWORKGSOTYPE_INVALID)
+        {
+            E1kLog3(("Invalid GSO context, won't allocate this packet, cb=%u %s%s\n",
+                     pThis->cbTxAlloc, pThis->fVTag ? "VLAN " : "", pThis->fGSO ? "GSO " : ""));
+            /* No valid GSO context is available, ignore this packet. */
+            pThis->cbTxAlloc = 0;
+            return VINF_SUCCESS;
+        }
 
         PPDMINETWORKUP pDrv = pThisCC->CTX_SUFF(pDrv);
         if (RT_UNLIKELY(!pDrv))
@@ -4421,8 +4429,10 @@ static int e1kFallbackAddSegment(PPDMDEVINS pDevIns, PE1KSTATE pThis, RTGCPHYS P
         {
             pThis->cbTxAlloc = RT_MIN(pThis->u32PayRemain,
                                        pThis->contextTSE.dw3.u16MSS)
-                                + pThis->contextTSE.dw3.u8HDRLEN
-                                + (pThis->fVTag ? 4 : 0);
+                                + pThis->contextTSE.dw3.u8HDRLEN;
+            /* Do not add VLAN tags to empty packets. */
+            if (pThis->fVTag && pThis->cbTxAlloc > 0)
+                pThis->cbTxAlloc += 4;
             rc = e1kXmitAllocBuf(pThis, pThisCC, false /* fGSO */);
         }
     }
@@ -5275,7 +5285,8 @@ static bool e1kLocateTxPacket(PE1KSTATE pThis)
             pThis->cbTxAlloc = (!fTSE || pThis->fGSO) ?
                 cbPacket :
                 RT_MIN(cbPacket, pThis->contextTSE.dw3.u16MSS + pThis->contextTSE.dw3.u8HDRLEN);
-            if (pThis->fVTag)
+            /* Do not add VLAN tags to empty packets. */
+            if (pThis->fVTag && pThis->cbTxAlloc > 0)
                 pThis->cbTxAlloc += 4;
             LogFlow(("%s e1kLocateTxPacket: RET true cbTxAlloc=%d cbPacket=%d%s%s\n",
                      pThis->szPrf, pThis->cbTxAlloc, cbPacket,
