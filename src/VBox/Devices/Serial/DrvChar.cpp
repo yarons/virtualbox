e4bdb75e4dea2507028cec8b61eb38339f9154d3
Alexander Eichner <alexander.eichner@oracle.com>
2020-01-20 15:35:50 +0000

    Devices/Serial: Some fixes for unconnected mode where the application inside the guest accessing the serial port could hang
    svn:sync-xref-src-repo-rev: r135759

--- src/VBox/Devices/Serial/DrvChar.cpp
+++ src/VBox/Devices/Serial/DrvChar.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvChar.cpp 81585 2019-10-30 11:19:27Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvChar.cpp 82798 2020-01-20 15:35:50Z alexander.eichner@oracle.com $ */
 /** @file
  * Driver that adapts PDMISTREAM into PDMISERIALCONNECTOR / PDMISERIALPORT.
  */
@@ -191,8 +191,8 @@ static DECLCALLBACK(int) drvCharChgBrk(PPDMISERIALCONNECTOR pInterface, bool fBr
  */
 static DECLCALLBACK(int) drvCharQueryStsLines(PPDMISERIALCONNECTOR pInterface, uint32_t *pfStsLines)
 {
-    /* Nothing to do here. */
-    *pfStsLines = 0;
+    /* Always carrier detect, data set read and clear to send. */
+    *pfStsLines = PDMISERIALPORT_STS_LINE_DCD | PDMISERIALPORT_STS_LINE_DSR | PDMISERIALPORT_STS_LINE_CTS;
     RT_NOREF(pInterface);
     return VINF_SUCCESS;
 }
