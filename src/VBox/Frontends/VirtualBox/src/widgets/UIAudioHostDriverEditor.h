0316732aedf9b2b0b03a38bcc7db7a35fc34b2ed
Sergey Dubov <sergey.dubov@oracle.com>
2020-01-22 14:14:02 +0000

    FE/Qt: bugref:9611: UIAudioHostDriverEditor: Get rid of hardcoded audio driver types, instead acquire these types through CSystemProperties interface.
    svn:sync-xref-src-repo-rev: r135797

--- src/VBox/Frontends/VirtualBox/src/widgets/UIAudioHostDriverEditor.h
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIAudioHostDriverEditor.h
@@ -1,4 +1,4 @@
-/* $Id: UIAudioHostDriverEditor.h 80081 2019-07-31 16:19:53Z sergey.dubov@oracle.com $ */
+/* $Id: UIAudioHostDriverEditor.h 82828 2020-01-22 14:14:02Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIAudioHostDriverEditor class declaration.
  */
@@ -56,6 +56,9 @@ public:
     /** Returns editor value. */
     KAudioDriverType value() const;
 
+    /** Returns the vector of supported values. */
+    QVector<KAudioDriverType> supportedValues() const { return m_supportedValues; }
+
 protected:
 
     /** Handles translation event. */
@@ -75,6 +78,12 @@ private:
 
     /** Holds whether descriptive label should be created. */
     bool  m_fWithLabel;
+
+    /** Holds the value to be selected. */
+    KAudioDriverType  m_enmValue;
+
+    /** Holds the vector of supported values. */
+    QVector<KAudioDriverType>  m_supportedValues;
 
     /** Holds the label instance. */
     QLabel     *m_pLabel;
