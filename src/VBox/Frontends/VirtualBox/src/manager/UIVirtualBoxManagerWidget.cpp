7c1a1901cfa84c04633c492fe2dda6c547e589a9
Sergey Dubov <sergey.dubov@oracle.com>
2020-02-26 19:06:10 +0000

    FE/Qt: bugref:9653: !VirtualBox Manager: More strict Details pane update when cloud VM state-change signal received, s.a. r136284.
    svn:sync-xref-src-repo-rev: r136285

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerWidget.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxManagerWidget.cpp 83163 2020-02-26 14:56:44Z sergey.dubov@oracle.com $ */
+/* $Id: UIVirtualBoxManagerWidget.cpp 83164 2020-02-26 19:06:10Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVirtualBoxManagerWidget class implementation.
  */
@@ -285,19 +285,17 @@ void UIVirtualBoxManagerWidget::sltHandleSlidingAnimationComplete(SlidingDirecti
 
 void UIVirtualBoxManagerWidget::sltHandleCloudMachineStateChange(const QString &strId)
 {
-    /* If that is machine or group item selected: */
-    if (isMachineItemSelected() || isGroupItemSelected())
-    {
-        /* Recache current item info: */
-        recacheCurrentItemInformation();
-    }
-
     /* Acquire current item: */
     UIVirtualMachineItem *pItem = currentItem();
 
-    /* Repeat the task only if we are still on the same item: */
+    /* If we still have same item selected: */
     if (pItem && pItem->id() == strId)
+    {
+        /* Propagate current items to update the Details-pane: */
+        m_pPaneToolsMachine->setItems(currentItems());
+        /* Repeat the task a bit delayed: */
         pItem->toCloud()->updateInfoAsync(true /* delayed? */);
+    }
 
     /* Pass the signal further: */
     emit sigCloudMachineStateChange(strId);
