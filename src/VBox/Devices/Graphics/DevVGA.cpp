1e8990b5d2f98818d3a76e228fe5d5a6637b2906
Michal Necasek <michal.necasek@oracle.com>
2020-03-13 12:19:26 +0000

    VMSVGA: Do not needlessly register VGA LFB handler when updating framebuffer mapping.
    svn:sync-xref-src-repo-rev: r136435

--- src/VBox/Devices/Graphics/DevVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA.cpp 83142 2020-02-24 19:24:26Z serkan.bayraktar@oracle.com $ */
+/* $Id: DevVGA.cpp 83280 2020-03-13 12:19:26Z michal.necasek@oracle.com $ */
 /** @file
  * DevVGA - VBox VGA/VESA device.
  */
@@ -5642,15 +5642,24 @@ static DECLCALLBACK(int) vgaR3PciIORegionVRamMapUnmap(PPDMDEVINS pDevIns, PPDMPC
         AssertLogRelRC(rc);
         if (RT_SUCCESS(rc))
         {
-            rc = PGMHandlerPhysicalRegister(PDMDevHlpGetVM(pDevIns), GCPhysAddress, GCPhysAddress + (pThis->vram_size - 1),
-                                            pThis->hLfbAccessHandlerType, pDevIns, pDevIns->pDevInsR0RemoveMe,
-                                            pDevIns->pDevInsForRC, "VGA LFB");
-            AssertLogRelRC(rc);
-            if (RT_SUCCESS(rc))
+# ifdef VBOX_WITH_VMSVGA
+            Assert(!pThis->svga.fEnabled || !pThis->svga.fVRAMTracking);
+            if (    !pThis->svga.fEnabled
+                ||  (   pThis->svga.fEnabled
+                     && pThis->svga.fVRAMTracking
+                    )
+               )
+# endif
             {
-                pThis->GCPhysVRAM = GCPhysAddress;
-                pThis->vbe_regs[VBE_DISPI_INDEX_FB_BASE_HI] = GCPhysAddress >> 16;
+                rc = PGMHandlerPhysicalRegister(PDMDevHlpGetVM(pDevIns), GCPhysAddress, GCPhysAddress + (pThis->vram_size - 1),
+                                                pThis->hLfbAccessHandlerType, pDevIns, pDevIns->pDevInsR0RemoveMe,
+                                                pDevIns->pDevInsForRC, "VGA LFB");
+                AssertLogRelRC(rc);
             }
+
+            pThis->GCPhysVRAM = GCPhysAddress;
+            pThis->vbe_regs[VBE_DISPI_INDEX_FB_BASE_HI] = GCPhysAddress >> 16;
+
             rc = VINF_PCI_MAPPING_DONE; /* caller doesn't care about any other status, so no problem overwriting error here */
         }
     }
