b10f9dc6ed3888b529335448ddf687ede79c49d4
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-03-20 09:15:13 +0000

    Validation Kit/Guest Control: Re-enabled all tests again.
    svn:sync-xref-src-repo-rev: r136542

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 83336 $"
+__version__ = "$Revision: 83344 $"
 
 # Standard Python imports.
 import errno
@@ -1353,7 +1353,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
             'copy_to', 'copy_from',
             'update_additions'
         ];
-        self.asTests        = 'copy_to'; # self.asTestsDef;
+        self.asTests        = self.asTestsDef;
         self.fSkipKnownBugs = False;
         self.oTestFiles     = None  # type: vboxtestfileset.TestFileSet
 
