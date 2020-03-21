ebb9d120f1dc5076eea516cb353f5119f3be60ab
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2020-03-21 13:02:08 +0000

    TestManager/db.py: Used new utils functions for address a todo in dbTimestampToDatetime.
    svn:sync-xref-src-repo-rev: r136562

--- src/VBox/ValidationKit/testmanager/core/db.py
+++ src/VBox/ValidationKit/testmanager/core/db.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: db.py 82968 2020-02-04 10:35:17Z knut.osmundsen@oracle.com $
+# $Id: db.py 83358 2020-03-21 13:02:08Z knut.osmundsen@oracle.com $
 
 """
 Test Manager - Database Interface.
@@ -26,7 +26,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 82968 $"
+__version__ = "$Revision: 83358 $"
 
 
 # Standard python imports.
@@ -74,7 +74,7 @@ def dbTimestampToDatetime(oValue):
     if isinstance(oValue, datetime.datetime):
         return oValue;
     if utils.isString(oValue):
-        raise Exception('TODO');
+        return utils.parseIsoTimestamp(oValue);
     return oValue.pydatetime();
 
 def dbTimestampToZuluDatetime(oValue):
