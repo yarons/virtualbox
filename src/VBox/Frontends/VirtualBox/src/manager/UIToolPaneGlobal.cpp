3d4a944743077d8be86d799090c45ac9e0a98a38
Sergey Dubov <sergey.dubov@oracle.com>
2020-03-30 13:09:47 +0000

    FE/Qt: bugref:9686: UIToolPaneGlobal: Small coding-style cleanup for r136477.
    svn:sync-xref-src-repo-rev: r136722

--- src/VBox/Frontends/VirtualBox/src/manager/UIToolPaneGlobal.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIToolPaneGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIToolPaneGlobal.cpp 83334 2020-03-19 16:17:08Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIToolPaneGlobal.cpp 83481 2020-03-30 13:09:47Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolPaneGlobal class implementation.
  */
@@ -170,6 +170,7 @@ void UIToolPaneGlobal::openTool(UIToolType enmType)
             }
             case UIToolType_VMResourceMonitor:
             {
+                /* Create VM Resource Monitor: */
                 m_pPaneResourceMonitor = new UIResourceMonitorWidget(EmbedTo_Stack, m_pActionPool, false /* show toolbar */);
                 AssertPtrReturnVoid(m_pPaneResourceMonitor);
                 {
@@ -180,6 +181,7 @@ void UIToolPaneGlobal::openTool(UIToolType enmType)
 
                     /* Configure pane: */
                     m_pPaneResourceMonitor->setProperty("ToolType", QVariant::fromValue(UIToolType_VMResourceMonitor));
+
                     /* Add into layout: */
                     m_pLayout->addWidget(m_pPaneResourceMonitor);
                     m_pLayout->setCurrentWidget(m_pPaneResourceMonitor);
