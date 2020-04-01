31e16393077b1fe01320e2f4c875a7b9f78fe22c
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-04-01 16:24:33 +0000

    Guest Control/VBoxService: Resolved another @todo: Added ability for guest processes to use argv[0] independently of the actual execution command (follow-up to r136639). ​​bugref:9320
    svn:sync-xref-src-repo-rev: r136802

--- src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceControlSession.cpp 83441 2020-03-26 14:38:25Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxServiceControlSession.cpp 83508 2020-04-01 16:24:33Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxServiceControlSession - Guest session handling. Also handles the spawned session processes.
  */
@@ -1719,12 +1719,15 @@ static RTEXITCODE vgsvcGstCtrlSessionSpawnWorker(PVBOXSERVICECTRLSESSION pSessio
         return VGSvcError("Error connecting to guest control service, rc=%Rrc\n", rc);
     g_fControlSupportsOptimizations = VbglR3GuestCtrlSupportsOptimizations(idClient);
     g_idControlSvcClient            = idClient;
-    VbglR3GuestCtrlQueryFeatures(idClient, &g_fControlHostFeatures0);
+
+    int rc2 = VbglR3GuestCtrlQueryFeatures(idClient, &g_fControlHostFeatures0);
+    if (RT_FAILURE(rc2)) /* Querying host features is not fatal -- do not use rc here. */
+        VGSvcVerbose(1, "Querying host features failed with %Rrc\n", rc2);
 
     rc = vgsvcGstCtrlSessionReadKeyAndAccept(idClient, pSession->StartupInfo.uSessionID);
     if (RT_SUCCESS(rc))
     {
-        VGSvcVerbose(1, "Using client ID=%RU32\n", idClient);
+        VGSvcVerbose(1, "Using client ID=%RU32, g_fControlHostFeatures0=%#x\n", idClient, g_fControlHostFeatures0);
 
         /*
          * Report started status.
