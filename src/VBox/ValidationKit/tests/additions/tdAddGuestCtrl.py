91f7001e34987471d5bcc2f2e1a149f64096d0d9
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-04-02 16:25:00 +0000

    Guest Control/Validation Kit: Adjustments for VBox < 6.0. bugref:9320
    svn:sync-xref-src-repo-rev: r136823

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 83515 $"
+__version__ = "$Revision: 83516 $"
 
 # Standard Python imports.
 import errno
@@ -1788,7 +1788,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
                 oFsObjInfo = oCurDir.read();
             except Exception as oXcpt:
                 if vbox.ComError.notEqual(oXcpt, vbox.ComError.VBOX_E_OBJECT_NOT_FOUND):
-                    if oTstDrv.fpApiVer > 5.2:
+                    if self.oTstDrv.fpApiVer > 5.2:
                         reporter.errorXcpt('Error reading directory "%s":' % (sCurDir,));
                     else:
                         # Unlike fileOpen, directoryOpen will not fail if the directory does not exist.
