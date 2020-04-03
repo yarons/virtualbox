95740b3cad561a1f09549988d2ad14c92c5c7a17
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-04-03 13:38:08 +0000

    Validation Kit/testdriver: Also skip logging of OnCursorPositionChanged events; too noisy when locally debugging stuff [pylint].
    svn:sync-xref-src-repo-rev: r136848

--- src/VBox/ValidationKit/testdriver/vbox.py
+++ src/VBox/ValidationKit/testdriver/vbox.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: vbox.py 83527 2020-04-03 13:25:44Z andreas.loeffler@oracle.com $
+# $Id: vbox.py 83529 2020-04-03 13:38:08Z andreas.loeffler@oracle.com $
 # pylint: disable=too-many-lines
 
 """
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 83527 $"
+__version__ = "$Revision: 83529 $"
 
 # pylint: disable=unnecessary-semicolon
 
@@ -716,8 +716,8 @@ class ConsoleEventHandlerBase(EventHandlerBase):
             except:
                 reporter.logXcpt();
         ## @todo implement the other events.
-        if  eType != vboxcon.VBoxEventType_OnMousePointerShapeChanged \
-        and eType != vboxcon.VBoxEventType_OnCursorPositionChanged:
+        if eType not in (vboxcon.VBoxEventType_OnMousePointerShapeChanged,
+                         vboxcon.VBoxEventType_OnCursorPositionChanged):
             if eType in self.dEventNo2Name:
                 reporter.log2('%s(%s)/%s' % (self.dEventNo2Name[eType], str(eType), self.sName));
             else:
