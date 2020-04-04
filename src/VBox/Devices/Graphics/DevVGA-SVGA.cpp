d0000abd606839675b569f13a382becfb2098fc4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2020-04-04 23:57:26 +0000

    DevVGA-SVGA.cpp: Use RTMemFreeZ with cursor data. bugref:9698
    svn:sync-xref-src-repo-rev: r136900

--- src/VBox/Devices/Graphics/DevVGA-SVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA.cpp 83507 2020-04-01 15:43:20Z michal.necasek@oracle.com $ */
+/* $Id: DevVGA-SVGA.cpp 83560 2020-04-04 23:57:26Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMware SVGA device.
  *
@@ -2808,7 +2808,7 @@ static void vmsvgaR3InstallNewCursor(PVGASTATECC pThisCC, PVMSVGAR3STATE pSVGASt
     AssertRC(rc);
 
     if (pSVGAState->Cursor.fActive)
-        RTMemFree(pSVGAState->Cursor.pData);
+        RTMemFreeZ(pSVGAState->Cursor.pData, pSVGAState->Cursor.cbData);
 
     pSVGAState->Cursor.fActive  = true;
     pSVGAState->Cursor.xHotspot = xHot;
@@ -2977,7 +2977,7 @@ static void vmsvgaR3CmdDefineCursor(PVGASTATE pThis, PVGASTATECC pThisCC, PVMSVG
             }
             break;
         default:
-            RTMemFree(pbCopy);
+            RTMemFreeZ(pbCopy, cbCopy);
             AssertFailedReturnVoid();
     }
 
@@ -3057,7 +3057,7 @@ static void vmsvgaR3CmdDefineCursor(PVGASTATE pThis, PVGASTATECC pThisCC, PVMSVG
             }
             break;
         default:
-            RTMemFree(pbCopy);
+            RTMemFreeZ(pbCopy, cbCopy);
             AssertFailedReturnVoid();
     }
 
@@ -6181,7 +6181,7 @@ static void vmsvgaR3StateTerm(PVGASTATE pThis, PVMSVGAR3STATE pSVGAState)
 
     if (pSVGAState->Cursor.fActive)
     {
-        RTMemFree(pSVGAState->Cursor.pData);
+        RTMemFreeZ(pSVGAState->Cursor.pData, pSVGAState->Cursor.cbData);
         pSVGAState->Cursor.pData = NULL;
         pSVGAState->Cursor.fActive = false;
     }
