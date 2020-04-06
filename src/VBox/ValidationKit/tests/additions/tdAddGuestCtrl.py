171aebffa3ab8b6683ff19bc59f1757df3fee5d8
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-04-06 11:15:38 +0000

    Validation Kit/tdAddGuestCtrl: Don't emit an error if debugging preparations are not available on a specific OS.
    svn:sync-xref-src-repo-rev: r136941

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 83530 $"
+__version__ = "$Revision: 83586 $"
 
 # Standard Python imports.
 import errno
@@ -1546,7 +1546,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
                 oTxsSession.syncExec(sFileVBoxServiceGst, (sFileVBoxServiceGst, "-vvvv", "--only-control", \
                                                            "--logfile", "/tmp/VBoxService-txs.log") );
             else: ## @todo Implement others.
-                return reporter.errorXcpt('Debugging not available on this guest OS yet');
+                reporter.log('Debugging not available on this guest OS yet, skipping ...');
 
         except:
             return reporter.errorXcpt('Unable to prepare for debugging');
