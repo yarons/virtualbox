a443506c41419211c800059f2012658b054932de
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2020-04-08 18:49:21 +0000

    SharedClipboard: Trying to make the darwin approach work better.  The problems reported in bugref:9620 is kind of addressed here, but there is more stuff to clean up here.  Lots more.
    svn:sync-xref-src-repo-rev: r137067

--- src/VBox/HostServices/SharedClipboard/darwin-pasteboard.h
+++ src/VBox/HostServices/SharedClipboard/darwin-pasteboard.h
@@ -1,4 +1,4 @@
-/* $Id: darwin-pasteboard.h 82968 2020-02-04 10:35:17Z knut.osmundsen@oracle.com $ */
+/* $Id: darwin-pasteboard.h 83628 2020-04-08 18:49:21Z knut.osmundsen@oracle.com $ */
 /** @file
  * Shared Clipboard Service - Mac OS X host implementation.
  */
@@ -28,7 +28,9 @@ void destroyPasteboard(PasteboardRef *pPasteboardRef);
 
 int queryNewPasteboardFormats(PasteboardRef pPasteboard, uint32_t *pfFormats, bool *pfChanged);
 int readFromPasteboard(PasteboardRef pPasteboard, uint32_t fFormat, void *pv, uint32_t cb, uint32_t *pcbActual);
-int writeToPasteboard(PasteboardRef pPasteboard, void *pv, uint32_t cb, uint32_t fFormat);
+int takePasteboardOwnership(PasteboardRef pPasteboard, uint64_t idOwnership,
+                            const char *pszOwnershipFlavor, const char *pszOwnershipValue);
+int writeToPasteboard(PasteboardRef pPasteboard, uint64_t idOwnership, void *pv, uint32_t cb, uint32_t fFormat);
 
 #endif /* !VBOX_INCLUDED_SRC_SharedClipboard_darwin_pasteboard_h */
 
