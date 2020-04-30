95d9c68f7ca196e5c04a5adb961ee7fd43c393db
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-04-30 17:21:37 +0000

    Validation Kit/tdSmokeTest1: Try waiting for a bit longer (15 minutes) until the CD is available to avoid running into timeouts.
    svn:sync-xref-src-repo-rev: r137706

--- src/VBox/ValidationKit/tests/smoketests/tdSmokeTest1.py
+++ src/VBox/ValidationKit/tests/smoketests/tdSmokeTest1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdSmokeTest1.py 84090 2020-04-29 17:32:17Z andreas.loeffler@oracle.com $
+# $Id: tdSmokeTest1.py 84116 2020-04-30 17:21:37Z andreas.loeffler@oracle.com $
 
 """
 VirtualBox Validation Kit - Smoke Test #1.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84090 $"
+__version__ = "$Revision: 84116 $"
 
 
 # Standard Python imports.
@@ -147,7 +147,7 @@ class tdSmokeTest1(vbox.TestDriver):
         # Simple test.
         self.logVmInfo(oVM);
         # Try waiting for a bit longer (5 minutes) until the CD is available to avoid running into timeouts.
-        oSession, oTxsSession = self.startVmAndConnectToTxsViaTcp(oTestVm.sVmName, fCdWait = True, cMsCdWait = 5 * 60 * 1000);
+        oSession, oTxsSession = self.startVmAndConnectToTxsViaTcp(oTestVm.sVmName, fCdWait = True, cMsCdWait = 15 * 60 * 1000);
         if oSession is not None:
             self.addTask(oTxsSession);
 
