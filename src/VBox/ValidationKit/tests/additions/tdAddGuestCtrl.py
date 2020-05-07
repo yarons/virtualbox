8c2ff9718e4c1875b730cf6462ffc1053f99bb8f
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-05-07 06:50:42 +0000

    Validation Kit/tdAddGuestCtrl: Logging.
    svn:sync-xref-src-repo-rev: r137786

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84177 $"
+__version__ = "$Revision: 84179 $"
 
 # Standard Python imports.
 import errno
@@ -1629,10 +1629,10 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
             self.oTstDrv.sleep(5); # Do some busy waiting.
 
         if not fRc:
-            reporter.testFailure('Waiting for Guest Additions facility "%s" timed out' % (sDesc));
+            reporter.error('Waiting for Guest Additions facility "%s" timed out' % (sDesc));
         else:
-            reporter.testFailure('Guest Additions facility "%s" reached requested status %s after %dms'
-                                 % (sDesc, str(eFacilityStatus), base.timestampMilli() - tsStart));
+            reporter.log('Guest Additions facility "%s" reached requested status %s after %dms'
+                         % (sDesc, str(eFacilityStatus), base.timestampMilli() - tsStart));
 
         return fRc;
 
