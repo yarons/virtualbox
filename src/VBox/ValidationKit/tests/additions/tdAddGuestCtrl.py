2437d28fa7fb7363246ae130bd4ba886f63a0dc4
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-05-12 18:59:49 +0000

    Validation Kit/tdAddGuestCtrl: One more std[err|out] output fix (for buffer objects).
    svn:sync-xref-src-repo-rev: r137915

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84246 $"
+__version__ = "$Revision: 84279 $"
 
 # Standard Python imports.
 import errno
@@ -2262,7 +2262,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
                                                 abBuf = abBuf.tobytes();
                                                 sBuf  = abBuf.decode("utf-8");
                                         if isinstance(abBuf, buffer):
-                                            sBuf = abBuf;
+                                            sBuf = str(abBuf);
                                         for sLine in sBuf.splitlines():
                                             reporter.log('%s: %s' % (sFdNm, sLine));
                                         acbFdOut[iFd] += len(abBuf);
