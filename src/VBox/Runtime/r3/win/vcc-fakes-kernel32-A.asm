73aafd38a8fdf9561a452d6726a948288f1ff340
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2020-05-20 14:27:07 +0000

    IPRT: Quick adjustments for making the vcc-fakes-kernel32* stuff build with VC++ 19.0. bugref:8489
    svn:sync-xref-src-repo-rev: r138102

--- src/VBox/Runtime/r3/win/vcc-fakes-kernel32-A.asm
+++ src/VBox/Runtime/r3/win/vcc-fakes-kernel32-A.asm
@@ -1,4 +1,4 @@
-; $Id: vcc-fakes-kernel32-A.asm 84365 2020-05-19 11:22:09Z knut.osmundsen@oracle.com $
+; $Id: vcc-fakes-kernel32-A.asm 84405 2020-05-20 14:27:07Z knut.osmundsen@oracle.com $
 ;; @file
 ; IPRT - Wrappers for kernel32 APIs missing in NT4 and earlier.
 ;
@@ -34,6 +34,8 @@ GLOBALNAME vcc100_kernel32_fakes_asm
 
 %ifdef VCC_FAKES_TARGET_VCC100
  %include "vcc-fakes-kernel32-100.h"
+%elifdef VCC_FAKES_TARGET_VCC140
+ %include "vcc-fakes-kernel32-141.h"
 %elifdef VCC_FAKES_TARGET_VCC141
  %include "vcc-fakes-kernel32-141.h"
 %elifdef VCC_FAKES_TARGET_VCC142
