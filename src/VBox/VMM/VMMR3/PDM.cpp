79bc80232e58940310741ae7b276af937cd6976d
Alexander Eichner <alexander.eichner@oracle.com>
2020-05-25 11:08:45 +0000

    VMM/PDM: Deregister the DBGF event source when destroying a device
    svn:sync-xref-src-repo-rev: r138220

--- src/VBox/VMM/VMMR3/PDM.cpp
+++ src/VBox/VMM/VMMR3/PDM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PDM.cpp 84459 2020-05-22 12:55:07Z alexander.eichner@oracle.com $ */
+/* $Id: PDM.cpp 84493 2020-05-25 11:08:45Z alexander.eichner@oracle.com $ */
 /** @file
  * PDM - Pluggable Device Manager.
  */
@@ -836,6 +836,14 @@ VMMR3_INT_DECL(int) PDMR3Term(PVM pVM)
             RTMemFree(pDevIns->Internal.s.paDbgfTraceTrack);
             pDevIns->Internal.s.paDbgfTraceTrack = NULL;
         }
+
+#ifdef VBOX_WITH_DBGF_TRACING
+        if (pDevIns->Internal.s.hDbgfTraceEvtSrc != NIL_DBGFTRACEREVTSRC)
+        {
+            DBGFR3TracerDeregisterEvtSrc(pVM, pDevIns->Internal.s.hDbgfTraceEvtSrc);
+            pDevIns->Internal.s.hDbgfTraceEvtSrc = NIL_DBGFTRACEREVTSRC;
+        }
+#endif
 
         TMR3TimerDestroyDevice(pVM, pDevIns);
         SSMR3DeregisterDevice(pVM, pDevIns, NULL, 0);
