86ff68dc1f088f21f8cfd511e050d1265c932cb2
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2020-05-30 19:05:40 +0000

    TestManager: early vcs revisions hack for xtracker.
    svn:sync-xref-src-repo-rev: r138354

--- src/VBox/ValidationKit/testmanager/htdocs/js/common.js
+++ src/VBox/ValidationKit/testmanager/htdocs/js/common.js
@@ -1,4 +1,4 @@
-/* $Id: common.js 84599 2020-05-29 01:12:32Z knut.osmundsen@oracle.com $ */
+/* $Id: common.js 84619 2020-05-30 19:05:40Z knut.osmundsen@oracle.com $ */
 /** @file
  * Common JavaScript functions
  */
@@ -1202,7 +1202,6 @@ function svnHistoryTooltipNewOnLoad()
 
 function svnHistoryTooltipNewOnReadState(oTooltip, oRestReq, oParent)
 {
-    console.log('svnHistoryTooltipNewOnReadState');
     console.log('svnHistoryTooltipNewOnReadState');
 
     /*
