7caaf82414de34089f4e71fc0432cb2b23f6708a
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-06-03 11:24:34 +0000

    Validation Kit/tdAddGuestCtrl.py: Check for buffer instances only with Python >= 2.7.
    svn:sync-xref-src-repo-rev: r138410

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84656 $"
+__version__ = "$Revision: 84657 $"
 
 # Standard Python imports.
 import errno
@@ -2221,8 +2221,8 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
                                                 if isinstance(abBuf, memoryview): ## @todo Why is this happening?
                                                     abBuf = abBuf.tobytes();
                                                     sBuf  = abBuf.decode("utf-8");
-                                            if isinstance(abBuf, buffer):
-                                                sBuf = str(abBuf);
+                                                if isinstance(abBuf, buffer):
+                                                    sBuf = str(abBuf);
                                             for sLine in sBuf.splitlines():
                                                 reporter.log4('%s: %s' % (sFdNm, sLine));
                                         acbFdOut[iFd] += len(abBuf);
