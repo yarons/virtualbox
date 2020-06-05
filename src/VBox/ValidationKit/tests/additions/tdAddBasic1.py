f7a9e25617f7ffb13e097fdb3165f1b93fcce1f7
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-06-05 08:20:38 +0000

    Validation Kit/tdAddBasic1.py: Test name was too long.
    svn:sync-xref-src-repo-rev: r138456

--- src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
+++ src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdAddBasic1.py 84686 2020-06-05 08:17:44Z andreas.loeffler@oracle.com $
+# $Id: tdAddBasic1.py 84687 2020-06-05 08:20:38Z andreas.loeffler@oracle.com $
 
 """
 VirtualBox Validation Kit - Additions Basics #1.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84686 $"
+__version__ = "$Revision: 84687 $"
 
 # Standard Python imports.
 import os;
@@ -338,7 +338,7 @@ class tdAddBasic1(vbox.TestDriver):                                         # py
         _ = oSession;
         if oTestVm.isWindows():
             sPathRegExe   = oTestVm.pathJoin(self.getGuestSystemDir(oTestVm), 'reg.exe');
-            self.txsRunTest(oTxsSession, ('Set env var \"%s\" ...' % (sName,)),
+            self.txsRunTest(oTxsSession, ('Set env var \"%s\"' % (sName,)),
                             30 * 1000, sPathRegExe,
                             (sPathRegExe, 'add',
                              '"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment"', '/v',
