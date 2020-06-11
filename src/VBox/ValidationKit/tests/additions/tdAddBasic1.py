68fa4ee3dbac6bcb696ce71787bafd81f893b820
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-06-11 21:06:48 +0000

    Validation Kit/tdAddBasic1: Attempt to make TXS' updating mechanism work with using the combined VISO (GA + VaKit). Follow-up fix to r138600.
    svn:sync-xref-src-repo-rev: r138606

--- src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
+++ src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdAddBasic1.py 84797 2020-06-11 15:14:20Z andreas.loeffler@oracle.com $
+# $Id: tdAddBasic1.py 84801 2020-06-11 21:06:48Z andreas.loeffler@oracle.com $
 
 """
 VirtualBox Validation Kit - Additions Basics #1.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84797 $"
+__version__ = "$Revision: 84801 $"
 
 # Standard Python imports.
 import os;
@@ -209,9 +209,9 @@ class tdAddBasic1(vbox.TestDriver):                                         # py
         self.logVmInfo(oVM);
 
         if oTestVm.isWindows():
-            self.sFileCdWait = 'VBoxWindowsAdditions.exe';
+            self.sFileCdWait = ('%s/VBoxWindowsAdditions.exe' % (self.sPathGaISO,));
         elif oTestVm.isLinux():
-            self.sFileCdWait = 'VBoxLinuxAdditions.run';
+            self.sFileCdWait = ('%s/VBoxLinuxAdditions.run' % (self.sPathGaISO,));
 
         reporter.testStart('Waiting for TXS + CD (%s)' % (self.sFileCdWait,));
         if oTestVm.isLinux():
