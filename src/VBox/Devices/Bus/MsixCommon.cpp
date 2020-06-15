720ec26d187671311361aef5d093a5cd5e678474
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2020-06-15 08:25:37 +0000

    AMD IOMMU: bugref:9654 PDM interface changes for supplying bus:device:function for devices' initiating PCI interrupts and MSIs. [build fix]
    svn:sync-xref-src-repo-rev: r138633

--- src/VBox/Devices/Bus/MsixCommon.cpp
+++ src/VBox/Devices/Bus/MsixCommon.cpp
@@ -1,4 +1,4 @@
-/* $Id: MsixCommon.cpp 84826 2020-06-15 08:20:40Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: MsixCommon.cpp 84827 2020-06-15 08:25:37Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * MSI-X support routines
  */
@@ -28,7 +28,7 @@
 #include <iprt/assert.h>
 
 #include "MsiCommon.h"
-#include "DevPCIInternal.h"
+#include "DevPciInternal.h"
 #include "PciInline.h"
 
 typedef struct
