23c535243e9d5bd42db8fae4cdcf980ecefc10a3
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-06-24 12:29:14 +0000

    Validation Kit/tdAddGuestCtrl.py: Also use getGuestSystemDir() here-
    svn:sync-xref-src-repo-rev: r138799

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84921 $"
+__version__ = "$Revision: 84927 $"
 
 # Standard Python imports.
 import errno
@@ -1657,7 +1657,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
                                         'HKLM\\SYSTEM\\CurrentControlSet\\Services\\VBoxService',
                                         '/v', 'ImagePath', '/t', 'REG_SZ', '/d', sImagePath, '/f'));
             elif oTestVm.isLinux():
-                sPathSed = '/bin/sed';
+                sPathSed = oTestVm.pathJoin(self.oTstDrv.getGuestSystemDir(oTestVm), 'sed');
                 fRestartVBoxService = self.oTstDrv.txsRunTest(oTxsSession, 'Enabling VBoxService verbose logging', 30 * 1000,
                                          sPathSed,
                                         (sPathSed, '-i', '-e', 's/'
