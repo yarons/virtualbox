25b64d82a0ea2b48eec58b3d962e2b0f5d867b7e
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-06-24 16:18:44 +0000

    Validation Kit/tdAddGuestCtrl.py: Skip empty sources in gctrlCopyFileFrom().
    svn:sync-xref-src-repo-rev: r138804

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84930 $"
+__version__ = "$Revision: 84931 $"
 
 # Standard Python imports.
 import errno
@@ -1709,6 +1709,13 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
         """
         Helper function to copy a single file from the guest to the host.
         """
+
+        # As we pass-in randomly generated file names, the source sometimes can be empty, which
+        # in turn will result in a (correct) error by the API. Simply skip this function then.
+        if not oTest.sSrc:
+            reporter.log2('Skipping guest file "%s"' % (limitString(oTest.sSrc)));
+            return True;
+
         #
         # Do the copying.
         #
