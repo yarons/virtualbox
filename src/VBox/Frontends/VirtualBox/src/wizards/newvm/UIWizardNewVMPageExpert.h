8566066aa2174ac5eeb4a381c00517871e2fd1f8
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2020-07-09 19:48:19 +0000

    FE/Qt: bugref:9515:    - Marking pages and widgets correctly when they break isComplete    - Disabling and enabling page/widgets better    - some more eye candy fixes
    svn:sync-xref-src-repo-rev: r139120

--- src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageExpert.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageExpert.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMPageExpert.h 85150 2020-07-09 12:56:45Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIWizardNewVMPageExpert.h 85157 2020-07-09 19:48:19Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMPageExpert class declaration.
  */
@@ -88,12 +88,11 @@ private:
     enum ExpertToolboxItems
     {
         ExpertToolboxItems_NameAndOSType,
+        ExpertToolboxItems_Disk,
+        ExpertToolboxItems_Hardware,
         ExpertToolboxItems_UsernameHostname,
         ExpertToolboxItems_GAInstall,
-        ExpertToolboxItems_ProductKey,
-        ExpertToolboxItems_Disk,
-        ExpertToolboxItems_Hardware
-
+        ExpertToolboxItems_ProductKey
     };
     /** Translation stuff. */
     void retranslateUi();
