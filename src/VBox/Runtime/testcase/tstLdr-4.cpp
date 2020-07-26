afe371c4198e4ca9baa1f63bcade36080aaf6297
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2020-07-26 11:42:11 +0000

    tstLdr-4: printf fix. bugref:9801
    svn:sync-xref-src-repo-rev: r139513

--- src/VBox/Runtime/testcase/tstLdr-4.cpp
+++ src/VBox/Runtime/testcase/tstLdr-4.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstLdr-4.cpp 85121 2020-07-08 19:33:26Z knut.osmundsen@oracle.com $ */
+/* $Id: tstLdr-4.cpp 85461 2020-07-26 11:42:11Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Testcase for RTLdrOpen using ldrLdrObjR0.r0.
  */
@@ -215,7 +215,7 @@ static int testLdrOne(const char *pszFilename)
             }
             typedef DECLCALLBACKPTR(int, PFNDISASMTEST1,(void));
             PFNDISASMTEST1 pfnDisasmTest1 = (PFNDISASMTEST1)(uintptr_t)Value;
-            RTPrintf("tstLdr-4: pfnDisasmTest1=%p / add-symbol-file %s %#x\n", pfnDisasmTest1, pszFilename, aLoads[i].pvBits);
+            RTPrintf("tstLdr-4: pfnDisasmTest1=%p / add-symbol-file %s %#p\n", pfnDisasmTest1, pszFilename, aLoads[i].pvBits);
             uint32_t iSeg = 0;
             RTLdrEnumSegments(aLoads[i].hLdrMod, testEnumSegment, &iSeg);
 
