10a4effbe6e8666d456698c7aefb8e6738fa3fb0
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-07-27 18:05:42 +0000

    DnD/VBoxClient: Deliver data to X in URI list format.
    svn:sync-xref-src-repo-rev: r139527

--- src/VBox/Additions/x11/VBoxClient/draganddrop.cpp
+++ src/VBox/Additions/x11/VBoxClient/draganddrop.cpp
@@ -1,4 +1,4 @@
-/* $Id: draganddrop.cpp 85371 2020-07-17 10:02:58Z andreas.loeffler@oracle.com $ */
+/* $Id: draganddrop.cpp 85473 2020-07-27 18:05:42Z andreas.loeffler@oracle.com $ */
 /** @file
  * X11 guest client - Drag and drop implementation.
  */
@@ -1932,11 +1932,13 @@ int DragInstance::hgDataReceive(PVBGLR3GUESTDNDMETADATA pMeta)
 
         case VBGLR3GUESTDNDMETADATATYPE_URI_LIST:
         {
-            VBClLogInfo(("URI transfer root directory is '%s'\n", DnDTransferListGetRootPathAbs(&pMeta->u.URI.Transfer)));
+            const char *pcszRootPath = DnDTransferListGetRootPathAbs(&pMeta->u.URI.Transfer);
+            AssertPtrBreakStmt(pcszRootPath, VERR_INVALID_POINTER);
 
-            /* Note: The transfer list already has its root set to a temporary directory, so no need to set/add a new
-             *       path base here. */
-            rc = DnDTransferListGetRootsEx(&pMeta->u.URI.Transfer, DNDTRANSFERLISTFMT_NATIVE, NULL /* pszPathBase */,
+            VBClLogInfo("Transfer list root directory is '%s'\n", pcszRootPath);
+
+            /* Note: Use the URI format here, as X' DnD spec says so. */
+            rc = DnDTransferListGetRootsEx(&pMeta->u.URI.Transfer, DNDTRANSFERLISTFMT_URI, pcszRootPath,
                                            DND_PATH_SEPARATOR, (char **)&pvData, &cbData);
             break;
         }
