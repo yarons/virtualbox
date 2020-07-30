18d43c888cc3db7e14f648d5dba5c50446901a23
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-07-30 09:59:46 +0000

    DnD/Main: Use PAGE_SIZE for meta data block alignment.
    svn:sync-xref-src-repo-rev: r139617

--- src/VBox/Main/include/GuestDnDPrivate.h
+++ src/VBox/Main/include/GuestDnDPrivate.h
@@ -1,4 +1,4 @@
-/* $Id: GuestDnDPrivate.h 85537 2020-07-30 06:55:32Z andreas.loeffler@oracle.com $ */
+/* $Id: GuestDnDPrivate.h 85549 2020-07-30 09:59:46Z andreas.loeffler@oracle.com $ */
 /** @file
  * Private guest drag and drop code, used by GuestDnDTarget +
  * GuestDnDSource.
@@ -158,6 +158,8 @@ struct GuestDnDMetaData
         if (cbSize == cbAllocated)
             return VINF_SUCCESS;
 
+        cbSize = RT_ALIGN_Z(cbSize, PAGE_SIZE);
+
         if (cbSize > _32M) /* Meta data can be up to 32MB. */
             return VERR_BUFFER_OVERFLOW;
 
@@ -165,13 +167,12 @@ struct GuestDnDMetaData
         if (!cbAllocated)
         {
             Assert(cbData == 0);
-            pvTmp = RTMemAllocZ(RT_ALIGN_Z(cbSize, 4096));
+            pvTmp = RTMemAllocZ(cbSize);
         }
         else
         {
             AssertPtr(pvData);
-            pvTmp = RTMemRealloc(pvData, RT_ALIGN_Z(cbSize, 4096));
-            RT_BZERO(pvTmp, cbSize);
+            pvTmp = RTMemRealloc(pvData, cbSize);
         }
 
         if (pvTmp)
