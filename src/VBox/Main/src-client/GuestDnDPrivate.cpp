d7cb205dea80f922769e5b29b82ce6a631faff33
Andreas Loeffler <andreas.loeffler@oracle.com>
2020-07-30 13:07:17 +0000

    DnD/Main: Renamed GuestDnDMsg::setNextXXX() -> GuestDnDMsg::appendXXX().
    svn:sync-xref-src-repo-rev: r139622

--- src/VBox/Main/src-client/GuestDnDPrivate.cpp
+++ src/VBox/Main/src-client/GuestDnDPrivate.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestDnDPrivate.cpp 85553 2020-07-30 12:47:52Z andreas.loeffler@oracle.com $ */
+/* $Id: GuestDnDPrivate.cpp 85554 2020-07-30 13:07:17Z andreas.loeffler@oracle.com $ */
 /** @file
  * Private guest drag and drop code, used by GuestDnDTarget + GuestDnDSource.
  */
@@ -1364,7 +1364,7 @@ int GuestDnDBase::sendCancel(void)
     GuestDnDMsg Msg;
     Msg.setType(HOST_DND_CANCEL);
     if (m_DataBase.uProtocolVersion >= 3)
-        Msg.setNextUInt32(0); /** @todo ContextID not used yet. */
+        Msg.appendUInt32(0); /** @todo ContextID not used yet. */
 
     LogRel2(("DnD: Cancelling operation on guest ...\n"));
 
