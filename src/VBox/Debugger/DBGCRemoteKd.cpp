89a135093cd35bcb7be27415b36e3efb0bdac141
Alexander Eichner <alexander.eichner@oracle.com>
2020-09-13 08:45:29 +0000

    Debugger/RemoteKd: Add beginnings of debug stub talking the KD remote protocol (WinDbg and friends), heavy work in progress [build fix]
    svn:sync-xref-src-repo-rev: r140341

--- src/VBox/Debugger/DBGCRemoteKd.cpp
+++ src/VBox/Debugger/DBGCRemoteKd.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGCRemoteKd.cpp 86108 2020-09-13 08:37:06Z alexander.eichner@oracle.com $ */
+/* $Id: DBGCRemoteKd.cpp 86109 2020-09-13 08:45:29Z alexander.eichner@oracle.com $ */
 /** @file
  * DBGC - Debugger Console, Windows Kd Remote Stub.
  */
@@ -236,7 +236,7 @@ typedef struct NTCONTEXT64
 } NTCONTEXT64;
 AssertCompileSize(NTCONTEXT64, 1232);
 AssertCompileMemberOffset(NTCONTEXT64, FxSave, 0x100);
-AssertCompileMemberOffset(NTCONTEXT64, aRegsVec[0], 0x300);
+AssertCompileMemberOffset(NTCONTEXT64, aRegsVec, 0x300);
 /** Pointer to an amd64 NT context. */
 typedef NTCONTEXT64 *PNTCONTEXT64;
 /** Pointer to a const amd64 NT context. */
