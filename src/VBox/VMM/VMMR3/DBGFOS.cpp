dff9bc0d54789a47d47198296993a74406904cba
Alexander Eichner <alexander.eichner@oracle.com>
2020-09-17 17:09:30 +0000

    DBGF,DBGPlugInWinNt: Extract NtBuildNumber and make it available through the query versions callback
    svn:sync-xref-src-repo-rev: r140410

--- src/VBox/VMM/VMMR3/DBGFOS.cpp
+++ src/VBox/VMM/VMMR3/DBGFOS.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFOS.cpp 86112 2020-09-13 08:50:10Z alexander.eichner@oracle.com $ */
+/* $Id: DBGFOS.cpp 86159 2020-09-17 17:09:30Z alexander.eichner@oracle.com $ */
 /** @file
  * DBGF - Debugger Facility, Guest OS Diggers.
  */
@@ -537,15 +537,17 @@ static DECLCALLBACK(int) dbgfR3OSEmtIDmesg_QueryKernelLog(PDBGFOSIDMESG pThis, P
 /**
  * @interface_method_impl{DBGFOSIWINNT,pfnQueryVersion, Generic EMT wrapper.}
  */
-static DECLCALLBACK(int) dbgfR3OSEmtIWinNt_QueryVersion(PDBGFOSIWINNT pThis, PUVM pUVM, uint32_t *puVersMajor, uint32_t *puVersMinor)
+static DECLCALLBACK(int) dbgfR3OSEmtIWinNt_QueryVersion(PDBGFOSIWINNT pThis, PUVM pUVM, uint32_t *puVersMajor, uint32_t *puVersMinor,
+                                                        uint32_t *puBuildNumber, bool *pf32Bit)
 {
     PDBGFOSEMTWRAPPER pWrapper = RT_FROM_MEMBER(pThis, DBGFOSEMTWRAPPER, uWrapper.WinNt);
     UVM_ASSERT_VALID_EXT_RETURN(pUVM, VERR_INVALID_VM_HANDLE);
     AssertReturn(pUVM == pWrapper->pUVM, VERR_INVALID_VM_HANDLE);
 
     return VMR3ReqPriorityCallWaitU(pWrapper->pUVM, 0 /*idDstCpu*/,
-                                   (PFNRT)pWrapper->uDigger.pWinNt->pfnQueryVersion, 4,
-                                    pWrapper->uDigger.pWinNt, pUVM, puVersMajor, puVersMinor);
+                                   (PFNRT)pWrapper->uDigger.pWinNt->pfnQueryVersion, 6,
+                                    pWrapper->uDigger.pWinNt, pUVM, puVersMajor, puVersMinor,
+                                    puBuildNumber, pf32Bit);
 }
 
 
