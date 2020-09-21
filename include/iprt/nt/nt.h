60866ae14864ba9c5d886912de120dee72ca1781
Alexander Eichner <alexander.eichner@oracle.com>
2020-09-21 09:59:04 +0000

    Runtime/mp-r0drv-nt.cpp: Dynamically determine the size of the KAFFINITY_EX structure as it is not static across Windows versions (increased lately with W10 20H2) [2nd attempt]
    svn:sync-xref-src-repo-rev: r140447

--- include/iprt/nt/nt.h
+++ include/iprt/nt/nt.h
@@ -1,4 +1,4 @@
-/* $Id: nt.h 86177 2020-09-19 16:06:57Z alexander.eichner@oracle.com $ */
+/* $Id: nt.h 86191 2020-09-21 09:59:04Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT - Header for code using the Native NT API.
  */
@@ -606,8 +606,11 @@ typedef struct _KAFFINITY_EX
     uint16_t                Size;
     /** Reserved / aligmment padding. */
     uint32_t                Reserved;
-    /** Bitmap where one bit corresponds to a CPU. */
-    uintptr_t               Bitmap[20];
+    /** Bitmap where one bit corresponds to a CPU.
+     * @note Started at 20 entries.  W10 20H2 increased it to 32.  Must be
+     *       probed by passing a big buffer to KeInitializeAffinityEx and check
+     *       the Size afterwards. */
+    uintptr_t               Bitmap[RT_FLEXIBLE_ARRAY_IN_NESTED_UNION];
 } KAFFINITY_EX;
 typedef KAFFINITY_EX *PKAFFINITY_EX;
 typedef KAFFINITY_EX const *PCKAFFINITY_EX;
