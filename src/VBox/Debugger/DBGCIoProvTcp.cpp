82c264df0cc0c7d7d5d0f413f8116225734870a2
Alexander Eichner <alexander.eichner@oracle.com>
2020-09-28 16:49:52 +0000

    Debugger: More doxygen fixes for r140613
    svn:sync-xref-src-repo-rev: r140616

--- src/VBox/Debugger/DBGCIoProvTcp.cpp
+++ src/VBox/Debugger/DBGCIoProvTcp.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGCIoProvTcp.cpp 86329 2020-09-28 16:40:45Z alexander.eichner@oracle.com $ */
+/* $Id: DBGCIoProvTcp.cpp 86330 2020-09-28 16:49:52Z alexander.eichner@oracle.com $ */
 /** @file
  * DBGC - Debugger Console, TCP I/O provider.
  */
@@ -214,7 +214,7 @@ static DECLCALLBACK(int) dbgcIoProvTcpWaitForConnect(DBGCIOPROV hDbgcIoProv, RTM
 
 
 /**
- * @interface_method_impl{DBGCIOPROV,pfnWaitInterrupt}
+ * @interface_method_impl{DBGCIOPROVREG,pfnWaitInterrupt}
  */
 static DECLCALLBACK(int) dbgcIoProvTcpWaitInterrupt(DBGCIOPROV hDbgcIoProv)
 {
