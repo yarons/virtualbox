db5341e39bf996a914ec7120ebb459e3dca66cbc
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2020-10-06 08:43:49 +0000

    APIC: Stat name nit.
    svn:sync-xref-src-repo-rev: r140760

--- src/VBox/VMM/VMMR3/APIC.cpp
+++ src/VBox/VMM/VMMR3/APIC.cpp
@@ -1,4 +1,4 @@
-/* $Id: APIC.cpp 85965 2020-08-31 23:47:31Z knut.osmundsen@oracle.com $ */
+/* $Id: APIC.cpp 86456 2020-10-06 08:43:49Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * APIC - Advanced Programmable Interrupt Controller.
  */
@@ -1549,10 +1549,10 @@ DECLCALLBACK(int) apicR3Construct(PPDMDEVINS pDevIns, int iInstance, PCFGMNODE p
         APIC_REG_COUNTER(&pApicCpu->StatMsrReadRZ,     "%u/RZ/MsrRead",     "Number of APIC MSR reads in RZ.");
         APIC_REG_COUNTER(&pApicCpu->StatMsrWriteRZ,    "%u/RZ/MsrWrite",    "Number of APIC MSR writes in RZ.");
 
-        APIC_REG_COUNTER(&pApicCpu->StatMmioReadR3,    "%u/R3/MmioReadR3",  "Number of APIC MMIO reads in R3.");
-        APIC_REG_COUNTER(&pApicCpu->StatMmioWriteR3,   "%u/R3/MmioWriteR3", "Number of APIC MMIO writes in R3.");
-        APIC_REG_COUNTER(&pApicCpu->StatMsrReadR3,     "%u/R3/MsrReadR3",   "Number of APIC MSR reads in R3.");
-        APIC_REG_COUNTER(&pApicCpu->StatMsrWriteR3,    "%u/R3/MsrWriteR3",  "Number of APIC MSR writes in R3.");
+        APIC_REG_COUNTER(&pApicCpu->StatMmioReadR3,    "%u/R3/MmioRead",    "Number of APIC MMIO reads in R3.");
+        APIC_REG_COUNTER(&pApicCpu->StatMmioWriteR3,   "%u/R3/MmioWrite",   "Number of APIC MMIO writes in R3.");
+        APIC_REG_COUNTER(&pApicCpu->StatMsrReadR3,     "%u/R3/MsrRead",     "Number of APIC MSR reads in R3.");
+        APIC_REG_COUNTER(&pApicCpu->StatMsrWriteR3,    "%u/R3/MsrWrite",    "Number of APIC MSR writes in R3.");
 
         APIC_REG_COUNTER(&pApicCpu->StatPostIntrAlreadyPending,
                                                        "%u/PostInterruptAlreadyPending", "Number of times an interrupt is already pending.");
