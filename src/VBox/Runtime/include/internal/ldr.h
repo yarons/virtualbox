d006dc120d1cb6ef7b636fdfebf7df6d6e240c1e
Alexander Eichner <alexander.eichner@oracle.com>
2020-10-11 18:26:52 +0000

    Runtime/include/internal/ldr.h: Doxygen
    svn:sync-xref-src-repo-rev: r140839

--- src/VBox/Runtime/include/internal/ldr.h
+++ src/VBox/Runtime/include/internal/ldr.h
@@ -1,4 +1,4 @@
-/* $Id: ldr.h 85626 2020-08-06 00:42:43Z knut.osmundsen@oracle.com $ */
+/* $Id: ldr.h 86524 2020-10-11 18:26:52Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT - Loader Internals.
  */
@@ -457,7 +457,6 @@ typedef struct RTLDROPS
      *                          pfnEnumDbgInfo.  Otherwise, pass UINT32_MAX.
      * @param   off             The offset into the image file.
      * @param   cb              The number of bytes to read.
-     * @param   pMod            Pointer to the loader module structure.
      */
     DECLCALLBACKMEMBER(int, pfnReadDbgInfo,(PRTLDRMODINTERNAL pMod, uint32_t iDbgInfo, RTFOFF off, size_t cb, void *pvBuf));
 
