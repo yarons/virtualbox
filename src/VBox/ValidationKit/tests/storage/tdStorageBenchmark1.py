408d85a94afc0e526ffe59a72280119145653ce6
Alexander Eichner <alexander.eichner@oracle.com>
2020-10-12 06:46:14 +0000

    ValidationKit/tests/tdStorageBenchmark1.py: Disable iozone and fio tests as a default in preparation to only support our own IoPerf utility in order to be able to run it on different guests
    svn:sync-xref-src-repo-rev: r140848

--- src/VBox/ValidationKit/tests/storage/tdStorageBenchmark1.py
+++ src/VBox/ValidationKit/tests/storage/tdStorageBenchmark1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdStorageBenchmark1.py 85159 2020-07-10 06:59:47Z alexander.eichner@oracle.com $
+# $Id: tdStorageBenchmark1.py 86533 2020-10-12 06:46:14Z alexander.eichner@oracle.com $
 
 """
 VirtualBox Validation Kit - Storage benchmark.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 85159 $"
+__version__ = "$Revision: 86533 $"
 
 
 # Standard Python imports.
@@ -569,7 +569,7 @@ class tdStorageBenchmark(vbox.TestDriver):                                      
         self.asDiskFormats           = self.asDiskFormatsDef;
         self.asDiskVariantsDef       = ['Dynamic', 'Fixed', 'DynamicSplit2G', 'FixedSplit2G', 'Network'];
         self.asDiskVariants          = self.asDiskVariantsDef;
-        self.asTestsDef              = ['iozone', 'fio', 'ioperf'];
+        self.asTestsDef              = ['ioperf'];
         self.asTests                 = self.asTestsDef;
         self.asTestSetsDef           = ['Fast', 'Functionality', 'Benchmark', 'Stress'];
         self.asTestSets              = self.asTestSetsDef;
