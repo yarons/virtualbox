75f4491e46efa29cea8b406b9db7e37c602d0f86
Alexander Eichner <alexander.eichner@oracle.com>
2020-10-25 10:51:52 +0000

    VMM/DBGF: Updates to the new breakpoint manager, implement global breakpoint table chunk allocation and register breakpoints which should work again, bugref:9837 [doxygen fix]
    svn:sync-xref-src-repo-rev: r141084

--- src/VBox/VMM/VMMR0/DBGFR0Bp.cpp
+++ src/VBox/VMM/VMMR0/DBGFR0Bp.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFR0Bp.cpp 86699 2020-10-25 10:44:39Z alexander.eichner@oracle.com $ */
+/* $Id: DBGFR0Bp.cpp 86700 2020-10-25 10:51:52Z alexander.eichner@oracle.com $ */
 /** @file
  * DBGF - Debugger Facility, R0 breakpoint management part.
  */
@@ -138,7 +138,7 @@ DECLHIDDEN(void) dbgfR0BpDestroy(PGVM pGVM)
  *
  * @returns VBox status code.
  * @param   pGVM            The global (ring-0) VM structure.
- * @param   ppBpLocL1R3     Where to return the ring-3 L1 lookup table address on success.
+ * @param   ppaBpLocL1R3    Where to return the ring-3 L1 lookup table address on success.
  * @thread  EMT(0)
  */
 static int dbgfR0BpInitWorker(PGVM pGVM, R3PTRTYPE(volatile uint32_t *) *ppaBpLocL1R3)
