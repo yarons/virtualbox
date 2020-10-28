62db570abc3939d04fd9456781625f96ed70ed53
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2020-10-28 11:54:22 +0000

    FE/Qt: bugref:9831. Adding an action to control toolbar visibility
    svn:sync-xref-src-repo-rev: r141126

--- src/VBox/Frontends/VirtualBox/src/helpbrowser/UIHelpBrowserWidget.h
+++ src/VBox/Frontends/VirtualBox/src/helpbrowser/UIHelpBrowserWidget.h
@@ -1,4 +1,4 @@
-/* $Id: UIHelpBrowserWidget.h 86735 2020-10-28 11:43:16Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIHelpBrowserWidget.h 86736 2020-10-28 11:54:22Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIHelpBrowserWidget class declaration.
  */
@@ -79,7 +79,8 @@ private slots:
 
     void sltHandleHelpEngineSetupFinished();
     void sltHandleContentWidgetItemClicked(const QModelIndex &index);
-    void sltHandleTabVisibility(bool togggled);
+    void sltHandleSideBarVisibility(bool togggled);
+    void sltHandleToolBarVisibility(bool togggled);
     void sltHandleHelpBrowserViewerSourceChange(const QUrl &source);
     void sltHandleContentsCreated();
     void sltHandleHomeAction();
@@ -141,7 +142,8 @@ private:
     QHelpIndexWidget    *m_pIndexWidget;
     QHelpContentModel   *m_pContentModel;
     QWidget             *m_pBookmarksWidget;
-    QAction             *m_pShowHideTabWidgetAction;
+    QAction             *m_pShowHideSideBarAction;
+    QAction             *m_pShowHideToolBarAction;
     QAction             *m_pHomeAction;
     QAction             *m_pForwardAction;
     QAction             *m_pBackwardAction;
