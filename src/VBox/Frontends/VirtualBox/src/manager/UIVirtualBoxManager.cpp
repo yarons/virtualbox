1a8ac53ff361e40c7b39fca5d07a1141828f32f3
Sergey Dubov <sergey.dubov@oracle.com>
2020-12-01 16:24:51 +0000

    FE/Qt: bugref:9653: !VirtualBox Manager: No VNC actions for Cloud Console stuff for now.
    svn:sync-xref-src-repo-rev: r141638

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxManager.cpp 87023 2020-12-01 14:42:29Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVirtualBoxManager.cpp 87027 2020-12-01 16:24:51Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVirtualBoxManager class implementation.
  */
@@ -3024,8 +3024,8 @@ void UIVirtualBoxManager::updateMenuMachineConsole(QMenu *pMenu)
         /* Copy command to clipboard actions: */
         pMenu->addAction(actionPool()->action(UIActionIndexMN_M_Machine_M_Console_S_CopyCommandSerialUnix));
         pMenu->addAction(actionPool()->action(UIActionIndexMN_M_Machine_M_Console_S_CopyCommandSerialWindows));
-        pMenu->addAction(actionPool()->action(UIActionIndexMN_M_Machine_M_Console_S_CopyCommandVNCUnix));
-        pMenu->addAction(actionPool()->action(UIActionIndexMN_M_Machine_M_Console_S_CopyCommandVNCWindows));
+//        pMenu->addAction(actionPool()->action(UIActionIndexMN_M_Machine_M_Console_S_CopyCommandVNCUnix));
+//        pMenu->addAction(actionPool()->action(UIActionIndexMN_M_Machine_M_Console_S_CopyCommandVNCWindows));
         pMenu->addSeparator();
 
         /* Default Connect action: */
