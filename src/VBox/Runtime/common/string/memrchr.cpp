6dcf8b18180673a78043c2e25ed23dfe01592442
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-01-08 15:06:12 +0000

    IPRT: memrchr.cpp fix, need it for arm. bugref:9898
    svn:sync-xref-src-repo-rev: r142154

--- src/VBox/Runtime/common/string/memrchr.cpp
+++ src/VBox/Runtime/common/string/memrchr.cpp
@@ -1,4 +1,4 @@
-/* $Id: memrchr.cpp 82968 2020-02-04 10:35:17Z knut.osmundsen@oracle.com $ */
+/* $Id: memrchr.cpp 87201 2021-01-08 15:06:12Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - CRT Strings, memrchr().
  */
@@ -35,15 +35,15 @@
  * Search for a given byte starting at the end of the block.
  *
  * @returns Pointer on a match or NULL otherwise.
- * @param   pb      Pointer to the block.
+ * @param   pv      Pointer to the block.
  * @param   ch      The char to search for.
  * @param   cb      The size of the block.
  */
-void *memrchr(const char *pb, int ch, size_t cb)
+void *memrchr(const void *pv, int ch, size_t cb)
 {
     if (cb)
     {
-        const char *pbCur = pb + cb - 1;
+        const char *pbCur = (const char *)pv + cb - 1;
 
         while (cb)
         {
