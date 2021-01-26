a7005a01dd1b89f85451e6ef49517aa4f1727a4f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-01-26 20:48:47 +0000

    VMM/HMVMX,HMSVM: Baked the hmR0VMXStartVMWrapXMM fun into the hmR0VmxStartVm template like already done for SVM.  Fixed copy&paste bug in non-volatile xmm register restoring in hmR0SvmVmRun. [build fix]
    svn:sync-xref-src-repo-rev: r142457

--- src/VBox/VMM/VMMR0/HMR0A.asm
+++ src/VBox/VMM/VMMR0/HMR0A.asm
@@ -1,4 +1,4 @@
-; $Id: HMR0A.asm 87443 2021-01-26 20:32:56Z knut.osmundsen@oracle.com $
+; $Id: HMR0A.asm 87444 2021-01-26 20:48:47Z knut.osmundsen@oracle.com $
 ;; @file
 ; HM - Ring-0 VMX, SVM world-switch and helper routines.
 ;
@@ -1121,9 +1121,12 @@ hmR0VmxStartVmTemplate _WithXcr0_SansIbpbEntry_WithL1dEntry_WithMdsEntry_WithIbp
 hmR0VmxStartVmTemplate _SansXcr0_WithIbpbEntry_WithL1dEntry_WithMdsEntry_WithIbpbExit %+ %2, 0, CPUMCTX_WSF_IBPB_ENTRY | CPUMCTX_WSF_L1D_ENTRY | CPUMCTX_WSF_MDS_ENTRY | CPUMCTX_WSF_IBPB_EXIT, %1
 hmR0VmxStartVmTemplate _WithXcr0_WithIbpbEntry_WithL1dEntry_WithMdsEntry_WithIbpbExit %+ %2, 1, CPUMCTX_WSF_IBPB_ENTRY | CPUMCTX_WSF_L1D_ENTRY | CPUMCTX_WSF_MDS_ENTRY | CPUMCTX_WSF_IBPB_EXIT, %1
 %endmacro
+
 hmR0VmxStartVmSseTemplate 0,,RT_NOTHING
+%ifdef VBOX_WITH_KERNEL_USING_XMM
 hmR0VmxStartVmSseTemplate 1,_SseManual,RT_NOTHING
 hmR0VmxStartVmSseTemplate 2,_SseXSave,RT_NOTHING
+%endif
 
 
 ;;
