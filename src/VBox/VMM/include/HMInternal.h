001094b988a05ed894fc791c2d2928222a09113f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-02-02 11:27:43 +0000

    VMM/HMVMX: Moving more stuff to HMR0PERVM. Set freed memory handles/pointers to NIL/NULL. bugref:9217
    svn:sync-xref-src-repo-rev: r142556

--- src/VBox/VMM/include/HMInternal.h
+++ src/VBox/VMM/include/HMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: HMInternal.h 87523 2021-02-01 22:51:13Z knut.osmundsen@oracle.com $ */
+/* $Id: HMInternal.h 87530 2021-02-02 11:27:43Z knut.osmundsen@oracle.com $ */
 /** @file
  * HM - Internal header file.
  */
@@ -588,9 +588,6 @@ typedef struct HM
         /** Virtual address of the identity page table used for real mode and protected
          *  mode without paging emulation in EPT mode. */
         R3PTRTYPE(PX86PD)           pNonPagingModeEPTPageTable;
-
-        /** Ring-0 memory object for per-VM VMX structures. */
-        RTR0MEMOBJ                  hMemObj;
     } vmx;
 
     struct
@@ -678,7 +675,15 @@ typedef struct HMR0PERVM
     /** Host kernel flags that HM might need to know (SUPKERNELFEATURES_XXX). */
     uint32_t                    fHostKernelFeatures;
 
-    /** SVM specific data. */
+    /** VT-x specific data. */
+    struct HMR0VMXVM
+    {
+        /** Ring-0 memory object for per-VM VMX structures. */
+        RTR0MEMOBJ                  hMemObj;
+
+    } vmx;
+
+    /** AMD-V specific data. */
     struct HMR0SVMVM
     {
         /** Set if erratum 170 affects the AMD cpu. */
