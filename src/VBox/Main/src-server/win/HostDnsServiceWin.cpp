a4804cc1a25d0918eee7356b053250b0489f44cd
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-02-03 15:43:22 +0000

    Main: Doxygen v1.8.20 seems to get confused by appendTokenizedStrings and dumpHostDnsStrVector prototypes and unable to match them to definitions, so just move the definitions up to the top and drop the prototypes.
    svn:sync-xref-src-repo-rev: r142606

--- src/VBox/Main/src-server/win/HostDnsServiceWin.cpp
+++ src/VBox/Main/src-server/win/HostDnsServiceWin.cpp
@@ -1,4 +1,4 @@
-/* $Id: HostDnsServiceWin.cpp 83794 2020-04-18 13:25:05Z knut.osmundsen@oracle.com $ */
+/* $Id: HostDnsServiceWin.cpp 87578 2021-02-03 15:43:22Z knut.osmundsen@oracle.com $ */
 /** @file
  * Host DNS listener for Windows.
  */
@@ -41,8 +41,41 @@
 #include <iprt/sanitized/string>
 #include <vector>
 
-static inline int registerNotification(const HKEY& hKey, HANDLE& hEvent);
-static void appendTokenizedStrings(std::vector<std::string> &vecStrings, const std::string &strToAppend, char chDelim = ' ');
+
+DECLINLINE(int) registerNotification(const HKEY &hKey, HANDLE &hEvent)
+{
+    LONG lrc = RegNotifyChangeKeyValue(hKey,
+                                       TRUE,
+                                       REG_NOTIFY_CHANGE_LAST_SET,
+                                       hEvent,
+                                       TRUE);
+    AssertMsgReturn(lrc == ERROR_SUCCESS,
+                    ("Failed to register event on the key. Please debug me!"),
+                    VERR_INTERNAL_ERROR);
+
+    return VINF_SUCCESS;
+}
+
+static void appendTokenizedStrings(std::vector<std::string> &vecStrings, const std::string &strToAppend, char chDelim /* = ' ' */)
+{
+    if (strToAppend.empty())
+        return;
+
+    std::istringstream stream(strToAppend);
+    std::string substr;
+
+    while (std::getline(stream, substr, chDelim))
+    {
+        if (substr.empty())
+            continue;
+
+        if (std::find(vecStrings.cbegin(), vecStrings.cend(), substr) != vecStrings.cend())
+            continue;
+
+        vecStrings.push_back(substr);
+    }
+}
+
 
 struct HostDnsServiceWin::Data
 {
@@ -441,39 +474,5 @@ HRESULT HostDnsServiceWin::updateInfo(void)
     HostDnsServiceBase::setInfo(info);
 
     return S_OK;
-}
-
-static inline int registerNotification(const HKEY& hKey, HANDLE& hEvent)
-{
-    LONG lrc = RegNotifyChangeKeyValue(hKey,
-                                       TRUE,
-                                       REG_NOTIFY_CHANGE_LAST_SET,
-                                       hEvent,
-                                       TRUE);
-    AssertMsgReturn(lrc == ERROR_SUCCESS,
-                    ("Failed to register event on the key. Please debug me!"),
-                    VERR_INTERNAL_ERROR);
-
-    return VINF_SUCCESS;
-}
-
-static void appendTokenizedStrings(std::vector<std::string> &vecStrings, const std::string &strToAppend, char chDelim /* = ' ' */)
-{
-    if (strToAppend.empty())
-        return;
-
-    std::istringstream stream(strToAppend);
-    std::string substr;
-
-    while (std::getline(stream, substr, chDelim))
-    {
-        if (substr.empty())
-            continue;
-
-        if (std::find(vecStrings.cbegin(), vecStrings.cend(), substr) != vecStrings.cend())
-            continue;
-
-        vecStrings.push_back(substr);
-    }
 }
 
