6351cdb90819a9eb4ed44ada8250f2bfa967adc6
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2021-02-22 04:24:22 +0000

    AMD IOMMU: bugref:9654 Since AVL tree IOTLB cache has been moved to ring-3, drop compiling AVLU64 tree in ring-0 code as nobody else uses it.
    svn:sync-xref-src-repo-rev: r142891

--- src/VBox/VMM/VMMR0/VMMR0.cpp
+++ src/VBox/VMM/VMMR0/VMMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: VMMR0.cpp 87792 2021-02-18 18:38:24Z knut.osmundsen@oracle.com $ */
+/* $Id: VMMR0.cpp 87829 2021-02-22 04:24:22Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VMM - Host Context Ring 0.
  */
@@ -53,7 +53,6 @@
 
 #include <iprt/asm-amd64-x86.h>
 #include <iprt/assert.h>
-#include <iprt/avl.h>
 #include <iprt/crc.h>
 #include <iprt/mp.h>
 #include <iprt/once.h>
@@ -150,11 +149,6 @@ RT_C_DECLS_END
  * The runtime lives here (in VMMR0.r0) and VBoxDD*R0.r0 links against us. */
 struct CLANG11WEIRDNOTHROW { PFNRT pfn; } g_VMMR0Deps[] =
 {
-    { (PFNRT)RTAvlU64Destroy },
-    { (PFNRT)RTAvlU64DoWithAll },
-    { (PFNRT)RTAvlU64Get },
-    { (PFNRT)RTAvlU64Insert },
-    { (PFNRT)RTAvlU64Remove },
     { (PFNRT)RTCrc32 },
     { (PFNRT)RTOnce },
 #if defined(RT_ARCH_X86) && (defined(RT_OS_SOLARIS) || defined(RT_OS_FREEBSD))
