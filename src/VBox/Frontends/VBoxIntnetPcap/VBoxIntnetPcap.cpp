f5d90f95b0648f8dcff0496a1630f29e6c24339a
Valery Ushakov <noreply@oracle.com>
2021-03-02 20:02:19 +0000

    VBoxIntnetPcap: Map --network to -i to match tcpdump short options. bugref:9959.
    svn:sync-xref-src-repo-rev: r143014

--- src/VBox/Frontends/VBoxIntnetPcap/VBoxIntnetPcap.cpp
+++ src/VBox/Frontends/VBoxIntnetPcap/VBoxIntnetPcap.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxIntnetPcap.cpp 87923 2021-03-02 19:58:53Z noreply@oracle.com $ */
+/* $Id: VBoxIntnetPcap.cpp 87924 2021-03-02 20:02:19Z noreply@oracle.com $ */
 /** @file
  * VBoxIntnetPcap - packet capture for VirtualBox internal networks
  */
@@ -43,7 +43,7 @@ size_t g_cbSnapLen;
 RTGETOPTDEF g_aGetOptDef[] =
 {
     { "--count",                'c',   RTGETOPT_REQ_UINT64 },
-    { "--network",              'n',   RTGETOPT_REQ_STRING },
+    { "--network",              'i',   RTGETOPT_REQ_STRING },
     { "--snaplen",              's',   RTGETOPT_REQ_UINT32 },
     { "--packet-buffered",      'U',   RTGETOPT_REQ_NOTHING },
     { "--write",                'w',   RTGETOPT_REQ_STRING },
@@ -83,7 +83,7 @@ main(int argc, char *argv[])
                 g_u64Count = Val.u64;
                 break;
 
-            case 'n':           /* --network */
+            case 'i':           /* --network */
                 if (strNetworkName.isNotEmpty())
                     return RTMsgErrorExit(RTEXITCODE_SYNTAX,
                                           "multiple --network options");
