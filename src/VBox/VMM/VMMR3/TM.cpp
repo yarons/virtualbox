1a9ec46eb461ffb938c7f00b59a201185716d8c7
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-03-10 19:24:05 +0000

    VMM/TM: doxygen fix. bugref:9943
    svn:sync-xref-src-repo-rev: r143216

--- src/VBox/VMM/VMMR3/TM.cpp
+++ src/VBox/VMM/VMMR3/TM.cpp
@@ -1,4 +1,4 @@
-/* $Id: TM.cpp 88075 2021-03-10 19:22:36Z knut.osmundsen@oracle.com $ */
+/* $Id: TM.cpp 88076 2021-03-10 19:24:05Z knut.osmundsen@oracle.com $ */
 /** @file
  * TM - Time Manager.
  */
@@ -2289,7 +2289,7 @@ static DECLCALLBACK(void) tmR3TimerCallback(PRTTIMER pTimer, void *pvUser, uint6
  *
  * @param   pVM             The cross context VM structure.
  * @param   pQueue          The queue to run.
- * @param   pNext           The head timer.  Caller already check that this is
+ * @param   pTimer          The head timer.  Caller already check that this is
  *                          not NULL.
  */
 static void tmR3TimerQueueRun(PVM pVM, PTMTIMERQUEUE pQueue, PTMTIMER pTimer)
@@ -2790,7 +2790,7 @@ VMMR3_INT_DECL(void) TMR3VirtualSyncFF(PVM pVM, PVMCPU pVCpu)
  * Service the special virtual sync timer queue.
  *
  * @param   pVM     The cross context VM structure.
- * @param   pQueue  The queue.
+ * @param   pVCpuDst    The destination VCpu.
  */
 static void tmR3TimerQueueDoVirtualSync(PVM pVM, PVMCPU pVCpuDst)
 {
