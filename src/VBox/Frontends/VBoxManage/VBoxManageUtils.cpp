7e78ba5661d6ab7073bd4e265fbefc4beb07f367
Valery Ushakov <noreply@oracle.com>
2021-03-11 13:00:16 +0000

    VBoxManage: getMaxNics() - massage a bit.  bugref:9966.
    svn:sync-xref-src-repo-rev: r143229

--- src/VBox/Frontends/VBoxManage/VBoxManageUtils.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageUtils.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageUtils.cpp 88086 2021-03-11 12:50:12Z noreply@oracle.com $ */
+/* $Id: VBoxManageUtils.cpp 88087 2021-03-11 13:00:16Z noreply@oracle.com $ */
 /** @file
  * VBoxManageUtils.h - VBoxManage utility functions.
  */
@@ -32,20 +32,20 @@ using namespace com;
 unsigned int getMaxNics(const ComPtr<IVirtualBox> &pVirtualBox,
                         const ComPtr<IMachine> &pMachine)
 {
-    ComPtr<ISystemProperties> info;
-    ChipsetType_T aChipset;
     ULONG NetworkAdapterCount = 0;
-    HRESULT rc;
-
     do {
-        CHECK_ERROR_BREAK(pVirtualBox, COMGETTER(SystemProperties)(info.asOutParam()));
-        CHECK_ERROR_BREAK(pMachine, COMGETTER(ChipsetType)(&aChipset));
-        CHECK_ERROR_BREAK(info, GetMaxNetworkAdapters(aChipset, &NetworkAdapterCount));
+        HRESULT rc;
 
-        return (unsigned int)NetworkAdapterCount;
+        ComPtr<ISystemProperties> info;
+        CHECK_ERROR_BREAK(pVirtualBox, COMGETTER(SystemProperties)(info.asOutParam()));
+
+        ChipsetType_T aChipset;
+        CHECK_ERROR_BREAK(pMachine, COMGETTER(ChipsetType)(&aChipset));
+
+        CHECK_ERROR_BREAK(info, GetMaxNetworkAdapters(aChipset, &NetworkAdapterCount));
     } while (0);
 
-    return 0;
+    return (unsigned int)NetworkAdapterCount;
 }
 
 
