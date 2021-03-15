062b0403742e330190d572d6af68c6808d3ab55b
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2021-03-15 19:08:28 +0000

    include/iprt/string.h, src/VBox/Devices/Network/slirp/slirp_config.h: Adjustments for building on Solaris 11.4.
    svn:sync-xref-src-repo-rev: r143273

--- src/VBox/Devices/Network/slirp/slirp_config.h
+++ src/VBox/Devices/Network/slirp/slirp_config.h
@@ -1,4 +1,4 @@
-/* $Id: slirp_config.h 82968 2020-02-04 10:35:17Z knut.osmundsen@oracle.com $ */
+/* $Id: slirp_config.h 88128 2021-03-15 19:08:28Z klaus.espenlaub@oracle.com $ */
 /** @file
  * NAT - compile-time configuration.
  */
@@ -210,3 +210,6 @@
 
 /* Define if you have <sys/type32.h> */
 #undef HAVE_SYS_TYPES32_H
+#ifdef RT_OS_SOLARIS
+# define HAVE_SYS_TYPES32_H
+#endif
