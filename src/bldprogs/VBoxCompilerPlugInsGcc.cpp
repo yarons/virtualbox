79aea808bbe4a788c81d75c236bf3b35e5094fe4
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2021-03-30 16:57:23 +0000

    gccplugin: gcc 7 fixes (and adjust the previously gcc 10 and later ones to 8, because 8.3.0 has them)
    svn:sync-xref-src-repo-rev: r143545

--- src/bldprogs/VBoxCompilerPlugInsGcc.cpp
+++ src/bldprogs/VBoxCompilerPlugInsGcc.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxCompilerPlugInsGcc.cpp 85502 2020-07-29 09:52:10Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxCompilerPlugInsGcc.cpp 88324 2021-03-30 16:57:23Z klaus.espenlaub@oracle.com $ */
 /** @file
  * gccplugin - GCC plugin for checking IPRT format strings.
  */
@@ -242,13 +242,17 @@ static const struct attribute_spec g_AttribSpecs[] =
         decl_required           : false,
         type_required           : true,
         function_type_required  : true,
-# if RT_GNUC_PREREQ(4, 6)
+// gcc 7.3 at least moves this field to after "handler", and with 8.3 it is back
+#if RT_GNUC_PREREQ(4, 6) && !(RT_GNUC_PREREQ(7, 0) && !RT_GNUC_PREREQ(8, 0))
         affects_type_identity   : false,
 #endif
         handler                 : AttributeHandler,
-# if RT_GNUC_PREREQ(10, 0)
+#if RT_GNUC_PREREQ(7, 0) && !RT_GNUC_PREREQ(8, 0)
+        affects_type_identity   : false,
+#endif
+#if RT_GNUC_PREREQ(8, 0)
         exclude                 : NULL,
-# endif
+#endif
     },
     {
         name                    : "iprt_format_maybe_null",
@@ -257,13 +261,16 @@ static const struct attribute_spec g_AttribSpecs[] =
         decl_required           : false,
         type_required           : true,
         function_type_required  : true,
-# if RT_GNUC_PREREQ(4, 6)
+#if RT_GNUC_PREREQ(4, 6) && !(RT_GNUC_PREREQ(7, 0) && !RT_GNUC_PREREQ(8, 0))
         affects_type_identity   : false,
 #endif
         handler                 : AttributeHandler,
-# if RT_GNUC_PREREQ(10, 0)
+#if RT_GNUC_PREREQ(7, 0) && !RT_GNUC_PREREQ(8, 0)
+        affects_type_identity   : false,
+#endif
+#if RT_GNUC_PREREQ(8, 0)
         exclude                 : NULL,
-# endif
+#endif
     }
 };
 
