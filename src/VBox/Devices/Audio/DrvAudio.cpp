dec23948bac3d7a21569ed8d6ddd374914fb732e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-04-10 23:50:03 +0000

    DrvAudio: Don't allocate more pre-buffer space than what the backend has buffer space.  It will only cause us to try write too much when pre-buffering is done.  bugref:9890
    svn:sync-xref-src-repo-rev: r143706

--- src/VBox/Devices/Audio/DrvAudio.cpp
+++ src/VBox/Devices/Audio/DrvAudio.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvAudio.cpp 88452 2021-04-10 00:19:06Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvAudio.cpp 88454 2021-04-10 23:50:03Z knut.osmundsen@oracle.com $ */
 /** @file
  * Intermediate audio driver - Connects the audio device emulation with the host backend.
  */
@@ -2837,7 +2837,9 @@ static int drvAudioStreamInitInternal(PDRVAUDIO pThis, PDRVAUDIOSTREAM pStreamEx
         if (CfgHostAcq.Backend.cFramesPreBuffering != 0)
         {
             pStreamEx->Out.cbPreBufThreshold = PDMAudioPropsFramesToBytes(&CfgHostAcq.Props, CfgHostAcq.Backend.cFramesPreBuffering);
-            pStreamEx->Out.cbPreBufAlloc = RT_ALIGN_32(pStreamEx->Out.cbPreBufThreshold + _8K, _4K);
+            pStreamEx->Out.cbPreBufAlloc = PDMAudioPropsFramesToBytes(&CfgHostAcq.Props, CfgHostAcq.Backend.cFramesBufferSize - 2);
+            pStreamEx->Out.cbPreBufAlloc = RT_MIN(RT_ALIGN_32(pStreamEx->Out.cbPreBufThreshold + _8K, _4K),
+                                                  pStreamEx->Out.cbPreBufAlloc);
             pStreamEx->Out.pbPreBuf = (uint8_t *)RTMemAllocZ(pStreamEx->Out.cbPreBufAlloc);
             AssertReturn(pStreamEx->Out.pbPreBuf, VERR_NO_MEMORY);
         }
