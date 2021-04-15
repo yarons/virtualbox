b7710fe736eb97cc274822c3a1fe7e3ce8a502e8
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-04-15 17:04:23 +0000

    FE/Qt: Passing the parent window to file selector instead of the parent widget.
    svn:sync-xref-src-repo-rev: r143841

--- src/VBox/Frontends/VirtualBox/src/widgets/UIFilePathSelector.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIFilePathSelector.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIFilePathSelector.cpp 88446 2021-04-09 18:04:37Z sergey.dubov@oracle.com $ */
+/* $Id: UIFilePathSelector.cpp 88551 2021-04-15 17:04:23Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIFilePathSelector class implementation.
  */
@@ -407,10 +407,10 @@ void UIFilePathSelector::selectPath()
     switch (m_enmMode)
     {
         case Mode_File_Open:
-            strSelPath = QIFileDialog::getOpenFileName(strInitPath, m_strFileDialogFilters, parentWidget(), m_strFileDialogTitle); break;
+            strSelPath = QIFileDialog::getOpenFileName(strInitPath, m_strFileDialogFilters, window(), m_strFileDialogTitle); break;
         case Mode_File_Save:
         {
-            strSelPath = QIFileDialog::getSaveFileName(strInitPath, m_strFileDialogFilters, parentWidget(), m_strFileDialogTitle);
+            strSelPath = QIFileDialog::getSaveFileName(strInitPath, m_strFileDialogFilters, window(), m_strFileDialogTitle);
             if (!strSelPath.isEmpty() && QFileInfo(strSelPath).suffix().isEmpty())
             {
                 if (m_strFileDialogDefaultSaveExtension.isEmpty())
@@ -421,7 +421,7 @@ void UIFilePathSelector::selectPath()
             break;
         }
         case Mode_Folder:
-            strSelPath = QIFileDialog::getExistingDirectory(strInitPath, parentWidget(), m_strFileDialogTitle); break;
+            strSelPath = QIFileDialog::getExistingDirectory(strInitPath, window(), m_strFileDialogTitle); break;
     }
 
     /* Do nothing if nothing chosen: */
