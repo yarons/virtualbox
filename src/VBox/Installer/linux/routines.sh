9746b577a4486a6153d48791e12b37b4d19653b3
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2021-04-15 14:05:33 +0000

    installer: linux: fix typo in routines.sh when detecting python installation.
    svn:sync-xref-src-repo-rev: r143826

--- src/VBox/Installer/linux/routines.sh
+++ src/VBox/Installer/linux/routines.sh
@@ -1,4 +1,4 @@
-# $Id: routines.sh 82968 2020-02-04 10:35:17Z knut.osmundsen@oracle.com $
+# $Id: routines.sh 88546 2021-04-15 14:05:33Z vadim.galitsyn@oracle.com $
 # Oracle VM VirtualBox
 # VirtualBox installer shell routines
 #
@@ -394,7 +394,7 @@ maybe_run_python_bindings_installer() {
 
     # Check for python2 only, because the generic package does not provide
     # any XPCOM bindings support for python3 since there is no standard ABI.
-    for p in python python2 python2.6 python 2.7; do
+    for p in python python2 python2.6 python2.7; do
         if [ "`$p -c 'import sys
 if sys.version_info >= (2, 6) and sys.version_info < (3, 0):
     print \"test\"' 2> /dev/null`" = "test" ]; then
