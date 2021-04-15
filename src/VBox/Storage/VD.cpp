e5b1ccd4cb250174903d3c96e4db4964f150d2a3
Alexander Eichner <alexander.eichner@oracle.com>
2021-04-15 11:56:09 +0000

    Storage/VD: Improved validation of the accessed range in VDRead/VDWrite/VDAsyncRead/VDAsyncWrite, bugref:9902
    svn:sync-xref-src-repo-rev: r143798

--- src/VBox/Storage/VD.cpp
+++ src/VBox/Storage/VD.cpp
@@ -1,4 +1,4 @@
-/* $Id: VD.cpp 87238 2021-01-13 13:42:37Z knut.osmundsen@oracle.com $ */
+/* $Id: VD.cpp 88528 2021-04-15 11:56:09Z alexander.eichner@oracle.com $ */
 /** @file
  * VD - Virtual disk container implementation.
  */
@@ -8323,6 +8323,12 @@ VBOXDDU_DECL(int) VDRead(PVDISK pDisk, uint64_t uOffset, void *pvBuf,
         AssertRC(rc2);
         fLockRead = true;
 
+        AssertMsgBreakStmt(   uOffset < pDisk->cbSize
+                           && cbRead <= pDisk->cbSize - uOffset,
+                           ("uOffset=%llu cbRead=%zu pDisk->cbSize=%llu\n",
+                            uOffset, cbRead, pDisk->cbSize),
+                           rc = VERR_INVALID_PARAMETER);
+
         PVDIMAGE pImage = pDisk->pLast;
         AssertPtrBreakStmt(pImage, rc = VERR_VD_NOT_OPENED);
 
@@ -8382,7 +8388,8 @@ VBOXDDU_DECL(int) VDWrite(PVDISK pDisk, uint64_t uOffset, const void *pvBuf,
         AssertRC(rc2);
         fLockWrite = true;
 
-        AssertMsgBreakStmt(uOffset + cbWrite <= pDisk->cbSize,
+        AssertMsgBreakStmt(   uOffset < pDisk->cbSize
+                           && cbWrite <= pDisk->cbSize - uOffset,
                            ("uOffset=%llu cbWrite=%zu pDisk->cbSize=%llu\n",
                             uOffset, cbWrite, pDisk->cbSize),
                            rc = VERR_INVALID_PARAMETER);
@@ -9737,7 +9744,8 @@ VBOXDDU_DECL(int) VDAsyncRead(PVDISK pDisk, uint64_t uOffset, size_t cbRead,
         AssertRC(rc2);
         fLockRead = true;
 
-        AssertMsgBreakStmt(uOffset + cbRead <= pDisk->cbSize,
+        AssertMsgBreakStmt(   uOffset < pDisk->cbSize
+                           && cbRead <= pDisk->cbSize - uOffset,
                            ("uOffset=%llu cbRead=%zu pDisk->cbSize=%llu\n",
                             uOffset, cbRead, pDisk->cbSize),
                            rc = VERR_INVALID_PARAMETER);
@@ -9808,7 +9816,8 @@ VBOXDDU_DECL(int) VDAsyncWrite(PVDISK pDisk, uint64_t uOffset, size_t cbWrite,
         AssertRC(rc2);
         fLockWrite = true;
 
-        AssertMsgBreakStmt(uOffset + cbWrite <= pDisk->cbSize,
+        AssertMsgBreakStmt(   uOffset < pDisk->cbSize
+                           && cbWrite <= pDisk->cbSize - uOffset,
                            ("uOffset=%llu cbWrite=%zu pDisk->cbSize=%llu\n",
                             uOffset, cbWrite, pDisk->cbSize),
                            rc = VERR_INVALID_PARAMETER);
