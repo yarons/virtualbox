fac649b8dc389afbe622bc2372f4ee11c69da9ab
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2021-04-19 15:53:34 +0000

    Intel IOMMU: bugref:9967 DevIoApic: Don't try to remap MSIs generated by the IOMMU itself (for upcoming changes).
    svn:sync-xref-src-repo-rev: r143879

--- src/VBox/Devices/PC/DevIoApic.cpp
+++ src/VBox/Devices/PC/DevIoApic.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevIoApic.cpp 86927 2020-11-20 08:47:25Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: DevIoApic.cpp 88581 2021-04-19 15:53:34Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IO APIC - Input/Output Advanced Programmable Interrupt Controller.
  */
@@ -913,25 +913,32 @@ static DECLCALLBACK(void) ioapicSendMsi(PPDMDEVINS pDevIns, PCIBDF uBusDevFn, PC
     XAPICINTR ApicIntr;
     RT_ZERO(ApicIntr);
 
-#ifdef VBOX_WITH_IOMMU_AMD
+#if defined(VBOX_WITH_IOMMU_AMD) || defined(VBOX_WITH_IOMMU_INTEL)
     /*
      * The MSI may need to be remapped (or discarded) if an IOMMU is present.
+     *
+     * If the Bus:Dev:Fn isn't valid, it is ASSUMED the device generating the
+     * MSI is the IOMMU itself and hence is not subject to remapping.
      */
-    MSIMSG MsiOut;
-    RT_ZERO(MsiOut);
-    Assert(PCIBDF_IS_VALID(uBusDevFn));
-    int rcRemap = pThisCC->pIoApicHlp->pfnIommuMsiRemap(pDevIns, uBusDevFn, pMsi, &MsiOut);
-    if (RT_SUCCESS(rcRemap))
+    if (PCIBDF_IS_VALID(uBusDevFn))
     {
-        STAM_COUNTER_INC(&pThis->StatIommuRemappedMsi);
-        ioapicGetApicIntrFromMsi(&MsiOut, &ApicIntr);
+        MSIMSG MsiOut;
+        RT_ZERO(MsiOut);
+        int rcRemap = pThisCC->pIoApicHlp->pfnIommuMsiRemap(pDevIns, uBusDevFn, pMsi, &MsiOut);
+        if (RT_SUCCESS(rcRemap))
+        {
+            STAM_COUNTER_INC(&pThis->StatIommuRemappedMsi);
+            ioapicGetApicIntrFromMsi(&MsiOut, &ApicIntr);
+        }
+        else
+        {
+            STAM_COUNTER_INC(&pThis->StatIommuDiscardedMsi);
+            Log(("IOAPIC: MSI (Addr=%#RX64 Data=%#RX32) remapping failed. rc=%Rrc", pMsi->Addr.u64, pMsi->Data.u32, rcRemap));
+            return;
+        }
     }
     else
-    {
-        STAM_COUNTER_INC(&pThis->StatIommuDiscardedMsi);
-        Log(("IOAPIC: MSI (Addr=%#RX64 Data=%#RX32) remapping failed. rc=%Rrc", pMsi->Addr.u64, pMsi->Data.u32, rcRemap));
-        return;
-    }
+        ioapicGetApicIntrFromMsi(pMsi, &ApicIntr);
 #else
     NOREF(uBusDevFn);
     ioapicGetApicIntrFromMsi(pMsi, &ApicIntr);
