56697e664e670ab705c12fbf4f78d7a514c08daf
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2021-04-21 02:56:01 +0000

    AMD IOMMU: bugref:9654 Build fix.
    svn:sync-xref-src-repo-rev: r143919

--- src/VBox/Devices/Bus/DevIommuAmd.cpp
+++ src/VBox/Devices/Bus/DevIommuAmd.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevIommuAmd.cpp 88616 2021-04-21 02:54:26Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: DevIommuAmd.cpp 88617 2021-04-21 02:56:01Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IOMMU - Input/Output Memory Management Unit - AMD implementation.
  */
@@ -772,6 +772,7 @@ static uint32_t iommuAmdGetEvtLogEntryCount(PIOMMU pThis)
 }
 
 
+#if defined(IN_RING3) || defined(LOG_ENABLED)
 /**
  * Gets the descriptive I/O permission name for a memory access.
  *
@@ -784,6 +785,7 @@ static const char *iommuAmdMemAccessGetPermName(uint8_t fPerm)
     Assert(fPerm > 0 && fPerm < RT_ELEMENTS(g_aszPerm));
     return g_aszPerm[fPerm & IOMMU_IO_PERM_MASK];
 }
+#endif
 
 
 /**
