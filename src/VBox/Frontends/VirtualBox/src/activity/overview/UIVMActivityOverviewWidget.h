bf671e6c051434c316add403990b66625836681e
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-04-26 16:48:01 +0000

    FE/Qt: bugref:8161. Clearing com data from activity overview model earlier
    svn:sync-xref-src-repo-rev: r144028

--- src/VBox/Frontends/VirtualBox/src/activity/overview/UIVMActivityOverviewWidget.h
+++ src/VBox/Frontends/VirtualBox/src/activity/overview/UIVMActivityOverviewWidget.h
@@ -1,4 +1,4 @@
-/* $Id: UIVMActivityOverviewWidget.h 88708 2021-04-26 16:25:43Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMActivityOverviewWidget.h 88711 2021-04-26 16:48:01Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMActivityOverviewWidget class declaration.
  */
@@ -86,6 +86,7 @@ private slots:
     void sltHandleTableSelectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
     void sltNotRunningVMVisibility(bool fShow);
     void sltSaveSettings();
+    void sltClearCOMData();
 
 private:
 
