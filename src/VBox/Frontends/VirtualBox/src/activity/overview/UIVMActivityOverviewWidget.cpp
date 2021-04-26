79040cb4516166e42fa22cb9af4bc8a40e7f4aaf
Sergey Dubov <sergey.dubov@oracle.com>
2021-04-26 17:28:13 +0000

    FE/Qt: bugref:8161: A bit of stuff missed in r144028.
    svn:sync-xref-src-repo-rev: r144029

--- src/VBox/Frontends/VirtualBox/src/activity/overview/UIVMActivityOverviewWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/activity/overview/UIVMActivityOverviewWidget.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVMActivityOverviewWidget.cpp 88711 2021-04-26 16:48:01Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMActivityOverviewWidget.cpp 88712 2021-04-26 17:28:13Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMActivityOverviewWidget class implementation.
  */
@@ -1034,7 +1034,13 @@ QVariant UIActivityOverviewModel::data(const QModelIndex &index, int role) const
 
 void UIActivityOverviewModel::clearData()
 {
+    /* We have a request to detach COM stuff,
+     * first of all we are removing all the items,
+     * this will detach COM wrappers implicitly: */
     m_itemList.clear();
+    /* Detaching perf. collector finally,
+     * please do not use it after all: */
+    m_performanceCollector.detach();
 }
 
 QVariant UIActivityOverviewModel::headerData(int section, Qt::Orientation orientation, int role) const
