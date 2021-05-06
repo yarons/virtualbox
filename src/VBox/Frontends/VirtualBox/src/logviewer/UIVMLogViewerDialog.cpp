a9d4dd84266551fa7bb3b1a728f30c60edd2295d
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-05-06 13:10:03 +0000

    FE/Qt: bugref:8161. repeating r144235 for log viewer dialog
    svn:sync-xref-src-repo-rev: r144236

--- src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerDialog.cpp
+++ src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerDialog.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVMLogViewerDialog.cpp 88873 2021-05-05 09:13:45Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMLogViewerDialog.cpp 88901 2021-05-06 13:10:03Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMLogViewerDialog class implementation.
  */
@@ -86,6 +86,18 @@ void UIVMLogViewerDialog::retranslateUi()
     button(ButtonType_Help)->setToolTip(UIVMLogViewerWidget::tr("Show Help (%1)").arg(button(ButtonType_Help)->shortcut().toString()));
 }
 
+void UIVMLogViewerDialog::resizeEvent(QResizeEvent *pEvent)
+{
+    QIWithRetranslateUI<QIManagerDialog>::resizeEvent(pEvent);
+    saveDialogGeometry();
+}
+
+void UIVMLogViewerDialog::moveEvent(QMoveEvent *pEvent)
+{
+    QIWithRetranslateUI<QIManagerDialog>::moveEvent(pEvent);
+    saveDialogGeometry();
+}
+
 void UIVMLogViewerDialog::configure()
 {
     /* Apply window icons: */
@@ -117,9 +129,10 @@ void UIVMLogViewerDialog::finalize()
     /* Apply language settings: */
     retranslateUi();
     manageEscapeShortCut();
+    loadDialogGeometry();
 }
 
-void UIVMLogViewerDialog::loadSettings()
+void UIVMLogViewerDialog::loadDialogGeometry()
 {
 
     const QRect availableGeo = gpDesktop->availableGeometry(this);
@@ -142,7 +155,7 @@ void UIVMLogViewerDialog::loadSettings()
     restoreGeometry(geo);
 }
 
-void UIVMLogViewerDialog::saveSettings()
+void UIVMLogViewerDialog::saveDialogGeometry()
 {
     /* Save geometry to extradata: */
     const QRect geo = currentGeometry();
