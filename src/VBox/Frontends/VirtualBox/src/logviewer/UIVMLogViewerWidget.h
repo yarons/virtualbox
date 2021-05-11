e5d8863641ce1143a3deeeb22c7153546de39bb6
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-05-11 10:20:30 +0000

    FE/Qt: bugref:9930. Adding label tabs per machine
    svn:sync-xref-src-repo-rev: r144309

--- src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerWidget.h
+++ src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerWidget.h
@@ -1,4 +1,4 @@
-/* $Id: UIVMLogViewerWidget.h 88903 2021-05-06 13:47:30Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMLogViewerWidget.h 88972 2021-05-11 10:20:30Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMLogViewerWidget class declaration.
  */
@@ -170,9 +170,6 @@ private:
         virtual void keyPressEvent(QKeyEvent *pEvent) /* override */;
     /** @} */
 
-
-    /** Returns the log-page from the tab with index @a pIndex. */
-    QPlainTextEdit* logPage(int pIndex) const;
     /** Returns the newly created log-page using @a strPage filename. */
     void createLogPage(const QString &strFileName, const QString &strMachineName,
                        const QUuid &machineId, int iLogFileId,
@@ -206,6 +203,8 @@ private:
     void setMachines(const QVector<QUuid> &machineIDs);
     /** Returns the content of the ith log file of @comMachine or possibly an empty string */
     QString readLogFile(CMachine &comMachine, int iLogFileId);
+    /** If the current tab is a label tab then switch to the next tab and return true. Returns false otherwise. */
+    bool labelTabHandler();
 
     /** Holds the widget's embedding type. */
     const EmbedTo m_enmEmbedding;
