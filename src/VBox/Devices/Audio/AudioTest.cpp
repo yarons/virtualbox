6e00338b9b081ed3190e8c00424a802fea97bbcb
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-05-17 23:19:51 +0000

    AudioTest.cpp: Added missing EOF check in AudioTestWaveFileRead. bugref:10008
    svn:sync-xref-src-repo-rev: r144466

--- src/VBox/Devices/Audio/AudioTest.cpp
+++ src/VBox/Devices/Audio/AudioTest.cpp
@@ -1,4 +1,4 @@
-/* $Id: AudioTest.cpp 89116 2021-05-17 17:00:42Z andreas.loeffler@oracle.com $ */
+/* $Id: AudioTest.cpp 89125 2021-05-17 23:19:51Z knut.osmundsen@oracle.com $ */
 /** @file
  * Audio testing routines.
  * Common code which is being used by the ValidationKit and the debug / ValdikationKit audio driver(s).
@@ -1123,13 +1123,24 @@ void AudioTestWaveFileClose(PAUDIOTESTWAVEFILE pWaveFile)
  */
 int AudioTestWaveFileRead(PAUDIOTESTWAVEFILE pWaveFile, void *pvBuf, size_t cbBuf, size_t *pcbRead)
 {
+    bool fEofAdjusted;
+    if (pWaveFile->offCur + cbBuf <= pWaveFile->cbSamples)
+        fEofAdjusted = false;
+    else if (pcbRead)
+    {
+        fEofAdjusted = true;
+        cbBuf = pWaveFile->cbSamples - pWaveFile->offCur;
+    }
+    else
+        return VERR_EOF;
+
     int rc = RTFileReadAt(pWaveFile->hFile, pWaveFile->offSamples + pWaveFile->offCur, pvBuf, cbBuf, pcbRead);
     if (RT_SUCCESS(rc))
     {
         if (pcbRead)
         {
             pWaveFile->offCur += (uint32_t)*pcbRead;
-            if (cbBuf > *pcbRead)
+            if (fEofAdjusted || cbBuf > *pcbRead)
                 rc = VINF_EOF;
             else if (!cbBuf && pWaveFile->offCur == pWaveFile->cbSamples)
                 rc = VINF_EOF;
