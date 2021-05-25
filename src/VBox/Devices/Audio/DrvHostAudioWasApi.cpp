47ae8086222c934a8497f661eeabc5640fcd7f02
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-05-25 10:23:08 +0000

    DrvHostAudioWasApi: Must destroy hEvtCachePurge if the pfnDoOnWorkerThread call fails. bugref:9890
    svn:sync-xref-src-repo-rev: r144603

--- src/VBox/Devices/Audio/DrvHostAudioWasApi.cpp
+++ src/VBox/Devices/Audio/DrvHostAudioWasApi.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostAudioWasApi.cpp 89258 2021-05-25 09:58:08Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvHostAudioWasApi.cpp 89260 2021-05-25 10:23:08Z knut.osmundsen@oracle.com $ */
 /** @file
  * Host audio driver - Windows Audio Session API.
  */
@@ -2829,7 +2829,12 @@ static DECLCALLBACK(void) drvHostAudioWasPowerOff(PPDMDRVINS pDrvIns)
         {
             rc = pThis->pIHostAudioPort->pfnDoOnWorkerThread(pThis->pIHostAudioPort, NULL/*pStream*/,
                                                              DRVHOSTAUDIOWAS_DO_PURGE_CACHE, NULL /*pvUser*/);
-            AssertRC(rc);
+            if (RT_FAILURE(rc))
+            {
+                LogFunc(("pfnDoOnWorkerThread/DRVHOSTAUDIOWAS_DO_PURGE_CACHE failed: %Rrc\n", rc));
+                RTSemEventMultiDestroy(pThis->hEvtCachePurge);
+                pThis->hEvtCachePurge = NIL_RTSEMEVENTMULTI;
+            }
         }
     }
 #endif
