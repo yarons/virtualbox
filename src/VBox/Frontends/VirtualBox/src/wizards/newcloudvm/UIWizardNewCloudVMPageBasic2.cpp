68d67b943b40d58ef79203c69f4ae0c9fc629f82
Sergey Dubov <sergey.dubov@oracle.com>
2021-07-02 10:27:25 +0000

    FE/Qt: bugref:9996: UIWizardNewCloudVM: A bit of page refactoring; Prepare-cleanup cascade fixes and coding-style.
    svn:sync-xref-src-repo-rev: r145474

--- src/VBox/Frontends/VirtualBox/src/wizards/newcloudvm/UIWizardNewCloudVMPageBasic2.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/newcloudvm/UIWizardNewCloudVMPageBasic2.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewCloudVMPageBasic2.cpp 89979 2021-06-30 13:24:20Z sergey.dubov@oracle.com $ */
+/* $Id: UIWizardNewCloudVMPageBasic2.cpp 89997 2021-07-02 10:27:25Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewCloudVMPageBasic2 class implementation.
  */
@@ -51,19 +51,16 @@ void UIWizardNewCloudVMPage2::refreshFormPropertiesTable(UIFormEditorWidgetPoint
 
 UIWizardNewCloudVMPageBasic2::UIWizardNewCloudVMPageBasic2()
 {
-    /* Create main layout: */
-    QVBoxLayout *pMainLayout = new QVBoxLayout(this);
-    if (pMainLayout)
+    /* Prepare main layout: */
+    QVBoxLayout *pLayoutMain = new QVBoxLayout(this);
+    if (pLayoutMain)
     {
-        /* Create label: */
+        /* Prepare label: */
         m_pLabel = new QIRichTextLabel(this);
         if (m_pLabel)
-        {
-            /* Add into layout: */
-            pMainLayout->addWidget(m_pLabel);
-        }
+            pLayoutMain->addWidget(m_pLabel);
 
-        /* Create form editor widget: */
+        /* Prepare form editor widget: */
         m_pFormEditor = new UIFormEditorWidget(this);
         if (m_pFormEditor)
         {
@@ -75,7 +72,7 @@ UIWizardNewCloudVMPageBasic2::UIWizardNewCloudVMPageBasic2()
                 m_pFormEditor->setMinimumHeight(8 * iDefaultSectionHeight);
 
             /* Add into layout: */
-            pMainLayout->addWidget(m_pFormEditor);
+            pLayoutMain->addWidget(m_pFormEditor);
         }
     }
 }
