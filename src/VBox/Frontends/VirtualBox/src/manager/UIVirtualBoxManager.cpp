8e6f15ea07e456efb53a52944273aeb8930f93d7
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-07-09 15:15:56 +0000

    FE/Qt: bugref:9996: Checking IUnattended for errors after setting hostname, username etc.
    svn:sync-xref-src-repo-rev: r145634

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxManager.cpp 90089 2021-07-08 09:43:15Z sergey.dubov@oracle.com $ */
+/* $Id: UIVirtualBoxManager.cpp 90132 2021-07-09 15:15:56Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVirtualBoxManager class implementation.
  */
@@ -2652,10 +2652,15 @@ void UIVirtualBoxManager::startUnattendedInstall(CUnattended &comUnattendedInsta
     comUnattendedInstaller.SetMachine(comMachine);
     checkUnattendedInstallError(comUnattendedInstaller);
     comUnattendedInstaller.SetUser(unattendedData.m_strUserName);
+    checkUnattendedInstallError(comUnattendedInstaller);
     comUnattendedInstaller.SetPassword(unattendedData.m_strPassword);
+    checkUnattendedInstallError(comUnattendedInstaller);
     comUnattendedInstaller.SetHostname(unattendedData.m_strHostname);
+    checkUnattendedInstallError(comUnattendedInstaller);
     comUnattendedInstaller.SetProductKey(unattendedData.m_strProductKey);
+    checkUnattendedInstallError(comUnattendedInstaller);
     comUnattendedInstaller.SetInstallGuestAdditions(unattendedData.m_fInstallGuestAdditions);
+    checkUnattendedInstallError(comUnattendedInstaller);
     comUnattendedInstaller.SetAdditionsIsoPath(unattendedData.m_strGuestAdditionsISOPath);
 
     comUnattendedInstaller.Prepare();
