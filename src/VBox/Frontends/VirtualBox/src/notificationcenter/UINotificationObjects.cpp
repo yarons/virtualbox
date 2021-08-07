3129d3285d74f3b23abc2bd33bbc0b64833a4b9d
Sergey Dubov <sergey.dubov@oracle.com>
2021-08-07 12:21:17 +0000

    FE/Qt: bugref:10067: Rename UIDownloaderAdditions to UIDownloaderGuestAdditions.
    svn:sync-xref-src-repo-rev: r146175

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.cpp
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINotificationObjects.cpp 90568 2021-08-07 12:10:50Z sergey.dubov@oracle.com $ */
+/* $Id: UINotificationObjects.cpp 90569 2021-08-07 12:21:17Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - Various UINotificationObjects implementations.
  */
@@ -21,8 +21,8 @@
 
 /* GUI includes: */
 #include "UICommon.h"
-#include "UIDownloaderAdditions.h"
 #include "UIDownloaderExtensionPack.h"
+#include "UIDownloaderGuestAdditions.h"
 #include "UIDownloaderUserManual.h"
 #include "UINotificationObjects.h"
 
@@ -1590,10 +1590,10 @@ QString UINotificationDownloaderGuestAdditions::details() const
 UIDownloader *UINotificationDownloaderGuestAdditions::createDownloader()
 {
     /* Create and configure the User Manual downloader: */
-    UIDownloaderAdditions *pDownloader = new UIDownloaderAdditions;
+    UIDownloaderGuestAdditions *pDownloader = new UIDownloaderGuestAdditions;
     if (pDownloader)
     {
-        connect(pDownloader, &UIDownloaderAdditions::sigDownloadFinished,
+        connect(pDownloader, &UIDownloaderGuestAdditions::sigDownloadFinished,
                 this, &UINotificationDownloaderGuestAdditions::sigGuestAdditionsDownloaded);
         return pDownloader;
     }
