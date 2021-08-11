1cc2cf152675d3037729f34eda933ecd23c387ce
Sergey Dubov <sergey.dubov@oracle.com>
2021-08-11 12:40:10 +0000

    FE/Qt: bugref:10067: Restore status for UINotificationNewVersionCheckerItem on recreation.
    svn:sync-xref-src-repo-rev: r146236

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjectItem.cpp
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjectItem.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINotificationObjectItem.cpp 90606 2021-08-10 16:07:13Z sergey.dubov@oracle.com $ */
+/* $Id: UINotificationObjectItem.cpp 90625 2021-08-11 12:40:10Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UINotificationObjectItem class implementation.
  */
@@ -400,7 +400,13 @@ UINotificationNewVersionCheckerItem::UINotificationNewVersionCheckerItem(QWidget
         m_pProgressBar = new QProgressBar(this);
         if (m_pProgressBar)
         {
-            m_pProgressBar->setMaximum(0);
+            if (!checker()->isDone())
+                m_pProgressBar->setMaximum(0);
+            else
+            {
+                m_pProgressBar->setMaximum(1);
+                m_pProgressBar->setValue(1);
+            }
             m_pLayoutMain->addWidget(m_pProgressBar);
         }
     }
