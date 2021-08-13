b76088f01f1ea0c8b916719bdb0b7cca471cd5af
Sergey Dubov <sergey.dubov@oracle.com>
2021-08-13 16:32:18 +0000

    FE/Qt: bugref:8161: Runtime UI / Normal mode: A bit of paranoia and sanity checks as well as proper cleanup stuff for status-bar indicator-pool; This is necessary now since COM can be deinitialized early.
    svn:sync-xref-src-repo-rev: r146297

--- src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.h
+++ src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.h
@@ -1,4 +1,4 @@
-/* $Id: UIMachineWindowNormal.h 90567 2021-08-07 11:50:28Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineWindowNormal.h 90685 2021-08-13 16:32:18Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineWindowNormal class declaration.
  */
@@ -108,6 +108,8 @@ private:
     void cleanupVisualState();
     /** Cleanup notification-center routine. */
     void cleanupNotificationCenter();
+    /** Cleanup status-bar routine. */
+    void cleanupStatusBar();
     /** Cleanup session connections routine. */
     void cleanupSessionConnections();
 
