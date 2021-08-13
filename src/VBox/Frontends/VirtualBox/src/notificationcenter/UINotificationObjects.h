14ce8edabdd5361791ab7a899ed533c51bc2c589
Sergey Dubov <sergey.dubov@oracle.com>
2021-08-13 10:45:43 +0000

    FE/Qt: bugref:10067: Interfaces for simple message notification.
    svn:sync-xref-src-repo-rev: r146291

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.h
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.h
@@ -1,4 +1,4 @@
-/* $Id: UINotificationObjects.h 90663 2021-08-12 13:41:42Z sergey.dubov@oracle.com $ */
+/* $Id: UINotificationObjects.h 90679 2021-08-13 10:45:43Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - Various UINotificationObjects declarations.
  */
@@ -42,6 +42,45 @@
 #include "CSession.h"
 #include "CSnapshot.h"
 #include "CVirtualSystemDescription.h"
+
+/** UINotificationObject extension for message functionality. */
+class SHARED_LIBRARY_STUFF UINotificationMessage : public UINotificationSimple
+{
+    Q_OBJECT;
+
+public:
+
+protected:
+
+    /** Constructs message notification-object.
+      * @param  strName          Brings the message name.
+      * @param  strDetails       Brings the message details.
+      * @param  strInternalName  Brings the message internal name. */
+    UINotificationMessage(const QString &strName,
+                                const QString &strDetails,
+                                const QString &strInternalName)
+        : UINotificationSimple(strName,
+                               strDetails,
+                               strInternalName)
+    {}
+
+private:
+
+    /** Creates message.
+      * @param  strName          Brings the message name.
+      * @param  strDetails       Brings the message details.
+      * @param  strInternalName  Brings the message internal name. */
+    static void createMessage(const QString &strName,
+                              const QString &strDetails,
+                              const QString &strInternalName);
+
+    /** Holds the message name. */
+    QString  m_strName;
+    /** Holds the message details. */
+    QString  m_strDetails;
+    /** Holds the message internal name. */
+    QString  m_strInternalName;
+};
 
 /** UINotificationProgress extension for medium create functionality. */
 class SHARED_LIBRARY_STUFF UINotificationProgressMediumCreate : public UINotificationProgress
