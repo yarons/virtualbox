59e79fe1b10472d5fead0831ace763b79e1a5af3
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-08-17 20:05:43 +0000

    VBoxTray: More of the same, from the DnD merge r91269, only use VBOXTRAY_RELEASE_LOG for controlling the release log.
    svn:sync-xref-src-repo-rev: r146329

--- src/VBox/Additions/WINNT/VBoxTray/VBoxTray.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxTray.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxTray.cpp 85371 2020-07-17 10:02:58Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxTray.cpp 90709 2021-08-17 20:05:43Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxTray - Guest Additions Tray Application
  */
@@ -579,11 +579,10 @@ static int vboxTrayLogCreate(void)
     int rc = RTLogCreateEx(&g_pLoggerRelease, fFlags,
 #ifdef DEBUG
                            "all.e.l.f",
-                           "VBOXTRAY_LOG",
 #else
                            "all",
-                           "VBOXTRAY_RELEASE_LOG",
 #endif
+                           "VBOXTRAY_RELEASE_LOG",
                            RT_ELEMENTS(s_apszGroups), s_apszGroups, UINT32_MAX, RTLOGDEST_STDOUT,
                            vboxTrayLogHeaderFooter, g_cHistory, g_uHistoryFileSize, g_uHistoryFileTime,
                            RTErrInfoInitStatic(&ErrInfo), NULL /*pszFilenameFmt*/);
