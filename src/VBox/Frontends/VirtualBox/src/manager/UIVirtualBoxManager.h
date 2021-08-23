5f58a0e5056ec67386c94430c363fd39c89b42e5
Sergey Dubov <sergey.dubov@oracle.com>
2021-08-23 14:51:16 +0000

    FE/Qt: bugref:10067: Notification signature for VM power-up progress which should now go to center instead of modal dialogs.
    svn:sync-xref-src-repo-rev: r146430

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.h
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.h
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxManager.h 90760 2021-08-20 15:06:17Z sergey.dubov@oracle.com $ */
+/* $Id: UIVirtualBoxManager.h 90795 2021-08-23 14:51:16Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVirtualBoxManager class declaration.
  */
@@ -362,8 +362,10 @@ private:
         /** Opens add machine dialog specifying initial name with @a strFileName. */
         void openAddMachineDialog(const QString &strFileName = QString());
 
+        /** Launches certain @a comMachine in specified @a enmLaunchMode. */
+        static void launchMachine(CMachine &comMachine, UICommon::LaunchMode enmLaunchMode = UICommon::LaunchMode_Default);
         /** Launches certain @a comMachine. */
-        void launchMachine(CCloudMachine &comMachine);
+        static void launchMachine(CCloudMachine &comMachine);
 
         /** Creates an unattended installer and uses it to install guest os to newly created vm. */
         void startUnattendedInstall(CUnattended &comUnattendedInstaller, const UIUnattendedInstallData &unattendedData);
