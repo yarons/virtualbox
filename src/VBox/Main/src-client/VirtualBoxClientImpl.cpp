e530ae3207eb148a9945d7641ac9011971a61d54
Alexey Diachenko <noreply@oracle.com>
2021-08-24 14:08:04 +0000

    Main: bugref:1909: Doxygen issues
    svn:sync-xref-src-repo-rev: r146484

--- src/VBox/Main/src-client/VirtualBoxClientImpl.cpp
+++ src/VBox/Main/src-client/VirtualBoxClientImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxClientImpl.cpp 90848 2021-08-24 13:54:26Z noreply@oracle.com $ */
+/* $Id: VirtualBoxClientImpl.cpp 90849 2021-08-24 14:08:04Z noreply@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -101,6 +101,8 @@ VBOX_LISTENER_DECLARE(VBoxEventListenerImpl)
 
 // constructor / destructor
 /////////////////////////////////////////////////////////////////////////////
+
+/** @relates VirtualBoxClient::FinalConstruct() */
 HRESULT VirtualBoxClient::FinalConstruct()
 {
     HRESULT rc = init();
