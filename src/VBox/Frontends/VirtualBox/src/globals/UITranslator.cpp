a1dc10e60665b12c6d69172f6f02abe390e2103f
Sergey Dubov <sergey.dubov@oracle.com>
2021-08-27 20:44:46 +0000

    FE/Qt: bugref:10067: Build fix for r146606.
    svn:sync-xref-src-repo-rev: r146607

--- src/VBox/Frontends/VirtualBox/src/globals/UITranslator.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UITranslator.cpp
@@ -1,4 +1,4 @@
-/* $Id: UITranslator.cpp 90967 2021-08-27 20:37:40Z sergey.dubov@oracle.com $ */
+/* $Id: UITranslator.cpp 90968 2021-08-27 20:44:46Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UITranslator class implementation.
  */
@@ -37,6 +37,9 @@
 #ifdef Q_OS_UNIX
 # include <iprt/env.h>
 #endif
+
+/* External includes: */
+#include <math.h>
 
 
 /** Port config cache. */
