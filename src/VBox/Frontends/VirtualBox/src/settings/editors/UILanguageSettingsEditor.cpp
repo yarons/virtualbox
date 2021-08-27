bcd90959efd07ccbdba88fe920c5dd8f68f957d8
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-08-27 11:48:23 +0000

    FE/Qt: bugref:10067: scm fix
    svn:sync-xref-src-repo-rev: r146587

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UILanguageSettingsEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UILanguageSettingsEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UILanguageSettingsEditor.cpp 90941 2021-08-27 10:03:18Z sergey.dubov@oracle.com $ */
+/* $Id: UILanguageSettingsEditor.cpp 90949 2021-08-27 11:48:23Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Qt GUI - UILanguageSettingsEditor class implementation.
  */
@@ -31,7 +31,7 @@
 
 /* Other VBox includes: */
 #include <iprt/assert.h>
-#include <iprt/err.h>
+#include <iprt/errcore.h>
 #include <iprt/path.h>
 
 
