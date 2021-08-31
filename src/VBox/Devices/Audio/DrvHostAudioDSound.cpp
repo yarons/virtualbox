12cb72df84a27ba3ef7eb8dc0488d25fe6d9f7c4
Andreas Loeffler <andreas.loeffler@oracle.com>
2021-08-31 08:01:09 +0000

    Audio: Toned down verbose release logging of the DSound backend a bit. ​bugref:10008
    svn:sync-xref-src-repo-rev: r146661

--- src/VBox/Devices/Audio/DrvHostAudioDSound.cpp
+++ src/VBox/Devices/Audio/DrvHostAudioDSound.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostAudioDSound.cpp 89579 2021-06-09 12:12:05Z knut.osmundsen@oracle.com $ */
+/* $Id: DrvHostAudioDSound.cpp 91020 2021-08-31 08:01:09Z andreas.loeffler@oracle.com $ */
 /** @file
  * Host audio driver - DirectSound (Windows).
  */
@@ -2248,7 +2248,7 @@ static int dsoundGetFreeOut(PDRVHOSTDSOUND pThis, PDSOUNDSTREAM pStreamDS, DWORD
             if (pStreamDS->Out.cbWritten == 0)
                 cbFree = pStreamDS->cbBufSize;
 
-            LogRel3(("DSound: offPlayCursor=%RU32, offWriteCursor=%RU32, offWritePos=%RU32 -> cbFree=%RI32\n",
+            LogRel4(("DSound: offPlayCursor=%RU32, offWriteCursor=%RU32, offWritePos=%RU32 -> cbFree=%RI32\n",
                      offPlayCursor, offWriteCursor, pStreamDS->Out.offWritePos, cbFree));
 
             *pdwFree = cbFree;
