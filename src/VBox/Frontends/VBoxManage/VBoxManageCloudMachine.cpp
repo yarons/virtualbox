79c6ebac323259b93734a72de8ab28b8e0918152
Valery Ushakov <noreply@oracle.com>
2021-09-08 13:59:58 +0000

    VBoxManageCloudMachine: add setCurrentSubcommand() placeholders.  bugref:10065.
    svn:sync-xref-src-repo-rev: r146810

--- src/VBox/Frontends/VBoxManage/VBoxManageCloudMachine.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageCloudMachine.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageCloudMachine.cpp 90349 2021-07-26 23:09:33Z noreply@oracle.com $ */
+/* $Id: VBoxManageCloudMachine.cpp 91156 2021-09-08 13:59:58Z noreply@oracle.com $ */
 /** @file
  * VBoxManageCloudMachine - The cloud machine related commands.
  */
@@ -291,7 +291,7 @@ handleCloudMachineImpl(HandlerArg *a, int iFirst,
 
     int rc;
 
-    // setCurrentSubcommand(...);
+    // setCurrentSubcommand(HELP_SCOPE_CLOUD_MACHINE);
 
     RTGETOPTSTATE OptState;
     rc = RTGetOptInit(&OptState, a->argc, a->argv,
@@ -392,6 +392,7 @@ listCloudMachinesImpl(HandlerArg *a, int iFirst,
     HRESULT hrc;
     int rc;
 
+    // setCurrentSubcommand(HELP_SCOPE_CLOUD_MACHINE_LIST);
 
     RTGETOPTSTATE OptState;
     rc = RTGetOptInit(&OptState, a->argc, a->argv,
@@ -533,6 +534,8 @@ handleCloudMachineInfo(HandlerArg *a, int iFirst,
                        const ComPtr<ICloudProfile> &pProfile)
 {
     HRESULT hrc;
+
+    // setCurrentSubcommand(HELP_SCOPE_CLOUD_MACHINE_INFO);
 
     if (iFirst == a->argc)
     {
@@ -794,6 +797,8 @@ handleCloudMachineConsoleHistory(HandlerArg *a, int iFirst,
                                  const ComPtr<ICloudProfile> &pProfile)
 {
     HRESULT hrc;
+
+    // setCurrentSubcommand(HELP_SCOPE_CLOUD_MACHINE_CONSOLEHISTORY);
 
     if (iFirst == a->argc)
     {
