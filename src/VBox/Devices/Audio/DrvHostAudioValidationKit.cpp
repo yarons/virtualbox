5f1f2fc8a4298f8055d1dd75af51bbab58acc7f0
Andreas Loeffler <andreas.loeffler@oracle.com>
2021-09-10 07:45:44 +0000

    Audio/Validation Kit: Added callbacks to the Validation Kit audio driver to know when a client [dis]connected. ​bugref:10008
    svn:sync-xref-src-repo-rev: r146846

--- src/VBox/Devices/Audio/DrvHostAudioValidationKit.cpp
+++ src/VBox/Devices/Audio/DrvHostAudioValidationKit.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostAudioValidationKit.cpp 91181 2021-09-09 15:36:41Z andreas.loeffler@oracle.com $ */
+/* $Id: DrvHostAudioValidationKit.cpp 91191 2021-09-10 07:45:44Z andreas.loeffler@oracle.com $ */
 /** @file
  * Host audio driver - ValidationKit - For dumping and injecting audio data from/to the device emulation.
  */
@@ -293,6 +293,28 @@ static void drvHostValKitCleanup(PDRVHOSTVALKITAUDIO pThis)
 /*********************************************************************************************************************************
 *   ATS callback implementations                                                                                                 *
 *********************************************************************************************************************************/
+
+/** @copydoc ATSCALLBACKS::pfnHowdy */
+static DECLCALLBACK(int) drvHostValKitHowdy(void const *pvUser)
+{
+    PDRVHOSTVALKITAUDIO pThis = (PDRVHOSTVALKITAUDIO)pvUser;
+    RT_NOREF(pThis);
+
+    LogRel(("ValKit: Client connected\n"));
+
+    return VINF_SUCCESS;
+}
+
+/** @copydoc ATSCALLBACKS::pfnBye */
+static DECLCALLBACK(int) drvHostValKitBye(void const *pvUser)
+{
+    PDRVHOSTVALKITAUDIO pThis = (PDRVHOSTVALKITAUDIO)pvUser;
+    RT_NOREF(pThis);
+
+    LogRel(("ValKit: Client disconnected\n"));
+
+    return VINF_SUCCESS;
+}
 
 /** @copydoc ATSCALLBACKS::pfnTestSetBegin */
 static DECLCALLBACK(int) drvHostValKitTestSetBegin(void const *pvUser, const char *pszTag)
@@ -1155,6 +1177,8 @@ static DECLCALLBACK(int) drvHostValKitAudioConstruct(PPDMDRVINS pDrvIns, PCFGMNO
 
     ATSCALLBACKS Callbacks;
     RT_ZERO(Callbacks);
+    Callbacks.pfnHowdy            = drvHostValKitHowdy;
+    Callbacks.pfnBye              = drvHostValKitBye;
     Callbacks.pfnTestSetBegin     = drvHostValKitTestSetBegin;
     Callbacks.pfnTestSetEnd       = drvHostValKitTestSetEnd;
     Callbacks.pfnTonePlay         = drvHostValKitRegisterGuestRecTest;
