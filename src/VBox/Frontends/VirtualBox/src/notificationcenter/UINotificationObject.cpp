f9a4c98e7476cdd3fb2faf3ee8e090111b9770ef
Sergey Dubov <sergey.dubov@oracle.com>
2021-09-14 17:08:48 +0000

    FE/Qt: bugref:10067: UINotificationCenter: Do not auto-close failed progresses.
    svn:sync-xref-src-repo-rev: r146895

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObject.cpp
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObject.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINotificationObject.cpp 91227 2021-09-14 10:49:11Z sergey.dubov@oracle.com $ */
+/* $Id: UINotificationObject.cpp 91237 2021-09-14 17:08:48Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UINotificationObject class implementation.
  */
@@ -187,7 +187,10 @@ void UINotificationProgress::sltHandleProgressFinished()
 {
     m_uPercent = 100;
     emit sigProgressFinished();
-    if (!gEDataManager->keepSuccessfullNotificationProgresses())
+
+    /* If there was no error and no reason to keep progress alive, - finish him! */
+    if (   !error().isEmpty()
+        && !gEDataManager->keepSuccessfullNotificationProgresses())
         close();
 }
 
