d2b64ee1b31945f6f5016346859ebc80cece8909
Alexander Eichner <alexander.eichner@oracle.com>
2021-09-22 15:24:30 +0000

    Main/NvramStore,FE/VBoxManage: Allow multiple NVRAM states (UEFI,TPM,etc.) to exist for a VM and and manage them in a central place. This allows to collect them in a single tar archive and provide a single interface to get access to the individual states (work in progress), bugref:10098 [Hopefully darwin build fix in an unrelated part because PVM clashes now...]
    svn:sync-xref-src-repo-rev: r146998

--- src/VBox/Main/src-server/HostImpl.cpp
+++ src/VBox/Main/src-server/HostImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: HostImpl.cpp 90828 2021-08-24 09:44:46Z noreply@oracle.com $ */
+/* $Id: HostImpl.cpp 91332 2021-09-22 15:24:30Z alexander.eichner@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation: Host
  */
@@ -63,6 +63,7 @@
 #if defined(RT_OS_DARWIN) && ARCH_BITS == 32
 # include <sys/types.h>
 # include <sys/sysctl.h>
+# undef PVM
 #endif
 
 #ifdef RT_OS_LINUX
