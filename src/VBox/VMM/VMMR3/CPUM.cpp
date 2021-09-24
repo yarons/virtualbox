a641712319ada90a710bed64fc22cf67fb01ceae
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2021-09-24 07:42:22 +0000

    VMM: Nested VMX: bugref:10092 Disable VPID for now, also correct confusing comment (bit 37 refers to bit 5 of the upper dword).
    svn:sync-xref-src-repo-rev: r147025

--- src/VBox/VMM/VMMR3/CPUM.cpp
+++ src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 91354 2021-09-24 06:43:55Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: CPUM.cpp 91355 2021-09-24 07:42:22Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -1451,7 +1451,7 @@ static void cpumR3InitVmxGuestMsrs(PVM pVM, PCVMXMSRS pHostVmxMsrs, PCCPUMFEATUR
          * and emulate any INVVPID flush type. However, it only makes sense to expose the types
          * when INVVPID instruction is supported just to be more compatible with guest
          * hypervisors that may make assumptions by only looking at this MSR even though they
-         * are technically supposed to refer to bit 37 of MSR_IA32_VMX_PROC_CTLS2 first.
+         * are technically supposed to refer to VMX_PROC_CTLS2_VPID first.
          *
          * See Intel spec. 25.1.2 "Instructions That Cause VM Exits Unconditionally".
          * See Intel spec. 30.3 "VMX Instructions".
@@ -1676,7 +1676,7 @@ void cpumR3InitVmxGuestFeaturesAndMsrs(PVM pVM, PCVMXMSRS pHostVmxMsrs, PVMXMSRS
     EmuFeat.fVmxDescTableExit         = 1;
     EmuFeat.fVmxRdtscp                = 1;
     EmuFeat.fVmxVirtX2ApicMode        = 0;
-    EmuFeat.fVmxVpid                  = EmuFeat.fVmxEpt;
+    EmuFeat.fVmxVpid                  = 0;
     EmuFeat.fVmxWbinvdExit            = 1;
     EmuFeat.fVmxUnrestrictedGuest     = pVM->cpum.s.fNestedVmxUnrestrictedGuest;
     EmuFeat.fVmxApicRegVirt           = 0;
