4e7e41b66896778e3304e8eb13a0390fc2dc5279
Andreas Loeffler <andreas.loeffler@oracle.com>
2021-09-30 08:37:40 +0000

    Audio/Validation Kit: Some more assertions for audioTestToneFileFind(). ​bugref:10008
    svn:sync-xref-src-repo-rev: r147178

--- src/VBox/Devices/Audio/AudioTest.cpp
+++ src/VBox/Devices/Audio/AudioTest.cpp
@@ -1,4 +1,4 @@
-/* $Id: AudioTest.cpp 91492 2021-09-30 08:36:00Z andreas.loeffler@oracle.com $ */
+/* $Id: AudioTest.cpp 91494 2021-09-30 08:37:40Z andreas.loeffler@oracle.com $ */
 /** @file
  * Audio testing routines.
  *
@@ -404,9 +404,11 @@ static uint64_t audioTestToneFileFind(RTFILE hFile, bool fFindSilence, uint64_t 
             || !cbRead)
             break;
 
-        Assert(PDMAudioPropsIsSizeAligned(&pToneParms->Props, (uint32_t)cbRead));
+        AssertReturn(PDMAudioPropsIsSizeAligned(&pToneParms->Props, (uint32_t)cbRead), 0);
 
         size_t const cbFrame = PDMAudioPropsFrameSize(&pToneParms->Props);
+        AssertReturn(cbFrame, 0);
+        AssertReturn(cbRead % cbFrame == 0, 0);
 
         for (size_t i = 0; i < cbRead / cbFrame; i += cbFrame) /** @todo Slow as heck, but works for now. */
         {
@@ -420,6 +422,7 @@ static uint64_t audioTestToneFileFind(RTFILE hFile, bool fFindSilence, uint64_t 
         }
     }
 
+    AssertReturn(PDMAudioPropsIsSizeAligned(&pToneParms->Props, offFound), 0);
     return offFound;
 }
 
