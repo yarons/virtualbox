796c83d1c383ed24941f3d0d241627adfacee547
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-10-07 21:28:07 +0000

    SharedClipboard/common: ShClEventSignal must clear pEvent->pPayload if RTSemEventMultiSignal fails, otherwise it the payload could be freed twice. Don't leak semaphores in ShClEventIdGenerateAndRegister failure path.  Three longish r=bird comments on the insanity of the event interface.  [scm fix] bugref:9437
    svn:sync-xref-src-repo-rev: r147354

--- src/VBox/GuestHost/SharedClipboard/clipboard-common.cpp
+++ src/VBox/GuestHost/SharedClipboard/clipboard-common.cpp
@@ -1,4 +1,4 @@
-/* $Id: clipboard-common.cpp 91628 2021-10-07 21:27:34Z knut.osmundsen@oracle.com $ */
+/* $Id: clipboard-common.cpp 91629 2021-10-07 21:28:07Z knut.osmundsen@oracle.com $ */
 /** @file
  * Shared Clipboard: Some helper function for converting between the various eol.
  */
@@ -368,7 +368,7 @@ int ShClEventUnregister(PSHCLEVENTSOURCE pSource, SHCLEVENTID uID)
  * @param   uTimeoutMs          Timeout (in ms) to wait.
  * @param   ppPayload           Where to store the (allocated) event payload on success. Needs to be free'd with
  *                              SharedClipboardPayloadFree(). Optional.
- * 
+ *
  * @todo    r=bird: Locking protocol is totally buggered here, or at least not
  *          explained in any way whatsoever.  We cannot really do shclEventGet
  *          and shclEventPayloadDetachInternal w/o holding the critical section
