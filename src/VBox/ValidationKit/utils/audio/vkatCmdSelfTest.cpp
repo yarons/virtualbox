cdcab9855d0f51a643e05841af92061b496d53a9
Andreas Loeffler <andreas.loeffler@oracle.com>
2021-10-21 13:33:57 +0000

    Audio/Validation Kit: Perform a quick audio driver stack self-test on start of the actual testing (guest + host), plus in self-test mode [SCM fix].​ bugref:10008
    svn:sync-xref-src-repo-rev: r147753

--- src/VBox/ValidationKit/utils/audio/vkatCmdSelfTest.cpp
+++ src/VBox/ValidationKit/utils/audio/vkatCmdSelfTest.cpp
@@ -1,4 +1,4 @@
-/* $Id: vkatCmdSelfTest.cpp 91946 2021-10-21 13:30:54Z andreas.loeffler@oracle.com $ */
+/* $Id: vkatCmdSelfTest.cpp 91947 2021-10-21 13:33:57Z andreas.loeffler@oracle.com $ */
 /** @file
  * Validation Kit Audio Test (VKAT) - Self test.
  *
@@ -114,7 +114,7 @@ static SELFTESTCTX g_Ctx;
 
 
 /*********************************************************************************************************************************
-*   Driver stack self-test implementation                                                                                                     *
+*   Driver stack self-test implementation                                                                                        *
 *********************************************************************************************************************************/
 
 /**
