be4ada0ef18bbd53ea0230b619d58d38eb40dd1b
Valery Ushakov <noreply@oracle.com>
2021-10-21 01:09:34 +0000

    IPRT: Make RTNetStrToIPv4Cidr() and RTNetStrToIPv4Cidr() accept zero prefix, mostly for the benefit of 0.0.0.0/0 and ::/0 respectively.
    While here fix RTNetStrToIPv6Cidr to do stricter checks on the prefix.
    When the number is too big RTStrToUInt8Ex returns a warning, not an
    error.  bugref:9330.
    
    
    svn:sync-xref-src-repo-rev: r147723

--- src/VBox/Runtime/common/net/netaddrstr2.cpp
+++ src/VBox/Runtime/common/net/netaddrstr2.cpp
@@ -1,4 +1,4 @@
-/* $Id: netaddrstr2.cpp 87524 2021-02-02 02:17:11Z noreply@oracle.com $ */
+/* $Id: netaddrstr2.cpp 91918 2021-10-21 01:09:34Z noreply@oracle.com $ */
 /** @file
  * IPRT - Network Address String Handling.
  */
@@ -290,7 +290,7 @@ RTDECL(int) RTNetStrToIPv4Cidr(const char *pcszAddr, PRTNETADDRIPV4 pAddr, int *
             return VERR_INVALID_PARAMETER;
     }
 
-    if (u8Prefix == 0 || u8Prefix > 32)
+    if (u8Prefix > 32)
         return VERR_INVALID_PARAMETER;
 
     *pAddr = Addr;
@@ -714,10 +714,10 @@ RTDECL(int) RTNetStrToIPv6Cidr(const char *pcszAddr, PRTNETADDRIPV6 pAddr, int *
 
     ++pszNext;
     rc = RTStrToUInt8Ex(pszNext, &pszNext, 10, &u8Prefix);
-    if (RT_FAILURE(rc) || rc == VWRN_TRAILING_CHARS)
+    if (rc != VINF_SUCCESS && rc != VWRN_TRAILING_SPACES)
         return VERR_INVALID_PARAMETER;
 
-    if (u8Prefix == 0 || u8Prefix > 128)
+    if (u8Prefix > 128)
         return VERR_INVALID_PARAMETER;
 
     *pAddr = Addr;
