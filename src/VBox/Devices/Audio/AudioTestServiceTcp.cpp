78efd99c79edfc76a17a1261bfdbcc9536f99216
Andreas Loeffler <andreas.loeffler@oracle.com>
2021-10-26 09:43:19 +0000

    Audio/Validation Kit: Logging tweaks. ​bugref:10008
    svn:sync-xref-src-repo-rev: r147886

--- src/VBox/Devices/Audio/AudioTestServiceTcp.cpp
+++ src/VBox/Devices/Audio/AudioTestServiceTcp.cpp
@@ -1,4 +1,4 @@
-/* $Id: AudioTestServiceTcp.cpp 92061 2021-10-26 07:33:26Z andreas.loeffler@oracle.com $ */
+/* $Id: AudioTestServiceTcp.cpp 92073 2021-10-26 09:43:19Z andreas.loeffler@oracle.com $ */
 /** @file
  * AudioTestServiceTcp - Audio test execution server, TCP/IP Transport Layer.
  */
@@ -556,7 +556,7 @@ static DECLCALLBACK(int) atsTcpSendPkt(PATSTRANSPORTINST pThis, PATSTRANSPORTCLI
         atsTcpDisconnectClient(pThis, pClient);
     }
 
-    LogRel3(("pClient=%RTsock, achOpcode=%.8s, cbSent=%zu -> %Rrc\n", pClient->hTcpClient, (const char *)pPktHdr->achOpcode, cbToSend, rc));
+    LogRel3(("atsTcpSendPkt: pClient=%RTsock, achOpcode=%.8s, cbSent=%zu -> %Rrc\n", pClient->hTcpClient, (const char *)pPktHdr->achOpcode, cbToSend, rc));
     return rc;
 }
 
@@ -698,8 +698,8 @@ static DECLCALLBACK(int) atsTcpRecvPkt(PATSTRANSPORTINST pThis, PATSTRANSPORTCLI
     }
 
     PATSPKTHDR pPktHdr = (PATSPKTHDR)pbData;
-    LogRel3(("pClient=%RTsock, achOpcode=%.8s, cbRead=%zu -> %Rrc\n",
-             pClient->hTcpClient, pPktHdr ? (const char *)pPktHdr->achOpcode : "<None>", cbData, rc));
+    LogRel3(("atsTcpRecvPkt: pClient=%RTsock, achOpcode=%.8s, cbRead=%zu -> %Rrc\n",
+             pClient->hTcpClient, pPktHdr ? (const char *)pPktHdr->achOpcode : "NONE    ", cbData, rc));
     return rc;
 }
 
