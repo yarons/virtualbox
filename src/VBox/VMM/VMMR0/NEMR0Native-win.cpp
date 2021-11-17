f4508707a2f4d8b3fd937922e91c2dda3d7d9a6b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-11-17 03:01:09 +0000

    VMM/NEM-linux: Some more code. Got far enough to log the BIOS version to the release log. bugref:9044
    svn:sync-xref-src-repo-rev: r148324

--- src/VBox/VMM/VMMR0/NEMR0Native-win.cpp
+++ src/VBox/VMM/VMMR0/NEMR0Native-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: NEMR0Native-win.cpp 92194 2021-11-03 15:06:02Z knut.osmundsen@oracle.com $ */
+/* $Id: NEMR0Native-win.cpp 92465 2021-11-17 03:01:09Z knut.osmundsen@oracle.com $ */
 /** @file
  * NEM - Native execution manager, native ring-0 Windows backend.
  */
@@ -1701,6 +1701,13 @@ NEM_TMPL_STATIC int nemR0WinExportState(PGVM pGVM, PGVMCPU pGVCpu, PCPUMCTX pCtx
         pInput->Elements[iReg].Value.Reg64          = pCtx->msrSFMASK;
         iReg++;
     }
+    if (fWhat & CPUMCTX_EXTRN_TSC_AUX)
+    {
+        HV_REGISTER_ASSOC_ZERO_PADDING_AND_HI64(&pInput->Elements[iReg]);
+        pInput->Elements[iReg].Name                 = HvX64RegisterTscAux;
+        pInput->Elements[iReg].Value.Reg64          = pCtxMsrs->msr.TscAux;
+        iReg++;
+    }
     if (fWhat & CPUMCTX_EXTRN_OTHER_MSRS)
     {
         HV_REGISTER_ASSOC_ZERO_PADDING_AND_HI64(&pInput->Elements[iReg]);
@@ -1770,10 +1777,6 @@ NEM_TMPL_STATIC int nemR0WinExportState(PGVM pGVM, PGVMCPU pGVCpu, PCPUMCTX pCtx
         HV_REGISTER_ASSOC_ZERO_PADDING_AND_HI64(&pInput->Elements[iReg]);
         pInput->Elements[iReg].Name                 = HvX64RegisterMtrrFix4kF8000;
         pInput->Elements[iReg].Value.Reg64          = pCtxMsrs->msr.MtrrFix4K_F8000;
-        iReg++;
-        HV_REGISTER_ASSOC_ZERO_PADDING_AND_HI64(&pInput->Elements[iReg]);
-        pInput->Elements[iReg].Name                 = HvX64RegisterTscAux;
-        pInput->Elements[iReg].Value.Reg64          = pCtxMsrs->msr.TscAux;
         iReg++;
 
 # if 0 /** @todo Why can't we write these on Intel systems? Not that we really care... */
