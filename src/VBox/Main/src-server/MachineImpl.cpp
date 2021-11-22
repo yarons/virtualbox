1da286bc08f9cacc2e6a875d09397255de8dfebc
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2021-11-22 14:22:14 +0000

    Main/Machine: Fix paravirt provider default for Windows 11
    svn:sync-xref-src-repo-rev: r148429

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 92539 2021-11-22 02:09:47Z knut.osmundsen@oracle.com $ */
+/* $Id: MachineImpl.cpp 92551 2021-11-22 14:22:14Z klaus.espenlaub@oracle.com $ */
 /** @file
  * Implementation of IMachine in VBoxSVC.
  */
@@ -1370,7 +1370,8 @@ HRESULT Machine::getEffectiveParavirtProvider(ParavirtProvider_T *aParavirtProvi
                 {
                     if (fOsXGuest)
                         *aParavirtProvider = ParavirtProvider_Minimal;
-                    else if (   mUserData->s.strOsType == "Windows10"
+                    else if (   mUserData->s.strOsType == "Windows11_64"
+                             || mUserData->s.strOsType == "Windows10"
                              || mUserData->s.strOsType == "Windows10_64"
                              || mUserData->s.strOsType == "Windows81"
                              || mUserData->s.strOsType == "Windows81_64"
