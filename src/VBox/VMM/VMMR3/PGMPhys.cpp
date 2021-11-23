3a78d49441ca018481915c2736644f4001f2ba0c
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-11-23 13:49:29 +0000

    VMM/PGM: Reset all relevant fields in the handy page array when pgmPhysFreePage finds the page being freed in the recently allocated page list. [build fix] bugref:10093 bugref:10147
    svn:sync-xref-src-repo-rev: r148447

--- src/VBox/VMM/VMMR3/PGMPhys.cpp
+++ src/VBox/VMM/VMMR3/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 92564 2021-11-23 13:37:27Z knut.osmundsen@oracle.com $ */
+/* $Id: PGMPhys.cpp 92565 2021-11-23 13:49:29Z knut.osmundsen@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -1442,7 +1442,7 @@ int pgmPhysFreePage(PVM pVM, PGMMFREEPAGESREQ pReq, uint32_t *pcPendingPages, PP
         if (pVM->pgm.s.aHandyPages[i].idPage == idPage)
         {
             pVM->pgm.s.aHandyPages[i].HCPhysGCPhys = NIL_GMMPAGEDESC_PHYS;
-            pVM->pgm.s.aHandyPages[i].fZero        = false;
+            pVM->pgm.s.aHandyPages[i].fZeroed      = false;
             pVM->pgm.s.aHandyPages[i].idPage       = NIL_GMM_PAGEID;
             break;
         }
