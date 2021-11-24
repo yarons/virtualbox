983edb9c994a7926d794350f0732e20fb33843a2
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2021-11-24 19:49:22 +0000

    scm fix.
    svn:sync-xref-src-repo-rev: r148473

--- src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoCreator.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoCreator.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVisoCreator.cpp 92588 2021-11-24 19:48:54Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVisoCreator.cpp 92589 2021-11-24 19:49:22Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVisoCreator class implementation.
  */
@@ -594,6 +594,7 @@ QUuid UIVisoCreatorWidget::createViso(UIActionPool *pActionPool, QWidget *pParen
     delete pVisoCreator;
     return QUuid();
 }
+
 
 /*********************************************************************************************************************************
 *   UIVisoCreatorDialog implementation.                                                                                          *
