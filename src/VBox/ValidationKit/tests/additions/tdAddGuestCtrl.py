9e3c2def8633e6d52bdf0187ddfcc8a1c168120a
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2021-12-01 20:47:39 +0000

    Validation Kit/tdAddGuestCtrl.py: disabled one frequently (always?) failing sub.test until it is properly fixed
    svn:sync-xref-src-repo-rev: r148579

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 84979 $"
+__version__ = "$Revision: 92681 $"
 
 # Standard Python imports.
 import errno
@@ -4722,8 +4722,9 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
                 [ tdTestCopyToDir(sSrc = sScratchEmptyDirHst, sDst = sScratchDstDir2Gst + oTestVm.pathSep()),
                   tdTestResultFailure() ],
                 # Copy with a different destination name just for the heck of it:
-                [ tdTestCopyToDir(sSrc = sScratchEmptyDirHst, sDst = oTestVm.pathJoin(sScratchDstDir1Gst, 'empty2')),
-                  tdTestResultSuccess() ],
+                # disabled at the moment, causes test failures due to the non-existing target
+                #[ tdTestCopyToDir(sSrc = sScratchEmptyDirHst, sDst = oTestVm.pathJoin(sScratchDstDir1Gst, 'empty2')),
+                #  tdTestResultSuccess() ],
             ]);
             atTests.extend([
                 # Now the same using a directory with files in it:
