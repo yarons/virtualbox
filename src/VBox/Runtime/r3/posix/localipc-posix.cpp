8c36af42362a8728cb9e0af24ad45517313a15fa
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2021-12-01 15:46:06 +0000

    iprt: Local Ipc Server: put synchronization primitives into rtLocalIpcSessionQueryUcred(), bugref:10134
    svn:sync-xref-src-repo-rev: r148576

--- src/VBox/Runtime/r3/posix/localipc-posix.cpp
+++ src/VBox/Runtime/r3/posix/localipc-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: localipc-posix.cpp 92678 2021-12-01 15:14:12Z knut.osmundsen@oracle.com $ */
+/* $Id: localipc-posix.cpp 92679 2021-12-01 15:46:06Z vadim.galitsyn@oracle.com $ */
 /** @file
  * IPRT - Local IPC Server & Client, Posix.
  */
@@ -1066,17 +1066,34 @@ static int rtLocalIpcSessionQueryUcred(RTLOCALIPCSESSION hSession, PRTPROCESS pP
 #if defined(RT_OS_LINUX)
     struct ucred PeerCred   = { (pid_t)NIL_RTPROCESS, (uid_t)NIL_RTUID, (gid_t)NIL_RTGID };
     socklen_t    cbPeerCred = sizeof(PeerCred);
-    if (getsockopt(RTSocketToNative(pThis->hSocket), SOL_SOCKET, SO_PEERCRED, &PeerCred, &cbPeerCred) >= 0)
+
+    rtLocalIpcSessionRetain(pThis);
+
+    int rc = RTCritSectEnter(&pThis->CritSect);;
+    if (RT_SUCCESS(rc))
     {
-        if (pProcess)
-            *pProcess = PeerCred.pid;
-        if (pUid)
-            *pUid = PeerCred.uid;
-        if (pGid)
-            *pGid = PeerCred.gid;
-        return VINF_SUCCESS;
+        if (getsockopt(RTSocketToNative(pThis->hSocket), SOL_SOCKET, SO_PEERCRED, &PeerCred, &cbPeerCred) >= 0)
+        {
+            if (pProcess)
+                *pProcess = PeerCred.pid;
+            if (pUid)
+                *pUid = PeerCred.uid;
+            if (pGid)
+                *pGid = PeerCred.gid;
+            rc = VINF_SUCCESS;
+        }
+        else
+        {
+            rc = RTErrConvertFromErrno(errno);
+        }
+
+        int rc2 = RTCritSectLeave(&pThis->CritSect);
+        AssertStmt(RT_SUCCESS(rc2), rc = RT_SUCCESS(rc) ? rc2 : rc);
     }
-    return RTErrConvertFromErrno(errno);
+
+    rtLocalIpcSessionRelease(pThis);
+
+    return rc;
 
 #else
     /** @todo Implement on other platforms too (mostly platform specific this).
