9af75af64e0a468c2ddd5956061b36750510e3b9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2021-12-29 10:31:27 +0000

    Main/os2_cid_install.cmd: license header.
    svn:sync-xref-src-repo-rev: r149111

--- src/VBox/Main/UnattendedTemplates/os2_cid_install.cmd
+++ src/VBox/Main/UnattendedTemplates/os2_cid_install.cmd
@@ -1,6 +1,19 @@
 @ECHO ON
-REM
+REM $Id: os2_cid_install.cmd 93097 2021-12-29 10:31:27Z knut.osmundsen@oracle.com $
+REM REM @fileREM
 REM VirtualBox CID Installation - main driver script for boot CD/floppy.
+REM
+
+REM
+REM Copyright (C) 2004-2020 Oracle Corporation
+REM
+REM This file is part of VirtualBox Open Source Edition (OSE), as
+REM available from http://www.virtualbox.org. This file is free software;
+REM you can redistribute it and/or modify it under the terms of the GNU
+REM General Public License (GPL) as published by the Free Software
+REM Foundation, in version 2 as it comes in the "COPYING" file of the
+REM VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+REM hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 REM
 
 echo .
