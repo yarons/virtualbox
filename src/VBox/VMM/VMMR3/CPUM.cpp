173a07a360fa291c52ae213c7bc14f3f09a1700a
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2022-01-13 12:07:19 +0000

    VMM: Nested VMX: bugref:10092 When Unrestricted Guest is supported by the CPU, the CPU still reports PE and PG bits as MB1 in IA32_VMX_CR0_FIXED0. It's just VMX instructions ignores them.
    svn:sync-xref-src-repo-rev: r149275

--- src/VBox/VMM/VMMR3/CPUM.cpp
+++ src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 93115 2022-01-01 11:31:46Z knut.osmundsen@oracle.com $ */
+/* $Id: CPUM.cpp 93223 2022-01-13 12:07:19Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -1441,7 +1441,7 @@ static void cpumR3InitVmxGuestMsrs(PVM pVM, PCVMXMSRS pHostVmxMsrs, PCCPUMFEATUR
     }
 
     /* CR0 Fixed-0. */
-    pGuestVmxMsrs->u64Cr0Fixed0 = pGuestFeatures->fVmxUnrestrictedGuest ? VMX_V_CR0_FIXED0_UX : VMX_V_CR0_FIXED0;
+    pGuestVmxMsrs->u64Cr0Fixed0 = VMX_V_CR0_FIXED0;
 
     /* CR0 Fixed-1. */
     {
