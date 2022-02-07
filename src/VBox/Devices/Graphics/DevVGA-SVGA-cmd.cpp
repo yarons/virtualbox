9d839c24858a2e7ac96a402de0706e232933820e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-02-07 10:50:26 +0000

    VMM/PGM,VMM/PDM,VGA: Consolidate the user parameters of the physical access handlers into a single uint64_t value that shouldn't be a pointer, at least not for ring-0 callbacks.  Special hack for devices where it's translated from a ring-0 device instance index into a current context PPDMDEVINS (not really tested yet). [doxygen fix] bugref:10094
    svn:sync-xref-src-repo-rev: r149781

--- src/VBox/Devices/Graphics/DevVGA-SVGA-cmd.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA-cmd.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA-cmd.cpp 93635 2022-02-07 10:43:45Z knut.osmundsen@oracle.com $ */
+/* $Id: DevVGA-SVGA-cmd.cpp 93636 2022-02-07 10:50:26Z knut.osmundsen@oracle.com $ */
 /** @file
  * VMware SVGA device - implementation of VMSVGA commands.
  */
@@ -357,8 +357,8 @@ const char *vmsvgaR3FifoCmdToString(uint32_t u32Cmd)
  * @param   cbBuf           How much it's reading/writing.
  * @param   enmAccessType   The access type.
  * @param   enmOrigin       Who is making the access.
- * @param   pvUser          User argument - VMM sets this to the address of the
- *                          device instance.
+ * @param   uUser           The VMM automatically sets this to the address of
+ *                          the device instance.
  */
 DECLCALLBACK(VBOXSTRICTRC)
 vmsvgaR3GboAccessHandler(PVM pVM, PVMCPU pVCpu, RTGCPHYS GCPhys, void *pvPhys, void *pvBuf, size_t cbBuf,
