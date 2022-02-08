403f60094d0347acb14c7b3fe08ce44116f6ea71
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-02-08 14:09:43 +0000

    VMM/TM,VMM/*: Moved RTTIMENANOTSDATAR0 into the ring-0 only part of the VM structure. Added a VMCC_CTX macro for selecting between tm and tmr0 VM components depending on the compilation context. Added a bunch of missing padding checks for GVM. [scm fix] bugref:10094
    svn:sync-xref-src-repo-rev: r149813

--- include/VBox/vmm/gvm.h
+++ include/VBox/vmm/gvm.h
@@ -1,4 +1,4 @@
-/* $Id: gvm.h 93655 2022-02-08 13:56:01Z knut.osmundsen@oracle.com $ */
+/* $Id: gvm.h 93658 2022-02-08 14:09:43Z knut.osmundsen@oracle.com $ */
 /** @file
  * GVM - The Global VM Data.
  */
@@ -338,5 +338,4 @@ AssertCompileSizeAlignment(GVM,             16384);
 /** @} */
 
 #endif /* !VBOX_INCLUDED_vmm_gvm_h */
-
 
