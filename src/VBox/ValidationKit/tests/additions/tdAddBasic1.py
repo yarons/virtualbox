7d6e755be670ca29498ca3792d02a90ff6dc9a85
Andreas Loeffler <andreas.loeffler@oracle.com>
2022-02-14 16:32:58 +0000

    Validation Kit/Guest Additions: Try gather the TxS log (if any, optional).
    svn:sync-xref-src-repo-rev: r149906

--- src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
+++ src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdAddBasic1.py 93115 2022-01-01 11:31:46Z knut.osmundsen@oracle.com $
+# $Id: tdAddBasic1.py 93734 2022-02-14 16:32:58Z andreas.loeffler@oracle.com $
 
 """
 VirtualBox Validation Kit - Additions Basics #1.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 93115 $"
+__version__ = "$Revision: 93734 $"
 
 # Standard Python imports.
 import os;
@@ -238,6 +238,13 @@ class tdAddBasic1(vbox.TestDriver):                                         # py
                 ## @todo Reset tests.
 
                 ## @todo Final test: Uninstallation.
+
+                # Download the TxS (Test Execution Service) log. This is not fatal when not being present.
+                if not fRc:
+                    self.txsDownloadFiles(oSession, oTxsSession,
+                                          [ (oTestVm.pathJoin(self.getGuestTempDir(oTestVm), 'vbox-txs-release.log'),
+                                                              'vbox-txs-%s.log' % oTestVm.sVmName) ],
+                                          fIgnoreErrors = True);
 
                 # Cleanup.
                 self.removeTask(oTxsSession);
