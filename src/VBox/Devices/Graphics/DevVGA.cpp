39d7865ad44f9de4f02e2d256de7015706e4366a
Michal Necasek <michal.necasek@oracle.com>
2022-02-17 13:25:24 +0000

    DevVGA: Show the MSR value in info vga since it's not dumped anywhere else.
    svn:sync-xref-src-repo-rev: r150028

--- src/VBox/Devices/Graphics/DevVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA.cpp 93796 2022-02-16 16:00:11Z michal.necasek@oracle.com $ */
+/* $Id: DevVGA.cpp 93827 2022-02-17 13:25:24Z michal.necasek@oracle.com $ */
 /** @file
  * DevVGA - VBox VGA/VESA device.
  */
@@ -4274,6 +4274,7 @@ static DECLCALLBACK(void) vgaR3InfoState(PPDMDEVINS pDevIns, PCDBGFINFOHLP pHlp,
     is_graph  = pThis->gr[6] & 1;
     char_dots = (pThis->sr[0x01] & 1) ? 8 : 9;
     double_scan = pThis->cr[9] >> 7;
+    pHlp->pfnPrintf(pHlp, "Misc status reg. MSR:%02X\n", pThis->msr);
     pHlp->pfnPrintf(pHlp, "pixel clock: %s\n", clocks[(pThis->msr >> 2) & 3]);
     pHlp->pfnPrintf(pHlp, "double scanning %s\n", double_scan ? "on" : "off");
     pHlp->pfnPrintf(pHlp, "double clocking %s\n", pThis->sr[1] & 0x08 ? "on" : "off");
