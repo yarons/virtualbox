f136754395233f3826c0d1bd604cf994883c3134
Sergey Dubov <sergey.dubov@oracle.com>
2022-02-22 11:17:24 +0000

    FE/Qt: bugref:6899: !VirtualBox Manager / Network Manager / Reworking NAT network details, group-box with embedded check-box isn't accessible through macOS screen-reader engine; To make it simple, reworking it everywhere for consistency.
    svn:sync-xref-src-repo-rev: r150102

--- src/VBox/Frontends/VirtualBox/src/networkmanager/UIDetailsWidgetNATNetwork.h
+++ src/VBox/Frontends/VirtualBox/src/networkmanager/UIDetailsWidgetNATNetwork.h
@@ -1,4 +1,4 @@
-/* $Id: UIDetailsWidgetNATNetwork.h 93115 2022-01-01 11:31:46Z knut.osmundsen@oracle.com $ */
+/* $Id: UIDetailsWidgetNATNetwork.h 93884 2022-02-22 11:17:24Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIDetailsWidgetNATNetwork class declaration.
  */
@@ -32,8 +32,6 @@
 /* Forward declarations: */
 class QAbstractButton;
 class QCheckBox;
-class QGridLayout;
-class QGroupBox;
 class QLabel;
 class QLineEdit;
 class QRadioButton;
@@ -210,14 +208,10 @@ private:
 
     /** @name Network variables.
       * @{ */
-        /** Holds the name layout instance. */
-        QGridLayout       *m_pLayoutName;
         /** Holds the network name label instance. */
         QLabel            *m_pLabelNetworkName;
         /** Holds the network name editor instance. */
         QLineEdit         *m_pEditorNetworkName;
-        /** Holds the IPv4 group-box instance. */
-        QGroupBox         *m_pGroupBoxIPv4;
         /** Holds the network IPv4 prefix label instance. */
         QLabel            *m_pLabelNetworkIPv4Prefix;
         /** Holds the network IPv4 prefix editor instance. */
@@ -225,7 +219,7 @@ private:
         /** Holds the 'supports DHCP' check-box instance. */
         QCheckBox         *m_pCheckboxSupportsDHCP;
         /** Holds the IPv4 group-box instance. */
-        QGroupBox         *m_pGroupBoxIPv6;
+        QCheckBox         *m_pCheckboxIPv6;
         /** Holds the network IPv6 prefix label instance. */
         QLabel            *m_pLabelNetworkIPv6Prefix;
         /** Holds the network IPv6 prefix editor instance. */
