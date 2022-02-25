50e3278d70b6edfdf5e023e6e438141bfd1a696a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-02-25 13:59:00 +0000

    Main/MachineImple: Fixed use of NULL ComObjPtr<Medium> object in error message formatting (found by gcc 11.2.1).
    svn:sync-xref-src-repo-rev: r150178

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 93891 2022-02-22 18:08:39Z vadim.galitsyn@oracle.com $ */
+/* $Id: MachineImpl.cpp 93948 2022-02-25 13:59:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * Implementation of IMachine in VBoxSVC.
  */
@@ -9340,10 +9340,9 @@ HRESULT Machine::i_loadStorageDevices(StorageController *aStorageController,
 
             default:
                 return setError(E_FAIL,
-                                tr("Device '%s' with unknown type is attached to the virtual machine '%s' ('%s')"),
-                                medium->i_getLocationFull().c_str(),
-                                mUserData->s.strName.c_str(),
-                                mData->m_strConfigFileFull.c_str());
+                                tr("Controller '%s' port %u unit %u has device with unknown type (%d) - virtual machine '%s' ('%s')"),
+                                data.strName.c_str(), dev.lPort, dev.lDevice, dev.deviceType,
+                                mUserData->s.strName.c_str(), mData->m_strConfigFileFull.c_str());
         }
 
         if (FAILED(rc))
