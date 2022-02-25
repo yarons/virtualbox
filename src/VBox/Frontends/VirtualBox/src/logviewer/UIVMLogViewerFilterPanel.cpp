cb9c7057f81d1f073ffaee61d28146b536f4e5b4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-02-25 14:01:40 +0000

    FE/Qt: Fixed a bunch of inverted NULL pointer checks in UIVMFilterLineEdit::createButtons - will probably change behaviour a bit. Again kudos to gcc 11.2.1 for pointing this out.
    svn:sync-xref-src-repo-rev: r150180

--- src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerFilterPanel.cpp
+++ src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerFilterPanel.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVMLogViewerFilterPanel.cpp 93115 2022-01-01 11:31:46Z knut.osmundsen@oracle.com $ */
+/* $Id: UIVMLogViewerFilterPanel.cpp 93950 2022-02-25 14:01:40Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMLogViewer class implementation.
  */
@@ -227,11 +227,11 @@ void UIVMFilterLineEdit::createButtons()
             m_pClearAllButton->setFixedSize(sh);
         }
     }
-    if (!m_pRemoveTermButton && !m_pClearAllButton)
+    if (m_pRemoveTermButton && m_pClearAllButton)
         setMinimumHeight(qMax(m_pRemoveTermButton->minimumHeight(), m_pClearAllButton->minimumHeight()));
-    else if (!m_pRemoveTermButton)
+    else if (m_pRemoveTermButton)
         setMinimumHeight(m_pRemoveTermButton->minimumHeight());
-    else if (!m_pClearAllButton)
+    else if (m_pClearAllButton)
         setMinimumHeight(m_pClearAllButton->minimumHeight());
 }
 
