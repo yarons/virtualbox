e6f83cc1c07b82ae2fcd68a8e9d0e7d8fd5624fa
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-03-02 15:33:28 +0000

    FE/Qt: qt6: QMouseEvent copy constructor is protected in 6.0, so tweak for mac. bugref:9898
    svn:sync-xref-src-repo-rev: r150298

--- src/VBox/Frontends/VirtualBox/src/extensions/QISplitter.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensions/QISplitter.cpp
@@ -1,4 +1,4 @@
-/* $Id: QISplitter.cpp 93990 2022-02-28 15:34:57Z knut.osmundsen@oracle.com $ */
+/* $Id: QISplitter.cpp 94060 2022-03-02 15:33:28Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Qt GUI - Qt extensions: QISplitter class implementation.
  */
@@ -311,12 +311,6 @@ bool QISplitter::eventFilter(QObject *pWatched, QEvent *pEvent)
                     if (   pHandle
                         && pHandle != pWatched)
                     {
-                        /* Create new mouse event with translated mouse positions. */
-                        QMouseEvent newME(pMouseEvent->type(),
-                                          pHandle->mapFromGlobal(pMouseEvent->globalPos()),
-                                          pMouseEvent->button(),
-                                          pMouseEvent->buttons(),
-                                          pMouseEvent->modifiers());
                         /* Check if we hit the handle */
                         bool fMarginHit = QRect(pHandle->mapToGlobal(QPoint(0, 0)), pHandle->size()).adjusted(-margin, 0, margin, 0).contains(pMouseEvent->globalPos());
                         if (pEvent->type() == QEvent::MouseButtonPress)
@@ -327,7 +321,11 @@ bool QISplitter::eventFilter(QObject *pWatched, QEvent *pEvent)
                             {
                                 m_fHandleGrabbed = true;
                                 UICursor::setCursor(this, Qt::SplitHCursor);
-                                qApp->postEvent(pHandle, new QMouseEvent(newME));
+                                qApp->postEvent(pHandle, new QMouseEvent(pMouseEvent->type(),
+                                                                         pHandle->mapFromGlobal(pMouseEvent->globalPos()),
+                                                                         pMouseEvent->button(),
+                                                                         pMouseEvent->buttons(),
+                                                                         pMouseEvent->modifiers()));
                                 return true;
                             }
                         }
@@ -339,15 +337,17 @@ bool QISplitter::eventFilter(QObject *pWatched, QEvent *pEvent)
                                     && pMouseEvent->buttons().testFlag(Qt::LeftButton)))
                             {
                                 UICursor::setCursor(this, Qt::SplitHCursor);
-                                qApp->postEvent(pHandle, new QMouseEvent(newME));
+                                qApp->postEvent(pHandle, new QMouseEvent(pMouseEvent->type(),
+                                                                         pHandle->mapFromGlobal(pMouseEvent->globalPos()),
+                                                                         pMouseEvent->button(),
+                                                                         pMouseEvent->buttons(),
+                                                                         pMouseEvent->modifiers()));
                                 return true;
                             }
-                            else
-                            {
-                                /* If not, reset the state. */
-                                m_fHandleGrabbed = false;
-                                UICursor::setCursor(this, Qt::ArrowCursor);
-                            }
+
+                            /* If not, reset the state. */
+                            m_fHandleGrabbed = false;
+                            UICursor::setCursor(this, Qt::ArrowCursor);
                         }
                     }
                 }
