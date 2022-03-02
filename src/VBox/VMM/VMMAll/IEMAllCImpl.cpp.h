c631dadf671c807feef76cb7ebed134ad8a32539
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2022-03-02 05:00:49 +0000

    VMM/IEM: Nested VMX: bugref:10092 Let the instruction specify the number of bytes it accesses in the VMX APIC-access page.
    svn:sync-xref-src-repo-rev: r150286

--- src/VBox/VMM/VMMAll/IEMAllCImpl.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImpl.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllCImpl.cpp.h 93922 2022-02-24 15:14:31Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: IEMAllCImpl.cpp.h 94051 2022-03-02 05:00:49Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * IEM - Instruction Implementation in C/C++ (code include).
  */
@@ -2756,6 +2756,7 @@ IEM_CIMPL_DEF_3(iemCImpl_enter, IEMMODE, enmEffOpSize, uint16_t, cbFrame, uint8_
     iemRegSubFromRspEx(pVCpu, &NewRsp, cbFrame);
 
     /** @todo Should probe write access at the new RSP according to AMD. */
+    /** @todo Should handle accesses to the VMX APIC-access page. */
 
     /* Commit it. */
     pVCpu->cpum.GstCtx.rbp = NewRbp.u;
@@ -7559,7 +7560,7 @@ IEM_CIMPL_DEF_1(iemCImpl_monitor, uint8_t, iEffSeg)
          *
          * See Intel spec. 29.4.4 "Instruction-Specific Considerations".
          */
-        rcStrict = iemVmxVirtApicAccessUnused(pVCpu, &GCPhysMem, IEM_ACCESS_TYPE_READ | IEM_ACCESS_WHAT_DATA);
+        rcStrict = iemVmxVirtApicAccessUnused(pVCpu, &GCPhysMem, 1, IEM_ACCESS_TYPE_READ | IEM_ACCESS_WHAT_DATA);
         if (   rcStrict != VINF_VMX_INTERCEPT_NOT_ACTIVE
             && rcStrict != VINF_VMX_MODIFIES_BEHAVIOR)
                 return rcStrict;
@@ -8217,7 +8218,7 @@ IEM_CIMPL_DEF_2(iemCImpl_clflush_clflushopt, uint8_t, iEffSeg, RTGCPTR, GCPtrEff
                  *
                  * See Intel spec. 29.4.4 "Instruction-Specific Considerations".
                  */
-                rcStrict = iemVmxVirtApicAccessUnused(pVCpu, &GCPhysMem, IEM_ACCESS_TYPE_READ | IEM_ACCESS_WHAT_DATA);
+                rcStrict = iemVmxVirtApicAccessUnused(pVCpu, &GCPhysMem, 1, IEM_ACCESS_TYPE_READ | IEM_ACCESS_WHAT_DATA);
                 if (   rcStrict != VINF_VMX_INTERCEPT_NOT_ACTIVE
                     && rcStrict != VINF_VMX_MODIFIES_BEHAVIOR)
                         return rcStrict;
