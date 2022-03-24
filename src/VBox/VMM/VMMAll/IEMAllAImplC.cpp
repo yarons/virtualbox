51b07d1ab8208f8ffa619c8a80945fbea938ef07
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-03-24 20:34:00 +0000

    VMM/IEM,tstIEMAImpl: Integer and decimal load/store tests. bugref:9898
    svn:sync-xref-src-repo-rev: r150670

--- src/VBox/VMM/VMMAll/IEMAllAImplC.cpp
+++ src/VBox/VMM/VMMAll/IEMAllAImplC.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAllAImplC.cpp 94350 2022-03-24 08:33:05Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllAImplC.cpp 94364 2022-03-24 20:34:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Implementation in Assembly, portable C variant.
  */
@@ -3175,21 +3175,21 @@ IEM_DECL_IMPL_DEF(void, iemAImpl_fld_r80_from_d80,(PCX86FXSTATE pFpuState, PIEMF
 }
 
 
-IEM_DECL_IMPL_DEF(void, iemAImpl_fild_i16_to_r80,(PCX86FXSTATE pFpuState, PIEMFPURESULT pFpuRes, int16_t const *pi16Val))
+IEM_DECL_IMPL_DEF(void, iemAImpl_fild_r80_from_i16,(PCX86FXSTATE pFpuState, PIEMFPURESULT pFpuRes, int16_t const *pi16Val))
 {
     RT_NOREF(pFpuState, pFpuRes, pi16Val);
     AssertReleaseFailed();
 }
 
 
-IEM_DECL_IMPL_DEF(void, iemAImpl_fild_i32_to_r80,(PCX86FXSTATE pFpuState, PIEMFPURESULT pFpuRes, int32_t const *pi32Val))
+IEM_DECL_IMPL_DEF(void, iemAImpl_fild_r80_from_i32,(PCX86FXSTATE pFpuState, PIEMFPURESULT pFpuRes, int32_t const *pi32Val))
 {
     RT_NOREF(pFpuState, pFpuRes, pi32Val);
     AssertReleaseFailed();
 }
 
 
-IEM_DECL_IMPL_DEF(void, iemAImpl_fild_i64_to_r80,(PCX86FXSTATE pFpuState, PIEMFPURESULT pFpuRes, int64_t const *pi64Val))
+IEM_DECL_IMPL_DEF(void, iemAImpl_fild_r80_from_i64,(PCX86FXSTATE pFpuState, PIEMFPURESULT pFpuRes, int64_t const *pi64Val))
 {
     RT_NOREF(pFpuState, pFpuRes, pi64Val);
     AssertReleaseFailed();
