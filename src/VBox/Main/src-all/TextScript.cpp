1c37a505e24c07c30ff95bcd1e35177cdbe1cd80
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2022-03-24 08:29:57 +0000

    Main/Unattended: ​​bugref:9781. Removing a rogue line.
    svn:sync-xref-src-repo-rev: r150655

--- src/VBox/Main/src-all/TextScript.cpp
+++ src/VBox/Main/src-all/TextScript.cpp
@@ -1,4 +1,4 @@
-/* $Id: TextScript.cpp 94347 2022-03-24 08:13:03Z serkan.bayraktar@oracle.com $ */
+/* $Id: TextScript.cpp 94349 2022-03-24 08:29:57Z serkan.bayraktar@oracle.com $ */
 /** @file
  * Classes for reading/parsing/saving text scripts (unattended installation, ++).
  */
@@ -375,5 +375,4 @@ HRESULT GeneralTextScript::appendLine(const Utf8Str &rStrLineToAppend)
         return E_OUTOFMEMORY;
     }
     return S_OK;
-
 }
