4b033e30212ec9b1eabcb94d4ac7f73d1017df7b
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-03-31 10:42:18 +0000

    tstIEMAImpl: Split the data out and put it in separate files to avoid turning it into a compiler memory usage test. bugref:9898
    svn:sync-xref-src-repo-rev: r150736

---
+++ src/VBox/VMM/testcase/tstIEMAImplDataInt-Amd.cpp
@@ -0,0 +1,19 @@
+/* $Id: tstIEMAImplDataInt-Amd.cpp 94409 2022-03-31 10:42:18Z knut.osmundsen@oracle.com $ */
+/** @file
+ * IEM Assembly Instruction Helper Testcase Data Amd - Stub.
+ */
+
+/*
+ * Copyright (C) 2022 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#include "tstIEMAImpl.h"
+
