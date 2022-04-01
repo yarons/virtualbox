bdecd4f8559432c3c6ce1a5f6aa72fed3b14b556
Andreas Loeffler <andreas.loeffler@oracle.com>
2022-04-01 10:44:59 +0000

    Backed out r150757 (HostDrivers/Support: Try to resolve IPRT-like errors and show those in the (release) logs) -- %Rrc doesn't work there.
    svn:sync-xref-src-repo-rev: r150760

--- src/VBox/HostDrivers/Support/SUPR3HardenedMain.cpp
+++ src/VBox/HostDrivers/Support/SUPR3HardenedMain.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPR3HardenedMain.cpp 94429 2022-04-01 09:52:43Z andreas.loeffler@oracle.com $ */
+/* $Id: SUPR3HardenedMain.cpp 94432 2022-04-01 10:44:59Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox Support Library - Hardened main().
  */
@@ -1651,7 +1651,7 @@ DECL_NO_RETURN(DECLHIDDEN(void)) supR3HardenedFatalMsgV(const char *pszWhere, SU
     /*
      * First to the log.
      */
-    supR3HardenedLog("Error %Rrc (%#x) in %s! (enmWhat=%d)\n", rc, rc, pszWhere, enmWhat);
+    supR3HardenedLog("Error %d in %s! (enmWhat=%d)\n", rc, pszWhere, enmWhat);
     va_list vaCopy;
     va_copy(vaCopy, va);
     supR3HardenedLogV(pszMsgFmt, vaCopy);
@@ -1674,7 +1674,7 @@ DECL_NO_RETURN(DECLHIDDEN(void)) supR3HardenedFatalMsgV(const char *pszWhere, SU
      * Then to the console.
      */
     suplibHardenedPrintPrefix();
-    suplibHardenedPrintF("Error %Rrc (%#x) in %s!\n", rc, rc, pszWhere);
+    suplibHardenedPrintF("Error %d in %s!\n", rc, pszWhere);
 
     suplibHardenedPrintPrefix();
     va_copy(vaCopy, va);
@@ -2692,3 +2692,4 @@ DECLHIDDEN(int) SUPR3HardenedMain(const char *pszProgName, uint32_t fFlags, int 
     g_enmSupR3HardenedMainState = SUPR3HARDENEDMAINSTATE_CALLED_TRUSTED_MAIN;
     return pfnTrustedMain(argc, argv, envp);
 }
+
