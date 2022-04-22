b494e2eb6b239902d84f8f873081b507d7552fc0
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2022-04-22 10:45:35 +0000

    Validation Kit/Unattended tests: bugref:9781. Disabling GAs for debian 9.x tests
    svn:sync-xref-src-repo-rev: r151055

--- src/VBox/ValidationKit/tests/installation/tdGuestOsUnattendedInst1.py
+++ src/VBox/ValidationKit/tests/installation/tdGuestOsUnattendedInst1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdGuestOsUnattendedInst1.py 94573 2022-04-12 13:12:41Z serkan.bayraktar@oracle.com $
+# $Id: tdGuestOsUnattendedInst1.py 94688 2022-04-22 10:45:35Z serkan.bayraktar@oracle.com $
 
 """
 VirtualBox Validation Kit - Guest OS unattended installation tests.
@@ -27,7 +27,7 @@ CDDL are applicable instead of those of the GPL.
 You may elect to license modified versions of this file under the
 terms and conditions of either the GPL or the CDDL or both.
 """
-__version__ = "$Revision: 94573 $"
+__version__ = "$Revision: 94688 $"
 
 
 # Standard Python imports.
@@ -526,9 +526,9 @@ class tdGuestOsInstTest1(vbox.TestDriver):
             UnattendedVm(oSet, 'tst-ubuntu-19.04-64',   'Ubuntu_64', '6.0/uaisos/ubuntu-19.04-desktop-amd64.iso',    # >=6GiB
                          UnattendedVm.kfNoGAs),
             UnattendedVm(oSet, 'tst-debian-9.3-64',     'Debian_64', '6.0/uaisos/debian-9.3.0-amd64-DVD-1.iso',      # >=6GiB?
-                         UnattendedVm.kfAvoidNetwork),
+                         UnattendedVm.kfAvoidNetwork | UnattendedVm.kfNoGAs),
             UnattendedVm(oSet, 'tst-debian-9.4-64',     'Debian_64', '6.0/uaisos/debian-9.4.0-amd64-DVD-1.iso',      # >=6GiB?
-                         UnattendedVm.kfAvoidNetwork),
+                         UnattendedVm.kfAvoidNetwork | UnattendedVm.kfNoGAs),
             #
             # OS/2.
             #
