6c487f7075ecf21e820fbea3dfbc87ffd1008530
Sergey Dubov <sergey.dubov@oracle.com>
2022-04-25 14:55:57 +0000

    FE/Qt/Ds: bugref:6899: Machine settings: Audio page accessibility improvements; Moving editor related stuff into separate UIAudioSettingsEditor widget.
    svn:sync-xref-src-repo-rev: r151083

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsAudio.h
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsAudio.h
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsAudio.h 94333 2022-03-23 11:21:34Z sergey.dubov@oracle.com $ */
+/* $Id: UIMachineSettingsAudio.h 94708 2022-04-25 14:55:57Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMachineSettingsAudio class declaration.
  */
@@ -25,10 +25,7 @@
 #include "UISettingsPage.h"
 
 /* Forward declarations: */
-class QCheckBox;
-class UIAudioControllerEditor;
-class UIAudioHostDriverEditor;
-class UIMachineAudioFeaturesEditor;
+class UIAudioSettingsEditor;
 struct UIDataSettingsMachineAudio;
 typedef UISettingsCache<UIDataSettingsMachineAudio> UISettingsCacheMachineAudio;
 
@@ -88,16 +85,8 @@ private:
 
     /** @name Widgets
      * @{ */
-        /** Holds the audio check-box instance. */
-        QCheckBox                    *m_pCheckBoxAudio;
-        /** Holds the audio settings widget instance. */
-        QWidget                      *m_pWidgetAudioSettings;
-        /** Holds the audio host driver editor instance. */
-        UIAudioHostDriverEditor      *m_pEditorAudioHostDriver;
-        /** Holds the audio host controller editor instance. */
-        UIAudioControllerEditor      *m_pEditorAudioController;
-        /** Holds the audio features editor instance. */
-        UIMachineAudioFeaturesEditor *m_pEditorAudioFeatures;
+        /** Holds the audio settings editor instance. */
+        UIAudioSettingsEditor *m_pEditorAudioSettings;
     /** @} */
 };
 
