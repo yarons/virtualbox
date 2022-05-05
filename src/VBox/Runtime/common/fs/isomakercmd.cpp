364eac452034efd418871cb2c767649cc5e6f0d1
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-05-05 20:37:04 +0000

    IPRT/isomakercmd.cpp: Another one needing alignment 4 lines down. bugref:9898
    svn:sync-xref-src-repo-rev: r151267

--- src/VBox/Runtime/common/fs/isomakercmd.cpp
+++ src/VBox/Runtime/common/fs/isomakercmd.cpp
@@ -1,4 +1,4 @@
-/* $Id: isomakercmd.cpp 94865 2022-05-05 19:17:28Z alexander.eichner@oracle.com $ */
+/* $Id: isomakercmd.cpp 94867 2022-05-05 20:37:04Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - ISO Image Maker Command.
  */
@@ -908,7 +908,7 @@ static int rtFsIsoMakerCmdVerifyImageInRandomOrder(PRTFSISOMAKERCMDOPTS pOpts, R
                     Assert(iHit < (int32_t)cBlocks);
                     if (iHit < 0)
                     {
-                        iHit = ASMBitFirstClear(pvBitmap, iBlock);
+                        iHit = ASMBitFirstClear(pvBitmap, RT_ALIGN_32(iBlock, 64));
                         Assert(iHit < (int32_t)cBlocks);
                     }
                     if (iHit >= 0)
