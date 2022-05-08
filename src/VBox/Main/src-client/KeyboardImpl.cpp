28da0b0f93850656be98d50796fd44b6870119b6
Alexander Eichner <alexander.eichner@oracle.com>
2022-05-08 19:56:34 +0000

    Main/src-client/KeyboardImpl.cpp: Adjust to the new rules wrt. to rc -> hrc,vrc usage, ​bugref:10223
    svn:sync-xref-src-repo-rev: r151331

--- src/VBox/Main/src-client/KeyboardImpl.cpp
+++ src/VBox/Main/src-client/KeyboardImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: KeyboardImpl.cpp 93444 2022-01-26 18:01:15Z knut.osmundsen@oracle.com $ */
+/* $Id: KeyboardImpl.cpp 94923 2022-05-08 19:56:34Z alexander.eichner@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -113,8 +113,8 @@ HRESULT Keyboard::init(Console *aParent)
     unconst(mParent) = aParent;
 
     unconst(mEventSource).createObject();
-    HRESULT rc = mEventSource->init();
-    AssertComRCReturnRC(rc);
+    HRESULT hrc = mEventSource->init();
+    AssertComRCReturnRC(hrc);
 
     /* Confirm a successful initialization */
     autoInitSpan.setSucceeded();
@@ -313,9 +313,9 @@ HRESULT Keyboard::releaseKeys()
 
     if (pUpPort)
     {
-        int rc = pUpPort->pfnReleaseKeys(pUpPort);
-        if (RT_FAILURE(rc))
-            AssertMsgFailed(("Failed to release keys on all keyboards! rc=%Rrc\n", rc));
+        int vrc = pUpPort->pfnReleaseKeys(pUpPort);
+        if (RT_FAILURE(vrc))
+            AssertMsgFailed(("Failed to release keys on all keyboards! vrc=%Rrc\n", vrc));
     }
 
     return S_OK;
