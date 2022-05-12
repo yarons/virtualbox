7a7832d86f2e010ddc4aa243a377fdc1629f3d30
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2022-05-12 10:53:07 +0000

    FE/Qt: bugref:10081. Hide clear action for virtual hard disks.
    svn:sync-xref-src-repo-rev: r151413

--- src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMediumManager.cpp 94824 2022-05-04 15:15:42Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIMediumManager.cpp 94988 2022-05-12 10:53:07Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMediumManager class implementation.
  */
@@ -1558,6 +1558,12 @@ void UIMediumManagerWidget::enableClearAction()
     if (!m_pActionPool || !m_pActionPool->action(UIActionIndexMN_M_Medium_S_Clear))
         return;
 
+    if (currentMediumType() == UIMediumDeviceType_HardDisk)
+    {
+        m_pActionPool->action(UIActionIndexMN_M_Medium_S_Clear)->setVisible(false);
+        return;
+    }
+    m_pActionPool->action(UIActionIndexMN_M_Medium_S_Clear)->setVisible(true);
     bool fEnable = ((currentMediumType() == UIMediumDeviceType_DVD) && m_fInaccessibleCD) ||
         ((currentMediumType() == UIMediumDeviceType_Floppy) && m_fInaccessibleFD);
     m_pActionPool->action(UIActionIndexMN_M_Medium_S_Clear)->setEnabled(fEnable);
