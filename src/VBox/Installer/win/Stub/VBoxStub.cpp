42a6c756485bbf9f8061f23bf65a0feaf57fc631
Andreas Loeffler <andreas.loeffler@oracle.com>
2022-05-17 09:55:12 +0000

    Windows installer/Stub: Fixed wrong logging when MSI arguments conversion failed.
    svn:sync-xref-src-repo-rev: r151475

--- src/VBox/Installer/win/Stub/VBoxStub.cpp
+++ src/VBox/Installer/win/Stub/VBoxStub.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxStub.cpp 95026 2022-05-16 18:15:31Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxStub.cpp 95030 2022-05-17 09:55:12Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxStub - VirtualBox's Windows installer stub.
  */
@@ -674,7 +674,7 @@ static RTEXITCODE ProcessMsiPackage(const char *pszMsi, const char *pszMsiArgs, 
     if (RT_FAILURE(rc))
     {
         RTUtf16Free(pwszMsi);
-        return ShowError("RTStrToUtf16 failed on '%s': %Rrc", pszMsi, rc);
+        return ShowError("RTStrToUtf16 failed on '%s': %Rrc", pszMsiArgs, rc);
     }
 
     UINT uStatus = MsiInstallProductW(pwszMsi, pwszMsiArgs);
