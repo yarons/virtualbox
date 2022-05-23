0e717156ab7e1e2f60d0ea5d1b5fd612d23aa846
Michal Necasek <michal.necasek@oracle.com>
2022-05-23 09:16:23 +0000

    Reverted accidental commit.
    svn:sync-xref-src-repo-rev: r151525

--- src/VBox/Devices/Storage/DevFdc.cpp
+++ src/VBox/Devices/Storage/DevFdc.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevFdc.cpp 95057 2022-05-23 09:15:10Z michal.necasek@oracle.com $ */
+/* $Id: DevFdc.cpp 95058 2022-05-23 09:16:23Z michal.necasek@oracle.com $ */
 /** @file
  * VBox storage devices - Floppy disk controller
  */
@@ -783,11 +783,7 @@ static void fdctrl_write (fdctrl_t *fdctrl, uint32_t reg, uint32_t value)
         fdctrl_write_tape(fdctrl, value);
         break;
     case FD_REG_DSR:
-#if 0
         fdctrl_write_rate(fdctrl, value);
-#else
-        fdctrl_write_data(fdctrl, value);
-#endif
         break;
     case FD_REG_FIFO:
         fdctrl_write_data(fdctrl, value);
