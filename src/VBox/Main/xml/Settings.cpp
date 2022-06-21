1fe1f91052e5dfa7d5bb513a8539927da192aef2
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-06-21 19:49:35 +0000

    GuestPropertySvc.h,Main/Settings.cpp: Made GUEST_PROP_MAX_NAME_LEN and GUEST_PROP_MAX_VALUE_LEN work like GUEST_PROP_MAX_PATTERN_LEN and be the max size of the respective strings, including the zero terminator.  This way a szName[GUEST_PROP_MAX_NAME_LEN] variable will be the exact max size of a property name, rather than one byte more than the max which was the previous weirdness. bugref:10185
    svn:sync-xref-src-repo-rev: r151926

--- src/VBox/Main/xml/Settings.cpp
+++ src/VBox/Main/xml/Settings.cpp
@@ -1,4 +1,4 @@
-/* $Id: Settings.cpp 95325 2022-06-21 14:18:21Z knut.osmundsen@oracle.com $ */
+/* $Id: Settings.cpp 95334 2022-06-21 19:49:35Z knut.osmundsen@oracle.com $ */
 /** @file
  * Settings File Manipulation API.
  *
@@ -4674,8 +4674,8 @@ void MachineConfigFile::readGuestProperties(const xml::ElementNode &elmGuestProp
              * should be less than GUEST_PROP_MAX_VALUE_LEN. Chop it down to an appropriate length. */
             /** @todo r=bird: Should add a RTCString method for this, as this may create a
              *        invalid UTF-8 encoding if we chop up a UTF-8 sequence. */
-            Assert(prop.strValue.length() + 1 >= GUEST_PROP_MAX_VALUE_LEN);
-            prop.strValue.mutableRaw()[GUEST_PROP_MAX_VALUE_LEN - 2] = '\0';
+            Assert(prop.strValue.length() + 1 > GUEST_PROP_MAX_VALUE_LEN);
+            prop.strValue.mutableRaw()[GUEST_PROP_MAX_VALUE_LEN - 1] = '\0';
             prop.strValue.jolt();
         }
         else if (RT_FAILURE(rc))
