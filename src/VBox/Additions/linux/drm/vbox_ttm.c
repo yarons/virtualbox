af77d65a3fc82b261790e3cd88f0a1ea84dc7466
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2022-06-28 19:20:29 +0000

    Linux host and guest drivers: fix build (3), ​bugref:10247.
    svn:sync-xref-src-repo-rev: r152017

--- src/VBox/Additions/linux/drm/vbox_ttm.c
+++ src/VBox/Additions/linux/drm/vbox_ttm.c
@@ -1,4 +1,4 @@
-/* $Id: vbox_ttm.c 95414 2022-06-28 19:17:31Z vadim.galitsyn@oracle.com $ */
+/* $Id: vbox_ttm.c 95415 2022-06-28 19:20:29Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -795,7 +795,7 @@ int vbox_bo_push_sysram(struct vbox_bo *bo)
 int vbox_mmap(struct file *filp, struct vm_area_struct *vma)
 {
 	struct drm_file *file_priv;
-	struct vbox_private *vbox
+	struct vbox_private *vbox;
 	int ret = -EINVAL;
 
 	if (unlikely(vma->vm_pgoff < DRM_FILE_PAGE_OFFSET))
