2207a98ac2c9ce2e88f550d672e4188409ca6778
Alexander Eichner <alexander.eichner@oracle.com>
2022-06-28 14:21:38 +0000

    NAT: host resolver must not return NXDOMAIN for query classes and query types that it doesn't support. Return successful replies with no answers instead. bugref:10248
    svn:sync-xref-src-repo-rev: r152005

--- src/VBox/Devices/Network/slirp/hostres.c
+++ src/VBox/Devices/Network/slirp/hostres.c
@@ -1,4 +1,4 @@
-/* $Id: hostres.c 93115 2022-01-01 11:31:46Z knut.osmundsen@oracle.com $ */
+/* $Id: hostres.c 95408 2022-06-28 14:21:38Z alexander.eichner@oracle.com $ */
 /** @file
  * Host resolver
  */
@@ -512,7 +512,7 @@ respond(struct response *res)
         && qclass != Class_ANY)
     {
         LogErr(("NAT: hostres: unsupported qclass %d\n", qclass));
-        return refuse(res, RCode_NXDomain);
+        return refuse(res, RCode_NoError);
     }
 
     if (   qtype != Type_A
@@ -521,7 +521,7 @@ respond(struct response *res)
         && qtype != Type_ANY)
     {
         LogErr(("NAT: hostres: unsupported qtype %d\n", qtype));
-        return refuse(res, RCode_NXDomain);
+        return refuse(res, RCode_NoError);
     }
 
 
