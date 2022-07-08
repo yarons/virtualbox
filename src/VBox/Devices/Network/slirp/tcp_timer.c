df45a5819898dc333d81a355965514ae5619565a
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2022-07-08 18:16:35 +0000

    Network/slirp: Advertising clause for Danny Gasparovsky was unintentional, should have always been 3-clause BSD. Replace 4-clause BSD license by 3-clause, see retroactive license change by UC Berkeley https://www.freebsd.org/copyright/license/
    svn:sync-xref-src-repo-rev: r152200

--- src/VBox/Devices/Network/slirp/tcp_timer.c
+++ src/VBox/Devices/Network/slirp/tcp_timer.c
@@ -1,4 +1,4 @@
-/* $Id: tcp_timer.c 93115 2022-01-01 11:31:46Z knut.osmundsen@oracle.com $ */
+/* $Id: tcp_timer.c 95573 2022-07-08 18:16:35Z klaus.espenlaub@oracle.com $ */
 /** @file
  * NAT - TCP timers.
  */
@@ -29,11 +29,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the University of
- *      California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
