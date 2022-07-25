27f0c0b42a562b518fb3a207171c45dfa179bb14
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-07-25 12:37:43 +0000

    IPRT/r3/process.cpp: Call GetCurrentProcessId directly instead of _getpid() on windows. bugref:10261
    svn:sync-xref-src-repo-rev: r152527

--- src/VBox/Runtime/r3/process.cpp
+++ src/VBox/Runtime/r3/process.cpp
@@ -1,4 +1,4 @@
-/* $Id: process.cpp 93115 2022-01-01 11:31:46Z knut.osmundsen@oracle.com $ */
+/* $Id: process.cpp 95805 2022-07-25 12:37:43Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Process, Common.
  */
@@ -37,7 +37,7 @@
 #include "internal/thread.h"
 
 #ifdef RT_OS_WINDOWS
-# include <process.h>
+# include <iprt/win/windows.h>
 #else
 # include <unistd.h>
 #endif
@@ -56,7 +56,7 @@ RTDECL(RTPROCESS) RTProcSelf(void)
 
     /* lazy init. */
 #ifdef _MSC_VER
-    Self = _getpid(); /* crappy ansi compiler */
+    Self = GetCurrentProcessId(); /* since NT 3.1, not 3.51+ as listed on geoffchappell.com */
 #else
     Self = getpid();
 #endif
