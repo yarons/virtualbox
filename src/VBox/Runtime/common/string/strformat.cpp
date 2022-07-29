e45f8286bb40fa93d1a08dca6e8ff157dfb2d985
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-07-29 00:11:43 +0000

    IPRT/nocrt: build fix. bugref:10261
    svn:sync-xref-src-repo-rev: r152687

--- src/VBox/Runtime/common/string/strformat.cpp
+++ src/VBox/Runtime/common/string/strformat.cpp
@@ -1,4 +1,4 @@
-/* $Id: strformat.cpp 95931 2022-07-28 23:21:27Z knut.osmundsen@oracle.com $ */
+/* $Id: strformat.cpp 95932 2022-07-29 00:11:43Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - String Formatter.
  */
@@ -776,7 +776,7 @@ RTDECL(size_t) RTStrFormatV(PFNRTSTROUTPUT pfnOutput, void *pvArgOutput, PFNSTRF
                     case 'a': /* [-]0xh.hhhhhhp+-dd */
                     case 'A': /* [-]0Xh.hhhhhhP+-dd */
                     {
-#if defined(IN_RING3) && !defined(IN_SUP_HARDENED_R3)
+#if defined(IN_RING3) && !defined(IN_SUP_HARDENED_R3) && !defined(IPRT_NO_FLOAT_FORMATTING)
                         size_t cchNum;
 # ifdef RT_COMPILER_WITH_80BIT_LONG_DOUBLE
                         if (chArgSize == 'L')
