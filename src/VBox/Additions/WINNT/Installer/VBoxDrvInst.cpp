bcf52894f032e6dd842b617062d697a56c1e4e13
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-08-24 21:07:05 +0000

    Add/NT/Inst: Fixed mixed up arguments for driver install/uninstall in VBoxDrvInst. bugref:10261
    svn:sync-xref-src-repo-rev: r153301

--- src/VBox/Additions/WINNT/Installer/VBoxDrvInst.cpp
+++ src/VBox/Additions/WINNT/Installer/VBoxDrvInst.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxDrvInst.cpp 96449 2022-08-23 23:55:38Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxDrvInst.cpp 96456 2022-08-24 21:07:05Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxDrvInst - Driver and service installation helper for Windows guests.
  */
@@ -528,7 +528,7 @@ static int VBoxInstallDriver(const BOOL fInstall, const wchar_t *pwszDriverPath,
 /** Handles 'driver install'. */
 static int handleDriverInstall(unsigned cArgs, wchar_t **papwszArgs)
 {
-    return VBoxInstallDriver(true /*fInstall*/, papwszArgs[1], false /*fSilent*/,
+    return VBoxInstallDriver(true /*fInstall*/, papwszArgs[0], false /*fSilent*/,
                              cArgs > 1 && papwszArgs[1][0] ? papwszArgs[1] : NULL /* pwszLogFile*/);
 }
 
@@ -536,7 +536,7 @@ static int handleDriverInstall(unsigned cArgs, wchar_t **papwszArgs)
 /** Handles 'driver uninstall'. */
 static int handleDriverUninstall(unsigned cArgs, wchar_t **papwszArgs)
 {
-    return VBoxInstallDriver(false /*fInstall*/, papwszArgs[1], false /*fSilent*/,
+    return VBoxInstallDriver(false /*fInstall*/, papwszArgs[0], false /*fSilent*/,
                              cArgs > 1 && papwszArgs[1][0] ? papwszArgs[1] : NULL /* pwszLogFile*/);
 }
 
