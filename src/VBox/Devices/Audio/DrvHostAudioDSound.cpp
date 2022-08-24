e694fb22221eb20b74b01e62eb1f9b7bf8114341
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-08-24 23:48:12 +0000

    ValKit/vkat,Devices/Audio: Made vkat build in no-CRT mode. bugref:10261
    svn:sync-xref-src-repo-rev: r153320

--- src/VBox/Devices/Audio/DrvHostAudioDSound.cpp
+++ src/VBox/Devices/Audio/DrvHostAudioDSound.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostAudioDSound.cpp 96407 2022-08-22 17:43:14Z klaus.espenlaub@oracle.com $ */
+/* $Id: DrvHostAudioDSound.cpp 96472 2022-08-24 23:48:12Z knut.osmundsen@oracle.com $ */
 /** @file
  * Host audio driver - DirectSound (Windows).
  */
@@ -2782,16 +2782,23 @@ static DECLCALLBACK(int) drvHostDSoundConstruct(PPDMDRVINS pDrvIns, PCFGMNODE pC
 # else
         PPDMIHOSTAUDIOPORT pIHostAudioPort = NULL;
 # endif
+#ifdef RT_EXCEPTIONS_ENABLED
         try
+#endif
         {
             pThis->m_pNotificationClient = new DrvHostAudioDSoundMMNotifClient(pIHostAudioPort,
                                                                                pThis->Cfg.pGuidCapture == NULL,
                                                                                pThis->Cfg.pGuidPlay == NULL);
         }
+#ifdef RT_EXCEPTIONS_ENABLED
         catch (std::bad_alloc &)
         {
             return VERR_NO_MEMORY;
         }
+#else
+        AssertReturn(pThis->m_pNotificationClient, VERR_NO_MEMORY);
+#endif
+
         hrc = pThis->m_pNotificationClient->Initialize();
         if (SUCCEEDED(hrc))
         {
