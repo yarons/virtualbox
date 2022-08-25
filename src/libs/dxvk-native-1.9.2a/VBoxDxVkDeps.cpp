c10d8b7df1e5ddd375921ca37e41617a3d046c3d
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2022-08-25 19:29:27 +0000

    libs/dxvk-native-1.9.2a: Fix scm issue after exporting.
    svn:sync-xref-src-repo-rev: r153353

--- src/libs/dxvk-native-1.9.2a/VBoxDxVkDeps.cpp
+++ src/libs/dxvk-native-1.9.2a/VBoxDxVkDeps.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxDxVkDeps.cpp 96497 2022-08-25 17:04:27Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxDxVkDeps.cpp 96500 2022-08-25 19:29:27Z klaus.espenlaub@oracle.com $ */
 /** @file
  * VBoxDxVk - For dragging in library objects.
  */
@@ -6,7 +6,23 @@
 /*
  * Copyright (C) 2022 Oracle and/or its affiliates.
  *
- * Oracle Corporation confidential
+ * This file is part of VirtualBox base platform packages, as
+ * available from https://www.virtualbox.org.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, in version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
  */
 
 
