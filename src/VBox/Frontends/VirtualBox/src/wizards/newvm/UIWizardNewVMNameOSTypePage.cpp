5fe337accece411152a113c674bacaee6c4b4808
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2022-09-07 19:18:46 +0000

    FE/Qt: bugref:9515. In guided new vm wizard disable OS type selectors if OS detection is successfull, not th  other way around.
    svn:sync-xref-src-repo-rev: r153523

--- src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMNameOSTypePage.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMNameOSTypePage.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMNameOSTypePage.cpp 96640 2022-09-07 18:10:07Z knut.osmundsen@oracle.com $ */
+/* $Id: UIWizardNewVMNameOSTypePage.cpp 96641 2022-09-07 19:18:46Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMPageBasicNameOSStype class implementation.
  */
@@ -621,7 +621,7 @@ void UIWizardNewVMNameOSTypePage::sltISOPathChanged(const QString &strPath)
     /* Disable OS type selector(s) to prevent user from changing guest OS type manually: */
     if (m_pNameAndSystemEditor)
     {
-        m_pNameAndSystemEditor->setOSTypeStuffEnabled(fOsTypeFixed);
+        m_pNameAndSystemEditor->setOSTypeStuffEnabled(!fOsTypeFixed);
 
         /* Redetect the OS type using the name if detection or the step above failed: */
         if (!fOsTypeFixed)
