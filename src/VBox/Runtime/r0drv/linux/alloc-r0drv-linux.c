d38a7d707bf144497c7e56de15563d4f905cb249
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2022-09-13 10:12:48 +0000

    Backed out r153600: Incorrect fix, bugref:4567:65.
    svn:sync-xref-src-repo-rev: r153601

--- src/VBox/Runtime/r0drv/linux/alloc-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/alloc-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: alloc-r0drv-linux.c 96715 2022-09-13 09:26:31Z vadim.galitsyn@oracle.com $ */
+/* $Id: alloc-r0drv-linux.c 96716 2022-09-13 10:12:48Z vadim.galitsyn@oracle.com $ */
 /** @file
  * IPRT - Memory Allocation, Ring-0 Driver, Linux.
  */
@@ -279,11 +279,7 @@ DECLHIDDEN(int) rtR0MemAllocEx(size_t cb, uint32_t fFlags, PRTMEMHDR *ppHdr)
 # endif /* !RTMEMALLOC_EXEC_HEAP */
 
 #elif defined(PAGE_KERNEL_EXEC) && defined(CONFIG_X86_PAE)
-# if RTLNX_VER_MIN(5,8,0)
-        pHdr = (PRTMEMHDR)__vmalloc(cb + sizeof(*pHdr), GFP_KERNEL | __GFP_HIGHMEM | __GFP_NOWARN);
-# else
         pHdr = (PRTMEMHDR)__vmalloc(cb + sizeof(*pHdr), GFP_KERNEL | __GFP_HIGHMEM | __GFP_NOWARN, MY_PAGE_KERNEL_EXEC);
-# endif
 #else
         pHdr = (PRTMEMHDR)vmalloc(cb + sizeof(*pHdr));
 #endif
