ffaaf09369f3dcdd53fb6b1e53f3d255b36164be
Michal Necasek <michal.necasek@oracle.com>
2022-10-12 15:08:10 +0000

    IEM: Removed todo, testcase confirmed that CMPXCHG8B implementation was right.
    svn:sync-xref-src-repo-rev: r154079

--- src/VBox/VMM/VMMAll/IEMAll.cpp
+++ src/VBox/VMM/VMMAll/IEMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAll.cpp 97050 2022-10-07 15:54:26Z michal.necasek@oracle.com $ */
+/* $Id: IEMAll.cpp 97117 2022-10-12 15:08:10Z michal.necasek@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager - All Contexts.
  */
@@ -4170,7 +4170,15 @@ DECL_NO_RETURN(void) iemRaisePageFaultJmp(PVMCPUCC pVCpu, RTGCPTR GCPtrWhere, ui
 /** \#MF(0) - 10.  */
 VBOXSTRICTRC iemRaiseMathFault(PVMCPUCC pVCpu)
 {
-    return iemRaiseXcptOrInt(pVCpu, 0, X86_XCPT_MF, IEM_XCPT_FLAGS_T_CPU_XCPT, 0, 0);
+    if (pVCpu->cpum.GstCtx.cr0 & X86_CR0_NE)
+        return iemRaiseXcptOrInt(pVCpu, 0, X86_XCPT_MF, IEM_XCPT_FLAGS_T_CPU_XCPT, 0, 0);
+    else
+    {
+        /* Convert a #MF into a FERR -> IRQ 13. See @bugref{6117}. */
+        PDMIsaSetIrq(pVCpu->CTX_SUFF(pVM), 13 /* u8Irq */, 1 /* u8Level */, 0 /* uTagSrc */);
+        iemRegUpdateRipAndClearRF(pVCpu);
+        return VINF_SUCCESS;
+    }
 }
 
 
