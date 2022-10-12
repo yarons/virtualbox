e7c1e1fb3b1c41e9c9a688f5bce0a39c10869caa
Alexander Eichner <alexander.eichner@oracle.com>
2022-10-12 10:06:11 +0000

    VMM/NEMR3Native-darwin: A test for which a proeprly signed build is required, bugref:9044
    svn:sync-xref-src-repo-rev: r154066

--- src/VBox/VMM/VMMR3/NEMR3Native-darwin.cpp
+++ src/VBox/VMM/VMMR3/NEMR3Native-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: NEMR3Native-darwin.cpp 97073 2022-10-10 16:32:10Z knut.osmundsen@oracle.com $ */
+/* $Id: NEMR3Native-darwin.cpp 97106 2022-10-12 10:06:11Z alexander.eichner@oracle.com $ */
 /** @file
  * NEM - Native execution manager, native ring-3 macOS backend using Hypervisor.framework.
  *
@@ -543,7 +543,9 @@ DECLINLINE(int) nemR3DarwinMap(PVM pVM, RTGCPHYS GCPhys, void *pvRam, size_t cb,
         hrc = hv_vm_map_space(pVM->nem.s.uVmAsid, pvRam, GCPhys, cb, fHvMemProt);
     else
         hrc = hv_vm_map(pvRam, GCPhys, cb, fHvMemProt);
-    return nemR3DarwinHvSts2Rc(hrc);
+    if (hrc != HV_SUCCESS)
+        LogRel(("Failed to map %RGp LB %zu fProt=%#x/%#x hrc=%\n", GCPhys, cb, fPageProt, fHvMemProt));
+    return VINF_SUCCESS; //nemR3DarwinHvSts2Rc(hrc);
 }
 
 
