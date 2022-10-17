10b7464b5e04fad38d932b4211c19989a3f1debe
Alexander Eichner <alexander.eichner@oracle.com>
2022-10-17 17:02:37 +0000

    VMM/NEMR3Native-darwin: Addendum for r154132, unmapping doesn't unwedge the map. Check whether it would be possible to make it work when we set g_fAppleHvNoWX upfront by determining the macOS version, bugref:9044
    svn:sync-xref-src-repo-rev: r154148

--- src/VBox/VMM/VMMR3/NEMR3Native-darwin.cpp
+++ src/VBox/VMM/VMMR3/NEMR3Native-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: NEMR3Native-darwin.cpp 97169 2022-10-17 16:02:48Z alexander.eichner@oracle.com $ */
+/* $Id: NEMR3Native-darwin.cpp 97174 2022-10-17 17:02:37Z alexander.eichner@oracle.com $ */
 /** @file
  * NEM - Native execution manager, native ring-3 macOS backend using Hypervisor.framework.
  *
@@ -2992,6 +2992,13 @@ int nemR3NativeInit(PVM pVM, bool fFallback, bool fForced)
      * Whether to enable LBR for the guest. This is disabled by default as it's only
      * useful while debugging and enabling it causes a noticeable performance hit. */
     rc = CFGMR3QueryBoolDef(pCfgNem, "VmxLbr", &pVM->nem.s.fLbr, false);
+    AssertRCReturn(rc, rc);
+
+    /** @cfgm{/NEM/CatalinaWxWorkaround, bool, false}
+     * Whether to allow only W^X guest mappings due to a bug in the Catalina AppleHV
+     * driver refusing RWX when a properly signed binary is used.
+     */
+    rc = CFGMR3QueryBoolDef(pCfgNem, "CatalinaWxWorkaround", &g_fAppleHvNoWX, false);
     AssertRCReturn(rc, rc);
 
     /*
