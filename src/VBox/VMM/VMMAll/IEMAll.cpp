d56ee1fff4c4d9c367e42a060ebfa7068a540bb9
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2022-11-05 12:42:14 +0000

    VMM/IEM,CPUM: Partial single stepping support in the interpreter. bugref:9898
    svn:sync-xref-src-repo-rev: r154411

--- src/VBox/VMM/VMMAll/IEMAll.cpp
+++ src/VBox/VMM/VMMAll/IEMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAll.cpp 97370 2022-11-02 00:53:30Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAll.cpp 97406 2022-11-05 12:42:14Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager - All Contexts.
  */
@@ -10011,6 +10011,9 @@ VMMDECL(VBOXSTRICTRC) IEMExecLots(PVMCPUCC pVCpu, uint32_t cMaxInstructions, uin
                  */
                 uint8_t b; IEM_OPCODE_GET_NEXT_U8(&b);
                 rcStrict = FNIEMOP_CALL(g_apfnOneByteMap[b]);
+#ifdef VBOX_STRICT
+                CPUMAssertGuestRFlagsCookie(pVM, pVCpu);
+#endif
                 if (RT_LIKELY(rcStrict == VINF_SUCCESS))
                 {
                     Assert(pVCpu->iem.s.cActiveMappings == 0);
