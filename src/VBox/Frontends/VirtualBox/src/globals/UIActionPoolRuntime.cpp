27308ace9199c7db148fdfbce6ea0a964cf96c7e
Sergey Dubov <sergey.dubov@oracle.com>
2023-01-04 08:44:38 +0000

    FE/Qt: bugref:10342: Get rid of UIMultiScreenLayout translation context.
    svn:sync-xref-src-repo-rev: r155064

--- src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolRuntime.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolRuntime.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIActionPoolRuntime.cpp 97972 2023-01-04 08:19:40Z sergey.dubov@oracle.com $ */
+/* $Id: UIActionPoolRuntime.cpp 97973 2023-01-04 08:44:38Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIActionPoolRuntime class implementation.
  */
@@ -3844,7 +3844,7 @@ void UIActionPoolRuntime::updateMenuView()
             /* Add 'Virtual Screen %1' menu: */
             QMenu *pSubMenu = pMenu->addMenu(UIIconPool::iconSet(":/virtual_screen_16px.png",
                                                                  ":/virtual_screen_disabled_16px.png"),
-                                             QApplication::translate("UIMultiScreenLayout", "Virtual Screen %1").arg(iGuestScreenIndex + 1));
+                                             QApplication::translate("UIActionPool", "Virtual Screen %1").arg(iGuestScreenIndex + 1));
             pSubMenu->setProperty("Guest Screen Index", iGuestScreenIndex);
             connect(pSubMenu, &QMenu::aboutToShow, this, &UIActionPoolRuntime::sltPrepareMenuViewScreen);
         }
@@ -3887,7 +3887,7 @@ void UIActionPoolRuntime::updateMenuViewPopup()
             /* Add 'Virtual Screen %1' menu: */
             QMenu *pSubMenu = pMenu->addMenu(UIIconPool::iconSet(":/virtual_screen_16px.png",
                                                                  ":/virtual_screen_disabled_16px.png"),
-                                             QApplication::translate("UIMultiScreenLayout", "Virtual Screen %1").arg(iGuestScreenIndex + 1));
+                                             QApplication::translate("UIActionPool", "Virtual Screen %1").arg(iGuestScreenIndex + 1));
             pSubMenu->setProperty("Guest Screen Index", iGuestScreenIndex);
             connect(pSubMenu, &QMenu::aboutToShow, this, &UIActionPoolRuntime::sltPrepareMenuViewScreen);
         }
@@ -4074,7 +4074,7 @@ void UIActionPoolRuntime::updateMenuViewRemap(QMenu *pMenu)
         for (int iHostScreenIndex = 0; iHostScreenIndex < m_cHostScreens; ++iHostScreenIndex)
         {
             /* Create exclusive 'remap' action: */
-            QAction *pAction = pActionGroup->addAction(QApplication::translate("UIMultiScreenLayout", "Use Host Screen %1")
+            QAction *pAction = pActionGroup->addAction(QApplication::translate("UIActionPool", "Use Host Screen %1")
                                                                                .arg(iHostScreenIndex + 1));
             if (pAction)
             {
