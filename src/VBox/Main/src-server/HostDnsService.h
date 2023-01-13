969103afe77ac7ba0e8c75b445be213d642d799a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-01-13 10:38:26 +0000

    Main/HostDnsServiceLinux.cpp: Massaged the resolv.conf monitoring code a bit more. bugref:10255
    svn:sync-xref-src-repo-rev: r155196

--- src/VBox/Main/src-server/HostDnsService.h
+++ src/VBox/Main/src-server/HostDnsService.h
@@ -1,4 +1,4 @@
-/* $Id: HostDnsService.h 96407 2022-08-22 17:43:14Z klaus.espenlaub@oracle.com $ */
+/* $Id: HostDnsService.h 98071 2023-01-13 10:38:26Z knut.osmundsen@oracle.com $ */
 /** @file
  * Host DNS listener.
  */
@@ -248,7 +248,7 @@ class HostDnsServiceLinux : public HostDnsServiceResolvConf
 {
 public:
 
-    HostDnsServiceLinux() : HostDnsServiceResolvConf(true) {}
+    HostDnsServiceLinux() : HostDnsServiceResolvConf(true), m_fdShutdown(-1) {}
     virtual ~HostDnsServiceLinux();
 
 public:
@@ -259,6 +259,10 @@ protected:
 
     int monitorThreadShutdown(RTMSINTERVAL uTimeoutMs);
     int monitorThreadProc(void);
+
+    /** Socket end to write shutdown notification to, so the monitor thread will
+     *  wake up and terminate. */
+    int m_fdShutdown;
 };
 
 #  endif
