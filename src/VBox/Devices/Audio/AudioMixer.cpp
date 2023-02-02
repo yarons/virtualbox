24319d71bf3744625f8eb8d1caba81971f6f8fc6
Andreas Loeffler <andreas.loeffler@oracle.com>
2023-02-02 19:19:49 +0000

    Audio: When waiting for draining fails, don't return early but release the critsect first. ​bugref:10354
    svn:sync-xref-src-repo-rev: r155703

--- src/VBox/Devices/Audio/AudioMixer.cpp
+++ src/VBox/Devices/Audio/AudioMixer.cpp
@@ -1,4 +1,4 @@
-/* $Id: AudioMixer.cpp 98406 2023-02-01 15:19:32Z andreas.loeffler@oracle.com $ */
+/* $Id: AudioMixer.cpp 98453 2023-02-02 19:19:49Z andreas.loeffler@oracle.com $ */
 /** @file
  * Audio mixing routines for multiplexing audio sources in device emulations.
  */
@@ -631,7 +631,6 @@ int AudioMixerSinkDrainAndStopEx(PAUDMIXSINK pSink, uint32_t cbComming, RTMSINTE
                         && pSink->AIO.cUpdateJobs)
                     {
                         rc = audioMixerSinkWaitForDrainedLocked(pSink, msTimeout);
-                        AssertRCReturn(rc, rc);
 #ifdef LOG_ENABLED
                         Log3Func(("Waiting for update jobs done (rc=%Rrc, took %RU64ms)\n", rc, RTTimeMilliTS() - msStart));
 #endif
@@ -673,7 +672,7 @@ int AudioMixerSinkDrainAndStopEx(PAUDMIXSINK pSink, uint32_t cbComming, RTMSINTE
 
     LogRel2(("Audio Mixer: Started draining sink '%s': %s\n", pSink->pszName, dbgAudioMixerSinkStatusToStr(pSink->fStatus, szStatus)));
     RTCritSectLeave(&pSink->CritSect);
-    return VINF_SUCCESS;
+    return rc;
 }
 
 
