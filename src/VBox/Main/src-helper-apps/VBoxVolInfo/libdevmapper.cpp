6ea3341cb8fd4706dd39015e092feae37858f813
Alexander Eichner <alexander.eichner@oracle.com>
2023-02-24 12:32:25 +0000

    Main/VBoxVolInfo: Don't link directly against libdevmapper but use the IPRT runtime loader to get rid of the buildtime dependency, bugref:10367 [scm fixes]
    svn:sync-xref-src-repo-rev: r156021

--- src/VBox/Main/src-helper-apps/VBoxVolInfo/libdevmapper.cpp
+++ src/VBox/Main/src-helper-apps/VBoxVolInfo/libdevmapper.cpp
@@ -1,4 +1,4 @@
-/* $Id: libdevmapper.cpp 98717 2023-02-24 11:04:07Z alexander.eichner@oracle.com $ */
+/* $Id: libdevmapper.cpp 98721 2023-02-24 12:32:25Z alexander.eichner@oracle.com $ */
 /** @file
  *
  * Module to dynamically load libdevmapper and load all symbols
@@ -24,16 +24,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <https://www.gnu.org/licenses>.
  *
- * The contents of this file may alternatively be used under the terms
- * of the Common Development and Distribution License Version 1.0
- * (CDDL), a copy of it is provided in the "COPYING.CDDL" file included
- * in the VirtualBox distribution, in which case the provisions of the
- * CDDL are applicable instead of those of the GPL.
- *
- * You may elect to license modified versions of this file under the
- * terms and conditions of either the GPL or the CDDL or both.
- *
- * SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
+ * SPDX-License-Identifier: GPL-3.0-only
  */
 
 #define LOG_GROUP RTLOGGROUP_LDR
