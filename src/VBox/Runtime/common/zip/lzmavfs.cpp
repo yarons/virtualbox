f59da44f046797771777e8115180b60a9c3d12fd
Alexander Eichner <alexander.eichner@oracle.com>
2023-02-28 08:56:51 +0000

    Runtime/lzmavfs.cpp: Need to include asm.h for RT_LE2H_U32 to work, bugref:10254
    svn:sync-xref-src-repo-rev: r156073

--- src/VBox/Runtime/common/zip/lzmavfs.cpp
+++ src/VBox/Runtime/common/zip/lzmavfs.cpp
@@ -1,4 +1,4 @@
-/* $Id: lzmavfs.cpp 98750 2023-02-27 14:45:01Z alexander.eichner@oracle.com $ */
+/* $Id: lzmavfs.cpp 98766 2023-02-28 08:56:51Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT - XZ/LZMA Compressor and Decompressor I/O Stream.
  */
@@ -41,6 +41,7 @@
 #include "internal/iprt.h"
 #include <iprt/zip.h>
 
+#include <iprt/asm.h>
 #include <iprt/assert.h>
 #include <iprt/crc.h>
 #include <iprt/ctype.h>
