e9f329641b7d0c0234e3311679b4ecbb40a936db
Andreas Loeffler <andreas.loeffler@oracle.com>
2023-03-23 08:44:26 +0000

    Validation Kit/Guest Control: Adjusted API calls + added testcases for setting the current working directory [build fix]. bugref:8053
    svn:sync-xref-src-repo-rev: r156494

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -37,7 +37,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 99128 $"
+__version__ = "$Revision: 99129 $"
 
 # Standard Python imports.
 import errno
@@ -3060,7 +3060,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
         sShell          = self.oTstDrv.getGuestSystemShell(oTestVm);
         sShellOpt       = '/C' if oTestVm.isWindows() or oTestVm.isOS2() else '-c';
         sSystemDir      = self.oTstDrv.getGuestSystemDir(oTestVm);
-        sTempDir        = self oTstDrv.getGuestTempDir(oTestVm);
+        sTempDir        = self.oTstDrv.getGuestTempDir(oTestVm);
         sFileForReading = self.oTstDrv.getGuestSystemFileForReading(oTestVm);
         if oTestVm.isWindows() or oTestVm.isOS2():
             sImageOut = self.oTstDrv.getGuestSystemShell(oTestVm);
