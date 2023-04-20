e2b70893e091cd6e554bb8ac2e667cc493214d52
Andreas Loeffler <andreas.loeffler@oracle.com>
2023-04-20 12:59:13 +0000

    Validation Kit/tdUnitTest1.py: Disabled running tstIntNet-1, tstLow and tstPin on darwin.arm64, as those are not supported (yet).
    svn:sync-xref-src-repo-rev: r156980

--- src/VBox/ValidationKit/tests/unittests/tdUnitTest1.py
+++ src/VBox/ValidationKit/tests/unittests/tdUnitTest1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdUnitTest1.py 99114 2023-03-22 13:09:16Z andreas.loeffler@oracle.com $
+# $Id: tdUnitTest1.py 99487 2023-04-20 12:59:13Z andreas.loeffler@oracle.com $
 
 """
 VirtualBox Validation Kit - Unit Tests.
@@ -37,7 +37,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 99114 $"
+__version__ = "$Revision: 99487 $"
 
 
 # Standard Python imports.
@@ -79,6 +79,9 @@ class tdUnitTest1(vbox.TestDriver):
             'testcase/tstAsmStructs': '',               # Fails on arm64 due to different sizes, also not required as there is no
                                                         # assembly code which needs to match with structs.
             'testcase/tstDarwinKeyboard': '',           # Fails for unknown reason.
+            'testcase/tstIntNet-1': '',                 # Not supported on arm64 right now.
+            'testcase/tstLow': '',                      # Ditto.
+            'testcase/tstPin': '',                      # Ditto.
         },
         'linux': {
             'testcase/tstRTFileAio': '',                # See xTracker #8035.
