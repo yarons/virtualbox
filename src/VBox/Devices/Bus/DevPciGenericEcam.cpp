83c14b8ccf58ff5aed6ba5dd45f71de942dd8422
Alexander Eichner <alexander.eichner@oracle.com>
2023-05-11 13:43:32 +0000

    Devices/Bus: Started a basic PCI bus implementation suitable for ARMv8, devices are detected by a Linux guest but interrupts don't work right now. The implementation shares most code with ICH9 PCI device, bugref:10445 [scm]
    svn:sync-xref-src-repo-rev: r157334

--- src/VBox/Devices/Bus/DevPciGenericEcam.cpp
+++ src/VBox/Devices/Bus/DevPciGenericEcam.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevPciGenericEcam.cpp 99750 2023-05-11 13:37:24Z alexander.eichner@oracle.com $ */
+/* $Id: DevPciGenericEcam.cpp 99752 2023-05-11 13:43:32Z alexander.eichner@oracle.com $ */
 /** @file
  * DevPciGeneric - Generic host to PCIe bridge emulation.
  */
@@ -61,6 +61,7 @@
 *********************************************************************************************************************************/
 /** Saved state version of the generic ECAM PCI bus device. */
 #define VBOX_PCIGENECAM_SAVED_STATE_VERSION             1
+
 
 /*********************************************************************************************************************************
 *   Internal Functions                                                                                                           *
