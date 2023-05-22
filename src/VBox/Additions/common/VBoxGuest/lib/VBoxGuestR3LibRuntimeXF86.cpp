4ec9af03e398f9a71eb02952a97965c2292dfa95
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-05-22 11:20:19 +0000

    Add/Vbgl: Workaround for gcc 13.1.1 and -ffreestanding BoxGuestR3LibRuntimeXF86.cpp. [correction]
    svn:sync-xref-src-repo-rev: r157535

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibRuntimeXF86.cpp
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibRuntimeXF86.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR3LibRuntimeXF86.cpp 99876 2023-05-20 01:02:14Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxGuestR3LibRuntimeXF86.cpp 99892 2023-05-22 11:20:19Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxGuestR3Lib - Ring-3 Support Library for VirtualBox guest additions,
  *                  implements the minimum of runtime functions needed for
@@ -46,7 +46,7 @@
 #if defined(VBOX_VBGLR3_XFREE86)
 extern "C" {
 # define XFree86LOADER
-# ifdef RT_GNUC_PREREQ(13,0) /* cmath gets dragged in and the c++/13/cmath header is allergic to -ffreestanding.  */
+# if RT_GNUC_PREREQ(13,0) /* cmath gets dragged in and the c++/13/cmath header is allergic to -ffreestanding.  */
 #  define _GLIBCXX_INCLUDE_NEXT_C_HEADERS
 # endif
 # include <xf86_ansic.h>
