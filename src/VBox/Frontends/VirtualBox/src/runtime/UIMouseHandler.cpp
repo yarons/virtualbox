4ce6cc8654841df5e0e308b08b7bddb221ab7c9f
Sergey Dubov <sergey.dubov@oracle.com>
2023-06-01 11:20:25 +0000

    FE/Qt: bugref:10450: Runtime UI: More remaining bits fixing Qt6 compatibility for UIMouseHandler, s.a. r157476.
    svn:sync-xref-src-repo-rev: r157712

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMouseHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMouseHandler.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMouseHandler.cpp 99839 2023-05-18 10:21:44Z sergey.dubov@oracle.com $ */
+/* $Id: UIMouseHandler.cpp 100034 2023-06-01 11:20:25Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIMouseHandler class implementation.
  */
@@ -1212,15 +1212,25 @@ bool UIMouseHandler::multiTouchEvent(QTouchEvent *pTouchEvent, ulong uScreenId)
 
     /* Pass all multi-touch events into guest: */
     int iTouchPointIndex = 0;
+#ifdef VBOX_IS_QT6_OR_LATER /* QTouchEvent::TouchPoint was replaced by QEventPoint in 6.0 */
+    foreach (const QEventPoint &touchPoint, pTouchEvent->points())
+#else
     foreach (const QTouchEvent::TouchPoint &touchPoint, pTouchEvent->touchPoints())
+#endif
     {
         /* Get touch-point state: */
         LONG iTouchPointState = KTouchContactState_None;
         switch (touchPoint.state())
         {
+#ifdef VBOX_IS_QT6_OR_LATER /* QTouchEvent::TouchPoint was replaced by QEventPoint in 6.0 */
+            case QEventPoint::Pressed:
+            case QEventPoint::Updated:
+            case QEventPoint::Stationary:
+#else
             case Qt::TouchPointPressed:
             case Qt::TouchPointMoved:
             case Qt::TouchPointStationary:
+#endif
                 iTouchPointState = KTouchContactState_InContact;
                 if (fTouchScreen)
                     iTouchPointState |= KTouchContactState_InRange;
