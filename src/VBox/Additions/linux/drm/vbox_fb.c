38d0a4f3cf5dfeb1348b6d6bd3c9b8975386cbbb
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2023-06-19 19:06:19 +0000

    Additions: Linux: vboxvideo: fix WARNING for EL 8.8 kernel which has backports from 5.19, bugref:10237.
    svn:sync-xref-src-repo-rev: r157929

--- src/VBox/Additions/linux/drm/vbox_fb.c
+++ src/VBox/Additions/linux/drm/vbox_fb.c
@@ -1,4 +1,4 @@
-/* $Id: vbox_fb.c 98870 2023-03-07 17:28:03Z vadim.galitsyn@oracle.com $ */
+/* $Id: vbox_fb.c 100219 2023-06-19 19:06:19Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -374,7 +374,7 @@ static int vboxfb_create(struct drm_fb_helper *helper,
 	info->screen_size = size;
 
 #ifdef CONFIG_FB_DEFERRED_IO
-# if RTLNX_VER_MIN(5,19,0)
+# if RTLNX_VER_MIN(5,19,0) || RTLNX_RHEL_RANGE(8,8, 8,99)
 	info->fix.smem_len = info->screen_size;
 # endif
 	info->fbdefio = &vbox_defio;
