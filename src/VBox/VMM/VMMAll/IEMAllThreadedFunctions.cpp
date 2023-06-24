fe7f78e3f270de92a7eb91aed1205d0e166af0f2
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-06-24 02:48:28 +0000

    VMM/IEM: Reworked the PC -> phys address translation in the recompiler. Generate CS.LIM checks if in the last page of a segment. (todo page crossing)  bugref:10369
    svn:sync-xref-src-repo-rev: r157992

--- src/VBox/VMM/VMMAll/IEMAllThreadedFunctions.cpp
+++ src/VBox/VMM/VMMAll/IEMAllThreadedFunctions.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAllThreadedFunctions.cpp 100231 2023-06-20 23:10:27Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllThreadedFunctions.cpp 100277 2023-06-24 02:48:28Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Emulation, Threaded Functions.
  */
@@ -607,6 +607,23 @@ static IEM_DECL_IMPL_DEF(VBOXSTRICTRC, iemThreadedFunc_BltIn_CheckMode,
            fExpectedExec, pVCpu->iem.s.fExec, fExpectedExec ^ pVCpu->iem.s.fExec));
     RT_NOREF(uParam1, uParam2);
     return VINF_IEM_REEXEC_MODE_CHANGED;
+}
+
+
+/**
+ * Built-in function that checks the EIP/IP + uParam0 is within CS.LIM,
+ * raising a \#GP(0) if this isn't the case.
+ */
+static IEM_DECL_IMPL_DEF(VBOXSTRICTRC, iemThreadedFunc_BltIn_CheckCsLim,
+                         (PVMCPU pVCpu, uint64_t uParam0, uint64_t uParam1, uint64_t uParam2))
+{
+    uint32_t const cbInstr = (uint32_t)uParam0;
+    if (pVCpu->cpum.GstCtx.eip - pVCpu->cpum.GstCtx.cs.u32Limit >= cbInstr)
+        return VINF_SUCCESS;
+    Log(("EIP out of bounds at %04x:%08RX32 LB %u - CS.LIM=%#RX32\n",
+         pVCpu->cpum.GstCtx.cs.Sel, pVCpu->cpum.GstCtx.eip, cbInstr, pVCpu->cpum.GstCtx.cs.u32Limit));
+    RT_NOREF(uParam1, uParam2);
+    return iemRaiseGeneralProtectionFault0(pVCpu);
 }
 
 /*
