3e5eacf540f07830999438adf42eea4aea79720b
Andreas Loeffler <andreas.loeffler@oracle.com>
2023-07-06 14:08:30 +0000

    Shared Clipboard: Windows host service now uses the same way as the Linux host service wrt transfer handling. ​​​bugref:9437
    svn:sync-xref-src-repo-rev: r158152

--- src/VBox/Additions/WINNT/VBoxTray/VBoxClipboard.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxClipboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxClipboard.cpp 100407 2023-07-06 11:18:29Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxClipboard.cpp 100412 2023-07-06 14:08:30Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxClipboard - Shared clipboard, Windows Guest Implementation.
  */
@@ -205,12 +205,26 @@ static DECLCALLBACK(void) vbtrShClTransferInitializedCallback(PSHCLTRANSFERCALLB
 
     switch(ShClTransferGetDir(pCbCtx->pTransfer))
     {
-        case SHCLTRANSFERDIR_FROM_REMOTE:
+        case SHCLTRANSFERDIR_TO_REMOTE: /* G->H */
         {
-            AssertPtrBreak(pCtx->Win.pDataObjInFlight);
-            rc = pCtx->Win.pDataObjInFlight->SetTransfer(pCbCtx->pTransfer);
-            if (RT_SUCCESS(rc))
-                rc = pCtx->Win.pDataObjInFlight->SetStatus(SharedClipboardWinDataObject::Running);
+            rc = SharedClipboardWinTransferGetRootsFromClipboard(&pCtx->Win, pCbCtx->pTransfer);
+            break;
+        }
+
+        case SHCLTRANSFERDIR_FROM_REMOTE: /* H->G */
+        {
+            SharedClipboardWinDataObject *pObj = pCtx->Win.pDataObjInFlight;
+            if (pObj)
+            {
+                rc = pObj->SetTransfer(pCbCtx->pTransfer);
+                if (RT_SUCCESS(rc))
+                    rc = pObj->SetStatus(SharedClipboardWinDataObject::Running);
+
+                pCtx->Win.pDataObjInFlight = NULL; /* Hand off to Windows. */
+            }
+            else
+                AssertMsgFailed(("No data object in flight!\n"));
+
             break;
         }
 
