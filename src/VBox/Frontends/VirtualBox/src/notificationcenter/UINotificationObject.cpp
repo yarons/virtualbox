20a8cd5596d01e87907d016f6b3e8a2681c3267f
Sergey Dubov <sergey.dubov@oracle.com>
2023-07-14 17:02:43 +0000

    FE/Qt: bugref:10323: Runtime UI: Close downloaders such as GA, ExtPack and UserManual on completion; Similar to what we do with other types of progresses.
    svn:sync-xref-src-repo-rev: r158420

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObject.cpp
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObject.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINotificationObject.cpp 99184 2023-03-27 11:05:18Z sergey.dubov@oracle.com $ */
+/* $Id: UINotificationObject.cpp 100590 2023-07-14 17:02:43Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UINotificationObject class implementation.
  */
@@ -336,6 +336,14 @@ void UINotificationDownloader::sltHandleProgressFinished()
     m_pDownloader = 0;
     m_fDone = true;
     emit sigProgressFinished();
+
+    /* If there was no error and no reason to keep progress alive, - finish him! */
+    if (   error().isEmpty()
+#ifdef VBOX_NOTIFICATION_CENTER_WITH_KEEP_BUTTON
+        && !gEDataManager->keepSuccessfullNotificationProgresses()
+#endif
+        )
+        close();
 }
 
 #endif /* VBOX_GUI_WITH_NETWORK_MANAGER */
