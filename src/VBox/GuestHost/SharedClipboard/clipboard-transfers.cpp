82ed8e934d145859ff27ce3c9a15c4d77451b619
Andreas Loeffler <andreas.loeffler@oracle.com>
2023-07-18 16:12:28 +0000

    Shared Clipboard: Relaxed check of shClTransferCancelOrError() a bit, so that it can be called if already cancelled. bugref:9437
    svn:sync-xref-src-repo-rev: r158471

--- src/VBox/GuestHost/SharedClipboard/clipboard-transfers.cpp
+++ src/VBox/GuestHost/SharedClipboard/clipboard-transfers.cpp
@@ -1,4 +1,4 @@
-/* $Id: clipboard-transfers.cpp 100636 2023-07-18 15:59:11Z andreas.loeffler@oracle.com $ */
+/* $Id: clipboard-transfers.cpp 100637 2023-07-18 16:12:28Z andreas.loeffler@oracle.com $ */
 /** @file
  * Shared Clipboard: Common clipboard transfer handling code.
  */
@@ -2257,25 +2257,27 @@ static int shClTransferCancelOrError(PSHCLTRANSFER pTransfer, int rc)
 
     shClTransferLock(pTransfer);
 
-    AssertMsgReturnStmt(pTransfer->State.enmStatus == SHCLTRANSFERSTATUS_STARTED,
-                        ("Wrong status (currently is %s)\n", ShClTransferStatusToStr(pTransfer->State.enmStatus)),
-                        shClTransferUnlock(pTransfer), VERR_WRONG_ORDER);
-
     int rc2;
-    if (rc == VERR_CANCELLED)
-    {
-        rc2 = shClTransferSetStatus(pTransfer, SHCLTRANSFERSTATUS_CANCELED);
 
-        if (pTransfer->Callbacks.pfnOnCompleted)
-            pTransfer->Callbacks.pfnOnCompleted(&pTransfer->CallbackCtx, VERR_CANCELLED);
-    }
-    else
+    if (pTransfer->State.enmStatus == SHCLTRANSFERSTATUS_STARTED)
     {
-        rc2 = shClTransferSetStatus(pTransfer, SHCLTRANSFERSTATUS_ERROR);
+        if (rc == VERR_CANCELLED)
+        {
+            rc2 = shClTransferSetStatus(pTransfer, SHCLTRANSFERSTATUS_CANCELED);
 
-        if (pTransfer->Callbacks.pfnOnError)
-            pTransfer->Callbacks.pfnOnError(&pTransfer->CallbackCtx, rc);
+            if (pTransfer->Callbacks.pfnOnCompleted)
+                pTransfer->Callbacks.pfnOnCompleted(&pTransfer->CallbackCtx, VERR_CANCELLED);
+        }
+        else
+        {
+            rc2 = shClTransferSetStatus(pTransfer, SHCLTRANSFERSTATUS_ERROR);
+
+            if (pTransfer->Callbacks.pfnOnError)
+                pTransfer->Callbacks.pfnOnError(&pTransfer->CallbackCtx, rc);
+        }
     }
+    else /* Nothing to do. */
+        rc2 = VINF_SUCCESS;
 
     shClTransferUnlock(pTransfer);
 
