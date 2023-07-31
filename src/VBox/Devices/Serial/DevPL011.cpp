538978e735ddaba373e0975e54ae01aaa8ca29b3
Alexander Eichner <alexander.eichner@oracle.com>
2023-07-31 12:59:20 +0000

    Devices/DevPL011: Add saved state support (untested), bugref:10403 [fix]
    svn:sync-xref-src-repo-rev: r158620

--- src/VBox/Devices/Serial/DevPL011.cpp
+++ src/VBox/Devices/Serial/DevPL011.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevPL011.cpp 100749 2023-07-31 12:58:14Z alexander.eichner@oracle.com $ */
+/* $Id: DevPL011.cpp 100750 2023-07-31 12:59:20Z alexander.eichner@oracle.com $ */
 /** @file
  * DevSerialPL011 - ARM PL011 PrimeCell UART.
  *
@@ -1451,7 +1451,7 @@ static DECLCALLBACK(int) pl011R3LoadExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM, ui
     rc = pHlp->pfnSSMGetGCPhys(pSSM, &GCPhysMmioBase);
     AssertRCReturn(rc, rc);
     if (GCPhysMmioBase != pThis->GCPhysMmioBase)
-        return pHlp->pfnSSMSetCfgError(pSSM, RT_SRC_POS, N_("Config mismatch - GCPhysMmioBase: saved=%RTiop config=%RTiop"), GCPhysMmioBase, pThis->GCPhysMmioBase);
+        return pHlp->pfnSSMSetCfgError(pSSM, RT_SRC_POS, N_("Config mismatch - GCPhysMmioBase: saved=%RGp config=%RGp"), GCPhysMmioBase, pThis->GCPhysMmioBase);
 
     if (uPass != SSM_PASS_FINAL)
         return VINF_SUCCESS;
