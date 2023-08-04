6b7cf2b0d3be144c353f6a1a358dd72e74c849de
Dmitrii Grigorev <dmitrii.grigorev@oracle.com>
2023-08-04 11:22:21 +0000

    ValKit: Use SATA Controller for tst-win11-64-efi VM (build fix)
    svn:sync-xref-src-repo-rev: r158688

--- src/VBox/ValidationKit/testdriver/vboxtestvms.py
+++ src/VBox/ValidationKit/testdriver/vboxtestvms.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# $Id: vboxtestvms.py 100796 2023-08-04 11:15:22Z dmitrii.grigorev@oracle.com $
+# $Id: vboxtestvms.py 100797 2023-08-04 11:22:21Z dmitrii.grigorev@oracle.com $
 
 """
 VirtualBox Test VMs
@@ -36,7 +36,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 100796 $"
+__version__ = "$Revision: 100797 $"
 
 # Standard Python imports.
 import copy;
@@ -2083,7 +2083,7 @@ class TestVmManager(object):
 
         # W11
         TestVm('tst-win11-64-efi',           kfGrpStdSmoke,       sHd = '7.0/win11/win11-64-edited.vdi',
-               sKind = 'Windows11_64', acCpusSup = range(1, 33), fIoApic = True, sFirmwareType = 'efi', 
+               sKind = 'Windows11_64', acCpusSup = range(1, 33), fIoApic = True, sFirmwareType = 'efi',
                sHddControllerType = 'SATA Controller'),
 
         # Nested hardware-virtualization
