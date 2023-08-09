0080f0faa914ab83a9aa1b65798daad26e35e6db
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-08-09 22:58:30 +0000

    VMM/IEM: Fixed a 32-bit xchg regression from r158734. bugref:10369
    svn:sync-xref-src-repo-rev: r158747

--- src/VBox/VMM/VMMAll/IEMAllInstOneByte.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllInstOneByte.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllInstOneByte.cpp.h 100839 2023-08-09 17:30:14Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllInstOneByte.cpp.h 100846 2023-08-09 22:58:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Emulation.
  */
@@ -5313,7 +5313,6 @@ FNIEMOP_DEF(iemOp_xchg_Ev_Gv)
                 IEM_MC_MEM_COMMIT_AND_UNMAP_RW(pu32Mem, bUnmapInfo);
                 IEM_MC_STORE_GREG_U32(IEM_GET_MODRM_REG(pVCpu, bRm), uTmpReg);
 
-                IEM_MC_CLEAR_HIGH_GREG_U64_BY_REF(pu32Reg);
                 IEM_MC_ADVANCE_RIP_AND_FINISH();
                 IEM_MC_END();
                 break;
