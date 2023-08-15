df17ff255ed2ebe788b49967211a5397d44a50b1
Alexander Eichner <alexander.eichner@oracle.com>
2023-08-15 08:24:46 +0000

    VMM/NEMR3Native-darwin: Disable intercepting #DB exceptions and mov drX instructions as it currently breaks using a debugger inside the guest. This will break using the hypervisor debugger but only very few people use it anyway and even less on macOS, bugref:10504 [doxygen]
    svn:sync-xref-src-repo-rev: r158797

--- src/VBox/VMM/VMMR3/NEMR3Native-darwin.cpp
+++ src/VBox/VMM/VMMR3/NEMR3Native-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: NEMR3Native-darwin.cpp 100877 2023-08-15 08:13:22Z alexander.eichner@oracle.com $ */
+/* $Id: NEMR3Native-darwin.cpp 100878 2023-08-15 08:24:46Z alexander.eichner@oracle.com $ */
 /** @file
  * NEM - Native execution manager, native ring-3 macOS backend using Hypervisor.framework.
  *
@@ -2800,7 +2800,7 @@ static void nemR3DarwinVmxSetupVmcsXcptBitmap(PVMCPUCC pVCpu, PVMXVMCSINFO pVmcs
      *       recursive #DBs can cause a CPU hang.
      */
     /** @todo The DRx handling is not quite correct breaking debugging inside the guest with gdb,
-     * see @ticketref{21413} and @ticketref{21546}, so intercepting #DB is disabled for now. See @bugref{10504}
+     * see @ticketref{21413} and @ticketref{21546}, so intercepting \#DB is disabled for now. See @bugref{10504}
      * as well. This will break the hypervisor debugger but only very few people use it and even less on macOS
      * using the NEM backend.
      */
