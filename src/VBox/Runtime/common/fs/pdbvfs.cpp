e535ab930a697ce113959bf2e71a8337b241a91d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-08-22 22:56:32 +0000

    IPRT/dbg: Rewroked the CV/DBG+PDB loading so that we support loading the PDB linked to by the windows 2000 area DBG files. Merged the PDB section loading code with the existing DBG one.
    svn:sync-xref-src-repo-rev: r158886

--- src/VBox/Runtime/common/fs/pdbvfs.cpp
+++ src/VBox/Runtime/common/fs/pdbvfs.cpp
@@ -1,4 +1,4 @@
-/* $Id: pdbvfs.cpp 100914 2023-08-21 02:32:45Z knut.osmundsen@oracle.com $ */
+/* $Id: pdbvfs.cpp 100948 2023-08-22 22:56:32Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - PDB Virtual Filesystem (read only).
  */
@@ -1631,7 +1631,7 @@ static int rtFsPdbVolTryInit(PRTFSPDBVOL pThis, RTVFS hVfsSelf, RTVFSFILE hVfsBa
         offRootPageMap    = RT_UOFFSETOF(RTPDB20HDR, aiRootPageMap);
     }
     else
-        return RTERRINFO_LOG_SET_F(pErrInfo, rc, "Unknown file header signature: %.44Rhxs", Buf.Hdr70.szSignature);
+        return RTERRINFO_LOG_SET_F(pErrInfo, VERR_INVALID_MAGIC, "Unknown file header signature: %.44Rhxs", Buf.Hdr70.szSignature);
 
     if (   pThis->cbPage != _4K
         && pThis->cbPage != _8K
@@ -1807,15 +1807,6 @@ static int rtFsPdbVolTryInit(PRTFSPDBVOL pThis, RTVFS hVfsSelf, RTVFSFILE hVfsBa
 }
 
 
-/**
- * Opens an PDB file system volume.
- *
- * @returns IPRT status code.
- * @param   hVfsFileIn      The file or device backing the volume.
- * @param   fFlags          0 or RTFSPDB_F_NO_NAMES
- * @param   phVfs           Where to return the virtual file system handle.
- * @param   pErrInfo        Where to return additional error information.
- */
 RTDECL(int) RTFsPdbVolOpen(RTVFSFILE hVfsFileIn, uint32_t fFlags, PRTVFS phVfs, PRTERRINFO pErrInfo)
 {
     /*
