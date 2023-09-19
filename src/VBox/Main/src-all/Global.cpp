af57ee86789c3b9285f7def1399cc1abb269f3e6
Andreas Loeffler <andreas.loeffler@oracle.com>
2023-09-19 17:01:07 +0000

    Main: Added first two generic guest OS types for ARM (as "Other", as we also do for x86). bugref:10384
    svn:sync-xref-src-repo-rev: r159151

--- src/VBox/Main/src-all/Global.cpp
+++ src/VBox/Main/src-all/Global.cpp
@@ -1,4 +1,4 @@
-/* $Id: Global.cpp 101164 2023-09-19 10:01:39Z brent.paulson@oracle.com $ */
+/* $Id: Global.cpp 101178 2023-09-19 17:01:07Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox COM global definitions
  *
@@ -57,6 +57,18 @@ const Global::OSType Global::sOSTypes[] =
       VBOXOSTYPE_Unknown_x64,     VBOXOSHINT_64BIT | VBOXOSHINT_PAE | VBOXOSHINT_HWVIRTEX | VBOXOSHINT_IOAPIC,
       1,   64,   4,  2 * _1G64, GraphicsControllerType_VBoxVGA, NetworkAdapterType_Am79C973, 0, StorageControllerType_PIIX4, StorageBus_IDE,
       StorageControllerType_PIIX4, StorageBus_IDE, ChipsetType_PIIX3, IommuType_None, AudioControllerType_AC97, AudioCodecType_STAC9700 },
+
+#ifdef VBOX_WITH_VIRT_ARMV8
+    { "Other",   "Other",             "",               "Other_ARM",          "Other/Unknown (ARM)",
+      VBOXOSTYPE_Unknown_arm32,       VBOXOSHINT_NONE | VBOXOSHINT_EFI,
+      1,   64,   4,  2 * _1G64, GraphicsControllerType_VBoxSVGA, NetworkAdapterType_I82540EM, 0, StorageControllerType_VirtioSCSI, StorageBus_VirtioSCSI,
+      StorageControllerType_VirtioSCSI, StorageBus_VirtioSCSI, ChipsetType_ARMv8Virtual, IommuType_None, AudioControllerType_VirtioSound, AudioCodecType_Null },
+
+    { "Other",   "Other",             "",               "Other_ARM64",        "Other/Unknown (ARM 64-bit)",
+      VBOXOSTYPE_Unknown_arm64,       VBOXOSHINT_64BIT | VBOXOSHINT_EFI,
+      1,   64,   4,  2 * _1G64, GraphicsControllerType_VBoxSVGA, NetworkAdapterType_I82540EM, 0, StorageControllerType_VirtioSCSI, StorageBus_VirtioSCSI,
+      StorageControllerType_VirtioSCSI, StorageBus_VirtioSCSI, ChipsetType_ARMv8Virtual, IommuType_None, AudioControllerType_VirtioSound, AudioCodecType_Null },
+#endif
 
     { "Windows", "Microsoft Windows", "",               "Windows31",          "Windows 3.1",
       VBOXOSTYPE_Win31,           VBOXOSHINT_FLOPPY,
