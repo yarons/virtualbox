cf9ff5f08ed9038595598ac215751183bf2f7f7e
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2023-09-22 16:38:35 +0000

    FE/Qt: bugref:10523. Select Oracle Linux by default when OS family is Linux.
    svn:sync-xref-src-repo-rev: r159227

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UINameAndSystemEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UINameAndSystemEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINameAndSystemEditor.cpp 101242 2023-09-22 15:41:24Z serkan.bayraktar@oracle.com $ */
+/* $Id: UINameAndSystemEditor.cpp 101244 2023-09-22 16:38:35Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UINameAndSystemEditor class implementation.
  */
@@ -447,10 +447,18 @@ void UINameAndSystemEditor::sltFamilyChanged(int index)
     }
     else
     {
+        /* Populate variant combo: */
+        /* If family is Linux then select Oracle Linux as variant: */
+        int iOracleIndex = -1;
         foreach (const QString &strVariant, variantList)
         {
             m_pComboVariant->addItem(strVariant);
+            if (strVariant == "Oracle Linux")
+                iOracleIndex = m_pComboVariant->count() - 1;
         }
+        if (iOracleIndex != -1)
+            m_pComboVariant->setCurrentIndex(iOracleIndex);
+
         populateTypeCombo(pGuestOSTypeManager->getTypeListForVariant(m_pComboVariant->currentText()));
     }
     m_pComboVariant->blockSignals(false);
