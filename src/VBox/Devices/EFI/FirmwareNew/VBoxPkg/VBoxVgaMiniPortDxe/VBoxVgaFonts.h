198329ddb68c539e196e9f06ed37c44f683437a4
Alexander Eichner <alexander.eichner@oracle.com>
2023-09-27 08:54:37 +0000

    EFI/Firmware: Preparing for updating to edk2-stable202308, bugref:4643
    svn:sync-xref-src-repo-rev: r159268

---
+++ src/VBox/Devices/EFI/FirmwareNew/VBoxPkg/VBoxVgaMiniPortDxe/VBoxVgaFonts.h
@@ -0,0 +1,13 @@
+/* $Id: VBoxVgaFonts.h 101283 2023-09-27 08:54:37Z alexander.eichner@oracle.com $ */
+/** @file
+ * Some of the VGA-ROM fonts from ftp://ftp.simtel.net/pub/simtelnet/msdos/screen/fntcol16.zip .
+ * The package is (C) Joseph (Yossi) Gil.
+ * The individual fonts are in the public domain.
+ *
+ * Note. These fonts are also used by the VGA BIOS.
+ */
+
+#include "VBoxVgaFont-8x8.h"
+#include "VBoxVgaFont-8x14.h"
+#include "VBoxVgaFont-8x16.h"
+
