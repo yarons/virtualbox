9d1503bc23f8cb907897af5300037bfad646f626
Sergey Dubov <sergey.dubov@oracle.com>
2023-10-03 14:23:38 +0000

    FE/Qt: bugref:10513: Small layout adjustment for VM settings / Storage page; This one prevents page size-hint blinking while changing selection from controller to attachment for the 1st time.
    svn:sync-xref-src-repo-rev: r159332

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UIStorageSettingsEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UIStorageSettingsEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIStorageSettingsEditor.cpp 101331 2023-10-03 14:21:11Z sergey.dubov@oracle.com $ */
+/* $Id: UIStorageSettingsEditor.cpp 101332 2023-10-03 14:23:38Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIStorageSettingsEditor class implementation.
  */
@@ -4683,22 +4683,34 @@ void UIStorageSettingsEditor::prepareAttachmentWidget()
                 /* Prepare attachment passthrough check-box: */
                 m_pCheckBoxPassthrough = new QCheckBox(pWidgetAttachment);
                 if (m_pCheckBoxPassthrough)
+                {
+                    m_pCheckBoxPassthrough->hide();
                     pLayoutAttachmentSettings->addWidget(m_pCheckBoxPassthrough);
+                }
 
                 /* Prepare attachment temporary eject check-box: */
                 m_pCheckBoxTempEject = new QCheckBox(pWidgetAttachment);
                 if (m_pCheckBoxTempEject)
+                {
+                    m_pCheckBoxTempEject->hide();
                     pLayoutAttachmentSettings->addWidget(m_pCheckBoxTempEject);
+                }
 
                 /* Prepare attachment non rotational check-box: */
                 m_pCheckBoxNonRotational = new QCheckBox(pWidgetAttachment);
                 if (m_pCheckBoxNonRotational)
+                {
+                    m_pCheckBoxNonRotational->hide();
                     pLayoutAttachmentSettings->addWidget(m_pCheckBoxNonRotational);
+                }
 
                 /* Prepare attachment hot pluggable check-box: */
                 m_pCheckBoxHotPluggable = new QCheckBox(pWidgetAttachment);
                 if (m_pCheckBoxHotPluggable)
+                {
+                    m_pCheckBoxHotPluggable->hide();
                     pLayoutAttachmentSettings->addWidget(m_pCheckBoxHotPluggable);
+                }
 
                 m_pLayoutAttachment->addLayout(pLayoutAttachmentSettings, 2, 2);
             }
