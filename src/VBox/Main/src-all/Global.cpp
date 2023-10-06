024aa3a96b55f729bd79d972e6f3046039f55b0f
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-10-06 01:23:31 +0000

    Main/Global.cpp: Move the macros for compose the x64/x86/arm64/arm32 guest OS IDs to the VirtualBox API header so the GUI and others can make use of them as well. bugref:10384
    svn:sync-xref-src-repo-rev: r159374

--- src/VBox/Main/src-all/Global.cpp
+++ src/VBox/Main/src-all/Global.cpp
@@ -1,4 +1,4 @@
-/* $Id: Global.cpp 101370 2023-10-06 01:23:09Z knut.osmundsen@oracle.com $ */
+/* $Id: Global.cpp 101371 2023-10-06 01:23:31Z knut.osmundsen@oracle.com $ */
 /** @file
  * VirtualBox COM global definitions
  *
@@ -43,20 +43,6 @@ DECLARE_TRANSLATION_CONTEXT(GlobalCtx);
 #define VBOX_OSTYPE_X64(a_OStype)       VBOXOSTYPE_ ## a_OStype ## _x64
 #define VBOX_OSTYPE_ARM32(a_OStype)     VBOXOSTYPE_ ## a_OStype ## _arm32
 #define VBOX_OSTYPE_ARM64(a_OStype)     VBOXOSTYPE_ ## a_OStype ## _arm64
-
-/** @todo r=bird: These macros should move to VBox/cdefs.h or a similarly easily
- *        accessible file, so that the GUI can make use of them too. */
-/* Note: Keep this naming for legacy reasons. Always meant x86 (32-bit). */
-#define GUEST_OS_ID_STR_X86(a_szOSid)     a_szOSid
-#if 0 /** @todo r=bird: revert to legacy naming: */
-#define GUEST_OS_ID_STR_X64(a_szOSid)     a_szOSid  "_64"
-#else
-#define GUEST_OS_ID_STR_X64(a_szOSid)     a_szOSid  "_x64"
-#endif
-/* New since VBox 7.1. */
-#define GUEST_OS_ID_STR_A32(a_szOSid)     a_szOSid  "_arm32"
-/* New since VBox 7.1. */
-#define GUEST_OS_ID_STR_A64(a_szOSid)     a_szOSid  "_arm64"
 
 /* static */
 const Global::OSType Global::sOSTypes[] =
