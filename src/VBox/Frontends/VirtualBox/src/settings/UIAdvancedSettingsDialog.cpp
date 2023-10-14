d22ae9832a28c4c8da5723db37841f6688578c43
Sergey Dubov <sergey.dubov@oracle.com>
2023-10-14 10:42:33 +0000

    FE/Qt: bugref:10513: UIAdvancedSettingsDialog: Painting fix for r159501.
    svn:sync-xref-src-repo-rev: r159505

--- src/VBox/Frontends/VirtualBox/src/settings/UIAdvancedSettingsDialog.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/UIAdvancedSettingsDialog.cpp
@@ -1,4 +1,4 @@
-﻿/* $Id: UIAdvancedSettingsDialog.cpp 101442 2023-10-13 16:08:20Z sergey.dubov@oracle.com $ */
+﻿/* $Id: UIAdvancedSettingsDialog.cpp 101446 2023-10-14 10:42:33Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIAdvancedSettingsDialog class implementation.
  */
@@ -297,8 +297,8 @@ void UIModeCheckBox::paintEvent(QPaintEvent *pEvent)
 
     /* Prepare left painting gradient: */
     const QColor backColor1 = pal.color(QPalette::Active, isChecked() ? QPalette::Window : QPalette::Highlight);
-    const QColor bcTone11 = backColor1.lighter(isChecked() ? 100 : 120);
-    const QColor bcTone12 = backColor1.lighter(isChecked() ? 120 : 140);
+    const QColor bcTone11 = backColor1.lighter(isChecked() ? 120 : 100);
+    const QColor bcTone12 = backColor1.lighter(isChecked() ? 140 : 120);
     QLinearGradient grad1(painterPath1.boundingRect().topLeft(), painterPath1.boundingRect().bottomRight());
     grad1.setColorAt(0, bcTone11);
     grad1.setColorAt(1, bcTone12);
