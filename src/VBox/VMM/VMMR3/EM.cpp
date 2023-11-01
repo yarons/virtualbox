597956cf8b120dac87546f58de5f5e9b601b2cd4
Alexander Eichner <alexander.eichner@oracle.com>
2023-11-01 15:34:19 +0000

    VMM/EM: Need to update the status code, bugref:10390
    svn:sync-xref-src-repo-rev: r159797

--- src/VBox/VMM/VMMR3/EM.cpp
+++ src/VBox/VMM/VMMR3/EM.cpp
@@ -1,4 +1,4 @@
-/* $Id: EM.cpp 101088 2023-09-12 10:22:20Z knut.osmundsen@oracle.com $ */
+/* $Id: EM.cpp 101703 2023-11-01 15:34:19Z alexander.eichner@oracle.com $ */
 /** @file
  * EM - Execution Monitor / Manager.
  */
@@ -1937,6 +1937,7 @@ int emR3ForcedActions(PVM pVM, PVMCPU pVCpu, int rc)
 
             fWakeupPending = true;
             rc2 = VINF_EM_RESCHEDULE;
+            UPDATE_RC();
         }
 #endif /* VBOX_VMM_TARGET_ARMV8 */
 
