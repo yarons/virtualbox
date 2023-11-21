8d72966703ff562d46bb4d1a59a52efde1a32c3e
Sergey Dubov <sergey.dubov@oracle.com>
2023-11-21 17:19:15 +0000

    FE/Qt: bugref:10513: Changed requirements for Global Preferences dialog; We've decided to hide shortcuts editor for Basic mode; Thus hiding Input settings page at all.
    svn:sync-xref-src-repo-rev: r160345

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UIShortcutConfigurationEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UIShortcutConfigurationEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIShortcutConfigurationEditor.cpp 101454 2023-10-16 16:10:42Z sergey.dubov@oracle.com $ */
+/* $Id: UIShortcutConfigurationEditor.cpp 102220 2023-11-21 17:19:15Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIShortcutConfigurationEditor class implementation.
  */
@@ -819,7 +819,7 @@ void UIShortcutConfigurationTable::cleanup()
 *********************************************************************************************************************************/
 
 UIShortcutConfigurationEditor::UIShortcutConfigurationEditor(QWidget *pParent /* = 0 */)
-    : UIEditor(pParent, true /* show in basic mode? */)
+    : UIEditor(pParent)
     , m_pModelManager(0)
     , m_pModelRuntime(0)
     , m_pTabWidget(0)
