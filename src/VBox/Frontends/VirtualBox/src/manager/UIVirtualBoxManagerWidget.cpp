4c1bb66b28ed5638239442c1c8fead1da8611b9c
Sergey Dubov <sergey.dubov@oracle.com>
2023-11-21 17:16:48 +0000

    FE/Qt: bugref:10513: Changed requirements for Basic set of Global/Machine tools; We've decided to bring global activity overview tools back to Basics; Besides that, we've decided to remove File Manager from Basic set of Machine tools.
    svn:sync-xref-src-repo-rev: r160344

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerWidget.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxManagerWidget.cpp 102124 2023-11-16 15:38:20Z sergey.dubov@oracle.com $ */
+/* $Id: UIVirtualBoxManagerWidget.cpp 102219 2023-11-21 17:16:48Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVirtualBoxManagerWidget class implementation.
  */
@@ -1130,15 +1130,15 @@ void UIVirtualBoxManagerWidget::cleanup()
 void UIVirtualBoxManagerWidget::updateToolsMenuGlobal()
 {
     /* Update global tools restrictions: */
-    QList<UIToolType> restrictedTypes;
+    QSet<UIToolType> restrictedTypes;
     const bool fExpertMode = gEDataManager->isSettingsInExpertMode();
     if (!fExpertMode)
         restrictedTypes << UIToolType_Media
-                        << UIToolType_Network
-                        << UIToolType_VMActivityOverview;
+                        << UIToolType_Network;
     if (restrictedTypes.contains(m_pMenuToolsGlobal->toolsType()))
         m_pMenuToolsGlobal->setToolsType(UIToolType_Welcome);
-    m_pMenuToolsGlobal->setRestrictedToolTypes(restrictedTypes);
+    const QList restrictions(restrictedTypes.begin(), restrictedTypes.end());
+    m_pMenuToolsGlobal->setRestrictedToolTypes(restrictions);
 
     /* Take restrictions into account, closing all restricted tools: */
     foreach (const UIToolType &enmRestrictedType, restrictedTypes)
@@ -1151,14 +1151,18 @@ void UIVirtualBoxManagerWidget::updateToolsMenuMachine(UIVirtualMachineItem *pIt
     const bool fCurrentItemIsOk = pItem && pItem->accessible();
 
     /* Update machine tools restrictions: */
-    QList<UIToolType> restrictedTypes;
+    QSet<UIToolType> restrictedTypes;
+    const bool fExpertMode = gEDataManager->isSettingsInExpertMode();
+    if (!fExpertMode)
+        restrictedTypes << UIToolType_FileManager;
     if (pItem && pItem->itemType() != UIVirtualMachineItemType_Local)
         restrictedTypes << UIToolType_Snapshots
                         << UIToolType_Logs
                         << UIToolType_FileManager;
     if (restrictedTypes.contains(m_pMenuToolsMachine->toolsType()))
         m_pMenuToolsMachine->setToolsType(UIToolType_Details);
-    m_pMenuToolsMachine->setRestrictedToolTypes(restrictedTypes);
+    const QList restrictions(restrictedTypes.begin(), restrictedTypes.end());
+    m_pMenuToolsMachine->setRestrictedToolTypes(restrictions);
     /* Update machine menu items availability: */
     m_pMenuToolsMachine->setItemsEnabled(fCurrentItemIsOk);
 
