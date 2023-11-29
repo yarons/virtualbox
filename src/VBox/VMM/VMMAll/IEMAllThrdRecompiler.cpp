f7d8939c2c1ba4788c3aeafcd3d93b4e0d91520d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-11-29 21:06:16 +0000

    VMM/IEM: Quick workaround for unaligned atomic instructions on arm64. bugref:10547
    svn:sync-xref-src-repo-rev: r160520

--- src/VBox/VMM/VMMAll/IEMAllThrdRecompiler.cpp
+++ src/VBox/VMM/VMMAll/IEMAllThrdRecompiler.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAllThrdRecompiler.cpp 102366 2023-11-28 14:22:42Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllThrdRecompiler.cpp 102384 2023-11-29 21:06:16Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Threaded Recompilation.
  *
@@ -2498,7 +2498,12 @@ VMM_INT_DECL(VBOXSTRICTRC) IEMExecRecompiler(PVMCC pVM, PVMCPUCC pVCpu)
     /*
      * Init the execution environment.
      */
-    iemInitExec(pVCpu, 0 /*fExecOpts*/);
+#ifdef RT_ARCH_ARM64 /** @todo ARM64: fix unaligned locked instructions properly. @bugreg{10547} */
+    if (pVM->cCpus == 1)
+        iemInitExec(pVCpu, IEM_F_X86_DISREGARD_LOCK /*fExecOpts*/);
+    else
+#endif
+        iemInitExec(pVCpu, 0 /*fExecOpts*/);
     if (RT_LIKELY(pVCpu->iem.s.msRecompilerPollNow != 0))
     { }
     else
