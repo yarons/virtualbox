581b058bd11c2125669c3a22793be9715b8426ff
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-11-29 22:20:04 +0000

    VMM/IEM: doxygen fix. bugref:10547
    svn:sync-xref-src-repo-rev: r160523

--- src/VBox/VMM/VMMAll/IEMAllThrdRecompiler.cpp
+++ src/VBox/VMM/VMMAll/IEMAllThrdRecompiler.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAllThrdRecompiler.cpp 102384 2023-11-29 21:06:16Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllThrdRecompiler.cpp 102387 2023-11-29 22:20:04Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Threaded Recompilation.
  *
@@ -2498,7 +2498,7 @@ VMM_INT_DECL(VBOXSTRICTRC) IEMExecRecompiler(PVMCC pVM, PVMCPUCC pVCpu)
     /*
      * Init the execution environment.
      */
-#ifdef RT_ARCH_ARM64 /** @todo ARM64: fix unaligned locked instructions properly. @bugreg{10547} */
+#ifdef RT_ARCH_ARM64 /** @todo ARM64: fix unaligned locked instructions properly. @bugref{10547} */
     if (pVM->cCpus == 1)
         iemInitExec(pVCpu, IEM_F_X86_DISREGARD_LOCK /*fExecOpts*/);
     else
