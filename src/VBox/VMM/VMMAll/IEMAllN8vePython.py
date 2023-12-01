fa6d5ff76f500be5fdf2a3f3cd5c1f49f36691be
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2023-12-01 23:00:26 +0000

    VMM/IEM: Continue refactoring IEM_MC_MEM_MAP into type specific MCs using bUnmapInfo. bugref:10371
    svn:sync-xref-src-repo-rev: r160568

--- src/VBox/VMM/VMMAll/IEMAllN8vePython.py
+++ src/VBox/VMM/VMMAll/IEMAllN8vePython.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: IEMAllN8vePython.py 102424 2023-12-01 22:43:39Z knut.osmundsen@oracle.com $
+# $Id: IEMAllN8vePython.py 102425 2023-12-01 23:00:26Z knut.osmundsen@oracle.com $
 # pylint: disable=invalid-name
 
 """
@@ -34,7 +34,7 @@ along with this program; if not, see <https://www.gnu.org/licenses>.
 
 SPDX-License-Identifier: GPL-3.0-only
 """
-__version__ = "$Revision: 102424 $"
+__version__ = "$Revision: 102425 $"
 
 # Standard python imports:
 import copy;
@@ -145,17 +145,19 @@ g_dMcStmtThreaded = {
     'IEM_MC_FETCH_MEM_FLAT_U128_AND_XREG_U128_AND_EAX_EDX_U32_SX_U64': (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_EX':                                    (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP':                                       (None, True,  False, ),
+    'IEM_MC_MEM_FLAT_MAP_I32_WO':                                (None, True,  False, ),
+    'IEM_MC_MEM_FLAT_MAP_I64_WO':                                (None, True,  False, ),
+    'IEM_MC_MEM_FLAT_MAP_R32_WO':                                (None, True,  False, ),
+    'IEM_MC_MEM_FLAT_MAP_R64_WO':                                (None, True,  False, ),
+    'IEM_MC_MEM_FLAT_MAP_R80_WO':                                (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_U16_RO':                                (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_U16_RW':                                (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_U32_RO':                                (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_U32_RW':                                (None, True,  False, ),
-    'IEM_MC_MEM_FLAT_MAP_I32_WO':                                (None, True,  False, ),
-    'IEM_MC_MEM_FLAT_MAP_R32_WO':                                (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_U64_RO':                                (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_U64_RW':                                (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_U8_RO':                                 (None, True,  False, ),
     'IEM_MC_MEM_FLAT_MAP_U8_RW':                                 (None, True,  False, ),
-    'IEM_MC_MEM_FLAT_MAP_R80_WO':                                (None, True,  False, ),
     'IEM_MC_STORE_MEM_FLAT_U128_ALIGN_SSE':                      (None, True,  False, ),
     'IEM_MC_STORE_MEM_FLAT_U128':                                (None, True,  False, ),
     'IEM_MC_STORE_MEM_FLAT_U16':                                 (None, True,  True,  ),
