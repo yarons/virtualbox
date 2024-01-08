f91b3fef001731a8c82d76ed2b836f389e0afffe
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-01-08 21:48:55 +0000

    ValKit/bs3-cpu-weird-1: Extended the PUSH/POP SReg testcase to cover all the basics. bugref:10371
    svn:sync-xref-src-repo-rev: r160970

--- src/VBox/ValidationKit/bootsectors/bs3-cpu-weird-1.c
+++ src/VBox/ValidationKit/bootsectors/bs3-cpu-weird-1.c
@@ -1,4 +1,4 @@
-/* $Id: bs3-cpu-weird-1.c 102779 2024-01-07 01:02:16Z knut.osmundsen@oracle.com $ */
+/* $Id: bs3-cpu-weird-1.c 102789 2024-01-08 21:48:55Z knut.osmundsen@oracle.com $ */
 /** @file
  * BS3Kit - bs3-cpu-weird-1, 16-bit C code.
  */
@@ -56,13 +56,13 @@ FNBS3TESTDOMODE bs3CpuWeird1_PushPopSReg_f16;
 *********************************************************************************************************************************/
 static const BS3TESTMODEBYONEENTRY g_aModeByOneTests[] =
 {
-#if 1 /** @todo fails in native recompiler atm. */ /** @todo asserts in ring-0 on VT-x! */
+#if 0 /** @todo fails in native recompiler atm. */ /** @todo asserts in ring-0 on VT-x! */
     { "dbg+inhibit+ringxfer", bs3CpuWeird1_DbgInhibitRingXfer_f16, 0 },
 #endif
-#if 1 /** @todo asserts in native recompiler debug builds, but seems to work otherwise. */
+#if 0 /** @todo asserts in native recompiler debug builds, but seems to work otherwise. */
     { "pc wrapping", bs3CpuWeird1_PcWrapping_f16, 0 },
 #endif
-    { "push/pop", bs3CpuWeird1_PushPop_f16, 0 },
+//    { "push/pop", bs3CpuWeird1_PushPop_f16, 0 },
     { "push/pop sreg", bs3CpuWeird1_PushPopSReg_f16, 0 },
 };
 
