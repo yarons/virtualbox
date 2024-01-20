462952402f44f6d5ec86d07a4d3a39bb4e3ee2eb
Alexander Eichner <alexander.eichner@oracle.com>
2024-01-20 18:41:51 +0000

    ValidationKit/tdGuestOsUnattendedInst1.py: Add Ubuntu 23.10 server image so we can unattended install, bugref:10551
    svn:sync-xref-src-repo-rev: r161212

--- src/VBox/ValidationKit/tests/installation/tdGuestOsUnattendedInst1.py
+++ src/VBox/ValidationKit/tests/installation/tdGuestOsUnattendedInst1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdGuestOsUnattendedInst1.py 102953 2024-01-18 15:11:51Z ksenia.s.stepanova@oracle.com $
+# $Id: tdGuestOsUnattendedInst1.py 102980 2024-01-20 18:41:51Z alexander.eichner@oracle.com $
 
 """
 VirtualBox Validation Kit - Guest OS unattended installation tests.
@@ -37,7 +37,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 102953 $"
+__version__ = "$Revision: 102980 $"
 
 
 # Standard Python imports.
@@ -557,6 +557,7 @@ class tdGuestOsInstTest1(vbox.TestDriver):
                          UnattendedVm.kfNoGAs),
             UnattendedVm(oSet, 'tst-ubuntu-23.10-64', 'Ubuntu_64', '7.0/uaisos/ubuntu-23.10.1-desktop-amd64.iso',    # >=6GiB ?
                          UnattendedVm.kfNoGAs),
+            UnattendedVm(oSet, 'tst-ubuntu-server-23.10-64', 'Ubuntu_64', '7.1/uaisos/ubuntu-23.10-live-server-amd64.iso'),
 
             #
             # Debian
