83c9363eb22a62d695de71d87617325c9bd9f19a
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-01-22 09:59:41 +0000

    tstRTStrFormat: Added an overflow test.
    svn:sync-xref-src-repo-rev: r161217

--- src/VBox/Runtime/testcase/tstRTStrFormat.cpp
+++ src/VBox/Runtime/testcase/tstRTStrFormat.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstRTStrFormat.cpp 98103 2023-01-17 14:15:46Z knut.osmundsen@oracle.com $ */
+/* $Id: tstRTStrFormat.cpp 102985 2024-01-22 09:59:41Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase - String formatting.
  */
@@ -301,6 +301,18 @@ static void testBasics(RTTEST hTest, char *pszBuf)
         RTTestIFailed("error:    '%s'\n"
                       "expected: '%s'\n",
                       pszBuf, szCorrect);
+
+    /* overflow */
+    size_t const cbOverflowBuf = 40;
+    static char const s_szOverflowExpect[] = "i=8                                    ";
+    char *pszBuf3 = (char *)RTTestGuardedAllocTail(hTest, cbOverflowBuf);
+    cch = RTStrPrintf(pszBuf3, cbOverflowBuf, "i=%d%*s\n", (int)8, cbOverflowBuf, "");
+    if (strcmp(pszBuf3, s_szOverflowExpect))
+        RTTestIFailed("error: '%s' (#2)\n"
+                      "wanted '%s' (#2)\n", pszBuf, s_szOverflowExpect);
+    else if (cch != sizeof(s_szOverflowExpect) - 1)
+        RTTestIFailed("error: got %zd, expected %zd (#2)\n", cch2, sizeof(s_szOverflowExpect) - 1);
+    RTTestGuardedFree(hTest, pszBuf3);
 }
 
 
