856c1930616214eb2da47a18eae61a7ceb3b2e34
Alexander Rudnev <alexander.rudnev@oracle.com>
2024-02-05 05:42:19 +0000

    fix for r161455, missing break? bugref:10372
    svn:sync-xref-src-repo-rev: r161457

--- src/VBox/VMM/VMMAll/IEMAllInstOneByte.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllInstOneByte.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllInstOneByte.cpp.h 103191 2024-02-04 23:41:47Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllInstOneByte.cpp.h 103192 2024-02-05 05:42:19Z alexander.rudnev@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Emulation.
  */
@@ -8449,6 +8449,7 @@ FNIEMOP_DEF(iemOp_Grp2_Ev_Ib)
         {
             IEMOP_MNEMONIC2(MI, SHL, shl, Ev, Ib, DISOPTYPE_HARMLESS, 0);
             GRP2_BODY_Ev_Ib(IEMTARGETCPU_EFL_BEHAVIOR_SELECT(g_iemAImpl_shl_eflags));
+            break;
         }
         /**
          * @opdone
