353e17731a6e6b3e39ed14222b3621a3aef96d12
Alexander Eichner <alexander.eichner@oracle.com>
2024-02-06 14:24:41 +0000

    ValidationKit/tests/cpu/tdCpuIemInstr1: Enable bs3-cpu-basic-3 now that we can load large floppies
    svn:sync-xref-src-repo-rev: r161498

--- src/VBox/ValidationKit/tests/cpu/tdCpuIemInstr1.py
+++ src/VBox/ValidationKit/tests/cpu/tdCpuIemInstr1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdCpuIemInstr1.py 103221 2024-02-06 09:04:06Z alexander.eichner@oracle.com $
+# $Id: tdCpuIemInstr1.py 103228 2024-02-06 14:24:41Z alexander.eichner@oracle.com $
 
 """
 VirtualBox Validation Kit - Test that runs various benchmarks.
@@ -37,7 +37,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 103221 $"
+__version__ = "$Revision: 103228 $"
 
 
 # Standard Python imports.
@@ -79,9 +79,7 @@ class tdCpuIemInstr1(vbox.TestDriver):
 
         kaTestVMs = (
             IemTestVm(self.oTestVmSet, self, 'bs3-cpu-basic-2'),
-
-            # @todo r=aeichner Image can not be found (probably it is too large for a floppy weighing in at 16MiB)
-            #IemTestVm(self.oTestVmSet, self, 'bs3-cpu-basic-3'),
+            IemTestVm(self.oTestVmSet, self, 'bs3-cpu-basic-3'),
 
             # @todo r=aeichner Fails currently in IEM
             #IemTestVm(self.oTestVmSet, self, 'bs3-cpu-decoding-1'),
