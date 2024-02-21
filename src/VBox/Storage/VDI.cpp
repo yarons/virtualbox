478df140130d26efabe3e206c505432e2024ce33
Alexander Eichner <alexander.eichner@oracle.com>
2024-02-21 15:43:55 +0000

    Storage/VDI.cpp: Fix harmless some parfait warnings, bugref:3409
    svn:sync-xref-src-repo-rev: r161843

--- src/VBox/Storage/VDI.cpp
+++ src/VBox/Storage/VDI.cpp
@@ -1,4 +1,4 @@
-/* $Id: VDI.cpp 99739 2023-05-11 01:01:08Z knut.osmundsen@oracle.com $ */
+/* $Id: VDI.cpp 103501 2024-02-21 15:43:55Z alexander.eichner@oracle.com $ */
 /** @file
  * Virtual Disk Image (VDI), Core Code.
  */
@@ -479,11 +479,11 @@ static int vdiValidateHeader(PVDIHEADER pHeader)
         fFailed = true;
     }
 
-    if (   getImageLCHSGeometry(pHeader)
-        && (getImageLCHSGeometry(pHeader))->cbSector != VDI_GEOMETRY_SECTOR_SIZE)
+    PVDIDISKGEOMETRY pLCHSGeom = getImageLCHSGeometry(pHeader);
+    if (   pLCHSGeom
+        && pLCHSGeom->cbSector != VDI_GEOMETRY_SECTOR_SIZE)
     {
-        LogRel(("VDI: wrong sector size (%d != %d)\n",
-               (getImageLCHSGeometry(pHeader))->cbSector, VDI_GEOMETRY_SECTOR_SIZE));
+        LogRel(("VDI: wrong sector size (%d != %d)\n", pLCHSGeom->cbSector, VDI_GEOMETRY_SECTOR_SIZE));
         fFailed = true;
     }
 
@@ -1512,8 +1512,7 @@ static DECLCALLBACK(int) vdiCreate(const char *pszFilename, uint64_t cbSize,
 
     /* Check size. Maximum 4PB-3M. No tricks with adjusting the 1M block size
      * so far, which would extend the size. */
-    if (   !cbSize
-        || cbSize >= _1P * 4 - _1M * 3
+    if (   cbSize >= _1P * 4 - _1M * 3
         || cbSize < VDI_IMAGE_DEFAULT_BLOCK_SIZE
         || (cbSize % 512))
         return VERR_VD_INVALID_SIZE;
@@ -2562,8 +2561,7 @@ static DECLCALLBACK(int) vdiResize(void *pBackendData, uint64_t cbSize,
 
     /* Check size. Maximum 4PB-3M. No tricks with adjusting the 1M block size
      * so far, which would extend the size. */
-    if (   !cbSize
-        || cbSize >= _1P * 4 - _1M * 3
+    if (   cbSize >= _1P * 4 - _1M * 3
         || cbSize < VDI_IMAGE_DEFAULT_BLOCK_SIZE)
         return VERR_VD_INVALID_SIZE;
 
