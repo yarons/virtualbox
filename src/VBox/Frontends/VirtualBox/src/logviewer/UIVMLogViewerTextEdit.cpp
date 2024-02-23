44bc1babc30fc2823ae70d0fd95eabd74fc76e56
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2024-02-23 09:53:32 +0000

    FE/Qt: bugref:9072. Integrating new icons for go-to-bottom/go-to-top buttons.
    svn:sync-xref-src-repo-rev: r161891

--- src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerTextEdit.cpp
+++ src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerTextEdit.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVMLogViewerTextEdit.cpp 103483 2024-02-21 07:19:45Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMLogViewerTextEdit.cpp 103544 2024-02-23 09:53:32Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMLogViewer class implementation.
  */
@@ -300,10 +300,10 @@ void UIVMLogViewerTextEdit::prepareWidgets()
     m_pScrollToTopLabel->installEventFilter(this);
 
     const int iIconMetric = QApplication::style()->pixelMetric(QStyle::PM_LargeIconSize);
-    m_pScrollToBottomLabel->setPixmap(UIIconPool::iconSet(":/vm_discard_32px.png").pixmap(iIconMetric, iIconMetric));
+    m_pScrollToBottomLabel->setPixmap(UIIconPool::iconSet(":/arrow_to_bottom_32px.png").pixmap(iIconMetric, iIconMetric));
     m_pScrollToBottomLabel->setOpacity(0.4);
 
-    m_pScrollToTopLabel->setPixmap(UIIconPool::iconSet(":/file_manager_go_up_24px.png").pixmap(iIconMetric, iIconMetric));
+    m_pScrollToTopLabel->setPixmap(UIIconPool::iconSet(":/arrow_to_top_32px.png").pixmap(iIconMetric, iIconMetric));
     m_pScrollToTopLabel->setOpacity(0.4);
 
     connect(this, &UIVMLogViewerTextEdit::blockCountChanged, this, &UIVMLogViewerTextEdit::sltUpdateLineNumberAreaWidth);
