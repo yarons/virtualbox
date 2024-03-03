8fd78eccaba4da486cbffb18f7fb185653d4b97f
Alexander Eichner <alexander.eichner@oracle.com>
2024-03-03 07:11:39 +0000

    VMM/IEM: Delay the RIP update for each instruction if possible to save on potentially two instructions for each executed guest instruction of which one is a memory write, bugref:10373
    svn:sync-xref-src-repo-rev: r162009

--- src/VBox/VMM/VMMR3/IEMR3.cpp
+++ src/VBox/VMM/VMMR3/IEMR3.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMR3.cpp 103554 2024-02-23 23:26:09Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMR3.cpp 103649 2024-03-03 07:11:39Z alexander.eichner@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager.
  */
@@ -530,6 +530,16 @@ VMMR3DECL(int)      IEMR3Init(PVM pVM)
         szPat[offFlagChar] = szVal[offFlagChar] = 'Z'; STAMR3RegisterPctOfSum(pVM->pUVM, STAMVISIBILITY_ALWAYS, STAMUNIT_PCT, szVal, true, szPat, "Skippable EFLAGS.ZF updating percentage", "/IEM/CPU%u/re/NativeLivenessEFlagsZfSkippablePct", idCpu);
         szPat[offFlagChar] = szVal[offFlagChar] = 'S'; STAMR3RegisterPctOfSum(pVM->pUVM, STAMVISIBILITY_ALWAYS, STAMUNIT_PCT, szVal, true, szPat, "Skippable EFLAGS.SF updating percentage", "/IEM/CPU%u/re/NativeLivenessEFlagsSfSkippablePct", idCpu);
         szPat[offFlagChar] = szVal[offFlagChar] = 'O'; STAMR3RegisterPctOfSum(pVM->pUVM, STAMVISIBILITY_ALWAYS, STAMUNIT_PCT, szVal, true, szPat, "Skippable EFLAGS.OF updating percentage", "/IEM/CPU%u/re/NativeLivenessEFlagsOfSkippablePct", idCpu);
+
+        STAMR3RegisterF(pVM, &pVCpu->iem.s.StatNativePcUpdateTotal,   STAMTYPE_COUNTER, STAMVISIBILITY_ALWAYS, STAMUNIT_COUNT, "Total RIP updates",   "/IEM/CPU%u/re/NativePcUpdateTotal", idCpu);
+        STAMR3RegisterF(pVM, &pVCpu->iem.s.StatNativePcUpdateDelayed, STAMTYPE_COUNTER, STAMVISIBILITY_ALWAYS, STAMUNIT_COUNT, "Delayed RIP updates", "/IEM/CPU%u/re/NativePcUpdateDelayed", idCpu);
+
+        /* Ratio of the status bit skippables. */
+        RTStrPrintf(szPat, sizeof(szPat), "/IEM/CPU%u/re/NativePcUpdateTotal", idCpu);
+        RTStrPrintf(szVal, sizeof(szVal), "/IEM/CPU%u/re/NativePcUpdateDelayed", idCpu);
+        STAMR3RegisterPctOfSum(pVM->pUVM, STAMVISIBILITY_ALWAYS, STAMUNIT_PCT, szVal, false, szPat,
+                               "Delayed RIP updating percentage",
+                               "/IEM/CPU%u/re/NativePcUpdateDelayed_StatusDelayedPct", idCpu);
 
 #  endif /* VBOX_WITH_STATISTICS */
 # endif /* VBOX_WITH_IEM_NATIVE_RECOMPILER */
