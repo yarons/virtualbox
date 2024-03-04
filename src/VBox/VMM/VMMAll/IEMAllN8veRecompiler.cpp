0eef01b8104cd4f3575999b7c58ee0f1bf01ecda
Alexander Eichner <alexander.eichner@oracle.com>
2024-03-04 14:05:20 +0000

    VMM/IEM: Replace some single bit tests with iemNativeEmitTestBitInGprAndJmpToLabelIf{Not}Set() to fix a debug only assertion, bugref:10371
    svn:sync-xref-src-repo-rev: r162029

--- src/VBox/VMM/VMMAll/IEMAllN8veRecompiler.cpp
+++ src/VBox/VMM/VMMAll/IEMAllN8veRecompiler.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMAllN8veRecompiler.cpp 103667 2024-03-04 13:46:36Z alexander.eichner@oracle.com $ */
+/* $Id: IEMAllN8veRecompiler.cpp 103669 2024-03-04 14:05:20Z alexander.eichner@oracle.com $ */
 /** @file
  * IEM - Native Recompiler
  *
@@ -7072,17 +7072,17 @@ iemNativeEmitMaybeRaiseSseRelatedXcpt(PIEMRECOMPILERSTATE pReNative, uint32_t of
      * if (cr0 & X86_CR0_EM)
      *     return raisexcpt();
      */
-    off = iemNativeEmitTestAnyBitsInGprAndJmpToLabelIfAnySet(pReNative, off, idxCr0Reg, X86_CR0_EM, idxLabelRaiseUd);
+    off = iemNativeEmitTestBitInGprAndJmpToLabelIfSet(pReNative, off, idxCr0Reg, X86_CR0_EM_BIT, idxLabelRaiseUd);
     /*
      * if (!(cr4 & X86_CR4_OSFXSR))
      *     return raisexcpt();
      */
-    off = iemNativeEmitTestAnyBitsInGprAndJmpToLabelIfNoneSet(pReNative, off, idxCr4Reg, X86_CR4_OSFXSR, idxLabelRaiseUd);
+    off = iemNativeEmitTestBitInGprAndJmpToLabelIfNotSet(pReNative, off, idxCr4Reg, X86_CR4_OSFXSR_BIT, idxLabelRaiseUd);
     /*
      * if (cr0 & X86_CR0_TS)
      *     return raisexcpt();
      */
-    off = iemNativeEmitTestAnyBitsInGprAndJmpToLabelIfAnySet(pReNative, off, idxCr0Reg, X86_CR0_TS, idxLabelRaiseNm);
+    off = iemNativeEmitTestBitInGprAndJmpToLabelIfSet(pReNative, off, idxCr0Reg, X86_CR0_TS_BIT, idxLabelRaiseNm);
 
     /* Free but don't flush the CR0 and CR4 register. */
     iemNativeRegFreeTmp(pReNative, idxCr0Reg);
@@ -7147,12 +7147,12 @@ iemNativeEmitMaybeRaiseAvxRelatedXcpt(PIEMRECOMPILERSTATE pReNative, uint32_t of
      * if (!(cr4 & X86_CR4_OSXSAVE))
      *     return raisexcpt();
      */
-    off = iemNativeEmitTestAnyBitsInGprAndJmpToLabelIfNoneSet(pReNative, off, idxCr4Reg, X86_CR4_OSXSAVE, idxLabelRaiseUd);
+    off = iemNativeEmitTestBitInGprAndJmpToLabelIfNotSet(pReNative, off, idxCr4Reg, X86_CR4_OSXSAVE_BIT, idxLabelRaiseUd);
     /*
      * if (cr0 & X86_CR0_TS)
      *     return raisexcpt();
      */
-    off = iemNativeEmitTestAnyBitsInGprAndJmpToLabelIfAnySet(pReNative, off, idxCr0Reg, X86_CR0_TS, idxLabelRaiseNm);
+    off = iemNativeEmitTestBitInGprAndJmpToLabelIfSet(pReNative, off, idxCr0Reg, X86_CR0_TS_BIT, idxLabelRaiseNm);
 
     /* Free but don't flush the CR0, CR4 and XCR0 register. */
     iemNativeRegFreeTmp(pReNative, idxCr0Reg);
