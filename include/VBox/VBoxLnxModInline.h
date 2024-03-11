10e33e9e88ba093a349ad903d49c76f85c7e4f7d
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2024-03-11 18:02:22 +0000

    Linux: Introduce common code for disabling modules loading from kernel command line (scm fix), bugref:8409.
    svn:sync-xref-src-repo-rev: r162160

--- include/VBox/VBoxLnxModInline.h
+++ include/VBox/VBoxLnxModInline.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxLnxModInline.h 103785 2024-03-11 17:42:43Z vadim.galitsyn@oracle.com $ */
+/* $Id: VBoxLnxModInline.h 103791 2024-03-11 18:02:22Z vadim.galitsyn@oracle.com $ */
 /** @file
  * A common code for VirtualBox Linux kernel modules.
  */
@@ -30,7 +30,6 @@
 
 #ifndef VBOX_INCLUDED_VBoxLnxModInline_h
 #define VBOX_INCLUDED_VBoxLnxModInline_h
-
 #ifndef RT_WITHOUT_PRAGMA_ONCE
 # pragma once
 #endif
