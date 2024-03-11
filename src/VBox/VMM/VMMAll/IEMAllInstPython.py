5fcf33785c6e0a3619650025ba7564f5a594a41d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-03-11 10:45:59 +0000

    VMM/IEM: Remove IEM_MC_NATIVE_IF from the native emitter code blocks and liveness analysis, also morping ARGS to LOCALS as appropriate. bugref:10376
    svn:sync-xref-src-repo-rev: r162125

--- src/VBox/VMM/VMMAll/IEMAllInstPython.py
+++ src/VBox/VMM/VMMAll/IEMAllInstPython.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: IEMAllInstPython.py 103671 2024-03-04 15:48:34Z alexander.eichner@oracle.com $
+# $Id: IEMAllInstPython.py 103756 2024-03-11 10:45:59Z knut.osmundsen@oracle.com $
 
 """
 IEM instruction extractor.
@@ -43,7 +43,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 103671 $"
+__version__ = "$Revision: 103756 $"
 
 # pylint: disable=anomalous-backslash-in-string,too-many-lines
 
@@ -1913,6 +1913,12 @@ class McStmtCond(McStmt):
         sRet += ' ' * cchIndent + '} IEM_MC%s_ENDIF();\n' % (self.sNativeInfix,);
         return sRet;
 
+class McStmtNativeIf(McStmtCond):
+    """ IEM_MC_NATIVE_IF """
+    def __init__(self, sName, asArchitectures):
+        McStmtCond.__init__(self, sName, ['|'.join(asArchitectures) if asArchitectures else '0',]);
+        self.asArchitectures = asArchitectures;
+
 class McStmtVar(McStmt):
     """ IEM_MC_LOCAL, IEM_MC_LOCAL_ASSIGN, IEM_MC_LOCAL_CONST """
     def __init__(self, sName, asParams, sType, sVarName, sValue = None):
@@ -2160,6 +2166,28 @@ class McBlock(object):
         """ Generic parser that returns a plain McStmtCond object. """
         _ = oSelf;
         return McStmtCond(sName, asParams);
+
+    kdArchVals = {
+        'RT_ARCH_VAL_X86':     True,
+        'RT_ARCH_VAL_AMD64':   True,
+        'RT_ARCH_VAL_ARM32':   True,
+        'RT_ARCH_VAL_ARM64':   True,
+        'RT_ARCH_VAL_SPARC32': True,
+        'RT_ARCH_VAL_SPARC64': True,
+    };
+
+    @staticmethod
+    def parseMcNativeIf(oSelf, sName, asParams):
+        """ IEM_MC_NATIVE_IF """
+        oSelf.checkStmtParamCount(sName, asParams, 1);
+        if asParams[0].strip() == '0':
+            asArchitectures = [];
+        else:
+            asArchitectures = [sArch.strip() for sArch in asParams[0].split('|')];
+            for sArch in asArchitectures:
+                if sArch not in oSelf.kdArchVals:
+                    oSelf.raiseStmtError(sName, 'Unknown architecture: %s' % (sArch,));
+        return McStmtNativeIf(sName, asArchitectures);
 
     @staticmethod
     def parseMcBegin(oSelf, sName, asParams):
@@ -3180,7 +3208,7 @@ g_dMcStmtParsers = {
     'IEM_MC_NATIVE_EMIT_5':                                      (McBlock.parseMcGeneric,           True,  True,  True,  ),
     'IEM_MC_NATIVE_EMIT_6':                                      (McBlock.parseMcGeneric,           True,  True,  True,  ),
     'IEM_MC_NATIVE_EMIT_7':                                      (McBlock.parseMcGeneric,           True,  True,  True,  ),
-    'IEM_MC_NATIVE_IF':                                          (McBlock.parseMcGenericCond,       False, False, True,  ),
+    'IEM_MC_NATIVE_IF':                                          (McBlock.parseMcNativeIf,          False, False, True,  ),
     'IEM_MC_NATIVE_ELSE':                                        (McBlock.parseMcGenericCond,       False, False, True,  ),
     'IEM_MC_NATIVE_ENDIF':                                       (McBlock.parseMcGenericCond,       False, False, True,  ),
     'IEM_MC_OR_2LOCS_U32':                                       (McBlock.parseMcGeneric,           False, False, False, ),
