863868c01e26b678065475838383247fe8f68fb3
Alexander Eichner <alexander.eichner@oracle.com>
2024-03-19 09:01:35 +0000

    VMM/IEM: Convert iemMemFetchDataU128AlignedSse/iemMemFetchDataU128AlignedSseJmp to be instantiated by the memory RW template code in preparation for SIMD memory fetches. Adds the possibility to define additional flags being passed to iemMemMap* in the template code, bugref:10614
    svn:sync-xref-src-repo-rev: r162282

--- src/VBox/VMM/include/IEMInline.h
+++ src/VBox/VMM/include/IEMInline.h
@@ -1,4 +1,4 @@
-/* $Id: IEMInline.h 103256 2024-02-07 15:07:09Z alexander.eichner@oracle.com $ */
+/* $Id: IEMInline.h 103908 2024-03-19 09:01:35Z alexander.eichner@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager - Inlined Functions.
  */
@@ -4089,6 +4089,22 @@ AssertCompile(((3U + 1U) << 16) == X86_CR0_AM);
 #define TMPL_MEM_FMT_DESC   "dqword"
 #include "../VMMAll/IEMAllMemRWTmplInline.cpp.h"
 #undef TMPL_MEM_NO_MAPPING
+
+
+/* Every template reyling on unaligned accesses inside a page not being okay should go below. */
+#undef TMPL_MEM_CHECK_UNALIGNED_WITHIN_PAGE_OK
+#define TMPL_MEM_CHECK_UNALIGNED_WITHIN_PAGE_OK(a_pVCpu, a_GCPtrEff, a_TmplMemType) 0
+
+#define TMPL_MEM_NO_MAPPING
+#define TMPL_MEM_TYPE       RTUINT128U
+#define TMPL_MEM_TYPE_ALIGN 15
+#define TMPL_MEM_TYPE_SIZE  16
+#define TMPL_MEM_FN_SUFF    U128AlignedSse
+#define TMPL_MEM_FMT_TYPE   "%.16Rhxs"
+#define TMPL_MEM_FMT_DESC   "dqword"
+#include "../VMMAll/IEMAllMemRWTmplInline.cpp.h"
+#undef  TMPL_MEM_NO_MAPPING
+
 
 #undef TMPL_MEM_CHECK_UNALIGNED_WITHIN_PAGE_OK
 
