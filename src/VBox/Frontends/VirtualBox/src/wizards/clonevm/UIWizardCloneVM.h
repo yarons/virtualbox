ecc565944bb35fa2c1590fc5ba04239a7d708288
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2024-03-20 16:38:13 +0000

    FE/Qt. bugref:10622. build fix.
    svn:sync-xref-src-repo-rev: r162349

--- src/VBox/Frontends/VirtualBox/src/wizards/clonevm/UIWizardCloneVM.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/clonevm/UIWizardCloneVM.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardCloneVM.h 103961 2024-03-20 14:34:36Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIWizardCloneVM.h 103972 2024-03-20 16:38:13Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIWizardCloneVM class declaration.
  */
@@ -80,7 +80,7 @@ public:
 
 protected:
 
-    virtual void populatePages() /* final override */;
+    virtual void populatePages() final override;
 
 private slots:
 
