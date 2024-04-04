42214ebbd00db958abeb5bcdce17eadf2c1f5ee2
Andreas Loeffler <andreas.loeffler@oracle.com>
2024-04-04 08:48:52 +0000

    VBoxSDS: Added Windows-specific help command line parameters ('/?', '/h', '/help'), to match the rest of the syntax.
    svn:sync-xref-src-repo-rev: r162571

--- src/VBox/Main/src-global/win/VBoxSDS.cpp
+++ src/VBox/Main/src-global/win/VBoxSDS.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxSDS.cpp 98280 2023-01-24 11:58:44Z knut.osmundsen@oracle.com $ */
+/* $Id: VBoxSDS.cpp 104149 2024-04-04 08:48:52Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxSDS - COM global service main entry (System Directory Service)
  */
@@ -812,6 +812,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         { "--loginterval",  'I',    RTGETOPT_REQ_UINT32 | RTGETOPT_FLAG_ICASE },
         { "-loginterval",   'I',    RTGETOPT_REQ_UINT32 | RTGETOPT_FLAG_ICASE },
         { "/loginterval",   'I',    RTGETOPT_REQ_UINT32 | RTGETOPT_FLAG_ICASE },
+        { "/?",             'h',    RTGETOPT_REQ_NOTHING }, /* Most Windows programs use '/?', so have this as an alias. */
+        { "/h",             'h',    RTGETOPT_REQ_NOTHING }, /* Ditto for '/h'. */
+        { "/help",          'h',    RTGETOPT_REQ_NOTHING }  /* Ditto for '/help'. */
     };
 
     bool            fRun = true;
