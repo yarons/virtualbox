679b531695c4ad811262ed8a54f9a2a49ba098b3
Alexander Eichner <alexander.eichner@oracle.com>
2024-04-17 13:12:02 +0000

    VMM/IEM: Implement native emitters for psubb, psubw, psubd and psubdq, bugref:10652 [missing file]
    svn:sync-xref-src-repo-rev: r162823

--- src/VBox/VMM/include/IEMInternal.h
+++ src/VBox/VMM/include/IEMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: IEMInternal.h 104321 2024-04-12 15:08:36Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMInternal.h 104340 2024-04-17 13:12:02Z alexander.eichner@oracle.com $ */
 /** @file
  * IEM - Internal header file.
  */
@@ -2041,7 +2041,7 @@ typedef struct IEMCPU
 # ifdef VBOX_WITH_IEM_RECOMPILER
     /** Statistics per threaded function call.
      * Updated by both the threaded and native recompilers. */
-    uint32_t                acThreadedFuncStats[0x5160 /*20832*/];
+    uint32_t                acThreadedFuncStats[0x6000 /*24576*/];
 # endif
 #endif
 } IEMCPU;
