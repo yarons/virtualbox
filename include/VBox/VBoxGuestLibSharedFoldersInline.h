04d367c2d487095f9f81cec6369efeb0fab62794
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2024-04-17 13:55:40 +0000

    Additions: Linux: UBSAN: vboxsf: update naming for r162827, bugref:10585.
    svn:sync-xref-src-repo-rev: r162830

--- include/VBox/VBoxGuestLibSharedFoldersInline.h
+++ include/VBox/VBoxGuestLibSharedFoldersInline.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestLibSharedFoldersInline.h 104344 2024-04-17 13:19:20Z vadim.galitsyn@oracle.com $ */
+/* $Id: VBoxGuestLibSharedFoldersInline.h 104345 2024-04-17 13:55:40Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VBoxGuestLib - Shared Folders Host Request Helpers (ring-0).
  */
@@ -1502,11 +1502,12 @@ DECLINLINE(int) VbglR0SfHostReqReadLinkContigSimple(SHFLROOT idRoot, const char 
                                                                            + SHFLSTRING_HEADER_SIZE + (uint32_t)cchPath);
         if (pReq)
         {
-            char *ach = pReq->StrPath.String.ach;
+            char *pszPath = pReq->StrPath.String.ach;
+
             pReq->StrPath.u16Length = (uint16_t)cchPath;
             pReq->StrPath.u16Size   = (uint16_t)cchPath + 1;
-            RT_BCOPY_UNFORTIFIED(pReq->StrPath.String.ach, pszPath, cchPath);
-            ach[cchPath] = '\0';
+            RT_BCOPY_UNFORTIFIED(pszPath, pszPath, cchPath);
+            pszPath[cchPath] = '\0';
 
             {
                 int vrc = VbglR0SfHostReqReadLinkContig(idRoot, pvBuf, PhysBuffer, cbBuffer, pReq);
--- src/VBox/Additions/linux/sharedfolders/utils.c
+++ src/VBox/Additions/linux/sharedfolders/utils.c
@@ -1,4 +1,4 @@
-/* $Id: utils.c 104344 2024-04-17 13:19:20Z vadim.galitsyn@oracle.com $ */
+/* $Id: utils.c 104345 2024-04-17 13:55:40Z vadim.galitsyn@oracle.com $ */
 /** @file
  * vboxsf - VBox Linux Shared Folders VFS, utility functions.
  *
@@ -1047,11 +1047,11 @@ static int vbsf_make_path(const char *caller, struct vbsf_inode_info *sf_i,
     if (fRoot)
         RT_BCOPY_UNFORTIFIED(&tmp->String.utf8[0], d_name, d_len + 1);
     else {
-        uint8_t *pUtf8 = tmp->String.utf8;
-        RT_BCOPY_UNFORTIFIED(&pUtf8[0], p_name, p_len);
-        pUtf8[p_len] = '/';
-        RT_BCOPY_UNFORTIFIED(&pUtf8[p_len + 1], d_name, d_len);
-        pUtf8[p_len + 1 + d_len] = '\0';
+        uint8_t *pszPath = tmp->String.utf8;
+        RT_BCOPY_UNFORTIFIED(&pszPath[0], p_name, p_len);
+        pszPath[p_len] = '/';
+        RT_BCOPY_UNFORTIFIED(&pszPath[p_len + 1], d_name, d_len);
+        pszPath[p_len + 1 + d_len] = '\0';
     }
 
     *result = tmp;
