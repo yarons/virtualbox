40450694d05224196cb563e388011384e7840faf
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2024-04-28 10:38:07 +0000

    FE/Qt. bugref:9510. Moving UIVMActivityToolWidget.
    svn:sync-xref-src-repo-rev: r162960

--- src/VBox/Frontends/VirtualBox/src/activity/vmactivity/UIVMActivityToolWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/activity/vmactivity/UIVMActivityToolWidget.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVMActivityToolWidget.cpp 104450 2024-04-28 10:31:01Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMActivityToolWidget.cpp 104451 2024-04-28 10:38:07Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMActivityToolWidget class implementation.
  */
