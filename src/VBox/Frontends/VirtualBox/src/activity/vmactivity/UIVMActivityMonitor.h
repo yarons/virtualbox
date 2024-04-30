cca806093ab2c284e9fb41c2ecd17f3d367ef925
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2024-04-30 13:08:41 +0000

    FE/Qt. bugref:9510. Adding 'show preferences' action to the context menu in runtime UI.
    svn:sync-xref-src-repo-rev: r162980

--- src/VBox/Frontends/VirtualBox/src/activity/vmactivity/UIVMActivityMonitor.h
+++ src/VBox/Frontends/VirtualBox/src/activity/vmactivity/UIVMActivityMonitor.h
@@ -1,4 +1,4 @@
-/* $Id: UIVMActivityMonitor.h 104463 2024-04-30 12:37:49Z serkan.bayraktar@oracle.com $ */
+/* $Id: UIVMActivityMonitor.h 104465 2024-04-30 13:08:41Z serkan.bayraktar@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIVMActivityMonitor class declaration.
  */
@@ -226,7 +226,7 @@ protected:
     int m_iMaximumLabelLength;
     int m_iMaximumQueueSize;
     QColor m_dataSeriesColor[DATA_SERIES_SIZE];
-
+    UIActionPool *m_pActionPool;
 private slots:
 
     /** Reads the metric values for several sources and calls corresponding update functions. */
@@ -240,7 +240,6 @@ private:
     /** Holds the instance of layout we create. */
     QVBoxLayout *m_pMainLayout;
     EmbedTo m_enmEmbedding;
-    UIActionPool *m_pActionPool;
 };
 
 class  SHARED_LIBRARY_STUFF UIVMActivityMonitorLocal : public UIVMActivityMonitor
