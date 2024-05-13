94f4942e102128fbf6dc9b6fa53dad871ea7f10a
Sergey Dubov <sergey.dubov@oracle.com>
2024-05-13 14:25:49 +0000

    FE/Qt: bugref:10450: main.cpp: We are handling all the QtMsgType values for many years already.
    svn:sync-xref-src-repo-rev: r163140

--- src/VBox/Frontends/VirtualBox/src/main.cpp
+++ src/VBox/Frontends/VirtualBox/src/main.cpp
@@ -1,4 +1,4 @@
-/* $Id: main.cpp 104127 2024-03-30 23:59:46Z sergey.dubov@oracle.com $ */
+/* $Id: main.cpp 104599 2024-05-13 14:25:49Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - The main() function.
  */
@@ -248,11 +248,9 @@ static void QtMessageOutput(QtMsgType enmType, const QMessageLogContext &context
             RTStrmPrintf(g_pStdErr, "Qt FATAL: %s\n", strMessage.toUtf8().constData());
 # endif
             break;
-# if QT_VERSION >= QT_VERSION_CHECK(5, 5, 0)
         case QtInfoMsg:
             /** @todo ignore? */
             break;
-# endif
     }
 }
 
