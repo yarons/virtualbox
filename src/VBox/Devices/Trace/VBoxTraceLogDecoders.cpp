92f3edb94a87ba4baaf04b697cb550926c42f768
Alexander Eichner <alexander.eichner@oracle.com>
2024-06-12 14:47:17 +0000

    Devices/Trace: Add support for tracing the ITPMCONNECTOR interface and start with a decoder plugin for dissecting TPM command/respons buffers, bugref:10701 [scm fix]
    svn:sync-xref-src-repo-rev: r163502

--- src/VBox/Devices/Trace/VBoxTraceLogDecoders.cpp
+++ src/VBox/Devices/Trace/VBoxTraceLogDecoders.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxTraceLogDecoders.cpp 104898 2024-06-12 14:27:22Z alexander.eichner@oracle.com $ */
+/* $Id: VBoxTraceLogDecoders.cpp 104900 2024-06-12 14:47:17Z alexander.eichner@oracle.com $ */
 /** @file
  * RTTraceLogDecoders - Implement decoders for the tracing driver.
  */
@@ -37,6 +37,7 @@
 #include <iprt/tracelog-decoder-plugin.h>
 
 #include <iprt/formats/tpm.h>
+
 
 /*********************************************************************************************************************************
 *   Structures and Typedefs                                                                                                      *
