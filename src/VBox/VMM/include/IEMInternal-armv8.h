02dcd2ec5297b4d03d2ccc2b8bfefab22c322757
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-07-19 00:29:21 +0000

    VMM/IEM: info itlb/dtlb improvements and fixes for global entries. [missing files] bugref:10687 bugref:10727
    svn:sync-xref-src-repo-rev: r164065

--- src/VBox/VMM/include/IEMInternal-armv8.h
+++ src/VBox/VMM/include/IEMInternal-armv8.h
@@ -1,4 +1,4 @@
-/* $Id: IEMInternal-armv8.h 105177 2024-07-08 09:29:14Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMInternal-armv8.h 105411 2024-07-19 00:29:21Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Internal header file, ARMv8 variant.
  */
@@ -355,10 +355,13 @@ typedef struct IEMTLB
     uint32_t            au32Padding[3+5];
 } IEMTLB;
 AssertCompileSizeAlignment(IEMTLB, 64);
+/** The width (in bits) of the address portion of the TLB tag.   */
+#define IEMTLB_TAG_ADDR_WIDTH   36
 /** IEMTLB::uTlbRevision increment.  */
-#define IEMTLB_REVISION_INCR    RT_BIT_64(36)
+#define IEMTLB_REVISION_INCR    RT_BIT_64(IEMTLB_TAG_ADDR_WIDTH)
 /** IEMTLB::uTlbRevision mask.  */
-#define IEMTLB_REVISION_MASK    (~(RT_BIT_64(36) - 1))
+#define IEMTLB_REVISION_MASK    (~(RT_BIT_64(IEMTLB_TAG_ADDR_WIDTH) - 1))
+
 /** IEMTLB::uTlbPhysRev increment.
  * @sa IEMTLBE_F_PHYS_REV */
 #define IEMTLB_PHYS_REV_INCR    RT_BIT_64(10)
--- src/VBox/VMM/include/IEMInternal.h
+++ src/VBox/VMM/include/IEMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: IEMInternal.h 105359 2024-07-16 14:14:06Z alexander.eichner@oracle.com $ */
+/* $Id: IEMInternal.h 105411 2024-07-19 00:29:21Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Internal header file.
  */
@@ -660,10 +660,13 @@ typedef struct IEMTLB
     IEMTLBENTRY         aEntries[IEMTLB_ENTRY_COUNT * 2];
 } IEMTLB;
 AssertCompileSizeAlignment(IEMTLB, 64);
+/** The width (in bits) of the address portion of the TLB tag.   */
+#define IEMTLB_TAG_ADDR_WIDTH   36
 /** IEMTLB::uTlbRevision increment.  */
-#define IEMTLB_REVISION_INCR    RT_BIT_64(36)
+#define IEMTLB_REVISION_INCR    RT_BIT_64(IEMTLB_TAG_ADDR_WIDTH)
 /** IEMTLB::uTlbRevision mask.  */
-#define IEMTLB_REVISION_MASK    (~(RT_BIT_64(36) - 1))
+#define IEMTLB_REVISION_MASK    (~(RT_BIT_64(IEMTLB_TAG_ADDR_WIDTH) - 1))
+
 /** IEMTLB::uTlbPhysRev increment.
  * @sa IEMTLBE_F_PHYS_REV */
 #define IEMTLB_PHYS_REV_INCR    RT_BIT_64(11)
