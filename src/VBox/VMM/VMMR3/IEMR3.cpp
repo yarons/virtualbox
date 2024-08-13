c4a054eac08bcb6017ee0339ec99ce012b90947d
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-08-13 23:35:12 +0000

    VMM/IEM: Corrected missing uRegFpCtrl initialization. bugref:10652
    svn:sync-xref-src-repo-rev: r164357

--- src/VBox/VMM/VMMR3/IEMR3.cpp
+++ src/VBox/VMM/VMMR3/IEMR3.cpp
@@ -1,4 +1,4 @@
-/* $Id: IEMR3.cpp 105616 2024-08-07 20:22:21Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMR3.cpp 105663 2024-08-13 23:35:12Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager.
  */
@@ -290,8 +290,9 @@ VMMR3DECL(int)      IEMR3Init(PVM pVM)
 
 #ifdef VBOX_WITH_IEM_RECOMPILER
         /*
-         * Distribute recompiler configuration.
+         * Recompiler state and configuration distribution.
          */
+        pVCpu->iem.s.uRegFpCtrl                    = IEMNATIVE_SIMD_FP_CTRL_REG_NOT_MODIFIED;
         pVCpu->iem.s.uTbNativeRecompileAtUsedCount = uTbNativeRecompileAtUsedCount;
 #endif
 
