38d0d490c39a5a6e9fb0ab75b14db4f029a7a0b3
Alexander Eichner <alexander.eichner@oracle.com>
2024-08-22 18:18:17 +0000

    Disassembler/ARMv8: Implement decoding of the ldp/stp unsigned variant instructions and add testcases, bugref:10394 [scm]
    svn:sync-xref-src-repo-rev: r164546

--- src/VBox/Disassembler/DisasmCore-armv8.cpp
+++ src/VBox/Disassembler/DisasmCore-armv8.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisasmCore-armv8.cpp 105830 2024-08-22 18:13:37Z alexander.eichner@oracle.com $ */
+/* $Id: DisasmCore-armv8.cpp 105832 2024-08-22 18:18:17Z alexander.eichner@oracle.com $ */
 /** @file
  * VBox Disassembler - Core Components.
  */
@@ -477,7 +477,7 @@ static int disArmV8ParseSImmMemOff(PDISSTATE pDis, uint32_t u32Insn, PCDISARMV8O
 
     pParam->armv8.cb = sizeof(int16_t);
     pParam->armv8.u.offBase = disArmV8ExtractBitVecFromInsnSignExtend(u32Insn, pInsnParm->idxBitStart, pInsnParm->cBits);
-    pParam->armv8.u.offBase <<= (pOp->fFlags & DISARMV8INSNCLASS_F_FORCED_32BIT) ? 2 : 3; 
+    pParam->armv8.u.offBase <<= (pOp->fFlags & DISARMV8INSNCLASS_F_FORCED_32BIT) ? 2 : 3;
     pDis->armv8.cbOperand   =   (pOp->fFlags & DISARMV8INSNCLASS_F_FORCED_32BIT) ? sizeof(uint32_t) : sizeof(uint64_t);
     return VINF_SUCCESS;
 }
