ad879305f78f866bc60141e67c9b74d1203c01d8
Alexander Eichner <alexander.eichner@oracle.com>
2024-10-16 13:16:40 +0000

    VMM/TMAll: Disable annoying assertion on ARMv8, bugref:1038
    svn:sync-xref-src-repo-rev: r165181

--- src/VBox/VMM/VMMAll/TMAll.cpp
+++ src/VBox/VMM/VMMAll/TMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: TMAll.cpp 106061 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: TMAll.cpp 106369 2024-10-16 13:16:40Z alexander.eichner@oracle.com $ */
 /** @file
  * TM - Timeout Manager, all contexts.
  */
@@ -208,7 +208,9 @@ VMMDECL(void) TMNotifyEndOfExecution(PVMCC pVM, PVMCPUCC pVCpu, uint64_t uTsc)
     uint64_t       cTicks = uTsc - pVCpu->tm.s.uTscStartExecuting - SUPGetTscDeltaByCpuSetIndex(pVCpu->iHostCpuSet);
     uint64_t const uCpuHz = SUPGetCpuHzFromGipBySetIndex(g_pSUPGlobalInfoPage, pVCpu->iHostCpuSet);
 # endif
+# ifndef VBOX_VMM_TARGET_ARMV8 /* This is perfectly valid on ARM if the guest is halting in the hypervisor. */
     AssertStmt(cTicks <= uCpuHz << 2, cTicks = uCpuHz << 2); /* max 4 sec */
+# endif
 
     uint64_t cNsExecutingDelta;
     if (uCpuHz < _4G)
