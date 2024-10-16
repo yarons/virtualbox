36f4721f004579fd8f2ff4d3a1b1e09f62664149
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-10-16 20:56:01 +0000

    VMM/IEM: Disable control flow guard for the core IEM jump table call loops. bugref:10720
    svn:sync-xref-src-repo-rev: r165219

--- src/VBox/VMM/VMMAll/IEMAllThrdTables.h
+++ src/VBox/VMM/VMMAll/IEMAllThrdTables.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllThrdTables.h 106061 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllThrdTables.h 106402 2024-10-16 20:56:01Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Instruction Decoding and Threaded Recompilation, Instruction Tables.
  */
@@ -402,7 +402,7 @@ DECL_FORCE_INLINE(int) iemThreadedCompileBackAtFirstInstruction(PVMCPU pVCpu, PI
 #define IEM_MC_DEFER_TO_CIMPL_0_RET(a_fFlags, a_fGstShwFlush, a_pfnCImpl) \
     return iemThreadedRecompilerMcDeferToCImpl0(pVCpu, a_fFlags, a_fGstShwFlush, a_pfnCImpl)
 
-DECLINLINE(VBOXSTRICTRC)
+IEM_DECL_MSC_GUARD_IGNORE  DECLINLINE(VBOXSTRICTRC)
 iemThreadedRecompilerMcDeferToCImpl0(PVMCPUCC pVCpu, uint32_t fFlags, uint64_t fGstShwFlush, PFNIEMCIMPL0 pfnCImpl)
 {
     LogFlow(("CImpl0: %04x:%08RX64 LB %#x: %#x %#RX64 %p\n",
--- src/VBox/VMM/include/IEMN8veRecompiler.h
+++ src/VBox/VMM/include/IEMN8veRecompiler.h
@@ -1,4 +1,4 @@
-/* $Id: IEMN8veRecompiler.h 106319 2024-10-15 08:50:24Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMN8veRecompiler.h 106402 2024-10-16 20:56:01Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager - Native Recompiler Internals.
  */
@@ -2148,7 +2148,8 @@ typedef FNIEMNATIVERECOMPFUNC *PFNIEMNATIVERECOMPFUNC;
 /** Defines a native recompiler worker for a threaded function.
  * @see FNIEMNATIVERECOMPFUNC  */
 #define IEM_DECL_IEMNATIVERECOMPFUNC_DEF(a_Name) \
-    uint32_t VBOXCALL a_Name(PIEMRECOMPILERSTATE pReNative, uint32_t off, PCIEMTHRDEDCALLENTRY pCallEntry)
+    IEM_DECL_MSC_GUARD_IGNORE uint32_t VBOXCALL \
+    a_Name(PIEMRECOMPILERSTATE pReNative, uint32_t off, PCIEMTHRDEDCALLENTRY pCallEntry)
 
 /** Prototypes a native recompiler function for a threaded function.
  * @see FNIEMNATIVERECOMPFUNC  */
@@ -2170,7 +2171,8 @@ typedef FNIEMNATIVELIVENESSFUNC *PFNIEMNATIVELIVENESSFUNC;
 /** Defines a native recompiler liveness analysis worker for a threaded function.
  * @see FNIEMNATIVELIVENESSFUNC  */
 #define IEM_DECL_IEMNATIVELIVENESSFUNC_DEF(a_Name) \
-    DECLCALLBACK(void) a_Name(PCIEMTHRDEDCALLENTRY pCallEntry, PCIEMLIVENESSENTRY pIncoming, PIEMLIVENESSENTRY pOutgoing)
+    IEM_DECL_MSC_GUARD_IGNORE DECLCALLBACK(void) \
+    a_Name(PCIEMTHRDEDCALLENTRY pCallEntry, PCIEMLIVENESSENTRY pIncoming, PIEMLIVENESSENTRY pOutgoing)
 
 /** Prototypes a native recompiler liveness analysis function for a threaded function.
  * @see FNIEMNATIVELIVENESSFUNC  */
