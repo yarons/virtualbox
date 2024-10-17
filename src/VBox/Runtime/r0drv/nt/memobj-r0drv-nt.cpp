cb1fe444b18b444bbdde13911d07899fdbe3748d
Alexander Eichner <alexander.eichner@oracle.com>
2024-10-17 11:37:24 +0000

    Runtime/r0drv: Work on getting it building on win.arm64 (the easy bits), bugref:10734
    svn:sync-xref-src-repo-rev: r165252

--- src/VBox/Runtime/r0drv/nt/memobj-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/memobj-r0drv-nt.cpp
@@ -1,4 +1,4 @@
-/* $Id: memobj-r0drv-nt.cpp 106061 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: memobj-r0drv-nt.cpp 106434 2024-10-17 11:37:24Z alexander.eichner@oracle.com $ */
 /** @file
  * IPRT - Ring-0 Memory Objects, NT.
  */
@@ -59,10 +59,10 @@
  * This is supposed to have a limit right below 256MB, but this appears
  * to actually be much lower. The values here have been determined experimentally.
  */
-#ifdef RT_ARCH_X86
+#if defined(RT_ARCH_X86) || defined(RT_ARCH_ARM32)
 # define MAX_LOCK_MEM_SIZE   (32*1024*1024) /* 32MB */
 #endif
-#ifdef RT_ARCH_AMD64
+#if defined(RT_ARCH_AMD64) || defined(RT_ARCH_ARM64)
 # define MAX_LOCK_MEM_SIZE   (24*1024*1024) /* 24MB */
 #endif
 
