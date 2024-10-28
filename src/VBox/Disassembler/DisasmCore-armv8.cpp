5c8100d9c1fb321ac1ee8fa3128e88137ac3e940
Alexander Eichner <alexander.eichner@oracle.com>
2024-10-28 12:06:08 +0000

    Disassembler: Calculate final displacement in the core already, some preparations for SIMD loads/stores, bugref:10394
    svn:sync-xref-src-repo-rev: r165628

--- src/VBox/Disassembler/DisasmCore-armv8.cpp
+++ src/VBox/Disassembler/DisasmCore-armv8.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisasmCore-armv8.cpp 106752 2024-10-28 09:38:32Z alexander.eichner@oracle.com $ */
+/* $Id: DisasmCore-armv8.cpp 106757 2024-10-28 12:06:08Z alexander.eichner@oracle.com $ */
 /** @file
  * VBox Disassembler - Core Components.
  */
@@ -263,7 +263,7 @@ static int disArmV8ParseImmRel(PDISSTATE pDis, uint32_t u32Insn, PCDISARMV8OPCOD
     Assert(pParam->armv8.enmType == kDisArmv8OpParmNone);
 
     pParam->armv8.enmType = kDisArmv8OpParmImmRel;
-    pParam->uValue = (int64_t)disArmV8ExtractBitVecFromInsnSignExtend(u32Insn, pInsnParm->idxBitStart, pInsnParm->cBits);
+    pParam->uValue = (int64_t)disArmV8ExtractBitVecFromInsnSignExtend(u32Insn, pInsnParm->idxBitStart, pInsnParm->cBits) * sizeof(uint32_t);
     if (pInsnParm->cBits <= 8)
     {
         pParam->armv8.cb = sizeof(int8_t);
