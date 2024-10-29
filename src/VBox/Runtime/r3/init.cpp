a8751d70bd91d599e1733422fa16e80aa94fc4cc
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-10-29 22:10:06 +0000

    IPRT/r3/init: Ignore SIGPIPE during init to avoid anyone using sockets or pipes directly to need to do so. bugref:10268
    svn:sync-xref-src-repo-rev: r165670

--- src/VBox/Runtime/r3/init.cpp
+++ src/VBox/Runtime/r3/init.cpp
@@ -1,4 +1,4 @@
-/* $Id: init.cpp 106061 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: init.cpp 106792 2024-10-29 22:10:06Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Init Ring-3.
  */
@@ -524,6 +524,29 @@ static int rtR3InitBody(uint32_t fFlags, int cArgs, char ***ppapszArgs, const ch
         RTThreadYield();
     }
 #endif /* IPRT_USE_SIG_CHILD_DUMMY */
+
+#ifndef RT_OS_WINDOWS
+    /*
+     * Ignore SIGPIPE if it is configured as SIG_DFL.
+     */
+    if (!(fFlags & RTR3INIT_FLAGS_UNOBTRUSIVE))
+    {
+        struct sigaction SigActOld;
+        rc = sigaction(SIGPIPE, 0, &SigActOld);
+        AssertMsg(rc == 0, ("%d/%d\n", rc, errno));
+        if (   rc == 0
+            && !(SigActOld.sa_flags & SA_SIGINFO)
+            && SigActOld.sa_handler == SIG_DFL)
+        {
+            struct sigaction SigAct;
+            memset(&SigAct, 0, sizeof(SigAct));
+            SigAct.sa_handler = SIG_IGN;
+            rc = sigaction(SIGPIPE, &SigAct, &SigActOld);
+            AssertMsg(rc == 0, ("%d/%d\n", rc, errno));
+            Assert(SigActOld.sa_handler == SIG_DFL || rc != 0);
+        }
+    }
+#endif
 
 #ifdef IPRT_WITH_ALIGNMENT_CHECKS
     /*
