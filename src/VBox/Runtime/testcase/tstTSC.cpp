9d9ce6374aa059007c1c525432cb047e030536f6
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2024-11-11 15:19:28 +0000

    IPRT/tstTSC: Nor is RTMpCpuId available on solaris. jiraref:VBP-1449
    svn:sync-xref-src-repo-rev: r165855

--- src/VBox/Runtime/testcase/tstTSC.cpp
+++ src/VBox/Runtime/testcase/tstTSC.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstTSC.cpp 106945 2024-11-11 15:17:35Z knut.osmundsen@oracle.com $ */
+/* $Id: tstTSC.cpp 106947 2024-11-11 15:19:28Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase - SMP TSC testcase.
  */
@@ -109,7 +109,7 @@ static DECLCALLBACK(int) ThreadFunction(RTTHREAD Thread, void *pvUser);
 /** Wrapper around RTMpCpuId/ASMGetStuff. */
 static RTCPUID MyGetCpuId(void)
 {
-#if (!defined(RT_OS_LINUX) && !defined(RT_OS_DARWIN)) || defined(RT_ARCH_ARM64) || defined(RT_ARCH_ARM32)
+#if (!defined(RT_OS_LINUX) && !defined(RT_OS_DARWIN) && !defined(RT_OS_SOLARIS)) || defined(RT_ARCH_ARM64) || defined(RT_ARCH_ARM32)
     RTCPUID idCpu = RTMpCpuId();
     if (idCpu != NIL_RTCPUID)
         return idCpu;
@@ -476,7 +476,7 @@ int main(int argc, char **argv)
                 return 1;
 
             case 'V':
-                RTPrintf("$Revision: 106945 $\n");
+                RTPrintf("$Revision: 106947 $\n");
                 return 0;
 
             default:
