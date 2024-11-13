df4519838742b476de9005b9e033f356dff31e4d
Alexander Eichner <alexander.eichner@oracle.com>
2024-11-13 16:19:17 +0000

    VMM/NEMR3Native-win-armv8.cpp: Some cleanups, bugref:10392 [build fix]
    svn:sync-xref-src-repo-rev: r165928

--- src/VBox/VMM/VMMR3/NEMR3Native-win-armv8.cpp
+++ src/VBox/VMM/VMMR3/NEMR3Native-win-armv8.cpp
@@ -1,4 +1,4 @@
-/* $Id: NEMR3Native-win-armv8.cpp 107007 2024-11-13 15:01:58Z alexander.eichner@oracle.com $ */
+/* $Id: NEMR3Native-win-armv8.cpp 107011 2024-11-13 16:19:17Z alexander.eichner@oracle.com $ */
 /** @file
  * NEM - Native execution manager, native ring-3 Windows backend.
  *
@@ -2358,8 +2358,7 @@ nemR3WinHandleExitHypercall(PVMCC pVM, PVMCPUCC pVCpu, MY_WHV_RUN_VP_EXIT_CONTEX
 
     /** @todo What to do if immediate is != 0? */
 
-    if (   rcStrict == VINF_SUCCESS
-        && fAdvancePc)
+    if (rcStrict == VINF_SUCCESS)
         pVCpu->cpum.GstCtx.Pc.u64 += sizeof(uint32_t);
 
     return rcStrict;
