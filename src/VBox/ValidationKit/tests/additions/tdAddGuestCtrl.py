afb1079c2979980f93c2aae9bf9830ecacc15310
Andreas Loeffler <andreas.loeffler@oracle.com>
2024-11-28 12:36:30 +0000

    ValidationKit/tests/additions/tdAddGuestCtrl.py: Added support for VBoxService logging on NT4 / W2K guests by using VBoxGuestInstallHelper.exe as registry editor by default [pylint].
    svn:sync-xref-src-repo-rev: r166173

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -37,7 +37,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 107210 $"
+__version__ = "$Revision: 107212 $"
 
 # Standard Python imports.
 import errno
@@ -1748,8 +1748,9 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
             if self.oTstDrv.fpApiVer >= 7.0 and self.oTstDrv.uRevision >= 166162:
                 # Right now we ASSUME that the installation directory always is the following:
                 sRegEditorExePath     = 'C:\\Program Files\\Oracle\\VirtualBox Guest Additions\\VBoxGuestInstallHelper.exe';
-                asRegEditorArgs       = [ sRegEditorExePath, 'registry', 'write', 'HKLM', 'SYSTEM\\CurrentControlSet\\Services\\VBoxService',
-                                          'ImagePath', 'REG_SZ', '"' + sImagePath + '"' ];
+                asRegEditorArgs       = [ sRegEditorExePath, 'registry', 'write', 'HKLM',
+                                         'SYSTEM\\CurrentControlSet\\Services\\VBoxService', 'ImagePath', 'REG_SZ',
+                                         '"' + sImagePath + '"' ];
             # reg.exe is not able to write keys on older Windows versions (NT4, 2k).
             elif oTestVm.sKind not in ('WindowsNT4', 'Windows2000',):
                 sRegEditorExePath     = oTestVm.pathJoin(self.oTstDrv.getGuestSystemDir(oTestVm), 'reg.exe');
