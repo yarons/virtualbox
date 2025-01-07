12e4edbcf0770b2284478ce5269dd78861928931
Alexander Eichner <alexander.eichner@oracle.com>
2025-01-07 10:54:10 +0000

    Devices/PC/DevACPI.cpp: Add missing error check, and re-arrange code reading custom ACPI tables to actually return an error if the check fails and not continue regardless, bugref:3409
    svn:sync-xref-src-repo-rev: r166512

--- src/VBox/Devices/PC/DevACPI.cpp
+++ src/VBox/Devices/PC/DevACPI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevACPI.cpp 106061 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: DevACPI.cpp 107516 2025-01-07 10:54:10Z alexander.eichner@oracle.com $ */
 /** @file
  * DevACPI - Advanced Configuration and Power Interface (ACPI) Device.
  */
@@ -2328,6 +2328,7 @@ static int acpiR3MapPmIoPorts(PPDMDEVINS pDevIns, PACPISTATE pThis)
     rc = PDMDevHlpIoPortMap(pDevIns, pThis->hIoPortGpe0Sts, acpiR3CalcPmPort(pThis, GPE0_OFFSET));
     AssertRCReturn(rc, rc);
     rc = PDMDevHlpIoPortMap(pDevIns, pThis->hIoPortGpe0En,  acpiR3CalcPmPort(pThis, GPE0_OFFSET + GPE0_BLK_LEN / 2));
+    AssertRCReturn(rc, rc);
 
     return VINF_SUCCESS;
 }
@@ -3679,28 +3680,29 @@ static int acpiR3ReadCustomTable(PPDMDEVINS pDevIns, uint8_t **ppu8CustBin, uint
         if (RT_SUCCESS(rc))
         {
             /* The following checks should be in sync the AssertReleaseMsg's below. */
-            if (    *pcbCustBin > cbBufAvail
-                ||  *pcbCustBin < sizeof(ACPITBLHEADER))
-                rc = VERR_TOO_MUCH_DATA;
-
-            /*
-             * Allocate buffer for the custom table binary data.
-             */
-            *ppu8CustBin = (uint8_t *)PDMDevHlpMMHeapAlloc(pDevIns, *pcbCustBin);
-            if (*ppu8CustBin)
+            if (    *pcbCustBin <= cbBufAvail
+                &&  *pcbCustBin > sizeof(ACPITBLHEADER))
             {
-                rc = RTFileRead(FileCUSTBin, *ppu8CustBin, *pcbCustBin, NULL);
-                if (RT_FAILURE(rc))
+                /*
+                 * Allocate buffer for the custom table binary data.
+                 */
+                *ppu8CustBin = (uint8_t *)PDMDevHlpMMHeapAlloc(pDevIns, *pcbCustBin);
+                if (*ppu8CustBin)
                 {
-                    AssertMsgFailed(("RTFileRead(,,%d,NULL) -> %Rrc\n", *pcbCustBin, rc));
-                    PDMDevHlpMMHeapFree(pDevIns, *ppu8CustBin);
-                    *ppu8CustBin = NULL;
+                    rc = RTFileRead(FileCUSTBin, *ppu8CustBin, *pcbCustBin, NULL);
+                    if (RT_FAILURE(rc))
+                    {
+                        AssertMsgFailed(("RTFileRead(,,%d,NULL) -> %Rrc\n", *pcbCustBin, rc));
+                        PDMDevHlpMMHeapFree(pDevIns, *ppu8CustBin);
+                        *ppu8CustBin = NULL;
+                    }
                 }
+                else
+                    rc = VERR_NO_MEMORY;
             }
             else
-            {
-                rc = VERR_NO_MEMORY;
-            }
+                rc = VERR_TOO_MUCH_DATA;
+
             RTFileClose(FileCUSTBin);
         }
     }
