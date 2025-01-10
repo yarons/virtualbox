29d738d6ccbf58f8613f5734147463b05c5d7d83
Andreas Loeffler <andreas.loeffler@oracle.com>
2025-01-10 15:21:51 +0000

    src/VBox/Main/src-client/GuestSessionImplTasks.cpp: Fixed warnings found by Parfait (change of signedness on implicit conversion). Fixes vrc for Guest Additions Update success status. Needs backport. jiraref:VBP-1424
    svn:sync-xref-src-repo-rev: r166758

--- src/VBox/Main/src-client/GuestSessionImplTasks.cpp
+++ src/VBox/Main/src-client/GuestSessionImplTasks.cpp
@@ -1,4 +1,4 @@
-/* $Id: GuestSessionImplTasks.cpp 107735 2025-01-10 15:20:08Z andreas.loeffler@oracle.com $ */
+/* $Id: GuestSessionImplTasks.cpp 107737 2025-01-10 15:21:51Z andreas.loeffler@oracle.com $ */
 /** @file
  * VirtualBox Main - Guest session tasks.
  */
@@ -3450,7 +3450,7 @@ int GuestSessionTaskUpdateAdditions::Run(void)
                     if (RT_SUCCESS(vrc))
                     {
                         LogRel(("Guest Additions Update: Automatic update succeeded\n"));
-                        hrc = setProgressSuccess();
+                        vrc = setProgressSuccess();
                     }
                 }
             }
