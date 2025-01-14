a39588b69a3b44c554b8159eac23e93eacf7b353
Andreas Loeffler <andreas.loeffler@oracle.com>
2025-01-14 10:11:28 +0000

    src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp: Fixed warnings found by Parfait (unused assignment) [build fix]. jiraref:VBP-1424
    svn:sync-xref-src-repo-rev: r166855

--- src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageInfo.cpp 107814 2025-01-14 09:48:26Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxManageInfo.cpp 107818 2025-01-14 10:11:28Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxManage - The 'showvminfo' command and helper routines.
  */
@@ -1147,9 +1147,9 @@ HRESULT showVMInfo(ComPtr<IVirtualBox> pVirtualBox,
                 RTPrintf("UUID:            %s\n", Utf8Str(uuid).c_str());
             if (details != VMINFO_MACHINEREADABLE)
             {
-                Bstr settingsFilePath;
+                Bstr settingsFilePath("<Error>");
                 CHECK_ERROR(machine, COMGETTER(SettingsFilePath)(settingsFilePath.asOutParam()));
-                RTPrintf(Info::tr("Config file:     %ls\n"), SUCCEEDED(hrc) ? settingsFilePath.raw() : L"<Error>");
+                RTPrintf(Info::tr("Config file:     %ls\n"), settingsFilePath.raw());
 
                 Bstr strCipher;
                 Bstr strPasswordId;
