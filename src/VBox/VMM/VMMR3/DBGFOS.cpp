37a70aa0449de3557adba4c4e964c5d6c009923b
Alexander Eichner <alexander.eichner@oracle.com>
2025-01-14 09:48:34 +0000

    VMM/VMMR3/DBGFOS.cpp: Fix unused assignment parfait warning, bugref:3409
    svn:sync-xref-src-repo-rev: r166849

--- src/VBox/VMM/VMMR3/DBGFOS.cpp
+++ src/VBox/VMM/VMMR3/DBGFOS.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFOS.cpp 107692 2025-01-10 10:03:51Z alexander.eichner@oracle.com $ */
+/* $Id: DBGFOS.cpp 107815 2025-01-14 09:48:34Z alexander.eichner@oracle.com $ */
 /** @file
  * DBGF - Debugger Facility, Guest OS Diggers.
  */
@@ -156,9 +156,10 @@ void dbgfR3OSTermPart2(PUVM pUVM)
         if (pOS->pReg->pfnDestruct)
             pOS->pReg->pfnDestruct(pUVM, VMMR3GetVTable(), pOS->abData);
 
-        PDBGFOSEMTWRAPPER pFree = pOS->pWrapperHead;
-        while ((pFree = pOS->pWrapperHead) != NULL)
+        while (pOS->pWrapperHead != NULL)
         {
+            PDBGFOSEMTWRAPPER pFree = pOS->pWrapperHead;
+
             pOS->pWrapperHead = pFree->pNext;
             pFree->pNext = NULL;
             MMR3HeapFree(pFree);
