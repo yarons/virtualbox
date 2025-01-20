2dd04a5fa38ea1a74a5e0c69f15ee33983567918
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2025-01-20 23:36:53 +0000

    FE/Qt: Fix typo in X11 keyboard layout detection failure message.
    svn:sync-xref-src-repo-rev: r167067

--- src/VBox/Frontends/VirtualBox/src/platform/nix/XKeyboard-new.cpp
+++ src/VBox/Frontends/VirtualBox/src/platform/nix/XKeyboard-new.cpp
@@ -1,4 +1,4 @@
-/* $Id: XKeyboard-new.cpp 106061 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: XKeyboard-new.cpp 107970 2025-01-20 23:36:53Z klaus.espenlaub@oracle.com $ */
 /** @file
  * VBox Qt GUI - Implementation of Linux-specific keyboard functions.
  */
@@ -89,7 +89,7 @@ static void printKey(Display *pDisplay, int cKeys)
 static void dumpLayout(Display *pDisplay)
 {
     LogRel(("Your keyboard layout does not appear to be fully supported by\n"
-            "VirtualBox. If you are experiencing keyboard problems this.\n"
+            "VirtualBox. If you are experiencing keyboard problems this\n"
             "information may help us to resolve them.\n"
             "(Note: please tell us if you are using a custom layout.)\n\n"
             "The correct table for your layout is:\n"));
