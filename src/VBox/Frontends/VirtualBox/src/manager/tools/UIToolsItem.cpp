f2d9a0e57199e3b17bb5795b5058acf1a432e503
Sergey Dubov <sergey.dubov@oracle.com>
2025-01-29 12:25:50 +0000

    FE/Qt: bugref:10814: VBox Manager / Tools pane: More refactoring for item class, s.a. r167239 and r167242.
    svn:sync-xref-src-repo-rev: r167243

--- src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIToolsItem.cpp 108117 2025-01-29 12:00:56Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsItem.cpp 108118 2025-01-29 12:25:50Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsItem class definition.
  */
@@ -187,9 +187,9 @@ private:
 UIToolsItem::UIToolsItem(QGraphicsScene *pScene, const QIcon &icon,
                          UIToolClass enmClass, UIToolType enmType)
     : m_pScene(pScene)
+    , m_icon(icon)
     , m_enmClass(enmClass)
     , m_enmType(enmType)
-    , m_icon(icon)
     , m_fHovered(false)
     , m_pHoveringMachine(0)
     , m_pHoveringAnimationForward(0)
