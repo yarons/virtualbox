a0161ecba2d127473aec5b53554264b7e8a09b75
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2025-02-03 14:12:04 +0000

    Additions: Linux: vboxsf: Introduce initial support for kernel 6.14, bugref:10854.
    svn:sync-xref-src-repo-rev: r167298

--- src/VBox/Additions/linux/sharedfolders/utils.c
+++ src/VBox/Additions/linux/sharedfolders/utils.c
@@ -1,4 +1,4 @@
-/* $Id: utils.c 107202 2024-11-27 11:59:47Z vadim.galitsyn@oracle.com $ */
+/* $Id: utils.c 108170 2025-02-03 14:12:04Z vadim.galitsyn@oracle.com $ */
 /** @file
  * vboxsf - VBox Linux Shared Folders VFS, utility functions.
  *
@@ -1162,7 +1162,11 @@ int vbsf_path_from_dentry(struct vbsf_super_info *pSuperInfo, struct vbsf_inode_
  *
  * @note Caller holds no relevant locks, just a dentry reference.
  */
-#if RTLNX_VER_MIN(3,6,0)
+
+#if RTLNX_VER_MIN(6,14,0)
+static int vbsf_dentry_revalidate(struct inode *parentDir, const struct qstr *parentName,
+                                  struct dentry *dentry, unsigned flags)
+#elif RTLNX_VER_MIN(3,6,0)
 static int vbsf_dentry_revalidate(struct dentry *dentry, unsigned flags)
 #elif RTLNX_VER_MIN(2,6,0)
 static int vbsf_dentry_revalidate(struct dentry *dentry, struct nameidata *nd)
@@ -1170,6 +1174,10 @@ static int vbsf_dentry_revalidate(struct dentry *dentry, struct nameidata *nd)
 static int vbsf_dentry_revalidate(struct dentry *dentry, int flags)
 #endif
 {
+#if RTLNX_VER_MIN(6,14,0)
+    /* Optional, not in use currently. */
+    RT_NOREF(parentDir, parentName);
+#endif
 #if RTLNX_VER_RANGE(2,6,0,  3,6,0)
     int const flags = nd ? nd->flags : 0;
 #endif
