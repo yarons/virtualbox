c535ce7d09572077e0dccfdd57d96abc3b110723
Sergey Dubov <sergey.dubov@oracle.com>
2025-02-03 17:33:51 +0000

    FE/Qt: bugref:10814: VBox Manager / Tools pane: Fixes for tool item icon and text location calculations.
    svn:sync-xref-src-repo-rev: r167308

--- src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIToolsItem.cpp 108178 2025-02-03 17:32:52Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsItem.cpp 108179 2025-02-03 17:33:51Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsItem class definition.
  */
@@ -846,8 +846,8 @@ void UIToolsItem::paintToolInfo(QPainter *pPainter, const QRect &rectangle) cons
     /* Paint left column: */
     {
         /* Prepare variables: */
-        int iPixmapX = model()->tools()->isPopup() ? iMargin : 2 * iMargin;
-        int iPixmapY = (iFullHeight - m_pixmap.height() / m_pixmap.devicePixelRatio()) / 2;
+        const int iPixmapX = model()->tools()->isPopup() ? iMargin : 2 * iMargin;
+        const int iPixmapY = (iFullHeight - m_pixmap.height() / m_pixmap.devicePixelRatio()) / 2;
         /* Paint pixmap: */
         paintPixmap(/* Painter: */
                     pPainter,
@@ -860,10 +860,12 @@ void UIToolsItem::paintToolInfo(QPainter *pPainter, const QRect &rectangle) cons
     /* Paint right column: */
     {
         /* Prepare variables: */
-        int iNameX = iMargin + m_pixmapSize.width() + iSpacing;
-        int iNameY = (iFullHeight - m_nameSize.height()) / 2;
-        /* Paint name: */
-        if (model()->tools()->isPopup())
+        const int iNameX = model()->tools()->isPopup() ? iMargin + m_pixmapSize.width() + iSpacing
+                                                       : 2 * iMargin + m_pixmapSize.width() + iSpacing;
+        const int iNameY = (iFullHeight - m_nameSize.height()) / 2;
+        /* Paint name (always for popup mode, if requested otherwise): */
+        if (   model()->tools()->isPopup()
+            || model()->showItemNames())
             paintText(/* Painter: */
                       pPainter,
                       /* Point to paint in: */
