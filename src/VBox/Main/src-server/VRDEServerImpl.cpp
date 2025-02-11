1c4fc912bf4a6092e5fd1a7a91d1faa11f82793a
Samantha Scholz <samantha.scholz@oracle.com>
2025-02-11 01:05:15 +0000

    Main/src-server: removed VRDE server certificate generation from getVRDEProperty() ticketref:22259 bugref:10310
    svn:sync-xref-src-repo-rev: r167459

--- src/VBox/Main/src-server/VRDEServerImpl.cpp
+++ src/VBox/Main/src-server/VRDEServerImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VRDEServerImpl.cpp 106164 2024-09-26 10:42:10Z knut.osmundsen@oracle.com $ */
+/* $Id: VRDEServerImpl.cpp 108310 2025-02-11 01:05:15Z samantha.scholz@oracle.com $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -650,21 +650,6 @@ HRESULT VRDEServer::setVRDEProperty(const com::Utf8Str &aKey, const com::Utf8Str
 
 HRESULT VRDEServer::getVRDEProperty(const com::Utf8Str &aKey, com::Utf8Str &aValue)
 {
-    /* Special case for the server certificate because it might be created automatically by the code below. */
-    if (   aKey == "Security/ServerCertificate"
-        || aKey == "Security/ServerPrivateKey")
-    {
-        BOOL certificateGenerated = false;
-        HRESULT hrc = i_certificateRepair(certificateGenerated);
-        if (FAILED(hrc))
-            LogRel((("Failed to auto generate server key and certificate: (%Rhrc)\n"), hrc));
-        else if (certificateGenerated)
-        {
-            AutoWriteLock mlock(mParent COMMA_LOCKVAL_SRC_POS);
-            mParent->i_setModified(Machine::IsModified_VRDEServer);
-            mlock.release();
-        }
-    }
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
     settings::StringsMap::const_iterator it = mData->mapProperties.find(aKey);
     if (it != mData->mapProperties.end())
