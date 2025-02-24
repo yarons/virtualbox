5d68b61effdf24312c5ff15756c886e3e90a92f7
Sergey Dubov <sergey.dubov@oracle.com>
2025-02-24 16:38:35 +0000

    FE/Qt: bugref:10814: VBox Manager / Tools pane: Backing out r167713 as I don't like the result.
    svn:sync-xref-src-repo-rev: r167719

--- src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIToolsItem.cpp 108532 2025-02-24 16:22:55Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsItem.cpp 108534 2025-02-24 16:38:35Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsItem class definition.
  */
@@ -840,11 +840,10 @@ void UIToolsItem::paintBackground(QPainter *pPainter, const QRect &rectangle) co
             /* Prepare icon sub-rect: */
             QRect subRect;
             subRect.setHeight(m_pixmap.height() / m_pixmap.devicePixelRatio() + iPadding * 2);
+            subRect.setWidth(subRect.height());
 #ifdef VBOX_WS_MAC
-            subRect.setWidth(rectangle.width() - iIndent - 4 * iMargin + 2 * iPadding);
             subRect.moveTopLeft(rectangle.topLeft() + QPoint(iIndent + 2 * iMargin - iPadding, iMargin - iPadding));
 #else
-            subRect.setWidth(rectangle.width() - iIndent - 3 * iMargin + 2 * iPadding);
             subRect.moveTopLeft(rectangle.topLeft() + QPoint(iIndent + 1.5 * iMargin - iPadding, iMargin - iPadding));
 #endif
 
