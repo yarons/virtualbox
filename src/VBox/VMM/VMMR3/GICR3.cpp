b4af041a83b339ede729c170176ff00ef745bd4f
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2025-03-12 07:31:50 +0000

    VMM/GIC: bugref:10404 Added a missing undefine.
    svn:sync-xref-src-repo-rev: r167914

--- src/VBox/VMM/VMMR3/GICR3.cpp
+++ src/VBox/VMM/VMMR3/GICR3.cpp
@@ -1,4 +1,4 @@
-/* $Id: GICR3.cpp 108714 2025-03-12 07:27:42Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: GICR3.cpp 108715 2025-03-12 07:31:50Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * GIC - Generic Interrupt Controller Architecture (GIC).
  */
@@ -726,6 +726,7 @@ DECLCALLBACK(int) gicR3Construct(PPDMDEVINS pDevIns, int iInstance, PCFGMNODE pC
         GIC_PROF_COUNTER(&pGicCpu->StatProfSetPpiR3,  "%u/Prof/SetPpi",  "Profiling of set PPI callback in R3.");
         GIC_PROF_COUNTER(&pGicCpu->StatProfSetSgiR3,  "%u/Prof/SetSgi",  "Profiling of SGIs generated in R3.");
     }
+# undef GIC_REG_COUNTER
 # undef GIC_PROF_COUNTER
 #endif
 
