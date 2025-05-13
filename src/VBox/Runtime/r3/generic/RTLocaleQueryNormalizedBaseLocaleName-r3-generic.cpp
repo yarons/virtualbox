beba908301e48b2a6def4bb351caf016d5bc03d5
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2025-05-13 12:26:56 +0000

    IPRT/RTLocaleQueryNormalizedBaseLocaleName-r3-generic.cpp: Shut up stupid asan warning.
    svn:sync-xref-src-repo-rev: r168824

--- src/VBox/Runtime/r3/generic/RTLocaleQueryNormalizedBaseLocaleName-r3-generic.cpp
+++ src/VBox/Runtime/r3/generic/RTLocaleQueryNormalizedBaseLocaleName-r3-generic.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTLocaleQueryNormalizedBaseLocaleName-r3-generic.cpp 106061 2024-09-16 14:03:52Z knut.osmundsen@oracle.com $ */
+/* $Id: RTLocaleQueryNormalizedBaseLocaleName-r3-generic.cpp 109515 2025-05-13 12:26:56Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - RTLocaleQueryNormalizedBaseLocaleName, ring-3 generic.
  */
@@ -70,7 +70,7 @@ RTDECL(int) RTLocaleQueryNormalizedBaseLocaleName(char *pszName, size_t cbName)
             pszLocale++;
         bool fSeenC = false;
         bool fSeenPOSIX = false;
-        do
+        for (;;)
         {
             const char *pszEnd = strchr(pszLocale, ';');
 
@@ -89,8 +89,13 @@ RTDECL(int) RTLocaleQueryNormalizedBaseLocaleName(char *pszName, size_t cbName)
                 fSeenPOSIX = true;
 
             /* advance */
-            pszLocale = pszEnd ? strchr(pszEnd + 1, '=') : NULL;
-        } while (pszLocale++);
+            if (!pszEnd)
+                break;
+            pszLocale = strchr(pszEnd + 1, '=');
+            if (!pszLocale)
+                break;
+            pszLocal++;
+        }
 
         if (fSeenC || fSeenPOSIX)
             return RTStrCopy(pszName, cbName, "C"); /* C and POSIX should be identical IIRC, so keep it simple. */
