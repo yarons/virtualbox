2475b2e337495f6d1e4811184fba700ff71dad48
Brent Paulson <brent.paulson@oracle.com>
2025-07-14 20:46:18 +0000

    ValidationKit/tests/api/{Makefile.kmk,tdImportExport1.py}: Add a new test case which exercises importing and exporting an EFI-based VM and verifies that the VM's NVRAM file is included in the OVF appliance upon export and in the VM's machine folder upon import.  bugref:10931
    svn:sync-xref-src-repo-rev: r169655

--- src/VBox/ValidationKit/tests/api/tdApi1.py
+++ src/VBox/ValidationKit/tests/api/tdApi1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdApi1.py 109051 2025-04-03 11:06:18Z brent.paulson@oracle.com $
+# $Id: tdApi1.py 110219 2025-07-14 20:46:18Z brent.paulson@oracle.com $
 
 """
 VirtualBox Validation Kit - API Test wrapper #1 combining all API sub-tests
@@ -37,7 +37,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 109051 $"
+__version__ = "$Revision: 110219 $"
 
 
 # Standard Python imports.
@@ -96,6 +96,7 @@ if __name__ == '__main__':
     from tdMoveVm1       import SubTstDrvMoveVm1;     # pylint: disable=relative-import
     from tdCloneMedium1  import SubTstDrvCloneMedium1;# pylint: disable=relative-import
     from tdSnapshots1    import SubTstDrvNestedLiveSnapshots1;# pylint: disable=relative-import
+    from tdImportExport1 import SubTstDrvImportExportEFIVM1;  # pylint: disable=relative-import
     sys.exit(tdApi1([SubTstDrvPython1, SubTstDrvAppliance1, SubTstDrvMoveMedium1,
                      SubTstDrvTreeDepth1, SubTstDrvMoveVm1, SubTstDrvCloneMedium1,
-                     SubTstDrvNestedLiveSnapshots1]).main(sys.argv))
+                     SubTstDrvNestedLiveSnapshots1, SubTstDrvImportExportEFIVM1]).main(sys.argv))
