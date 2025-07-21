407f44151886460b4178fcde8c4fb63f803801aa
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2025-07-21 10:05:19 +0000

    Devices/Graphics: disable D3D11.1 features when using dxvk
    svn:sync-xref-src-repo-rev: r169790

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx-dx11.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx-dx11.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA3d-dx-dx11.cpp 110302 2025-07-18 10:50:07Z alexander.eichner@oracle.com $ */
+/* $Id: DevVGA-SVGA3d-dx-dx11.cpp 110335 2025-07-21 10:05:19Z vitali.pelenjow@oracle.com $ */
 /** @file
  * DevVMWare - VMWare SVGA device
  */
@@ -3887,8 +3887,12 @@ static DECLCALLBACK(int) vmsvga3dBackQueryCaps(PVGASTATECC pThisCC, SVGA3dDevCap
         *pu32Val = VBSVGA3D_CAP_3D;
         if (pState->pBackend->dxDevice.pVideoDevice)
             *pu32Val |= VBSVGA3D_CAP_VIDEO;
+#if !defined(VBOX_WITH_DXVK)
+        /* D3D11_RASTERIZER_DESC1::ForcedSampleCount does not work with dxvk (at least up to 2.6.2).
+         * Therefore only 11.0 level can be supported in the guest. */
         if (FeatureLevel >= D3D_FEATURE_LEVEL_11_1)
             *pu32Val |= VBSVGA3D_CAP_RASTERIZER_STATE_V2;
+#endif
         break;
 
     case SVGA3D_DEVCAP_MAX_LIGHTS:
