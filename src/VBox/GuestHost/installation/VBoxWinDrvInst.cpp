b342609d42e73e4bf74bdb2b5c60973d457d25aa
Andreas Loeffler <andreas.loeffler@oracle.com>
2025-08-05 16:03:21 +0000

    Windows driver installation: Added more fine-grained installation steps and a fallback in vboxWinDrvInstallInfSectionEx(). This should fix the WinXP mouse driver installation issues [spaces]. github:gh-115
    svn:sync-xref-src-repo-rev: r170056

--- src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
+++ src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxWinDrvInst.cpp 110562 2025-08-05 15:56:52Z andreas.loeffler@oracle.com $ */
+/* $Id: VBoxWinDrvInst.cpp 110565 2025-08-05 16:03:21Z andreas.loeffler@oracle.com $ */
 /** @file
  * VBoxWinDrvInst - Windows driver installation handling.
  */
@@ -931,10 +931,10 @@ static int vboxWinDrvInstallInfSectionEx(PVBOXWINDRVINSTINTERNAL pCtx, HINF hInf
          * This gives us more flexibility and control compared to the InstallHinfSectionW() fallback below. */
         struct
         {
-            UINT                uFlags;
-            UINT                uCopyFlags;
+            UINT  uFlags;
+            UINT  uCopyFlags;
             PVOID pCallback;
-            PVOID               pContext;
+            PVOID pContext;
         } s_aFromInfSectionInstall[] =
         {
             /* Install files. */
