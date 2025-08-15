73c4cdcefe29f607d6d98c7722b4e60fe12f6dfc
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2025-08-15 14:28:01 +0000

    Main/tstSettings: Merged in changes from 7.0/r170229 to trunk. bugref:10952
    svn:sync-xref-src-repo-rev: r170247

--- src/VBox/Main/testcase/tstSettings.cpp
+++ src/VBox/Main/testcase/tstSettings.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstSettings.cpp 110684 2025-08-11 17:18:47Z klaus.espenlaub@oracle.com $ */
+/* $Id: tstSettings.cpp 110736 2025-08-15 14:28:01Z knut.osmundsen@oracle.com $ */
 /** @file
  * Settings testcases - No Main API involved.
  */
@@ -45,73 +45,116 @@
 using namespace com;
 using namespace settings;
 
-/** The current logging verbosity level. */
-static unsigned  g_uVerbosity = 0;
-
 
 /**
  * Tests a single .vbox machine configuration file.
  *
- * @param   strFileSrc          Absolute path of machine configuration file to test.
- *
- * @note    The source configuration file will not be modified (i.e. written to disk).
+ * @param   pszSrcFile  The source XML file (readonly).
+ * @param   pszOutput   Optional output file. If NULL, a temporary file is used.
  */
-static void tstFileSingle(const Utf8Str &strFileSrc)
+static void tstFileMachine(const char *pszSrcFile, const char *pszOutput)
 {
-    RTTestIPrintf(RTTESTLVL_ALWAYS, "Testing file: %s\n", strFileSrc.c_str());
-
+    RTTestIPrintf(RTTESTLVL_ALWAYS, "Testing machine file: %s\n", pszSrcFile);
     char szFileDst[RTPATH_MAX] = { 0 };
 
     try
     {
+        Utf8Str const     strFileSrc(pszSrcFile);
         MachineConfigFile fileSrc(&strFileSrc);
-        if (fileSrc == fileSrc) /* Only have a == operator. */
-        {
-        }
-        else
-            throw "Source file doesn't match itself! == operator bug!?";
-
-        RTTestIPrintf(RTTESTLVL_ALWAYS, "Source file version is: v%u\n", fileSrc.getSettingsVersion());
+        RTTestIPrintf(RTTESTLVL_ALWAYS, "Successfully read in the file. Version: %u\n", fileSrc.getSettingsVersion());
+        RTTESTI_CHECK(fileSrc == fileSrc /* Only have a == operator. */);
 
         RTTESTI_CHECK_RC_OK_RETV(RTPathTemp(szFileDst, sizeof(szFileDst)));
         RTTESTI_CHECK_RC_OK_RETV(RTPathAppend(szFileDst, sizeof(szFileDst), "tstSettings-XXXXXX.vbox"));
         RTTESTI_CHECK_RC_OK_RETV(RTFileCreateTemp(szFileDst, 0600));
 
-#ifdef DEBUG_andy
-        RTStrPrintf(szFileDst, sizeof(szFileDst), "/tmp/out.vbox");
-#endif
         /* Write source file to a temporary destination file. */
+        if (!pszOutput)
+        {
+            RTTESTI_CHECK_RC_OK_RETV(RTPathTemp(szFileDst, sizeof(szFileDst)));
+            RTTESTI_CHECK_RC_OK_RETV(RTPathAppend(szFileDst, sizeof(szFileDst), "tstSettings-XXXXXX.vbox"));
+            RTTESTI_CHECK_RC_OK_RETV(RTFileCreateTemp(szFileDst, 0600));
+            pszOutput = szFileDst;
+        }
         fileSrc.write(szFileDst);
         RTTestIPrintf(RTTESTLVL_DEBUG, "Destination file written to: %s\n", szFileDst);
 
         try
         {
             /* Load destination file to see if it works. */
-            Utf8Str strFileDst(szFileDst);
+            Utf8Str const     strFileDst(pszOutput);
             MachineConfigFile fileDst(&strFileDst);
-            RTTestIPrintf(RTTESTLVL_ALWAYS, "Destination file version is: v%u\n", fileDst.getSettingsVersion());
-            if (fileSrc == fileDst) /* Only have a == operator. */
-            {
-            }
-            else
-                throw std::runtime_error("Source and destination files don't match!");
+            RTTestIPrintf(RTTESTLVL_ALWAYS, "Successfully read the file we wrote. Version: %u\n", fileDst.getSettingsVersion());
+            RTTESTI_CHECK(fileSrc == fileDst /* Only have a == operator. */);
         }
         catch (const std::exception &err)
         {
-            RTTestIFailed("Testing destination file failed: %s\n", err.what());
+            RTTestIFailed("Exception caught when re-reading: %s\n", err.what());
         }
     }
     catch (const std::exception &err)
     {
-        RTTestIFailed("Testing source file failed: %s\n", err.what());
+        RTTestIFailed("Exception caught: %s\n", err.what());
     }
 
-#ifndef DEBUG_andy
     /* Clean up. */
-    if (strlen(szFileDst))
-        RTTESTI_CHECK_RC_OK_RETV(RTFileDelete(szFileDst));
-#endif
+    if (szFileDst[0])
+        RTTESTI_CHECK_RC_OK(RTFileDelete(szFileDst));
 }
+
+
+/**
+ * Tests a single VirtualBox.xml configuration file.
+ *
+ * @param   pszSrcFile  The source XML file (readonly).
+ * @param   pszOutput   Optional output file. If NULL, a temporary file is used.
+ */
+static void tstFileMain(const char *pszSrcFile, const char *pszOutput)
+{
+    RTTestIPrintf(RTTESTLVL_ALWAYS, "Testing main file: %s\n", pszSrcFile);
+    char szFileDst[RTPATH_MAX] = { 0 };
+
+    try
+    {
+        Utf8Str const  strFileSrc(pszSrcFile);
+        MainConfigFile fileSrc(&strFileSrc);
+        RTTestIPrintf(RTTESTLVL_ALWAYS, "Successfully read in the file. Version: %u\n", fileSrc.getSettingsVersion());
+        //RTTESTI_CHECK(fileSrc == fileSrc /* Only have a == operator. */); - no comparison operator for MainConfigFile
+
+        /* Write source file to a temporary destination file. */
+        if (!pszOutput)
+        {
+            RTTESTI_CHECK_RC_OK_RETV(RTPathTemp(szFileDst, sizeof(szFileDst)));
+            RTTESTI_CHECK_RC_OK_RETV(RTPathAppend(szFileDst, sizeof(szFileDst), "tstSettings-XXXXXX.vbox"));
+            RTTESTI_CHECK_RC_OK_RETV(RTFileCreateTemp(szFileDst, 0600));
+            pszOutput = szFileDst;
+        }
+        fileSrc.write(pszOutput);
+        RTTestIPrintf(RTTESTLVL_ALWAYS, "Successfully wrote it out: %s\n", pszOutput);
+
+        try
+        {
+            /* Load destination file to see if it works. */
+            Utf8Str const  strFileDst(pszOutput);
+            MainConfigFile fileDst(&strFileDst);
+            RTTestIPrintf(RTTESTLVL_ALWAYS, "Successfully read the file we wrote. Version: %u\n", fileDst.getSettingsVersion());
+            //RTTESTI_CHECK(fileSrc == fileDst /* Only have a == operator. */); - no comparison operator for MainConfigFile
+        }
+        catch (const std::exception &err)
+        {
+            RTTestIFailed("Exception caught when re-reading: %s\n", err.what());
+        }
+    }
+    catch (const std::exception &err)
+    {
+        RTTestIFailed("Exception caught: %s\n", err.what());
+    }
+
+    /* Clean up. */
+    if (szFileDst[0])
+        RTTESTI_CHECK_RC_OK(RTFileDelete(szFileDst));
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -120,20 +163,24 @@ int main(int argc, char *argv[])
     if (rcExit != RTEXITCODE_SUCCESS)
         return rcExit;
     RTTestBanner(hTest);
-
+                                                  //-r162547=2.9.14; r162738=2.12.6; r164446=2.13.2
     /*
      * Process options.
      */
     static const RTGETOPTDEF aOpts[] =
     {
-        { "--verbose",             'v',               RTGETOPT_REQ_STRING },
+        { "--output",              'o',               RTGETOPT_REQ_STRING },
+        { "--machine",             'm',               RTGETOPT_REQ_NOTHING },
+        { "--virtualbox-xml",      'M',               RTGETOPT_REQ_NOTHING },
     };
 
     RTGETOPTSTATE GetState;
     int rc = RTGetOptInit(&GetState, argc, argv, aOpts, RT_ELEMENTS(aOpts), 1 /*idxFirst*/, 0 /*fFlags - must not sort! */);
     AssertRCReturn(rc, RTEXITCODE_INIT);
 
-    unsigned      cFiles = 0;
+    unsigned      cFiles      = 0;
+    bool          fMachineXml = true;
+    const char   *pszOutput   = NULL;
 
     int           ch;
     RTGETOPTUNION ValueUnion;
@@ -141,16 +188,26 @@ int main(int argc, char *argv[])
     {
         switch (ch)
         {
-            case 'v':
-                g_uVerbosity++;
+            case 'o':
+                pszOutput = ValueUnion.psz;
+                if (!*pszOutput)
+                    pszOutput = NULL;
+                break;
+
+            case 'm':
+                fMachineXml = true;
+                break;
+            case 'M':
+                fMachineXml = false;
                 break;
 
             case VINF_GETOPT_NOT_OPTION:
-            {
                 cFiles++;
-                tstFileSingle(ValueUnion.psz);
+                if (fMachineXml)
+                    tstFileMachine(ValueUnion.psz, pszOutput);
+                else
+                    tstFileMain(ValueUnion.psz, pszOutput);
                 break;
-            }
 
             default:
                 return RTGetOptPrintError(ch, &ValueUnion);
@@ -159,7 +216,6 @@ int main(int argc, char *argv[])
 
     if (!cFiles)
         return RTTestSkipAndDestroy(hTest, "At least one .vbox machine file must be specified to test!\n");
-
     return RTTestSummaryAndDestroy(hTest);
 }
 
