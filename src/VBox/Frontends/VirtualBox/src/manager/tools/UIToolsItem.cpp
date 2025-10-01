fd508a443aa3287b0853e88f77d91a0565ec32de
Sergey Dubov <sergey.dubov@oracle.com>
2025-10-01 11:15:15 +0000

    FE/Qt: bugref:10977: VBox Manager / Tools pane: Cleanup for Tools item; For now just reordering code, no other changes there.
    svn:sync-xref-src-repo-rev: r170736

--- src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIToolsItem.cpp 111197 2025-10-01 11:07:45Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsItem.cpp 111198 2025-10-01 11:15:15Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsItem class definition.
  */
@@ -68,6 +68,16 @@ public:
         : QAccessibleObject(pObject)
     {}
 
+    /** Returns the role. */
+    virtual QAccessible::Role role() const RT_OVERRIDE
+    {
+        /* Make sure item still alive: */
+        AssertPtrReturn(item(), QAccessible::NoRole);
+
+        /* ListItem by default: */
+        return QAccessible::ListItem;
+    }
+
     /** Returns the parent. */
     virtual QAccessibleInterface *parent() const RT_OVERRIDE
     {
@@ -76,6 +86,18 @@ public:
 
         /* Return the parent: */
         return QAccessible::queryAccessibleInterface(item()->model()->view());
+    }
+
+    /** Returns the rect. */
+    virtual QRect rect() const RT_OVERRIDE
+    {
+        /* Now goes the mapping: */
+        const QSize   itemSize         = item()->size().toSize();
+        const QPointF itemPosInScene   = item()->mapToScene(QPointF(0, 0));
+        const QPoint  itemPosInView    = item()->model()->view()->mapFromScene(itemPosInScene);
+        const QPoint  itemPosInScreen  = item()->model()->view()->mapToGlobal(itemPosInView);
+        const QRect   itemRectInScreen = QRect(itemPosInScreen, itemSize);
+        return itemRectInScreen;
     }
 
     /** Returns the number of children. */
@@ -110,45 +132,6 @@ public:
         return -1;
     }
 
-    /** Returns the rect. */
-    virtual QRect rect() const RT_OVERRIDE
-    {
-        /* Now goes the mapping: */
-        const QSize   itemSize         = item()->size().toSize();
-        const QPointF itemPosInScene   = item()->mapToScene(QPointF(0, 0));
-        const QPoint  itemPosInView    = item()->model()->view()->mapFromScene(itemPosInScene);
-        const QPoint  itemPosInScreen  = item()->model()->view()->mapToGlobal(itemPosInView);
-        const QRect   itemRectInScreen = QRect(itemPosInScreen, itemSize);
-        return itemRectInScreen;
-    }
-
-    /** Returns a text for the passed @a enmTextRole. */
-    virtual QString text(QAccessible::Text enmTextRole) const RT_OVERRIDE
-    {
-        /* Make sure item still alive: */
-        AssertPtrReturn(item(), QString());
-
-        switch (enmTextRole)
-        {
-            case QAccessible::Name:        return item()->name();
-            case QAccessible::Description: return item()->description();
-            default: break;
-        }
-
-        /* Null-string by default: */
-        return QString();
-    }
-
-    /** Returns the role. */
-    virtual QAccessible::Role role() const RT_OVERRIDE
-    {
-        /* Make sure item still alive: */
-        AssertPtrReturn(item(), QAccessible::NoRole);
-
-        /* ListItem by default: */
-        return QAccessible::ListItem;
-    }
-
     /** Returns the state. */
     virtual QAccessible::State state() const RT_OVERRIDE
     {
@@ -170,6 +153,23 @@ public:
 
         /* Return the state: */
         return state;
+    }
+
+    /** Returns a text for the passed @a enmTextRole. */
+    virtual QString text(QAccessible::Text enmTextRole) const RT_OVERRIDE
+    {
+        /* Make sure item still alive: */
+        AssertPtrReturn(item(), QString());
+
+        switch (enmTextRole)
+        {
+            case QAccessible::Name:        return item()->name();
+            case QAccessible::Description: return item()->description();
+            default: break;
+        }
+
+        /* Null-string by default: */
+        return QString();
     }
 
 private:
