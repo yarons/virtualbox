67f2b0ded1e55479638b0a1f5e83223d5bea23ef
Sergey Dubov <sergey.dubov@oracle.com>
2025-10-03 14:20:57 +0000

    FE/Qt: bugref:10977: VBox Manager / Tools pane: No strong focus, only Tab one; We don't want this toolbar to be focused by mouse-clicks.
    svn:sync-xref-src-repo-rev: r170778

--- src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsView.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsView.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIToolsView.cpp 111228 2025-10-03 11:08:48Z sergey.dubov@oracle.com $ */
+/* $Id: UIToolsView.cpp 111239 2025-10-03 14:20:57Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UIToolsView class implementation.
  */
@@ -322,6 +322,7 @@ void UIToolsView::prepareThis()
     model()->setView(this);
 
     /* Set minimum size-hint policy: */
+    setFocusPolicy(Qt::TabFocus);
     setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
 
     /* Setup frame: */
