5fee45f40946c7796c8a224ff82597a332444b2b
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2025-10-06 07:14:06 +0000

    VMM/APIC: bugref:9993 Build fix.
    svn:sync-xref-src-repo-rev: r170786

--- src/VBox/VMM/VMMAll/APICAll.cpp
+++ src/VBox/VMM/VMMAll/APICAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: APICAll.cpp 111246 2025-10-06 07:09:04Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: APICAll.cpp 111247 2025-10-06 07:14:06Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * APIC - Advanced Programmable Interrupt Controller - All Contexts.
  */
@@ -78,7 +78,7 @@ static const uint32_t g_au32LvtExtValidMask[] =
  * Instantiate the APIC all-context common code.
  */
 #define VMM_APIC_TEMPLATE_ALL_COMMON
-#ifdef LOG_ENABLED
+#ifdef RT_STRICT
 /* We currently only use the info functions in debug builds (log, debug asserts). */
 # define VMM_APIC_TEMPLATE_USES_INFO_FUNCS
 #endif
