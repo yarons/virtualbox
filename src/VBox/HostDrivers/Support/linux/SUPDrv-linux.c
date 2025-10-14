10d4e1b4ecbfa6e411eb9fe547392d1daf07efe8
Ramshankar Venkataraman <ramshankar.venkataraman@oracle.com>
2025-10-14 16:01:27 +0000

    HostDrivers/Support: bugref:10963 bugref:10991 The KVM virtualization API is no longer exported starting with 6.18.0.
    svn:sync-xref-src-repo-rev: r170950

--- src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
+++ src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: SUPDrv-linux.c 111342 2025-10-13 12:03:00Z ramshankar.venkataraman@oracle.com $ */
+/* $Id: SUPDrv-linux.c 111393 2025-10-14 16:01:27Z ramshankar.venkataraman@oracle.com $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Linux specifics.
  */
@@ -79,7 +79,7 @@
 # include <iprt/asm-amd64-x86.h>
 #endif
 
-#if (RTLNX_VER_MIN(6,16,0)) && defined(CONFIG_KVM_GENERIC_HARDWARE_ENABLING) && defined(VBOX_WITH_HOST_VMX)
+#if (RTLNX_VER_RANGE(6, 16, 0, 6, 18, 0)) && defined(CONFIG_KVM_GENERIC_HARDWARE_ENABLING) && defined(VBOX_WITH_HOST_VMX)
 # if defined(RT_ARCH_AMD64) || defined(RT_ARCH_X86)
 #  include <linux/kvm_host.h>
 #  define SUPDRV_LINUX_HAS_KVM_HWVIRT_API
