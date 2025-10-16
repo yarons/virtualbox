8e68163d7b55a2d6098ba0fc4a62d826e513a29c
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2025-10-16 09:44:17 +0000

    VirtioNet: ​bugref:10971 Skip zero-length segments
    svn:sync-xref-src-repo-rev: r170989

--- src/VBox/Devices/Network/DevVirtioNet.cpp
+++ src/VBox/Devices/Network/DevVirtioNet.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVirtioNet.cpp 110684 2025-08-11 17:18:47Z klaus.espenlaub@oracle.com $ */
+/* $Id: DevVirtioNet.cpp 111430 2025-10-16 09:44:17Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * VBox storage devices - Virtio NET Driver
  *
@@ -2891,10 +2891,14 @@ static int virtioNetR3TransmitPkts(PPDMDEVINS pDevIns, PVIRTIONET pThis, PVIRTIO
                     cbCopied = RT_MIN((uint64_t)cbRemain, srcSgLen - (srcSgCur - srcSgStart));
                     /*
                      * Guest sent a bogus S/G chain, there doesn't seem to be a way to report an error but
-                     * as this shouldn't happen anyway we just stop proccessing this chain.
+                     * as this shouldn't happen anyway we just skip this zero-length segment.
                      */
                     if (RT_UNLIKELY(!cbCopied))
-                        break;
+                    {
+                        virtioCoreGCPhysChainGetNextSeg(pSgPhysSend, &cbCopied);
+                        LogFunc((".... Skipped zero-length segment.\n"));
+                        continue;
+                    }
                     virtioCoreGCPhysRead(pVirtio, pDevIns,
                                          (RTGCPHYS)pSgPhysSend->GCPhysCur,
                                          ((uint8_t *)pSgBufToPdmLeafDevice->aSegs[0].pvSeg) + uOffset, cbCopied);
